91ed42f6e7bd51baf5f81cf8dd1b20ff57c2403a
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2019-09-12 13:31:59 -0700

    add a bit more into README
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -4,7 +4,6 @@ echo 'Uninstalling stuff'
 pip uninstall -y nemo_toolkit
 pip uninstall -y nemo_asr
 pip uninstall -y nemo_nlp
-pip uninstall -y nemo_lpr
 pip uninstall -y nemo_simple_gan
 
 echo 'Installing stuff'
@@ -13,8 +12,6 @@ python setup.py develop
 cd ../collections/nemo_asr
 python setup.py develop
 cd ../nemo_nlp
-python setup.py develop
-cd ../nemo_lpr
 python setup.py develop
 cd ../nemo_simple_gan
 python setup.py develop
