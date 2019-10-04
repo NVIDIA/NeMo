2e68a3eab58bd851d45e42d5087163c48ced7479
Hoo Chang Shin <hshin@nvidia.com>
2019-10-04 08:44:46 -0700

    modify options string.

--- examples/nlp/shard_text_input_file.py
+++ examples/nlp/shard_text_input_file.py
@@ -5,8 +5,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Dataset sharding')
 
-parser.add_argument('input_file', type=str)
-parser.add_argument('output_file', type=str)
+parser.add_argument('--input_file', type=str)
+parser.add_argument('--output_file', type=str)
 
 args = parser.parse_args()
 
