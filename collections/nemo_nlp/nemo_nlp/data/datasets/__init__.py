6fe1cab29943127b21742d368b6376cbdfe8871e
ekmb <e.bakhturina@gmail.com>
2019-10-09 09:58:41 -0700

    style fixes
    Signed-off-by: ekmb <e.bakhturina@gmail.com>
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- collections/nemo_nlp/nemo_nlp/data/datasets/__init__.py
+++ collections/nemo_nlp/nemo_nlp/data/datasets/__init__.py
@@ -1,9 +1,9 @@
 from .bert_pretraining import BertPretrainingDataset
+from .glue import GLUEDataset
 from .joint_intent_slot import (BertJointIntentSlotDataset,
                                 BertJointIntentSlotInferDataset)
 from .language_modeling import LanguageModelingDataset
 from .ner import BertNERDataset
-from .glue import GLUEDataset
 from .sentence_classification import BertSentenceClassificationDataset
 from .token_classification import BertTokenClassificationDataset
 from .translation import TranslationDataset
