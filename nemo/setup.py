456f0fdd4a4850146c72e39718e7fae75911a3fd
Oleksii Kuchaiev <okuchaiev@users.noreply.github.com>
2019-10-10 13:04:13 -0700

    Update setup.py

--- nemo/setup.py
+++ nemo/setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License"
     ],
     install_requires=[
-        'torch>=1.2.0',
+        'torch==1.2.0',
         'torchvision',
         'tensorboardX',
         'pandas',
