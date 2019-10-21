7cf2c8be190b63dcc36c7493c2bbab78303b3f8a
ekmb <e.bakhturina@gmail.com>
2019-10-21 12:22:32 -0700

    wip punct renlp
    Signed-off-by: ekmb <e.bakhturina@gmail.com>

--- collections/nemo_nlp/nemo_nlp/data/datasets/__init__.py
+++ collections/nemo_nlp/nemo_nlp/data/datasets/__init__.py
@@ -4,6 +4,7 @@ from .joint_intent_slot import (BertJointIntentSlotDataset,
                                 BertJointIntentSlotInferDataset)
 from .language_modeling import LanguageModelingDataset
 from .ner import BertCornellNERDataset
+from .punctuation import BertPunctuationDataset
 from .token_classification import BertTokenClassificationDataset
 from .sentence_classification import BertSentenceClassificationDataset
 from .translation import TranslationDataset
