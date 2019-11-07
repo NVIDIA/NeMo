c1e6014ab713e6f73609fad8f2e04f65556600d9
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2019-11-07 13:22:41 -0800

    add abstract export API
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- nemo/nemo/core/neural_modules.py
+++ nemo/nemo/core/neural_modules.py
@@ -23,9 +23,14 @@ from .neural_types import (CanNotInferResultNeuralType,
 
 class WeightShareTransform(Enum):
     """When sharing parameters, what kind of transform to apply."""
-
     SAME = 0
     TRANSPOSE = 1
+
+
+class DeploymentFormat(Enum):
+    """Which format to use when exporting a Neural Module for deployment"""
+    ONNX = 0
+    TORCHSCRIPT = 1
 
 
 PretrainedModelInfo = namedtuple("PretrainedModleInfo",
@@ -195,6 +200,22 @@ class NeuralModule(ABC):
 
     def __str__(self):
         return self.__class__.__name__
+
+    def deployment_export(self,
+                          output: str,
+                          d_format: DeploymentFormat,
+                          input_example=None,
+                          output_example=None):
+        """Exports Neural Module instance for deployment.
+
+        Args:
+            output (str): where export results should be saved
+            d_format (DeploymentFormat): which deployment format to use
+            input_example: sometimes tracing will require input examples
+            output_example: Should match inference on input_example
+        """
+        raise NotImplemented("Deployment export is not implemented for this"
+                             "neural module.")
 
     @abstractmethod
     def get_weights(self) -> Optional[Dict[(str, bool)]]:
