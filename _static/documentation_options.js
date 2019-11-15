0c1343eb48fcd3161bfaa2bfb01b04fb06c8d00b
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2019-11-14 18:12:57 -0800

    update docs
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.1',
+    VERSION: '0.8.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
