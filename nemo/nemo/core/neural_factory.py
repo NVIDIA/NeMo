033d004270f5adaf321650a81c811e6f42ca1830
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2019-11-21 15:03:45 -0800

    fixed module/modules typos
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- nemo/nemo/core/neural_factory.py
+++ nemo/nemo/core/neural_factory.py
@@ -157,7 +157,7 @@ class Actions(ABC):
 
         Args:
             optimizer: Specifies which optimizer to use.
-            things_to_optimize: A list of neural module or tensors to be
+            things_to_optimize: A list of neural modules or tensors to be
                 optimized.
             optimizer_params: Specifies the parameters of the optimizer
 
@@ -239,14 +239,14 @@ class NeuralModuleFactory(object):
     _DEFAULT = None
 
     """
-    Neural Module Factory instance is used to create neural module and
+    Neural Module Factory instance is used to create neural modules and
     trainers
 
     Args:
         backend (Backend): Currently only Backend.PyTorch is supported
         local_rank (int): Process rank. Should be set by distributed runner
         optimization_level (Optimization): Level of optimization to use. Will
-            be passed to neural module and actions created by this factory.
+            be passed to neural modules and actions created by this factory.
         placement (DeviceType: where to place NeuralModule instances by default
         cudnn_benchmark (bool): (default False) If set to True it will use
             cudnnFind method to find the best kernels instead of using
@@ -261,7 +261,7 @@ class NeuralModuleFactory(object):
         master_process (bool): (default True) Flag for master process
             indication
         set_default (bool): (default True) True if should set this instance as
-            default factory for module instantiating.
+            default factory for modules instantiating.
     """
 
     def __init__(
