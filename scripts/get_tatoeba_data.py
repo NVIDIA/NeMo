1e1f2ff3e8ffe64e9df3614c5fe249f1544eb168
Evelina Bakhturina <ebakhturina@nvidia.com>
2019-12-10 09:07:55 -0800

    wip
    Signed-off-by: Evelina Bakhturina <ebakhturina@nvidia.com>

--- scripts/get_tatoeba_data.py
+++ scripts/get_tatoeba_data.py
@@ -92,6 +92,47 @@ def __process_english_sentences(in_file,
         out_file.write(' '.join(lines_to_combine) + '\n')
 
 
+def __process_sentences(in_file,
+                                out_file,
+                                percent_to_cut,
+                                num_to_combine,
+                                num_samples=-1):
+    """
+
+
+    """
+    if not os.path.exists(in_file):
+        raise FileNotFoundError(f'{in_file} not found.')
+
+    in_file = open(in_file, 'r')
+    out_file = open(out_file, 'w')
+    lines_to_combine = []
+    samples_count = 0
+
+    for line in in_file:
+        line = line.strip()
+        if len(line) > 0 and re.match('^[A-Z][a-z.,?\s]+$', line):  # nopep8
+            # chop some sentences in the middle
+            if percent_to_cut > 0:
+                line = line.split()
+                if random.random() < percent_to_cut:
+                    line = line[:len(line)//2]
+                line = ' '.join(line)
+
+            # combine multiple sentences into a single example
+            # to make it harder for the model to learn eos punctuation
+            if len(lines_to_combine) > num_to_combine:
+                if samples_count == num_samples:
+                    return
+                out_file.write(' '.join(lines_to_combine) + '\n')
+                lines_to_combine = []
+                samples_count += 1
+            else:
+                lines_to_combine.append(line)
+
+    if len(lines_to_combine) > 0 and samples_count < num_samples:
+        out_file.write(' '.join(lines_to_combine) + '\n')
+
 def __split_into_train_dev(in_file,
                            train_file,
                            dev_file,
@@ -208,33 +249,38 @@ if __name__ == "__main__":
     if args.dataset != 'tatoeba':
         raise ValueError("Unsupported dataset.")
 
-    print(f'Downloading tatoeba dataset')
-    tatoeba_dataset = os.path.join(args.data_dir, args.dataset + '.csv')
-    __maybe_download_file(tatoeba_dataset, args.dataset)
+    # print(f'Downloading tatoeba dataset')
+    # tatoeba_dataset = os.path.join(args.data_dir, args.dataset + '.csv')
+    # __maybe_download_file(tatoeba_dataset, args.dataset)
+
+
+    tatoeba_dataset = '/home/ebakhturina/data/tutorial_punct/dataset/new_format/all_eng_sentences.csv'
+
 
     print(f'Processing English sentences...')
-    eng_sentences = os.path.join(args.data_dir, 'eng_sentences.csv')
+    eng_sentences = os.path.join(args.data_dir, 'clean_all_eng_sentences.txt')
     __process_english_sentences(tatoeba_dataset,
                                 eng_sentences,
                                 args.percent_to_cut,
                                 args.num_lines_to_combine,
                                 args.num_samples)
 
-    train_file = os.path.join(args.data_dir, 'train.txt')
-    dev_file = os.path.join(args.data_dir, 'dev.txt')
+    # eng_sentences = os.path.join('/home/ebakhturina/data/tutorial_punct/dataset/new_format/shuf_final.txt')
+    # train_file = os.path.join(args.data_dir, 'train.txt')
+    # dev_file = os.path.join(args.data_dir, 'dev.txt')
 
-    print(f'Splitting the {args.dataset} dataset into train and dev sets')
-    __split_into_train_dev(eng_sentences,
-                           train_file,
-                           dev_file,
-                           args.percent_dev)
+    # print(f'Splitting the {args.dataset} dataset into train and dev sets')
+    # __split_into_train_dev(eng_sentences,
+    #                        train_file,
+    #                        dev_file,
+    #                        args.percent_dev)
 
-    __create_text_and_labels(args.data_dir, 'train.txt')
-    __create_text_and_labels(args.data_dir, 'dev.txt')
+    # __create_text_and_labels(args.data_dir, 'train.txt')
+    # __create_text_and_labels(args.data_dir, 'dev.txt')
 
-    # clean data_dir
-    __delete_file(eng_sentences)
-    __delete_file(tatoeba_dataset)
-    __delete_file(train_file)
-    __delete_file(dev_file)
-    print(f'Processing of the {args.dataset} is complete')
+    # # clean data_dir
+    # __delete_file(eng_sentences)
+    # __delete_file(tatoeba_dataset)
+    # __delete_file(train_file)
+    # __delete_file(dev_file)
+    # print(f'Processing of the {args.dataset} is complete')
