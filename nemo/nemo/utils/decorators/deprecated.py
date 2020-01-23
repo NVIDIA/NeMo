2749870212a88da64b1a46ec87f7d7857625fa7f
Tomasz Kornuta <tkornuta@nvidia.com>
2020-01-23 15:37:36 -0800

    header change
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- nemo/nemo/utils/decorators/deprecated.py
+++ nemo/nemo/utils/decorators/deprecated.py
@@ -1,4 +1,4 @@
-# Copyright (C) tkornuta, NVIDIA AI Applications Team. All Rights Reserved.
+# Copyright (C) NVIDIA. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
