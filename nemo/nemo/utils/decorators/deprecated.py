a9e5b23de626f353a87d56e0cfdf109f21c11bf7
Tomasz Kornuta <tkornuta@nvidia.com>
2020-01-23 12:33:41 -0800

    Added arguments to decorator enabling to pass version and alternative suggestion
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- nemo/nemo/utils/decorators/deprecated.py
+++ nemo/nemo/utils/decorators/deprecated.py
@@ -21,13 +21,51 @@ from unittest.mock import patch
 import nemo
 
 
-def deprecated_function(func):
-    """ Decorator function used for indicating that a function is depricated
-    and going to be removed."""
-    def wrapper(*args, **kwargs):
-        # Display the depricated warning.
-        nemo.logging.warning(
-            "Function ``{}`` is depricated.".format(func.__name__))
-        # Call the function.
-        func(*args, **kwargs)
-    return wrapper
+class deprecated(object):
+    """ Decorator class used for indicating that a function is
+    depricated and going to be removed.
+    Tracks down which functions printed the warning and 
+    will print it only once per function. 
+    """
+
+    # Static variable - list of names of functions that we already printed
+    # the warning for.
+    warned_functions = {}
+
+    def __init__(self, version=None, alternative_function=None):
+        """
+        If there are decorator arguments, the function
+        to be decorated is not passed to the constructor!
+        """
+        self.version = version
+        self.alternative_function = alternative_function
+
+    def __call__(self, func):
+        """
+        If there are decorator arguments, __call__() is only called
+        once, as part of the decoration process! You can only give
+        it a single argument, which is the function object.
+        """
+
+        def wrapper(*args, **kwargs):
+
+            # Prepare the warning message.
+            msg = "Function ``{}`` is depricated.".format(func.__name__)
+
+            # Optionally, add version and alternative.
+            if self.version is not None:
+                msg = msg + \
+                    " It is going to be removed in version {}.".format(
+                        self.version)
+
+            if self.alternative_function is not None:
+                msg = msg + \
+                    " Please use ``{}`` instead.".format(
+                        self.alternative_function)
+            # Display the depricated warning.
+            nemo.logging.warning(msg)
+
+            # Call the function.
+            func(*args, **kwargs)
+
+        return wrapper
