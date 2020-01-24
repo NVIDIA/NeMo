6d2f047675a58f5bd6d351ae581212da12ed6267
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2020-01-23 17:25:56 -0800

    Reinstall Script Updated
    Signed-off-by: DEKHTIARJonathan <contact@jonathandekhtiar.eu>

--- reinstall.sh
+++ reinstall.sh
@@ -1,23 +1,16 @@
+#!/usr/bin/env bash
 set -e
 
 echo 'Uninstalling stuff'
 pip uninstall -y nemo_toolkit
+
+# Kept for legacy purposes
 pip uninstall -y nemo_asr
 pip uninstall -y nemo_nlp
 pip uninstall -y nemo_tts
 pip uninstall -y nemo_simple_gan
 
 echo 'Installing stuff'
-cd nemo
-pip install -e .
-cd ../collections/nemo_asr
-pip install -e .
-cd ../nemo_nlp
-pip install -e .
-cd ../nemo_simple_gan
-pip install -e .
-cd ../nemo_tts
-pip install -e .
+pip install -e ".[all]"
 
-cd ..
 echo 'All done!'
