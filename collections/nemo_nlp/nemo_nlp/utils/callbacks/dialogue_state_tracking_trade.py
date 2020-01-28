da70cf8a9076fff3cdb377a860aec11e0de4cba6
VahidooX <vnoroozi@nvidia.com>
2020-01-28 15:23:29 -0800

    Changed data_layers to data_desc in the callbacks.
    Signed-off-by: VahidooX <vnoroozi@nvidia.com>

--- collections/nemo_nlp/nemo_nlp/utils/callbacks/dialogue_state_tracking_trade.py
+++ collections/nemo_nlp/nemo_nlp/utils/callbacks/dialogue_state_tracking_trade.py
@@ -11,7 +11,7 @@ logger = get_logger('')
 
 def eval_iter_callback(tensors,
                        global_vars,
-                       eval_data_layer):
+                       data_desc):
 
     if 'loss' not in global_vars:
         global_vars['loss'] = []
@@ -27,36 +27,40 @@ def eval_iter_callback(tensors,
             loss_numpy = v[0].cpu().numpy()
             global_vars['loss'].append(loss_numpy)
         if kv.startswith('point_outputs'):
-            point_outputs = v[0] #.cpu().numpy()
+            point_outputs = v[0].cpu().numpy()
+            # point_outputs = v[0]  # .cpu().numpy()
         if kv.startswith('gate_outputs'):
-            gate_outputs = v[0] #.cpu().numpy()
+            gate_outputs = v[0].cpu().numpy()
+            # gate_outputs = v[0]  # .cpu().numpy()
         if kv.startswith('gating_labels'):
             gating_labels = v[0].cpu().numpy()
             global_vars['gating_labels'].extend(gating_labels)
         if kv.startswith('tgt_ids'):
-            tgt_ids = v[0] #.cpu().numpy()
+            tgt_ids = v[0].cpu().numpy()
+            # tgt_ids = v[0] #.cpu().numpy()
 
-    point_outputs_max = torch.argmax(point_outputs, dim=-1)
-    mask_paddings = (tgt_ids == eval_data_layer.pad_id)
-    comp_res = ((point_outputs_max == tgt_ids) | mask_paddings)
-    comp_res = torch.all(comp_res, axis=-1, keepdims=False)
-
-    # point_outputs_max = np.argmax(point_outputs, axis=-1)
+    # point_outputs_max = torch.argmax(point_outputs, dim=-1)
     # mask_paddings = (tgt_ids == eval_data_layer.pad_id)
-    # comp_res = np.logical_or(point_outputs_max == tgt_ids, mask_paddings)
-    # comp_res = np.all(comp_res, axis=-1, keepdims=False)
+    # comp_res = ((point_outputs_max == tgt_ids) | mask_paddings)
+    # comp_res = torch.all(comp_res, axis=-1, keepdims=False)
 
-    global_vars['comp_res'].extend(comp_res.cpu().numpy())
-    #global_vars['gating_preds'].extend(np.argmax(gate_outputs, axis=-1))
-    global_vars['gating_preds'].extend(torch.argmax(gate_outputs, axis=-1).cpu().numpy())
+    point_outputs_max = np.argmax(point_outputs, axis=-1)
+    mask_paddings = (tgt_ids == data_desc.pad_id)
+    comp_res = np.logical_or(point_outputs_max == tgt_ids, mask_paddings)
+    comp_res = np.all(comp_res, axis=-1, keepdims=False)
+
+    #global_vars['comp_res'].extend(comp_res.cpu().numpy())
+    global_vars['comp_res'].extend(comp_res)
+    global_vars['gating_preds'].extend(np.argmax(gate_outputs, axis=-1))
+    #global_vars['gating_preds'].extend(torch.argmax(gate_outputs, axis=-1).cpu().numpy())
 
 
-def eval_epochs_done_callback(global_vars, eval_data_layer):
+def eval_epochs_done_callback(global_vars, data_desc):
     joint_acc, turn_acc = \
         evaluate_metrics(global_vars['comp_res'],
                          global_vars['gating_labels'],
                          global_vars['gating_preds'],
-                         eval_data_layer.gating_dict["ptr"])
+                         data_desc.gating_dict["ptr"])
 
     gating_comp_flatten = (np.asarray(global_vars['gating_labels']) == np.asarray(global_vars['gating_preds'])).ravel()
     gating_acc = np.sum(gating_comp_flatten) / len(gating_comp_flatten)
