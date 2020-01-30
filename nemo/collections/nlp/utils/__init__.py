c92d6bd09a719b03226a5c7b1e6bd61f0eeaa866
VahidooX <vnoroozi@nvidia.com>
2020-01-30 15:40:18 -0800

    Fixed bugs in utils folder.
    Signed-off-by: VahidooX <vnoroozi@nvidia.com>

--- nemo/collections/nlp/utils/__init__.py
+++ nemo/collections/nlp/utils/__init__.py
@@ -1,3 +1,3 @@
 from nemo.collections.nlp.utils.callback_utils import *
-from nemo.collections.nlp.utils.nlp_utils import *
 from nemo.collections.nlp.utils.loss_utils import *
+from nemo.collections.nlp.utils.nlp_utils import *
