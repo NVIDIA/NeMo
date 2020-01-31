b3c15133fbc40e7b636bfa629947b9861b90fd43
VahidooX <vnoroozi@nvidia.com>
2020-01-31 13:57:02 -0800

    Fixed transformer package.
    Signed-off-by: VahidooX <vnoroozi@nvidia.com>

--- nemo/collections/nlp/nm/trainables/common/__init__.py
+++ nemo/collections/nlp/nm/trainables/common/__init__.py
@@ -1,5 +1,5 @@
 import nemo.collections.nlp.nm.trainables.common.huggingface
-import nemo.collections.nlp.nm.trainables.common.transformer
 from nemo.collections.nlp.nm.trainables.common.sequence_classification_nm import *
 from nemo.collections.nlp.nm.trainables.common.sequence_regression_nm import *
 from nemo.collections.nlp.nm.trainables.common.token_classification_nm import *
+from nemo.collections.nlp.nm.trainables.common.transformer import *
