4f8126048286a0dde97856a57431ca9c074108c7
VahidooX <vnoroozi@nvidia.com>
2020-01-31 12:56:23 -0800

    Updated thw whole test folder.
    Signed-off-by: VahidooX <vnoroozi@nvidia.com>

--- tests/test_neural_factory.py
+++ tests/test_neural_factory.py
@@ -23,17 +23,17 @@ from tests.common_setup import NeMoUnitTest
 class TestNeuralFactory(NeMoUnitTest):
     def test_creation(self):
         neural_factory = nemo.core.NeuralModuleFactory(
-            backend=nemo.core.Backend.PyTorch, local_rank=None, create_tb_writer=False,
+            backend=nemo.core.Backend.PyTorch, local_rank=None, create_tb_writer=False
         )
         instance = neural_factory.get_module(name="TaylorNet", collection="toys", params={"dim": 4})
         self.assertTrue(isinstance(instance, nemo.backends.pytorch.tutorials.TaylorNet))
 
     def test_simple_example(self):
         neural_factory = nemo.core.neural_factory.NeuralModuleFactory(
-            backend=nemo.core.Backend.PyTorch, local_rank=None, create_tb_writer=False,
+            backend=nemo.core.Backend.PyTorch, local_rank=None, create_tb_writer=False
         )
         dl = neural_factory.get_module(
-            name="RealFunctionDataLayer", collection="toys", params={"n": 10000, "batch_size": 128},
+            name="RealFunctionDataLayer", collection="toys", params={"n": 10000, "batch_size": 128}
         )
         fx = neural_factory.get_module(name="TaylorNet", collection="toys", params={"dim": 4})
         loss = neural_factory.get_module(name="MSELoss", collection="toys", params={})
@@ -43,6 +43,4 @@ class TestNeuralFactory(NeMoUnitTest):
         loss_tensor = loss(predictions=y_pred, target=y)
 
         optimizer = neural_factory.get_trainer()
-        optimizer.train(
-            [loss_tensor], optimizer="sgd", optimization_params={"lr": 1e-3, "num_epochs": 1},
-        )
+        optimizer.train([loss_tensor], optimizer="sgd", optimization_params={"lr": 1e-3, "num_epochs": 1})
--- tests/test_pytorch_trainers.py
+++ tests/test_pytorch_trainers.py
@@ -32,27 +32,25 @@ class TestPytorchTrainers(NeMoUnitTest):
 
         optimizer = nemo.backends.pytorch.actions.PtActions()
         optimizer.train(
-            tensors_to_optimize=[loss_tensor], optimizer="sgd", optimization_params={"lr": 0.0003, "num_epochs": 1},
+            tensors_to_optimize=[loss_tensor], optimizer="sgd", optimization_params={"lr": 0.0003, "num_epochs": 1}
         )
 
     def test_simple_train_named_output(self):
         print('Simplest train test with using named output.')
-        data_source = nemo.backends.pytorch.tutorials.RealFunctionDataLayer(n=10000, batch_size=128,)
+        data_source = nemo.backends.pytorch.tutorials.RealFunctionDataLayer(n=10000, batch_size=128)
         trainable_module = nemo.backends.pytorch.tutorials.TaylorNet(dim=4)
         loss = nemo.backends.pytorch.tutorials.MSELoss()
 
         data = data_source()
         self.assertEqual(
-            first=type(data).__name__,
-            second='RealFunctionDataLayerOutput',
-            msg='Check output class naming coherence.',
+            first=type(data).__name__, second='RealFunctionDataLayerOutput', msg='Check output class naming coherence.'
         )
         y_pred = trainable_module(x=data.x)
         loss_tensor = loss(predictions=y_pred, target=data.y)
 
         optimizer = nemo.backends.pytorch.actions.PtActions()
         optimizer.train(
-            tensors_to_optimize=[loss_tensor], optimizer="sgd", optimization_params={"lr": 0.0003, "num_epochs": 1},
+            tensors_to_optimize=[loss_tensor], optimizer="sgd", optimization_params={"lr": 0.0003, "num_epochs": 1}
         )
 
     def test_simple_chained_train(self):
@@ -70,5 +68,5 @@ class TestPytorchTrainers(NeMoUnitTest):
 
         optimizer = nemo.backends.pytorch.actions.PtActions()
         optimizer.train(
-            tensors_to_optimize=[loss_tensor], optimizer="sgd", optimization_params={"lr": 0.0003, "num_epochs": 1},
+            tensors_to_optimize=[loss_tensor], optimizer="sgd", optimization_params={"lr": 0.0003, "num_epochs": 1}
         )
