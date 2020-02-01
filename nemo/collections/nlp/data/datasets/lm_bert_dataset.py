58715fea6691e5d6a6589ed8fd51c8e29c646ddd
VahidooX <vnoroozi@nvidia.com>
2020-01-31 16:09:20 -0800

    Added license to all the files except examples.
    Signed-off-by: VahidooX <vnoroozi@nvidia.com>

--- nemo/collections/nlp/data/datasets/lm_bert_dataset.py
+++ nemo/collections/nlp/data/datasets/lm_bert_dataset.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
+
 """Pytorch Dataset for training BERT."""
 
 import array
--- nemo/collections/nlp/data/datasets/token_classification_dataset.py
+++ nemo/collections/nlp/data/datasets/token_classification_dataset.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 Utility functions for Token Classification NLP tasks
 Some parts of this code were adapted from the HuggingFace library at
