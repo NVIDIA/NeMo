79d1d31dd277618a55186addd7bd1ca92de0654f
Sergei Nikolaev <snikolaev@nvidia.com>
2020-02-01 21:56:11 -0800

    Green tests
    Signed-off-by: Sergei Nikolaev <snikolaev@nvidia.com>

--- tests/test_deploy_export.py
+++ tests/test_deploy_export.py
@@ -48,15 +48,30 @@ class TestDeployExport(NeMoUnitTest):
         )
 
         self.assertTrue(out.exists())
-        outputs_fwd = module.forward(input_example[0])
-        sess_options = ort.SessionOptions()
-        sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_BASIC
-        ort_session = ort.InferenceSession(out_name, sess_options)
-        outputs_ort = ort_session.run(
-            None, {'audio_signal': input_example[0].cpu().numpy(), 'encoded_lengths': input_example[1].cpu().numpy()}
-        )
-        outputs_ort = torch.from_numpy(outputs_ort[0]).cuda()
-        self.assertLess((outputs_ort - outputs_fwd).norm(p=2), 5.0e-4)
+        if mode == nemo.core.DeploymentFormat.ONNX:
+            if isinstance(input_example, tuple):
+                outputs_fwd = module.forward(*input_example)
+            else:
+                outputs_fwd = module.forward(input_example)
+            sess_options = ort.SessionOptions()
+            sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
+            ort_session = ort.InferenceSession(out_name, sess_options)
+            inputs = dict()
+            input_names = list(module.input_ports)
+            for i in range(len(input_names)):
+                input_name = (
+                    "encoded_lengths"
+                    if type(module).__name__ == "JasperEncoder" and input_names[i] == "length"
+                    else input_names[i]
+                )
+                inputs[input_name] = (
+                    input_example[i].cpu().numpy() if isinstance(input_example, tuple) else input_example.cpu().numpy()
+                )
+            outputs_ort = ort_session.run(None, inputs)
+            outputs_ort = torch.from_numpy(outputs_ort[0]).cuda()
+            self.assertLess(
+                (outputs_ort - (outputs_fwd[0] if isinstance(outputs_fwd, tuple) else outputs_fwd)).norm(p=2), 5.0e-4
+            )
         if out.exists():
             os.remove(out)
 
