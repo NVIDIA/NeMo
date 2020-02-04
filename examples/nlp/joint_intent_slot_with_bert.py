5e63d5dc0c6854ef8e6ea1f684a8c2bbe84953ff
Tomasz Kornuta <tkornuta@nvidia.com>
2020-02-03 18:30:46 -0800

    Cleanups related to removing factory from BERT init calls
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- examples/nlp/joint_intent_slot_with_bert.py
+++ examples/nlp/joint_intent_slot_with_bert.py
@@ -88,12 +88,12 @@ nemo_nlp.huggingface.BERT.list_pretrained_models()
 """
 if args.bert_checkpoint and args.bert_config:
     pretrained_bert_model = nemo.collections.nlp.nm.trainables.common.huggingface.BERT(
-        config_filename=args.bert_config, factory=nf
+        config_filename=args.bert_config
     )
     pretrained_bert_model.restore_from(args.bert_checkpoint)
 else:
     pretrained_bert_model = nemo.collections.nlp.nm.trainables.common.huggingface.BERT(
-        pretrained_model_name=args.pretrained_bert_model, factory=nf
+        pretrained_model_name=args.pretrained_bert_model
     )
 
 hidden_size = pretrained_bert_model.hidden_size
