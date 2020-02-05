0808d1362b71dd03762b269c78d399e41370f723
VahidooX <vnoroozi@nvidia.com>
2020-02-05 10:04:39 -0800

    Added multiwoz license.
    Signed-off-by: VahidooX <vnoroozi@nvidia.com>

--- examples/nlp/scripts/multiwoz/process_multiwoz.py
+++ examples/nlp/scripts/multiwoz/process_multiwoz.py
@@ -15,7 +15,7 @@
 # =============================================================================
 
 # =============================================================================
-# Copyright 2019 Salesforce Research.
+# Copyright 2019 Salesforce Research - Paweł Budzianowski.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
