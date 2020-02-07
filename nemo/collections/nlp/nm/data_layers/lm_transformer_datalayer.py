b51ae820a162b426723527e81457e7466f75cb25
Yang Zhang <yangzhang@nvidia.com>
2020-02-07 14:16:33 -0800

    update copyright header
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/nm/data_layers/lm_transformer_datalayer.py
+++ nemo/collections/nlp/nm/data_layers/lm_transformer_datalayer.py
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright 2019 AI Applications Design Team at NVIDIA. All Rights Reserved.
+# Copyright 2019 NVIDIA. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
