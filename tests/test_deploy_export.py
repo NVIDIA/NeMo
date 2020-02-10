09ac88c065367147bfe741b4df2ae2fc63707b87
Sergei Nikolaev <snikolaev@nvidia.com>
2020-02-09 21:35:24 -0800

    All possible format permutations and Quartz coverage

--- tests/test_deploy_export.py
+++ tests/test_deploy_export.py
@@ -21,9 +21,12 @@ from pathlib import Path
 
 # git clone git@github.com:microsoft/onnxruntime.git
 # cd onnxruntime
-# ./build.sh --update --build --config RelWithDebInfo --build_shared_lib --parallel --use_cuda \
-#            --cudnn_home /usr/lib/x86_64-linux-gnu --cuda_home /usr/local/cuda --enable_pybind --build_wheel
-# pip install --upgrade ./build/Linux/RelWithDebInfo/dist/onnxruntime_gpu-1.1.0-cp37-cp37m-linux_x86_64.whl
+#
+# ./build.sh --update --build --config RelWithDebInfo  --build_shared_lib --parallel \
+#     --cudnn_home /usr/lib/x86_64-linux-gnu --cuda_home /usr/local/cuda \
+#     --tensorrt_home /home/snikolaev/CODE/TensorRT.BIN --use_tensorrt --enable_pybind --build_wheel
+#
+# pip install --upgrade ./build/Linux/RelWithDebInfo/dist/*.whl
 import onnxruntime as ort
 import torch
 from ruamel.yaml import YAML
@@ -58,6 +61,7 @@ class TestDeployExport(NeMoUnitTest):
         )
 
         tol = 2.0e-3
+        out = Path(out_name)
         self.assertTrue(out.exists())
         if mode == nemo.core.DeploymentFormat.ONNX:
             # Must recompute beause *module* might be different now
@@ -66,7 +70,7 @@ class TestDeployExport(NeMoUnitTest):
             )
             sess_options = ort.SessionOptions()
             sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
-            ort_session = ort.InferenceSession(out_name, sess_options)
+            ort_session = ort.InferenceSession(out_name, sess_options, ['CPUExecutionProvider'])
             inputs = dict()
             input_names = list(module.input_ports)
             for i in range(len(input_names)):
@@ -78,10 +82,9 @@ class TestDeployExport(NeMoUnitTest):
                 inputs[input_name] = (
                     input_example[i].cpu().numpy() if isinstance(input_example, tuple) else input_example.cpu().numpy()
                 )
+            print('Execution Providers: ', ort_session.get_providers())
             outputs_scr = ort_session.run(None, inputs)
             outputs_scr = torch.from_numpy(outputs_scr[0]).cuda()
-            outputs_fwd = outputs_fwd[0] if isinstance(outputs_fwd, tuple) else outputs_fwd
-            tol = 5.0e-4
         elif mode == nemo.core.DeploymentFormat.TORCHSCRIPT:
             scr = torch.jit.load(out_name)
             if isinstance(module, nemo.backends.pytorch.tutorials.TaylorNet):
@@ -93,76 +96,53 @@ class TestDeployExport(NeMoUnitTest):
         elif mode == nemo.core.DeploymentFormat.PYTORCH:
             module.load_state_dict(torch.load(out_name))
             module.eval()
-            outputs_scr = module.forward(*input_example)
+            outputs_scr = (
+                module.forward(*input_example) if isinstance(input_example, tuple) else module.forward(input_example)
+            )
 
+        outputs_scr = outputs_scr[0] if isinstance(outputs_scr, tuple) else outputs_scr
+        outputs_fwd = outputs_fwd[0] if isinstance(outputs_fwd, tuple) else outputs_fwd
         self.assertLess((outputs_scr - outputs_fwd).norm(p=2), tol)
 
         if out.exists():
             os.remove(out)
 
+    def __test_export_route_all(self, module, out_name, input_example=None):
+        if input_example is not None:
+            self.__test_export_route(module, out_name + '.onnx', nemo.core.DeploymentFormat.ONNX, input_example)
+            self.__test_export_route(module, out_name + '.pt', nemo.core.DeploymentFormat.PYTORCH, input_example)
+        self.__test_export_route(module, out_name + '.ts', nemo.core.DeploymentFormat.TORCHSCRIPT, input_example)
+
     def test_simple_module_export(self):
         simplest_module = nemo.backends.pytorch.tutorials.TaylorNet(dim=4)
-        self.__test_export_route(
-            module=simplest_module,
-            out_name="simple.pt",
-            mode=nemo.core.DeploymentFormat.TORCHSCRIPT,
-            input_example=None,
+        self.__test_export_route_all(
+            module=simplest_module, out_name="simple", input_example=None,
         )
 
     def test_TokenClassifier_module_export(self):
         t_class = nemo.collections.nlp.nm.trainables.common.token_classification_nm.TokenClassifier(
             hidden_size=512, num_classes=16, use_transformer_pretrained=False
         )
-        self.__test_export_route(
-            module=t_class,
-            out_name="t_class.pt",
-            mode=nemo.core.DeploymentFormat.TORCHSCRIPT,
-            input_example=torch.randn(16, 16, 512).cuda(),
+        self.__test_export_route_all(
+            module=t_class, out_name="t_class", input_example=torch.randn(16, 16, 512).cuda(),
         )
 
-    def test_TokenClassifier_module_onnx_export(self):
-        t_class = nemo.collections.nlp.nm.trainables.common.token_classification_nm.TokenClassifier(
-            hidden_size=512, num_classes=16, use_transformer_pretrained=False
-        )
-        self.__test_export_route(
-            module=t_class,
-            out_name="t_class.onnx",
-            mode=nemo.core.DeploymentFormat.ONNX,
-            input_example=torch.randn(16, 16, 512).cuda(),
-        )
-
-    def test_jasper_decoder_export_ts(self):
+    def test_jasper_decoder(self):
         j_decoder = nemo_asr.JasperDecoderForCTC(feat_in=1024, num_classes=33)
-        self.__test_export_route(
-            module=j_decoder,
-            out_name="j_decoder.ts",
-            mode=nemo.core.DeploymentFormat.TORCHSCRIPT,
-            input_example=torch.randn(34, 1024, 1).cuda(),
+        self.__test_export_route_all(
+            module=j_decoder, out_name="j_decoder", input_example=torch.randn(34, 1024, 1).cuda(),
         )
 
-    def test_hf_bert_ts(self):
+    def test_hf_bert(self):
         bert = nemo.collections.nlp.nm.trainables.common.huggingface.BERT(pretrained_model_name="bert-base-uncased")
         input_example = (
             torch.randint(low=0, high=16, size=(2, 16)).cuda(),
             torch.randint(low=0, high=1, size=(2, 16)).cuda(),
             torch.randint(low=0, high=1, size=(2, 16)).cuda(),
         )
-        self.__test_export_route(
-            module=bert, out_name="bert.ts", mode=nemo.core.DeploymentFormat.TORCHSCRIPT, input_example=input_example
-        )
+        self.__test_export_route_all(module=bert, out_name="bert", input_example=input_example)
 
-    def test_hf_bert_pt(self):
-        bert = nemo.collections.nlp.nm.trainables.common.huggingface.BERT(pretrained_model_name="bert-base-uncased")
-        input_example = (
-            torch.randint(low=0, high=16, size=(2, 16)).cuda(),
-            torch.randint(low=0, high=1, size=(2, 16)).cuda(),
-            torch.randint(low=0, high=1, size=(2, 16)).cuda(),
-        )
-        self.__test_export_route(
-            module=bert, out_name="bert.pt", mode=nemo.core.DeploymentFormat.PYTORCH, input_example=input_example,
-        )
-
-    def test_jasper_encoder_to_onnx(self):
+    def test_jasper_encoder(self):
         with open("tests/data/jasper_smaller.yaml") as file:
             yaml = YAML(typ="safe")
             jasper_model_definition = yaml.load(file)
@@ -173,9 +153,24 @@ class TestDeployExport(NeMoUnitTest):
             **jasper_model_definition['JasperEncoder']
         )
 
-        self.__test_export_route(
+        self.__test_export_route_all(
             module=jasper_encoder,
-            out_name="jasper_encoder.onnx",
-            mode=nemo.core.DeploymentFormat.ONNX,
+            out_name="jasper_encoder",
             input_example=(torch.randn(16, 64, 256).cuda(), torch.randn(256).cuda()),
+        )
+
+    def test_quartz_encoder(self):
+        with open("tests/data/quartznet_test.yaml") as file:
+            yaml = YAML(typ="safe")
+            quartz_model_definition = yaml.load(file)
+
+        jasper_encoder = nemo_asr.JasperEncoder(
+            feat_in=quartz_model_definition['AudioToMelSpectrogramPreprocessor']['features'],
+            **quartz_model_definition['JasperEncoder']
+        )
+
+        self.__test_export_route_all(
+            module=jasper_encoder,
+            out_name="quartz_encoder",
+            input_example=(torch.randn(16, 64, 256).cuda(), torch.randint(20, (16,)).cuda()),
         )
