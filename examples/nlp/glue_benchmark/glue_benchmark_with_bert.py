57220e1434add05c783f945dd7b0b6c3fb178b23
Yang Zhang <yangzhang@nvidia.com>
2020-02-12 13:53:56 -0800

    incorporate PR feedback
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- examples/nlp/glue_benchmark/glue_benchmark_with_bert.py
+++ examples/nlp/glue_benchmark/glue_benchmark_with_bert.py
@@ -199,13 +199,7 @@ else:
     Replace BERT-STEP-150000.pt with the path to your checkpoint.
     """
     if args.tokenizer == "sentencepiece":
-        special_tokens = {
-            "sep_token": "[SEP]",
-            "pad_token": "[PAD]",
-            "bos_token": "[CLS]",
-            "eos_token": "[SEP]",
-            "cls_token": "[CLS]",
-        }
+        special_tokens = nemo_nlp.utils.MODEL_SPECIAL_TOKENS['bert']
         tokenizer = SentencePieceTokenizer(model_path=args.tokenizer_model, special_tokens=special_tokens)
     elif args.tokenizer == "nemobert":
         tokenizer = NemoBertTokenizer(args.pretrained_bert_model)
