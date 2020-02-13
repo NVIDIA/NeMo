5b728dce595ca062fd82a5ad8b982d1592666e42
Jason <jasoli@nvidia.com>
2020-02-12 18:21:14 -0800

    cache is now offloaded to cpu mem
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/backends/pytorch/actions.py
+++ nemo/backends/pytorch/actions.py
@@ -715,6 +715,7 @@ class PtActions(Actions):
                 loop_iterator = eval_dataloader
 
             for epoch_i, data in enumerate(loop_iterator, 0):
+                print(torch.cuda.memory_allocated())
                 if verbose and (num_batches < 10 or (epoch_i % int(num_batches / 10) == 0)):
                     logging.info(f"Evaluating batch {epoch_i} out of {num_batches}")
                 tensors = []
@@ -724,9 +725,9 @@ class PtActions(Actions):
                     for t in tensors_to_return:
                         if t.unique_name in registered_e_tensors:
                             del registered_e_tensors[t.unique_name]
-                        else:
-                            registered_e_tensors[t.unique_name] = t.cuda()
                     # Need to check for device type mismatch
+                    for t in registered_e_tensors:
+                        registered_e_tensors[t].to(dl_device)
                 else:
                     if isinstance(data, torch.Tensor):
                         data = (data,)
@@ -815,6 +816,9 @@ class PtActions(Actions):
         """Simpler helper function to add results of __nm_graph_forward_pass to
         current cache.
         """
+        if offload_to_cpu:
+            for t in registered_tensors:
+                registered_tensors[t] = registered_tensors[t].cpu()
         self.cache.append(registered_tensors)
 
     def clear_cache(self):
