d02367a1e6b9c4c191044838ed884a029d30640d
Evelina Bakhturina <ebakhturina@nvidia.com>
2020-02-13 15:55:55 -0800

    import fix
    Signed-off-by: Evelina Bakhturina <ebakhturina@nvidia.com>

--- nemo/collections/nlp/data/datasets/qa_squad_dataset/qa_squad_dataset.py
+++ nemo/collections/nlp/data/datasets/qa_squad_dataset/qa_squad_dataset.py
@@ -404,38 +404,6 @@ class SquadDataset(Dataset):
         return exact_match, f1, all_predictions
 
 
-class InputFeatures(object):
-    """A single set of features of data."""
-
-    def __init__(
-        self,
-        unique_id,
-        example_index,
-        doc_span_index,
-        tokens,
-        token_to_orig_map,
-        token_is_max_context,
-        input_ids,
-        input_mask,
-        segment_ids,
-        start_position=None,
-        end_position=None,
-        is_impossible=None,
-    ):
-        self.unique_id = unique_id
-        self.example_index = example_index
-        self.doc_span_index = doc_span_index
-        self.tokens = tokens
-        self.token_to_orig_map = token_to_orig_map
-        self.token_is_max_context = token_is_max_context
-        self.input_ids = input_ids
-        self.input_mask = input_mask
-        self.segment_ids = segment_ids
-        self.start_position = start_position
-        self.end_position = end_position
-        self.is_impossible = is_impossible
-
-
 class SquadProcessor(DataProcessor):
     """
     Processor for the SQuAD data set.
--- nemo/collections/nlp/data/datasets/qa_squad_dataset/qa_squad_processing.py
+++ nemo/collections/nlp/data/datasets/qa_squad_dataset/qa_squad_processing.py
@@ -1,7 +1,6 @@
 import collections
 
 from nemo import logging
-from nemo.collections.nlp.data.datasets.qa_squad_dataset.qa_squad_dataset import InputFeatures
 
 
 def convert_examples_to_features(
@@ -229,3 +228,35 @@ def _check_is_max_context(doc_spans, cur_span_index, position):
             best_span_index = span_index
 
     return cur_span_index == best_span_index
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(
+        self,
+        unique_id,
+        example_index,
+        doc_span_index,
+        tokens,
+        token_to_orig_map,
+        token_is_max_context,
+        input_ids,
+        input_mask,
+        segment_ids,
+        start_position=None,
+        end_position=None,
+        is_impossible=None,
+    ):
+        self.unique_id = unique_id
+        self.example_index = example_index
+        self.doc_span_index = doc_span_index
+        self.tokens = tokens
+        self.token_to_orig_map = token_to_orig_map
+        self.token_is_max_context = token_is_max_context
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.start_position = start_position
+        self.end_position = end_position
+        self.is_impossible = is_impossible
