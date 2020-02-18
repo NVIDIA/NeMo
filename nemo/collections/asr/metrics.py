59ce1127007bd82e8180c070f3570c0991386151
Somshubra Majumdar <titu1994@gmail.com>
2020-02-18 14:08:40 -0800

    Alias crossentropyloss as CrossEntropyLossNM in asr __init__ eval_metric now accepts list of ints for top-k classification acc Removed redundant CrossEntropyLossNM in asr losses.py removed softmax computation from metrics and added no_grad
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/asr/metrics.py
+++ nemo/collections/asr/metrics.py
@@ -32,7 +32,6 @@ def word_error_rate(hypotheses: List[str], references: List[str], use_cer=False)
     Computes Average Word Error rate between two texts represented as
     corresponding lists of string. Hypotheses and references must have same
     length.
-
     Args:
       hypotheses: list of hypotheses
       references: list of references
@@ -70,18 +69,15 @@ def classification_accuracy(
     """
     Computes the top-k classification accuracy provided with
     un-normalized logits of a model and ground truth targets.
-
     If top_k is not provided, defaults to top_1 accuracy.
     If top_k is provided as a list, then the values are sorted
     in ascending order.
-
     Args:
         logits: Un-normalized logits of a model. Softmax will be
             applied to these logits prior to computation of accuracy.
         targets: Vector of integers which represent indices of class
             labels.
         top_k: Optional list of integers in the range [1, max_classes].
-
     Returns:
         A list of length `top_k`, where each value represents top_i
         accuracy (i in `top_k`).
@@ -90,15 +86,14 @@ def classification_accuracy(
         top_k = [1]
     max_k = max(top_k)
 
-    probabilities = torch.softmax(logits, dim=-1)
+    with torch.no_grad():
+        _, predictions = logits.topk(max_k, dim=1, largest=True, sorted=True)
+        predictions = predictions.t()
+        correct = predictions.eq(targets.view(1, -1)).expand_as(predictions)
 
-    _, predictions = probabilities.topk(max_k, dim=1, largest=True, sorted=True)
-    predictions = predictions.t()
-    correct = predictions.eq(targets.view(1, -1)).expand_as(predictions)
-
-    results = []
-    for k in top_k:
-        correct_k = correct[:k].view(-1).float().mean().to('cpu').numpy()
-        results.append(correct_k)
+        results = []
+        for k in top_k:
+            correct_k = correct[:k].view(-1).float().mean().to('cpu').numpy()
+            results.append(correct_k)
 
     return results
