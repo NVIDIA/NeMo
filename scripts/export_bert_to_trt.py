61b47d30acfd8e9542470bb410d9eec54158d7e9
Purnendu Mukherjee <purnendu@rechargers.co.in>
2020-02-20 08:03:00 -0800

    Adding BERT QA export to TRT (#379)
    * Adding squad QnA for conversion
    
    * adding mlp.layer0 with prefix
    
    * qa_prefix bug fix
    
    * Remove prefix from init_dict index name as per NeMo checkpoints
    
    * Code formatting
    
    Signed-off-by: Purnendu Mukherjee <purnendu@rechargers.co.in>

--- scripts/export_bert_to_trt.py
+++ scripts/export_bert_to_trt.py
@@ -243,22 +243,6 @@ def bert_pooler(prefix, init_dict, network, input_tensor):
     return pooler.get_output(0)
 
 
-def squad_output(prefix, init_dict, network, input_tensor):
-    """
-    Create the squad output
-    """
-
-    idims = input_tensor.shape
-    assert len(idims) == 5
-
-    W_out = init_dict[prefix + SQD_W]
-    B_out = init_dict[prefix + SQD_B]
-
-    dense = network.add_fully_connected(input_tensor, 2, W_out, B_out)
-    set_layer_name(dense, prefix, "dense")
-    return dense
-
-
 def sequence_class_output(prefix, init_dict, network, input_tensor, softmax=True):
     logging.info(input_tensor.shape)
     seq_len = input_tensor.shape[1]
@@ -321,6 +305,22 @@ def token_class_output(prefix, init_dict, network, input_tensor, softmax=True):
 
     logging.info("tok class: ", classifier.get_output(0).shape)
     return classifier
+
+
+def squad_output(prefix, init_dict, network, input_tensor):
+    """
+    Create the squad output
+    """
+
+    idims = input_tensor.shape
+    assert len(idims) == 5
+
+    W_out = init_dict["mlp.layer0." + SQD_W]
+    B_out = init_dict["mlp.layer0." + SQD_B]
+
+    dense = network.add_fully_connected(input_tensor, 2, W_out, B_out)
+    set_layer_name(dense, prefix, "dense")
+    return dense
 
 
 def load_weights(inputbase):
@@ -406,6 +406,7 @@ def main(
     max_batch=None,
     seq_class_prefix=None,
     tok_class_prefix=None,
+    qa_prefix=None,
 ):
     bert_config_path = config_path
     TRT_LOGGER.log(TRT_LOGGER.INFO, bert_config_path)
@@ -483,6 +484,13 @@ def main(
                 seq_class_logits_out.dtype = trt.DataType.FLOAT
                 network.mark_output(seq_class_logits_out)
 
+            if qa_prefix is not None:
+                qa_logits = squad_output(qa_prefix, classifiers_dict, network, bert_out)
+                qa_logits_out = qa_logits.get_output(0)
+                qa_logits_out.name = "qa_logits"
+                qa_logits_out.dtype = trt.DataType.FLOAT
+                network.mark_output(qa_logits_out)
+
             with builder.build_engine(network, builder_config) as engine:
                 TRT_LOGGER.log(TRT_LOGGER.VERBOSE, "Serializing Engine...")
                 serialized_engine = engine.serialize()
@@ -505,7 +513,9 @@ if __name__ == "__main__":
     parser.add_argument(
         '-s', '--seq-classifier', required=False, default=None, help="Name of the sequence classifier",
     )
-
+    parser.add_argument(
+        '-qa', '--qa', required=False, default=None, help="Name of the Question Answering classifier",
+    )
     parser.add_argument(
         '-o', '--output', required=True, help='The bert engine file, ex bert.engine',
     )
@@ -554,6 +564,7 @@ if __name__ == "__main__":
     config_path = opt.config
     logging.info("token class:", opt.token_classifier)
     logging.info("seq class:  ", opt.seq_classifier)
+    logging.info("QA class:  ", opt.qa)
     main(
         opt.bert_weight,
         opt.class_weight,
@@ -565,4 +576,5 @@ if __name__ == "__main__":
         max_batch=opt.max_batch_size,
         tok_class_prefix=opt.token_classifier,
         seq_class_prefix=opt.seq_classifier,
+        qa_prefix=opt.qa,
     )
