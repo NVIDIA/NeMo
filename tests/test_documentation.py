795e095301c18637e3201468c62c5e18452006ad
Tomasz Kornuta <tkornuta@nvidia.com>
2020-02-25 15:11:04 -0800

    polish
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- tests/test_documentation.py
+++ tests/test_documentation.py
@@ -16,6 +16,7 @@ class DocTest(unittest.TestCase):
     all_files = True
 
     def test_html_documentation(self):
+        """ Tests whether the HTML documentation can be build properly. """
         app = Sphinx(
             self.source_dir,
             self.config_dir,
@@ -25,25 +26,3 @@ class DocTest(unittest.TestCase):
             warningiserror=True,
         )
         app.build(force_all=self.all_files)
-        # TODO: additional checks here if needed
-
-    def test_text_documentation(self):
-        # The same, but with different buildername
-        app = Sphinx(
-            self.source_dir,
-            self.config_dir,
-            self.output_dir,
-            self.doctree_dir,
-            buildername='text',
-            warningiserror=False,
-        )
-        app.build(force_all=self.all_files)
-        # TODO:  additional checks if needed
-
-    def tearDown(self):
-        # TODO: clean up the output directory
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
