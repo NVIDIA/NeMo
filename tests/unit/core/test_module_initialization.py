a75cf3f0283fc4d82d916c02e725662c0f52006f
Tomasz Kornuta <tkornuta@nvidia.com>
2020-02-26 16:16:04 -0800

    neural types unit tests
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- tests/unit/core/test_module_initialization.py
+++ tests/unit/core/test_module_initialization.py
@@ -21,20 +21,21 @@ from unittest import TestCase
 
 import pytest
 
-import nemo
 from nemo.backends.pytorch.nm import TrainableNM
+from nemo.backends.pytorch.tutorials import TaylorNet
+from nemo.core.neural_modules import NmTensor
 from nemo.core.neural_types import ChannelType, NeuralType
-from tests.common_setup import NeMoUnitTest
 
 
-class ModuleInitializationTestCase(NeMoUnitTest):
+@pytest.mark.usefixtures("neural_factory")
+class ModuleInitializationTestCase(TestCase):
     class TestNM1(TrainableNM):
         def __init__(self, var1=1, var2=2, var3=3):
-            super(TestNM1, self).__init__()
+            super().__init__()
 
     class TestNM2(TestNM1):
         def __init__(self, var2):
-            super(TestNM2, self).__init__(var2=var2)
+            super().__init__(var2=var2)
 
     def setUp(self) -> None:
         super().setUp()
@@ -43,6 +44,7 @@ class ModuleInitializationTestCase(NeMoUnitTest):
         ModuleInitializationTestCase.TestNM1.__abstractmethods__ = set()
         ModuleInitializationTestCase.TestNM2.__abstractmethods__ = set()
 
+    @pytest.mark.unit
     def test_default_init_params(self):
         simple_nm = ModuleInitializationTestCase.TestNM1(var1=1)
         init_params = simple_nm.init_params
@@ -50,6 +52,7 @@ class ModuleInitializationTestCase(NeMoUnitTest):
         self.assertEqual(init_params["var2"], 2)
         self.assertEqual(init_params["var3"], 3)
 
+    @pytest.mark.unit
     def test_simple_init_params(self):
         simple_nm = ModuleInitializationTestCase.TestNM1(var1=10, var3=30)
         init_params = simple_nm.init_params
@@ -57,24 +60,27 @@ class ModuleInitializationTestCase(NeMoUnitTest):
         self.assertEqual(init_params["var2"], 2)
         self.assertEqual(init_params["var3"], 30)
 
+    @pytest.mark.unit
     def test_nested_init_params(self):
         simple_nm = ModuleInitializationTestCase.TestNM2(var2="hello")
         init_params = simple_nm.init_params
         self.assertEqual(init_params["var2"], "hello")
 
+    @pytest.mark.unit
     def test_constructor_TaylorNet(self):
-        tn = nemo.backends.pytorch.tutorials.TaylorNet(dim=4)
+        tn = TaylorNet(dim=4)
         self.assertEqual(tn.init_params["dim"], 4)
 
+    @pytest.mark.unit
     def test_call_TaylorNet(self):
-        x_tg = nemo.core.neural_modules.NmTensor(
+        x_tg = NmTensor(
             producer=None,
             producer_args=None,
             name=None,
             ntype=NeuralType(elements_type=ChannelType(), axes=('B', 'D')),
         )
 
-        tn = nemo.backends.pytorch.tutorials.TaylorNet(dim=4)
+        tn = TaylorNet(dim=4)
         # note that real port's name: x was used
         y_pred = tn(x=x_tg)
         self.assertEqual(y_pred.producer, tn)
