2e6b52e20a045de9aa8c060e5e07eb76eb709287
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2020-02-28 10:46:26 -0800

    fix styles
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- examples/asr/minimal_asr.py
+++ examples/asr/minimal_asr.py
@@ -1,10 +1,19 @@
 # this is nemo's "core" package
+from functools import partial
+
+from ruamel.yaml import YAML
+
 import nemo
+
 # this is nemos's ASR collection of speech-recognition related Neural Modules
 import nemo.collections.asr as nemo_asr
+from nemo.collections.asr.helpers import monitor_asr_train_progress
+
+######### AFTER ###############################
+from nemo.core.composite_neural_module import CompositeNeuralModule
+
 train_manifest = "/mnt/D1/Data/an4_dataset/an4_train.json"
 val_manifest = "/mnt/D1/Data/an4_dataset/an4_val.json"
-from ruamel.yaml import YAML
 model_config_file = "/home/okuchaiev/repos/NeMo/examples/asr/configs/jasper_an4.yaml"
 
 yaml = YAML(typ="safe")
@@ -14,57 +23,53 @@ with open(model_config_file) as f:
 vocab = jasper_params['labels']
 
 nf = nemo.core.NeuralModuleFactory()
-data_layer = nemo_asr.AudioToTextDataLayer.import_from_config(model_config_file,
-    "AudioToTextDataLayer_train", overwrite_params={"manifest_filepath": train_manifest, "batch_size": 16})
+data_layer = nemo_asr.AudioToTextDataLayer.import_from_config(
+    model_config_file,
+    "AudioToTextDataLayer_train",
+    overwrite_params={"manifest_filepath": train_manifest, "batch_size": 16},
+)
 
 data_preprocessor = nemo_asr.AudioToMelSpectrogramPreprocessor.import_from_config(
-        model_config_file, "AudioToMelSpectrogramPreprocessor")
+    model_config_file, "AudioToMelSpectrogramPreprocessor"
+)
 
 jasper_encoder = nemo_asr.JasperEncoder.import_from_config(model_config_file, "JasperEncoder")
 jasper_decoder = nemo_asr.JasperDecoderForCTC.import_from_config(
-        model_config_file, "JasperDecoderForCTC", overwrite_params={"num_classes": len(vocab)}
-    )
+    model_config_file, "JasperDecoderForCTC", overwrite_params={"num_classes": len(vocab)}
+)
 ctc_loss = nemo_asr.CTCLossNM(num_classes=len(vocab))
 greedy_decoder = nemo_asr.GreedyCTCDecoder()
+
+######### BEFORE ###############################
+
 # audio_signal, audio_signal_len, transcript, transcript_len = data_layer()
 # processed_signal, processed_signal_len = data_preprocessor(input_signal=audio_signal, length=audio_signal_len)
 # encoded, encoded_len = jasper_encoder(audio_signal=processed_signal, length=processed_signal_len)
 # log_probs = jasper_decoder(encoder_output=encoded)
-from nemo.core.composite_neural_module import CompositeNeuralModule
-# with CompositeNeuralModule(modules=[data_preprocessor, jasper_encoder, jasper_decoder]) as cm:
-#     processed_signal, processed_signal_len = data_preprocessor(input_signal=audio_signal,
-#                                                                length=audio_signal_len)
-#     encoded, encoded_len = jasper_encoder(audio_signal=processed_signal, length=processed_signal_len)
-#     log_probs = jasper_decoder(encoder_output=encoded)
+
+
 def pipeline(audio_signal, audio_signal_len):
-    processed_signal, processed_signal_len = data_preprocessor(input_signal=audio_signal,
-                                                               length=audio_signal_len)
+    processed_signal, processed_signal_len = data_preprocessor(input_signal=audio_signal, length=audio_signal_len)
     encoded, encoded_len = jasper_encoder(audio_signal=processed_signal, length=processed_signal_len)
     log_probs = jasper_decoder(encoder_output=encoded)
     return log_probs, encoded_len
 
-Jasper = CompositeNeuralModule(modules=[data_preprocessor, jasper_encoder, jasper_decoder], pipeline=pipeline)
 
+Jasper = CompositeNeuralModule(modules=[data_preprocessor, jasper_encoder, jasper_decoder], pipeline=pipeline)
 audio_signal, audio_signal_len, transcript, transcript_len = data_layer()
 log_probs, encoded_len = Jasper(audio_signal, audio_signal_len)
 
 predictions = greedy_decoder(log_probs=log_probs)
-loss = ctc_loss(log_probs=log_probs, targets=transcript,
-                input_length=encoded_len, target_length=transcript_len)
-tensors_to_evaluate=[loss, predictions, transcript, transcript_len]
+loss = ctc_loss(log_probs=log_probs, targets=transcript, input_length=encoded_len, target_length=transcript_len)
+tensors_to_evaluate = [loss, predictions, transcript, transcript_len]
 
-from nemo.collections.asr.helpers import monitor_asr_train_progress
-from functools import partial
 train_callback = nemo.core.SimpleLossLoggerCallback(
-    tensors=tensors_to_evaluate,
-    print_func=partial(monitor_asr_train_progress, labels=vocab))
+    tensors=tensors_to_evaluate, print_func=partial(monitor_asr_train_progress, labels=vocab)
+)
 
 nf.train(
     tensors_to_optimize=[loss],
     optimizer="novograd",
     callbacks=[train_callback],
-    optimization_params={
-            "num_epochs": 50,
-            "lr": 0.01
-    }
+    optimization_params={"num_epochs": 50, "lr": 0.01},
 )
--- nemo/core/composite_neural_module.py
+++ nemo/core/composite_neural_module.py
@@ -1,23 +1,47 @@
-from typing import Set, Dict, Tuple, Optional, List
-import uuid
-from nemo.core import NeuralModule, WeightShareTransform, NeuralType
+# ! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2019-, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, List, Optional, Set, Tuple
+
+from nemo.core import NeuralModule, NeuralType, WeightShareTransform
 
 
 class CompositeNeuralModule(NeuralModule):
-    def __init__(self, modules, pipeline):
+    def __init__(
+        self,
+        modules: List[NeuralModule],
+        pipeline,
+        input_ports: Optional[Dict[str, NeuralType]] = None,
+        output_ports: Optional[Dict[str, NeuralType]] = None,
+    ):
         self.modules = modules
         self.pipeline = pipeline
-        self.__input_ports = modules[0].input_ports
-        self.__output_ports = modules[-1].output_ports
+        if input_ports is None:
+            self.__input_ports = modules[0].input_ports
+        else:
+            self.__input_ports = input_ports
+
+        if output_ports is None:
+            self.__output_ports = modules[-1].output_ports
+        else:
+            self.__output_ports = output_ports
 
     def __call__(self, *args, **kwargs):
         return self.pipeline(*args, **kwargs)
-
-    # def __enter__(self):
-    #     return self
-    #
-    # def __exit__(self, type, value, traceback):
-    #     return None
 
     @property
     def input_ports(self) -> Optional[Dict[str, NeuralType]]:
@@ -28,28 +52,47 @@ class CompositeNeuralModule(NeuralModule):
         return self.__output_ports
 
     def get_weights(self) -> Optional[Dict[(str, bool)]]:
-        pass
+        raise NotImplemented("Not implemented for composite modules")
 
-    def set_weights(self, name2weight: Dict[(str, Tuple[str, bool])],
-                    name2name_and_transform: Dict[(str, Tuple[str, WeightShareTransform])] = None):
-        pass
+    def set_weights(
+        self,
+        name2weight: Dict[(str, Tuple[str, bool])],
+        name2name_and_transform: Dict[(str, Tuple[str, WeightShareTransform])] = None,
+    ):
+        raise NotImplemented("Not implemented for composite modules")
 
-    def tie_weights_with(self, module, weight_names=List[str],
-                         name2name_and_transform: Dict[(str, Tuple[str, WeightShareTransform])] = None):
-        pass
+    def tie_weights_with(
+        self,
+        module,
+        weight_names=List[str],
+        name2name_and_transform: Dict[(str, Tuple[str, WeightShareTransform])] = None,
+    ):
+        raise NotImplemented("Not implemented for composite modules")
 
     def save_to(self, path: str):
+        # TODO
         pass
 
     def restore_from(self, path: str):
+        # TODO
         pass
 
     def freeze(self, weights: Set[str] = None):
-        pass
+        if weights is None:
+            for module in self.modules:
+                module.freeze()
+        else:
+            raise NotImplemented("For composite modules, freeze only works for all weights (e.g. weights=None)")
 
     def unfreeze(self, weights: Set[str] = None):
-        pass
+        if weights is None:
+            for module in self.modules:
+                module.unfreeze()
+        else:
+            raise NotImplemented("For composite modules, unfreeze only works for all weights (e.g. weights=None)")
 
     @property
     def num_weights(self):
-        pass
+        num_weights = 0
+        for module in self.modules:
+            num_weights += module.num_weights
