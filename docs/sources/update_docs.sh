b543bca2a50447386583e17c3c4719e7c2b4de30
Tomasz Kornuta <tkornuta@nvidia.com>
2020-03-31 15:56:09 -0700

    Fast fixes
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- docs/sources/update_docs.sh
+++ docs/sources/update_docs.sh
@@ -6,8 +6,8 @@ cp -r build/html ../html
 make clean
 
 # generate mandarin doc
-echo "begin to generate mandarin html"
-cd ../docs_zh/sources
-./update_docs.sh
-cp -r ../zh ../../html/
-echo "Finish"
+#echo "begin to generate mandarin html"
+#cd ../docs_zh/sources
+#./update_docs.sh
+#cp -r ../zh ../../html/
+#echo "Finish"
