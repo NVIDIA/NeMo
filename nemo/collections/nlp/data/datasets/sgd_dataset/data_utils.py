35c5eefe448fc37975056022a10a195933122069
Evelina Bakhturina <ebakhturina@nvidia.com>
2020-04-07 16:37:49 -0700

    add check
    Signed-off-by: Evelina Bakhturina <ebakhturina@nvidia.com>

--- nemo/collections/nlp/data/datasets/sgd_dataset/data_utils.py
+++ nemo/collections/nlp/data/datasets/sgd_dataset/data_utils.py
@@ -97,7 +97,6 @@ class Dstc8DataProcessor(object):
 
         self.dstc8_data_dir = dstc8_data_dir
         self.dialogues_examples_dir = dialogues_example_dir
-        os.makedirs(dialogues_example_dir, exist_ok=True)
 
         self._log_data_warnings = log_data_warnings
         self._task_name = task_name
@@ -147,7 +146,7 @@ class Dstc8DataProcessor(object):
                 logging.debug(f"Finish generating the dialogue examples for {dataset} dataset.")
 
     def get_dialog_examples(self, dataset):
-        if (self._task_name, dataset) not in self.dial_files:
+        if (self._task_name, dataset) not in self.dial_files or not os.path.exists(self.dial_files[(self._task_name, dataset)]):
             raise ValueError(
                 f"{dataset} dialogue examples were not processed for {self._task_name} task. Re-initialize Dstc8DataProcessor and add {dataset} dataset to datasets arg."
             )
