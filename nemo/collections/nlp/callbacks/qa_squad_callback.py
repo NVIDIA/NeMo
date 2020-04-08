71e40ff32ef1352692ddee256781f58f86b699d8
Yang Zhang <yzhang123@users.noreply.github.com>
2020-04-08 15:31:41 -0700

    fix qa issues (#566)
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/callbacks/qa_squad_callback.py
+++ nemo/collections/nlp/callbacks/qa_squad_callback.py
@@ -56,7 +56,7 @@ def eval_epochs_done_callback(
     version_2_with_negative,
     null_score_diff_threshold,
 ):
-    exact_match, f1, _ = eval_data_layer.dataset.evaluate(
+    exact_match, f1, _, _ = eval_data_layer.dataset.evaluate(
         unique_ids=global_vars["eval_unique_ids"],
         start_logits=global_vars["eval_start_logits"],
         end_logits=global_vars["eval_end_logits"],
