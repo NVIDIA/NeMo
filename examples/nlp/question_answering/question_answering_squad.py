fa1d350decd7db594cc672373277852b722da116
Yang Zhang <yzhang123@users.noreply.github.com>
2020-04-20 16:21:25 -0700

    change default optimizer in squad (#583)
    * change default optimizer in squad and add warning to usage of fused optimizers

--- examples/nlp/question_answering/question_answering_squad.py
+++ examples/nlp/question_answering/question_answering_squad.py
@@ -136,7 +136,7 @@ def parse_args():
         choices=["nemobert", "sentencepiece"],
         help="tokenizer to use, only relevant when using custom pretrained checkpoint.",
     )
-    parser.add_argument("--optimizer", default="fused_adam", type=str, help="Optimizer kind")
+    parser.add_argument("--optimizer", default="adam_w", type=str, help="Optimizer kind")
     parser.add_argument("--lr_policy", default="WarmupAnnealing", type=str)
     parser.add_argument("--lr", default=3e-5, type=float, help="The initial learning rate.")
     parser.add_argument("--lr_warmup_proportion", default=0.0, type=float)
