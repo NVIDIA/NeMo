314cf1029b6d0619427d17b7fec36effe783d232
Tomasz Kornuta <tkornuta@nvidia.com>
2020-04-27 17:12:34 -0700

    Unification of singleton meta class
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- nemo/utils/app_state.py
+++ nemo/utils/app_state.py
@@ -15,33 +15,9 @@
 # limitations under the License.
 # =============================================================================
 
-import threading
-
 # Sadly have to import this to avoid circular dependencies.
 import nemo
-
-
-class Singleton(type):
-    """ Implementation of a generic, tread-safe singleton meta-class.
-        Can be used as meta-class, i.e. will create 
-    """
-
-    # List of instances - one per class.
-    __instances = {}
-    # Lock used for accessing the instance.
-    __lock = threading.Lock()
-
-    def __call__(cls, *args, **kwargs):
-        """ Returns singleton instance.A thread safe implementation. """
-        if cls not in cls.__instances:
-            # Enter critical section.
-            with cls.__lock:
-                # Check once again.
-                if cls not in cls.__instances:
-                    # Create a new object instance - one per class.
-                    cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        # Return the instance.
-        return cls.__instances[cls]
+from nemo.utils.metaclasses import Singleton
 
 
 class AppState(metaclass=Singleton):
--- nemo/utils/metaclasses.py
+++ nemo/utils/metaclasses.py
@@ -13,17 +13,30 @@
 # limitations under the License.****
 
 __all__ = [
-    "SingletonMetaClass",
+    "Singleton",
 ]
 
+import threading
 
-class SingletonMetaClass(type):
 
-    _instances = {}
+class Singleton(type):
+    """ Implementation of a generic, tread-safe singleton meta-class.
+        Can be used as meta-class, i.e. will create 
+    """
+
+    # List of instances - one per class.
+    __instances = {}
+    # Lock used for accessing the instance.
+    __lock = threading.Lock()
 
     def __call__(cls, *args, **kwargs):
-
-        if cls not in cls._instances:
-            cls._instances[cls] = super(SingletonMetaClass, cls).__call__(*args, **kwargs)
-
-        return cls._instances[cls]
+        """ Returns singleton instance.A thread safe implementation. """
+        if cls not in cls.__instances:
+            # Enter critical section.
+            with cls.__lock:
+                # Check once again.
+                if cls not in cls.__instances:
+                    # Create a new object instance - one per class.
+                    cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        # Return the instance.
+        return cls.__instances[cls]
