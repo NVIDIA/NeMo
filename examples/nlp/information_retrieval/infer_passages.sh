c7dc0f243c0dd852ded4178363d5ee462e2247da
AlexGrinch <grinchuk.alexey@gmail.com>
2020-05-21 04:08:30 -0700

    added infer

--- examples/nlp/information_retrieval/infer_passages.sh
+++ examples/nlp/information_retrieval/infer_passages.sh
@@ -1,4 +1,4 @@
-for chunk_id in {0..9}
+for chunk_id in {0..19}
 do
   python -m torch.distributed.launch --nproc_per_node=8 dpr_infer.py --data_dir /infer --batch_size 1024 --restore_path=/result/dpr_reranking/checkpoints/PassageBERT-STEP-$1.pt --data_for_eval passages --work_dir /result/dpr_reranking --chunk_id ${chunk_id}
 done
