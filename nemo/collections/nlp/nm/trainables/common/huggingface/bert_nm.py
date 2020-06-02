f64a8c0e33e698e1deeae3b59861c1d9ca19821b
Yang Zhang <yangzhang@nvidia.com>
2020-06-02 11:49:42 -0700

    fix style
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/nm/trainables/common/huggingface/bert_nm.py
+++ nemo/collections/nlp/nm/trainables/common/huggingface/bert_nm.py
@@ -152,7 +152,10 @@ class BERT(TrainableNM):
         pretrained_models = []
         for key in BERT_PRETRAINED_MODEL_ARCHIVE_LIST:
             model_info = PretrainedModelInfo(
-                pretrained_model_name=key, description="weights by HuggingFace", parameters=BERT_PRETRAINED_CONFIG_ARCHIVE_MAP[key], location="",
+                pretrained_model_name=key,
+                description="weights by HuggingFace",
+                parameters=BERT_PRETRAINED_CONFIG_ARCHIVE_MAP[key],
+                location="",
             )
             pretrained_models.append(model_info)
         return pretrained_models
