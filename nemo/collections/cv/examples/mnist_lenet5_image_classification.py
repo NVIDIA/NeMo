6fd38edd00c61b84263652c50cf01f54ffa918c3
Tomasz Kornuta <tkornuta@nvidia.com>
2020-06-13 13:26:18 -0700

    backwards working - without automatic tensor collection
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- nemo/collections/cv/examples/mnist_lenet5_image_classification.py
+++ nemo/collections/cv/examples/mnist_lenet5_image_classification.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
         outputs = training_graph.forward(batch)
         print("outputs: ", outputs.loss)
 
-        training_graph.backward()
+        training_graph.backward(losses=[outputs.loss])
 
     # Invoke the "train" action.
     # nf.train(
