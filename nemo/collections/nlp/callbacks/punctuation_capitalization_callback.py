0433cd2cc5b895be51451b3b3ace780285626545
Evelina Bakhturina <ebakhturina@nvidia.com>
2020-06-18 05:31:05 -0700

    fix
    Signed-off-by: Evelina Bakhturina <ebakhturina@nvidia.com>

--- nemo/collections/nlp/callbacks/punctuation_capitalization_callback.py
+++ nemo/collections/nlp/callbacks/punctuation_capitalization_callback.py
@@ -92,17 +92,14 @@ def eval_epochs_done_callback(global_vars, punct_label_ids, capit_label_ids, gra
     punct_accuracy, punct_class_report = _eval_epochs_done_callback('punct', global_vars, punct_label_ids, graph_fold, normalize_cm)
 
     for label in punct_class_report:
-        
-        label_name = label[: label.index('(label id') - 1] if 'label id' in label else label
-        print ('label name:', label_name)
-        print(punct_class_report)
-        if label in punct_class_report and 'f1-score' in punct_class_report[label]:
+        if label != 'accuracy':
+            label_name = label[: label.index('(label id') - 1] if 'label id' in label else label
             results['punct F1 ' + label_name] = round(punct_class_report[label]['f1-score'] * 100, 2)
     
     capit_accuracy, capit_class_report = _eval_epochs_done_callback('capit', global_vars, capit_label_ids, graph_fold, normalize_cm)
     for label in capit_class_report:
-        label_name = label[: label.index('(label id') - 1] if 'label id' in label else label
-        if label in capit_class_report and 'f1-score' in capit_class_report[label]:
+        if label != 'accuracy':
+            label_name = label[: label.index('(label id') - 1] if 'label id' in label else label
             results['capit F1: ' + label_name] = round(capit_class_report[label]['f1-score'] * 100, 2)   
     
     # {"Punct_accuracy": punct_accuracy, "Capit_acc": capit_accuracy}
