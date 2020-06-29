5327e20f776a9e093a1240b2dc26f2af63fb5e8d
ericharper <complex451@gmail.com>
2020-06-29 11:52:44 -0600

    renamed from spech2text.py
    Signed-off-by: ericharper <complex451@gmail.com>

--- examples/asr/speech_to_text.py
+++ examples/asr/speech_to_text.py
@@ -13,50 +13,50 @@
 # limitations under the License.
 
 # TODO: This is WIP and needs a lot of polishing
+# python examples/asr/spech2text.py --asr_model=examples/asr/bad_asr_config.yaml --train_data=/Users/okuchaiev/Data/an4_dataset/an4_train.json --eval_dataset=/Users/okuchaiev/Data/an4_dataset/an4_val.json
+# python spech2text.py --asr_model=./bad_asr_config.yaml --train_data=./an4/train_manifest.json --eval_dataset=./an4/test_manifest.json
+
 from argparse import ArgumentParser
 
 import pytorch_lightning as pl
 from ruamel.yaml import YAML
 
 from nemo.collections.asr.models import EncDecCTCModel
+from nemo.collections.asr.arguments import add_asr_args
+
 
 def main(args):
-    
-    # Instantiate the model which we'll train
-    if args.asr_model.endswith('.yaml'):
-        print(f"SpeechToText: Will train from scratch using config from {args.asr_model}")
-        yaml = YAML(typ="safe")
-        with open(args.asr_model) as f:
-            model_config = yaml.load(f)
-    else:
-        raise NotImplemented("from_pretrained not implemented yet")
+
+    yaml = YAML(typ="safe")
+    with open(args.asr_model) as f:
+        model_config = yaml.load(f)
 
     asr_model = EncDecCTCModel(
-        preprocessor_params=model_config['AudioToMelSpectrogramPreprocessor'],
-        encoder_params=model_config['JasperEncoder'],
-        decoder_params=model_config['JasperDecoder'],
+        preprocessor_params=model_config['preprocessor_params'],
+        encoder_params=model_config['encoder_params'],
+        decoder_params=model_config['decoder_params'],
+        spec_augment_params=model_config.get('spec_augment_params', None),
     )
 
     # Setup where your training data is
+    model_config['AudioToTextDataLayer']['manifest_filepath'] = args.train_dataset
+    model_config['AudioToTextDataLayer_eval']['manifest_filepath'] = args.eval_dataset
     asr_model.setup_training_data(model_config['AudioToTextDataLayer'])
     asr_model.setup_validation_data(model_config['AudioToTextDataLayer_eval'])
     asr_model.setup_optimization(optim_params={'lr': 0.0003})
     # trainer = pl.Trainer(
-    #    val_check_interval=5, amp_level='O1', precision=16, gpus=2, max_epochs=30, distributed_backend='ddp'
+    #     val_check_interval=1, amp_level='O1', precision=16, gpus=4, max_epochs=123, distributed_backend='ddp'
     # )
-    trainer = pl.Trainer(val_check_interval=5, max_epochs=100, gpus=4, distributed_backend='ddp')
+    #trainer = pl.Trainer(val_check_interval=5, max_epochs=args.num_epochs)
+    trainer = pl.Trainer.from_argparse_args(args)
     trainer.fit(asr_model)
 
-if __name__ == '__main__':
-    parent_parser = ArgumentParser(add_help=False) 
-    parent_parser.add_argument(
-        "--asr_model",
-        type=str,
-        default="QuartzNet15x5-En",
-        required=True,
-        help="Pass: 'QuartzNet15x5-En', 'QuartzNet15x5-Zh', or 'JasperNet10x5-En' to train from pre-trained models. To train from scratch pass path to modelfile ending with .yaml.",
-    )
 
-    args = parent_parser.parse_args()
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser = add_asr_args(parser) 
+
+    args = parser.parse_args()
 
     main(args)
