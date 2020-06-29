280fa66acbc4320428335bdad376a55e459b5e37
ericharper <complex451@gmail.com>
2020-06-29 17:47:35 -0600

    style fix
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/core/__init__.py
+++ nemo/core/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.core.classes import *
 import nemo.core.neural_types
+from nemo.core.classes import *
