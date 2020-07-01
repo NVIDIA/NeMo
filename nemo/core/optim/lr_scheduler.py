0fc969c2a3504ac0286889e721a4dc91bbcd07c0
Somshubra Majumdar <titu1994@gmail.com>
2020-07-01 15:55:30 -0700

    Add schedulers
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/core/optim/lr_scheduler.py
+++ nemo/core/optim/lr_scheduler.py
@@ -12,25 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import inspect
 import math
-import sys
 import warnings
 
 from torch.optim.lr_scheduler import _LRScheduler
 
-#
-# __all__ = [
-#     'WarmupPolicy',
-#     'WarmupHoldPolicy',
-#     'SquareAnnealing',
-#     'CosineAnnealing',
-#     'WarmupAnnealing',
-#     'InverseSquareRootAnnealing',
-#     'SquareRootAnnealing',
-#     'PolynomialDecayAnnealing',
-#     'PolynomialHoldDecayAnnealing',
-# ]
+__all__ = [
+    'WarmupPolicy',
+    'WarmupHoldPolicy',
+    'SquareAnnealing',
+    'CosineAnnealing',
+    'WarmupAnnealing',
+    'InverseSquareRootAnnealing',
+    'SquareRootAnnealing',
+    'PolynomialDecayAnnealing',
+    'PolynomialHoldDecayAnnealing',
+]
 
 
 class WarmupPolicy(_LRScheduler):
@@ -49,8 +46,8 @@ class WarmupPolicy(_LRScheduler):
         ), "Either use particular number of step or ratio"
         assert warmup_ratio is None or total_steps is not None, "If there is a ratio, there should be a total steps"
 
-        super().__init__(optimizer, last_epoch)
-
+        # It is necessary to assign all attributes *before* __init__,
+        # as class is wrapped by inner class.
         self.total_steps = total_steps
         if warmup_steps is not None:
             self.warmup_steps = warmup_steps
@@ -58,6 +55,8 @@ class WarmupPolicy(_LRScheduler):
             self.warmup_steps = int(warmup_ratio * total_steps)
         else:
             self.warmup_steps = 0
+
+        super().__init__(optimizer, last_epoch)
 
     def get_lr(self):
         if not self._get_lr_called_within_step:
@@ -107,11 +106,18 @@ class WarmupHoldPolicy(WarmupPolicy):
     ):
         assert not (hold_steps is not None and hold_ratio is not None), "Either use particular number of step or ratio"
         assert hold_ratio is None or total_steps is not None, "If there is a ratio, there should be a total steps"
-        super().__init__(optimizer, warmup_steps=warmup_steps, warmup_ratio=warmup_ratio, total_steps=total_steps,
-                         last_epoch=last_epoch)
 
         self._min_lr = min_lr
         self._last_warmup_lr = 0.0
+
+        # Necessary to duplicate as class attributes are hidden in inner class
+        self.total_steps = total_steps
+        if warmup_steps is not None:
+            self.warmup_steps = warmup_steps
+        elif warmup_ratio is not None:
+            self.warmup_steps = int(warmup_ratio * total_steps)
+        else:
+            self.warmup_steps = 0
 
         if hold_steps is not None:
             self.hold_steps = hold_steps + self.warmup_steps
@@ -119,6 +125,14 @@ class WarmupHoldPolicy(WarmupPolicy):
             self.hold_steps = int(hold_ratio * total_steps) + self.warmup_steps
         else:
             self.hold_steps = 0
+
+        super().__init__(
+            optimizer,
+            warmup_steps=warmup_steps,
+            warmup_ratio=warmup_ratio,
+            total_steps=total_steps,
+            last_epoch=last_epoch,
+        )
 
     def get_lr(self):
         if not self._get_lr_called_within_step:
@@ -141,161 +155,163 @@ class WarmupHoldPolicy(WarmupPolicy):
             return [0.0 for _ in self.base_lrs]
 
         return self._get_lr(step)
-#
-#
-# def _squareroot_annealing(initial_lr, step, total_steps, min_lr):
-#     mult = ((total_steps - step) / total_steps) ** 0.5
-#     out_lr = initial_lr * mult
-#     out_lr = max(out_lr, min_lr)
-#     return out_lr
-#
-#
-# def _square_annealing(initial_lr, step, total_steps, min_lr):
-#     mult = ((total_steps - step) / total_steps) ** 2
-#     out_lr = initial_lr * mult
-#     out_lr = max(out_lr, min_lr)
-#     return out_lr
-#
-#
-# def _cosine_annealing(initial_lr, step, total_steps, min_lr):
-#     mult = 0.5 * (1 + math.cos(math.pi * step / total_steps))
-#     out_lr = (initial_lr - min_lr) * mult + min_lr
-#     return out_lr
-#
-#
-# def _poly_decay(initial_lr, step, decay_steps, power, min_lr, cycle):
-#     if cycle:
-#         multiplier = 1.0 if step == 0 else math.ceil(step / decay_steps)
-#         decay_steps *= multiplier
-#     else:
-#         step = min(step, decay_steps)
-#     p = step / decay_steps
-#     lr = (initial_lr - min_lr) * math.pow(1.0 - p, power)
-#     lr += min_lr
-#     return lr
-#
-#
-# class SquareAnnealing(WarmupPolicy):
-#     def __init__(self, total_steps, min_lr=1e-5, **kwargs):
-#         super().__init__(total_steps=total_steps, **kwargs)
-#         self.min_lr = min_lr
-#
-#     def _get_lr(self, initial_lr, step, epoch):
-#         return _square_annealing(
-#             initial_lr=initial_lr,
-#             step=step - self.warmup_steps,
-#             total_steps=self.total_steps - self.warmup_steps,
-#             min_lr=self.min_lr,
-#         )
-#
-#
-# class SquareRootAnnealing(WarmupPolicy):
-#     def __init__(self, total_steps, min_lr=0, **kwargs):
-#         super().__init__(total_steps=total_steps, **kwargs)
-#         self.min_lr = min_lr
-#
-#     def _get_lr(self, initial_lr, step, epoch):
-#         return _squareroot_annealing(
-#             initial_lr=initial_lr, step=step, total_steps=self.total_steps, min_lr=self.min_lr,
-#         )
-#
-#
-# class CosineAnnealing(WarmupPolicy):
-#     def __init__(self, total_steps, min_lr=0, **kwargs):
-#         self.min_lr = min_lr
-#         super().__init__(total_steps=total_steps, **kwargs)
-#
-#     def _get_lr(self, initial_lr, step, epoch):
-#         if initial_lr < self.min_lr:
-#             raise ValueError(
-#                 f"{self} received an initial learning rate that " f"was lower than the minimum learning rate."
-#             )
-#         return _cosine_annealing(
-#             initial_lr=initial_lr,
-#             step=step - self.warmup_steps,
-#             total_steps=self.total_steps - self.warmup_steps,
-#             min_lr=self.min_lr,
-#         )
-#
-#
-# class WarmupAnnealing(WarmupPolicy):
-#     def __init__(self, total_steps, **kwargs):
-#         super().__init__(total_steps=total_steps, **kwargs)
-#
-#     def _get_lr(self, initial_lr, step, epoch):
-#         progress = float(step / self.total_steps)
-#         warmup_ratio = float(self.warmup_steps / self.total_steps)
-#
-#         mult = max((progress - 1.0) / (warmup_ratio - 1.0), 0.0)
-#         out_lr = initial_lr * mult
-#
-#         return out_lr
-#
-#
-# class InverseSquareRootAnnealing(WarmupPolicy):
-#     def __init__(self, total_steps, **kwargs):
-#         super().__init__(total_steps=total_steps, **kwargs)
-#
-#     def _get_lr(self, initial_lr, step, epoch):
-#         denom = ((step + 1) / (self.warmup_steps + 1)) ** 0.5
-#         out_lr = initial_lr / denom
-#         return out_lr
-#
-#
-# class PolynomialDecayAnnealing(WarmupPolicy):
-#     def __init__(self, total_steps, min_lr=0.0, power=1.0, cycle=False, **kwargs):
-#         super().__init__(total_steps=total_steps, **kwargs)
-#         self.min_lr = min_lr
-#         self.power = power
-#         self.cycle = cycle
-#
-#     def _get_lr(self, initial_lr, step, epoch):
-#         return _poly_decay(
-#             initial_lr,
-#             step=step - self.warmup_steps,
-#             decay_steps=self.total_steps - self.warmup_steps,
-#             power=self.power,
-#             min_lr=self.min_lr,
-#             cycle=self.cycle,
-#         )
-#
-#
-# class PolynomialHoldDecayAnnealing(WarmupHoldPolicy):
-#     def __init__(self, total_steps, min_lr=0.0, power=1.0, cycle=False, **kwargs):
-#         super().__init__(total_steps=total_steps, **kwargs)
-#         self.min_lr = min_lr
-#         self.power = power
-#         self.cycle = cycle
-#
-#     def _get_lr(self, initial_lr, step, epoch):
-#         return _poly_decay(
-#             initial_lr,
-#             step=step - self.hold_steps,
-#             decay_steps=self.total_steps - max(self.warmup_steps, self.hold_steps),
-#             power=self.power,
-#             min_lr=self.min_lr,
-#             cycle=self.cycle,
-#         )
 
 
-def get_all_lr_classes():
-    """ Get all LR classes defined within this module
-    """
-    lr_classes = {}
-    for name, obj in inspect.getmembers(sys.modules[__name__]):
-        if inspect.isclass(obj) and name != 'ABC':
-            lr_classes[name] = obj
-    return lr_classes
+def _squareroot_annealing(initial_lr, step, total_steps, min_lr):
+    mult = ((total_steps - step) / total_steps) ** 0.5
+    out_lr = initial_lr * mult
+    out_lr = max(out_lr, min_lr)
+    return out_lr
 
 
-def get_lr_policy(lr_policy, **kwargs):
-    lr_classes = get_all_lr_classes()
-    if lr_policy not in lr_classes:
-        raise ValueError(
-            f'{lr_policy} is not a supported lr policy. ' f'Supported lr policies are {lr_classes.keys()}.'
-        )
-    return lr_classes[lr_policy](**kwargs)
+def _square_annealing(initial_lr, step, total_steps, min_lr):
+    mult = ((total_steps - step) / total_steps) ** 2
+    out_lr = initial_lr * mult
+    out_lr = max(out_lr, min_lr)
+    return out_lr
 
 
-if __name__ == '__main__':
-    pass
+def _cosine_annealing(initial_lr, step, total_steps, min_lr):
+    mult = 0.5 * (1 + math.cos(math.pi * step / total_steps))
+    out_lr = (initial_lr - min_lr) * mult + min_lr
+    return out_lr
+
+
+def _poly_decay(initial_lr, step, decay_steps, power, min_lr, cycle):
+    if cycle:
+        multiplier = 1.0 if step == 0 else math.ceil(step / decay_steps)
+        decay_steps *= multiplier
+    else:
+        step = min(step, decay_steps)
+    p = step / decay_steps
+    lr = (initial_lr - min_lr) * math.pow(1.0 - p, power)
+    lr += min_lr
+    return lr
+
+
+class SquareAnnealing(WarmupPolicy):
+    def __init__(self, optimizer, *, total_steps, min_lr=1e-5, last_epoch=-1, **kwargs):
+        self.min_lr = min_lr
+
+        super().__init__(optimizer=optimizer, total_steps=total_steps, last_epoch=last_epoch, **kwargs)
+
+    def _get_lr(self, step):
+        new_lrs = [
+            _square_annealing(
+                initial_lr=initial_lr,
+                step=step - self.warmup_steps,
+                total_steps=self.total_steps - self.warmup_steps,
+                min_lr=self.min_lr,
+            )
+            for initial_lr in self.base_lrs
+        ]
+        return new_lrs
+
+
+class SquareRootAnnealing(WarmupPolicy):
+    def __init__(self, optimizer, *, total_steps, min_lr=0, last_epoch=-1, **kwargs):
+        self.min_lr = min_lr
+
+        super().__init__(optimizer=optimizer, total_steps=total_steps, last_epoch=last_epoch, **kwargs)
+
+    def _get_lr(self, step):
+        new_lrs = [
+            _squareroot_annealing(initial_lr=initial_lr, step=step, total_steps=self.total_steps, min_lr=self.min_lr,)
+            for initial_lr in self.base_lrs
+        ]
+        return new_lrs
+
+
+class CosineAnnealing(WarmupPolicy):
+    def __init__(self, optimizer, *, total_steps, min_lr=0, last_epoch=-1, **kwargs):
+        self.min_lr = min_lr
+
+        super().__init__(optimizer=optimizer, total_steps=total_steps, last_epoch=last_epoch, **kwargs)
+
+    def _get_lr(self, step):
+        for initial_lr in self.base_lrs:
+            if initial_lr < self.min_lr:
+                raise ValueError(
+                    f"{self} received an initial learning rate that " f"was lower than the minimum learning rate."
+                )
+
+        new_lrs = [
+            _cosine_annealing(
+                initial_lr=initial_lr,
+                step=step - self.warmup_steps,
+                total_steps=self.total_steps - self.warmup_steps,
+                min_lr=self.min_lr,
+            )
+            for initial_lr in self.base_lrs
+        ]
+        return new_lrs
+
+
+class WarmupAnnealing(WarmupPolicy):
+    def __init__(self, optimizer, *, total_steps, last_epoch=-1, **kwargs):
+        super().__init__(optimizer=optimizer, total_steps=total_steps, last_epoch=last_epoch, **kwargs)
+
+    def _get_lr(self, step):
+        progress = float(step / self.total_steps)
+        warmup_ratio = float(self.warmup_steps / self.total_steps)
+
+        mult = max((progress - 1.0) / (warmup_ratio - 1.0), 0.0)
+        out_lr = [initial_lr * mult for initial_lr in self.base_lrs]
+
+        return out_lr
+
+
+class InverseSquareRootAnnealing(WarmupPolicy):
+    def __init__(self, optimizer, *, total_steps, last_epoch=-1, **kwargs):
+        super().__init__(optimizer=optimizer, total_steps=total_steps, **kwargs, last_epoch=last_epoch)
+
+    def _get_lr(self, step):
+        denom = ((step + 1) / (self.warmup_steps + 1)) ** 0.5
+        out_lr = [initial_lr / denom for initial_lr in self.base_lrs]
+        return out_lr
+
+
+class PolynomialDecayAnnealing(WarmupPolicy):
+    def __init__(self, optimizer, *, total_steps, min_lr=0.0, power=1.0, cycle=False, last_epoch=-1, **kwargs):
+        self.min_lr = min_lr
+        self.power = power
+        self.cycle = cycle
+
+        super().__init__(optimizer=optimizer, total_steps=total_steps, last_epoch=last_epoch, **kwargs)
+
+    def _get_lr(self, step):
+        new_lrs = [
+            _poly_decay(
+                initial_lr,
+                step=step - self.warmup_steps,
+                decay_steps=self.total_steps - self.warmup_steps,
+                power=self.power,
+                min_lr=self.min_lr,
+                cycle=self.cycle,
+            )
+            for initial_lr in self.base_lrs
+        ]
+        return new_lrs
+
+
+class PolynomialHoldDecayAnnealing(WarmupHoldPolicy):
+    def __init__(self, optimizer, *, total_steps, min_lr=0.0, power=1.0, cycle=False, last_epoch=-1, **kwargs):
+        self.min_lr = min_lr
+        self.power = power
+        self.cycle = cycle
+
+        super().__init__(optimizer=optimizer, total_steps=total_steps, last_epoch=last_epoch, **kwargs)
+
+    def _get_lr(self, step):
+        new_lrs = [
+            _poly_decay(
+                initial_lr,
+                step=step - self.hold_steps,
+                decay_steps=self.total_steps - max(self.warmup_steps, self.hold_steps),
+                power=self.power,
+                min_lr=self.min_lr,
+                cycle=self.cycle,
+            )
+            for initial_lr in self.base_lrs
+        ]
+        return new_lrs
