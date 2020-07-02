9adcaafd3c3d3880543fa867ed769ea140570d53
Yang Zhang <yzhang123@users.noreply.github.com>
2020-07-01 20:18:12 -0700

    Nlp candidate bert (#798)
    * adding bert preprocessed
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * add doc strings, refactor loss
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * included pr feedback
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * fix doc string
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * remove file
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/losses/__init__.py
+++ nemo/collections/nlp/losses/__init__.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+# =============================================================================
+# Copyright 2020 NVIDIA. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,5 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# =============================================================================
 
-from nemo.collections.common import losses, tokenizers
+from nemo.collections.nlp.losses.smoothed_cross_entropy import *
