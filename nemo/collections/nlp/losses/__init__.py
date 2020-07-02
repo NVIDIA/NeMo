7031eddb2e1981fb1776f796db495b46a4432f80
Vahid Noroozi <VahidooX@users.noreply.github.com>
2020-07-01 21:19:56 -0700

    Fixed smoothed cross entropy.
    Signed-off-by: Vahid Noroozi <VahidooX@users.noreply.github.com>

--- nemo/collections/nlp/losses/__init__.py
+++ nemo/collections/nlp/losses/__init__.py
@@ -13,5 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
-
-from nemo.collections.nlp.losses.crossentropy_loss import CrossEntropyLoss
