21e06bb5b6b20254794f365085e671b524f54810
Yang Zhang <yangzhang@nvidia.com>
2020-07-06 11:39:36 -0700

    fix style
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/common/parts/data_utils.py
+++ nemo/collections/common/parts/data_utils.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List
 import os
+from typing import List
 
 __all__ = ['if_exist']
 
