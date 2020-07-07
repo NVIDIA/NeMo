73bb5fb2da26f57da5dadde7663b9def7f17df2e
Yang Zhang <yzhang123@users.noreply.github.com>
2020-07-07 15:17:19 -0700

    Nlp candidate bert (#825)
    add bert ci
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -26,6 +26,41 @@ pipeline {
         sh 'python setup.py style'
       }
     }
+
+    stage('Installation') {
+      steps {
+        sh './reinstall.sh'
+      }
+    }
+
+    stage('L2: NLP-BERT pretraining BERT on the fly preprocessing') {
+      when {
+        anyOf{
+          branch 'candidate'
+          changeRequest()
+        }
+      }
+      failFast true
+        steps {
+          sh 'cd examples/nlp && CUDA_VISIBLE_DEVICES=0 python bert_pretraining_from_text.py --precision 16 --amp_level=O1 --data_dir /home/TestData/nlp/wikitext-2/  --batch_size 64 --config_file /home/TestData/nlp/bert_configs/bert_3200.json --lr 0.01 --warmup_ratio 0.05 --max_steps=300 --tokenizer_name=sentencepiece --sample_size 10000000 --mask_probability 0.15 --short_seq_prob 0.1'
+          sh 'rm -rf examples/nlp/lightning_logs'
+        }
+    }
+
+    stage('L2: NLP-BERT pretraining BERT offline preprocessing') {
+      when {
+        anyOf{
+          branch 'candidate'
+          changeRequest()
+        }
+      }
+      failFast true
+        steps {
+          sh 'cd examples/nlp && CUDA_VISIBLE_DEVICES=0 python bert_pretraining_from_preprocessed.py --precision 16 --amp_level=O1 --data_dir /home/TestData/nlp/wiki_book_mini/training --batch_size 8 --config_file /home/TestData/nlp/bert_configs/uncased_L-12_H-768_A-12.json  --gpus 1 --warmup_ratio 0.01 --optimizer adamw  --opt_args weight_decay=0.01  --lr 0.875e-4 --max_steps 300'
+          sh 'rm -rf examples/nlp/lightning_logs'
+        }
+    }
+
   }
   post {
     always {
--- examples/nlp/bert_pretraining_from_text.py
+++ examples/nlp/bert_pretraining_from_text.py
@@ -15,20 +15,21 @@
 import json
 import os
 import random
+import sys
 from argparse import ArgumentParser
 
 import pytorch_lightning as pl
 from transformers import BertConfig
 
 from nemo.collections.nlp.models.lm_model import BERTLMModel
+from nemo.core.optim.lr_scheduler import CosineAnnealing, SquareRootAnnealing
+from nemo.utils.arguments import add_nlp_args, add_optimizer_args, add_scheduler_args
 
 
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument("--data_dir", type=str, required=True, help="Path to data folder")
+def add_args(parser):
     parser.add_argument("--max_seq_length", default=128, type=int)
     parser.add_argument("--sample_size", default=1e7, type=int, help="Data sample size.")
-    parser.add_argument("--num_epochs", default=10, type=int, help="Number of training epochs.")
+    parser.add_argument("--max_epochs", default=10, type=int, help="Number of training epochs.")
     parser.add_argument(
         "--mask_probability",
         default=0.15,
@@ -41,39 +42,16 @@ def parse_args():
         type=float,
         help="Probability of having a sequence shorter than the maximum sequence length `max_seq_length` in data processing.",
     )
-    parser.add_argument("--config_file", default=None, type=str, help="The BERT model config")
-    parser.add_argument("--pretrained_model_name", default='bert-base-cased', type=str, help="pretrained model name")
-    parser.add_argument("--do_lower_case", action='store_true', help="lower case data")
     parser.add_argument(
-        "--tokenizer_name", default='nemobert', type=str, choices=['sentencepiece', 'nemobert'], help="Tokenizer type"
+        "--gpus",
+        default=1,
+        help="Select GPU devices. Could be either int, string or list. See https://pytorch-lightning.readthedocs.io/en/latest/multi_gpu.html#select-gpu-devices",
     )
-    parser.add_argument("--tokenizer_model", default=None, type=str, help="Tokenizer file for sentence piece")
-    parser.add_argument("--num_gpus", default=1, type=int, help="Number Gpus")
+    parser.add_argument("--num_nodes", default=1, type=int, help="Number Nodes")
     parser.add_argument("--batch_size", default=1, type=int, help="Batch size per worker for each model pass.")
     parser.add_argument(
         "--accumulate_grad_batches", default=1, type=int, help="Accumulates grads every k batches.",
     )
-    parser.add_argument("--lr", default=0.01, type=float, help="Initial learning rate.")
-    parser.add_argument(
-        "--lr_policy", default=None, type=str, help="Learning rate policy.",
-    )
-    parser.add_argument(
-        "--lr_warmup_proportion", default=0.05, type=float, help="Warm up proportion of total training iterations."
-    )
-    parser.add_argument(
-        "--optimizer",
-        default="adam_w",
-        type=str,
-        choices=["adam", "adam_w"],
-        help="Optimizer algorithm for training.",
-    )
-    parser.add_argument(
-        "--betas",
-        default=(0.9, 0.999),
-        type=tuple,
-        help="coefficients used for computing running averages of gradient and its square",
-    )
-    parser.add_argument("--max_pred_length", default=128, type=int, help="Number Gpus")
     parser.add_argument(
         "--amp_level",
         default="O0",
@@ -81,48 +59,33 @@ def parse_args():
         choices=["O0", "O1", "O2"],
         help="Automatic Mixed Precision optimization level.",
     )
-    parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay parameter of the optimizer.")
+    parser.add_argument("--gradient_clip_val", type=float, default=0, help="gradient clipping")
     parser.add_argument(
-        "--only_mlm_loss", action="store_true", default=False, help="use only masked language model loss"
+        "--max_steps", default=None, type=int, help="Number of training steps.",
     )
     parser.add_argument(
-        "--load_dir",
-        default=None,
+        "--precision", default=32, type=int, choices=[16, 32], help="precision.",
+    )
+    parser.add_argument(
+        "--scheduler",
+        default='CosineAnnealing',
         type=str,
-        help="Directory with weights and optimizer checkpoints. Used for resuming training.",
+        choices=["SquareRootAnnealing", "CosineAnnealing"],
+        help="Scheduler.",
     )
-    parser.add_argument(
-        "--bert_checkpoint",
-        default=None,
-        type=str,
-        help="Path to BERT encoder weights file. Used for encoder initialization for finetuning.",
-    )
-    parser.add_argument("--grad_norm_clip", type=float, default=-1, help="gradient clipping")
-    parser.add_argument("--save_epoch_freq", default=1, type=int, help="Save checkpoints every given epoch.")
-    parser.add_argument("--save_step_freq", default=100, type=int, help="Save checkpoints every given iteration.")
-    parser.add_argument(
-        "--train_step_freq", default=25, type=int, help="Print training metrics every given iteration."
-    )
-    parser.add_argument(
-        "--eval_step_freq", default=25, type=int, help="Print evaluation metrics every given iteration."
-    )
-    parser.add_argument(
-        "--max_predictions_per_seq",
-        default=20,
-        type=int,
-        help="Maximum number of masked tokens to predict. Need to match the number of masked tokens in the input data sets.",
-    )
-    parser.add_argument(
-        "--max_steps", default=100, type=int, help="Number of training steps.",
-    )
-
+    parser.add_argument("--val_check_interval", default=1.0, type=int, help="validation after this many steps.")
     args = parser.parse_args()
     return args
 
 
 def main():
+    parser = ArgumentParser()
+    # parser = pl.Trainer.add_argparse_args(parser)
+    parser = add_optimizer_args(parser)
+    parser = add_scheduler_args(parser)
+    parser = add_nlp_args(parser)
+    args = add_args(parser)
 
-    args = parse_args()
     train_data_file = os.path.join(args.data_dir, 'train.txt')
     valid_data_file = os.path.join(args.data_dir, 'valid.txt')
     vocab_size = None
@@ -145,7 +108,6 @@ def main():
     bert_model.setup_training_data(
         train_data_layer_params={
             'dataset': train_data_file,
-            'max_predictions_per_seq': args.max_predictions_per_seq,
             'batch_size': args.batch_size,
             'max_seq_length': args.max_seq_length,
             'mask_probability': args.short_seq_prob,
@@ -155,26 +117,46 @@ def main():
     bert_model.setup_validation_data(
         val_data_layer_params={
             'dataset': valid_data_file,
-            'max_predictions_per_seq': args.max_predictions_per_seq,
             'batch_size': args.batch_size,
             'max_seq_length': args.max_seq_length,
             'mask_probability': args.short_seq_prob,
             'short_seq_prob': args.short_seq_prob,
         },
     )
+    scheduler_args = {
+        'monitor': 'val_loss',  # pytorch lightning requires this value
+        'warmup_ratio': args.warmup_ratio,
+        'warmup_steps': args.warmup_steps,
+        'min_lr': args.min_lr,
+        'last_epoch': args.last_epoch,
+    }
+    if args.max_epochs:
+        iters_per_batch = args.max_epochs / float(args.gpus * args.num_nodes * args.accumulate_grad_batches)
+        scheduler_args['iters_per_batch'] = iters_per_batch
+    if args.max_steps:
+        scheduler_args['max_steps'] = args.max_steps
 
-    optim_params = {'lr': args.lr, 'weight_decay': args.weight_decay, 'betas': args.betas}
-    bert_model.setup_optimization(optim_params, optimizer=args.optimizer)
+    bert_model.setup_optimization(
+        optim_params={
+            'optimizer': args.optimizer,
+            'lr': args.lr,
+            'opt_args': args.opt_args,
+            'scheduler': getattr(sys.modules[__name__], args.scheduler),
+            'scheduler_args': scheduler_args,
+        }
+    )
 
     trainer = pl.Trainer(
-        num_sanity_val_steps=10,
+        num_sanity_val_steps=0,
+        val_check_interval=args.val_check_interval,
         amp_level=args.amp_level,
-        precision=16,
-        gpus=args.num_gpus,
-        max_epochs=args.num_epochs,
-        # max_steps=args.max_steps,
+        precision=args.precision,
+        gpus=args.gpus,
+        max_epochs=args.max_epochs,
+        max_steps=args.max_steps,
         distributed_backend='ddp',
         accumulate_grad_batches=args.accumulate_grad_batches,
+        gradient_clip_val=args.gradient_clip_val,
     )
     trainer.fit(bert_model)
 
