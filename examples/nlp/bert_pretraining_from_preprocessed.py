0ab77cd07185139ac69502942c9d33aee2f52503
Yang Zhang <yangzhang@nvidia.com>
2020-07-06 18:10:15 -0700

    fix
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- examples/nlp/bert_pretraining_from_preprocessed.py
+++ examples/nlp/bert_pretraining_from_preprocessed.py
@@ -140,7 +140,7 @@ def main():
         distributed_backend='ddp',
         replace_sampler_ddp=False,
         accumulate_grad_batches=args.accumulate_grad_batches,
-        gradient_clip_val=args.gradient_clip_val
+        gradient_clip_val=args.gradient_clip_val,
         progress_bar_refresh_rate=args.progress_bar_refresh_rate
     )
     trainer.fit(bert_model)
