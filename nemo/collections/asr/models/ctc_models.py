ebbfa869a43a287b43c90640f6e5a749112c956e
ericharper <complex451@gmail.com>
2020-07-13 13:37:02 -0600

    phase 2 work in progress
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/asr/models/ctc_models.py
+++ nemo/collections/asr/models/ctc_models.py
@@ -89,20 +89,20 @@ class EncDecCTCModel(ASRModel):
             num_workers=config.get('num_workers', 0),
         )
 
-    def setup_training_data(self, train_data_layer_config: Optional[Union[DictConfig, Dict]]):
-        if 'shuffle' not in train_data_layer_config:
+    def setup_training_data(self, train_data_config: Optional[Union[DictConfig, Dict]]):
+        if 'shuffle' not in train_data_config:
             train_data_layer_config['shuffle'] = True
-        self._train_dl = self.__setup_dataloader_from_config(config=train_data_layer_config)
+        self._train_dl = self.__setup_dataloader_from_config(config=train_data_config)
 
-    def setup_validation_data(self, val_data_layer_config: Optional[Union[DictConfig, Dict]]):
-        if 'shuffle' not in val_data_layer_config:
+    def setup_validation_data(self, val_data_config: Optional[Union[DictConfig, Dict]]):
+        if 'shuffle' not in val_data_config:
             val_data_layer_config['shuffle'] = False
         self._validation_dl = self.__setup_dataloader_from_config(config=val_data_layer_config)
 
-    def setup_test_data(self, test_data_layer_params: Optional[Union[DictConfig, Dict]]):
-        if 'shuffle' not in test_data_layer_params:
-            test_data_layer_params['shuffle'] = False
-        self._test_dl = self.__setup_dataloader_from_config(config=test_data_layer_params)
+    def setup_test_data(self, test_data_config: Optional[Union[DictConfig, Dict]]):
+        if 'shuffle' not in test_data_config:
+            test_data_config['shuffle'] = False
+        self._test_dl = self.__setup_dataloader_from_config(config=test_data_config)
 
     @classmethod
     def list_available_models(cls) -> Optional[Dict[str, str]]:
