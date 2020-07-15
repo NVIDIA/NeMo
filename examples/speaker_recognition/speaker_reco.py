b399b56fa11f988a30c233846a15af1b75bd7252
nithinraok <nithinrao.koluguri@gmail.com>
2020-07-15 16:12:43 -0700

    Doc strings updated and removed classification metric
    Signed-off-by: nithinraok <nithinrao.koluguri@gmail.com>

--- examples/speaker_recognition/speaker_reco.py
+++ examples/speaker_recognition/speaker_reco.py
@@ -12,54 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import hydra
 import pytorch_lightning as pl
 
 from nemo.collections.asr.models import EncDecSpeechLabelModel
 from nemo.utils import logging
-import os
+
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 """
 Basic run (on CPU for 50 epochs):
-    python examples/asr/speech_to_text.py \
-        model.train_ds.manifest_filepath="/Users/okuchaiev/Data/an4_dataset/an4_train.json" \
-        model.validation_ds.manifest_filepath="/Users/okuchaiev/Data/an4_dataset/an4_val.json" \
+    python examples/speaker_recognition/speaker_reco.py \
+        model.train_ds.manifest_filepath="<train_manifest_file>" \
+        model.validation_ds.manifest_filepath="<validation_manifest_file>" \
         hydra.run.dir="." \
         pl.trainer.gpus=0 \
         pl.trainer.max_epochs=50
 
 
 Add PyTorch Lightning Trainer arguments from CLI:
-    python speech_to_text.py \
+    python speaker_reco.py \
         ... \
         +pl.trainer.fast_dev_run=true
 
 Hydra logs will be found in "$(./outputs/$(date +"%y-%m-%d")/$(date +"%H-%M-%S")/.hydra)"
 PTL logs will be found in "$(./outputs/$(date +"%y-%m-%d")/$(date +"%H-%M-%S")/lightning_logs)"
-
-Override some args of optimizer:
-    python speech_to_text.py \
-    model.train_ds.manifest_filepath="./an4/train_manifest.json" \
-    model.validation_ds.manifest_filepath="./an4/test_manifest.json" \
-    hydra.run.dir="." \
-    pl.trainer.gpus=2 \
-    pl.trainer.max_epochs=2 \
-    model.optim.args.params.betas=[0.8,0.5] \
-    model.optim.args.params.weight_decay=0.0001
-
-Overide optimizer entirely
-    python speech_to_text.py \
-    model.train_ds.manifest_filepath="./an4/train_manifest.json" \
-    model.validation_ds.manifest_filepath="./an4/test_manifest.json" \
-    hydra.run.dir="." \
-    pl.trainer.gpus=2 \
-    pl.trainer.max_epochs=2 \
-    model.optim.name=adamw \
-    model.optim.lr=0.001 \
-    ~model.optim.args \
-    +model.optim.args.betas=[0.8,0.5]\
-    +model.optim.args.weight_decay=0.0005
 
 """
 
@@ -69,9 +48,9 @@ def main(cfg):
     # omegaconf merg trainer stuff to optim - this is necessary to be able to correctly setup LR scheduler
     cfg.model.pl = cfg.pl
     logging.info(f'Hydra config: {cfg.pretty()}')
-    asr_model = EncDecSpeechLabelModel(cfg=cfg.model)
+    speaker_model = EncDecSpeechLabelModel(cfg=cfg.model)
     trainer = pl.Trainer(**cfg.pl.trainer)
-    trainer.fit(asr_model)
+    trainer.fit(speaker_model)
 
 
 if __name__ == '__main__':
