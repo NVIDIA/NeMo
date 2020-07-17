3f41c5a636af8bca9dee6970e5244f44e53a3770
Jason <jasoli@nvidia.com>
2020-07-17 14:06:39 -0700

    check failure
    Signed-off-by: Jason <jasoli@nvidia.com>

--- tests/core/test_asr_metrics.py
+++ tests/core/test_asr_metrics.py
@@ -123,4 +123,4 @@ class WordErrorRateTests(TestCase):
             logging.debug(s1)
             logging.debug(n2)
             logging.debug(s2)
-            self.assertEqual(get_wer(prediction=s1, reference=s2), word_error_rate(hypotheses=[s1], references=[s2]))
+            self.assertEqual(get_wer(prediction=s1, reference=s2), word_error_rate(hypotheses=[s1], references=[s2]+1))
