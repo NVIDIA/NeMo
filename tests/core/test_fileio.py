a5ff540448d5c0dfc305d1d587543783c0a8911d
Tomasz Kornuta <tkornuta@nvidia.com>
2020-07-17 17:43:54 -0700

    finished refactored tests, save->restore is still not working
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- tests/core/test_fileio.py
+++ tests/core/test_fileio.py
@@ -91,32 +91,43 @@ def asr_model():
     return EncDecCTCModel(cfg=modelConfig)
 
 
-class FileIOTest:
+class TestFileIO:
     @pytest.mark.unit
     def test_to_from_config_file(self, asr_model):
+        """" Test makes sure that the second instance created with the same configuration (BUT NOT checkpoint)
+        has different weights. """
 
         with tempfile.NamedTemporaryFile() as fp:
             yaml_filename = fp.name
             asr_model.to_config_file(path2yaml_file=yaml_filename)
             next_instance = EncDecCTCModel.from_config_file(path2yaml_file=yaml_filename)
-            assert isinstance(next_instance, EncDecCTCModel) == True
-            self.assertEqual(len(next_instance.decoder.vocabulary), 28)
-            self.assertEqual(asr_model.num_weights, next_instance.num_weights)
+
+            assert isinstance(next_instance, EncDecCTCModel)
+
+            assert len(next_instance.decoder.vocabulary) == 28
+            assert asr_model.num_weights == next_instance.num_weights
+
             w1 = asr_model.encoder.encoder[0].mconv[0].conv.weight.data.detach().cpu().numpy()
             w2 = next_instance.encoder.encoder[0].mconv[0].conv.weight.data.detach().cpu().numpy()
-            self.assertFalse(np.array_equal(w1, w2))
+
+            assert np.array_equal(w1, w2) == False
 
     @pytest.mark.unit
     @pytest.mark.pleasefixme
     def test_save_restore_from_nemo_file(self, asr_model):
+        """" Test makes sure that the second instance created from the same configuration AND checkpoint 
+        has the same weights. """
 
         with tempfile.NamedTemporaryFile() as fp:
             filename = fp.name
             asr_model.save_to(save_path=filename)
 
+            # Restore!
             asr_model2 = EncDecCTCModel.restore_from(restore_path=filename)
-            self.assertEqual(len(asr_model.decoder.vocabulary), len(asr_model2.decoder.vocabulary))
-            self.assertEqual(asr_model.num_weights, asr_model2.num_weights)
+
+            assert len(asr_model.decoder.vocabulary) == len(asr_model2.decoder.vocabulary)
+            assert asr_model.num_weights == asr_model2.num_weights
+
             w1 = asr_model.encoder.encoder[0].mconv[0].conv.weight.data.detach().cpu().numpy()
             w2 = asr_model2.encoder.encoder[0].mconv[0].conv.weight.data.detach().cpu().numpy()
-            self.assertTrue(np.array_equal(w1, w2))
+            assert np.array_equal(w1, w2)
