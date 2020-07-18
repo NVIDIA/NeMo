0507bef8cd9712907f78969eb368375719378a83
Tomasz Kornuta <tkornuta@nvidia.com>
2020-07-17 17:07:48 -0700

    Refactored tests - rely on pure pytest
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- tests/collections/asr/test_asr_modules.py
+++ tests/collections/asr/test_asr_modules.py
@@ -12,33 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import TestCase
-
 import pytest
 import torch
 
 from nemo.collections.asr.modules import AudioToMelSpectrogramPreprocessor, SpectrogramAugmentation
 
 
-class ASRModulesBasicTests(TestCase):
+class TestASRModulesBasicTests:
     @pytest.mark.unit
     def test_AudioToMelSpectrogramPreprocessor(self):
         # Make sure constructor works
         instance1 = AudioToMelSpectrogramPreprocessor(dither=0)
-        self.assertTrue(isinstance(instance1, AudioToMelSpectrogramPreprocessor))
+        assert isinstance(instance1, AudioToMelSpectrogramPreprocessor)
 
         # Make sure forward doesn't throw with expected input
         input_signal = torch.randn(size=(4, 512))
         length = torch.randint(low=4, high=444, size=[4])
         res = instance1(input_signal=input_signal, length=length)
-        self.assertTrue(isinstance(res, tuple))
-        self.assertEqual(len(res), 2)
+        assert isinstance(res, tuple)
+        assert len(res) == 2
 
     @pytest.mark.unit
     def test_SpectrogramAugmentationr(self):
         # Make sure constructor works
         instance1 = SpectrogramAugmentation(freq_masks=10, time_masks=3, rect_masks=3)
-        self.assertTrue(isinstance(instance1, SpectrogramAugmentation))
+        assert isinstance(instance1, SpectrogramAugmentation)
 
         # Make sure forward doesn't throw with expected input
         instance0 = AudioToMelSpectrogramPreprocessor(dither=0)
@@ -46,4 +44,5 @@ class ASRModulesBasicTests(TestCase):
         length = torch.randint(low=4, high=444, size=[4])
         res0 = instance0(input_signal=input_signal, length=length)
         res = instance1(input_spec=res0[0])
-        self.assertEqual(res.shape, res0[0].shape)
+
+        assert res.shape == res0[0].shape
