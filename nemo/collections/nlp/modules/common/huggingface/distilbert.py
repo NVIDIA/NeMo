50a0be6864d992f0f1f1ddf89de78f3d77273288
ekmb <ebakhturina@nvidia.com>
2020-07-20 13:33:38 -0700

    update log message
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- nemo/collections/nlp/modules/common/huggingface/distilbert.py
+++ nemo/collections/nlp/modules/common/huggingface/distilbert.py
@@ -41,7 +41,7 @@ class DistilBertEncoder(DistilBertModel, BertModule):
     def forward(self, **kwargs):
         # distilBert does not use token_type_ids as the most of the other Bert models
         if 'token_type_ids' in kwargs:
-            logging.info("DistilBert doesn’t use token_type_ids.")
+            logging.info("DistilBert doesn’t use token_type_ids, and it is ignored.")
             del kwargs['token_type_ids']
 
         res = super().forward(**kwargs)[0]
