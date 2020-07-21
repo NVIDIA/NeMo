83a6df3d830ac9b4823fbfffbf9989130eb59da3
Eric Harper <complex451@gmail.com>
2020-07-21 17:05:09 -0600

    NLP Collection - Porting SQUAD Question Answering to Hydra (#885)
    * porting squad to hydra
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * moved args to config
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * updated model init to use config
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * porting squad to hydra
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * porting to hydra
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * added setup_optimization to constructor
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * updated default lr
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * updated Jenkins test
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * style fix
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fixes
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fixes
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fixes
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * add parallel to jenkins test
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fixes
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fixes
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fix
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fix
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * adding roberta jenkins parallel test
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * added exp_manager
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * style fix
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * small fix
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * added fast_dev_run to jenkins test
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * updated defaults
    
    Signed-off-by: ericharper <complex451@gmail.com>

---
+++ examples/nlp/question_answering/question_answering_squad.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import hydra
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+
+from nemo.collections.nlp.models.qa_model import QAModel
+from nemo.utils import logging
+from nemo.utils.exp_manager import exp_manager
+
+
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    logging.info(f'Config: {cfg.pretty()}')
+    trainer = pl.Trainer(**cfg.pl.trainer)
+    exp_manager(trainer, cfg.get("exp_manager", None))
+    question_answering_model = QAModel(cfg.model, trainer=trainer)
+    trainer.fit(question_answering_model)
+
+
+if __name__ == '__main__':
+    main()
