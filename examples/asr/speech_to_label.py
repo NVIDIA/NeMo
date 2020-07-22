d4e2a68d13c6369c25e99562740a4dcf6319a9e5
Somshubra Majumdar <titu1994@gmail.com>
2020-07-22 09:29:59 -0700

    Update docstring
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- examples/asr/speech_to_label.py
+++ examples/asr/speech_to_label.py
@@ -19,46 +19,14 @@ from nemo.utils.exp_manager import exp_manager
 
 
 """
-Basic run (on CPU for 50 epochs):
-    python examples/asr/speech_to_text.py \
-        model.train_ds.manifest_filepath="/Users/okuchaiev/Data/an4_dataset/an4_train.json" \
-        model.validation_ds.manifest_filepath="/Users/okuchaiev/Data/an4_dataset/an4_val.json" \
-        hydra.run.dir="." \
-        pl.trainer.gpus=0 \
-        pl.trainer.max_epochs=50
-
-
-Add PyTorch Lightning Trainer arguments from CLI:
-    python speech_to_text.py \
-        ... \
-        +pl.trainer.fast_dev_run=true
-
-Hydra logs will be found in "$(./outputs/$(date +"%y-%m-%d")/$(date +"%H-%M-%S")/.hydra)"
-PTL logs will be found in "$(./outputs/$(date +"%y-%m-%d")/$(date +"%H-%M-%S")/lightning_logs)"
-
-Override some args of optimizer:
-    python speech_to_text.py \
-    model.train_ds.manifest_filepath="./an4/train_manifest.json" \
-    model.validation_ds.manifest_filepath="./an4/test_manifest.json" \
-    hydra.run.dir="." \
-    pl.trainer.gpus=2 \
-    pl.trainer.max_epochs=2 \
-    model.optim.args.params.betas=[0.8,0.5] \
-    model.optim.args.params.weight_decay=0.0001
-
-Overide optimizer entirely
-    python speech_to_text.py \
-    model.train_ds.manifest_filepath="./an4/train_manifest.json" \
-    model.validation_ds.manifest_filepath="./an4/test_manifest.json" \
-    hydra.run.dir="." \
-    pl.trainer.gpus=2 \
-    pl.trainer.max_epochs=2 \
-    model.optim.name=adamw \
-    model.optim.lr=0.001 \
-    ~model.optim.args \
-    +model.optim.args.betas=[0.8,0.5]\
-    +model.optim.args.weight_decay=0.0005
-
+Basic run (on 1 GPU + AMP O1 for 200 epochs):
+    python examples/asr/speech_to_label.py \
+        model.train_ds.manifest_filepath="<path to train manifest>" \
+        model.validation_ds.manifest_filepath="<path to test manifest>" \
+        pl.trainer.gpus=1 \
+        pl.trainer.max_epochs=200 \
+        +pl.trainer.precision=16 \
+        +pl.trainer.amp_level=O1
 """
 
 
