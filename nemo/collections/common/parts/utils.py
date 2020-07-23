daf3f0a6e697f916a4be2b669f32be7bd2cbe028
Yang Zhang <yangzhang@nvidia.com>
2020-07-22 22:20:13 -0700

    change default config
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/common/parts/utils.py
+++ nemo/collections/common/parts/utils.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import math
+import os
 from typing import List
 
 __all__ = ['if_exist', '_compute_softmax']
