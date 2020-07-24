86771f18defea101fb273fe700184f06fdbc9543
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2020-07-23 22:39:53 -0700

    fix style
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- examples/asr/speech_to_text_infer.py
+++ examples/asr/speech_to_text_infer.py
@@ -14,25 +14,32 @@
 
 # WIP. Ignore. The plan is to have non-PTL dependent inference
 
-from nemo.collections.asr.models import EncDecCTCModel
 from nemo.collections.asr.metrics.wer import WER, word_error_rate
+from nemo.collections.asr.models import EncDecCTCModel
 
 
 def main():
-    asr_model = EncDecCTCModel.restore_from(restore_path="/Users/okuchaiev/Workspace/NeMo1.0.0.beta/CheckPoints/WeightConversion/QN15x5En/QuartzNet15x5Base-En.nemo")
+    asr_model = EncDecCTCModel.restore_from(
+        restore_path="/Users/okuchaiev/Workspace/NeMo1.0.0.beta/CheckPoints/WeightConversion/QN15x5En/QuartzNet15x5Base-En.nemo"
+    )
     asr_model.eval()
-    asr_model.setup_test_data(test_data_config={
-        'manifest_filepath': '/Users/okuchaiev/Data/an4_dataset/an4_val.json',
-        'sample_rate': 16000,
-        'labels': asr_model.decoder.vocabulary,
-        'batch_size': 1
-    })
+    asr_model.setup_test_data(
+        test_data_config={
+            'manifest_filepath': '/Users/okuchaiev/Data/an4_dataset/an4_val.json',
+            'sample_rate': 16000,
+            'labels': asr_model.decoder.vocabulary,
+            'batch_size': 1,
+        }
+    )
     wer = WER(vocabulary=asr_model.decoder.vocabulary)
     test_outs = []
     for test_batch in asr_model.test_dataloader():
-        log_probs, encoded_len, greedy_predictions = asr_model(input_signal=test_batch[0], input_signal_length=test_batch[1])
+        log_probs, encoded_len, greedy_predictions = asr_model(
+            input_signal=test_batch[0], input_signal_length=test_batch[1]
+        )
         test_outs.append(wer.ctc_decoder_predictions_tensor(greedy_predictions))
     print(test_outs)
+
 
 if __name__ == '__main__':
     main()  # noqa pylint: disable=no-value-for-parameter
