f8879e63c3588ce4ab429cf14b09ea2abf2aad93
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2020-07-24 17:19:56 -0700

    minor adjustments to speech_to_text_infer.py
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- examples/asr/speech_to_text_infer.py
+++ examples/asr/speech_to_text_infer.py
@@ -12,23 +12,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# WIP. Ignore. The plan is to have non-PTL dependent inference
+"""
+This example demonstrates basic batch-based inference using NeMo's ASR model
+"""
+
+from argparse import ArgumentParser
 
 from nemo.collections.asr.metrics.wer import WER, word_error_rate
 from nemo.collections.asr.models import EncDecCTCModel
+from nemo.utils import logging
 
 
 def main():
-    asr_model = EncDecCTCModel.restore_from(
-        restore_path="/Users/okuchaiev/Workspace/NeMo1.0.0.beta/CheckPoints/WeightConversion/QN15x5En/QuartzNet15x5Base-En.nemo"
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--asr_model",
+        type=str,
+        default="QuartzNet15x5-En",
+        required=True,
+        help="Pass: 'QuartzNet15x5-En', 'QuartzNet15x5-Zh', or 'JasperNet10x5-En'",
     )
+    parser.add_argument("--dataset", type=str, required=True, help="path to evaluation data")
+    parser.add_argument("--eval_batch_size", type=int, default=1, help="batch size to use for evaluation")
+    parser.add_argument("--wer_target", type=float, default=None, help="used by test")
+    parser.add_argument("--wer_tolerance", type=float, default=1.0, help="used by test")
+    parser.add_argument("--trim_silence", default=True, type=bool, help="trim audio from silence or not")
+    parser.add_argument(
+        "--normalize_text", default=True, type=bool, help="Normalize transcripts or not. Set to False for non-English."
+    )
+    args = parser.parse_args()
+
+    if args.asr_model.endswith('.nemo'):
+        logging.info(f"Using local ASR model from {args.asr_model}")
+        asr_model = EncDecCTCModel.restore_from(restore_path=args.asr_model)
+    else:
+        logging.info(f"Using NGC cloud ASR model {args.asr_model}")
+        asr_model = EncDecCTCModel.from_pretrained(name=args.asr_model)
     asr_model.eval()
+
     asr_model.setup_test_data(
         test_data_config={
-            'manifest_filepath': '/Users/okuchaiev/Data/an4_dataset/an4_val.json',
+            'manifest_filepath': args.dataset,
             'sample_rate': 16000,
             'labels': asr_model.decoder.vocabulary,
-            'batch_size': 1,
+            'batch_size': args.eval_batch_size,
+            'trim_silence': args.trim_silence,
+            'normalize_transcripts': args.normalize_text,
+            'shuffle': False,
         }
     )
     wer = WER(vocabulary=asr_model.decoder.vocabulary)
