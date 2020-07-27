78f338c63b8ed4e733d5e22e37cc105a598cae74
Tomasz Kornuta <tkornuta@nvidia.com>
2020-07-27 11:47:35 -0700

    removed the # ==== showstoppers in all headers
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- external/get_modules.py
+++ external/get_modules.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# =============================================================================
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =============================================================================
 
 """ Script responsible for generation of a JSON file containing list of modules of a given collection. """
 
