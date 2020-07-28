b74d37e2b3cd5fc3ad4b1a5736be086937497b06
Jocelyn Huang <jocelynh@nvidia.com>
2020-07-28 15:29:36 -0700

    Fixed behavior of tarred dataset when user sets limit_train_batches
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>

--- nemo/collections/asr/models/ctc_models.py
+++ nemo/collections/asr/models/ctc_models.py
@@ -228,9 +228,14 @@ class EncDecCTCModel(ASRModel):
         # of samples rather than the number of batches, and this messes up the tqdm progress bar.
         # So we set the number of steps manually (to the correct number) to fix this.
         if train_data_config['is_tarred']:
-            self._trainer.limit_train_batches = ceil(
-                (len(self._train_dl) / self.world_size) / train_data_config['batch_size']
-            )
+            # We also need to check if limit_train_batches is already set.
+            # If it's an int, we assume that the user has set it to something sane, i.e. <= # training batches,
+            # and don't change it. Otherwise, adjust batches accordingly if it's a float (including 1.0).
+            if isinstance(self._trainer.limit_train_batches, float):
+                self._trainer.limit_train_batches = int(
+                    self._trainer.limit_train_batches
+                    * ceil((len(self._train_dl) / self.world_size) / train_data_config['batch_size'])
+                )
 
     def setup_validation_data(self, val_data_config: Optional[Union[DictConfig, Dict]]):
         if 'shuffle' not in val_data_config:
