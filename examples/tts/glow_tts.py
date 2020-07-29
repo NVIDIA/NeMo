a27e1b98fa97940fad84d817a3cb8a8b50c323a2
Samuel Kriman <samuelkriman@gmail.com>
2020-07-29 10:18:02 -0500

    removed limit
    Signed-off-by: Samuel Kriman <samuelkriman@gmail.com>

--- examples/tts/glow_tts.py
+++ examples/tts/glow_tts.py
@@ -7,7 +7,7 @@ from nemo.utils.exp_manager import exp_manager
 
 @hydra_runner(config_path="conf", config_name="glow_tts_config")
 def main(cfg):
-    trainer = pl.Trainer(**cfg.trainer, limit_train_batches=0.05)
+    trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
     glow_tts_model = GlowTTSModel(cfg=cfg.model, trainer=trainer)
 
