423d3b1d5cd2d30d90cdc8df500a2a9a41f7ad50
Jocelyn <jocelynh@nvidia.com>
2020-08-05 09:46:50 -0700

    Porting Tarred ASR Dataset (#945)
    * Working port of tarred ASR dataset
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Fixed tarred ASR Dataset shard partitioning
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Update tarred ASR dataset partitioning, fix tqdm progress by manually setting limit_train_batches
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Documentation ported for tarred ASR Dataset, minor fix
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Fixed behavior of tarred dataset when user sets limit_train_batches
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Remove tarred options from ASR validation dataset config
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Add unit tests for tarred ASR dataset
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Working port of tarred ASR dataset
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Fixed tarred ASR Dataset shard partitioning
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Remove tarred options from ASR validation dataset config
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Update unit tests, fix conflicts by updating featurizer creation in tarred Dataset
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Small fixes from rebase
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Small fix to Jasper for torch 1.6.0, fix to ctc_models.py to get global rank
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Style fix of shame
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * isort fix of shame
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>
    
    * Update logging import, use num_gpus instead of num_processes
    
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>

--- nemo/collections/asr/data/audio_to_text.py
+++ nemo/collections/asr/data/audio_to_text.py
@@ -11,17 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import io
+import os
 from typing import Callable, Dict, List, Optional, Union
 
+import braceexpand
 import torch
+import webdataset as wd
 
 from nemo.collections.asr.parts import collections, parsers
 from nemo.collections.asr.parts.features import WaveformFeaturizer
-from nemo.core.classes import Dataset
+from nemo.core.classes import Dataset, IterableDataset
 from nemo.core.neural_types import *
+from nemo.utils import logging
 from nemo.utils.decorators import experimental
 
-__all__ = ['AudioToCharDataset', 'AudioToBPEDataset', 'AudioLabelDataset']
+__all__ = ['AudioToCharDataset', 'AudioToBPEDataset', 'AudioLabelDataset', 'TarredAudioToCharDataset']
 
 
 def _speech_collate_fn(batch, pad_id):
@@ -179,13 +184,6 @@ class _AudioTextDataset(Dataset):
         return len(self.collection)
 
     def _collate_fn(self, batch):
-        """collate batch of audio sig, audio len, tokens, tokens len
-        Args:
-            batch (Optional[FloatTensor], Optional[LongTensor], LongTensor,
-                   LongTensor):  A tuple of tuples of signal, signal lengths,
-                   encoded tokens, and encoded tokens length.  This collate func
-                   assumes the signals are 1d torch tensors (i.e. mono audio).
-        """
         return _speech_collate_fn(batch, pad_id=self.pad_id)
 
 
@@ -205,8 +203,10 @@ class AudioToCharDataset(_AudioTextDataset):
         manifest_filepath: Path to manifest json as described above. Can
             be comma-separated paths.
         labels: String containing all the possible characters to map to
-        featurizer: Initialized featurizer class that converts paths of
-            audio to feature tensors
+        sample_rate (int): Sample rate to resample loaded audio to
+        int_values (bool): If true, load samples as 32-bit integers. Defauts to False.
+        augmentor (nemo.collections.asr.parts.perturb.AudioAugmentor): An AudioAugmentor
+            object used to augment loaded audio
         max_duration: If audio exceeds this length, do not include in dataset
         min_duration: If audio is less than this length, do not include
             in dataset
@@ -438,3 +438,231 @@ class AudioLabelDataset(Dataset):
                    assumes the signals are 1d torch tensors (i.e. mono audio).
         """
         return _speech_collate_fn(batch, pad_id=0)
+
+
+@experimental
+class TarredAudioToCharDataset(IterableDataset):
+    """
+    A similar Dataset to the AudioToCharDataset, but which loads tarred audio files.
+
+    Accepts a single comma-separated JSON manifest file (in the same style as for the AudioToCharDataset),
+    as well as the path(s) to the tarball(s) containing the wav files. Each line of the manifest should
+    contain the information for one audio file, including at least the transcript and name of the audio
+    file within the tarball.
+
+    Valid formats for the audio_tar_filepaths argument include:
+    (1) a single string that can be brace-expanded, e.g. 'path/to/audio.tar' or 'path/to/audio_{1..100}.tar.gz', or
+    (2) a list of file paths that will not be brace-expanded, e.g. ['audio_1.tar', 'audio_2.tar', ...].
+
+    See the WebDataset documentation for more information about accepted data and input formats.
+
+    If using multiple processes the number of shards should be divisible by the number of workers to ensure an
+    even split among workers. If it is not divisible, logging will give a warning but training will proceed.
+    In addition, if using mutiprocessing, each shard MUST HAVE THE SAME NUMBER OF ENTRIES after filtering
+    is applied. We currently do not check for this, but your program may hang if the shards are uneven!
+
+    Notice that a few arguments are different from the AudioToCharDataset; for example, shuffle (bool) has been
+    replaced by shuffle_n (int).
+
+    Additionally, please note that the len() of this DataLayer is assumed to be the length of the manifest
+    after filtering. An incorrect manifest length may lead to some DataLoader issues down the line.
+
+    Args:
+        audio_tar_filepaths: Either a list of audio tarball filepaths, or a
+            string (can be brace-expandable).
+        manifest_filepath (str): Path to the manifest.
+        labels (list): List of characters that can be output by the ASR model.
+            For Jasper, this is the 28 character set {a-z '}. The CTC blank
+            symbol is automatically added later for models using ctc.
+        sample_rate (int): Sample rate to resample loaded audio to
+        int_values (bool): If true, load samples as 32-bit integers. Defauts to False.
+        augmentor (nemo.collections.asr.parts.perturb.AudioAugmentor): An AudioAugmentor
+            object used to augment loaded audio
+        shuffle_n (int): How many samples to look ahead and load to be shuffled.
+            See WebDataset documentation for more details.
+            Defaults to 0.
+        min_duration (float): Dataset parameter.
+            All training files which have a duration less than min_duration
+            are dropped. Note: Duration is read from the manifest JSON.
+            Defaults to 0.1.
+        max_duration (float): Dataset parameter.
+            All training files which have a duration more than max_duration
+            are dropped. Note: Duration is read from the manifest JSON.
+            Defaults to None.
+        max_utts (int): Limit number of utterances. 0 means no maximum.
+        blank_index (int): Blank character index, defaults to -1.
+        unk_index (int): Unknown character index, defaults to -1.
+        normalize (bool): Dataset parameter.
+            Whether to use automatic text cleaning.
+            It is highly recommended to manually clean text for best results.
+            Defaults to True.
+        trim (bool): Whether to use trim silence from beginning and end
+            of audio signal using librosa.effects.trim().
+            Defaults to False.
+        bos_id (id): Dataset parameter.
+            Beginning of string symbol id used for seq2seq models.
+            Defaults to None.
+        eos_id (id): Dataset parameter.
+            End of string symbol id used for seq2seq models.
+            Defaults to None.
+        pad_id (id): Token used to pad when collating samples in batches.
+            If this is None, pads using 0s.
+            Defaults to None.
+        global_rank (int): Worker rank, used for partitioning shards. Defaults to 0.
+        world_size (int): Total number of processes, used for partitioning shards. Defaults to 0.
+    """
+
+    @property
+    def output_types(self) -> Optional[Dict[str, NeuralType]]:
+        """Returns definitions of module output ports.
+        """
+        return {
+            'audio_signal': NeuralType(
+                ('B', 'T'),
+                AudioSignal(freq=self._sample_rate)
+                if self is not None and hasattr(self, '_sample_rate')
+                else AudioSignal(),
+            ),
+            'a_sig_length': NeuralType(tuple('B'), LengthsType()),
+            'transcripts': NeuralType(('B', 'T'), LabelsType()),
+            'transcript_length': NeuralType(tuple('B'), LengthsType()),
+        }
+
+    def __init__(
+        self,
+        audio_tar_filepaths,
+        manifest_filepath,
+        labels,
+        sample_rate,
+        int_values=False,
+        augmentor=None,
+        shuffle_n=0,
+        min_duration=None,
+        max_duration=None,
+        max_utts=0,
+        blank_index=-1,
+        unk_index=-1,
+        normalize=True,
+        trim=False,
+        bos_id=None,
+        eos_id=None,
+        parser='en',
+        add_misc=False,
+        pad_id=0,
+        global_rank=0,
+        world_size=0,
+    ):
+        self.collection = collections.ASRAudioText(
+            manifests_files=manifest_filepath.split(','),
+            parser=parsers.make_parser(
+                labels=labels, name=parser, unk_id=unk_index, blank_id=blank_index, do_normalize=normalize
+            ),
+            min_duration=min_duration,
+            max_duration=max_duration,
+            max_number=max_utts,
+            index_by_file_id=True,  # Must set this so the manifest lines can be indexed by file ID
+        )
+
+        self.featurizer = WaveformFeaturizer(sample_rate=sample_rate, int_values=int_values, augmentor=augmentor)
+        self.trim = trim
+        self.eos_id = eos_id
+        self.bos_id = bos_id
+        self.pad_id = pad_id
+        self._add_misc = add_misc
+
+        # Check for distributed and partition shards accordingly
+        if world_size > 1:
+            if isinstance(audio_tar_filepaths, str):
+                audio_tar_filepaths = list(braceexpand.braceexpand(audio_tar_filepaths))
+
+            if len(audio_tar_filepaths) % world_size != 0:
+                logging.warning(
+                    f"Number of shards in tarred dataset ({len(audio_tar_filepaths)}) is not divisible "
+                    f"by number of distributed workers ({world_size})."
+                )
+
+            begin_idx = (len(audio_tar_filepaths) // world_size) * global_rank
+            end_idx = begin_idx + (len(audio_tar_filepaths) // world_size)
+            audio_tar_filepaths = audio_tar_filepaths[begin_idx:end_idx]
+            logging.info(
+                "Partitioning tarred dataset: process (%d) taking shards [%d, %d)", global_rank, begin_idx, end_idx
+            )
+
+        # Put together WebDataset
+        self._dataset = (
+            wd.Dataset(audio_tar_filepaths)
+            .shuffle(shuffle_n)
+            .rename(audio='wav', key='__key__')
+            .to_tuple('audio', 'key')
+            .pipe(self._filter)
+            .map(f=self._build_sample)
+        )
+
+    def _filter(self, iterator):
+        """This function is used to remove samples that have been filtered out by ASRAudioText already.
+        Otherwise, we would get a KeyError as _build_sample attempts to find the manifest entry for a sample
+        that was filtered out (e.g. for duration).
+        Note that if using multi-GPU training, filtering may lead to an imbalance in samples in each shard,
+        which may make your code hang as one process will finish before the other.
+        """
+
+        class TarredAudioFilter:
+            def __init__(self, collection):
+                self.iterator = iterator
+                self.collection = collection
+
+            def __iter__(self):
+                return self
+
+            def __next__(self):
+                while True:
+                    audio_bytes, audio_filename = next(self.iterator)
+                    file_id, _ = os.path.splitext(os.path.basename(audio_filename))
+                    if file_id in self.collection.mapping:
+                        return audio_bytes, audio_filename
+
+        return TarredAudioFilter(self.collection)
+
+    def _collate_fn(self, batch):
+        return _speech_collate_fn(batch, self.pad_id)
+
+    def _build_sample(self, tup):
+        """Builds the training sample by combining the data from the WebDataset with the manifest info.
+        """
+        audio_bytes, audio_filename = tup
+
+        # Grab manifest entry from self.collection
+        file_id, _ = os.path.splitext(os.path.basename(audio_filename))
+        manifest_idx = self.collection.mapping[file_id]
+        manifest_entry = self.collection[manifest_idx]
+
+        offset = manifest_entry.offset
+        if offset is None:
+            offset = 0
+
+        # Convert audio bytes to IO stream for processing (for SoundFile to read)
+        audio_filestream = io.BytesIO(audio_bytes)
+        features = self.featurizer.process(
+            audio_filestream, offset=offset, duration=manifest_entry.duration, trim=self.trim,
+        )
+        audio_filestream.close()
+
+        # Audio features
+        f, fl = features, torch.tensor(features.shape[0]).long()
+
+        # Text features
+        t, tl = manifest_entry.text_tokens, len(manifest_entry.text_tokens)
+        if self.bos_id is not None:
+            t = [self.bos_id] + t
+            tl += 1
+        if self.eos_id is not None:
+            t = t + [self.eos_id]
+            tl += 1
+
+        return f, fl, torch.tensor(t).long(), torch.tensor(tl).long()
+
+    def __iter__(self):
+        return self._dataset.__iter__()
+
+    def __len__(self):
+        return len(self.collection)
