0fe1b372453697c1e1c42f3c49b57d88f6aaf90e
Somshubra Majumdar <titu1994@gmail.com>
2020-08-05 11:02:57 -0700

    Patch `is_tarred` key update when not in config
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/asr/models/ctc_models.py
+++ nemo/collections/asr/models/ctc_models.py
@@ -162,7 +162,7 @@ class EncDecCTCModel(ASRModel):
         shuffle = config['shuffle']
 
         # Instantiate tarred dataset loader or normal dataset loader
-        if config.get('is_tarred', False):
+        if 'is_tarred' in config and config.get('is_tarred', False):
             dataset = TarredAudioToCharDataset(
                 audio_tar_filepaths=config['tarred_audio_filepaths'],
                 manifest_filepath=config['manifest_filepath'],
@@ -170,7 +170,7 @@ class EncDecCTCModel(ASRModel):
                 sample_rate=config['sample_rate'],
                 int_values=config.get('int_values', False),
                 augmentor=augmentor,
-                shuffle_n=50,
+                shuffle_n=config.get('shuffle_n', 50),
                 max_duration=config.get('max_duration', None),
                 min_duration=config.get('min_duration', None),
                 max_utts=config.get('max_utts', 0),
@@ -220,7 +220,7 @@ class EncDecCTCModel(ASRModel):
         # Need to set this because if using an IterableDataset, the length of the dataloader is the total number
         # of samples rather than the number of batches, and this messes up the tqdm progress bar.
         # So we set the number of steps manually (to the correct number) to fix this.
-        if train_data_config['is_tarred']:
+        if 'is_tarred' in train_data_config and train_data_config['is_tarred']:
             # We also need to check if limit_train_batches is already set.
             # If it's an int, we assume that the user has set it to something sane, i.e. <= # training batches,
             # and don't change it. Otherwise, adjust batches accordingly if it's a float (including 1.0).
