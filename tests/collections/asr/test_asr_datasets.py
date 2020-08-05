058b18644f751bad90d185ebb99d5a49c5bd5317
Somshubra Majumdar <titu1994@gmail.com>
2020-08-05 12:17:01 -0700

    Cleanup tests
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- tests/collections/asr/test_asr_datasets.py
+++ tests/collections/asr/test_asr_datasets.py
@@ -56,10 +56,7 @@ class TestASRDatasets:
 
     @pytest.mark.unit
     def test_tarred_dataset(self, test_data_dir):
-        batch_size = 4
         manifest_path = os.path.abspath(os.path.join(test_data_dir, 'asr/tarred_an4/tarred_audio_manifest.json'))
-
-        featurizer = WaveformFeaturizer()
 
         # Test braceexpand loading
         tarpath = os.path.abspath(os.path.join(test_data_dir, 'asr/tarred_an4/audio_{0..1}.tar'))
@@ -84,10 +81,7 @@ class TestASRDatasets:
 
     @pytest.mark.unit
     def test_tarred_bpe_dataset(self, test_data_dir):
-        batch_size = 4
         manifest_path = os.path.abspath(os.path.join(test_data_dir, 'asr/tarred_an4/tarred_audio_manifest.json'))
-
-        featurizer = WaveformFeaturizer()
 
         tokenizer_path = os.path.join(test_data_dir, "asr", "tokenizers", "an4_wpe_128", 'vocab.txt')
         tokenizer = tokenizers.NemoBertTokenizer(vocab_file=tokenizer_path)
