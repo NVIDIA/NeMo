8a8bfa7d8dbb85c1ebc53c0a3d2c5f31710df6d8
Jason <jasoli@nvidia.com>
2020-08-05 14:54:58 -0400

    Add Copyright Headers Test to Jenkins (#988)
    * add copyright check into Jenkins
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * update name
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * fix copyright headers
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * fix copyright headers
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * fix copyright headers
    
    Signed-off-by: Jason <jasoli@nvidia.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # ! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright 2020 NVIDIA. All Rights Reserved.
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
