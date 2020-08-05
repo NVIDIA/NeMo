27a7e8ad7467ed3b0cf4f2352a5e06e1e69bc696
Somshubra Majumdar <titu1994@gmail.com>
2020-08-05 14:19:12 -0700

    Add BPE support for Tarred Datasets (#990)
    * Style fix
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Update configs and add BPE support to Tarred Datasets
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Add Tarred dataset support to BPE Models
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Correct test to actually test list mode
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Cleanup tests
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Address commen
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Revert additional check
    
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- tests/collections/asr/test_asr_datasets.py
+++ tests/collections/asr/test_asr_datasets.py
@@ -13,11 +13,13 @@
 # limitations under the License.
 
 import os
+
 import pytest
 import torch
 
-from nemo.collections.asr.data.audio_to_text import TarredAudioToCharDataset
+from nemo.collections.asr.data.audio_to_text import TarredAudioToBPEDataset, TarredAudioToCharDataset
 from nemo.collections.asr.parts.features import WaveformFeaturizer
+from nemo.collections.common import tokenizers
 
 
 class TestASRDatasets:
@@ -54,10 +56,7 @@ class TestASRDatasets:
 
     @pytest.mark.unit
     def test_tarred_dataset(self, test_data_dir):
-        batch_size = 4
         manifest_path = os.path.abspath(os.path.join(test_data_dir, 'asr/tarred_an4/tarred_audio_manifest.json'))
-
-        featurizer = WaveformFeaturizer()
 
         # Test braceexpand loading
         tarpath = os.path.abspath(os.path.join(test_data_dir, 'asr/tarred_an4/audio_{0..1}.tar'))
@@ -76,6 +75,34 @@ class TestASRDatasets:
             audio_tar_filepaths=tarpath, manifest_filepath=manifest_path, labels=self.labels, sample_rate=16000
         )
         count = 0
+        for _ in ds_list_load:
+            count += 1
+        assert count == 32
+
+    @pytest.mark.unit
+    def test_tarred_bpe_dataset(self, test_data_dir):
+        manifest_path = os.path.abspath(os.path.join(test_data_dir, 'asr/tarred_an4/tarred_audio_manifest.json'))
+
+        tokenizer_path = os.path.join(test_data_dir, "asr", "tokenizers", "an4_wpe_128", 'vocab.txt')
+        tokenizer = tokenizers.NemoBertTokenizer(vocab_file=tokenizer_path)
+
+        # Test braceexpand loading
+        tarpath = os.path.abspath(os.path.join(test_data_dir, 'asr/tarred_an4/audio_{0..1}.tar'))
+        ds_braceexpand = TarredAudioToBPEDataset(
+            audio_tar_filepaths=tarpath, manifest_filepath=manifest_path, tokenizer=tokenizer, sample_rate=16000
+        )
+        assert len(ds_braceexpand) == 32
+        count = 0
         for _ in ds_braceexpand:
+            count += 1
+        assert count == 32
+
+        # Test loading via list
+        tarpath = [os.path.abspath(os.path.join(test_data_dir, f'asr/tarred_an4/audio_{i}.tar')) for i in range(2)]
+        ds_list_load = TarredAudioToBPEDataset(
+            audio_tar_filepaths=tarpath, manifest_filepath=manifest_path, tokenizer=tokenizer, sample_rate=16000
+        )
+        count = 0
+        for _ in ds_list_load:
             count += 1
         assert count == 32
