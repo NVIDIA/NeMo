94669a5fef188af8303ff6a3dfac2702f2e0676c
Somshubra Majumdar <titu1994@gmail.com>
2020-08-25 17:53:55 -0700

    Add temp debug statement
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/nlp/data/question_answering_squad/qa_dataset.py
+++ nemo/collections/nlp/data/question_answering_squad/qa_dataset.py
@@ -399,7 +399,7 @@ class SquadDataset(Dataset):
         version_2_with_negative: bool,
         null_score_diff_threshold: float,
     ):
-
+        logging.info(f"All UNIQUE IDS FOR VALIDATION : {unique_ids}")
         (all_predictions, all_nbest_json, scores_diff_json) = self.get_predictions(
             unique_ids,
             start_logits,
