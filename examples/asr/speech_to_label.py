feb19f8cd296fa44d263eb5b1a022ff028afbb7f
fayejf <fayejf07@gmail.com>
2020-08-28 16:19:00 -0700

    few updates based on feedback
    Signed-off-by: fayejf <fayejf07@gmail.com>

--- examples/asr/speech_to_label.py
+++ examples/asr/speech_to_label.py
@@ -56,7 +56,7 @@ python process_vad_data.py \
     --background_data_root=<path where the background data are stored> \
     --rebalance_method=<'under' or 'over' of 'fixed'> \ 
     --log
-    (Optional --generate (for demonstration in tutorial). If you want to use your own background noise data, make sure to delete --generates)
+    (Optional --demo (for demonstration in tutorial). If you want to use your own background noise data, make sure to delete --demo)
 ```
 
 ## Train to convergence
@@ -96,6 +96,7 @@ def main(cfg):
         trainer = pl.Trainer(gpus=gpu)
         if asr_model.prepare_test(trainer):
             trainer.test(asr_model)
+
 
 
 if __name__ == '__main__':
