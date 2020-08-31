96dfa9620a65c71bdb11c1e465ee067a08a7ec9d
ekmb <ebakhturina@nvidia.com>
2020-08-31 04:31:19 -0700

    wip
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- examples/nlp/token_classification/punctuation_capitalization.py
+++ examples/nlp/token_classification/punctuation_capitalization.py
@@ -21,28 +21,53 @@ from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
 
 
+"""
+To run this script, use:
+python punctuation_and_capitalization.py model.dataset.data_dir=PATH_TO_DATA_DIR
+
+More details on the task and data format could be found in tutorials/nlp/Punctuation_and_Capitalization.ipynb
+"""
+
+
 @hydra_runner(config_path="conf", config_name="punctuation_capitalization_config")
 def main(cfg: DictConfig) -> None:
     logging.info(f'Config: {cfg.pretty()}')
     trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
-    model = PunctuationCapitalizationModel(cfg.model, trainer=trainer)
-    trainer.fit(model)
-    if cfg.model.nemo_path:
-        model.save_to(cfg.model.nemo_path)
+    # model = PunctuationCapitalizationModel(cfg.model, trainer=trainer)
+    # trainer.fit(model)
+    # if cfg.model.nemo_path:
+    #     model.save_to(cfg.model.nemo_path)
+    #
+    # # run an inference on a few examples
+    # queries = [
+    #     'we bought four shirts and one mug from the nvidia gear store in santa clara',
+    #     'what can i do for you today',
+    #     'how are you',
+    #     'how is the weather in',
+    # ]
+    # inference_results = model.add_punctuation_capitalization(queries)
+    #
+    # for query, result in zip(queries, inference_results):
+    #     logging.info(f'Query   : {query}')
+    #     logging.info(f'Combined: {result.strip()}\n')
 
-    # run an inference on a few examples
-    queries = [
-        'we bought four shirts and one mug from the nvidia gear store in santa clara',
-        'what can i do for you today',
-        'how are you',
-        'how is the weather in',
-    ]
-    inference_results = model.add_punctuation_capitalization(queries)
+    pretrained_model = PunctuationCapitalizationModel.restore_from(
+        '/home/ebakhturina/nemo_ckpts/punctuation/ptl/bert_on_tatoeba_v3/1383000/punct_capit_distil.nemo'
+    )
+    # then we need to setup the data dir to get class weights statistics
+    pretrained_model.update_data_dir('/home/ebakhturina/tatoeba/sample')
 
-    for query, result in zip(queries, inference_results):
-        logging.info(f'Query   : {query}')
-        logging.info(f'Combined: {result.strip()}\n')
+    # setup train and validation Pytorch DataLoaders
+    pretrained_model.setup_training_data()
+    pretrained_model.setup_validation_data()
+
+    # and now we can create a PyTorch Lightning trainer and call `fit` again
+    # for this tutorial we are setting fast_dev_run to True, and the trainer will run 1 training batch and 1 validation batch
+    # for actual model training, disable the flag
+    fast_dev_run = True
+    trainer = pl.Trainer(gpus=[1], fast_dev_run=fast_dev_run)
+    trainer.fit(pretrained_model)
 
 
 if __name__ == '__main__':
--- nemo/collections/nlp/data/token_classification/punctuation_capitalization_dataset.py
+++ nemo/collections/nlp/data/token_classification/punctuation_capitalization_dataset.py
@@ -265,7 +265,10 @@ class BertPunctuationCapitalizationDataset(Dataset):
         tokenizer_type = type(tokenizer.tokenizer).__name__
         vocab_size = getattr(tokenizer, "vocab_size", 0)
         features_pkl = os.path.join(
-            data_dir, "cached_{}_{}_{}_{}".format(filename, tokenizer_type, str(max_seq_length), str(vocab_size)),
+            data_dir,
+            "cached_{}_{}_{}_{}_{}".format(
+                filename, tokenizer_type, str(max_seq_length), str(vocab_size), str(num_samples)
+            ),
         )
 
         self.punct_label_ids_file = os.path.join(data_dir, 'punct_label_ids.csv')
