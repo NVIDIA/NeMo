52754c3d8fab4a071d72e23f0cd9df71dddc3913
Tomasz Kornuta <tkornuta@nvidia.com>
2020-08-31 19:40:35 -0700

    upgraded trainer config to PTL 0.9
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- nemo/core/classes/modelPT.py
+++ nemo/core/classes/modelPT.py
@@ -79,7 +79,7 @@ class ModelPT(LightningModule, Model):
 
         self._cfg = config
 
-        #self.save_hyperparameters(self._cfg)
+        self.save_hyperparameters(self._cfg)
         self._train_dl = None
         self._validation_dl = None
         self._test_dl = None
