fa57a92a799cf3f011f189ea4319d5c5be275e85
Tomasz Kornuta <tkornuta@nvidia.com>
2020-09-01 19:19:20 -0700

    Switching between different MNIST tasks works - by setting _target_
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- nemo/collections/cv/datasets/xmnist_configs.py
+++ nemo/collections/cv/datasets/xmnist_configs.py
@@ -23,14 +23,20 @@ cs = ConfigStore.instance()
 @dataclass
 class MNISTDatasetConfig:
     """
-    Structured config for MNISTDataset class.
+    Structured config for MNIST dataset.
+
+    For more details please refer to:
+    https://pytorch.org/docs/stable/torchvision/datasets.html#mnist
+    http://yann.lecun.com/exdb/mnist/
 
     Args:
+        _target_: specification of TorchVision dataset class
         height: image height (DEFAULT: 28)
         width: image width (DEFAULT: 28)
         data_folder: path to the folder with data, can be relative to user (DEFAULT: "~/data/mnist")
         train: use train or test splits (DEFAULT: True)
-        labels: Labels of the classes.
+        download: download the data (DEFAULT: True)
+        labels: Labels of the classes (coma separated).
     """
     _target_: str = "torchvision.datasets.MNIST"
     height: int = 28
@@ -38,11 +44,82 @@ class MNISTDatasetConfig:
     data_folder: str = "~/data/mnist"
     train: bool = True
     download: bool = True
-    labels: str = "Zero One Two Three Four Five Six Seven Eight Nine"
+    labels: str = "Zero,One,Two,Three,Four,Five,Six,Seven,Eight,Nine"
 
 # Register the MNIST config.
 cs.store(
     group="nemo.collections.cv.datasets",
     name="MNIST",
     node=ObjectConf(target="nemo.collections.cv.datasets.xMNISTDataset", params=MNISTDatasetConfig()),
+)
+
+
+@dataclass
+class FashionMNISTDatasetConfig:
+    """
+    Structured config for Fashion-MNIST dataset - a variation of MNIST provided by Zelando.
+
+    For more details please refer to:
+    https://pytorch.org/docs/stable/torchvision/datasets.html#fashion-mnist
+    https://github.com/zalandoresearch/fashion-mnist
+
+    Args:
+        _target_: specification of TorchVision dataset class
+        height: image height (DEFAULT: 28)
+        width: image width (DEFAULT: 28)
+        data_folder: path to the folder with data, can be relative to user (DEFAULT: "~/data/fashion-mnist")
+        train: use train or test splits (DEFAULT: True)
+        download: download the data (DEFAULT: True)
+        labels: Labels of the classes (coma separated).
+    """
+    _target_: str = "torchvision.datasets.FashionMNIST"
+    height: int = 28
+    width: int = 28
+    data_folder: str = "~/data/fashion-mnist"
+    train: bool = True
+    download: bool = True
+    labels: str = "T-shirt/top,Trouser,Pullover,Dress,Coat,Sandal,Shirt,Sneaker,Bag,Ankle boot"
+
+
+# Register the FashionMNIST config.
+cs.store(
+    group="nemo.collections.cv.datasets",
+    name="FashionMNIST",
+    node=ObjectConf(target="nemo.collections.cv.datasets.xMNISTDataset", params=FashionMNISTDatasetConfig()),
+)
+
+
+@dataclass
+class KMNISTDatasetConfig:
+    """
+    Structured config for Kuzushiji-MNIST dataset - a variation of MNIST with 10 Hiragana characters.
+    (28x28 grayscale, 70,000 images)
+
+    For more details please refer to:
+    https://pytorch.org/docs/stable/torchvision/datasets.html#kmnist
+    https://github.com/rois-codh/kmnist
+
+    Args:
+        _target_: specification of TorchVision dataset class
+        height: image height (DEFAULT: 28)
+        width: image width (DEFAULT: 28)
+        data_folder: path to the folder with data, can be relative to user (DEFAULT: "~/data/fashion-mnist")
+        train: use train or test splits (DEFAULT: True)
+        download: download the data (DEFAULT: True)
+        labels: Labels of the classes (coma separated).
+    """
+    _target_: str = "torchvision.datasets.KMNIST"
+    height: int = 28
+    width: int = 28
+    data_folder: str = "~/data/kuzushiji-mnist"
+    train: bool = True
+    download: bool = True
+    labels: str = "0,1,2,3,4,5,6,7,8,9"
+
+
+# Register the KMNIST config.
+cs.store(
+    group="nemo.collections.cv.datasets",
+    name="KMNIST",
+    node=ObjectConf(target="nemo.collections.cv.datasets.xMNISTDataset", params=KMNISTDatasetConfig()),
 )
