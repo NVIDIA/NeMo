02a7f7b95d8469175454ce678513bd4093359638
Yang Zhang <yangzhang@nvidia.com>
2020-09-04 16:06:13 -0700

    fix
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/models/token_classification/punctuation_capitalization_model.py
+++ nemo/collections/nlp/models/token_classification/punctuation_capitalization_model.py
@@ -203,7 +203,6 @@ class PunctuationCapitalizationModel(ModelPT):
             tokenizer_model=cfg.tokenizer_model,
             special_tokens=OmegaConf.to_container(cfg.special_tokens) if cfg.special_tokens else None,
             vocab_file=cfg.vocab_file,
-            do_lower_case=cfg.do_lower_case,
         )
         self.tokenizer = tokenizer
 
