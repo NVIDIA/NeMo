6304e362b794314ab68759a93ed7b49425dd696d
Yang Zhang <yangzhang@nvidia.com>
2020-09-08 19:08:56 -0700

    fix test
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/modules/common/tokenizer_utils.py
+++ nemo/collections/nlp/modules/common/tokenizer_utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 import nemo
 from nemo.collections.common.tokenizers.char_tokenizer import CharTokenizer
