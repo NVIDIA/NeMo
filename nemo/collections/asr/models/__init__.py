1cf61fc338f655b9fe2c75caca1bf50cf07251ca
Somshubra Majumdar <titu1994@gmail.com>
2020-09-16 14:00:37 -0700

    Add CTC BPE Model pretrained checkpoint (#1174)
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/asr/models/__init__.py
+++ nemo/collections/asr/models/__init__.py
@@ -14,5 +14,6 @@
 
 from nemo.collections.asr.models.asr_model import ASRModel
 from nemo.collections.asr.models.classification_models import EncDecClassificationModel
+from nemo.collections.asr.models.ctc_bpe_models import EncDecCTCModelBPE
 from nemo.collections.asr.models.ctc_models import EncDecCTCModel
 from nemo.collections.asr.models.label_models import EncDecSpeakerLabelModel, ExtractSpeakerEmbeddingsModel
