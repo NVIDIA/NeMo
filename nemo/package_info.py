133e96a88f7b329fbe4a321d4c1c6a0750297e87
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2020-09-16 14:09:39 -0700

    bumping version to 1.0.0alpha2
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- nemo/package_info.py
+++ nemo/package_info.py
@@ -16,7 +16,7 @@
 MAJOR = 1
 MINOR = 0
 PATCH = 0
-PRE_RELEASE = 'alpha1'
+PRE_RELEASE = 'alpha2'
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
@@ -27,9 +27,9 @@ __version__ = '.'.join(map(str, VERSION[:3])) + ''.join(VERSION[3:])
 __package_name__ = 'nemo_toolkit'
 __contact_names__ = 'NVIDIA'
 __contact_emails__ = 'nemo-toolkit@nvidia.com'
-__homepage__ = 'https://nvidia.github.io/NeMo/'
+__homepage__ = 'https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/main/'
 __repository_url__ = 'https://github.com/nvidia/nemo'
 __download_url__ = 'https://github.com/NVIDIA/NeMo/releases'
-__description__ = 'NEMO core package. Necessary for all collections'
+__description__ = 'NeMo - a toolkit for Conversational AI'
 __license__ = 'Apache2'
 __keywords__ = 'deep learning, machine learning, gpu, NLP, NeMo, nvidia, pytorch, torch, tts, speech, language'
