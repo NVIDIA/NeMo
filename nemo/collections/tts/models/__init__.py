8621854b9400db4a4b2214c8cc1206f431fb43e5
Felix Kreuk <fkreuk@nvidia.com>
2020-09-23 13:44:38 +0300

    fixed more formatting
    Signed-off-by: Felix Kreuk <felixkreuk@gmail.com>

--- nemo/collections/tts/models/__init__.py
+++ nemo/collections/tts/models/__init__.py
@@ -15,7 +15,7 @@
 from nemo.collections.tts.models.glow_tts import GlowTTSModel
 from nemo.collections.tts.models.squeezewave import SqueezeWaveModel
 from nemo.collections.tts.models.tacotron2 import Tacotron2Model
-from nemo.collections.tts.models.waveglow import WaveGlowModel
 from nemo.collections.tts.models.uniglow import UniGlowModel
+from nemo.collections.tts.models.waveglow import WaveGlowModel
 
 __all__ = ["GlowTTSModel", "SqueezeWaveModel", "Tacotron2Model", "WaveGlowModel", "UniGlowModel"]
