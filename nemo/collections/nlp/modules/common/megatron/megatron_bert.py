766ad331da6ba233b752b9d889684e677f8d192b
ericharper <complex451@gmail.com>
2020-09-24 11:57:10 -0600

    typo
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/megatron_bert.py
+++ nemo/collections/nlp/modules/common/megatron/megatron_bert.py
@@ -149,7 +149,7 @@ class MegatronBertEncoder(BertModule):
         extended_attention_mask = bert_extended_attention_mask(
             attention_mask, next(self.language_model.parameters()).dtype
         )
-        logging.info(f'input_ids.device: {input_ids.device}')
+        #logging.info(f'input_ids.device: {input_ids.device}')
         position_ids = bert_position_ids(input_ids)
 
         sequence_output = self.language_model(
