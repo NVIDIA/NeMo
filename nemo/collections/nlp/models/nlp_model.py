651754565815a2f368bb4469dda4301e092e8686
ericharper <complex451@gmail.com>
2020-09-26 16:08:42 -0600

    add header
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/nlp_model.py
+++ nemo/collections/nlp/models/nlp_model.py
@@ -11,28 +11,11 @@
 
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from abc import ABC, abstractmethod
 
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-from typing import Dict, List
+from abc import ABC
 
 import torch
 from megatron import mpu
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning.overrides.data_parallel import LightningDistributedDataParallel
 
