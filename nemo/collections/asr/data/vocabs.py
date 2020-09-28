0d3bf4033fc2b6ff37c85bfed030eeed394f4164
Stanislav Beliaev <stasbelyaev96@gmail.com>
2020-09-28 16:35:21 +0300

    Add copyright for vocabs.
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>

--- nemo/collections/asr/data/vocabs.py
+++ nemo/collections/asr/data/vocabs.py
@@ -1,4 +1,16 @@
-"""Vocabularies for different datasets."""
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import abc
 import itertools
@@ -97,7 +109,14 @@ class Phonemes(Base):
     # fmt: on
 
     def __init__(
-        self, punct=True, stresses=False, spaces=True, *, space=' ', silence=None, oov=Base.OOV,
+        self,
+        punct=True,
+        stresses=False,
+        spaces=True,
+        *,
+        space=' ',
+        silence=None,
+        oov=Base.OOV,
     ):
         labels = []
         self.space, labels = len(labels), labels + [space]  # Space
