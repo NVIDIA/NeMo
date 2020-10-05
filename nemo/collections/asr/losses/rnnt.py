5175af7609bfa3c09b2f67f52ef37ce3647b031f
Somshubra Majumdar <titu1994@gmail.com>
2020-10-05 14:05:21 -0700

    Add placeholders for rnnt
    Signed-off-by: smajumdar <titu1994@gmail.com>

---
+++ nemo/collections/asr/losses/rnnt.py
@@ -0,0 +1,73 @@
+# ! /usr/bin/python
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+from torch import nn
+
+from nemo.core.classes import Loss, typecheck
+from nemo.core.neural_types import LabelsType, LengthsType, LogprobsType, LossType, NeuralType
+
+try:
+    import warprnnt_pytorch as warprnnt
+
+    WARP_RNNT_AVAILABLE = True
+except (ImportError, ModuleNotFoundError):
+    WARP_RNNT_AVAILABLE = False
+
+
+class RNNTLoss(Loss):
+    @property
+    def input_types(self):
+        """Input types definitions for CTCLoss.
+        """
+        return {
+            "log_probs": NeuralType(('B', 'T', 'D'), LogprobsType()),
+            "targets": NeuralType(('B', 'T'), LabelsType()),
+            "input_lengths": NeuralType(tuple('B'), LengthsType()),
+            "target_lengths": NeuralType(tuple('B'), LengthsType()),
+        }
+
+    @property
+    def output_types(self):
+        """Output types definitions for CTCLoss.
+        loss:
+            NeuralType(None)
+        """
+        return {"loss": NeuralType(elements_type=LossType())}
+
+    def __init__(self, num_classes):
+        super(RNNTLoss, self).__init__()
+        self._blank = num_classes
+
+        if not WARP_RNNT_AVAILABLE:
+            raise ImportError("Could not import `warprnnt_pytorch`.\n"
+                              "Please visit https://github.com/SeanNaren/warp-ctc and follow the steps in the readme "
+                              "to build and install the pytorch bindings for RNNT Loss, or use the provided docker "
+                              "container that supports RNN-T loss.")
+
+    @typecheck()
+    def forward(self, log_probs, targets, input_lengths, target_lengths):
+        # override forward implementation
+        # custom logic, if necessary
+        input_lengths = input_lengths.long()
+        target_lengths = target_lengths.long()
+        targets = targets.long()
+        # here we transpose because we expect [B, T, D] while PyTorch assumes [T, B, D]
+        log_probs = log_probs.transpose(1, 0)
+        loss = super().forward(
+            log_probs=log_probs, targets=targets, input_lengths=input_lengths, target_lengths=target_lengths
+        )
+        loss = torch.mean(loss)
+        return loss
