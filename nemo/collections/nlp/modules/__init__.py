892a031a4fdbd9761b8b1a4317746bb07e0452aa
nvidia <tkornuta@nvidia.com>
2020-10-06 16:44:25 -0700

    reverting comments - test
    Signed-off-by: nvidia <tkornuta@nvidia.com>

--- nemo/collections/nlp/modules/__init__.py
+++ nemo/collections/nlp/modules/__init__.py
@@ -13,19 +13,19 @@
 # limitations under the License.
 
 
-# from nemo.collections.nlp.modules.common import (
-#    AlbertEncoder,
-#    BertEncoder,
-#    BertModule,
-#    DistilBertEncoder,
-#    MegatronBertEncoder,
-#    RobertaEncoder,
-#    SequenceClassifier,
-#    SequenceRegression,
-#    SequenceTokenClassifier,
-#    get_lm_model,
-#    get_megatron_lm_models_list,
-#    get_pretrained_lm_models_list,
-#    get_tokenizer,
-#    get_tokenizer_list,
-# )
+from nemo.collections.nlp.modules.common import (
+    AlbertEncoder,
+    BertEncoder,
+    BertModule,
+    DistilBertEncoder,
+    MegatronBertEncoder,
+    RobertaEncoder,
+    SequenceClassifier,
+    SequenceRegression,
+    SequenceTokenClassifier,
+    get_lm_model,
+    get_megatron_lm_models_list,
+    get_pretrained_lm_models_list,
+    get_tokenizer,
+    get_tokenizer_list,
+)
