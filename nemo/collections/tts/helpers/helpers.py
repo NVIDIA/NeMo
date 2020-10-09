b90a75fcbcb792aded754bd96a2082381c3e7d48
Stanislav Beliaev <stasbelyaev96@gmail.com>
2020-10-09 19:22:38 +0300

    TalkNet Durs and Spect predictors (#1229)
    * TalkNet durs and spect predictors.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Add copyright for vocabs.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Style fix.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Fix LGTM.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Fix style.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Fix remarks rounds.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Fix CPU mask creation.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Fix comments.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>

--- nemo/collections/tts/helpers/helpers.py
+++ nemo/collections/tts/helpers/helpers.py
@@ -29,7 +29,7 @@ from nemo.utils import logging
 def get_mask_from_lengths(lengths, max_len=None):
     if not max_len:
         max_len = torch.max(lengths).item()
-    ids = torch.arange(0, max_len, out=torch.cuda.LongTensor(max_len))
+    ids = torch.arange(0, max_len, device=lengths.device, dtype=torch.long)
     mask = (ids < lengths.unsqueeze(1)).bool()
     return mask
 
