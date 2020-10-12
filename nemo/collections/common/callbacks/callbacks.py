cb90b1b2834ce1183b670af5673225aca20d94db
PeganovAnton <peganoff2@mail.ru>
2020-10-12 12:07:01 +0300

    Add pl callback
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/common/callbacks/callbacks.py
+++ nemo/collections/common/callbacks/callbacks.py
@@ -13,8 +13,11 @@
 # limitations under the License.
 import time
 
+import numpy as np
 from pytorch_lightning.callbacks.base import Callback
 from pytorch_lightning.utilities import rank_zero_only
+
+from nemo.collections.common.metrics.sacrebleu import corpus_bleu
 
 
 class LogEpochTimeCallback(Callback):
@@ -30,3 +33,59 @@ class LogEpochTimeCallback(Callback):
         curr_time = time.time()
         duration = curr_time - self.epoch_start
         trainer.logger.log_metrics({"epoch_time": duration}, step=trainer.global_step)
+
+class MachineTranslationLogEvalCallback(Callback):
+    def _on_eval_end(self, trainer, pl_module, mode):
+        counts = np.array(self._non_pad_tokens)
+        eval_loss = np.sum(np.array(self._losses) * counts) / np.sum(counts)
+        token_bleu = corpus_bleu(self._translations, self._ground_truths, tokenize="fairseq")
+        sacre_bleu = corpus_bleu(self._translations, self._ground_truths, tokenize="13a")
+        trainer.logger.info(f"{mode} results".capitalize())
+        for i in range(3):
+            sent_id = np.random.randint(len(self._translations))
+            trainer.logger.info(f"Ground truth: {self._ground_truths[sent_id]}\n")
+            trainer.logger.info(f"Translation: {self._translations[sent_id]}\n")
+        trainer.logger.info("-" * 50)
+        trainer.logger.info(f"loss: {eval_loss:.3f}")
+        trainer.logger.info(f"TokenBLEU: {token_bleu:.2f}")
+        trainer.logger.info(f"SacreBLEU: {sacre_bleu:.2f}")
+        trainer.logger.info("-" * 50)
+
+    @rank_zero_only
+    def on_test_end(self, trainer, pl_module):
+        self._on_eval_end(trainer, pl_module, "Test")
+
+    @rank_zero_only
+    def on_validation_end(self, trainer, pl_module):
+        self._on_eval_end(trainer, pl_module, "Validation")
+
+    def _on_eval_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        for tr in pl_module.last_eval_beam_results:
+            self._translations.append(pl_module.tgt_tokenizer.ids_to_text(tr))
+        for tgt in batch[2]:
+            self._ground_truths.append(pl_module.tgt_tokenizer.ids_to_text(tgt))
+            non_pad_tokens = np.not_equal(tgt, pl_module.tgt_tokenizer.pad_id).sum().item()
+            self._non_pad_tokens.append(non_pad_tokens)
+        self._losses.append(pl_module.last_eval_loss)
+
+    @rank_zero_only
+    def on_test_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        self._on_eval_batch_end(trainer, pl_module, batch, batch_idx, dataloader_idx)
+
+    @rank_zero_only
+    def on_validation_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        self._on_eval_batch_end(trainer, pl_module, batch, batch_idx, dataloader_idx)
+
+    def _on_eval_start(self, trainer, pl_module):
+        self._translations = []
+        self._ground_truths = []
+        self._losses = []
+        self._non_pad_tokens = []
+
+    @rank_zero_only
+    def on_test_start(self, trainer, pl_module):
+        self._on_eval_start(trainer, pl_module)
+
+    @rank_zero_only
+    def on_validation_start(self, trainer, pl_module):
+        self._on_eval_start(trainer, pl_module)
