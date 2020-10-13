c3eaff2836c80fa495410d35975128e81a5783f1
Jason <jasoli@nvidia.com>
2020-10-13 14:34:15 -0700

    typo
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/core/config/pytorch_lightning.py
+++ nemo/core/config/pytorch_lightning.py
@@ -62,8 +62,8 @@ class TrainerConfig:
     limit_val_batches: Any = 1.0
     limit_test_batches: Any = 1.0
     val_check_interval: Any = 1.0
-    flush_logs_every_n_steps:: int = 100
-    log_every_n_steps:: int = 50
+    flush_logs_every_n_steps: int = 100
+    log_every_n_steps: int = 50
     distributed_backend: Optional[str] = None
     sync_batchnorm: bool = False
     precision: int = 32
