28b5682ddd83697bb9f15f994412dd6ca3eb3b17
PeganovAnton <peganoff2@mail.ru>
2020-10-13 01:06:18 -0700

    Add ngc config
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/conf/ngc_commands/attention_is_all_you_need_orig.sh
@@ -0,0 +1,16 @@
+pip install -r requirements/requirements.txt \
+  && pip install -r requirements/requirements_nlp.txt \
+  && cd .. \
+  && mkdir -p data \
+  && cd data \
+  && wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2013.de -O valid.de \
+  && wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2013.en -O valid.en \
+  && cp valid.de test.de \
+  && cp valid.en test.en \
+  && wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.de \
+  && wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.en \
+  && cat train.en train.de > yttm_train.ende \
+  && yttm bpe --data yttm_train.ende --model bpe_32k_en_de_yttm.model --vocab_size 32000 \
+  && cd  ../NeMo/examples/nlp/machine_translation \
+  && python transformer_mt.py -cn ngc_8gpu
+
