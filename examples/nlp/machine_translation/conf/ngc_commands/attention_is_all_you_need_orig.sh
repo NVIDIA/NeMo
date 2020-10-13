5551b14e70a06da1517e7b583ac099669a62ada0
PeganovAnton <peganoff2@mail.ru>
2020-10-13 02:14:30 -0700

    Add NeMo to PYTHONPATH in ngc command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/ngc_commands/attention_is_all_you_need_orig.sh
+++ examples/nlp/machine_translation/conf/ngc_commands/attention_is_all_you_need_orig.sh
@@ -11,6 +11,7 @@ pip install -r requirements/requirements.txt \
   && wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.en \
   && cat train.en train.de > yttm_train.ende \
   && yttm bpe --data yttm_train.ende --model bpe_32k_en_de_yttm.model --vocab_size 32000 \
+  && export PYTHONPATH=/NeMo \
   && cd  ../NeMo/examples/nlp/machine_translation \
   && python transformer_mt.py -cn ngc_8gpu
 
