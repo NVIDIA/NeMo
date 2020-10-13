2d0a4981c09fea13ceb67d013c01a81c2ee11060
PeganovAnton <peganoff2@mail.ru>
2020-10-13 05:35:41 -0700

    Reduce batch size in ngc model
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/ngc_commands/attention_is_all_you_need_orig.sh
+++ examples/nlp/machine_translation/conf/ngc_commands/attention_is_all_you_need_orig.sh
@@ -1,6 +1,7 @@
 pip install -r requirements/requirements.txt \
   && pip install -r requirements/requirements_nlp.txt \
   && export nemo_path=$(pwd) \
+  && export HYDRA_FULL_ERROR=1 \
   && echo "NeMo path: ${nemo_path}" \
   && export PYTHONPATH="${nemo_path}" \
   && mkdir -p /data \
