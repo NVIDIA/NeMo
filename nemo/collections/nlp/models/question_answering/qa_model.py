e416fd5325f4585c2467690e10456a5e0fb5ece9
ericharper <complex451@gmail.com>
2020-10-15 11:14:08 -0600

    updated
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/question_answering/qa_model.py
+++ nemo/collections/nlp/models/question_answering/qa_model.py
@@ -82,8 +82,9 @@ class QAModel(NLPModel):
         logits = self.forward(input_ids=input_ids, token_type_ids=input_type_ids, attention_mask=input_mask)
         loss, _, _ = self.loss(logits=logits, start_positions=start_positions, end_positions=end_positions)
 
-        tensorboard_logs = {'train_loss': loss, 'lr': self._optimizer.param_groups[0]['lr']}
-        return {'loss': loss, 'log': tensorboard_logs}
+        #tensorboard_logs = {'train_loss': loss, 'lr': self._optimizer.param_groups[0]['lr']}
+        self.log('train_loss', loss)
+        self.log('lr', self._optimizer.param_groups[0]['lr'])
 
     def validation_step(self, batch, batch_idx):
         input_ids, input_type_ids, input_mask, unique_ids, start_positions, end_positions = batch
@@ -97,6 +98,8 @@ class QAModel(NLPModel):
             'start_logits': start_logits,
             'end_logits': end_logits,
         }
+        self.log('val_loss', loss)
+        self.log('eval_tensors', eval_tensors)
         return {'val_loss': loss, 'eval_tensors': eval_tensors}
 
     def test_step(self, batch, batch_idx):
