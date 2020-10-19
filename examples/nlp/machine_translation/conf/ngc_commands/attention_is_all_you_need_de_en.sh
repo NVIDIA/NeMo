48196de89d6d92cce95a9698d668d29d3e467dee
PeganovAnton <peganoff2@mail.ru>
2020-10-19 14:38:46 +0300

    Add de->en config
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/ngc_commands/attention_is_all_you_need_de_en.sh
+++ examples/nlp/machine_translation/conf/ngc_commands/attention_is_all_you_need_de_en.sh
@@ -16,6 +16,6 @@ pip install -r requirements/requirements.txt \
   && echo "current path when creating yttm model: $(pwd)" \
   && yttm bpe --data yttm_train.ende --model bpe_32k_en_de_yttm.model --vocab_size 32000 \
   && cd  "${nemo_path}/examples/nlp/machine_translation" \
-  && python train.py -cn en_de_ngc_8gpu
-  && python test.py -cn en_de_ngc_8gpu
+  && python train.py -cn de_en_ngc_8gpu
+  && python test.py -cn de_en_ngc_8gpu
 
