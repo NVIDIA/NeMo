09d217c72f92ae9a2df77a454834f69fff413646
Vahid <vnoroozi@nvidia.com>
2020-10-21 23:59:39 -0700

    Fixed license header.
    Signed-off-by: Vahid <vnoroozi@nvidia.com>

--- nemo/collections/asr/modules/multi_head_attention.py
+++ nemo/collections/asr/modules/multi_head_attention.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-# Copyright 2017 Johns Hopkins University (Shinji Watanabe)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
