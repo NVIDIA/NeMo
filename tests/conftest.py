dfd62b4538811379ad000ac0c0b8ea5cdb3e326d
Jason <jasoli@nvidia.com>
2020-10-23 10:11:46 -0700

    'deprecated'
    Signed-off-by: Jason <jasoli@nvidia.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,14 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pathlib import Path
 import shutil
-
-
 import tarfile
 import urllib.request
 from os import mkdir
 from os.path import dirname, exists, getsize, join
+from pathlib import Path
 from shutil import rmtree
 
 import pytest
