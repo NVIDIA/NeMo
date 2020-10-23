81dff89ee556df1a2367e3b19b792b4e46200c4f
Vahid <vnoroozi@nvidia.com>
2020-10-23 14:56:02 -0700

    moved back padding in features.py.
    Signed-off-by: Vahid <vnoroozi@nvidia.com>

--- nemo/collections/asr/parts/features.py
+++ nemo/collections/asr/parts/features.py
@@ -357,21 +357,6 @@ class FilterbankFeatures(nn.Module):
         # dot with filterbank energies
         x = torch.matmul(self.fb.to(x.dtype), x)
 
-        # mask to zero any values beyond seq_len in batch, pad to multiple of
-        # `pad_to` (for efficiency)
-        max_len = x.size(-1)
-        mask = torch.arange(max_len).to(x.device)
-        mask = mask.expand(x.size(0), max_len) >= seq_len.unsqueeze(1)
-        x = x.masked_fill(mask.unsqueeze(1).type(torch.bool).to(device=x.device), self.pad_value)
-        del mask
-        pad_to = self.pad_to
-        if pad_to == "max":
-            x = nn.functional.pad(x, (0, self.max_length - x.size(-1)), value=self.pad_value)
-        elif pad_to > 0:
-            pad_amt = x.size(-1) % pad_to
-            if pad_amt != 0:
-                x = nn.functional.pad(x, (0, pad_to - pad_amt), value=self.pad_value)
-
         # log features if required
         if self.log:
             if self.log_zero_guard_type == "add":
@@ -388,5 +373,20 @@ class FilterbankFeatures(nn.Module):
         # normalize if required
         if self.normalize:
             x = normalize_batch(x, seq_len, normalize_type=self.normalize)
+
+        # mask to zero any values beyond seq_len in batch, pad to multiple of
+        # `pad_to` (for efficiency)
+        max_len = x.size(-1)
+        mask = torch.arange(max_len).to(x.device)
+        mask = mask.expand(x.size(0), max_len) >= seq_len.unsqueeze(1)
+        x = x.masked_fill(mask.unsqueeze(1).type(torch.bool).to(device=x.device), self.pad_value)
+        del mask
+        pad_to = self.pad_to
+        if pad_to == "max":
+            x = nn.functional.pad(x, (0, self.max_length - x.size(-1)), value=self.pad_value)
+        elif pad_to > 0:
+            pad_amt = x.size(-1) % pad_to
+            if pad_amt != 0:
+                x = nn.functional.pad(x, (0, pad_to - pad_amt), value=self.pad_value)
 
         return x, seq_len
