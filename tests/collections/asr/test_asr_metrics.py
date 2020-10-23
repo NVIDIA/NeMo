Fix multi dataloader and ASR logging + patch asr wer calculation (#1337)
* Fix multi dataloader and ASR logging + patch asr wer

Signed-off-by: smajumdar <titu1994@gmail.com>

* Change default name of val_dl_idx

Signed-off-by: smajumdar <titu1994@gmail.com>

* Update configs to select minimum wer

Signed-off-by: smajumdar <titu1994@gmail.com>

* Correct return value

Signed-off-by: smajumdar <titu1994@gmail.com>

tests/collections/asr/test_asr_metrics.py
@@ -80,15 +80,13 @@ class TestWordErrorRate:
         return torch.Tensor(string_in_id_form).unsqueeze(0)
 
     def get_wer(self, wer, prediction: str, reference: str):
-        res = (
-            wer(
-                predictions=self.__string_to_ctc_tensor(prediction),
-                targets=self.__reference_string_to_tensor(reference),
-                target_lengths=torch.tensor([len(reference)]),
-            )
-            .detach()
-            .cpu()
+        wer(
+            predictions=self.__string_to_ctc_tensor(prediction),
+            targets=self.__reference_string_to_tensor(reference),
+            target_lengths=torch.tensor([len(reference)]),
         )
+        res, _, _ = wer.compute()
+        res = res.detach().cpu()
         # return res[0] / res[1]
         return res.item()
 