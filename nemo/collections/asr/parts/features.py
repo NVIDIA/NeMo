a97064b8d99b780379b6dd11961301bd5a5cae68
Jason <jasoli@nvidia.com>
2020-10-29 12:13:06 -0700

    update
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/asr/parts/features.py
+++ nemo/collections/asr/parts/features.py
@@ -309,13 +309,13 @@ class FilterbankFeatures(nn.Module):
         # log_zero_guard_value is the the small we want to use, we support
         # an actual number, or "tiny", or "eps"
         self.log_zero_guard_type = log_zero_guard_type
-        logging.debug(f"sr: {sample_rate}")
-        logging.debug(f"n_fft: {self.n_fft}")
-        logging.debug(f"win_length: {self.win_length}")
-        logging.debug(f"hop_length: {self.hop_length}")
-        logging.debug(f"n_mels: {nfilt}")
-        logging.debug(f"fmin: {lowfreq}")
-        logging.debug(f"fmax: {highfreq}")
+        logging.info(f"sr: {sample_rate}")
+        logging.info(f"n_fft: {self.n_fft}")
+        logging.info(f"win_length: {self.win_length}")
+        logging.info(f"hop_length: {self.hop_length}")
+        logging.info(f"n_mels: {nfilt}")
+        logging.info(f"fmin: {lowfreq}")
+        logging.info(f"fmax: {highfreq}")
 
     def log_zero_guard_value_fn(self, x):
         if isinstance(self.log_zero_guard_value, str):
