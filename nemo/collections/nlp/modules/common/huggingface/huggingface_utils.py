2ff66269a5cf641c9bcac4a832a55b3964e9ff4e
Eric Harper <complex451@gmail.com>
2020-10-30 16:03:49 -0600

    QA notebook fix v1.0.0b2 (#1383)
    * add f-string to config download
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * get_huggingface will attempt to use config dict and pretrained model name if config file is specified but not found
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * add fstring to config wget
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * style
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * fixed typo
    
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/modules/common/huggingface/huggingface_utils.py
+++ nemo/collections/nlp/modules/common/huggingface/huggingface_utils.py
@@ -84,26 +84,29 @@ def get_huggingface_lm_model(
     try:
         automodel = AutoModel.from_pretrained(pretrained_model_name)
     except Exception as e:
-        raise ValueError(f'{pretrained_model_name} is not supported by HuggingFace. {e}')
+        raise ValueError(f"{pretrained_model_name} is not supported by HuggingFace. {e}")
 
     model_type = type(automodel).__name__
     if model_type in HUGGINGFACE_MODELS:
         model_class = HUGGINGFACE_MODELS[model_type]["class"]
         if config_file:
             if not os.path.exists(config_file):
-                logging.warning(f'Config file was not found at {config_file}')
-            config_class = HUGGINGFACE_MODELS[model_type]["config"]
-            return model_class(config_class.from_json_file(config_file))
+                logging.warning(
+                    f"Config file was not found at {config_file}. Will attempt to use config_dict or pretrained_model_name."
+                )
+            else:
+                config_class = HUGGINGFACE_MODELS[model_type]["config"]
+                return model_class(config_class.from_json_file(config_file))
         if config_dict:
             config_class = HUGGINGFACE_MODELS[model_type]["config"]
             return model_class(config=config_class(**config_dict))
         else:
             return model_class.from_pretrained(pretrained_model_name)
     else:
-        raise ValueError(f'Use HuffingFace API directly in NeMo for {pretrained_model_name}')
+        raise ValueError(f"Use HuffingFace API directly in NeMo for {pretrained_model_name}")
 
 
-def get_huggingface_pretrained_lm_models_list(include_external: bool = False) -> List[str]:
+def get_huggingface_pretrained_lm_models_list(include_external: bool = False,) -> List[str]:
     """
     Returns the list of pretrained HuggingFace language models
     
