763a8bc43106697f0e7b709a3f1049a9f10bb437
Sergei Nikolaev <snikolaev@nvidia.com>
2020-11-02 22:05:17 -0800

    QAModel exportable format
    Signed-off-by: Sergei Nikolaev <snikolaev@nvidia.com>

--- nemo/collections/nlp/models/question_answering/qa_model.py
+++ nemo/collections/nlp/models/question_answering/qa_model.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import json
+import os
 from typing import Dict, List, Optional
 
 import onnx
