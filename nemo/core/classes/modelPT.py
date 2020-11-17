53afd746a69bb42f241050457144964dc529e3a8
SeanNaren <sean@grid.ai>
2020-11-17 21:13:32 +0000

    Ensure lr scheduler uses limit_train_batches when working out max steps
    Signed-off-by: SeanNaren <sean@grid.ai>

--- nemo/core/classes/modelPT.py
+++ nemo/core/classes/modelPT.py
@@ -628,6 +628,7 @@ class ModelPT(LightningModule, Model):
                 # Store information needed to calculate max_steps
                 optim_config['sched']['t_max_epochs'] = self._trainer.max_epochs
                 optim_config['sched']['t_accumulate_grad_batches'] = self._trainer.accumulate_grad_batches
+                optim_config['sched']['t_limit_train_batches'] = self._trainer.limit_train_batches
                 if self._trainer.distributed_backend is None:
                     optim_config['sched']['t_num_workers'] = self._trainer.num_gpus or 1
                 elif self._trainer.distributed_backend == "ddp_cpu":
