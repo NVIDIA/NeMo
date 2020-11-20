a708048fdfe863014a9aac73463593d98c15ea7f
Jason <jasoli@nvidia.com>
2020-11-20 11:31:26 -0800

    new vocabs
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/asr/data/vocabs.py
+++ nemo/collections/asr/data/vocabs.py
@@ -1,30 +1,28 @@
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""Vocabularies for different datasets."""
 
 import abc
 import itertools
 import string
 from typing import List
 
-from nemo.collections.asr.parts import parsers
+import nltk
+
+import nemo
 
 try:
-    import g2p_en
+    nltk.data.find('taggers/averaged_perceptron_tagger.zip')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger', quiet=True)
+try:
+    nltk.data.find('corpora/cmudict.zip')
+except LookupError:
+    nltk.download('cmudict', quiet=True)
 
-    HAVE_G2P = True
-except (FileNotFoundError, LookupError):
-    HAVE_G2P = False
+
+import g2p_en  # noqa
+
+_g2p = g2p_en.G2p()
+_g2p.variables = None
 
 
 class Base(abc.ABC):
@@ -38,9 +36,8 @@ class Base(abc.ABC):
     )
     # fmt: on
     PAD, BLANK, OOV = '<pad>', '<blank>', '<oov>'
-    SEP = ''
 
-    def __init__(self, labels, *, pad=PAD, blank=BLANK, oov=OOV):
+    def __init__(self, labels, *, pad=PAD, blank=BLANK, oov=OOV, sep=''):
         super().__init__()
 
         labels = list(labels)
@@ -48,6 +45,7 @@ class Base(abc.ABC):
         self.blank, labels = len(labels), labels + [blank]  # Reserved for blank from QN
         self.oov, labels = len(labels), labels + [oov]  # Out Of Vocabulary
         self.labels = labels
+        self.sep = sep
 
         self._util_ids = {self.pad, self.blank, self.oov}
         self._label2id = {l: i for i, l in enumerate(labels)}
@@ -60,16 +58,18 @@ class Base(abc.ABC):
 
     def decode(self, tokens: List[int]) -> str:
         """Turns ints tokens into str text."""
-        return self.SEP.join(self._id2label[t] for t in tokens if t not in self._util_ids)
+        return self.sep.join(self._id2label[t] for t in tokens if t not in self._util_ids)
 
 
 class Chars(Base):
     """Chars vocabulary."""
 
-    def __init__(self, punct=True, spaces=False):
+    def __init__(self, punct=True, spaces=False, apostrophe=True):
         labels = []
         self.space, labels = len(labels), labels + [' ']  # Space
-        labels.extend(string.ascii_lowercase + "'")  # Apostrophe for saving "don't" and "Joe's"
+        labels.extend(string.ascii_lowercase)
+        if apostrophe:
+            labels.append("'")  # Apostrophe for saving "don't" and "Joe's"
 
         if punct:
             labels.extend(self.PUNCT)
@@ -79,7 +79,7 @@ class Chars(Base):
         self.punct = punct
         self.spaces = spaces
 
-        self._parser = parsers.ENCharParser(labels)
+        self._parser = nemo.collections.asr.parts.parsers.ENCharParser(labels)
 
     def encode(self, text):
         """See base class."""
@@ -96,9 +96,6 @@ class Chars(Base):
 class Phonemes(Base):
     """Phonemes vocabulary."""
 
-    _G2P = None
-
-    SEP = '|'  # To be able to distinguish between 2/3 letters codes.
     # fmt: off
     VOWELS = (
         'AA', 'AE', 'AH', 'AO', 'AW',
@@ -114,16 +111,17 @@ class Phonemes(Base):
     # fmt: on
 
     def __init__(
-        self, punct=True, stresses=False, spaces=True, *, space=' ', silence=None, oov=Base.OOV,
+        self,
+        punct=True,
+        stresses=False,
+        spaces=True,
+        *,
+        space=' ',
+        silence=None,
+        apostrophe=True,
+        oov=Base.OOV,
+        sep='|',  # To be able to distinguish between 2/3 letters codes.
     ):
-        if HAVE_G2P:
-            Phonemes._G2P = g2p_en.G2p()
-        else:
-            raise ImportError(
-                f"G2P could not be imported properly. Please attempt to import `g2p_py` "
-                f"before using {self.__class__.__name__}."
-            )
-
         labels = []
         self.space, labels = len(labels), labels + [space]  # Space
         if silence:
@@ -133,12 +131,13 @@ class Phonemes(Base):
         if stresses:
             vowels = [f'{p}{s}' for p, s in itertools.product(vowels, (0, 1, 2))]
         labels.extend(vowels)
-        labels.append("'")  # Apostrophe
+        if apostrophe:
+            labels.append("'")  # Apostrophe
 
         if punct:
             labels.extend(self.PUNCT)
 
-        super().__init__(labels, oov=oov)
+        super().__init__(labels, oov=oov, sep=sep)
 
         self.punct = punct
         self.stresses = stresses
@@ -146,25 +145,29 @@ class Phonemes(Base):
 
     def encode(self, text):
         """See base class."""
-        ps, space = [], self.labels[self.space]
+        ps, space, labels = [], self.labels[self.space], set(self.labels)
 
-        for p in self._G2P(text):
-            if len(p) == 3 and not self.stresses:
+        for p in _g2p(text):  # noqa
+            # Remove stress
+            if p.isalnum() and len(p) == 3 and not self.stresses:
                 p = p[:2]
 
-            if p == space and ps[-1] != space:
+            # Add space if last one isn't one.
+            if p == space and len(ps) > 0 and ps[-1] != space:
                 ps.append(p)
 
-            if p.isalnum() or p == "'":
+            # Add next phoneme
+            if (p.isalnum() or p == "'") and p in labels:
                 ps.append(p)
 
-            if p in self.PUNCT and self.punct:
-                if not self.spaces and len(ps) and ps[-1] == space:
+            # Add punct and remove space if needed
+            if (p in self.PUNCT) and self.punct:
+                if not self.spaces and len(ps) > 0 and ps[-1] == space:
                     ps.pop()
-
                 ps.append(p)
 
-        if ps[-1] == space:
+        # Remove trailing spaces
+        while ps[-1] == space:
             ps.pop()
 
         return [self._label2id[p] for p in ps]
