4314f9ec37f2024d8ca37f4ef196d7a77578060b
ericharper <complex451@gmail.com>
2020-12-07 11:12:30 -0700

    style
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/megatron_bert.py
+++ nemo/collections/nlp/modules/common/megatron/megatron_bert.py
@@ -15,10 +15,10 @@
 # limitations under the License.
 
 import os
-from megatron.checkpointing import set_checkpoint_version
 
 import torch
 from megatron import get_args, initialize_megatron
+from megatron.checkpointing import set_checkpoint_version
 from megatron.model import get_language_model
 from megatron.model.bert_model import bert_attention_mask_func, bert_extended_attention_mask, bert_position_ids
 from megatron.mpu import get_model_parallel_group, model_parallel_is_initialized
