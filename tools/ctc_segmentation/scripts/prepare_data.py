2c90d5db62b88916b76464f9d1b195155c696530
ekmb <ebakhturina@nvidia.com>
2020-12-11 10:37:35 -0800

    replace
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- tools/ctc_segmentation/scripts/prepare_data.py
+++ tools/ctc_segmentation/scripts/prepare_data.py
@@ -138,6 +138,7 @@ def split_text(
         .replace("”", "")
         .replace("„", "")
         .replace("´", "")
+        .replace("--", " -- ")
     )
     # remove extra space
     transcript = re.sub(r' +', ' ', transcript)
