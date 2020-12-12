86c22601b68d3a98ac065b9bad5db9f15ed26ea4
Vlad Getselevich <vgetselevich@nvidia.com>
2020-12-11 19:22:53 -0800

    Removing unused import
    Signed-off-by: Vlad Getselevich <vgetselevich@nvidia.com>

--- examples/nlp/intent_slot_classification/intent_slot_classification.py
+++ examples/nlp/intent_slot_classification/intent_slot_classification.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 import pytorch_lightning as pl
 from omegaconf import DictConfig, OmegaConf
 
