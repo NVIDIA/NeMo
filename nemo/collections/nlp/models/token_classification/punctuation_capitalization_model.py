9aa4ada92b2e6ddd418704dc392e8576d44e5f41
Evelina <10428420+ekmb@users.noreply.github.com>
2020-12-15 09:30:23 -0800

    Bio megatron download fix (#1558)
    * wer/cer added
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * bio_meg
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * revert test
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * revert extra files
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * VAD to main
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- nemo/collections/nlp/models/token_classification/punctuation_capitalization_model.py
+++ nemo/collections/nlp/models/token_classification/punctuation_capitalization_model.py
@@ -29,11 +29,9 @@ from nemo.collections.nlp.metrics.classification_report import ClassificationRep
 from nemo.collections.nlp.models.nlp_model import NLPModel
 from nemo.collections.nlp.modules.common import TokenClassifier
 from nemo.collections.nlp.modules.common.lm_utils import get_lm_model
-from nemo.collections.nlp.modules.common.tokenizer_utils import get_tokenizer
 from nemo.collections.nlp.parts.utils_funcs import tensor2list
 from nemo.core.classes.common import PretrainedModelInfo, typecheck
 from nemo.core.classes.exportable import Exportable
-from nemo.core.classes.modelPT import ModelPT
 from nemo.core.neural_types import LogitsType, NeuralType
 from nemo.utils import logging
 from nemo.utils.export_utils import attach_onnx_to_onnx
