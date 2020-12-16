3482e98a1f172e100a4a9ef52b2f51e9bbb286c6
Tomasz Kornuta <tkornuta@nvidia.com>
2020-12-15 18:52:13 -0800

    Furher reverts and cleanups
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- nemo/utils/exp_manager.py
+++ nemo/utils/exp_manager.py
@@ -209,7 +209,7 @@ def exp_manager(trainer: 'pytorch_lightning.Trainer', cfg: Optional[Union[DictCo
 
     if is_global_rank_zero():
         if cfg.create_checkpoint_callback:
-            configure_checkpointing(trainer, log_dir, checkpoint_name, params=cfg.checkpoint_callback_params)
+            configure_checkpointing(trainer, log_dir, checkpoint_name, cfg.checkpoint_callback_params)
 
         # Move files_to_copy to folder and add git information if present
         if cfg.files_to_copy:
