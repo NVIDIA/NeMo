bda42456e3223c599382ceb8b81c410b1c8e70cd
Vahid <vnoroozi@nvidia.com>
2020-12-17 10:47:05 -0800

    fixed var_noise.
    Signed-off-by: Vahid <vnoroozi@nvidia.com>

--- nemo/core/classes/modelPT.py
+++ nemo/core/classes/modelPT.py
@@ -166,7 +166,7 @@ class ModelPT(LightningModule, Model):
         # ModelPT wrappers over subclass implementations
         self.training_step = model_utils.wrap_training_step(self.training_step)
 
-        self._var_noise_std = None
+        self._var_noise_std = 0
         self._var_noise_start = 0
 
     def register_artifact(self, config_path: str, src: str):
@@ -795,7 +795,7 @@ class ModelPT(LightningModule, Model):
             optimizer_args['lr'] = lr
 
         if 'var_noise' in optimizer_args:
-            self._var_noise_std = optimizer_args['var_noise'].get('std', None)
+            self._var_noise_std = optimizer_args['var_noise'].get('std', 0)
             self._var_noise_start = optimizer_args['var_noise'].get('start_step', 0)
             optimizer_args.pop('var_noise')
         else:
