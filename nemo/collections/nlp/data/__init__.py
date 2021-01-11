d97eb031cd4a218490df14f223d54edd6e246043
Yang Zhang <yangzhang@nvidia.com>
2021-01-11 12:53:18 -0800

    style
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/data/__init__.py
+++ nemo/collections/nlp/data/__init__.py
@@ -14,8 +14,8 @@
 
 from nemo.collections.nlp.data.data_utils import *
 from nemo.collections.nlp.data.dialogue_state_tracking.sgd.data_processor import SGDDataProcessor
-from nemo.collections.nlp.data.dialogue_state_tracking.sgd.schema import Schema
 from nemo.collections.nlp.data.dialogue_state_tracking.sgd.dataset import SGDDataset
+from nemo.collections.nlp.data.dialogue_state_tracking.sgd.schema import Schema
 from nemo.collections.nlp.data.information_retrieval.information_retrieval_dataset import (
     BertInformationRetrievalDataset,
 )
