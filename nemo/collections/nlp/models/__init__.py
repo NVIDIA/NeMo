2da3de6cc19cf8a5eedb0de86c5c9371c181406f
Yang Zhang <yangzhang@nvidia.com>
2021-01-12 13:14:30 -0800

    change import
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/models/__init__.py
+++ nemo/collections/nlp/models/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from nemo.collections.nlp.models.glue_benchmark.glue_benchmark_model import GLUEModel
+from nemo.collections.nlp.models.dialogue_state_tracking.sgdqa_model import SGDQAModel
 from nemo.collections.nlp.models.information_retrieval import BertDPRModel, BertJointIRModel
 from nemo.collections.nlp.models.intent_slot_classification import IntentSlotClassificationModel
 from nemo.collections.nlp.models.language_modeling.bert_lm_model import BERTLMModel
