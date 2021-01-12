b460711aaa986a868e32182587fb06da2c2c4ed4
SeanNaren <sean@grid.ai>
2021-01-12 17:48:16 +0000

    Remove import
    Signed-off-by: SeanNaren <sean@grid.ai>

--- nemo/collections/asr/models/wav2vec/wav2vec_model.py
+++ nemo/collections/asr/models/wav2vec/wav2vec_model.py
@@ -15,7 +15,6 @@
 
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from dataclasses import dataclass
 
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
