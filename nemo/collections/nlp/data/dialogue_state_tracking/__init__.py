e46c5928ce95dbcb7ea48bf413232d105e7456f3
Yang Zhang <yangzhang@nvidia.com>
2021-01-12 13:18:58 -0800

    change import
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/data/dialogue_state_tracking/__init__.py
+++ nemo/collections/nlp/data/dialogue_state_tracking/__init__.py
@@ -1,4 +1,3 @@
-
 from nemo.collections.nlp.data.dialogue_state_tracking.sgd.data_processor import SGDDataProcessor
 from nemo.collections.nlp.data.dialogue_state_tracking.sgd.dataset import SGDDataset
 from nemo.collections.nlp.data.dialogue_state_tracking.sgd.schema import Schema
--- nemo/collections/nlp/models/__init__.py
+++ nemo/collections/nlp/models/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.collections.nlp.models.glue_benchmark.glue_benchmark_model import GLUEModel
 from nemo.collections.nlp.models.dialogue_state_tracking.sgdqa_model import SGDQAModel
+from nemo.collections.nlp.models.glue_benchmark.glue_benchmark_model import GLUEModel
 from nemo.collections.nlp.models.information_retrieval import BertDPRModel, BertJointIRModel
 from nemo.collections.nlp.models.intent_slot_classification import IntentSlotClassificationModel
 from nemo.collections.nlp.models.language_modeling.bert_lm_model import BERTLMModel
