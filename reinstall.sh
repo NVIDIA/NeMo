b6965ba777839401fc233c37f05616cfa4618a99
Yang Zhang <yzhang123@users.noreply.github.com>
2021-01-13 14:54:55 -0800

    remove requirements from reinstall (#1631)
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -22,8 +22,6 @@ ${PIP} uninstall -y nemo_simple_gan
 
 ${PIP} install -U setuptools
 
-for f in $(ls requirements/*.txt); do ${PIP} install ${PIP_FLAGS}--disable-pip-version-check --no-cache-dir -r $f; done 
-
 echo 'Installing stuff'
 ${PIP} install ${PIP_INSTALL_OPTION} ".[all]"
 
