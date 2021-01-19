95aa7b53354d5c1cb663be2ec7ec3213360275de
PeganovAnton <peganoff2@mail.ru>
2021-01-19 01:11:30 -0800

    Replace absolute import with relative
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- tests/collections/common/loss_inputs.py
+++ tests/collections/common/loss_inputs.py
@@ -30,7 +30,7 @@ from dataclasses import dataclass
 
 import torch
 
-from tests.collections.common.pl_utils import BATCH_SIZE, NUM_BATCHES, NUM_CLASSES
+from .pl_utils import BATCH_SIZE, NUM_BATCHES, NUM_CLASSES
 
 
 @dataclass(frozen=True)
