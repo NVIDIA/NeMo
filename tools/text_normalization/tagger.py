b3aa990194242fea56a7fea262041792a25989d5
Yang Zhang <yangzhang@nvidia.com>
2021-01-26 09:07:41 -0800

    fix lgtm
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- tools/text_normalization/tagger.py
+++ tools/text_normalization/tagger.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 
 import regex as re
 from tools.text_normalization.tag import Tag, TagType
