fb239b93a2a37417bd3bfb0aad68766092dbaffa
fayejf <fayejf07@gmail.com>
2021-01-27 12:20:32 -0800

    tiny fix
    Signed-off-by: fayejf <fayejf07@gmail.com>

--- nemo/collections/asr/data/audio_to_label.py
+++ nemo/collections/asr/data/audio_to_label.py
@@ -436,7 +436,7 @@ class AudioToSpeechLabelDataset(_AudioLabelDataset):
     ):
 
         logging.info("Time length considered for collate func is {}".format(time_length))
-        logging.info("Shift length considered for collate func is {}".format(time_length))
+        logging.info("Shift length considered for collate func is {}".format(shift_length))
         self.time_length = time_length
         self.shift_length = shift_length
         self.normalize_audio = normalize_audio
