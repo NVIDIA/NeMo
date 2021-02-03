1d0515c72bb14c27e2e2f5ed23623e1135e1f4ce
Boris Fomitchev <bfomitchev@nvidia.com>
2021-02-02 19:50:43 -0800

    Added override option for forward()
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>

--- tests/collections/tts/test_waveglow.py
+++ tests/collections/tts/test_waveglow.py
@@ -23,15 +23,7 @@ from omegaconf import DictConfig
 
 from nemo.collections.tts.models import WaveGlowModel
 from nemo.collections.tts.modules import WaveGlowModule
-
-try:
-    import tensorrt as trt
-
-    print("TRT is available!\n")
-    trt_available = True
-except Exception as e:
-    print("TRT is not available!\n")
-    trt_available = False
+from nemo.core.classes import typecheck
 
 mcfg = DictConfig(
     {
@@ -60,9 +52,21 @@ wcfg = DictConfig({"waveglow": mcfg, "sigma": 1.0, "preprocessor": pcfg,})
 
 
 def input_example(sz):
-    mel = torch.randn(1, 80, sz).cuda().half()
-    z = torch.randn(1, 8, sz * 256 // 8).cuda().half()
+    mel = torch.randn(1, 1, 80, sz).cuda().half()
+    z = torch.randn(1, 8, sz * 256 // 8, 1).cuda().half()
     return {"spec": mel, "z": z}
+
+
+def taco2wg(spec, z):
+    spec = spec.permute(0, 3, 2, 1).contiguous()
+    return spec.view(spec.size(0), spec.size(1), -1), z.view(z.size(0), z.size(1), -1)
+
+
+# Wrapper method to convert Jasper's Taco2 output to WG input and call inference
+def forward_wrapper(self, spec, z=None):
+    spec, z = taco2wg(spec, z)
+    audio = self.norm_dist_to_audio(spec=spec, sigma=1.0, z=z)
+    return audio
 
 
 class TestWaveGlow:
@@ -72,6 +76,7 @@ class TestWaveGlow:
         model = WaveGlowModel(wcfg)
         # model = WaveGlowModel.restore_from("../WaveGlow-22050Hz-268M.nemo")
         model = model.cuda().half()
+        typecheck.set_typecheck_enabled(enabled=False)
         with tempfile.TemporaryDirectory() as tmpdir, model.nemo_infer():
             # Generate filename in the temporary directory.
             tmp_file_name = os.path.join("waveglow.onnx")
@@ -79,22 +84,22 @@ class TestWaveGlow:
             n_mels = 80
             # Test export.
             inp = input_example(n_mels)
-
-            inp2 = inp
-            inp3 = inp2
-            res1 = model.waveglow(**inp)
-            res2 = model.waveglow(**inp2)
+            inp1 = taco2wg(**inp)
+            inp2 = inp1
+            res1 = model.waveglow(*inp1)
+            res2 = model.waveglow(*inp2)
             assert torch.allclose(res1, res2, rtol=0.01, atol=0.1)
 
             model.export(
                 tmp_file_name,
                 verbose=True,
-                input_example=inp3,
+                input_example=inp,
                 output_example=res1,
                 try_script=False,
                 check_trace=False,
                 do_constant_folding=True,
-                use_dynamic_axes=False,
+                dynamic_axes={"spec": [0], "z": [0], "audio": [0]},
+                forward_method=forward_wrapper,
             )
 
             try:
@@ -108,61 +113,6 @@ class TestWaveGlow:
                 sess = onnxruntime.InferenceSession(omodel.SerializeToString())
                 output = sess.run(None, {"spec": inp["spec"].cpu().numpy(), "z": inp["z"].cpu().numpy()})[0]
                 assert torch.allclose(torch.from_numpy(output), res2.cpu(), rtol=1, atol=100)
-
-            if False:  # trt_available:
-                opt_profiles = [
-                    [
-                        {"input": "spec", "min": (1, 80, n_mels), "opt": (1, 80, n_mels), "max": (1, 80, n_mels)},
-                        {
-                            "input": "z",
-                            "min": (1, 8, n_mels * 256 // 8),
-                            "opt": (1, 8, n_mels * 256 // 8),
-                            "max": (1, 8, n_mels * 256 // 8),
-                        },
-                    ]
-                ]
-                engine = build_trt_engine_from_onnx(tmp_file_name, opt_profiles, verbose=True)
-                assert engine is not None
-                with open(tmp_file_name + '.trt', 'wb') as f:
-                    f.write(engine.serialize())
-
-
-def build_trt_engine_from_onnx(onnx_path, opt_profiles, verbose=False):
-    """Builds TRT engine from an ONNX file
-		"""
-    TRT_LOGGER = trt.Logger(trt.Logger.VERBOSE) if verbose else trt.Logger(trt.Logger.WARNING)
-    builder = trt.Builder(TRT_LOGGER)
-    builder.max_batch_size = 1
-
-    with open(onnx_path, 'rb') as model_fh:
-        model = model_fh.read()
-        model_onnx = onnx.load_model_from_string(model)
-
-    if True:  # self.model.args.fp16:
-        builder.fp16_mode = True
-        config_flags = 1 << int(trt.BuilderFlag.FP16)  # | 1 << int(trt.BuilderFlag.STRICT_TYPES)
-    else:
-        config_flags = 0
-
-    builder.max_workspace_size = 256 * 1024 * 1024
-
-    config = builder.create_builder_config()
-    config.flags = config_flags
-    config.max_workspace_size = 1 << 29
-
-    for x in opt_profiles:
-        profile = builder.create_optimization_profile()
-        for p in x:
-            profile.set_shape(**p)
-        config.add_optimization_profile(profile)
-
-    explicit_batch = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
-    network = builder.create_network(explicit_batch)
-
-    with trt.OnnxParser(network, TRT_LOGGER) as parser:
-        parsed = parser.parse(model)
-        print(f"Parsing returned {parsed}, building TRT engine ...")
-        return builder.build_engine(network, config=config)
 
 
 if __name__ == "__main__":
