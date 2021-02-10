9abd7b5bd802d741427da177cf44b8c504e82254
Jason <jasoli@nvidia.com>
2021-02-10 11:31:22 -0800

    fix
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/asr/parts/features.py
+++ nemo/collections/asr/parts/features.py
@@ -175,10 +175,12 @@ class STFTExactPad(STFTPatch):
                 win_length=self.win_length,
                 n_fft=self.filter_length,
                 dtype=np.float32,
-            ).to(magnitude.device)
+            )
             # remove modulation effects
             approx_nonzero_indices = torch.from_numpy(np.where(window_sum > tiny(window_sum))[0])
-            window_sum = torch.autograd.Variable(torch.from_numpy(window_sum), requires_grad=False)
+            window_sum = torch.autograd.Variable(torch.from_numpy(window_sum), requires_grad=False).to(
+                magnitude.device
+            )
             inverse_transform[:, :, approx_nonzero_indices] /= window_sum[approx_nonzero_indices]
 
             # scale by hop ratio
