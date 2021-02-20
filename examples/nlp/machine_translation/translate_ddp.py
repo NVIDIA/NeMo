7d2c6777c6d65cf9eb4c415473d9625f7ec9a024
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2021-02-19 23:41:58 -0800

    Add batch translate function to NMT model, refactor dpp translate and monolingual webdataset fix
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/machine_translation/translate_ddp.py
+++ examples/nlp/machine_translation/translate_ddp.py
@@ -17,10 +17,8 @@ import os
 from argparse import ArgumentParser
 
 import torch
-import torch.distributed as dist
 import torch.multiprocessing as mp
 from sacremoses import MosesDetokenizer
-from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
 
 from nemo.collections.common.tokenizers.pangu_jieba_detokenizer import PanguJiebaDetokenizer
@@ -44,32 +42,19 @@ def get_args():
         '--metadata_path', type=str, required=True, help="Path to the JSON file that contains dataset info"
     )
     parser.add_argument('--topk', type=int, default=500, help="Value of k for topk sampling")
-    parser.add_argument('--source_lang', type=str, required=True, help="Source lang ID for detokenization")
-    parser.add_argument('--target_lang', type=str, required=True, help="Target lang ID for detokenization")
+    parser.add_argument('--src_language', type=str, required=True, help="Source lang ID for detokenization")
+    parser.add_argument('--tgt_language', type=str, required=True, help="Target lang ID for detokenization")
     parser.add_argument(
         '--reverse_lang_direction',
         action="store_true",
         help="Reverse source and target language direction for parallel dataset",
     )
+    parser.add_argument('--n_gpus', type=int, default=-1, help="Number of GPUs to use")
     args = parser.parse_args()
     return args
 
 
-def setup(rank, world_size, args):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-
-    # initialize the process group
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
-
-
-def cleanup():
-    dist.destroy_process_group()
-
-
 def translate(rank, world_size, args):
-    setup(rank, world_size, args)
-    torch.set_grad_enabled(False)
     if args.model.endswith(".nemo"):
         logging.info("Attempting to initialize from .nemo file")
         model = MTEncDecModel.restore_from(restore_path=args.model)
@@ -77,8 +62,8 @@ def translate(rank, world_size, args):
         logging.info("Attempting to initialize from .ckpt file")
         model = MTEncDecModel.load_from_checkpoint(checkpoint_path=args.model)
     model.replace_beam_with_sampling(topk=args.topk)
-    ddp_model = DDP(model.to(rank), device_ids=[rank])
-    ddp_model.eval()
+    model.eval()
+    model.to(rank)
     if args.twoside:
         dataset = TarredTranslationDataset(
             text_tar_filepaths=args.text2translate,
@@ -108,19 +93,6 @@ def translate(rank, world_size, args):
     translations_file_name = os.path.join(result_dir, 'translations.txt')
     num_translated_sentences = 0
 
-    if args.target_lang != 'zh':
-        target_detokenizer = MosesDetokenizer(lang=args.target_lang)
-    elif args.target_lang == 'zh':
-        target_detokenizer = PanguJiebaDetokenizer()
-
-    if args.target_lang or args.source_lang == 'ja':
-        sp_detokenizer = SentencePieceDetokenizer()
-
-    if args.source_lang != 'zh':
-        source_detokenizer = MosesDetokenizer(lang=args.source_lang)
-    elif args.source_lang == 'zh':
-        source_detokenizer = PanguJiebaDetokenizer()
-
     with open(originals_file_name, 'w') as of, open(translations_file_name, 'w') as tf:
         for batch_idx, batch in enumerate(loader):
             for i in range(len(batch)):
@@ -137,26 +109,17 @@ def translate(rank, world_size, args):
                     f"rank {rank}"
                 )
             num_translated_sentences += len(src_ids)
-            translations = ddp_model(src_ids, src_mask, None, None)
-            translations = translations.cpu().numpy()
-            for t in translations:
-                t = model.decoder_tokenizer.ids_to_text(t)
-                if args.target_lang == 'ja':
-                    t = sp_detokenizer.detokenize(t.split())
-                translation = target_detokenizer.detokenize(t.split())
+            inputs, translations = model.batch_translate(
+                src=src_ids, src_mask=src_mask, source_lang=args.src_language, target_lang=args.tgt_language
+            )
+            for src, translation in zip(inputs, translations):
+                of.write(src + '\n')
                 tf.write(translation + '\n')
-            for o in src_ids:
-                o = model.encoder_tokenizer.ids_to_text(o)
-                if args.target_lang == 'ja':
-                    o = sp_detokenizer.detokenize(o.split())
-                translation = source_detokenizer.detokenize(o.split())
-                of.write(translation + '\n')
-    cleanup()
 
 
 def main() -> None:
-    world_size = torch.cuda.device_count()
     args = get_args()
+    world_size = torch.cuda.device_count() if args.n_gpus == -1 else args.n_gpus
     mp.spawn(translate, args=(world_size, args), nprocs=world_size, join=True)
 
 
