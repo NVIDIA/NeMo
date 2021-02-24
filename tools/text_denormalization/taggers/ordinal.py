d832151e9376505f90d75e3c72e67530b63cb5b3
Yang Zhang <yangzhang@nvidia.com>
2021-02-24 15:44:38 -0800

    add google header
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- tools/text_denormalization/taggers/ordinal.py
+++ tools/text_denormalization/taggers/ordinal.py
@@ -1,10 +1,5 @@
-import pynini
-from denormalization.data_loader_utils import get_abs_path
-from denormalization.graph_utils import NEMO_CHAR, GraphFst, convert_space, delete_space
-from denormalization.taggers.cardinal import CardinalFst
-from pynini.lib import pynutil
-
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+# Copyright 2015 and onwards Google, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +12,12 @@ from pynini.lib import pynutil
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import pynini
+from denormalization.data_loader_utils import get_abs_path
+from denormalization.graph_utils import NEMO_CHAR, GraphFst, convert_space, delete_space
+from denormalization.taggers.cardinal import CardinalFst
+from pynini.lib import pynutil
 
 
 class OrdinalFst(GraphFst):
