b467d6f20a13965e65522b3fbaf833b7cbf9f3ac
Sandeep Subramanian <sandeep.subramanian.1@umontreal.ca>
2021-02-25 13:08:23 -0500

    Refactor of tokenization and detokenization within the NMT model (#1789)
    * Cleanup tokenization and detokenization
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Fixes to merge moses and chinese/japanese call formats
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Style fixes and remove methods not part of rc
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Fix circular imports
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Fix docstring
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Remove unused imports
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Remove unused import
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Remove sentencepiece tokenizer from within model class
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    * Update nemo/collections/common/tokenizers/japanese_tokenizers.py
    
    Co-authored-by: Mike Chrzanowski <mike.chrzanowski0@gmail.com>
    
    * Add docstring for JapaneseTokenizer and rename variable
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    Co-authored-by: Mike Chrzanowski <mike.chrzanowski0@gmail.com>

--- nemo/collections/common/tokenizers/chinese_tokenizers.py
+++ nemo/collections/common/tokenizers/chinese_tokenizers.py
@@ -34,14 +34,30 @@
 import re
 from typing import List
 
+import jieba
+import opencc
 from pangu import spacing
 
 
-class PanguJiebaDetokenizer:
-    def detokenize(self, text):
+class ChineseDetokenizer:
+    def detokenize(self, text: List[str]) -> str:
         RE_WS_IN_FW = re.compile(
             r'([\u2018\u2019\u201c\u201d\u2e80-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff00-\uffef])\s+(?=[\u2018\u2019\u201c\u201d\u2e80-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff00-\uffef])'
         )
 
         detokenize = lambda s: spacing(RE_WS_IN_FW.sub(r'\1', s)).strip()
         return detokenize(' '.join(text))
+
+
+class ChineseTokenizer:
+    def tokenize(self, text: str, escape=False, return_str=True):
+        text = jieba.cut(text)
+        return ' '.join(text) if return_str else text
+
+
+class Traditional2Simplified:
+    def __init__(self):
+        self.normalizer = opencc.OpenCC('t2s.json')
+
+    def normalize(self, text: str) -> str:
+        return self.normalizer.convert(text)
