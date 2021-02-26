d7a394c6fb833a1b433773f16cd5993fa02d7d75
Ryan Prenger <ryanprenger@baidu.com>
2021-02-25 16:03:18 -0800

    Adding option to always create .nemo file when writing checkpoint (#1794)
    * Adding option to always create .nemo file when writing checkpoint
    
    Signed-off-by: rprenger <rprenger@nvidia.com>
    
    * Fixing an issue where save_best_model=True would have made the trainer start from the best model at every checkpoint save instead of from the latest model
    
    Signed-off-by: rprenger <rprenger@nvidia.com>
    
    * Caching the path of the best model so we don't re-generate .nemo files when they haven't changed
    
    Signed-off-by: rprenger <rprenger@nvidia.com>
    
    * style fix
    
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>
    
    Co-authored-by: rprenger <rprenger@nvidia.com>
    Co-authored-by: Jason <jasoli@nvidia.com>
    Co-authored-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- nemo/utils/exp_manager.py
+++ nemo/utils/exp_manager.py
@@ -16,11 +16,13 @@ import os
 import subprocess
 import sys
 import time
+from copy import deepcopy
 from dataclasses import dataclass
 from pathlib import Path
 from shutil import copy, move
 from typing import Any, Dict, List, Optional, Union
 
+import torch
 from hydra.core.hydra_config import HydraConfig
 from hydra.utils import get_original_cwd
 from omegaconf import DictConfig, OmegaConf
@@ -69,6 +71,7 @@ class CallbackParams:
     prefix: Optional[str] = None  # If None, exp_manager will attempt to handle the filepath
     postfix: str = ".nemo"
     save_best_model: bool = False
+    always_save_nemo: bool = False
 
 
 @dataclass
@@ -564,12 +567,44 @@ class NeMoModelCheckpoint(ModelCheckpoint):
     """ Light wrapper around Lightning's ModelCheckpoint to force a saved checkpoint on train_end
     """
 
-    def __init__(self, save_best_model=False, postfix=".nemo", **kwargs):
+    def __init__(self, always_save_nemo=False, save_best_model=False, postfix=".nemo", **kwargs):
         # Parse and store "extended" parameters: save_best model and postfix.
+        self.always_save_nemo = always_save_nemo
         self.save_best_model = save_best_model
         self.postfix = postfix
+        self.previous_best_path = ""
+
         # Call the parent class constructor with the remaining kwargs.
         super().__init__(**kwargs)
+
+    @rank_zero_only
+    def on_save_checkpoint(self, trainer, pl_module):
+        output = super().on_save_checkpoint(trainer, pl_module)
+
+        if not self.always_save_nemo:
+            return output
+
+        # Load the best model and then re-save it
+        if self.save_best_model:
+            if not os.path.exists(self.best_model_path):
+                return output
+
+            if self.best_model_path == self.previous_best_path:
+                return output
+
+            self.previous_model_path = self.best_model_path
+            old_state_dict = deepcopy(pl_module.state_dict())
+            checkpoint = torch.load(self.best_model_path, map_location='cpu')
+            if 'state_dict' in checkpoint:
+                checkpoint = checkpoint['state_dict']
+            # get a new instanace of the model
+            pl_module.load_state_dict(checkpoint, strict=True)
+            pl_module.save_to(save_path=os.path.join(self.dirpath, self.prefix + self.postfix))
+            pl_module.load_state_dict(old_state_dict, strict=True)
+        else:
+            pl_module.save_to(save_path=os.path.join(self.dirpath, self.prefix + self.postfix))
+
+        return output
 
     @rank_zero_only
     def on_train_end(self, trainer, pl_module):
