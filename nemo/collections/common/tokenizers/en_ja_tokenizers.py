7e8a576dc3a48a0fca53b9a7d817da924cda4061
Mike Chrzanowski <mchrzanowski@nvidia.com>
2021-03-03 11:45:45 -0800

    remove unused re imort
    Signed-off-by: Mike Chrzanowski <mchrzanowski@nvidia.com>

--- nemo/collections/common/tokenizers/en_ja_tokenizers.py
+++ nemo/collections/common/tokenizers/en_ja_tokenizers.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import re
 from typing import List
 
 from sacremoses import MosesDetokenizer, MosesPunctNormalizer, MosesTokenizer
