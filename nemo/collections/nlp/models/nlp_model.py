9dcc1f64b1c17437a1403e44e44ef03cfce3e122
ericharper <complex451@gmail.com>
2021-03-02 20:00:41 -0700

    set find_unused_paramters to True by default for NLP models
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/nlp_model.py
+++ nemo/collections/nlp/models/nlp_model.py
@@ -274,7 +274,7 @@ class NLPModel(ModelPT, Exportable):
         # TODO: implement model parallel for test stage
         if stage == 'fit':
             # set find_unused_parameters to True by default for NLP models
-            trainer.accelerator_backend.training_type_plugin._ddp_kwargs['find_unused_parameters'] = True
+            self._trainer.accelerator_backend.training_type_plugin._ddp_kwargs['find_unused_parameters'] = True
 
             # adds self.bert_model config to .nemo file
             if hasattr(self, 'bert_model') and self.bert_model is not None:
