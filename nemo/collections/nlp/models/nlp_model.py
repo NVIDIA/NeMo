182eb34c53839e8f01983b14cdbaec77bb53c4e1
ericharper <complex451@gmail.com>
2021-03-03 08:46:30 -0700

    check hasattr first
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/nlp_model.py
+++ nemo/collections/nlp/models/nlp_model.py
@@ -54,9 +54,6 @@ class NLPModel(ModelPT, Exportable):
     def __init__(self, cfg: DictConfig, trainer: Trainer = None):
         super().__init__(cfg, trainer)
         self.set_world_size(trainer)
-        # set find_unused_parameters to True by default for NLP models
-        if hasattr(trainer, 'accelerator_backend'):
-            trainer.accelerator_backend.training_type_plugin._ddp_kwargs['find_unused_parameters'] = True
 
     @rank_zero_only
     def register_bert_model(self):
@@ -276,6 +273,9 @@ class NLPModel(ModelPT, Exportable):
         """
         # TODO: implement model parallel for test stage
         if stage == 'fit':
+            # set find_unused_parameters to True by default for NLP models
+            if hasattr(self._trainer, 'accelerator_backend'):
+                self._trainer.accelerator_backend.training_type_plugin._ddp_kwargs['find_unused_parameters'] = True
 
             # adds self.bert_model config to .nemo file
             if hasattr(self, 'bert_model') and self.bert_model is not None:
