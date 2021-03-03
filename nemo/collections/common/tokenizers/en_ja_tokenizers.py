30a282a658fc6ce6d5e6b58268b286473c85d081
Mike Chrzanowski <mchrzanowski@nvidia.com>
2021-03-03 11:43:49 -0800

    fix style
    Signed-off-by: Mike Chrzanowski <mchrzanowski@nvidia.com>

--- nemo/collections/common/tokenizers/en_ja_tokenizers.py
+++ nemo/collections/common/tokenizers/en_ja_tokenizers.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import re
-
 from typing import List
 
 from sacremoses import MosesDetokenizer, MosesPunctNormalizer, MosesTokenizer
+
 
 class EnJaProcessor:
     """
