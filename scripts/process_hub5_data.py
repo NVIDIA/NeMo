e4fcaa6e57a5edd7cf1f11d9f51ed408ec2fd230
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2021-03-05 11:46:11 -0800

    fix copyright headers
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- scripts/process_hub5_data.py
+++ scripts/process_hub5_data.py
@@ -1,14 +1,17 @@
-# Copyright (c) 2019 NVIDIA Corporation
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
 #
-# USAGE:
-# python process_hub5_data.py \
-#   --data_root=<HUB5 root> \
-#   --dest_root=<destination dir root>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Converts the .sph audio files in the HUB5 dataset to .wav, and performs some
-# extra processing to segment audio files and prepare transcripts.
-# Requires sph2pipe to be installed.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This script is heavily derived from the Patter HUB5 processing script written
 # by Ryan Leary
 
