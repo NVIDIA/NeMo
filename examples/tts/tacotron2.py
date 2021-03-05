ae1b98511f5edffb642ce74f33032b0b082cd216
Jason <jasoli@nvidia.com>
2021-03-05 08:25:16 -0800

    fix t2 header
    Signed-off-by: Jason <jasoli@nvidia.com>

--- examples/tts/tacotron2.py
+++ examples/tts/tacotron2.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytorch_lightning as pl
 from pytorch_lightning.plugins import DDPPlugin
 
@@ -13,7 +27,7 @@ from nemo.utils.exp_manager import exp_manager
 @hydra_runner(config_path="conf", config_name="tacotron2")
 def main(cfg):
     # Define the Lightning trainer
-    trainer = pl.Trainer(plugins=[DDPPlugin(find_unused_parameters=True)], **cfg.trainer)
+    trainer = pl.Trainer(plugins=[DDPPlugin(find_unused_parameters=False)], **cfg.trainer)
     # exp_manager is a NeMo construct that helps with logging and checkpointing
     exp_manager(trainer, cfg.get("exp_manager", None))
     # Define the Tacotron 2 model, this will construct the model as well as
