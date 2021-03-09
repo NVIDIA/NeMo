3ebbcb4200055c741800f1b7b5c53e25b657e891
Stanislav Beliaev <stasbelyaev96@gmail.com>
2021-03-10 01:47:18 +0300

    TalkNet 1.x (#1562)
    * Some renaming.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * TalkNet 1.x draft.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Three pipelines complete.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Fix some comments.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Fix style issues.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * TTS Notebook and PR issues.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * TalkNet style issues.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * TalkNet doc.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * Small fix.
    
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>
    
    * pin lightning
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * print more torch stuff
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    Co-authored-by: Jason <jasoli@nvidia.com>

--- nemo/collections/asr/data/audio_to_text.py
+++ nemo/collections/asr/data/audio_to_text.py
@@ -29,7 +29,7 @@ from nemo.utils import logging
 
 __all__ = [
     'AudioToCharDataset',
-    'AudioToCharWithDursDataset',
+    'AudioToCharWithDursF0Dataset',
     'AudioToBPEDataset',
     'TarredAudioToCharDataset',
     'TarredAudioToBPEDataset',
@@ -288,11 +288,11 @@ class AudioToCharDataset(_AudioTextDataset):
         )
 
 
-class AudioToCharWithDursDataset(AudioToCharDataset):
+class AudioToCharWithDursF0Dataset(AudioToCharDataset):
     """
     Dataset that loads tensors via a json file containing paths to audio
-    files, transcripts, and durations (in seconds). Each new line is a
-    different sample. Example below:
+    files, transcripts, and durations (in seconds) and F0 along mel length.
+    Each new line is a different sample. Example below:
     {"audio_filepath": "/path/to/audio.wav", "text_filepath":
     "/path/to/audio.txt", "duration": 23.147}
     ...
@@ -300,12 +300,19 @@ class AudioToCharWithDursDataset(AudioToCharDataset):
     transcription", "offset": 301.75, "duration": 0.82, "utt":
     "utterance_id", "ctm_utt": "en_4156", "side": "A"}
 
-    Additionally, user provides path to precomputed durations, which is a pickled python dict with 'tags' and 'durs'
-    keys, both of which are list of examples values. Tag is a unique example identifier, which is a wav filename
-    without suffix. Durations are an additional tuple of two tensors: graphemes durations and blanks durations.
+    Additionally, user provides path to precomputed durations via "durs_file" arg, which is a pickled python dict,
+    mapping example tag to it's durations tensor. Tag is a unique example identifier, which is a wav filename
+    without suffix. Durations are an additional dict of two tensors: graphemes durations and blanks durations with
+    "blanks" and "tokens" keys respectively.
     Example below:
-    {'tags': ['LJ050-0234', 'LJ019-0373'],
-     'durs': [(graphemes_durs0, blanks_durs0), (graphemes_durs1, blanks_durs1)]}
+    {'LJ050-0234': {'blanks': `blanks_durs_tensor`, 'tokens': `tokens_durs_tensor`},
+    ...}
+
+    Additionally, F0 statistics is passed precomputed along mel length via "f0_file" arg, which is a pickled python
+    dict, mapping example tag to it's f0 tensor.
+    Example below:
+    {'LJ050-0234': `f0_tensor`,
+    ...}
 
     Args:
         **kwargs: Passed to AudioToCharDataset constructor.
@@ -329,6 +336,8 @@ class AudioToCharWithDursDataset(AudioToCharDataset):
             'text': NeuralType(('B', 'T'), LabelsType()),
             'text_len': NeuralType(('B',), LengthsType()),
             'durs': NeuralType(('B', 'T'), LengthsType()),
+            'f0': NeuralType(('B', 'T'), FloatType()),
+            'f0_mask': NeuralType(('B', 'T'), MaskType()),
         }
 
     @staticmethod
@@ -353,40 +362,46 @@ class AudioToCharWithDursDataset(AudioToCharDataset):
         return vocab
 
     def __init__(self, **kwargs):
-        durs_path = kwargs.pop('durs_path')
-        rep = kwargs.pop('rep', False)
+        durs_file = kwargs.pop('durs_file', None)
+        f0_file = kwargs.pop('f0_file', None)
+        blanking = kwargs.pop('blanking', False)
         self.vocab = self.make_vocab(**kwargs.pop('vocab', {}))
-        kwargs.setdefault('labels', [])
-
+        kwargs.setdefault('labels', [])  # For compatibility.
         super().__init__(**kwargs)
 
-        pth = torch.load(durs_path)
-        tag2d = dict(zip(pth['tags'], pth['durs']))
-        durs = []
+        tags = []
         for i, e in enumerate(self.collection):
             tag = os.path.splitext(os.path.basename(e.audio_file))[0]
-            durs.append(tag2d[tag])
-        self.durs = durs
-        self.rep = rep
+            tags.append(tag)
+        if durs_file:
+            tag2durs = torch.load(durs_file)
+            durs = []
+            for tag in tags:
+                tag_durs = tag2durs[tag]
+                durs.append(self.interleave(tag_durs['blanks'], tag_durs['tokens']))
+            self.durs = durs
+        if f0_file:
+            tag2f0 = torch.load(f0_file)
+            self.f0 = [tag2f0[tag] for tag in tags]
+        self.blanking = blanking
 
     def __getitem__(self, item):
         sample = self.collection[item]
         audio, audio_len, _, _ = super().__getitem__(item)  # noqa
         text = self.vocab.encode(sample.text_raw)
         text, text_len = torch.tensor(text).long(), torch.tensor(len(text)).long()
-        blanks_durs, graphemes_durs = self.durs[item]
-
+        durs, f0 = self.durs[item], self.f0[item]
         return (
             audio,
             audio_len,
             text,
             text_len,
-            blanks_durs,
-            graphemes_durs,
+            durs,
+            f0,
         )
 
     @staticmethod
-    def _merge(tensors, dim=0, value=0, dtype=None):
+    def merge(tensors, dim=0, value=0, dtype=None):
         """Merges list of tensors into one."""
         tensors = [tensor if isinstance(tensor, torch.Tensor) else torch.tensor(tensor) for tensor in tensors]
         dim = dim if dim != -1 else len(tensors[0].shape) - 1
@@ -399,8 +414,27 @@ class AudioToCharWithDursDataset(AudioToCharDataset):
             new_tensors.append(F.pad(tensor, pad=pad, value=value))
         return torch.stack(new_tensors).to(dtype=dtype)
 
+    @classmethod
+    def repeat_merge(cls, x, reps, pad):
+        """Repeats `x` values according to `reps` tensor and merges."""
+        return cls.merge(
+            tensors=[torch.repeat_interleave(text1, durs1) for text1, durs1 in zip(x, reps)], value=pad, dtype=x.dtype,
+        )
+
     @staticmethod
-    def _interleave(x, y):
+    def make_mask(lengths, max_length=None):
+        """Makes mask from list of lengths."""
+        device = lengths.device if torch.is_tensor(lengths) else 'cpu'
+        lengths = lengths if torch.is_tensor(lengths) else torch.tensor(lengths)
+        max_length = max_length or torch.max(lengths)
+        start = torch.tensor(0).int()
+        indices = torch.arange(start=start, end=max_length, device=device)  # noqa
+        mask = indices.lt(lengths.view(-1, 1))
+
+        return mask
+
+    @staticmethod
+    def interleave(x, y):
         """Interleave two tensors."""
         xy = torch.stack([x[:-1], y], dim=1).view(-1)
         xy = F.pad(xy, pad=[0, 1], value=x[-1])
@@ -412,24 +446,20 @@ class AudioToCharWithDursDataset(AudioToCharDataset):
         asr_batch = _speech_collate_fn(list(zip(*batch[:4])), pad_id=self.vocab.pad)
         audio, audio_len, text, text_len = asr_batch
 
-        text = [
-            self._interleave(
-                x=torch.empty(len(t) + 1, dtype=torch.long, device=t.device,).fill_(self.vocab.blank), y=t,
-            )
-            for t in text
-        ]
-        text = self._merge(text, value=self.vocab.pad, dtype=torch.long)
-        text_len = text_len * 2 + 1
+        if self.blanking:
+            text = [
+                self.interleave(
+                    x=torch.empty(len(t) + 1, dtype=torch.long, device=t.device).fill_(self.vocab.blank), y=t,
+                )
+                for t in text
+            ]
+            text = self.merge(text, value=self.vocab.pad, dtype=torch.long)
+            text_len = text_len * 2 + 1
 
-        blanks_durs, graphemes_durs = batch[4:]
-        durs = [self._interleave(b, c) for b, c in zip(blanks_durs, graphemes_durs)]
-        durs = self._merge(durs, dtype=torch.long).to(text.device)
-
-        if self.rep:
-            text = self._merge(
-                tensors=[torch.repeat_interleave(text1, durs1) for text1, durs1 in zip(text, durs)], dtype=torch.long,
-            )
-            text_len = durs.sum(-1)
+        durs, f0 = batch[4:]
+        durs = self.merge(durs, dtype=torch.long)
+        f0_mask = self.make_mask([f.shape[-1] for f in f0])  # noqa
+        f0 = self.merge(f0, dtype=torch.float)
 
         return (
             audio,
@@ -437,6 +467,8 @@ class AudioToCharWithDursDataset(AudioToCharDataset):
             text,
             text_len,
             durs,
+            f0,
+            f0_mask,
         )
 
 
