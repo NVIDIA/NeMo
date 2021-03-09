0bd3eab14fdce525a3ecfaabdaf05070a2d68de4
ericharper <complex451@gmail.com>
2021-03-09 13:29:42 -0700

    refactor decoder config
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/machine_translation/mt_enc_dec_config.py
+++ nemo/collections/nlp/models/machine_translation/mt_enc_dec_config.py
@@ -80,7 +80,7 @@ class AAYNBaseConfig(MTEncDecModelConfig):
         library='nemo',
         model_name=None,
         pretrained=False,
-        transformer_config=NeMoTransformerEncoderConfig(
+        transformer=NeMoTransformerEncoderConfig(
             vocab_size=MISSING,
             hidden_size=512,
             inner_size=2048,
@@ -93,14 +93,21 @@ class AAYNBaseConfig(MTEncDecModelConfig):
         encoder=True,
     )
 
-    decoder: NeMoTransformerConfig = NeMoTransformerConfig(
-        hidden_size=512,
-        inner_size=2048,
-        num_layers=6,
-        num_attention_heads=8,
-        ffn_dropout=0.1,
-        attn_score_dropout=0.1,
-        attn_layer_dropout=0.1,
+    decoder: TransformerConfig = TransformerConfig(
+        library='nemo',
+        model_name=None,
+        pretrained=False,
+        transformer=NeMoTransformerConfig(
+            vocab_size=MISSING,
+            hidden_size=512,
+            inner_size=2048,
+            num_layers=6,
+            num_attention_heads=8,
+            ffn_dropout=0.1,
+            attn_score_dropout=0.1,
+            attn_layer_dropout=0.1,
+        ),
+        encoder=False,
     )
 
     head: TokenClassifierConfig = TokenClassifierConfig(log_softmax=True)
