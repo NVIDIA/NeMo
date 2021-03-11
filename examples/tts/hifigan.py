befa161ecfcf0f46331b30cc1693bdf046e2eea5
Felix Kreuk <felixkreuk@gmail.com>
2021-03-12 01:43:46 +0200

    fix LGTM bot error
    Signed-off-by: Felix Kreuk <felixkreuk@gmail.com>

--- examples/tts/hifigan.py
+++ examples/tts/hifigan.py
@@ -14,7 +14,6 @@
 
 import pytorch_lightning as pl
 
-from nemo.collections.common.callbacks import LogEpochTimeCallback
 from nemo.collections.tts.models import HifiGanModel
 from nemo.core.config import hydra_runner
 from nemo.utils.exp_manager import exp_manager
