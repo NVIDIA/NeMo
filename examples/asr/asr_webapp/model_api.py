64f25908b598f8304a182fba07dc8fc23bc82e66
Somshubra Majumdar <titu1994@gmail.com>
2021-03-16 18:38:48 -0700

    Add header
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- examples/asr/asr_webapp/model_api.py
+++ examples/asr/asr_webapp/model_api.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import contextlib
 import torch
 
@@ -30,6 +44,7 @@ def initialize_model(model_name):
     if model_name not in MODEL_CACHE:
         model = nemo_asr.models.ASRModel.from_pretrained(model_name, map_location='cpu')
         model.freeze()
+
         # cache model
         MODEL_CACHE[model_name] = model
 
@@ -63,6 +78,7 @@ def transcribe_all(filepaths, model_name, use_gpu_if_available=True):
 
     logging.info(f"Finished transcribing {len(filepaths)} files !")
 
+    # Force onto CPU
     model = model.cpu()
     MODEL_CACHE[model_name] = model
 
