ec7ba1d973ee197c1f1b0ff7d07e567fa16dc76c
Yang Zhang <yangzhang@nvidia.com>
2021-03-16 18:00:11 -0700

    add header
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- tools/text_denormalization/clean_eval_data.py
+++ tools/text_denormalization/clean_eval_data.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import sys
 from argparse import ArgumentParser
@@ -15,9 +29,9 @@ from denormalization.data_loader_utils import (
 from utils import num_to_word
 
 
-'''
+"""
 filter_loaded_data() cleans loaded normalization data for usage for denormalization
-'''
+"""
 
 
 class Filter:
