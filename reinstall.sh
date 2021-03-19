b2326672abfde91da3f381530181808c99b8a8bf
Yang Zhang <yangzhang@nvidia.com>
2021-03-19 11:18:03 -0700

    try fix
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -3,11 +3,6 @@ set -e
 
 INSTALL_OPTION=${1:-"development"}
 
-if [[ "$INSTALL_OPTION" == "development" ]]; then
-    PIP_INSTALL_OPTION="--editable" # for development mode, if unset release mode
-else
-    PIP_INSTALL_OPTION=""
-fi
 
 PIP=pip
 
@@ -20,10 +15,18 @@ ${PIP} uninstall -y nemo_asr
 ${PIP} uninstall -y nemo_nlp
 ${PIP} uninstall -y nemo_tts
 ${PIP} uninstall -y nemo_simple_gan
+${PIP} uninstall -y nemo_cv
 
 ${PIP} install -U setuptools
 
-echo 'Installing stuff'
-${PIP} install ${PIP_INSTALL_OPTION} ".[all]"
+echo 'Installing nemo'
+if [[ "$INSTALL_OPTION" == "development" ]]; then
+    ${PIP} install --editable ".[all]"
+else
+    rm -rf dist/
+    python setup.py bdist_wheel
+    DIST_FILE=$(find ./dist -name "*.whl" | head -n 1)
+    ${PIP} install "dist/${DIST_FILE}[all]"
+fi
 
 echo 'All done!'
