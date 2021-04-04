2ea8a614a2f8c08fad7cf79899e22972930c884c
Somshubra Majumdar <titu1994@gmail.com>
2021-04-04 13:45:37 -0700

    Add more warnings
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/asr/parts/jasper.py
+++ nemo/collections/asr/parts/jasper.py
@@ -32,7 +32,6 @@ try:
 except ImportError:
     PYTORCH_QUANTIZATION_AVAILABLE = False
 
-
 jasper_activations = {"hardtanh": nn.Hardtanh, "relu": nn.ReLU, "selu": nn.SELU, "swish": Swish}
 
 
@@ -69,7 +68,7 @@ def compute_new_kernel_size(kernel_size, kernel_width):
     return new_kernel_size
 
 
-def get_same_padding(kernel_size, stride, dilation):
+def get_same_padding(kernel_size, stride, dilation) -> int:
     if stride > 1 and dilation > 1:
         raise ValueError("Only stride OR dilation may be greater than 1")
     return (dilation * (kernel_size - 1)) // 2
@@ -79,21 +78,32 @@ def get_asymtric_padding(kernel_size, stride, dilation, future_context):
     if stride > 1 and dilation > 1:
         raise ValueError("Only stride OR dilation may be greater than 1")
 
+    left_context = kernel_size - 1 - future_context
+    right_context = future_context
+
+    symmetric_padding = get_same_padding(kernel_size, stride, dilation)
+
     if kernel_size <= future_context:
         # kernel size is smaller than future context, equivalent to using entire context of kernel
         # simply return symmetric padding for this scenario
-        return get_same_padding(kernel_size, stride, dilation)
+        logging.warning(
+            f"Future context window is larger than the kernel size!\n"
+            f"Left context = {left_context} | Right context = greater than {right_context} | "
+            f"Kernel size = {kernel_size}\n"
+            f"Switching to symmetric padding (left context = right context = {symmetric_padding})"
+        )
+        return symmetric_padding
 
-    left_context = (kernel_size - 1 - future_context)
-    right_context = future_context
-
-    if left_context < ((kernel_size - 1) // 2):
-        logging.warning(f"Future context window is larger than half the kernel size!\n"
-                        f"Left context = {left_context} | Right context = {right_context} | "
-                        f"Kernel size = {kernel_size}")
+    if left_context < symmetric_padding:
+        logging.warning(
+            f"Future context window is larger than half the kernel size!\n"
+            f"Conv layer therefore uses more future information than past to compute its output!\n"
+            f"Left context = {left_context} | Right context = {right_context} | "
+            f"Kernel size = {kernel_size}"
+        )
 
     if dilation > 1:
-        left_context = (dilation * kernel_size - 1 - dilation * future_context)
+        left_context = dilation * kernel_size - 1 - dilation * future_context
         right_context = dilation * future_context
         return (left_context, right_context)
 
