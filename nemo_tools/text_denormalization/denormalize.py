04110d1bc8a97502492b4c1928315fa670b6a6e5
ekmb <ebakhturina@nvidia.com>
2021-04-06 12:10:51 -0700

    add time
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- nemo_tools/text_denormalization/denormalize.py
+++ nemo_tools/text_denormalization/denormalize.py
@@ -209,5 +209,9 @@ DENORMALIZERS = {
 
 
 if __name__ == "__main__":
+    import time
+
+    start = time.perf_counter()
     s = sys.argv[1]  # input string
     denormalize(s, verbose=True)
+    print((time.perf_counter() - start) * 1000)
