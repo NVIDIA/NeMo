5b5598226e0a60aab6c6c0fa30c90800f4be7a24
Aleksei Kalinov <alekseia@nvidia.com>
2021-04-06 05:58:45 -0700

    Add comment to explain cached masks.
    Signed-off-by: Aleksei Kalinov <alekseia@nvidia.com>

--- nemo/collections/asr/parts/jasper.py
+++ nemo/collections/asr/parts/jasper.py
@@ -182,6 +182,8 @@ class MaskedConv1d(nn.Module):
             2 * self.conv.padding[0] == self.conv.dilation[0] * (self.conv.kernel_size[0] - 1)
         )
 
+        # `self.lens` caches consecutive integers from 0 to `self.max_len` that are used to compute the mask for a
+        # batch. Recomputed to bigger size as needed. Stored on a device of the latest batch lens.
         if self.use_mask:
             self.max_len = 0
             self.lens = None
