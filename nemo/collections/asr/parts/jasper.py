befd040436488ab00fab70fd39558c58f99253e5
Somshubra Majumdar <titu1994@gmail.com>
2021-04-07 23:25:21 -0700

    Add asymmetric padding support to Jasper Block (#2024)
    * Implement first pass asymtric pad
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Implement first pass asymtric pad
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Implement first pass asymtric pad
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Implement first pass asymtric pad
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Implement first pass asymtric pad
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Add more configs
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Update configs
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Add more warnings
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Add DDP flag
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Add efficient caching of lengths
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Add jasper tests
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Revert change
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * revert
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Fix style
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    Signed-off-by: Virginia Adams <vadams@nvidia.com>

--- nemo/collections/asr/parts/jasper.py
+++ nemo/collections/asr/parts/jasper.py
@@ -225,9 +225,17 @@ class MaskedConv1d(nn.Module):
             )
         self.use_mask = use_mask
         self.heads = heads
+
+        # Calculations for "same" padding cache
         self.same_padding = (self.conv.stride[0] == 1) and (
             2 * self.conv.padding[0] == self.conv.dilation[0] * (self.conv.kernel_size[0] - 1)
         )
+        if self.pad_layer is None:
+            self.same_padding_asymmetric = False
+        else:
+            self.same_padding_asymmetric = (self.conv.stride[0] == 1) and (
+                sum(self._padding) == self.conv.dilation[0] * (self.conv.kernel_size[0] - 1)
+            )
 
         # `self.lens` caches consecutive integers from 0 to `self.max_len` that are used to compute the mask for a
         # batch. Recomputed to bigger size as needed. Stored on a device of the latest batch lens.
