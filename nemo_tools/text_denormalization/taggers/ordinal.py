e44a4053d63b0fb1ba4915930223c98593dd1610
Evelina <10428420+ekmb@users.noreply.github.com>
2021-04-09 14:31:44 -0700

    python denorm speed up (#2038)
    * adding files for export
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * adding docker build and launch
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * adding docker build and launch
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * adding kbps mbps
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * fix dockerfile
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * adding doc string
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * spell fix, doc fix
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * spell fix, doc fix
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * spell fix, doc fix
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * restart jenkins
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * shared graphs
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * add states count
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * clean up
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * style
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * rearr args
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    Co-authored-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo_tools/text_denormalization/taggers/ordinal.py
+++ nemo_tools/text_denormalization/taggers/ordinal.py
@@ -15,7 +15,6 @@
 import pynini
 from nemo_tools.text_denormalization.data_loader_utils import get_abs_path
 from nemo_tools.text_denormalization.graph_utils import NEMO_CHAR, GraphFst
-from nemo_tools.text_denormalization.taggers.cardinal import CardinalFst
 from pynini.lib import pynutil
 
 
@@ -23,13 +22,15 @@ class OrdinalFst(GraphFst):
     """
     Finite state transducer for classifying ordinal
         e.g. thirteenth -> ordinal { integer: "13" }
+
+    Args:
+        cardinal: Cardinal GraphFST
     """
 
-    def __init__(self):
+    def __init__(self, cardinal: GraphFst):
         super().__init__(name="ordinal", kind="classify")
 
-        cardinal_graph = CardinalFst().graph_no_exception
-
+        cardinal_graph = cardinal.graph_no_exception
         graph_digit = pynini.string_file(get_abs_path("data/ordinals/digit.tsv"))
         graph_teens = pynini.string_file(get_abs_path("data/ordinals/teen.tsv"))
         # change to General UTF8
