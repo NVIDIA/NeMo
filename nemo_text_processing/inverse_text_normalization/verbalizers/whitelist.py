5499f582a0cd136fad1e28d56e39f2d61fcc6b1a
Somshubra Majumdar <titu1994@gmail.com>
2021-04-12 21:31:28 -0700

    Cleanup of NeMo tests and tools (#2054)
    * Correct compat for numba cuda check on CPU
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Patch tests
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Fix Dockerfile
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Import guard all pynini, patch all tests for pynini
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Style fixes
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    Signed-off-by: Карпов Николай Вячеславович <karpov.n.vyac@sberbank.ru>

--- nemo_text_processing/inverse_text_normalization/verbalizers/whitelist.py
+++ nemo_text_processing/inverse_text_normalization/verbalizers/whitelist.py
@@ -14,9 +14,15 @@
 # limitations under the License.
 
 
-import pynini
 from nemo_text_processing.inverse_text_normalization.graph_utils import NEMO_CHAR, NEMO_SIGMA, GraphFst, delete_space
-from pynini.lib import pynutil
+
+try:
+    import pynini
+    from pynini.lib import pynutil
+
+    PYNINI_AVAILABLE = True
+except (ModuleNotFoundError, ImportError):
+    PYNINI_AVAILABLE = False
 
 
 class WhiteListFst(GraphFst):
