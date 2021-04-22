c60e24958a0c4166b6e0f93186513a97de4e424d
Somshubra Majumdar <titu1994@gmail.com>
2021-04-21 20:52:03 -0700

    Add Multi Node Rank tests
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/core/classes/modelPT.py
+++ nemo/core/classes/modelPT.py
@@ -232,6 +232,7 @@ class ModelPT(LightningModule, Model):
         return_config: bool = False,
         trainer: Optional[Trainer] = None,
         save_restore_connector: SaveRestoreConnector = None,
+        trainer: Optional['Trainer'] = None,
     ):
         """
         Restores model instance (weights and configuration) from .nemo file.
@@ -248,6 +249,8 @@ class ModelPT(LightningModule, Model):
             trainer: Optional, a pytorch lightning Trainer object that will be forwarded to the
                 instantiated model's constructor.
             save_restore_connector (SaveRestoreConnector): Can be overrided to add custom save and restore logic.
+            trainer: Optional, a pytorch lightning Trainer object that will be forwarded to the
+                instantiated model's constructor.
 
             Example:
                 ```
