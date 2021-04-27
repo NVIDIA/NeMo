0021dcb40e5155904cc078fcb89df60d8a3941a8
Yang Zhang <yangzhang@nvidia.com>
2021-04-27 11:36:59 -0700

    reuse token parser from tn
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/inverse_normalize.py
+++ nemo_text_processing/inverse_text_normalization/inverse_normalize.py
@@ -18,8 +18,8 @@ from collections import OrderedDict
 from typing import List
 
 from nemo_text_processing.inverse_text_normalization.taggers.tokenize_and_classify import ClassifyFst
-from nemo_text_processing.text_normalization.token_parser import PRESERVE_ORDER_KEY, TokenParser
 from nemo_text_processing.inverse_text_normalization.verbalizers.verbalize_final import VerbalizeFinalFst
+from nemo_text_processing.text_normalization.token_parser import PRESERVE_ORDER_KEY, TokenParser
 from tqdm import tqdm
 
 try:
