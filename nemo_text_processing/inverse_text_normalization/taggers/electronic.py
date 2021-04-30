6da283910888e54e9ffd90e085685ed461d2a8b7
Yang Zhang <yzhang123@users.noreply.github.com>
2021-04-29 18:58:54 -0700

    Itn add classes (#2141)
    * move do_training flag to config
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * added telephone to itn
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    
    * add telephone and email to itn
    
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/taggers/electronic.py
+++ nemo_text_processing/inverse_text_normalization/taggers/electronic.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+# Copyright 2015 and onwards Google, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +13,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo_text_processing.inverse_text_normalization.graph_utils import GraphFst
+from nemo_text_processing.inverse_text_normalization.data_loader_utils import get_abs_path
+from nemo_text_processing.inverse_text_normalization.graph_utils import NEMO_ALPHA, GraphFst, insert_space
+
+try:
+    import pynini
+    from pynini.lib import pynutil
+
+    PYNINI_AVAILABLE = True
+except (ModuleNotFoundError, ImportError):
+    PYNINI_AVAILABLE = False
 
 
 class ElectronicFst(GraphFst):
     """
-    Finite state transducer for classifying electronic
+    Finite state transducer for classifying electronic: as URLs, email addresses, etc.
+        e.g. c d f one at a b c dot e d u -> tokens { electronic { username: "cdf1" domain: "abc.edu" } }
     """
 
     def __init__(self):
         super().__init__(name="electronic", kind="classify")
-        # protocol, username, password, domain, port, path, query_string, fragment_id
+
+        delete_extra_space = pynutil.delete(" ")
+        alpha_num = (
+            NEMO_ALPHA
+            | pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
+            | pynini.string_file(get_abs_path("data/numbers/zero.tsv"))
+        )
+        username = (
+            pynutil.insert("username: \"")
+            + pynini.closure(
+                alpha_num + delete_extra_space + pynini.closure(pynini.cross("dot", '.') + delete_extra_space, 0, 1)
+            )
+            + alpha_num
+            + pynutil.insert("\"")
+        )
+        single_alphanum = pynini.closure(alpha_num + delete_extra_space) + alpha_num
+        server = single_alphanum | pynini.string_file(get_abs_path("data/electronic/server_name.tsv")).invert()
+        domain = single_alphanum | pynini.string_file(get_abs_path("data/electronic/domain.tsv")).invert()
+        domain_graph = (
+            pynutil.insert("domain: \"")
+            + server
+            + delete_extra_space
+            + pynini.cross("dot", ".")
+            + delete_extra_space
+            + domain
+            + pynutil.insert("\"")
+        )
+        graph = username + delete_extra_space + pynutil.delete("at") + insert_space + delete_extra_space + domain_graph
+
+        final_graph = self.add_tokens(graph)
+        self.fst = final_graph.optimize()
