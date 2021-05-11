5141c871dd53dfffc84fdaafd6792066600ded71
Somshubra Majumdar <titu1994@gmail.com>
2021-05-11 13:21:39 -0700

    Correct stride level when computing context window
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/asr/parts/asr_module_utils.py
+++ nemo/collections/asr/parts/asr_module_utils.py
@@ -72,7 +72,7 @@ def _update_se_context_window(model: 'ASRModel', context_window: int, cfg: Optio
 
         if type(m) == jasper.MaskedConv1d:
             if m.conv.stride[0] > 1 and 'mconv' in name:
-                context_window = context_window // 2
+                context_window = context_window // m.conv.stride[0]
 
         if type(m) == jasper.SqueezeExcite:
             m.change_context_window(context_window=context_window)
