748197b4732b16dac9a4ca476060d83f907b032a
fayejf <fayejf07@gmail.com>
2021-05-13 16:58:12 -0700

    bugfix post_ln
    Signed-off-by: fayejf <fayejf07@gmail.com>

--- nemo/collections/nlp/modules/common/transformer/transformer_decoders.py
+++ nemo/collections/nlp/modules/common/transformer/transformer_decoders.py
@@ -173,15 +173,19 @@ class TransformerDecoder(nn.Module):
         cached_mems_list = [memory_states]
 
         for i, layer in enumerate(self.layers):
-            if self.pre_ln and i == len(self.layers) - 1:
-                decoder_states = layer(decoder_states)
+            if self.pre_ln:
+                if i == len(self.layers) - 1:
+                    decoder_states = layer(decoder_states)
+                else:
+                    decoder_states = layer(
+                        decoder_states, decoder_attn_mask, memory_states, encoder_states, encoder_attn_mask
+                    )
+                if i == len(self.layers) - 2:
+                    continue
             else:
                 decoder_states = layer(
                     decoder_states, decoder_attn_mask, memory_states, encoder_states, encoder_attn_mask
                 )
-                if i == len(self.layers) - 2:
-                    continue
-
             memory_states = self._get_memory_states(decoder_states, decoder_mems_list, i + 1)
             cached_mems_list.append(memory_states)
 
