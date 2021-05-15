3f5b0e7e594f03007c1f0aff52625cd68c7e9c13
fayejf <fayejf07@gmail.com>
2021-05-14 17:18:44 -0700

    fix and improve
    Signed-off-by: fayejf <fayejf07@gmail.com>

--- nemo/collections/nlp/modules/common/transformer/transformer_encoders.py
+++ nemo/collections/nlp/modules/common/transformer/transformer_encoders.py
@@ -118,10 +118,7 @@ class TransformerEncoder(nn.Module):
     ):
         super().__init__()
 
-        self.pre_ln = pre_ln
-        self.pre_ln_final_layer_norm = pre_ln_final_layer_norm
-
-        if self.pre_ln and self.pre_ln_final_layer_norm:
+        if pre_ln and pre_ln_final_layer_norm:
             self.final_layer_norm = nn.LayerNorm(hidden_size, eps=1e-5)
         else:
             self.final_layer_norm = None
@@ -168,7 +165,7 @@ class TransformerEncoder(nn.Module):
             memory_states = self._get_memory_states(encoder_states, encoder_mems_list, i + 1)
             cached_mems_list.append(memory_states)
 
-        if self.pre_ln and self.pre_ln_final_layer_norm:
+        if self.final_layer_norm is not None:
             encoder_states = self.final_layer_norm(encoder_states)
             memory_states = self._get_memory_states(encoder_states, encoder_mems_list, i + 1)
             cached_mems_list.append(memory_states)
