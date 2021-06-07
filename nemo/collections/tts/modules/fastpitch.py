5b4048d62d16b3b86f94243a894b49e8722e105f
Jason <jasoli@nvidia.com>
2021-06-07 12:14:44 -0700

    update
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/tts/modules/fastpitch.py
+++ nemo/collections/tts/modules/fastpitch.py
@@ -104,9 +104,9 @@ def average_pitch(pitch, durs):
 class ConvReLUNorm(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=1, dropout=0.0):
         super(ConvReLUNorm, self).__init__()
-        self.conv = torch.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=(kernel_size // 2))
-        self.norm = torch.LayerNorm(out_channels)
-        self.dropout = torch.Dropout(dropout)
+        self.conv = torch.nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=(kernel_size // 2))
+        self.norm = torch.nn.LayerNorm(out_channels)
+        self.dropout = torch.nn.Dropout(dropout)
 
     def forward(self, signal):
         out = torch.nn.functional.relu(self.conv(signal))
