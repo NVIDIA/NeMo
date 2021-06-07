db68d606a916f60af9e7cb5c00d1844b9203f069
Somshubra Majumdar <titu1994@gmail.com>
2021-06-07 12:23:49 -0700

    Update OmegaConf compatibility
    Signed-off-by: smajumdar <titu1994@gmail.com>
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/core/classes/modelPT.py
+++ nemo/core/classes/modelPT.py
@@ -307,10 +307,7 @@ class ModelPT(LightningModule, Model):
         if self.artifacts is not None and len(self.artifacts) > 0:
             conf = OmegaConf.load(path2yaml_file)
             for conf_path, item in self.artifacts.items():
-                if item.hashed_path is None:
-                    conf.update_node(conf_path, item.path)
-                else:
-                    conf.update_node(conf_path, item.hashed_path)
+                OmegaConf.update(conf, conf_path, item.path)
             with open(path2yaml_file, 'w') as fout:
                 OmegaConf.save(config=conf, f=fout, resolve=True)
 
