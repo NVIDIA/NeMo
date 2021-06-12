fabff20e0c737708c5540cb30ac577e4f943a13c
Somshubra Majumdar <titu1994@gmail.com>
2021-06-12 16:59:05 -0700

    Double test of cuda numba
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/core/utils/numba_utils.py
+++ nemo/core/utils/numba_utils.py
@@ -100,7 +100,10 @@ def numba_cuda_is_supported(min_version: str) -> bool:
         if hasattr(cuda, 'is_supported_version'):
             try:
                 cuda_available = cuda.is_available()
-                cuda_compatible = cuda.is_supported_version()
+                if cuda_available:
+                    cuda_compatible = cuda.is_supported_version()
+                else:
+                    cuda_available = False
 
                 if is_numba_compat_strict():
                     return cuda_available and cuda_compatible
