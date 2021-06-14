66b4c6bc459a8dae7687b3230b4fb7459ff8f978
ericharper <complex451@gmail.com>
2021-06-14 15:30:13 -0600

    convert cfg.trainer to dict
    Signed-off-by: ericharper <complex451@gmail.com>

--- examples/nlp/machine_translation/enc_dec_nmt.py
+++ examples/nlp/machine_translation/enc_dec_nmt.py
@@ -15,7 +15,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, open_dict
 from pytorch_lightning import Trainer
 
 from nemo.collections.nlp.data.machine_translation.preproc_mt_data import MTDataPreproc
@@ -109,7 +109,10 @@ def main(cfg: MTEncDecConfig) -> None:
     logging.info(f'Config: {OmegaConf.to_yaml(cfg)}')
 
     # training is managed by PyTorch Lightning
-    trainer = Trainer(plugins=[NLPDDPPlugin()], **cfg.trainer)
+    trainer_cfg = OmegaConf.to_container(cfg.trainer)
+    trainer_cfg.pop('plugins')
+    # trainer = Trainer(plugins=[NLPDDPPlugin()], **cfg.trainer)
+    trainer = Trainer(plugins=[NLPDDPPlugin()], **trainer_cfg)
 
     # tokenizers will be trained and and tarred training data will be created if needed
     # model config is then updated
