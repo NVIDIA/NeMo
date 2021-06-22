5c4d4229fcb5669fedc56303b498443c7eb3ecc9
Micha Livne <mlivne@nvidia.com>
2021-06-21 21:38:31 -0400

    1. Working on bottleneck transformers.

--- nemo/collections/nlp/models/machine_translation/__init__.py
+++ nemo/collections/nlp/models/machine_translation/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.collections.nlp.models.machine_translation.mt_enc_dec_model import MTEncDecModel
+from nemo.collections.nlp.models.machine_translation.mt_enc_dec_model import MTEncDecModel, MTBottleneckModel
