33921c7546031476d43df1a902c02b1034d4134f
ekmb <ebakhturina@nvidia.com>
2021-06-25 14:03:09 -0700

    ru itn + audio updates wip
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

---
+++ examples/nlp/text_normalization/adding_covering_grammars.py
@@ -0,0 +1,95 @@
+try:
+    import pynini
+    from pynini.lib import pynutil
+    from pynini.lib import rewrite
+    from nemo_text_processing.text_normalization.taggers.cardinal import CardinalFst
+    from nemo_text_processing.text_normalization.graph_utils import (
+        NEMO_NON_BREAKING_SPACE,
+        NEMO_SIGMA,
+        SINGULAR_TO_PLURAL,
+        convert_space,
+        delete_space,
+        insert_space,
+    )
+    from pynini.export import export
+
+    PYNINI_AVAILABLE = True
+except (ModuleNotFoundError, ImportError):
+    PYNINI_AVAILABLE = False
+
+from typing import Dict
+
+grammar_dir = '/home/ebakhturina/TextNormalizationCoveringGrammars/src/en'
+
+grammars = {
+    'float.far': ['FLOAT'],
+    'factorization.far': ['FRACTIONAL_PART_UNGROUPED', 'FRACTIONAL_PART_GROUPED', 'FRACTIONAL_PART_UNPARSED'],
+    'numbers.far': ['CARDINAL_NUMBERS', 'ORDINAL_NUMBERS', 'ORDINAL_NUMBERS_UNMARKED'],
+    'money.far': ['MONEY'],
+    'spoken_punct.far': ['SPOKEN_PUNCT'],
+    'spelled.far': ['SPELLED_NO_LETTER', 'SPELLED'],
+    'number_names.far': ['CARDINAL_NUMBER_NAME', 'ORDINAL_NUMBER_NAME'],
+    'numbers_plus.far': ['NUMBERS_PLUS'],
+    'miscellaneous.far': ['MISCELLANEOUS'],
+    'time.far': ['TIME'],
+    'extra_numbers.far': ['DIGITS', 'MIXED_NUMBERS'],
+    'math.far': ['ARITHMETIC'],
+    'urls.far': ['URL', 'EMAIL1', 'EMAIL2', 'EMAILS'],
+    'lexical_map.far': ['LEXICAL_MAP'],
+    'verbalizer.far': ['VERBALIZER'],
+}
+
+grammars = {'verbalizer/verbalizer.far': ['VERBALIZER']}
+
+# grammars = {CARDINAL_NUMBERS = Optimize[
+# numbers.grm:export ORDINAL_NUMBERS_UNMARKED = Optimize[
+# numbers.grm:export ORDINAL_NUMBERS_MARKED = Optimize[
+# numbers.grm:export ORDINAL_NUMBERS }
+fsts = {}
+zero_state_fsts = {}
+final_fst = None
+for far, exports in grammars.items():
+    far_path = f'{grammar_dir}/{far}'
+    for export_ in exports:
+        fst = pynini.Far(far_path, mode='r')[export_]
+        if final_fst is None:
+            final_fst = fst
+        else:
+            final_fst |= fst
+        num_states = fst.num_states()
+        if num_states > 0:
+            fsts[far.replace('.far', '')] = fst
+            # print(far.name, num_states, 'states')
+        else:
+            zero_state_fsts[f'{far}-{export_}'] = fst
+
+print('zero states:', zero_state_fsts.keys())
+print('final num states:', final_fst.num_states())
+print(rewrite.rewrites("28", final_fst))
+input = "двадцать восемь"
+invert = final_fst.invert()
+print(rewrite.rewrites(input, invert.optimize()))
+import pdb
+
+pdb.set_trace()
+print()
+
+
+def _generator_main(file_name: str, graphs: Dict[str, pynini.FstLike]):
+    """
+    Exports graph as OpenFst finite state archive (FAR) file with given file name and rule name.
+
+    Args:
+        file_name: exported file name
+        graph: Pynini WFST graph to be exported
+        rule_name: rule name for graph in created FAR file
+
+    """
+    exporter = export.Exporter(file_name)
+    for rule, graph in graphs.items():
+        exporter[rule] = graph.optimize()
+    exporter.close()
+    print(f'Created {file_name}')
+
+
+# _generator_main("cg_eng.far", {"VERBALIZER": final_fst})
---
+++ nemo_text_processing/inverse_text_normalization/taggers/decimal_ru.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+# Copyright 2015 and onwards Google, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nemo_text_processing.inverse_text_normalization.utils import get_abs_path
+from nemo_text_processing.text_normalization.graph_utils import NEMO_DIGIT, GraphFst, delete_extra_space, delete_space
+
+try:
+    import pynini
+    from pynini.lib import pynutil
+
+    PYNINI_AVAILABLE = True
+except (ModuleNotFoundError, ImportError):
+    PYNINI_AVAILABLE = False
+
+
+def get_quantity(decimal: 'pynini.FstLike', cardinal_up_to_hundred: 'pynini.FstLike') -> 'pynini.FstLike':
+    """
+    Returns FST that transforms either a cardinal or decimal followed by a quantity into a numeral,
+    e.g. one million -> integer_part: "1" quantity: "million"
+    e.g. one point five million -> integer_part: "1" fractional_part: "5" quantity: "million"
+
+    Args: 
+        decimal: decimal FST
+        cardinal_up_to_hundred: cardinal FST
+    """
+    numbers = cardinal_up_to_hundred @ (
+        pynutil.delete(pynini.closure("0")) + pynini.difference(NEMO_DIGIT, "0") + pynini.closure(NEMO_DIGIT)
+    )
+    suffix = pynini.union("million", "billion", "trillion", "quadrillion", "quintillion", "sextillion")
+    res = (
+        pynutil.insert("integer_part: \"")
+        + numbers
+        + pynutil.insert("\"")
+        + delete_extra_space
+        + pynutil.insert("quantity: \"")
+        + suffix
+        + pynutil.insert("\"")
+    )
+    res |= decimal + delete_extra_space + pynutil.insert("quantity: \"") + (suffix | "thousand") + pynutil.insert("\"")
+    return res
+
+
+class DecimalFst(GraphFst):
+    """
+    Finite state transducer for classifying decimal
+        e.g. minus twelve point five o o six billion -> decimal { negative: "true" integer_part: "12"  fractional_part: "5006" quantity: "billion" }
+        e.g. one billion -> decimal { integer_part: "1" quantity: "billion" }
+    Args:
+        cardinal: CardinalFst
+    """
+
+    def __init__(self, cardinal: GraphFst):
+        super().__init__(name="decimal", kind="classify")
+
+        cardinal_graph = cardinal.graph_no_exception
+
+        graph_decimal = pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
+        graph_decimal |= pynini.string_file(get_abs_path("data/numbers/zero.tsv")) | pynini.cross("o", "0")
+
+        graph_decimal = pynini.closure(graph_decimal + delete_space) + graph_decimal
+        self.graph = graph_decimal
+
+        point = pynutil.delete("point")
+
+        optional_graph_negative = pynini.closure(
+            pynutil.insert("negative: ") + pynini.cross("minus", "\"true\"") + delete_extra_space, 0, 1
+        )
+
+        graph_fractional = pynutil.insert("fractional_part: \"") + graph_decimal + pynutil.insert("\"")
+        graph_integer = pynutil.insert("integer_part: \"") + cardinal_graph + pynutil.insert("\"")
+        final_graph_wo_sign = (
+            pynini.closure(graph_integer + delete_extra_space, 0, 1) + point + delete_extra_space + graph_fractional
+        )
+        final_graph = optional_graph_negative + final_graph_wo_sign
+
+        self.final_graph_wo_negative = final_graph_wo_sign | get_quantity(
+            final_graph_wo_sign, cardinal.graph_hundred_component_at_least_one_none_zero_digit
+        )
+        final_graph |= optional_graph_negative + get_quantity(
+            final_graph_wo_sign, cardinal.graph_hundred_component_at_least_one_none_zero_digit
+        )
+        final_graph = self.add_tokens(final_graph)
+        self.fst = final_graph.optimize()
