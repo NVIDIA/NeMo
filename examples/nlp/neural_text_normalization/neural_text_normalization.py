2775d62d91d8783803a99965d5216e0beeb6e88b
Tuan Lai <tuanl@nvidia.com>
2021-06-25 13:18:48 -0700

    Add options to do training or not for tagger/decoder
    Signed-off-by: Tuan Lai <tuanl@nvidia.com>

--- examples/nlp/neural_text_normalization/neural_text_normalization.py
+++ examples/nlp/neural_text_normalization/neural_text_normalization.py
@@ -14,24 +14,26 @@ def main(cfg: DictConfig) -> None:
     logging.info(f'Config Params: {OmegaConf.to_yaml(cfg)}')
 
     # Train the tagger
-    logging.info("================================================================================================")
-    logging.info('Starting training tagger...')
-    tagger_trainer, tagger_model = initialize_model_and_trainer(cfg, TAGGER_MODEL, True)
-    exp_manager(tagger_trainer, cfg.get('tagger_exp_manager', None))
-    tagger_trainer.fit(tagger_model)
-    if cfg.tagger_model.nemo_path:
-        tagger_model.save_to(cfg.tagger_model.nemo_path)
-    logging.info('Training finished!')
+    if cfg.tagger_model.do_training:
+        logging.info("================================================================================================")
+        logging.info('Starting training tagger...')
+        tagger_trainer, tagger_model = initialize_model_and_trainer(cfg, TAGGER_MODEL, True)
+        exp_manager(tagger_trainer, cfg.get('tagger_exp_manager', None))
+        tagger_trainer.fit(tagger_model)
+        if cfg.tagger_model.nemo_path:
+            tagger_model.save_to(cfg.tagger_model.nemo_path)
+        logging.info('Training finished!')
 
     # Train the decoder
-    logging.info("================================================================================================")
-    logging.info('Starting training decoder...')
-    decoder_trainer, decoder_model = initialize_model_and_trainer(cfg, DECODER_MODEL, True)
-    exp_manager(decoder_trainer, cfg.get('decoder_exp_manager', None))
-    decoder_trainer.fit(decoder_model)
-    if cfg.decoder_model.nemo_path:
-        decoder_model.save_to(cfg.decoder_model.nemo_path)
-    logging.info('Training finished!')
+    if cfg.decoder_model.do_training:
+        logging.info("================================================================================================")
+        logging.info('Starting training decoder...')
+        decoder_trainer, decoder_model = initialize_model_and_trainer(cfg, DECODER_MODEL, True)
+        exp_manager(decoder_trainer, cfg.get('decoder_exp_manager', None))
+        decoder_trainer.fit(decoder_model)
+        if cfg.decoder_model.nemo_path:
+            decoder_model.save_to(cfg.decoder_model.nemo_path)
+        logging.info('Training finished!')
 
 if __name__ == '__main__':
     main()
