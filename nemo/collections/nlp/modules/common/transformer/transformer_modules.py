020d36fd72fe267a0759c1558d28feef64d0d432
Micha Livne <mlivne@nvidia.com>
2021-07-09 20:25:38 -0400

    1. Debugging.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/modules/common/transformer/transformer_modules.py
+++ nemo/collections/nlp/modules/common/transformer/transformer_modules.py
@@ -22,7 +22,7 @@ from omegaconf.omegaconf import MISSING
 from torch import nn
 from torch.nn.functional import gelu
 
-from nemo.collections.common.parts import form_attention_mask
+# from nemo.collections.common.parts import form_attention_mask
 
 # __all__ = ["FixedPositionalEncoding", "TransformerEmbedding",
 #            "MultiHeadAttention", "PositionWiseFF", "AttentionBridge"]
