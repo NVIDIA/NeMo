7b12727959d83f283efbb6bfea7814b755698003
Micha Livne <mlivne@nvidia.com>
2021-07-12 16:54:11 -0400

    1. Fixed import statement.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- examples/nlp/machine_translation/enc_dec_nmt-bottleneck.py
+++ examples/nlp/machine_translation/enc_dec_nmt-bottleneck.py
@@ -20,7 +20,7 @@ from pytorch_lightning import Trainer
 
 from nemo.collections.nlp.data.machine_translation.preproc_mt_data import MTDataPreproc
 from nemo.collections.nlp.models.machine_translation.mt_enc_dec_config import MTBottleneckModelConfig
-from nemo.collections.nlp.models.machine_translation.mt_enc_dec_model import MTBottleneckModel
+from nemo.collections.nlp.models.machine_translation.mt_enc_dec_bottleneck_model import MTBottleneckModel
 from nemo.collections.nlp.parts.nlp_overrides import NLPDDPPlugin
 from nemo.core.config import hydra_runner
 from nemo.core.config.modelPT import NemoConfig
