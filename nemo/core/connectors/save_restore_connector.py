97cf17511afeaf717aaa7ed74bcc7bfc1524fc68
ericharper <complex451@gmail.com>
2021-07-20 11:33:25 -0600

    add _default_save_to
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/core/connectors/save_restore_connector.py
+++ nemo/core/connectors/save_restore_connector.py
@@ -14,7 +14,41 @@
 # limitations under the License.
 
 
+from os import path
+import tempfile
+
+import torch
+from nemo.utils import app_state
+
+from nemo.utils.app_state import AppState
+
+
 class SaveRestoreConnector:
     def __init__(self):
         pass
 
+    def _default_save_to(self, save_path: str):
+        """
+        Saves model instance (weights and configuration) into .nemo file.
+        You can use "restore_from" method to fully restore instance from .nemo file.
+
+        .nemo file is an archive (tar.gz) with the following:
+            model_config.yaml - model configuration in .yaml format. You can deserialize this into cfg argument for model's constructor
+            model_wights.chpt - model checkpoint
+
+        Args:
+            save_path: Path to .nemo file where model instance should be saved
+
+        """
+	app_state = AppState()
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            config_yaml = path.join(tmpdir, app_state.model_config_yaml)
+            model_weights = path.join(tmpdir, app_state.model_weights_ckpt)
+            self.to_config_file(path2yaml_file=config_yaml)
+            if hasattr(self, 'artifacts') and self.artifacts is not None:
+                self._handle_artifacts(nemo_file_folder=tmpdir)
+                # We should not update self._cfg here - the model can still be in use
+                self._update_artifact_paths(path2yaml_file=config_yaml)
+            torch.save(self.state_dict(), model_weights)
+            self._make_nemo_file_from_folder(filename=save_path, source_dir=tmpdir)
