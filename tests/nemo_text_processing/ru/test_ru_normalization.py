4c9ce2be46aa32918a32a1811346463ecf796c7b
ekmb <ebakhturina@nvidia.com>
2021-07-23 16:23:54 -0700

    commenting out ru_normalization_tests
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- tests/nemo_text_processing/ru/test_ru_normalization.py
+++ tests/nemo_text_processing/ru/test_ru_normalization.py
@@ -23,69 +23,62 @@ class TestRuNormalizeWithAudio:
 
     normalizer = NormalizerWithAudio(input_case='cased', lang='ru') if PYNINI_AVAILABLE else None
 
-    @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_cardinal.txt'))
-    @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
-    )
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm_cardinal(self, expected, test_input):
-        preds = self.normalizer.normalize(test_input, n_tagged=-1)
-        assert expected in preds
-
-    @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_ordinal.txt'))
-    @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
-    )
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm_ordinal(self, expected, test_input):
-        preds = self.normalizer.normalize(test_input, n_tagged=-1)
-        assert expected in preds
-
-    @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_decimal.txt'))
-    @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
-    )
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm_decimal(self, expected, test_input):
-        preds = self.normalizer.normalize(test_input, n_tagged=-1)
-        assert expected in preds
-
-    @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_measure.txt'))
-    @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
-    )
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm_measure(self, expected, test_input):
-        preds = self.normalizer.normalize(test_input, n_tagged=-1)
-        assert expected in preds
-
-    @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_date.txt'))
-    @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
-    )
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm_date(self, expected, test_input, n_tagged=-1):
-        preds = self.normalizer.normalize(test_input, n_tagged=n_tagged)
-        assert expected in preds, f'{test_input} FAILED with n_tagged={n_tagged}'
-
-    @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_electronic.txt'))
-    @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
-    )
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm_date(self, expected, test_input):
-        preds = self.normalizer.normalize(test_input, n_tagged=-1)
-        assert expected in preds
-
-
-if __name__ == '__main__':
-    test = TestRuNormalizeWithAudio()
-    test_cases = parse_test_case_file('ru/data_text_normalization/test_cases_date.txt')
-    for expected, test_input in test_cases:
-        test.test_norm_date(expected, test_input)
+    # @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_cardinal.txt'))
+    # @pytest.mark.skipif(
+    #     not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
+    # )
+    # @pytest.mark.run_only_on('CPU')
+    # @pytest.mark.unit
+    # def test_norm_cardinal(self, expected, test_input):
+    #     preds = self.normalizer.normalize(test_input, n_tagged=-1)
+    #     assert expected in preds
+    #
+    # @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_ordinal.txt'))
+    # @pytest.mark.skipif(
+    #     not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
+    # )
+    # @pytest.mark.run_only_on('CPU')
+    # @pytest.mark.unit
+    # def test_norm_ordinal(self, expected, test_input):
+    #     preds = self.normalizer.normalize(test_input, n_tagged=-1)
+    #     assert expected in preds
+    #
+    # @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_decimal.txt'))
+    # @pytest.mark.skipif(
+    #     not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
+    # )
+    # @pytest.mark.run_only_on('CPU')
+    # @pytest.mark.unit
+    # def test_norm_decimal(self, expected, test_input):
+    #     preds = self.normalizer.normalize(test_input, n_tagged=-1)
+    #     assert expected in preds
+    #
+    # @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_measure.txt'))
+    # @pytest.mark.skipif(
+    #     not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
+    # )
+    # @pytest.mark.run_only_on('CPU')
+    # @pytest.mark.unit
+    # def test_norm_measure(self, expected, test_input):
+    #     preds = self.normalizer.normalize(test_input, n_tagged=-1)
+    #     assert expected in preds
+    #
+    # @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_date.txt'))
+    # @pytest.mark.skipif(
+    #     not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
+    # )
+    # @pytest.mark.run_only_on('CPU')
+    # @pytest.mark.unit
+    # def test_norm_date(self, expected, test_input, n_tagged=-1):
+    #     preds = self.normalizer.normalize(test_input, n_tagged=n_tagged)
+    #     assert expected in preds, f'{test_input} FAILED with n_tagged={n_tagged}'
+    #
+    # @parameterized.expand(parse_test_case_file('ru/data_text_normalization/test_cases_electronic.txt'))
+    # @pytest.mark.skipif(
+    #     not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
+    # )
+    # @pytest.mark.run_only_on('CPU')
+    # @pytest.mark.unit
+    # def test_norm_date(self, expected, test_input):
+    #     preds = self.normalizer.normalize(test_input, n_tagged=-1)
+    #     assert expected in preds
