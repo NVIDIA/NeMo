92263990db7e8ef0f9142105ae9a5a8e172927dc
Micha Livne <mlivne@nvidia.com>
2021-07-28 15:36:08 -0400

    1. Added TransformerBottleneckEncoderNM.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/modules/common/transformer/transformer_bottleneck.py
+++ nemo/collections/nlp/modules/common/transformer/transformer_bottleneck.py
@@ -70,23 +70,33 @@ class TransformerBottleneckEncoderNM(TransformerEncoderNM):
         arch='',
         hidden_steps=-1,
     ):
-        super().__init__()
-
-        self._vocab_size = vocab_size
-        self._hidden_size = hidden_size
-        self._max_sequence_length = max_sequence_length
-
-        self._embedding = TransformerEmbedding(
-            vocab_size=self._vocab_size,
-            hidden_size=self._hidden_size,
+        super().__init__(
+            vocab_size=vocab_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            inner_size=inner_size,
+            num_attention_heads=num_attention_heads,
             max_sequence_length=max_sequence_length,
             num_token_types=num_token_types,
             embedding_dropout=embedding_dropout,
             learn_positional_encodings=learn_positional_encodings,
+            ffn_dropout=ffn_dropout,
+            attn_score_dropout=attn_score_dropout,
+            attn_layer_dropout=attn_layer_dropout,
+            hidden_act=hidden_act,
+            mask_future=mask_future,
+            pre_ln=pre_ln,
+            pre_ln_final_layer_norm=pre_ln_final_layer_norm,
         )
 
-        self._encoder = TransformerEncoder(
-            hidden_size=self._hidden_size,
+        self._arch = arch
+        self._hidden_steps = hidden_steps
+
+        # replace encoder
+        self._encoder = self._build_encoder(
+            arch=arch,
+            hidden_steps=hidden_steps,
+            hidden_size=hidden_size,
             num_layers=num_layers,
             inner_size=inner_size,
             num_attention_heads=num_attention_heads,
@@ -99,42 +109,39 @@ class TransformerBottleneckEncoderNM(TransformerEncoderNM):
             pre_ln_final_layer_norm=pre_ln_final_layer_norm,
         )
 
+    def _build_encoder(self, arch, **kwargs):
+        """
+        Returns a decoder based on architecture arch and kwargs
+        """
+        if arch not in self.supported_arch:
+            raise ValueError("Unknown arch = {arch}, supported arch = {supported arch}".format(
+                arch=arch,
+                supported_arch=self.supported_arch,
+            ))
+
+        # default non-bottleneck transformer encoder
+        if (not arch) or (arch == "seq2seq"):
+            encoder = self.encoder
+
+        return encoder
+
+    @property
+    def supported_arch(self):
+        return [None, "", "seq2seq", "bridge", "perceiver"]
+
+    @property
+    def arch(self):
+        return self._arch
+
+    @property
+    def hidden_steps(self):
+        return self._hidden_steps
+
     @typecheck()
     def forward(self, input_ids, encoder_mask):
         embeddings = self._embedding(input_ids=input_ids)
         encoder_hidden_states = self._encoder(encoder_states=embeddings, encoder_mask=encoder_mask)
         return encoder_hidden_states
-
-    @property
-    def hidden_size(self):
-        return self._hidden_size
-
-    @property
-    def vocab_size(self):
-        return self._vocab_size
-
-    @property
-    def max_sequence_length(self):
-        return self._max_sequence_length
-
-    @property
-    def embedding(self):
-        return self._embedding
-
-    @property
-    def encoder(self):
-        return self._encoder
-
-    def input_example(self):
-        """
-        Generates input examples for tracing etc.
-        Returns:
-            A tuple of input examples.
-        """
-        sample = next(self.parameters())
-        input_ids = torch.randint(low=0, high=2048, size=(2, 16), device=sample.device)
-        encoder_mask = torch.randint(low=0, high=1, size=(2, 16), device=sample.device)
-        return tuple([input_ids, encoder_mask])
 
 
 class TransformerBottleneckDecoderNM(TransformerDecoderNM):
@@ -183,7 +190,7 @@ class TransformerBottleneckDecoderNM(TransformerDecoderNM):
         self._decoder = self._build_decoder(
             arch=arch,
             hidden_steps=hidden_steps,
-            hidden_size=self.hidden_size,
+            hidden_size=hidden_size,
             num_layers=num_layers,
             inner_size=inner_size,
             num_attention_heads=num_attention_heads,
@@ -193,7 +200,7 @@ class TransformerBottleneckDecoderNM(TransformerDecoderNM):
             hidden_act=hidden_act,
             pre_ln=pre_ln,
             pre_ln_final_layer_norm=pre_ln_final_layer_norm,
-            )
+        )
 
     def _build_decoder(self, arch, **kwargs):
         """
@@ -207,7 +214,7 @@ class TransformerBottleneckDecoderNM(TransformerDecoderNM):
 
         # usual non-bottleneck transformer decoder
         if (not arch) or (arch == "seq2seq"):
-            decoder = TransformerDecoder(**kwargs)
+            decoder = self.decoder
 
         return decoder
 
