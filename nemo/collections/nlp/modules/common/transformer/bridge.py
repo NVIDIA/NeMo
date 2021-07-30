acb2971d6fed0287e1a407876ab6eee9fc23e5a1
Micha Livne <mlivne@nvidia.com>
2021-07-30 11:56:16 -0400

    1. Added BridgeEncoder.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

---
+++ nemo/collections/nlp/modules/common/transformer/bridge.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import Optional
+
+import torch
+from omegaconf.omegaconf import MISSING
+
+from nemo.collections.nlp.modules.common.transformer.transformer import TransformerEncoderNM
+from nemo.collections.nlp.modules.common.transformer.perceiver_encoders import PerceiverEncoder
+
+from nemo.core.classes.common import typecheck
+from nemo.core.classes.exportable import Exportable
+
+
+__all__ = ["PerceiverEncoderNM"]
+
+class PerceiverEncoderNM(TransformerEncoderNM):
+    def __init__(
+        self,
+        vocab_size: int,
+        hidden_size: int,
+        hidden_steps: int,
+        num_layers: int,
+        inner_size: int,
+        num_attention_heads: int,
+        max_sequence_length: int = 512,
+        num_token_types: int = 2,
+        embedding_dropout: float = 0.0,
+        learn_positional_encodings: bool = False,
+        ffn_dropout: float = 0.0,
+        attn_score_dropout: float = 0.0,
+        attn_layer_dropout: float = 0.0,
+        hidden_act: str = 'relu',
+        mask_future: bool = False,
+        pre_ln: bool = False,
+        pre_ln_final_layer_norm: bool = True,
+    ):
+        super().__init__(
+            vocab_size=vocab_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            inner_size=inner_size,
+            num_attention_heads=num_attention_heads,
+            max_sequence_length=max_sequence_length,
+            num_token_types=num_token_types,
+            embedding_dropout=embedding_dropout,
+            learn_positional_encodings=learn_positional_encodings,
+            ffn_dropout=ffn_dropout,
+            attn_score_dropout=attn_score_dropout,
+            attn_layer_dropout=attn_layer_dropout,
+            hidden_act=hidden_act,
+            mask_future=mask_future,
+            pre_ln=pre_ln,
+            pre_ln_final_layer_norm=pre_ln_final_layer_norm,
+        )
+
+        self._encoder = PerceiverEncoder(
+            hidden_size=self._hidden_size,
+            hidden_steps=hidden_steps,
+            num_layers=num_layers,
+            inner_size=inner_size,
+            num_attention_heads=num_attention_heads,
+            ffn_dropout=ffn_dropout,
+            attn_score_dropout=attn_score_dropout,
+            attn_layer_dropout=attn_layer_dropout,
+            hidden_act=hidden_act,
+            mask_future=mask_future,
+            pre_ln=pre_ln,
+            pre_ln_final_layer_norm=pre_ln_final_layer_norm,
+        )
--- nemo/collections/nlp/modules/common/transformer/bridge_encoders.py
+++ nemo/collections/nlp/modules/common/transformer/bridge_encoders.py
@@ -21,18 +21,19 @@ from omegaconf.omegaconf import MISSING
 
 from nemo.collections.common.parts import form_attention_mask
 from nemo.collections.nlp.modules.common.transformer.transformer_modules import AttentionBridge
-from nemo.collections.nlp.modules.common.transformer.transformer_decoders import TransformerDecoder
 from nemo.collections.nlp.modules.common.transformer.transformer_encoders import TransformerEncoder
+from nemo.collections.nlp.modules.common.transformer import AttentionBridge
 
 
-__all__ = ["PerceiverEncoder"]
+__all__ = ["BridgeEncoder"]
 
 
-class PerceiverEncoder(TransformerEncoder):
+class BridgeEncoder(TransformerEncoder):
     def __init__(
         self,
         num_layers: int,
         hidden_size: int,
+        hidden_steps: int,
         inner_size: int,
         mask_future: bool = False,
         num_attention_heads: int = 1,
@@ -42,9 +43,7 @@ class PerceiverEncoder(TransformerEncoder):
         hidden_act: str = "relu",
         pre_ln: bool = False,
         pre_ln_final_layer_norm: bool = True,
-        hidden_steps: int = 1,
-        hidden_init_method: str = "params",
-        hidden_blocks=2,
+        hidden_blocks=1,
     ):
         super().__init__(
             num_layers=num_layers,
@@ -61,38 +60,17 @@ class PerceiverEncoder(TransformerEncoder):
         )
 
         self._hidden_steps = hidden_steps
-        self._hidden_init_method = hidden_init_method
         self._hidden_blocks = hidden_blocks
 
-        # self-attention encoder
-        self.cross_att = TransformerDecoder(
-            num_layers=1,
+        if (hidden_blocks < 1):
+            raise ValueError(f"hidden_blocks = {hidden_blocks} but is expected to be >= 1")
+
+        # attention bride
+        self.att_bridge = AttentionBridge(
             hidden_size=hidden_size,
-            inner_size=inner_size,
-            num_attention_heads=num_attention_heads,
-            attn_score_dropout=attn_score_dropout,
-            attn_layer_dropout=attn_layer_dropout,
-            ffn_dropout=ffn_dropout,
-            hidden_act=hidden_act,
-            pre_ln=pre_ln,
-            pre_ln_final_layer_norm=pre_ln_final_layer_norm,
+            k=hidden_steps,
+            bridge_size=inner_size,
         )
-
-        if self._hidden_init_method == "params":
-            # learnable initial hidden values
-            self.init_hidden = torch.nn.Parameter(
-                torch.nn.init.xavier_normal_(torch.empty(hidden_steps, hidden_size))
-            )
-        elif self._hidden_init_method == "bridge":
-            # initialize latent with attention bridge
-            self.att_bridge = AttentionBridge(
-                hidden_size=hidden_size,
-                k=hidden_steps,
-                bridge_size=inner_size,
-            )
-        else:
-            raise ValueError("Unknown hidden_init_method = {hidden_init_method}. Supported methods: params, bridge")
-
 
     @property
     def hidden_steps(self):
@@ -102,45 +80,26 @@ class PerceiverEncoder(TransformerEncoder):
     def hidden_blocks(self):
         return self._hidden_blocks
 
-    @property
-    def hidden_init_method(self):
-        return self._hidden_init_method
-
     def forward(self, encoder_states, encoder_mask):
         """
         Args:
             encoder_states: output of the encoder (B x L_enc x H)
             encoder_mask: encoder inputs mask (B x L_enc)
         """
+        # self-attention over input
+        hidden_states = self(encoder_states=encoder_states, encoder_mask=encoder_mask)
+
+        # project encoder states to a fixed steps hidden using k attention heads
+        hidden_states= self.att_bridge(hidden=hidden_states, hidden_mask=encoder_mask)
+
         # all hidden values are active
         hidden_mask = torch.ones(encoder_states.shape[0], self._hidden_steps,
                                  dtype=encoder_mask.dtype, device=encoder_mask.device)
 
-        # initialize hidden state
-        if self._hidden_init_method == "params":
-            # initialize latent with learned parameters
-            hidden_states = self.init_hidden
-        elif self._hidden_init_method == "bridge":
-            # initialize latent with attention bridge
-            hidden_states = self.att_bridge(
-                hidden=encoder_states,
-                hidden_mask=encoder_mask,
-            )
 
-        # apply block (cross-attention, self-attention) multiple times
-        for block in range(self._hidden_blocks):
-            # cross attention of hidden over encoder states
-            hidden_states = self.cross_att(
-                decoder_states=hidden_states,
-                decoder_mask=hidden_mask,
-                encoder_states=encoder_states,
-                encoder_mask=encoder_mask,
-            )
 
-            # self-attention over hidden
-            hidden_states = self(
-                encoder_states=hidden_states,
-                encoder_mask=hidden_mask,
-            )
+        # apply self-attention over fixed-size hidden_states
+        for block in range(self._hidden_blocks-1):
+            hidden_states = super().forward(encoder_states=hidden_states, encoder_mask=hidden_mask)
 
         return hidden_states, hidden_mask
