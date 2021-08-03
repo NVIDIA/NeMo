45a58be7c4ff7a05fcca14196b0845270a460765
Somshubra Majumdar <titu1994@gmail.com>
2021-08-03 11:01:51 -0700

    Integrate NVIDIA DALI 1.4 to NeMo ASR (#2567)
    * Initial prototype of ASR DALI integration with DALI 1.4
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Update dali support to 1.4
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Fix docs
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Address comments
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
    
    * Address comments
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Correct module utils
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    Co-authored-by: Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/asr/models/ctc_bpe_models.py
+++ nemo/collections/asr/models/ctc_bpe_models.py
@@ -195,6 +195,19 @@ class EncDecCTCModelBPE(EncDecCTCModel, ASRBPEMixin):
             augmentor = None
 
         shuffle = config['shuffle']
+        device = 'gpu' if torch.cuda.is_available() else 'cpu'
+        if config.get('use_dali', False):
+            device_id = self.local_rank if device == 'gpu' else None
+            dataset = audio_to_text_dataset.get_dali_bpe_dataset(
+                config=config,
+                tokenizer=self.tokenizer,
+                shuffle=shuffle,
+                device_id=device_id,
+                global_rank=self.global_rank,
+                world_size=self.world_size,
+                preprocessor_cfg=self._cfg.preprocessor,
+            )
+            return dataset
 
         # Instantiate tarred dataset loader or normal dataset loader
         if config.get('is_tarred', False):
