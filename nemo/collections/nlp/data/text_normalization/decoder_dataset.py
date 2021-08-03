700abaff06f9d2e0da70ef9aa72c1ff6370c96d4
Evelina <10428420+ekmb@users.noreply.github.com>
2021-08-03 12:03:14 -0700

    import fix (#2597)
    * import fix
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * remove unused import
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * example import fix
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/nlp/data/text_normalization/decoder_dataset.py
+++ nemo/collections/nlp/data/text_normalization/decoder_dataset.py
@@ -19,7 +19,7 @@ import random
 from tqdm import tqdm
 from transformers import PreTrainedTokenizerBase
 
-import nemo.collections.nlp.data.text_normalization.constants as constants
+from nemo.collections.nlp.data.text_normalization import constants
 from nemo.collections.nlp.data.text_normalization.utils import basic_tokenize, read_data_file
 from nemo.core.classes import Dataset
 from nemo.utils import logging
