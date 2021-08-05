d16bd97590e16e0090f236e7193fbc02c0454257
Micha Livne <mlivne@nvidia.com>
2021-08-05 13:10:05 -0400

    1. Debugging.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/modules/common/transformer/transformer_bottleneck.py
+++ nemo/collections/nlp/modules/common/transformer/transformer_bottleneck.py
@@ -18,7 +18,7 @@ from typing import Optional
 import torch
 from omegaconf.omegaconf import MISSING
 
-from nemo.core.neural_type import NeuralType, BoolType, MaskType
+from nemo.core.neural_types import NeuralType, BoolType, MaskType
 from nemo.collections.nlp.modules.common.transformer.transformer import (
     TransformerEncoderNM,
     TransformerDecoderNM,
