f9e1fbe72bb7d8951a26e2ec9c5c910116864288
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2021-08-06 14:05:01 -0700

    Update license headers
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- tools/nmt_grpc_service/asr_nmt_client.py
+++ tools/nmt_grpc_service/asr_nmt_client.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
