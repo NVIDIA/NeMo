7aa3884040559be9f873e82da93bbd5ce7decf68
sergiogcharles <sergiogcharles@gmail.com>
2021-08-11 10:39:53 -0700

    Clean up

--- nemo/core/classes/distillation.py
+++ nemo/core/classes/distillation.py
@@ -277,6 +277,8 @@ class DistillationModelPT(ModelPT):
         self.teacher.freeze()
         self.teacher.reset_distillation_registry()
         self.student.reset_distillation_registry()
+        
+        import pdb; pdb.set_trace()
 
         # Delegate train steps, dynamically replacing self with self.teacher to maintain model
         # level unawareness.
