fc217406eecfc3c01911989a22a7ed9ec8366d43
Jason <jasoli@nvidia.com>
2021-08-13 13:16:25 -0700

    style
    Signed-off-by: Jason <jasoli@nvidia.com>

--- tests/collections/tts/test_torch_tts.py
+++ tests/collections/tts/test_torch_tts.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
-import pytest
 import os
 
+import pytest
+import torch
 
 from nemo.collections.tts.torch.data import CharMelAudioDataset
 
