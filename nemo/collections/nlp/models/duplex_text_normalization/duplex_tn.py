006ea9dca6df343b2d498c8dd5b415a355bf7644
Yang Zhang <yangzhang@nvidia.com>
2021-08-16 16:16:39 -0700

    add semiotic class labels ground truth to error log
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/models/duplex_text_normalization/duplex_tn.py
+++ nemo/collections/nlp/models/duplex_text_normalization/duplex_tn.py
@@ -192,11 +192,11 @@ class DuplexTextNormalizationModel(nn.Module):
                         tn_error_ctx += 1
                     formatted_input_str = get_formatted_string(basic_tokenize(_input, lang=self.lang))
                     formatted_tag_pred_str = get_formatted_string(tag_pred)
-                    formatted_class_str = get_formatted_string(classes)
+                    class_str = " ".join(classes)
                     error_f.write(f'Original Input : {_input}\n')
                     error_f.write(f'Input          : {formatted_input_str}\n')
                     error_f.write(f'Predicted Tags : {formatted_tag_pred_str}\n')
-                    error_f.write(f'Ground Classes : {formatted_class_str}\n')
+                    error_f.write(f'Ground Classes : {class_str}\n')
                     error_f.write(f'Predicted Str  : {final_pred}\n')
                     error_f.write(f'Ground-Truth   : {target}\n')
                     error_f.write('\n')
