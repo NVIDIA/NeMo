29ad334e0bcda757a4c634b7217c6538ffed3aa0
Jason <jasoli@nvidia.com>
2021-08-16 13:03:34 -0700

    final
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/tts/models/__init__.py
+++ nemo/collections/tts/models/__init__.py
@@ -12,22 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.collections.tts.models.aligner import AlignerModel
-from nemo.collections.tts.models.degli import DegliModel
-from nemo.collections.tts.models.ed_mel2spec import EDMel2SpecModel
-from nemo.collections.tts.models.fastpitch import FastPitchModel
-from nemo.collections.tts.models.fastpitch_hifigan_e2e import FastPitchHifiGanE2EModel
-from nemo.collections.tts.models.fastspeech2 import FastSpeech2Model
-from nemo.collections.tts.models.fastspeech2_hifigan_e2e import FastSpeech2HifiGanE2EModel
-from nemo.collections.tts.models.glow_tts import GlowTTSModel
-from nemo.collections.tts.models.hifigan import HifiGanModel
-from nemo.collections.tts.models.melgan import MelGanModel
-from nemo.collections.tts.models.squeezewave import SqueezeWaveModel
-from nemo.collections.tts.models.tacotron2 import Tacotron2Model
-from nemo.collections.tts.models.talknet import TalkNetDursModel, TalkNetPitchModel, TalkNetSpectModel
-from nemo.collections.tts.models.two_stages import GriffinLimModel, MelPsuedoInverseModel, TwoStagesModel
-from nemo.collections.tts.models.uniglow import UniGlowModel
-from nemo.collections.tts.models.waveglow import WaveGlowModel
+try:
+    from nemo.collections.tts.models.aligner import AlignerModel
+    from nemo.collections.tts.models.degli import DegliModel
+    from nemo.collections.tts.models.ed_mel2spec import EDMel2SpecModel
+    from nemo.collections.tts.models.fastpitch import FastPitchModel
+    from nemo.collections.tts.models.fastpitch_hifigan_e2e import FastPitchHifiGanE2EModel
+    from nemo.collections.tts.models.fastspeech2 import FastSpeech2Model
+    from nemo.collections.tts.models.fastspeech2_hifigan_e2e import FastSpeech2HifiGanE2EModel
+    from nemo.collections.tts.models.glow_tts import GlowTTSModel
+    from nemo.collections.tts.models.hifigan import HifiGanModel
+    from nemo.collections.tts.models.melgan import MelGanModel
+    from nemo.collections.tts.models.squeezewave import SqueezeWaveModel
+    from nemo.collections.tts.models.tacotron2 import Tacotron2Model
+    from nemo.collections.tts.models.talknet import TalkNetDursModel, TalkNetPitchModel, TalkNetSpectModel
+    from nemo.collections.tts.models.two_stages import GriffinLimModel, MelPsuedoInverseModel, TwoStagesModel
+    from nemo.collections.tts.models.uniglow import UniGlowModel
+    from nemo.collections.tts.models.waveglow import WaveGlowModel
+except ModuleNotFoundError:
+    pass
 
 __all__ = [
     "GlowTTSModel",
