913b369cad5500289ac0fcec0c90713bfe1cd25a
PeganovAnton <peganoff2@mail.ru>
2021-08-17 22:29:01 +0300

    remove sys.path modification
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/punctuate_capitalize.py
+++ examples/nlp/token_classification/punctuate_capitalize.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-sys.path = ["/home/lab/NeMo"] + sys.path
-
 import argparse
 import json
 from pathlib import Path
