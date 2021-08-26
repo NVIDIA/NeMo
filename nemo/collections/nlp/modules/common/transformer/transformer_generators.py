2e12d80e8c50e9be9fd980a7a352b23458474a50
Micha Livne <mlivne@nvidia.com>
2021-08-25 18:48:55 -0700

    1. FIxed style.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/modules/common/transformer/transformer_generators.py
+++ nemo/collections/nlp/modules/common/transformer/transformer_generators.py
@@ -121,9 +121,9 @@ class GreedySequenceGenerator:
 
         # for encoder-decoder generation, maximum length of generated sequence
         # is min(max_sequence_length, src_len + max_delta_length)
-        if (encoder_hidden_states is not None):
+        if encoder_hidden_states is not None:
             batch_size, src_len, _ = encoder_hidden_states.size()
-            if (self.max_delta_len > 0):
+            if self.max_delta_len > 0:
                 max_seq_length = min(self.max_seq_length, src_len + self.max_delta_len)
             else:
                 max_seq_length = self.max_seq_length
@@ -512,9 +512,9 @@ class EnsembleBeamSearchSequenceGenerator:
 
         # for encoder-decoder generation, maximum length of generated sequence
         # is min(max_sequence_length, src_len + max_delta_length)
-        if (encoder_hidden_states is not None):
+        if encoder_hidden_states is not None:
             batch_size, src_len, _ = encoder_hidden_states.size()
-            if (self.max_delta_len > 0):
+            if self.max_delta_len > 0:
                 max_seq_length = min(self.max_seq_length, src_len + self.max_delta_len)
             else:
                 max_seq_length = self.max_seq_length
