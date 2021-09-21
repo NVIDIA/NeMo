f7daf4d45daed9836129dcdcb87271a082305b40
ericharper <complex451@gmail.com>
2021-09-21 15:46:13 -0600

    add predict script
    Signed-off-by: ericharper <complex451@gmail.com>

---
+++ examples/nlp/language_modeling/megatron_gpt_predict.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nemo.utils.app_state import AppState
+from pathlib import Path
+from nemo.collections.nlp.modules.common.megatron.megatron_utils import compute_model_parallel_rank
+from omegaconf.omegaconf import OmegaConf
+from pytorch_lightning import Trainer
+
+from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
+from nemo.collections.nlp.parts.nlp_overrides import (
+    NLPCheckpointConnector,
+    NLPDDPPlugin,
+    NLPNativeMixedPrecisionPlugin,
+    NLPPrecisionPlugin,
+    NLPSaveRestoreConnector,
+)
+from nemo.core.config import hydra_runner
+from nemo.utils import logging
+from nemo.utils.exp_manager import exp_manager
+
+
+@hydra_runner(config_path="conf", config_name="megatron_gpt_config")
+def main(cfg) -> None:
+    logging.info("\n\n************** Experiment configuration ***********")
+    logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
+
+    app_state = AppState()
+    app_state.model_parallel_size = cfg.model.tensor_model_parallel_size
+
+    if cfg.model.fp16:
+        trainer = Trainer(
+            plugins=[NLPDDPPlugin(num_nodes=cfg.trainer.num_nodes), NLPNativeMixedPrecisionPlugin()], **cfg.trainer
+        )
+    else:
+        trainer = Trainer(plugins=[NLPDDPPlugin(num_nodes=cfg.trainer.num_nodes), NLPPrecisionPlugin()], **cfg.trainer)
+
+    # TODO: possibly add model parallel size arg to exp_manager
+    # exp_manager(trainer, cfg.exp_manager)
+
+    # update resume from checkpoint found by exp_manager
+    resume_from_checkpoint = trainer.resume_from_checkpoint
+    if resume_from_checkpoint is not None:
+        mp_rank = compute_model_parallel_rank(trainer.local_rank, cfg.model.tensor_model_parallel_size)
+        resume_from_checkpoint = Path(resume_from_checkpoint)
+        resume_from_checkpoint = resume_from_checkpoint.parent.parent.joinpath(f'mp_rank_{mp_rank:02d}').joinpath(
+            resume_from_checkpoint.name
+        )
+        resume_from_checkpoint = str(resume_from_checkpoint)
+        logging.info(f'Resuming training from checkpoint: {resume_from_checkpoint}')
+
+    trainer.checkpoint_connector = NLPCheckpointConnector(trainer, resume_from_checkpoint=resume_from_checkpoint)
+
+    model = MegatronGPTModel.restore_from(
+        '/raid/nemo_experiments/gpt_debug/megatron_gpt/2021-09-20_12-27-43/checkpoints/megatron_gpt.nemo',
+        trainer=trainer,
+        save_restore_connector=NLPSaveRestoreConnector(),
+    )
+
+    trainer.test(model)
+
+
+if __name__ == '__main__':
+    main()
