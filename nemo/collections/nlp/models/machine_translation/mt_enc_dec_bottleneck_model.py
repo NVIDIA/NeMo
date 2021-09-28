3d1181305cc908d6edb0384c8b8377ce6245a059
Micha Livne <michalivne@users.noreply.github.com>
2021-09-28 12:08:24 -0400

    Nmt encoder decoder hidden size fix (#2856)
    * 1. Enabled encoder/decoder with different size in bottleneck architecture.
    2. Validating encoder/decoder with the same size in non-bottleneck parent class.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixed style.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixed typo.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Added hidden_size ot error message.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixed missing defaults.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixing CI tests to have same hidden_size.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Updated error message.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Updating Jenkins CI test.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Updating CI to hidden=48
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixed missing hidden_size when loading pre-trained huggingface model.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixed missing hidden_size in config for pre-trained models.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixed style.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Updated missng hidden_size in config.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Testing encoder and decoder objects' hidden_size instead of config to support pre-trained models.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Updated Jenkinsfile test values.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Fixed Jenkinsfile test values (NMT Megatron Model Parallel Size 2 Encoder)
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    * 1. Updating missing arguments for Jenkinsfile test.
    
    Signed-off-by: Micha Livne <mlivne@nvidia.com>
    
    Co-authored-by: Micha Livne <mlivne@nvidia.com>
    Co-authored-by: Sandeep Subramanian <sandeep.subramanian.1@umontreal.ca>

--- nemo/collections/nlp/models/machine_translation/mt_enc_dec_bottleneck_model.py
+++ nemo/collections/nlp/models/machine_translation/mt_enc_dec_bottleneck_model.py
@@ -102,6 +102,13 @@ class MTBottleneckModel(MTEncDecModel):
             # for probabilistic latent variable models we also need variance
             self.hidden2latent_logv = build_linear_or_identity(self.encoder.hidden_size, self.latent_size)
 
+    def _validate_encoder_decoder_hidden_size(self):
+        """
+        Validate encoder and decoder hidden sizes, and enforce same size.
+        We support here encoder/decoder with different hidden_size, so do nothing.
+        """
+        pass
+
     def eval_epoch_end(self, outputs, mode):
         # call parent for logging
         super().eval_epoch_end(outputs, mode)
