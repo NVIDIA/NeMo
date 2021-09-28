ac19a803d15d87883ca5308986213d9e09349b39
tbartley94 <tbartley@nvidia.com>
2021-09-28 13:50:05 -0700

    style change for verbalizer/ordinal.py
    Signed-off-by: tbartley94 <tbartley@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/fr/verbalizers/ordinal.py
+++ nemo_text_processing/inverse_text_normalization/fr/verbalizers/ordinal.py
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 from nemo_text_processing.inverse_text_normalization.fr.graph_utils import (
+    NEMO_DIGIT,
     NEMO_NOT_QUOTE,
     GraphFst,
     delete_space,
     insert_space,
-    NEMO_DIGIT
 )
 from nemo_text_processing.inverse_text_normalization.fr.utils import get_abs_path
 
@@ -67,9 +67,14 @@ class OrdinalFst(GraphFst):
         graph_roman_hundreds = pynini.string_file(get_abs_path("data/roman/hundreds_large.tsv")).invert()
         graph_roman_zero_digit = pynutil.delete("0")
 
-        graph_roman_hundreds = NEMO_DIGIT ** 3 @ (graph_roman_hundreds + pynini.union(graph_roman_ties, graph_roman_zero_digit)
-        + pynini.union(graph_roman_digits, graph_roman_zero_digit))
-        graph_roman_ties = NEMO_DIGIT ** 2 @ (graph_roman_ties + pynini.union(graph_roman_digits, graph_roman_zero_digit))
+        graph_roman_hundreds = NEMO_DIGIT ** 3 @ (
+            graph_roman_hundreds
+            + pynini.union(graph_roman_ties, graph_roman_zero_digit)
+            + pynini.union(graph_roman_digits, graph_roman_zero_digit)
+        )
+        graph_roman_ties = NEMO_DIGIT ** 2 @ (
+            graph_roman_ties + pynini.union(graph_roman_digits, graph_roman_zero_digit)
+        )
         graph_roman_digits = NEMO_DIGIT @ graph_roman_digits
 
         graph_roman_integers = graph_roman_hundreds | graph_roman_ties | graph_roman_digits
