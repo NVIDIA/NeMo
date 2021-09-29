f990b4e05032b13dcd22503683bc98503eb98e42
ericharper <complex451@gmail.com>
2021-09-29 09:37:04 -0600

    clean up MegatronModule
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/module.py
+++ nemo/collections/nlp/modules/common/megatron/module.py
@@ -36,10 +36,10 @@ class MegatronModule(torch.nn.Module):
         super(MegatronModule, self).__init__()
         self.share_word_embeddings = share_word_embeddings
 
-    def state_dict_for_save_checkpoint(self, destination=None, prefix='', keep_vars=False):
-        """Use this function to override the state dict for
-        saving checkpoints."""
-        return self.state_dict(destination, prefix, keep_vars)
+    # def state_dict_for_save_checkpoint(self, destination=None, prefix='', keep_vars=False):
+    #     """Use this function to override the state dict for
+    #     saving checkpoints."""
+    #     return self.state_dict(destination, prefix, keep_vars)
 
     def word_embeddings_weight(self):
         if mpu.is_pipeline_first_stage(ignore_virtual=True):
@@ -97,81 +97,3 @@ class MegatronModule(torch.nn.Module):
                 "this needs to be handled manually. If you are training "
                 "something is definitely wrong."
             )
-
-
-def conversion_helper(val, conversion):
-    """Apply conversion to val. Recursively apply conversion if `val`
-    #is a nested tuple/list structure."""
-    if not isinstance(val, (tuple, list)):
-        return conversion(val)
-    rtn = [conversion_helper(v, conversion) for v in val]
-    if isinstance(val, tuple):
-        rtn = tuple(rtn)
-    return rtn
-
-
-def fp32_to_float16(val, float16_convertor):
-    """Convert fp32 `val` to fp16/bf16"""
-
-    def half_conversion(val):
-        val_typecheck = val
-        if isinstance(val_typecheck, (Parameter, Variable)):
-            val_typecheck = val.data
-        if isinstance(val_typecheck, _FLOAT_TYPES):
-            val = float16_convertor(val)
-        return val
-
-    return conversion_helper(val, half_conversion)
-
-
-def float16_to_fp32(val):
-    """Convert fp16/bf16 `val` to fp32"""
-
-    def float_conversion(val):
-        val_typecheck = val
-        if isinstance(val_typecheck, (Parameter, Variable)):
-            val_typecheck = val.data
-        if isinstance(val_typecheck, (_BF16_TYPES, _HALF_TYPES)):
-            val = val.float()
-        return val
-
-    return conversion_helper(val, float_conversion)
-
-
-class Float16Module(MegatronModule):
-    def __init__(self, module, args):
-        super(Float16Module, self).__init__()
-
-        if args.fp16:
-            self.add_module('module', module.half())
-
-            def float16_convertor(val):
-                return val.half()
-
-        elif args.bf16:
-            self.add_module('module', module.bfloat16())
-
-            def float16_convertor(val):
-                return val.bfloat16()
-
-        else:
-            raise Exception('should not be here')
-
-        self.float16_convertor = float16_convertor
-
-    def forward(self, *inputs, **kwargs):
-        if mpu.is_pipeline_first_stage():
-            inputs = fp32_to_float16(inputs, self.float16_convertor)
-        outputs = self.module(*inputs, **kwargs)
-        if mpu.is_pipeline_last_stage():
-            outputs = float16_to_fp32(outputs)
-        return outputs
-
-    def state_dict(self, destination=None, prefix='', keep_vars=False):
-        return self.module.state_dict(destination, prefix, keep_vars)
-
-    def state_dict_for_save_checkpoint(self, destination=None, prefix='', keep_vars=False):
-        return self.module.state_dict_for_save_checkpoint(destination, prefix, keep_vars)
-
-    def load_state_dict(self, state_dict, strict=True):
-        self.module.load_state_dict(state_dict, strict=strict)
