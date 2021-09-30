7510799a0bd52dbe8c1298a2a8f01387d9f5aa20
Micha Livne <mlivne@nvidia.com>
2021-09-30 16:40:05 -0400

    1. Debugging.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/models/machine_translation/mt_enc_dec_bottleneck_model.py
+++ nemo/collections/nlp/models/machine_translation/mt_enc_dec_bottleneck_model.py
@@ -376,7 +376,7 @@ class MTBottleneckModel(MTEncDecModel):
         # TODO: remove me when timing callbacks work
         if self.timer.has("timing_data"):
             if "train_step_timing" in self.timer.timing_data:
-            tensorboard_logs["train_step_timing"] = self.timer.timing_data["train_step_timing"]
+                tensorboard_logs["train_step_timing"] = self.timer.timing_data["train_step_timing"]
 
         return {'loss': train_loss, 'log': tensorboard_logs}
 
