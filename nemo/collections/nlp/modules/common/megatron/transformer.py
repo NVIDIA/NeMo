5634a37ef8024e518c8d527d20e58adb6e63b550
ericharper <complex451@gmail.com>
2021-10-01 13:28:37 -0600

    typo
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/transformer.py
+++ nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -615,7 +615,7 @@ class ParallelTransformer(MegatronModule):
     ):
         super(ParallelTransformer, self).__init__()
 
-        assert not (fused_fp16 and fused_fp16), "both fused_fp16 and fused_bf16 flags cannot be True at the same time."
+        assert not (fused_fp16 and fused_bf16), "both fused_fp16 and fused_bf16 flags cannot be True at the same time."
 
         if kv_channels is None:
             assert (
