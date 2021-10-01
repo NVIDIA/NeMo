aecd38719ba7816b3a44d156af9fc14bfef97596
ericharper <complex451@gmail.com>
2021-10-01 10:11:14 -0600

    remove unused imports
    Signed-off-by: ericharper <complex451@gmail.com>

--- examples/nlp/language_modeling/megatron_gpt_eval.py
+++ examples/nlp/language_modeling/megatron_gpt_eval.py
@@ -13,17 +13,13 @@
 # limitations under the License.
 
 
-from pytorch_lightning.trainer import trainer
-import nemo.collections.nlp as nemo_nlp
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
 from pytorch_lightning.trainer.trainer import Trainer
-from nemo.utils import  logging
+from nemo.utils import logging
 from argparse import ArgumentParser
 
 import torch
 
-from nemo.collections.asr.metrics.wer import WER, word_error_rate
-from nemo.collections.asr.models import EncDecCTCModel
 from nemo.utils import logging
 
 
@@ -33,19 +29,33 @@ assert torch.cuda.is_available()
 def main():
     parser = ArgumentParser()
     parser.add_argument("--model_file", type=str, default="", required=True, help="Pass path to model's .nemo file")
-    parser.add_argument("--prompt", type=str, default="", required=True, help="Prompt for the model (a text to complete)")
-    parser.add_argument("--tokens_to_generate", type=int, default="64", required=True, help="How many tokens to add to prompt")
-    parser.add_argument("--stop_after_sentence", type=bool, default="True", required=False, help="True/False: whether to stop after full sentence has been generated.")
+    parser.add_argument(
+        "--prompt", type=str, default="", required=True, help="Prompt for the model (a text to complete)"
+    )
+    parser.add_argument(
+        "--tokens_to_generate", type=int, default="64", required=True, help="How many tokens to add to prompt"
+    )
+    parser.add_argument(
+        "--stop_after_sentence",
+        type=bool,
+        default="True",
+        required=False,
+        help="True/False: whether to stop after full sentence has been generated.",
+    )
 
     args = parser.parse_args()
     torch.set_grad_enabled(False)
 
-    # TODO: not sure why we require PTL for restoring
-    trainer = Trainer()    
+    # trainer required for restoring model parallel models
+    trainer = Trainer()
     model = MegatronGPTModel.restore_from(restore_path=args.model_file, trainer=trainer)
-    res = model.complete({"prompt": args.prompt,
-                          "tokens_to_generate": args.tokens_to_generate,
-                          "stop_after_sentence": args.stop_after_sentence})
+    res = model.complete(
+        {
+            "prompt": args.prompt,
+            "tokens_to_generate": args.tokens_to_generate,
+            "stop_after_sentence": args.stop_after_sentence,
+        }
+    )
     print("***************************")
     print(res['completion']['text'])
     print("***************************")
