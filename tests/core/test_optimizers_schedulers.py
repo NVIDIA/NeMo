20640c592af980550da45dd32abfa76f3d7389c1
ericharper <complex451@gmail.com>
2021-10-13 15:12:14 -0600

    test_get_optimizer on gpu
    Signed-off-by: ericharper <complex451@gmail.com>

--- tests/core/test_optimizers_schedulers.py
+++ tests/core/test_optimizers_schedulers.py
@@ -111,6 +111,8 @@ class TestOptimizersSchedulers:
     MIN_LR = 1e-3
     MAX_STEPS = 10
 
+    # fused_adam is looking for CUDA and this test is being run on CPU only tests
+    @pytest.mark.run_only_on('GPU')
     @pytest.mark.unit
     def test_get_optimizer(self):
         model = TempModel()
