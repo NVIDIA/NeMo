b387398b004370359fdc74774122c3f3080c2cd2
ericharper <complex451@gmail.com>
2021-10-18 10:13:50 -0600

    return None
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -388,7 +388,7 @@ class MegatronGPTModel(NLPModel):
         return response
 
     def list_available_models():
-        pass
+        return None
 
     def _vocab_size_with_padding(self, orig_vocab_size, make_vocab_size_divisible_by, tensor_model_parallel_size):
         """Pad vocab size so it is divisible by model parallel size and
