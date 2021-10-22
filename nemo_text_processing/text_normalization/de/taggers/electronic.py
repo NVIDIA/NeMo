73156b21187e6e37c9003f4673a332acce87b10c
Yang Zhang <yangzhang@nvidia.com>
2021-10-22 14:13:45 -0700

    style fix
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo_text_processing/text_normalization/de/taggers/electronic.py
+++ nemo_text_processing/text_normalization/de/taggers/electronic.py
@@ -46,72 +46,40 @@ class ElectronicFst(GraphFst):
     def __init__(self, deterministic: bool = True):
         super().__init__(name="electronic", kind="classify", deterministic=deterministic)
 
-        # tagger
-        accepted_symbols = []
-        with open(get_abs_path("data/electronic/symbols.tsv"), 'r') as f:
-            for line in f:
-                symbol, _ = line.split('\t')
-                accepted_symbols.append(pynini.accep(symbol))
-        username = (
-            pynutil.insert("username: \"")
-            + NEMO_ALPHA
-            + pynini.closure(NEMO_ALPHA | NEMO_DIGIT | pynini.union(*accepted_symbols))
-            + pynutil.insert("\"")
-            + pynini.cross('@', ' ')
-        )
-        domain_graph = (
-            NEMO_ALPHA
-            + (pynini.closure(NEMO_ALPHA | NEMO_DIGIT | pynini.accep('-') | pynini.accep('.')))
-            + (NEMO_ALPHA | NEMO_DIGIT)
-        )
-        domain_graph = pynutil.insert("domain: \"") + domain_graph + pynutil.insert("\"")
-        tagger_graph = (username + domain_graph).optimize()
+        def get_input_symbols(f):
+            accepted_symbols = []
+            with open(f, 'r') as f:
+                for line in f:
+                    symbol, _ = line.split('\t')
+                    accepted_symbols.append(pynini.accep(symbol))
+            return accepted_symbols
 
-        # verbalizer
-        graph_digit = pynini.string_file(get_abs_path("data/numbers/digits_nominative_case.tsv")).optimize()
+        accepted_symbols = get_input_symbols(get_abs_path("data/electronic/symbols.tsv"))
+        accepted_common_domains = get_input_symbols(get_abs_path("data/electronic/domain.tsv"))
+        accepted_symbols = NEMO_ALPHA + pynini.closure(NEMO_ALPHA | NEMO_DIGIT | pynini.union(*accepted_symbols))
         graph_symbols = pynini.string_file(get_abs_path("data/electronic/symbols.tsv")).optimize()
-        user_name = (
-            pynutil.delete("username:")
-            + delete_space
-            + pynutil.delete("\"")
-            + (
-                pynini.closure(
-                    pynutil.add_weight(graph_digit + insert_space, 1.09)
-                    | pynutil.add_weight(pynini.closure(graph_symbols + pynutil.insert(" ")), 1.09)
-                    | pynutil.add_weight(NEMO_NOT_QUOTE + insert_space, 1.1)
-                )
-            )
-            + pynutil.delete("\"")
+
+        username = pynutil.insert("username: \"") + accepted_symbols + pynutil.insert("\"") + pynini.cross('@', ' ')
+        domain_graph = accepted_symbols + pynini.accep('.') + accepted_symbols
+        domain_graph = pynutil.insert("domain: \"") + domain_graph + pynutil.insert("\"")
+        domain_common_graph = (
+            pynutil.insert("domain: \"")
+            + accepted_symbols
+            + pynini.union(*accepted_common_domains)
+            + pynutil.insert("\"")
         )
 
-        domain_default = (
-            pynini.closure(NEMO_NOT_QUOTE + insert_space)
-            + pynini.cross(".", "точка ")
-            + NEMO_NOT_QUOTE
-            + pynini.closure(insert_space + NEMO_NOT_QUOTE)
+        protocol_start = pynini.accep("https://") | pynini.accep("http://")
+        protocol_symbols = pynini.closure(
+            (NEMO_ALPHA | pynutil.add_weight(graph_symbols | pynini.cross(":", "colon"), -0.1)) + pynutil.insert(" ")
         )
+        protocol_end = pynini.accep("www.")
+        protocol = protocol_start | protocol_end | (protocol_start + protocol_end)
+        protocol = pynini.compose(protocol, protocol_symbols)
+        protocol = pynutil.insert("protocol: \"") + protocol + pynutil.insert("\"")
+        graph = username + domain_graph
+        graph |= domain_common_graph
+        graph |= protocol + pynutil.insert(" ") + domain_graph
 
-        server_default = (
-            pynini.closure((graph_digit | NEMO_ALPHA) + insert_space, 1)
-            + pynini.closure(graph_symbols + insert_space)
-            + pynini.closure((graph_digit | NEMO_ALPHA) + insert_space, 1)
-        )
-        server_common = pynini.string_file(get_abs_path("data/electronic/server_name.tsv")) + insert_space
-        domain_common = pynini.cross(".", "точка ") + pynini.string_file(get_abs_path("data/electronic/domain.tsv"))
-        domain = (
-            pynutil.delete("domain:")
-            + delete_space
-            + pynutil.delete("\"")
-            + (pynutil.add_weight(server_common, 1.09) | pynutil.add_weight(server_default, 1.1))
-            + (pynutil.add_weight(domain_common, 1.09) | pynutil.add_weight(domain_default, 1.1))
-            + delete_space
-            + pynutil.delete("\"")
-        )
-
-        graph = user_name + delete_space + pynutil.insert("собака ") + delete_space + domain + delete_space
-        # replace all latin letters with their Ru verbalization
-        verbalizer_graph = (graph.optimize() @ (pynini.closure(TO_CYRILLIC | RU_ALPHA | pynini.accep(" ")))).optimize()
-        verbalizer_graph = verbalizer_graph.optimize()
-
-        self.final_graph = (tagger_graph @ verbalizer_graph).optimize()
-        self.fst = self.add_tokens(pynutil.insert("username: \"") + self.final_graph + pynutil.insert("\"")).optimize()
+        final_graph = self.add_tokens(graph)
+        self.fst = final_graph.optimize()
