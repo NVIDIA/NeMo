dca8cc9f26ee70794d6066a2012c6b46552f3ce3
Jason <jasoli@nvidia.com>
2021-10-22 18:02:30 -0400

    Always save last checkpoint on train end even if folder does not exist (#2976)
    * add fix for no checkpoint folder when training ends
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * update
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * fix test
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * fixes
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * typo
    
    Signed-off-by: Jason <jasoli@nvidia.com>
    
    * change check
    
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/utils/exp_manager.py
+++ nemo/utils/exp_manager.py
@@ -793,7 +793,6 @@ class NeMoModelCheckpoint(ModelCheckpoint):
                 )
             else:
                 trainer.checkpoint_connector.restore(self.best_model_path)
-
         pl_module.save_to(save_path=os.path.join(self.dirpath, self.prefix + self.postfix))
 
     def _del_model_without_trainer(self, filepath: str) -> None:
