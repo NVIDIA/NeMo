e5dcf71af3041a9ceecbbc6c8466f16c3b2c8e2f
Somshubra Majumdar <titu1994@gmail.com>
2021-11-01 16:50:25 -0700

    Remove logs
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- scripts/tokenizers/process_asr_text_tokenizer.py
+++ scripts/tokenizers/process_asr_text_tokenizer.py
@@ -81,20 +81,6 @@
 #
 #   --log: Whether the script should display log messages
 
-"""
-
-python process_asr_text_tokenizer.py \
-    --manifest=/media/smajumdar/data/Datasets/ASR_SET_LM/ASR_SET_2.0/tarred_audio_manifest.json \
-    --data_root="/home/smajumdar/PycharmProjects/nemo-eval/nemo_beta_eval/asrset/manifests/nemo/asrset_2.0/tokenizers/temp/" \
-    --tokenizer=spe \
-    --no_lower_case \
-    --spe_eos \
-    --spe_bos \
-    --log \
-    --spe_type=bpe \
-    --vocab_size=1024
-"""
-
 import argparse
 import json
 import logging
