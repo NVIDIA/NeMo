b52790dddc26730bf1033d641528fe428a8cf9ba
PeganovAnton <peganoff2@mail.ru>
2021-11-04 14:45:38 +0300

    Fix processor setting for nmt inference
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/nlp/models/machine_translation/mt_enc_dec_model.py
+++ nemo/collections/nlp/models/machine_translation/mt_enc_dec_model.py
@@ -855,8 +855,7 @@ class MTEncDecModel(EncDecNLPModel):
             list of translated strings
         """
         # __TODO__: This will reset both source and target processors even if you want to reset just one.
-        if source_lang is not None or target_lang is not None:
-            self.setup_pre_and_post_processing_utils(source_lang, target_lang)
+        self.setup_pre_and_post_processing_utils(source_lang, target_lang)
 
         mode = self.training
         prepend_ids = []
