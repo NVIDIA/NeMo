b43f521824cae2e8fd8d9f6151e5603d7a93bc88
PeganovAnton <peganoff2@mail.ru>
2021-11-05 11:56:03 +0300

    Add debug script for more tokenizer in creating tarred dataset
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/data/debug_create_tarred_dataset.sh
+++ examples/nlp/token_classification/data/debug_create_tarred_dataset.sh
@@ -3,4 +3,6 @@ python create_punctuation_capitalization_tarred_dataset.py \
   --labels /media/apeganov/DATA/wiki_unsplit_48_65_filtered_characters/very_small_wiki/train/bert_labels.txt \
   --output_dir /media/apeganov/DATA/wiki_unsplit_48_65_filtered_characters/very_small_wiki/train_tarred \
   --lines_per_dataset_fragment 100000 \
-  --num_batches_per_tarfile 50
+  --num_batches_per_tarfile 50 \
+  --tokenizer_name yttm \
+  --tokenizer_model ~/NeMo/examples/speech_translation/punct_prepared_datasets/prepared_punctuation_data_min_punctuation_29.10.2021_2.43/input.BPE.8192.model
