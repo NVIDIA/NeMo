1f7a5fb1b9a27ac8f54c58c4417f868d669eb0a1
Madhukar Korupolu <madhukar@nvidia.com>
2021-11-05 05:46:41 -0700

    Pretrain gpt script changes
    Signed-off-by: Madhukar Korupolu <madhukar@nvidia.com>

--- train_scripts/pretrain_gpt.py
+++ train_scripts/pretrain_gpt.py
@@ -19,19 +19,22 @@ def main(cfg):
             args[index] = "{}={}".format(k, v.replace("'", "\\'"))
 
     train_args = [x.replace("training.", "") for x in args if x[:9] == "training."]
-    train_args = [x.replace("None", "null") for x in train_args if "run." not in x and "slurm." not in x]
+    train_args = [x.replace("None", "null") for x in train_args if "run." not in x and "slurm." not in x and "bcp." not in x]
     hydra_train_args = " ".join(train_args)
 
     bignlp_path = cfg["bignlp_path"]
     training_config = cfg["training_config"]
+    code_dir = cfg["code_dir"]
     code_path = (
-        "/opt/bignlp/NeMo/examples/nlp/language_modeling/megatron_gpt_pretraining.py"
+        f"{code_dir}/examples/nlp/language_modeling/megatron_gpt_pretraining.py"
     )
     training_config_path = os.path.join(bignlp_path, "conf/training")
     gpu_mapping = "CUDA_VISIBLE_DEVICES={}".format(re.sub('[\[\] ]', '', str(rank2gpu)))
     core_mapping = f"exec numactl --physcpubind={dgxa100_gpu2core[rank2gpu[int(os.environ.get('LOCAL_RANK'))]]} --membind={dgxa100_gpu2mem[rank2gpu[int(os.environ.get('LOCAL_RANK'))]]} -- "
     flags = f"--config-path={training_config_path} --config-name={training_config} "
-    cmd = f'cd /opt/bignlp/NeMo; git rev-parse HEAD; cd /opt/bignlp/NeMo/nemo/collections/nlp/data/language_modeling/megatron; make; export PYTHONPATH="/opt/bignlp/NeMo/.:$PYTHONPATH"; export TRANSFORMERS_CACHE="/temp_root/.cache/"; {gpu_mapping} {core_mapping} python3 {code_path} {hydra_train_args} {flags}'
+    # cmd = f'cd /opt/bignlp/NeMo; git rev-parse HEAD; cd /opt/bignlp/NeMo/nemo/collections/nlp/data/language_modeling/megatron; make; export PYTHONPATH="/opt/bignlp/NeMo/.:$PYTHONPATH"; export TRANSFORMERS_CACHE="/temp_root/.cache/"; {gpu_mapping} {core_mapping} python3 {code_path} {hydra_train_args} {flags}'
+    cmd = f'cd {code_dir}; git rev-parse HEAD; cd {code_dir}/nemo/collections/nlp/data/language_modeling/megatron; make; export PYTHONPATH="{code_dir}/.:$PYTHONPATH"; export TRANSFORMERS_CACHE="/temp_root/.cache/"; cp /workspace-common/megatron_gpt_pretraining.py {code_path}; python3 {code_path} +cluster_type=BCP {hydra_train_args} {flags}'
+    print(f" Command is: {cmd}")
     os.system(f"{cmd}")
 
 
