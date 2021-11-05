613ba8eef1afb70944dbdcd914a9efc26d023d08
PeganovAnton <peganoff2@mail.ru>
2021-11-05 12:19:01 +0300

    Set hf tokenizer in debug script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/data/debug_create_tarred_dataset.sh
+++ examples/nlp/token_classification/data/debug_create_tarred_dataset.sh
@@ -4,5 +4,4 @@ python create_punctuation_capitalization_tarred_dataset.py \
   --output_dir /media/apeganov/DATA/wiki_unsplit_48_65_filtered_characters/very_small_wiki_debug/train_tarred \
   --lines_per_dataset_fragment 100000 \
   --num_batches_per_tarfile 50 \
-  --tokenizer_name yttm \
-  --tokenizer_model ~/NeMo/examples/speech_translation/punct_prepared_datasets/prepared_punctuation_data_min_punctuation_29.10.2021_2.43/input.BPE.8192.model
+  --tokenizer_name bert-base-uncased
