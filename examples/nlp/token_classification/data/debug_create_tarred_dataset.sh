03e6c042082cb03630de69465390102e90a81cab
PeganovAnton <peganoff2@mail.ru>
2021-11-05 11:56:48 +0300

    Update output path in debug script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/data/debug_create_tarred_dataset.sh
+++ examples/nlp/token_classification/data/debug_create_tarred_dataset.sh
@@ -1,7 +1,7 @@
 python create_punctuation_capitalization_tarred_dataset.py \
   --text /media/apeganov/DATA/wiki_unsplit_48_65_filtered_characters/very_small_wiki/train/input.txt \
   --labels /media/apeganov/DATA/wiki_unsplit_48_65_filtered_characters/very_small_wiki/train/bert_labels.txt \
-  --output_dir /media/apeganov/DATA/wiki_unsplit_48_65_filtered_characters/very_small_wiki/train_tarred \
+  --output_dir /media/apeganov/DATA/wiki_unsplit_48_65_filtered_characters/very_small_wiki_debug/train_tarred \
   --lines_per_dataset_fragment 100000 \
   --num_batches_per_tarfile 50 \
   --tokenizer_name yttm \
