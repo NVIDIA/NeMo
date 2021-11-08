b73ff624884131e71ce19fcef361ac581868fb18
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2021-11-08 08:38:46 -0800

    Training script update for PTL 1.5
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/language_modeling/megatron_t5_pretraining.py
+++ examples/nlp/language_modeling/megatron_t5_pretraining.py
@@ -16,20 +16,23 @@ from pathlib import Path
 
 from omegaconf.omegaconf import OmegaConf
 from pytorch_lightning import Trainer
+from pytorch_lightning.plugins.environments.torchelastic_environment import TorchElasticEnvironment
+from pytorch_lightning.callbacks.timer import Timer
+from pytorch_lightning.trainer.connectors.checkpoint_connector import CheckpointConnector
+from pytorch_lightning.plugins.precision.native_amp import NativeMixedPrecisionPlugin
 
 from nemo.collections.nlp.models.language_modeling.megatron_t5_model import MegatronT5Model
 from nemo.collections.nlp.modules.common.megatron.megatron_utils import compute_model_parallel_rank
 from nemo.collections.nlp.parts.nlp_overrides import (
-    NLPCheckpointConnector,
     NLPDDPPlugin,
     NLPNativeBfloat16PrecisionPlugin,
     NLPNativeMixedPrecisionPlugin,
     NLPPrecisionPlugin,
-    NLPSaveRestoreConnector,
+    GradScaler
 )
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
-from nemo.utils.exp_manager import exp_manager
+from nemo.utils.exp_manager import StatelessTimer, exp_manager
 
 
 @hydra_runner(config_path="conf", config_name="megatron_t5_config")
@@ -37,26 +40,19 @@ def main(cfg) -> None:
     logging.info("\n\n************** Experiment configuration ***********")
     logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
 
+    plugins = [NLPDDPPlugin(num_nodes=cfg.trainer.num_nodes)]
     if cfg.trainer.precision == 16:
-        trainer = Trainer(
-            plugins=[
-                NLPDDPPlugin(num_nodes=cfg.trainer.num_nodes),
-                NLPNativeMixedPrecisionPlugin(
-                    init_scale=cfg.model.get('native_amp_init_scale', 2 ** 32),
-                    growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
-                ),
-            ],
-            **cfg.trainer,
+        scaler = GradScaler(
+            init_scale=cfg.model.get('native_amp_init_scale', 2 ** 32),
+            growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
         )
-    elif cfg.trainer.precision == 'bf16':
-        trainer = Trainer(
-            plugins=[NLPDDPPlugin(num_nodes=cfg.trainer.num_nodes), NLPNativeBfloat16PrecisionPlugin(),],
-            **cfg.trainer,
-        )
-    else:
-        trainer = Trainer(plugins=[NLPDDPPlugin(num_nodes=cfg.trainer.num_nodes), NLPPrecisionPlugin()], **cfg.trainer)
+        plugins.append(NativeMixedPrecisionPlugin(precision=16, device='cuda', scaler=scaler))
 
-    # TODO: possibly add model parallel size arg to exp_manager
+    if cfg.get('cluster_type', None) == 'BCP':
+        plugins.append(TorchElasticEnvironment())
+
+    trainer = Trainer(plugins=plugins, **cfg.trainer)
+
     exp_manager(trainer, cfg.exp_manager)
 
     # update resume from checkpoint found by exp_manager
@@ -70,11 +66,13 @@ def main(cfg) -> None:
         resume_from_checkpoint = str(resume_from_checkpoint)
         logging.info(f'Resuming training from checkpoint: {resume_from_checkpoint}')
 
-    trainer.checkpoint_connector = NLPCheckpointConnector(trainer, resume_from_checkpoint=resume_from_checkpoint)
+    trainer.checkpoint_connector = CheckpointConnector(trainer, resume_from_checkpoint=resume_from_checkpoint)
+    # Override timer callback to a stateless one
+    for idx, callback in enumerate(trainer.callbacks):
+        if isinstance(callback, Timer):
+            trainer.callbacks[idx] = StatelessTimer(cfg.trainer.max_time,)
 
     model = MegatronT5Model(cfg.model, trainer)
-
-    model._save_restore_connector = NLPSaveRestoreConnector()
 
     trainer.fit(model)
 
