8d29c30df15e00b08a7da9b3e5f260999ec6e8a7
yuya <yuya>
2021-11-09 14:59:20 -0800

    add conversion script and config

--- conversion_scripts/convert.py
+++ conversion_scripts/convert.py
@@ -1,27 +1,27 @@
 import sys
 import os
 import subprocess
+import glob
 
 import hydra
 import omegaconf
 
 
 def create_slurm_file(
-    new_script_path,
-    eval_cmd1,
-    eval_cmd2,
-    job_name,
-    flags="",
-    dependency=None,
-    time="04:00:00",
-    exclusive=True,
-    mem=0,
-    overcommit=True,
-    nodes=1,
-    ntasks_per_node=8,
-    gpus_per_task=1,
-    partition="batch",
-    account=None,
+        new_script_path,
+        convert_cmd,
+        job_name,
+        flags="",
+        dependency=None,
+        time="04:00:00",
+        exclusive=True,
+        mem=0,
+        overcommit=True,
+        nodes=1,
+        ntasks_per_node=8,
+        gpus_per_task=1,
+        partition="batch",
+        account=None,
 ):
     with open(new_script_path, "w") as f:
         f.writelines("#!/bin/bash\n")
@@ -43,8 +43,7 @@ def create_slurm_file(
         if overcommit:
             f.writelines("#SBATCH --overcommit\n")
         f.writelines(f"#SBATCH --time={time}\n\n")
-        f.writelines(f'srun {flags} --ntasks=1 sh -c "{eval_cmd1}"\n\n')
-        f.writelines(f'srun {flags} --ntasks={ntasks_per_node} sh -c "{eval_cmd2}"\n\n')
+        f.writelines(f'srun {flags} --ntasks={ntasks_per_node} sh -c "{convert_cmd}"\n\n')
         f.writelines("set +x\n")
 
 
@@ -53,12 +52,12 @@ def run_evaluation(cfg, dependency=None):
     bignlp_path = cfg.get("bignlp_path")
     container = cfg.get("container")
     container_mounts = cfg.get("container_mounts")
-    eval_cfg = cfg.get("evaluation")
-    run_cfg = eval_cfg.get("run")
-    model_cfg = eval_cfg.get("model")
+    convert_cfg = cfg.get("conversion")
+    run_cfg = convert_cfg.get("run")
+    model_cfg = convert_cfg.get("model")
 
     # SLURM parameters
-    slurm_cfg = eval_cfg.get("slurm")
+    slurm_cfg = convert_cfg.get("slurm")
     partition = slurm_cfg.get("partition")
     account = slurm_cfg.get("account")
     time_limit = slurm_cfg.get("time_limit")
@@ -73,20 +72,29 @@ def run_evaluation(cfg, dependency=None):
     job_name = slurm_cfg.get("job_name")
 
     # Model parameters
-    model_type = model_cfg.get("type")
-    checkpoint = model_cfg.get("checkpoint_path")
+    checkpoint_folder = model_cfg.get("checkpoint_folder")
+    checkpoint_name = model_cfg.get("checkpoint_name")
     tensor_model_parallel_size = model_cfg.get("tensor_model_parallel_size")
-    batch_size = model_cfg.get("eval_batch_size")
-    vocab_file = model_cfg.get("vocab_file")
-    merge_file = model_cfg.get("merge_file")
+
+    if tensor_model_parallel_size > 1:
+        checkpoint = os.path.join(checkpoint_folder, "mp_rank_00", checkpoint_name)
+    else:
+        checkpoint = os.path.join(checkpoint_folder, checkpoint_name)
+    checkpoint_list = glob.glob(checkpoint)
+    if len(checkpoint_list) > 1:
+        raise ValueError("Too many checkpoints fit the checkpoint name pattern in convert.yaml.")
+    if len(checkpoint_list) == 0:
+        raise ValueError("No checkpoint found with the checkpoint name pattern in convert.yaml.")
+    checkpoint_name = os.path.basename(checkpoint_list[0])
 
     # Run parameters
     name = run_cfg.get("name")
-    tasks = run_cfg.get("tasks")
+    nemo_file_name = run_cfg.get("nemo_file_name")
     log_dir = os.path.join(bignlp_path, run_cfg.get("output_path"), name)
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    
+    nemo_file_path = os.path.join(log_dir, nemo_file_name)
+
     # Process container-mounts.
     mounts_str = f"{bignlp_path}:{bignlp_path}"
     if container_mounts is not None:
@@ -102,25 +110,18 @@ def run_evaluation(cfg, dependency=None):
         f"-o {log_dir}/{name}-%j.log "
         f"-e {log_dir}/{name}-%j.error "
     )
-    cache_dir = os.path.join(bignlp_path, "eval_scripts/data_cache")
-    code_path1 = os.path.join(bignlp_path, "eval_scripts/eval_harness/download.py")
-    eval_cmd1 = f"python {code_path1} --tasks {tasks} --cache_dir {cache_dir} " \
 
-    new_script_path = os.path.join(bignlp_path, "eval_scripts/eval_script.sh")
-    code_path2 = os.path.join(bignlp_path, "eval_scripts/eval_harness/evaluate.py")
-    eval_cmd2 = f"python -u {code_path2} " \
-                f"--name {name} " \
-                f"--model {model_type} " \
-                f"--tasks {tasks} " \
-                f"--cache_dir {cache_dir} " \
-                f"--batch_size {batch_size} " \
-                f"--output_path {log_dir} " \
-                f"--model_args nemo_model={checkpoint},tensor_model_parallel_size={tensor_model_parallel_size},vocab_file={vocab_file},merges_file={merge_file} "
+    new_script_path = os.path.join(bignlp_path, "conversion_scripts/convert_script.sh")
+    code_path = "/opt/bignlp/NeMo/examples/nlp/language_modeling/megatron_gpt_ckpt_to_nemo.py"
+    convert_cmd = f"python -u {code_path} " \
+                  f"--checkpoint_folder {checkpoint_folder} " \
+                  f"--checkpoint_name {checkpoint_name} " \
+                  f"--nemo_file_path {nemo_file_path} " \
+                  f"--tensor_model_parallel_size {tensor_model_parallel_size} "
 
     create_slurm_file(
         new_script_path=new_script_path,
-        eval_cmd1=eval_cmd1,
-        eval_cmd2=eval_cmd2,
+        convert_cmd=convert_cmd,
         job_name=job_name,
         flags=flags,
         dependency=dependency,
