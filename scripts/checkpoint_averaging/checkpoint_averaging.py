90c528f23be0f21e04e96554ec3813dbfc6eba45
Micha Livne <mlivne@nvidia.com>
2021-12-02 15:34:49 +0200

    1. Debugging.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- scripts/checkpoint_averaging/checkpoint_averaging.py
+++ scripts/checkpoint_averaging/checkpoint_averaging.py
@@ -46,6 +46,7 @@ def main():
     )
     parser.add_argument(
         '--import_fname_list',
+        dest='import_fname_list',
         type=str,
         nargs='+',
         help='A list of Python file names to "from FILE import *" (Needed when some classes were defined in __main__ of a script)',
