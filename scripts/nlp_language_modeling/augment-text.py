9176ebdd01bce8ca8294d74032158625e60a3b8f
Micha Livne <mlivne@nvidia.com>
2021-12-06 11:28:09 -0800

    1. Fixed style.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- scripts/nlp_language_modeling/augment-text.py
+++ scripts/nlp_language_modeling/augment-text.py
@@ -6,30 +6,31 @@ Support letetrs swap/drop, and AugLy <https://github.com/facebookresearch/AugLy>
 
 import os
 import sys
-from tqdm import tqdm
 from argparse import ArgumentParser
+
 import numpy as np
+from tqdm import tqdm
 
 try:
     import augly.text as txtaugs
 except:
     txtaugs = None
 
-#=============================================================================#
+# =============================================================================#
 # Augmentations
-#=============================================================================#
+# =============================================================================#
 
 
 def aug_switch_near_letters(word, p=0.0):
     """
     Switch two consecutive letters in a word
     """
-    if (np.random.rand() < p):
+    if np.random.rand() < p:
         if len(word) > 1:
-            i = np.random.randint(len(word)-1)
-            j = i+1
+            i = np.random.randint(len(word) - 1)
+            j = i + 1
 
-            word = (word[:i] + word[j] + word[i] + word[j+1:])
+            word = word[:i] + word[j] + word[i] + word[j + 1 :]
 
     return word
 
@@ -42,28 +43,31 @@ def aug_drop_letter(word, p=0.0):
         if len(word) > 1:
             i = np.random.randint(len(word))
 
-            word = (word[:i] + word[i+1:])
+            word = word[:i] + word[i + 1 :]
 
     return word
 
-#=============================================================================#
+
+# =============================================================================#
 # Main
-#=============================================================================#
+# =============================================================================#
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--source", type=str, required=True,
-                        help="Input file")
-    parser.add_argument("--target", type=str, default="",
-                        help="Output file")
-    parser.add_argument("--switch_near_letters_order", type=float, default=0.0,
-                        help="Rate of switching two consecutive letters in a word")
-    parser.add_argument("--drop_letter", type=float, default=0.0,
-                        help="Rate of dropping a letter in a word")
+    parser.add_argument("--source", type=str, required=True, help="Input file")
+    parser.add_argument("--target", type=str, default="", help="Output file")
+    parser.add_argument(
+        "--switch_near_letters_order",
+        type=float,
+        default=0.0,
+        help="Rate of switching two consecutive letters in a word",
+    )
+    parser.add_argument("--drop_letter", type=float, default=0.0, help="Rate of dropping a letter in a word")
     # AugLy
-    parser.add_argument("--augly_p", type=float, default=0.0,
-                        help="Probability of augly to apply a transformation (per word)")
+    parser.add_argument(
+        "--augly_p", type=float, default=0.0, help="Probability of augly to apply a transformation (per word)"
+    )
 
     args = parser.parse_args()
 
@@ -73,13 +77,9 @@ def main():
     # collect ops
     ops = []
     if args.switch_near_letters_order > 0:
-        ops.append(lambda w: aug_switch_near_letters(
-            w,
-            p=args.switch_near_letters_order))
+        ops.append(lambda w: aug_switch_near_letters(w, p=args.switch_near_letters_order))
     if args.drop_letter > 0:
-        ops.append(lambda w: aug_drop_letter(
-            w,
-            p=args.drop_letter))
+        ops.append(lambda w: aug_drop_letter(w, p=args.drop_letter))
 
     # apply ops
     with open(args.target, 'w') as target_f:
@@ -91,16 +91,12 @@ def main():
             # clean double spaces from dropped words
             line = " ".join(words).replace("  ", " ")
 
-            if (args.augly_p > 0):
-                line = txtaugs.simulate_typos([line],
-                    aug_char_p=args.augly_p,
-                    aug_word_p=args.augly_p,
-                    aug_char_min=0,
-                    aug_word_min=0,
+            if args.augly_p > 0:
+                line = txtaugs.simulate_typos(
+                    [line], aug_char_p=args.augly_p, aug_word_p=args.augly_p, aug_char_min=0, aug_word_min=0,
                 )[0]
 
             target_f.write(line + "\n")
-
 
 
 if __name__ == '__main__':
