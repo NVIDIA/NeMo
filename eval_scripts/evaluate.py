0ec802d397df9da0d8a9e748f0c6b86241f75360
Madhukar Korupolu <madhukar@nvidia.com>
2021-12-12 15:49:52 -0800

    Conversion and eval harness changes for BCP
    Signed-off-by: Madhukar Korupolu <madhukar@nvidia.com>

--- eval_scripts/evaluate.py
+++ eval_scripts/evaluate.py
@@ -5,72 +5,53 @@ import subprocess
 import hydra
 import omegaconf
 
+def create_bcp_submit_cmd(
+    job_name,
+    container,
+    workspace_common,
+    workspace_scripts,
+    bignlp_path,
+    bcp_script,
+    instance,
+    num_nodes,
+    array_type="PYTORCH",
+    total_runtime="10H"
+):
+    base_cmd = f"cd {bignlp_path}; {bcp_script}"
+    if (num_nodes == 1):
+        num_nodes = 2  # bcprun needs at least 2 nodes
+    submit_cmd = f"ngc batch run --name \"{job_name}\" --image \"{container}\" \
+    --commandline \"{base_cmd}\" --workspace {workspace_common}:/workspace-common \
+    --workspace {workspace_scripts}:/workspace-scripts --result /result \
+    --preempt RUNONCE --instance {instance} --replicas {num_nodes} \
+    --array-type {array_type} --total-runtime {total_runtime}"
+    
+    return submit_cmd        
 
-def create_slurm_file(
+def create_bcp_file(
+    bignlp_path,
     new_script_path,
     eval_cmd1,
     eval_cmd2,
-    job_name,
-    flags="",
-    dependency=None,
-    time="04:00:00",
-    exclusive=True,
-    mem=0,
-    overcommit=True,
-    nodes=1,
-    ntasks_per_node=8,
-    gpus_per_task=1,
-    partition="batch",
-    account=None,
-):
+    num_nodes,
+    ntasks_per_node,
+    log_file,
+    err_file
+):        
     with open(new_script_path, "w") as f:
-        f.writelines("#!/bin/bash\n")
-        f.writelines(f"#SBATCH --nodes={nodes}\n")
-        f.writelines(f"#SBATCH --ntasks-per-node={ntasks_per_node}\n")
-        if gpus_per_task is not None:
-            f.writelines(f"#SBATCH --gpus-per-task={gpus_per_task}\n")
-        if dependency is not None:
-            if dependency != "singleton":
-                dependency = f"afterany:{dependency}"
-            f.writelines(f"#SBATCH --dependency={dependency}\n")
-        f.writelines(f"#SBATCH -p {partition}\n")
-        if account is not None:
-            f.writelines(f"#SBATCH -A {account}\n")
-        f.writelines(f"#SBATCH --job-name={job_name}\n")
-        f.writelines(f"#SBATCH --mem={mem}\n")
-        if exclusive:
-            f.writelines("#SBATCH --exclusive\n")
-        if overcommit:
-            f.writelines("#SBATCH --overcommit\n")
-        f.writelines(f"#SBATCH --time={time}\n\n")
-        f.writelines(f'srun {flags} --ntasks=1 sh -c "{eval_cmd1}"\n\n')
-        f.writelines(f'srun {flags} --ntasks={ntasks_per_node} sh -c "{eval_cmd2}"\n\n')
-        f.writelines("set +x\n")
-
-
+        # Replace {bignlp_path}/bcprun2 below by bcprun once new bcprun is deployed
+        f.writelines(f'{bignlp_path}/bcprun2 -n {num_nodes} --npernode 1 -c "{eval_cmd1}" >> {log_file} 2>>{err_file} \n\n')
+        f.writelines(f'{bignlp_path}/bcprun2 -n {num-nodes} --npernode {ntasks_per_node} -c "{eval_cmd2}" >> {log_file} 2>>{err_file} \n\n')
+        f.writelines("set +x\n") 
+    os.chmod(new_script_path, 0o755)
+    
 def run_evaluation(cfg, dependency=None):
     # Read config
     bignlp_path = cfg.get("bignlp_path")
     container = cfg.get("container")
-    container_mounts = cfg.get("container_mounts")
     eval_cfg = cfg.get("evaluation")
     run_cfg = eval_cfg.get("run")
     model_cfg = eval_cfg.get("model")
-
-    # SLURM parameters
-    slurm_cfg = eval_cfg.get("slurm")
-    partition = slurm_cfg.get("partition")
-    account = slurm_cfg.get("account")
-    time_limit = slurm_cfg.get("time_limit")
-    nodes = slurm_cfg.get("nodes")
-    exclusive = slurm_cfg.get("exclusive")
-    mem = slurm_cfg.get("mem")
-    overcommit = slurm_cfg.get("overcommit")
-    ntasks_per_node = slurm_cfg.get("ntasks_per_node")
-    gpus_per_task = slurm_cfg.get("gpus_per_task")
-    if dependency is None:
-        dependency = slurm_cfg.get("dependency")
-    job_name = slurm_cfg.get("job_name")
 
     # Model parameters
     model_type = model_cfg.get("type")
@@ -80,33 +61,28 @@ def run_evaluation(cfg, dependency=None):
     vocab_file = model_cfg.get("vocab_file")
     merge_file = model_cfg.get("merge_file")
 
+    # BCP parameters
+    bcp_cfg = eval_cfg.get("bcp")
+    nodes = bcp_cfg.get("nodes")
+    ntasks_per_node = bcp_cfg.get("ntasks_per_node")
+    gpus_per_task = bcp_cfg.get("gpus_per_task")
+    instance = bcp_cfg.get("instance")
+    time_limit = bcp_cfg.get("time_limit")
+  
     # Run parameters
     name = run_cfg.get("name")
     tasks = run_cfg.get("tasks")
-    log_dir = os.path.join(bignlp_path, run_cfg.get("output_path"), name)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
     
-    # Process container-mounts.
-    mounts_str = f"{bignlp_path}:{bignlp_path}"
-    if container_mounts is not None:
-        assert isinstance(container_mounts, omegaconf.listconfig.ListConfig), "container_mounts must be a list."
-        for mount in container_mounts:
-            if mount is not None and isinstance(mount, str):
-                mounts_str += f",{mount}:{mount}"
-
-    flags = (
-        f"--no-container-mount-home "
-        f"--container-image {container} "
-        f"--container-mounts {mounts_str} "
-        f"-o {log_dir}/{name}-%j.log "
-        f"-e {log_dir}/{name}-%j.error "
-    )
-    cache_dir = os.path.join(bignlp_path, "eval_scripts/data_cache")
+    output_path = run_cfg.get("output_path")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    log_dir = output_path
+        
+    cache_dir = os.path.join(output_path, "data_cache")
     code_path1 = os.path.join(bignlp_path, "eval_scripts/eval_harness/download.py")
     eval_cmd1 = f"python {code_path1} --tasks {tasks} --cache_dir {cache_dir} " \
 
-    new_script_path = os.path.join(bignlp_path, "eval_scripts/eval_script.sh")
+    new_script_path = os.path.join(bignlp_path, f"eval_scripts/{name}.sh")
     code_path2 = os.path.join(bignlp_path, "eval_scripts/eval_harness/evaluate.py")
     eval_cmd2 = f"python -u {code_path2} " \
                 f"--name {name} " \
@@ -117,26 +93,29 @@ def run_evaluation(cfg, dependency=None):
                 f"--output_path {log_dir} " \
                 f"--model_args nemo_model={checkpoint},tensor_model_parallel_size={tensor_model_parallel_size},vocab_file={vocab_file},merges_file={merge_file} "
 
-    create_slurm_file(
+    create_bcp_file(
+        bignlp_path=bignlp_path,
         new_script_path=new_script_path,
         eval_cmd1=eval_cmd1,
         eval_cmd2=eval_cmd2,
-        job_name=job_name,
-        flags=flags,
-        dependency=dependency,
-        exclusive=exclusive,
-        mem=mem,
-        overcommit=overcommit,
-        time=time_limit,
-        nodes=nodes,
+        num_nodes=nodes,
         ntasks_per_node=ntasks_per_node,
-        gpus_per_task=gpus_per_task,
-        partition=partition,
-        account=account,
+        log_file=f"{log_dir}/log.txt",
+        err_file=f"{log_dir}/err.txt"
     )
-    job_id = subprocess.check_output(
-        [f"sbatch --parsable {new_script_path}"], shell=True
+
+    submit_cmd = create_bcp_submit_cmd(
+        job_name=bcp_cfg.get("job_name"),
+        container=container,
+        workspace_common=bcp_cfg.get("workspace_common"),
+        workspace_scripts=bcp_cfg.get("workspace_scripts"),
+        bignlp_path=bignlp_path,
+        bcp_script=new_script_path,
+        instance=instance,
+        num_nodes=nodes,
+        array_type="PYTORCH",
+        total_runtime=time_limit
     )
-    dependency = job_id.decode("utf-8")
-    print(f"Submitted Evaluation script with job id: {dependency}")
-    return dependency
+    
+    print(f"\n Submit command after conversion done:\n {submit_cmd}")
+    print(f"\n Script file: {new_script_path}")
