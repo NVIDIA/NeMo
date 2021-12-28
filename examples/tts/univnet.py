c13f6d535378b89240d6e892f05d88d247f395de
L0SG <tkdrlf9202@gmail.com>
2021-12-28 15:28:36 +0900

    lr_logger and epoch_time_logger callbacks
    Signed-off-by: L0SG <tkdrlf9202@gmail.com>

--- examples/tts/univnet.py
+++ examples/tts/univnet.py
@@ -14,6 +14,7 @@
 
 import pytorch_lightning as pl
 
+from nemo.collections.common.callbacks import LogEpochTimeCallback
 from nemo.collections.tts.models import UnivNetModel
 from nemo.core.config import hydra_runner
 from nemo.utils.exp_manager import exp_manager
@@ -24,6 +25,9 @@ def main(cfg):
     trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
     model = UnivNetModel(cfg=cfg.model, trainer=trainer)
+    lr_logger = pl.callbacks.LearningRateMonitor()
+    epoch_time_logger = LogEpochTimeCallback()
+    trainer.callbacks.extend([lr_logger, epoch_time_logger])
     trainer.fit(model)
 
 
