eecae4fe34519dbc04198bfaa8bf78410a915d0d
PeganovAnton <peganoff2@mail.ru>
2022-01-11 09:49:23 +0300

    create pickled features file regardless of value of use_cache
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/nlp/data/token_classification/punctuation_capitalization_dataset.py
+++ nemo/collections/nlp/data/token_classification/punctuation_capitalization_dataset.py
@@ -984,10 +984,6 @@ class BertPunctuationCapitalizationDataset(Dataset):
             if self.verbose:
                 logging.info(f'Features restored from {features_pkl}')
             features = features[:-2]
-        if torch.distributed.is_initialized():
-            torch.distributed.barrier()
-        if master_device and not use_cache:
-            features_pkl.unlink()
 
         self.input_ids, self.subtokens_mask, self.punct_labels, self.capit_labels = features
         self.punct_label_ids, self.capit_label_ids = punct_label_ids, capit_label_ids
