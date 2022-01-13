dafb7f5a95db828cc9764034f44228751fe788a2
Somshubra Majumdar <titu1994@gmail.com>
2022-01-13 15:57:14 -0800

    Temp commit
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- scripts/speech_recognition/convert_hf_dataset_to_nemo.py
+++ scripts/speech_recognition/convert_hf_dataset_to_nemo.py
@@ -12,16 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from dataclasses import dataclass, is_dataclass
 from typing import Optional
 from omegaconf import OmegaConf, open_dict
 import hydra
 from hydra.core.config_store import ConfigStore
 
+from datasets import load_dataset, Dataset, IterableDataset
+
 
 @dataclass
 class HFDatasetConvertionConfig:
+    path: str
+    name: Optional[str] = None
+    split: Optional[str] = None
+
+    output_dir: str = "."
+    resolved_output_dir: str = ''
+    split_output_dir: str = None
+
+
+def prepare_output_dirs(cfg: HFDatasetConvertionConfig):
+    output_dir = os.path.abspath(cfg.output_dir)
+    output_dir = os.path.join(output_dir, cfg.path)
+
+    if cfg.name is not None:
+        output_dir = os.path.join(output_dir, cfg.name)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    cfg.resolved_output_dir = output_dir
+
+
+def process_dataset(dataset: IterableDataset, cfg: HFDatasetConvertionConfig):
     pass
+
 
 
 @hydra.main(config_name='hfds_config', config_path=None)
@@ -29,10 +56,27 @@ def main(cfg: HFDatasetConvertionConfig):
     if is_dataclass(cfg):
         cfg = OmegaConf.structured(cfg)
 
-    print(cfg)
+    prepare_output_dirs(cfg)
+
+    # Load dataset in streaming mode
+    dataset = load_dataset(
+        path=cfg.path, name=cfg.name, split=cfg.split, cache_dir=cfg.resolved_output_dir, streaming=True
+    )
+
+    if isinstance(dataset, dict):
+        print("Multiple splits found for dataset", cfg.path, ":", list(dataset.keys()))
+
+    else:
+        print("Single split found for dataset", cfg.path)
+
+        process_dataset(dataset, cfg)
 
 
 ConfigStore.instance().store(name='hfds_config', node=HFDatasetConvertionConfig)
 
 if __name__ == '__main__':
-    main()
+    cfg = HFDatasetConvertionConfig(
+        path='timit_asr', name=None, split='train', output_dir='/media/smajumdar/data/Datasets/Timit'
+    )
+
+    main(cfg)
