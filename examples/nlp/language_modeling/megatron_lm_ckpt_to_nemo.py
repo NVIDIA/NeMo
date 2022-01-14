0483b5de89729dc2dbe5119410861691302041ba
Yi Dong <yidong@nvidia.com>
2022-01-14 14:25:47 -0800

    register vocab to nemo file
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- examples/nlp/language_modeling/megatron_lm_ckpt_to_nemo.py
+++ examples/nlp/language_modeling/megatron_lm_ckpt_to_nemo.py
@@ -197,6 +197,14 @@ def load_from_checkpoint(
             model = cls._load_model_state(
                 checkpoint, strict=strict, cfg=checkpoint[cls.CHECKPOINT_HYPER_PARAMS_KEY].cfg, **kwargs
             )
+            # register the artifacts
+            cfg = checkpoint[cls.CHECKPOINT_HYPER_PARAMS_KEY].cfg
+            if cfg.tokenizer.model is not None:
+                model.register_artifact("tokenizer.tokenizer_model", cfg.tokenizer.model)
+            if cfg.tokenizer.vocab_file is not None:
+                model.register_artifact("tokenizer.vocab_file", cfg.tokenizer.vocab_file)
+            if cfg.tokenizer.merge_file is not None:
+                model.register_artifact("tokenizer.merge_file", cfg.tokenizer.merge_file)
         checkpoint = model
 
     finally:
