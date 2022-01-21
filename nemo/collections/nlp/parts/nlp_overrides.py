6662d893f543fe3545f4537c58c297ec72b818d7
ericharper <complex451@gmail.com>
2022-01-21 13:12:37 -0700

    update fwd/bwd for fp16
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/parts/nlp_overrides.py
+++ nemo/collections/nlp/parts/nlp_overrides.py
@@ -512,7 +512,7 @@ class MegatronHalfPrecisionPlugin(NativeMixedPrecisionPlugin):
                     _ = closure()
             else:
                 _ = closure()
-                optimizer.allreduce_main_grads()
+                # optimizer.allreduce_main_grads()
 
             self._after_closure(model, optimizer, optimizer_idx)
             return optimizer.step(**kwargs)
@@ -538,7 +538,6 @@ class MegatronHalfPrecisionPlugin(NativeMixedPrecisionPlugin):
             # note: the scaler will skip the `optimizer.step` if nonfinite gradients are found
             self.scaler.step(optimizer, **kwargs)
             self.scaler.update()
-            model.log('grad_scale', self.scaler.get_scale())
 
     @contextmanager
     def forward_context(self) -> Generator[None, None, None]:
@@ -547,16 +546,6 @@ class MegatronHalfPrecisionPlugin(NativeMixedPrecisionPlugin):
             yield
         finally:
             pass
-
-
-class NLPFitLoop(FitLoop):
-    pass
-
-    class NLPFitLoop(FitLoop):
-        pass
-
-
-# TODO: implement
 
 
 class NLPDataConnector(DataConnector):
