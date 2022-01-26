81d9898555d9b6bf45bd8303adefb3339de45c6e
Samuel Kriman <samuelkriman@gmail.com>
2022-01-26 09:44:33 -0800

    Self-supervised tutorial & update (#3344)
    * update
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * version test
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * version test
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * image for tutorial
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * enc_final in model_defaults
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * enc_final in model_defaults
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * self-supervised tutorial
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * fix
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * fix
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * contextnet ssl config
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * remove test_ds from config
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update recon decoder
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * don't save -last if val_loss is nan
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * check if val_loss is there
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * keep entries from same file together when tarring
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * keep entries from same file together when tarring
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * print num of files in shard
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * style
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * moving configs, add docstrings
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * tutorial updates
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update test
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update loading
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update loading
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update loading
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update loading
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * fix
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * fix
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * citrinet configs
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * citrinet configs update
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * update
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * default include all
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * comments
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>
    
    * docstring hydra example
    
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/core/classes/modelPT.py
+++ nemo/core/classes/modelPT.py
@@ -856,6 +856,36 @@ class ModelPT(LightningModule, Model):
         """
         return self._test_names[dataloader_idx]
 
+    def load_part_of_state_dict(self, state_dict, include, exclude, load_from_string):
+
+        excluded_param_names = []
+        # create dict
+        dict_to_load = {}
+        for k, v in state_dict.items():
+            should_add = False
+            # if any string in include is present, should add
+            for p in include:
+                if p in k:
+                    should_add = True
+                    break
+            # except for if any string from exclude is present
+            for e in exclude:
+                if e in k:
+                    excluded_param_names.append(k)
+                    should_add = False
+                    break
+            if should_add:
+                dict_to_load[k] = v
+
+        # Restore checkpoint part into current model
+        self.load_state_dict(dict_to_load, strict=False)
+        logging.info(f'Model checkpoint partially restored from {load_from_string}')
+        if len(excluded_param_names) > 0:
+            logging.info(
+                f'The following parameters were excluded from loading from {load_from_string} : {excluded_param_names}'
+            )
+            logging.info(f'Make sure that this is what you wanted!')
+
     @rank_zero_only
     def maybe_init_from_pretrained_checkpoint(self, cfg: OmegaConf, map_location: str = 'cpu'):
         """
@@ -864,15 +894,36 @@ class ModelPT(LightningModule, Model):
         requirement of exact model parameters matching.
 
         Initializations:
-            init_from_nemo_model: Str path to a .nemo model, which will be instantiated in order
-                to extract the state dict.
+            init_from_nemo_model: Str path to a .nemo model in order to load state_dict from single nemo file;
+            if loading from multiple files, pass in a dict where the values have the following fields:
+
+                path: Str path to .nemo model
+
+                include: Optional list of strings, at least one of which needs to be contained in parameter name
+                to be loaded from this .nemo file. Default: everything is included.
+
+                exclude: Optional list of strings, which can be used to exclude any parameter containing one of
+                these strings from being loaded from this .nemo file. Default: nothing is excluded.
+
+                hydra usage example:
+
+                init_from_nemo_model:
+                    model0:
+                        path:<path/to/model1>
+                        include:["encoder"]
+                    model1:
+                        path:<path/to/model2>
+                        include:["decoder"]
+                        exclude:["embed"]
 
             init_from_pretrained_model: Str name of a pretrained model checkpoint (obtained via cloud).
                 The model will be downloaded (or a cached copy will be used), instantiated and then
-                its state dict will be extracted.
+                its state dict will be extracted. If loading from multiple models, you can pass in a dict
+                with the same format as for init_from_nemo_model, except with "name" instead of "path"
 
             init_from_ptl_ckpt: Str name of a Pytorch Lightning checkpoint file. It will be loaded and
-                the state dict will extracted.
+                the state dict will extracted. If loading from multiple files, you can pass in a dict
+                with the same format as for init_from_nemo_model.
 
         Args:
             cfg: The config used to instantiate the model. It need only contain one of the above keys.
@@ -880,7 +931,11 @@ class ModelPT(LightningModule, Model):
                 (from the pretrained model or checkpoint) will be loaded.
 
         """
-        args = ['init_from_nemo_model', 'init_from_pretrained_model', 'init_from_ptl_ckpt']
+        args = [
+            'init_from_nemo_model',
+            'init_from_pretrained_model',
+            'init_from_ptl_ckpt',
+        ]
         arg_matches = [(1 if arg in cfg and arg is not None else 0) for arg in args]
 
         if sum(arg_matches) == 0:
@@ -895,57 +950,119 @@ class ModelPT(LightningModule, Model):
 
         if 'init_from_nemo_model' in cfg and cfg.init_from_nemo_model is not None:
             with open_dict(cfg):
-                # Restore model
-                model_path = cfg.pop('init_from_nemo_model')
-                restored_model = self.restore_from(
-                    model_path, map_location=map_location, strict=cfg.get("init_strict", True)
-                )
+                if isinstance(cfg.init_from_nemo_model, str):
+                    model_path = cfg.init_from_nemo_model
+                    # Restore model
+                    restored_model = self.restore_from(
+                        model_path, map_location=map_location, strict=cfg.get("init_strict", True)
+                    )
+                    # Restore checkpoint into current model
+                    self.load_state_dict(restored_model.state_dict(), strict=False)
+                    logging.info(f'Model checkpoint restored from nemo file with path : `{model_path}`')
+                    del restored_model
+                elif isinstance(cfg.init_from_nemo_model, dict):
+                    model_load_dict = cfg.init_from_nemo_model
+                    for model_load_cfg in model_load_dict.values():
+                        model_path = model_load_cfg.path
+                        # Restore model
+                        restored_model = self.restore_from(
+                            model_path, map_location=map_location, strict=cfg.get("init_strict", True)
+                        )
 
-                # Restore checkpoint into current model
-                self.load_state_dict(restored_model.state_dict(), strict=False)
-                logging.info(f'Model checkpoint restored from nemo file with path : `{model_path}`')
+                        include = model_load_cfg.pop('include', [""])
+                        exclude = model_load_cfg.pop('exclude', [])
 
-                del restored_model
+                        self.load_part_of_state_dict(
+                            restored_model.state_dict(), include, exclude, f'nemo file with path `{model_path}`'
+                        )
+
+                        del restored_model
+                else:
+                    raise TypeError("Invalid type: init_from_nemo_model is not a string or a dict!")
 
         if 'init_from_pretrained_model' in cfg and cfg.init_from_pretrained_model is not None:
             with open_dict(cfg):
                 # Restore model
-                model_name = cfg.pop('init_from_pretrained_model')
 
-                # Check if model is being resumed or not - only works if `Trainer` is attached to model
-                if hasattr(self, 'trainer') and self.trainer is not None:
-                    trainer = self.trainer
-                    if (
-                        hasattr(trainer, 'resume_from_checkpoint')
-                        and trainer.checkpoint_connector.resume_checkpoint_path is not None
-                    ):
-                        logging.info(
-                            "Model training is being resumed via Pytorch Lightning.\n"
-                            "Initialization from pretrained model (via cloud) will be skipped."
+                if isinstance(cfg.init_from_pretrained_model, str):
+                    model_name = cfg.pop('init_from_pretrained_model')
+
+                    # Check if model is being resumed or not - only works if `Trainer` is attached to model
+                    if hasattr(self, 'trainer') and self.trainer is not None:
+                        trainer = self.trainer
+                        if (
+                            hasattr(trainer, 'resume_from_checkpoint')
+                            and trainer.checkpoint_connector.resume_checkpoint_path is not None
+                        ):
+                            logging.info(
+                                "Model training is being resumed via Pytorch Lightning.\n"
+                                "Initialization from pretrained model (via cloud) will be skipped."
+                            )
+                            return
+
+                    restored_model = self.from_pretrained(
+                        model_name, map_location=map_location, strict=cfg.get("init_strict", True)
+                    )
+
+                    # Restore checkpoint into current model
+                    self.load_state_dict(restored_model.state_dict(), strict=False)
+                    logging.info(f'Model checkpoint restored from pretrained chackpoint with name : `{model_name}`')
+
+                    del restored_model
+                elif isinstance(cfg.init_from_pretrained_model, dict):
+                    model_load_dict = cfg.init_from_pretrained_model
+                    for model_load_cfg in model_load_dict.values():
+                        model_name = model_load_cfg.name
+                        # Restore model
+                        restored_model = self.from_pretrained(
+                            model_name, map_location=map_location, strict=cfg.get("init_strict", True)
                         )
-                        return
 
-                restored_model = self.from_pretrained(
-                    model_name, map_location=map_location, strict=cfg.get("init_strict", True)
-                )
+                        include = model_load_cfg.pop('include', [""])
+                        exclude = model_load_cfg.pop('exclude', [])
 
-                # Restore checkpoint into current model
-                self.load_state_dict(restored_model.state_dict(), strict=False)
-                logging.info(f'Model checkpoint restored from pretrained chackpoint with name : `{model_name}`')
+                        self.load_part_of_state_dict(
+                            restored_model.state_dict(),
+                            include,
+                            exclude,
+                            f'pretrained chackpoint with name `{model_name}`',
+                        )
 
-                del restored_model
+                        del restored_model
+                else:
+                    raise TypeError("Invalid type: init_from_pretrained_model is not a string or a dict!")
 
         if 'init_from_ptl_ckpt' in cfg and cfg.init_from_ptl_ckpt is not None:
             with open_dict(cfg):
-                # Restore checkpoint
-                ckpt_path = cfg.pop('init_from_ptl_ckpt')
-                ckpt = torch.load(ckpt_path, map_location=map_location)
+                if isinstance(cfg.init_from_ptl_ckpt, str):
+                    # Restore checkpoint
+                    ckpt_path = cfg.pop('init_from_ptl_ckpt')
+                    ckpt = torch.load(ckpt_path, map_location=map_location)
 
-                # Restore checkpoint into current model
-                self.load_state_dict(ckpt['state_dict'], strict=False)
-                logging.info(f'Model checkpoint restored from pytorch lightning chackpoint with path : `{ckpt_path}`')
+                    # Restore checkpoint into current model
+                    self.load_state_dict(ckpt['state_dict'], strict=False)
+                    logging.info(
+                        f'Model checkpoint restored from pytorch lightning chackpoint with path : `{ckpt_path}`'
+                    )
 
-                del ckpt
+                    del ckpt
+                elif isinstance(cfg.init_from_ptl_ckpt, dict):
+                    model_load_dict = cfg.init_from_ptl_ckpt
+                    for model_load_cfg in model_load_dict.values():
+                        ckpt_path = model_load_cfg.path
+                        # Restore model
+                        ckpt = torch.load(ckpt_path, map_location=map_location)
+
+                        include = model_load_cfg.pop('include', [""])
+                        exclude = model_load_cfg.pop('exclude', [])
+
+                        self.load_part_of_state_dict(
+                            ckpt['state_dict'], include, exclude, f'nemo file with path `{model_path}`'
+                        )
+
+                        del ckpt
+                else:
+                    raise TypeError("Invalid type: init_from_ptl_ckpt is not a string or a dict!")
 
     def teardown(self, stage: str):
         """
