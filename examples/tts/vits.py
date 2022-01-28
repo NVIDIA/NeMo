33d21d38a02a65d0291770d92be8af5aed1428eb
treacker <emshabalin@yandex.ru>
2022-01-28 08:19:28 -0800

    Added scaler

--- examples/tts/vits.py
+++ examples/tts/vits.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 
 import pytorch_lightning as pl
+from pytorch_lightning.plugins.precision.native_amp import NativeMixedPrecisionPlugin
+
+from torch.cuda.amp import GradScaler
 
 from nemo.collections.common.callbacks import LogEpochTimeCallback
 from nemo.collections.tts.models.vits import VitsModel
@@ -22,7 +25,19 @@ from nemo.utils.exp_manager import exp_manager
 
 @hydra_runner(config_path="conf", config_name="vits")
 def main(cfg):
-    trainer = pl.Trainer(**cfg.trainer)
+    plugins = []
+    if cfg.trainer.precision in [16, 'bf16']:
+        scaler = GradScaler(enabled=True)
+        # if cfg.trainer.precision == 16:
+        #     scaler = GradScaler(
+        #         init_scale=cfg.model.get('native_amp_init_scale', 2 ** 32),
+        #         growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
+        #     )
+
+        plugins.append(NativeMixedPrecisionPlugin(precision=cfg.trainer.precision, device='cuda', scaler=scaler))
+
+
+    trainer = pl.Trainer(plugins=plugins, **cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
     model = VitsModel(cfg=cfg.model, trainer=trainer)
     trainer.callbacks.extend([pl.callbacks.LearningRateMonitor(), LogEpochTimeCallback()])
