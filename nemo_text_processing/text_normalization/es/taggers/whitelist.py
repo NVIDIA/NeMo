59cc488f3481f600fbb2f1720dbd489c2e90b9f1
tbartley94 <tbartley@nvidia.com>
2022-01-28 16:56:18 -0800

    Added date tagger and subsequent tests. Also added roman numberal -> digit fst to en/graph_utils to avoid constant rewriting of it.
    Signed-off-by: tbartley94 <tbartley@nvidia.com>
    Signed-off-by: Bonham79 <travismbartley@gmail.com>

--- nemo_text_processing/text_normalization/es/taggers/whitelist.py
+++ nemo_text_processing/text_normalization/es/taggers/whitelist.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo_text_processing.text_normalization.de.utils import get_abs_path, load_labels
+from nemo_text_processing.text_normalization.es.utils import get_abs_path, load_labels
 from nemo_text_processing.text_normalization.en.graph_utils import GraphFst, convert_space
 
 try:
