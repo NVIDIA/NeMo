d986d76a7e5473c47cab02f46db7b5d09dcc3264
ericharper <complex451@gmail.com>
2022-01-28 20:11:17 -0700

    adding pipeline for complete method
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -23,7 +23,7 @@ from apex.transformer.pipeline_parallel.schedules.fwd_bwd_no_pipelining import f
 from apex.transformer.pipeline_parallel.schedules.fwd_bwd_pipelining_without_interleaving import (
     forward_backward_pipelining_without_interleaving,
 )
-from apex.transformer.pipeline_parallel.utils import get_num_microbatches
+from apex.transformer.pipeline_parallel.utils import get_num_microbatches, _reconfigure_microbatch_calculator
 from omegaconf.dictconfig import DictConfig
 from omegaconf.omegaconf import open_dict
 from pytorch_lightning.plugins.precision.native_amp import NativeMixedPrecisionPlugin
@@ -49,10 +49,11 @@ from nemo.collections.nlp.modules.common.megatron.utils import (
 )
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
 from nemo.collections.nlp.parts.nlp_overrides import GradScaler
+from nemo.collections.nlp.parts.schedules import nemo_forward_backward_pipelining_without_interleaving
 from nemo.core.optim import MainParamsOptimizerWrapper, prepare_lr_scheduler
 from nemo.collections.nlp.parts.nlp_overrides import NLPDataConnector
 from nemo.collections.nlp.parts.utils_funcs import get_last_rank, inject_model_parallel_rank
-from nemo.utils import AppState, logging
+from nemo.utils import AppState, app_state, logging
 
 
 class MegatronGPTModel(NLPModel):
@@ -357,6 +358,21 @@ class MegatronGPTModel(NLPModel):
                 loss = self.loss_func(loss_mask, output_tensor)
                 reduced_loss = average_losses_across_data_parallel_group([loss])
                 return loss, {'avg': reduced_loss}
+
+            return output_tensor, loss_func
+
+        return fwd_output_and_loss_func
+
+    def get_forward_output_no_loss_func(self):
+        """ Used for generating text. No labels."""
+
+        def fwd_output_and_loss_func(batch, model):
+            tokens, position_ids, attention_mask = batch
+            attention_mask = attention_mask[0:1]
+            output_tensor = model(tokens, position_ids, attention_mask)
+
+            def loss_func(output_tensor):
+                return None, None
 
             return output_tensor, loss_func
 
@@ -835,9 +851,33 @@ class MegatronGPTModel(NLPModel):
                         eod_mask_loss=self.cfg.get('eod_mask_loss', False),
                     )
 
-                # No labels during inference. Still need masks to not attend to the right
-                output_tensor = self(tokens, position_ids, attention_mask, prompt_tags=prompt_tags, labels=None)
-                output_tensor = tensor_parallel.gather_from_tensor_model_parallel_region(output_tensor)
+                # get output tensor
+                # batch_for_pipeline = self.process_global_batch(batch)
+                if self.cfg.get('pipeline_model_parallel_size', 1) > 1:
+                    app_state = AppState()
+                    _reconfigure_microbatch_calculator(
+                        rank=self.trainer.global_rank,
+                        rampup_batch_size=None,
+                        global_batch_size=1,
+                        micro_batch_size=1,
+                        data_parallel_size=1,
+                    )
+                    batch = [tokens, position_ids, attention_mask]
+                    # tensor shape = [seq len, micro batch size, hidden size]
+                    tensor_shape = [len(tokens), 1, self.cfg.hidden_size]
+                    output_tensors = nemo_forward_backward_pipelining_without_interleaving(
+                        forward_step_func=self.get_forward_output_no_loss_func(),
+                        batch=batch,
+                        model=self.model,
+                        forward_only=True,
+                        tensor_shape=tensor_shape,
+                        dtype=self.autocast_dtype,
+                    )
+                else:
+                    # No labels during inference. Still need masks to not attend to the right
+                    output_tensor = self(tokens, position_ids, attention_mask, prompt_tags=prompt_tags, labels=None)
+                    output_tensor = tensor_parallel.gather_from_tensor_model_parallel_region(output_tensor)
+
                 log_probs, token_ids = torch.max(logsoftmaxlayer(output_tensor), dim=-1)
                 reached_eos = token_ids[0, -1].item() == self.tokenizer.eos_id
                 tokens = torch.cat([tokens, torch.unsqueeze(token_ids[:, -1], 1)], dim=1)
