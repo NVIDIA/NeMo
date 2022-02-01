dd0c767f35c65785c9308f077d7d179ec0c84ec5
Yang Zhang <yangzhang@nvidia.com>
2022-02-01 13:16:14 -0800

    added ssn
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/en/taggers/telephone.py
+++ nemo_text_processing/inverse_text_normalization/en/taggers/telephone.py
@@ -59,35 +59,25 @@ class TelephoneFst(GraphFst):
             ]
         )
         double_digit.invert()
-        
+
         # to handle cases like "one twenty three"
         two_digit_cardinal = pynini.compose(cardinal.graph_no_exception, NEMO_DIGIT ** 2)
-        double_digit_to_digit = pynini.compose(double_digit, str_to_digit + pynutil.delete(" ") + str_to_digit) | two_digit_cardinal
-        
-        number_part = (
-            pynini.closure(digit_to_str + insert_space, 2, 2)
-            + digit_to_str
-            + pynutil.delete("-")
-            + insert_space
-            + pynini.closure(digit_to_str + insert_space, 2, 2)
-            + digit_to_str
-            + pynutil.delete("-")
-            + insert_space
-            + pynini.closure(digit_to_str + insert_space, 3, 3)
-            + digit_to_str
-        )
-        number_part = (
-            pynutil.insert("number_part: \"")
-            + pynini.cdrewrite(double_digit, "", "", NEMO_SIGMA) @ pynini.invert(number_part)
-            + pynutil.insert("\"")
+        double_digit_to_digit = (
+            pynini.compose(double_digit, str_to_digit + pynutil.delete(" ") + str_to_digit) | two_digit_cardinal
         )
 
+        single_or_double_digit = (double_digit_to_digit | str_to_digit).optimize()
+        single_or_double_digit = (
+            single_or_double_digit + pynini.closure(pynutil.delete(" ") + single_or_double_digit)
+        ).optimize()
 
-        cardinal_option = (
-            (str_to_digit + pynutil.delete(" ") + double_digit_to_digit)
-            | double_digit_to_digit
-            | (double_digit_to_digit + pynutil.delete(" ") + str_to_digit)
-        )
+        number_part = pynini.compose(
+            single_or_double_digit,
+            NEMO_DIGIT ** 3 + pynutil.insert("-") + NEMO_DIGIT ** 3 + pynutil.insert("-") + NEMO_DIGIT ** 4,
+        ).optimize()
+        number_part = pynutil.insert("number_part: \"") + number_part.optimize() + pynutil.insert("\"")
+
+        cardinal_option = pynini.compose(single_or_double_digit, NEMO_DIGIT ** (2, 3))
 
         country_code = (
             pynutil.insert("country_code: \"")
@@ -98,19 +88,16 @@ class TelephoneFst(GraphFst):
         optional_country_code = pynini.closure(country_code + pynutil.delete(" ") + insert_space, 0, 1).optimize()
         graph = optional_country_code + number_part
 
-        # card number
-        
-        card_graph = (double_digit_to_digit | str_to_digit).optimize()
-        card_graph = (card_graph + pynini.closure(pynutil.delete(" ") + card_graph)).optimize()
-        # reformat card number, group by four
+        # credit card number
         space_four_digits = insert_space + NEMO_DIGIT ** 4
-        credit_card_graph = pynini.compose(card_graph, NEMO_DIGIT ** 4 + space_four_digits ** 3).optimize()
+        credit_card_graph = pynini.compose(single_or_double_digit, NEMO_DIGIT ** 4 + space_four_digits ** 3).optimize()
         graph |= pynutil.insert("number_part: \"") + credit_card_graph.optimize() + pynutil.insert("\"")
 
-        
-
         # SSN
-        ssn_graph = pynini.compose(card_graph, NEMO_DIGIT ** 3 + pynutil.insert("-") + NEMO_DIGIT ** 2 + pynutil.insert("-") + NEMO_DIGIT ** 4).optimize()
+        ssn_graph = pynini.compose(
+            single_or_double_digit,
+            NEMO_DIGIT ** 3 + pynutil.insert("-") + NEMO_DIGIT ** 2 + pynutil.insert("-") + NEMO_DIGIT ** 4,
+        ).optimize()
         graph |= pynutil.insert("number_part: \"") + ssn_graph.optimize() + pynutil.insert("\"")
 
         # ip
