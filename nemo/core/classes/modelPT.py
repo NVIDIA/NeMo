b44bf2f3eadfa79b023d896022b5b04d454d0851
nithinraok <nithinrao.koluguri@gmail.com>
2022-02-02 10:17:04 -0800

    moved trainer devices back to num_gpus
    Signed-off-by: nithinraok <nithinrao.koluguri@gmail.com>

--- nemo/core/classes/modelPT.py
+++ nemo/core/classes/modelPT.py
@@ -474,11 +474,11 @@ class ModelPT(LightningModule, Model):
                 optim_config['sched']['t_accumulate_grad_batches'] = self._trainer.accumulate_grad_batches
                 optim_config['sched']['t_limit_train_batches'] = self._trainer.limit_train_batches
                 if self._trainer._distrib_type is None:
-                    optim_config['sched']['t_num_workers'] = self._trainer.devices or 1
+                    optim_config['sched']['t_num_workers'] = self._trainer.num_gpus or 1
                 elif self._trainer._distrib_type.value == "ddp_cpu":
                     optim_config['sched']['t_num_workers'] = self._trainer.num_processes * self._trainer.num_nodes
                 elif self._trainer._distrib_type.value == "ddp":
-                    optim_config['sched']['t_num_workers'] = self._trainer.devices * self._trainer.num_nodes
+                    optim_config['sched']['t_num_workers'] = self._trainer.num_gpus * self._trainer.num_nodes
                 elif HAVE_NLPPLUGIN and isinstance(self._trainer.accelerator.training_type_plugin, NLPDDPPlugin):
                     app = AppState()
                     optim_config['sched']['t_num_workers'] = app.data_parallel_size
@@ -487,7 +487,7 @@ class ModelPT(LightningModule, Model):
                         f"The lightning trainer received strategy: {self._trainer._distrib_type}. We "
                         "recommend to use 'ddp' instead."
                     )
-                    optim_config['sched']['t_num_workers'] = self._trainer.devices * self._trainer.num_nodes
+                    optim_config['sched']['t_num_workers'] = self._trainer.num_gpus * self._trainer.num_nodes
             else:
                 optim_config['sched']['max_steps'] = self._trainer.max_steps
 
@@ -1180,7 +1180,7 @@ class ModelPT(LightningModule, Model):
                     "  trainer.test(model)\n\n"""
 
         if trainer is not None:
-            if trainer.devices > 1:
+            if trainer.num_gpus > 1:
                 logging.warning(DDP_WARN)
                 return False
 
@@ -1210,8 +1210,8 @@ class ModelPT(LightningModule, Model):
         # Update AppState with world information from trainer
         if isinstance(trainer, Trainer):
             app_state = AppState()
-            if self._trainer.devices and self._trainer.num_nodes:
-                app_state.world_size = self._trainer.devices * self._trainer.num_nodes
+            if self._trainer.num_gpus and self._trainer.num_nodes:
+                app_state.world_size = self._trainer.num_gpus * self._trainer.num_nodes
         else:
             logging.warning(f'World size can only be set by PyTorch Lightning Trainer.')
 
