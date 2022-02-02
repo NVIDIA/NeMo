b12bf684be5b3e38fce1c44071a83038c4027649
Micha Livne <mlivne@nvidia.com>
2022-02-02 23:05:02 +0200

    1. Debugging.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_lm_encoder_decoder_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_lm_encoder_decoder_model.py
@@ -28,7 +28,7 @@ from nemo.collections.nlp.data.language_modeling.megatron.data_samplers import (
 )
 from nemo.collections.nlp.data.language_modeling.megatron.dataset_utils import build_train_valid_test_datasets
 from nemo.collections.nlp.models.language_modeling.megatron.tokens_encoder_decoder import TokensEncoderDecoderModule
-from nemo.collections.nlp.models.language_modeling.megatron.megatron_base_model import MegatronBaseModel
+from nemo.collections.nlp.models.language_modeling.megatron_base_model import MegatronBaseModel
 from nemo.collections.nlp.models.nlp_model import NLPModel
 from nemo.collections.nlp.modules.common.megatron.clip_grads import clip_grad_norm_fp32
 from nemo.collections.nlp.modules.common.megatron.megatron_init import (
