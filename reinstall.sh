31b1927bb05c66761ffadb48e7aa9ffb13016b93
Somshubra Majumdar <titu1994@gmail.com>
2022-02-04 17:07:18 -0800

    Fix wheel build (#3610)
    Signed-off-by: smajumdar <titu1994@gmail.com>
    Signed-off-by: Bonham79 <travismbartley@gmail.com>

--- reinstall.sh
+++ reinstall.sh
@@ -25,7 +25,7 @@ if [[ "$INSTALL_OPTION" == "dev" ]]; then
 else
     rm -rf dist/
     ${PIP} install build
-    python -m build --no-isolation
+    python -m build --no-isolation --wheel
     DIST_FILE=$(find ./dist -name "*.whl" | head -n 1)
     ${PIP} install "${DIST_FILE}[all]"
 fi
