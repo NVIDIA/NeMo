4395442a17f964431619443ee108a919a61e00e2
Micha Livne <mlivne@nvidia.com>
2022-02-09 17:16:34 +0200

    1. Moved tokens_encoder_decoder.py to modules.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_lm_encoder_decoder_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_lm_encoder_decoder_model.py
@@ -26,9 +26,9 @@ from nemo.collections.nlp.data.language_modeling.megatron.data_samplers import (
     MegatronPretrainingRandomSampler,
     MegatronPretrainingSampler,
 )
-from nemo.collections.nlp.models.language_modeling.megatron.tokens_encoder_decoder import TokensEncoderDecoderModule
 from nemo.collections.nlp.models.language_modeling.megatron_base_model import MegatronBaseModel
 from nemo.collections.nlp.modules.common.megatron.clip_grads import clip_grad_norm_fp32
+from nemo.collections.nlp.modules.common.megatron.tokens_encoder_decoder import TokensEncoderDecoderModule
 from nemo.collections.nlp.modules.common.megatron.utils import average_losses_across_data_parallel_group
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
 from nemo.utils import AppState, logging
