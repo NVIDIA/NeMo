6a14a227650e8964dc23f44154709110543a835b
nithinraok <nithinrao.koluguri@gmail.com>
2022-02-09 11:03:39 -0800

    revert distributed backend type
    Signed-off-by: nithinraok <nithinrao.koluguri@gmail.com>

--- nemo/collections/tts/helpers/helpers.py
+++ nemo/collections/tts/helpers/helpers.py
@@ -90,18 +90,16 @@ def get_batch_size(train_dataloader):
         raise ValueError(f'Could not find batch_size from train_dataloader: {train_dataloader}')
 
 
-# TODO change trainer.training_type_plugin.distributed_backend to trainer.strategy after PTL 1.6 version releases
 def get_num_workers(trainer):
-    if trainer.training_type_plugin is None:
+    if trainer.accelerator is None:
         return trainer.num_gpus or 1
-    elif trainer.training_type_plugin.distributed_backend.value == "ddp_cpu":
+    elif trainer.accelerator == "ddp_cpu":
         return trainer.num_processes * trainer.num_nodes
-    elif trainer.training_type_plugin.distributed_backend.value == "ddp":
+    elif trainer.accelerator == "ddp":
         return trainer.num_gpus * trainer.num_nodes
     else:
         logging.warning(
-            f"The lightning trainer received strategy: {trainer.training_type_plugin.distributed_backend.value}. We "
-            "recommend to use 'ddp' instead."
+            f"The lightning trainer received accelerator: {trainer.accelerator}. We " "recommend to use 'ddp' instead."
         )
         return trainer.num_gpus * trainer.num_nodes
 
