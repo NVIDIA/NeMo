bea35fc1b017159baafaa2b998c9c3580979ddda
ericharper <complex451@gmail.com>
2022-02-10 18:24:48 -0700

    remove config check
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -74,7 +74,7 @@ class MegatronGPTModel(NLPModel):
         super().__init__(cfg, trainer=trainer)
         self.cfg = cfg
 
-        self._validate_config()
+        self._validate_trainer()
 
         # used in NVIDIA NGC PyTorch containers
         self._enable_nvidia_optimizations()
@@ -1045,13 +1045,6 @@ class MegatronGPTModel(NLPModel):
             Microbatches are transferred from CPU to GPU inside the pipeline. 
         """
         return batch
-
-    def _validate_config(self):
-        """ Certain config options can break training.
-            Here we try to catch them and raise an error.
-        """
-
-        pass
 
     def _validate_trainer(self):
         """ Certain trainer configurations can break training.
