34133f9e81dcd97734d18404a99f625e3d2e89fe
Bonham79 <travismbartley@gmail.com>
2022-02-14 10:12:06 -0500

    Tweaking copyright headers.
    Signed-off-by: Bonham79 <travismbartley@gmail.com>

--- nemo_text_processing/text_normalization/es/graph_utils.py
+++ nemo_text_processing/text_normalization/es/graph_utils.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
-# Copyright 2015 and onwards Google, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
