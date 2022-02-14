007b9e20c9365c16d7f167f1a670090f4e383517
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2022-02-14 15:43:56 -0800

    Style fixes
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/language_modeling/megatron_t5_pretraining.py
+++ examples/nlp/language_modeling/megatron_t5_pretraining.py
@@ -15,8 +15,8 @@
 
 from omegaconf.omegaconf import OmegaConf, open_dict
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks.timer import Timer
 from pytorch_lightning.callbacks import ModelSummary
+from pytorch_lightning.callbacks.timer import Timer
 from pytorch_lightning.plugins.environments.torchelastic_environment import TorchElasticEnvironment
 from pytorch_lightning.plugins.precision.native_amp import NativeMixedPrecisionPlugin
 from pytorch_lightning.trainer.connectors.checkpoint_connector import CheckpointConnector
