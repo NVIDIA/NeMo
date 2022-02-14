5f1c1b3c8733c4f12c3c1bebc1fa5e1febdc0795
Subhankar Ghosh <subhankar2321@gmail.com>
2022-02-14 14:27:33 -0800

    reformatting change
    Signed-off-by: Subhankar Ghosh <subhankar2321@gmail.com>

--- scripts/dataset_processing/tts/libritts/get_data.py
+++ scripts/dataset_processing/tts/libritts/get_data.py
@@ -73,7 +73,12 @@ def __process_transcript(file_path: str):
         speaker_id = file_path.split('/')[-3]
         assert os.path.exists(wav_file), f"{wav_file} not found!"
         duration = subprocess.check_output(f"soxi -D {wav_file}", shell=True)
-        entry = {'audio_filepath': os.path.abspath(wav_file), 'duration': float(duration), 'text': text, 'speaker': int(speaker_id)}
+        entry = {
+            'audio_filepath': os.path.abspath(wav_file), 
+            'duration': float(duration), 
+            'text': text, 
+            'speaker': int(speaker_id),
+        }
 
         entries.append(entry)
 
