2c3c5355e005a8df5f5576a3af6a4b38d3aa1b7c
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2022-02-16 14:15:03 -0800

    Update license header
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- nemo/collections/nlp/modules/common/megatron/transformer.py
+++ nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-# Copyright 2020 The HuggingFace Inc. team.
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
