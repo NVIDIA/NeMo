b709e68131a1d4c5d76e4200b8d2041dfafd5965
Somshubra Majumdar <titu1994@gmail.com>
2022-02-17 17:00:06 -0800

    Format check imports
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- tests/core_ptl/check_imports.py
+++ tests/core_ptl/check_imports.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import inspect
 import argparse
-import wrapt
+import inspect
 import traceback
+
+import wrapt
 
 from nemo.core import Model
 from nemo.utils import model_utils
