0d3027b6a202528f27b105b52c87027786c90557
Dima Rekesh <bmwshop@gmail.com>
2022-02-22 10:33:39 -0800

    removed unused import
    Signed-off-by: Dima Rekesh <bmwshop@gmail.com>

--- nemo/collections/common/tokenizers/aggregate_tokenizer.py
+++ nemo/collections/common/tokenizers/aggregate_tokenizer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Union
 
 import numpy as np
 
