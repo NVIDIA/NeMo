639cc2e4393f673fbdf79c3a81c3327fc76e480f
yaoyu-33 <yaoyu.094@gmail.com>
2022-02-23 16:46:14 -0800

    Fix bug in checkpoint conversion - rewrite ckpt conversion wrapper.

--- bignlp/conversion_scripts/convert.py
+++ bignlp/conversion_scripts/convert.py
@@ -1,10 +1,30 @@
 import sys
 import os
 import subprocess
+import glob
 
 import hydra
-import omegaconf
+from omegaconf import OmegaConf
 from bignlp.bignlp_utils import add_container_mounts
+
+
+def inject_model_parallel_rank(filepath, tensor_model_parallel_size=1, pipeline_model_parallel_size=1):
+    """
+    Injects tensor/pipeline model parallel ranks into the filepath.
+    Does nothing if not using model parallelism.
+    """
+    tensor_model_parallel_rank = pipeline_model_parallel_rank = 0
+    if tensor_model_parallel_size > 1 or pipeline_model_parallel_size > 1:
+        # filepath needs to be updated to include mp_rank
+        dirname = os.path.dirname(filepath)
+        basename = os.path.basename(filepath)
+        if pipeline_model_parallel_size is None or pipeline_model_parallel_size == 1:
+            filepath = f'{dirname}/mp_rank_{tensor_model_parallel_rank:02d}/{basename}'
+        else:
+            filepath = f'{dirname}/tp_rank_{tensor_model_parallel_rank:02d}_pp_rank_{pipeline_model_parallel_rank:03d}/{basename}'
+        return filepath
+    else:
+        return filepath
 
 
 def create_slurm_file(
@@ -49,11 +69,11 @@ def create_slurm_file(
 
 
 def create_bcp_file(
-    cmd_str,
-    num_nodes,
-    ntasks_per_node,
-    log_file,
-    new_script_path
+        cmd_str,
+        num_nodes,
+        ntasks_per_node,
+        log_file,
+        new_script_path
 ):
     with open(new_script_path, "w") as f:
         f.writelines(f'bcprun -n {num_nodes} -p {ntasks_per_node} -c "{cmd_str}" >> {log_file} 2>&1\n\n')
@@ -61,7 +81,6 @@ def create_bcp_file(
     os.chmod(new_script_path, 0o755)
 
 
-        
 def convert_ckpt(cfg, hydra_args="", dependency=None):
     # Read config
     bignlp_path = cfg.bignlp_path
@@ -78,21 +97,68 @@ def convert_ckpt(cfg, hydra_args="", dependency=None):
     nodes = run_cfg.nodes
     time_limit = run_cfg.time_limit
     ntasks_per_node = run_cfg.ntasks_per_node
-    gpus_per_task = run_cfg.gpus_per_task
     convert_name = run_cfg.convert_name
     model_train_name = run_cfg.model_train_name
     results_dir = run_cfg.results_dir
     output_path = run_cfg.output_path
     nemo_file_name = run_cfg.nemo_file_name
-    
+    gpus_per_node = run_cfg.ntasks_per_node
+    nemo_file_path = os.path.join(output_path, nemo_file_name)
+
+    # Model parameters
+    model_type = model_cfg.model_type
+    checkpoint_folder = model_cfg.checkpoint_folder
+    checkpoint_name = model_cfg.checkpoint_name
+    hparams_file = model_cfg.hparams_file
+    tensor_model_parallel_size = model_cfg.tensor_model_parallel_size
+    pipeline_model_parallel_size = model_cfg.pipeline_model_parallel_size
+    vocab_file = model_cfg.vocab_file
+    merge_file = model_cfg.merge_file
+
     os.makedirs(output_path, exist_ok=True)
     os.makedirs(results_dir, exist_ok=True)
 
-    new_script_path = os.path.join(bignlp_path, f"bignlp/conversion_scripts/{model_train_name}.sh")
-    code_path = os.path.join(bignlp_path, "bignlp/conversion_scripts/convert_ckpt.py")
 
-    hydra_args = hydra_args.replace(" ", " \\\n  ")
-    cmd_str = f"python3 -u {code_path} \\\n  {hydra_args}"
+    # Checkpoint search
+    if checkpoint_name == "latest":
+        checkpoints = os.path.join(checkpoint_folder, '*.ckpt')
+        checkpoints = inject_model_parallel_rank(checkpoints, tensor_model_parallel_size, pipeline_model_parallel_size)
+        checkpoint_list = glob.glob(checkpoints)
+        latest_checkpoint = max(checkpoint_list, key=os.path.getctime)
+        checkpoint_name = os.path.basename(latest_checkpoint)
+
+    checkpoint = os.path.join(checkpoint_folder, checkpoint_name)
+    checkpoint = inject_model_parallel_rank(checkpoint, tensor_model_parallel_size, pipeline_model_parallel_size)
+    checkpoint_list = glob.glob(checkpoint)
+    if len(checkpoint_list) > 1:
+        raise ValueError("Too many checkpoints fit the checkpoint name pattern in conversion config.")
+    if len(checkpoint_list) == 0:
+        raise ValueError("No checkpoint found with the checkpoint name pattern in conversion config.")
+    checkpoint_name = os.path.basename(checkpoint_list[0])
+
+    # Create hparam override file for vocab and merge
+    hparams_override_file = os.path.join(results_dir, "hparams_override.yaml")
+    conf = OmegaConf.load(hparams_file)
+    if vocab_file is not None:
+        conf.cfg.tokenizer.vocab_file = vocab_file
+    if merge_file is not None:
+        conf.cfg.tokenizer.merge_file = merge_file
+    with open(hparams_override_file, 'w') as f:
+        OmegaConf.save(config=conf, f=f)
+
+    new_script_path = os.path.join(bignlp_path, f"bignlp/conversion_scripts/{model_train_name}.sh")
+    code_path = "/opt/bignlp/NeMo/examples/nlp/language_modeling/megatron_ckpt_to_nemo.py"
+    args = f"--gpus_per_node={gpus_per_node} " \
+           f"--model_type={model_type} " \
+           f"--checkpoint_folder={checkpoint_folder} " \
+           f"--checkpoint_name={checkpoint_name} " \
+           f"--hparams_file={hparams_override_file} " \
+           f"--nemo_file_path={nemo_file_path} " \
+           f"--tensor_model_parallel_size={tensor_model_parallel_size} " \
+           f"--pipeline_model_parallel_size={pipeline_model_parallel_size}"
+
+    args = args.replace(" ", " \\\n  ")
+    cmd_str = f"python3 -u {code_path} \\\n  {args}"
 
     if cfg.cluster_type == "bcm":
         # BCM parameters
@@ -110,13 +176,13 @@ def convert_ckpt(cfg, hydra_args="", dependency=None):
             f"--no-container-mount-home "
             f"--container-image {container} "
             f"--container-mounts {mounts_str} "
-            f"-o {base_results_dir}/{model_train_name}/{convert_name}/convert-%j.log "
-            f"-e {base_results_dir}/{model_train_name}/{convert_name}/convert-%j.error "
+            f"-o {results_dir}/convert-%j.log "
+            f"-e {results_dir}/convert-%j.error "
         )
         create_slurm_file(
             new_script_path=new_script_path,
             convert_cmd=cmd_str,
-            job_name=job_name_prefix+job_name,
+            job_name=job_name_prefix + job_name,
             flags=flags,
             dependency=dependency,
             exclusive=exclusive,
@@ -142,7 +208,7 @@ def convert_ckpt(cfg, hydra_args="", dependency=None):
             cmd_str=cmd_str,
             num_nodes=nodes,
             ntasks_per_node=ntasks_per_node,
-            log_file=f"{base_results_dir}/{model_train_name}/{convert_name}/convert_log.txt",
+            log_file=f"{results_dir}/convert_log.txt",
         )
 
         submit_cmd = f"NGC_TASKS_PER_NODE={ntasks_per_node} {new_script_path}"
