9911c51676ce65da3bd239610ae06e80ef369375
ekmb <ebakhturina@nvidia.com>
2022-03-08 07:16:44 -0800

    " added for measure options, TV to whitelist"
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- nemo_text_processing/text_normalization/en/verbalizers/measure.py
+++ nemo_text_processing/text_normalization/en/verbalizers/measure.py
@@ -13,7 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo_text_processing.text_normalization.en.graph_utils import NEMO_NOT_QUOTE, GraphFst, delete_space, insert_space
+from nemo_text_processing.text_normalization.en.graph_utils import (
+    NEMO_NOT_QUOTE,
+    NEMO_SIGMA,
+    GraphFst,
+    delete_space,
+    insert_space,
+)
 
 try:
     import pynini
@@ -48,6 +54,9 @@ class MeasureFst(GraphFst):
             + pynutil.delete("\"")
             + delete_space
         )
+
+        if not deterministic:
+            unit |= pynini.compose(unit, pynini.cross(pynini.union("inch", "inches"), "\""))
 
         graph_decimal = (
             pynutil.delete("decimal {")
