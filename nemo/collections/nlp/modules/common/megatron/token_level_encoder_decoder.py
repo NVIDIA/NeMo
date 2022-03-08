213d6685d8adfb943ba763d1c7e1e4eb9c68fb62
Yi Dong <43824965+yidong72@users.noreply.github.com>
2022-03-08 11:41:19 -0500

    Add T5 model P-tuning support (#3768)
    * initial checking
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix a few bug
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix efficiency of decoding
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * everything is working
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix the style
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * address the comments
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix LGTM
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    Co-authored-by: Micha Livne <michalivne@users.noreply.github.com>

--- nemo/collections/nlp/modules/common/megatron/token_level_encoder_decoder.py
+++ nemo/collections/nlp/modules/common/megatron/token_level_encoder_decoder.py
@@ -219,7 +219,9 @@ class MegatronTokenLevelEncoderDecoderModule(MegatronModule):
         tokentype_ids=None,
         labels=None,
         enc_hidden_states=None,
+        enc_output_mask=None,
         output_enc_hidden_only=False,
+        enc_input=None,
     ):
         """
         Return value is per token / per dimension (i.e., non collapsed loss value)
@@ -227,8 +229,9 @@ class MegatronTokenLevelEncoderDecoderModule(MegatronModule):
         ret_dict = {}
 
         # encoder embeddings
-        enc_position_ids = build_position_ids(enc_input_ids)
-        enc_input = self.encoder_embedding(enc_input_ids, enc_position_ids, tokentype_ids=tokentype_ids)
+        if enc_input is None:
+            enc_position_ids = build_position_ids(enc_input_ids)
+            enc_input = self.encoder_embedding(enc_input_ids, enc_position_ids, tokentype_ids=tokentype_ids)
 
         if output_enc_hidden_only:
             enc_output, enc_output_mask = self.enc_dec_model.encode(
@@ -248,8 +251,8 @@ class MegatronTokenLevelEncoderDecoderModule(MegatronModule):
                     dec_attn_mask=dec_attn_mask,
                     enc_layer_past=None,
                     enc_get_key_value=False,
-                    enc_output=None,
-                    enc_output_mask=None,
+                    enc_output=enc_hidden_states,
+                    enc_output_mask=enc_output_mask,
                     dec_layer_past=None,
                     dec_get_key_value=False,
                 )
