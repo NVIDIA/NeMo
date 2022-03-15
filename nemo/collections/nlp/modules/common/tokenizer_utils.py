b29a924a8f063fa38f50324639f303ab20a99bc4
Micha Livne <mlivne@nvidia.com>
2022-03-14 17:15:32 -0700

    1. FIxed style.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/modules/common/tokenizer_utils.py
+++ nemo/collections/nlp/modules/common/tokenizer_utils.py
@@ -20,9 +20,9 @@ import nemo
 from nemo.collections.common.tokenizers.bytelevel_tokenizers import ByteLevelTokenizer
 from nemo.collections.common.tokenizers.char_tokenizer import CharTokenizer
 from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTokenizer
+from nemo.collections.common.tokenizers.regex_tokenizer import RegExTokenizer
 from nemo.collections.common.tokenizers.word_tokenizer import WordTokenizer
 from nemo.collections.common.tokenizers.youtokentome_tokenizer import YouTokenToMeTokenizer
-from nemo.collections.common.tokenizers.regex_tokenizer import RegExTokenizer
 from nemo.collections.nlp.modules.common.huggingface.huggingface_utils import get_huggingface_pretrained_lm_models_list
 from nemo.collections.nlp.modules.common.lm_utils import get_pretrained_lm_models_list
 from nemo.collections.nlp.parts.nlp_overrides import HAVE_APEX
