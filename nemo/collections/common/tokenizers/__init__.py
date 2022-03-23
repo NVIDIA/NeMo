9275515417ee10a97ce935052ffa79a2024a45a7
Micha Livne <mlivne@nvidia.com>
2022-03-23 22:38:38 +0200

    1. Updated init file.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/common/tokenizers/__init__.py
+++ nemo/collections/common/tokenizers/__init__.py
@@ -20,3 +20,4 @@ from nemo.collections.common.tokenizers.sentencepiece_tokenizer import SentenceP
 from nemo.collections.common.tokenizers.tokenizer_spec import TokenizerSpec
 from nemo.collections.common.tokenizers.word_tokenizer import WordTokenizer
 from nemo.collections.common.tokenizers.youtokentome_tokenizer import YouTokenToMeTokenizer
+from nemo.collections.common.tokenizers.regex_tokenizer import RegExTokenizer
