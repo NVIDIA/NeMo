fixed online normalization
Signed-off-by: Vahid <vnoroozi@nvidia.com>

examples/asr/speech_to_text_streaming_infer.py
@@ -48,6 +48,7 @@ def perform_streaming(asr_model, streaming_buffer, compare_vs_offline=False, deb
     batch_size = len(streaming_buffer.streams_length)
     if compare_vs_offline:
         with autocast():
+            processed_signal, processed_signal_length = streaming_buffer.get_all_audios()
             with torch.no_grad():
                 (
                     pred_out_offline,
@@ -56,8 +57,8 @@ def perform_streaming(asr_model, streaming_buffer, compare_vs_offline=False, deb
                     cache_last_time_next,
                     best_hyp,
                 ) = asr_model.stream_step(
-                    processed_signal=streaming_buffer.buffer,
-                    processed_signal_length=streaming_buffer.streams_length,
+                    processed_signal=processed_signal,
+                    processed_signal_length=processed_signal_length,
                     return_transcribtion=True,
                 )
         final_offline_tran = extract_transcribtions(transcribed_texts)
@@ -106,7 +107,7 @@ def perform_streaming(asr_model, streaming_buffer, compare_vs_offline=False, deb
         pred_out_stream_cat = torch.cat(pred_out_stream)
         pred_out_offline_cat = torch.cat(pred_out_offline)
         if pred_out_stream_cat.size() == pred_out_offline_cat.size():
-            diff_num = torch.sum(pred_out_stream_cat != pred_out_stream_cat).cpu().numpy()
+            diff_num = torch.sum(pred_out_stream_cat != pred_out_offline_cat).cpu().numpy()
             logging.info(
                 f"Found {diff_num} differences in the outputs of the model in streaming mode vs offline mode."
             )
@@ -163,6 +164,13 @@ def main():
         except:
             asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name=args.asr_model)
 
+    # asr_model.encoder.setup_streaming_params(
+    #     chunk_size=72*4,
+    #     shift_size=72*4,
+    #     cache_drop_size=0,
+    #     pre_encode_cache_size=8,
+    #     valid_out_len=18,
+    # )
     asr_model.encoder.setup_streaming_params(
         chunk_size=72,
         shift_size=72,
@@ -210,7 +218,7 @@ def main():
     asr_model = asr_model.to(args.device)
     asr_model.eval()
 
-    streaming_buffer = FramewiseStreamingAudioBuffer(model=asr_model, online_normalization=False)
+    streaming_buffer = FramewiseStreamingAudioBuffer(model=asr_model, online_normalization=args.online_normalization)
     if args.audio_file is not None:
         processed_signal, processed_signal_length, stream_id = streaming_buffer.append_audio_file(
             args.audio_file, stream_id=-1
@@ -238,6 +246,7 @@ def main():
                 sample['audio_filepath'], stream_id=-1
             )
             all_refs_text.append(sample["text"])
+            print(sample["audio_filepath"])
             if (sample_idx + 1) % args.batch_size == 0 or sample_idx == len(samples) - 1:
                 logging.info(
                     f"Starting to stream samples from {sample_idx - len(streaming_buffer) + 1} to {sample_idx}..."

nemo/collections/asr/modules/conformer_encoder.py
@@ -344,7 +344,7 @@ class ConformerEncoder(NeuralModule, Exportable, StreamingEncoderMixin):
         length = length.to(audio_signal.device)
         if max_audio_length > self.max_audio_length:
             self.set_max_audio_length(max_audio_length)
-
+        #print(audio_signal.shape)
         if length is None:
             length = audio_signal.new_full(
                 audio_signal.size(0), max_audio_length, dtype=torch.int32, device=self.seq_range.device
@@ -402,6 +402,8 @@ class ConformerEncoder(NeuralModule, Exportable, StreamingEncoderMixin):
 
         pad_mask = ~pad_mask
         att_mask = ~att_mask
+
+        #print(audio_signal.shape)
 
         for lth, layer in enumerate(self.layers):
             audio_signal = layer(

nemo/collections/asr/parts/submodules/conformer_modules.py
@@ -179,7 +179,7 @@ class ConformerConvolution(nn.Module):
         if conv_context_size is None or (
             conv_context_size[0] == (kernel_size - 1) // 2 and conv_context_size[1] == (kernel_size - 1) // 2
         ):
-            self.depthwise_conv = nn.Conv1d(
+            self.depthwise_conv = CausalConv1D(
                 in_channels=d_model,
                 out_channels=d_model,
                 kernel_size=kernel_size,

nemo/collections/asr/parts/utils/streaming_utils.py
@@ -1249,9 +1249,7 @@ class FramewiseStreamingAudioBuffer:
                 else:
                     zeros_pads = None
 
-                audio_chunk = torch.cat((cache_pre_encode, audio_chunk), dim=-1)
-                added_len = cache_pre_encode.size(-1)
-
+                chunk_lengths = self.streams_length - self.buffer_idx
                 if self.online_normalization:
                     audio_chunk, x_mean, x_std = normalize_batch(
                         x=audio_chunk,
@@ -1261,11 +1259,15 @@ class FramewiseStreamingAudioBuffer:
                     # print(x_mean)
                     # print(x_std)
 
-                if zeros_pads is not None:
-                    audio_chunk = torch.cat((zeros_pads, audio_chunk), dim=-1)
-                    added_len = zeros_pads.size(-1)
+                audio_chunk = torch.cat((cache_pre_encode, audio_chunk), dim=-1)
+                added_len = cache_pre_encode.size(-1)
 
-            chunk_lengths = self.streams_length - self.buffer_idx + added_len
+                if zeros_pads is not None:
+                    # TODO: check here when zero_pads is not None and added_len is alreasy non-zero
+                    audio_chunk = torch.cat((zeros_pads, audio_chunk), dim=-1)
+                    added_len += zeros_pads.size(-1)
+
+            chunk_lengths = chunk_lengths + added_len
             chunk_lengths = torch.clamp(chunk_lengths, min=0, max=audio_chunk.size(-1))
 
             self.buffer_idx += shift_size
@@ -1374,3 +1376,13 @@ class FramewiseStreamingAudioBuffer:
             input_signal=audio_signal, length=audio_signal_len
         )
         return processed_signal, processed_signal_length
+
+    def get_all_audios(self):
+        processed_signal = self.buffer
+        if self.online_normalization:
+            processed_signal, x_mean, x_std = normalize_batch(
+                x=processed_signal,
+                seq_len=torch.tensor(self.streams_length),
+                normalize_type=self.model_normalize_type,
+            )
+        return processed_signal, self.streams_length