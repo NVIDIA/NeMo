84a56061fdfba9f1c56cf4c3917c095ad116b6a3
Ramanathan Arunachalam <rarunachalam@nvidia.com>
2022-04-01 11:29:07 -0700

    Cleanup legacy conversion code
    Signed-off-by: Ramanathan Arunachalam <rarunachalam@nvidia.com>

--- scripts/legacy_megatronbert_nlp_to_current_version.py
+++ scripts/legacy_megatronbert_nlp_to_current_version.py
@@ -36,36 +36,28 @@ import torch
 from omegaconf import OmegaConf, open_dict
 
 from nemo.core import ModelPT
-from nemo.core.classes import Exportable, typecheck
 from nemo.core.config import TrainerConfig
-
-try:
-    from contextlib import nullcontext
-except ImportError:
-    # handle python < 3.7
-    from contextlib import suppress as nullcontext
 
 
 def get_args(argv):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description=f"Export NLP models pretrained with Megatron Bert on NeMo < 1.5.0 to current version ",
+        description=f"Convert NLP models pretrained with Megatron Bert on NeMo < 1.5.0 to current version ",
     )
     parser.add_argument("source", help="Source .nemo file")
     parser.add_argument("out", help="Location to write result to")
-    parser.add_argument("--autocast", action="store_true", help="Use autocast when exporting")
     parser.add_argument(
         "--megatron-checkpoint",
         type=str,
         help="Path of the MegatronBert nemo checkpoint converted from MegatronLM using megatron_lm_ckpt_to_nemo.py file (Not NLP model checkpoint)",
+        required=True,
     )
     parser.add_argument("--verbose", default=None, help="Verbose level for logging, numeric")
-    parser.add_argument("--device", default="cuda", help="Device to export for")
     args = parser.parse_args(argv)
     return args
 
 
-def nemo_export(argv):
+def nemo_convert(argv):
     args = get_args(argv)
     loglevel = logging.INFO
     # assuming loglevel is bound to the string value obtained from the
@@ -84,7 +76,7 @@ def nemo_export(argv):
     logging.basicConfig(level=loglevel, format='%(asctime)s [%(levelname)s] %(message)s')
     logging.info("Logging level set to {}".format(loglevel))
 
-    """Convert a .nemo saved model into .riva Riva input format."""
+    """Convert a .nemo saved model trained on NeMo < 1.5.0 into a nemo fie with current version."""
     nemo_in = args.source
     out = args.out
 
@@ -100,22 +92,26 @@ def nemo_export(argv):
     trainer = pl.Trainer(cfg_trainer)
 
     logging.info("Restoring NeMo model from '{}'".format(nemo_in))
-    autocast = nullcontext
     try:
-        with torch.inference_mode():
-            # If the megatron based NLP model was trained on NeMo < 1.5, then we need to update the lm_checkpoint on the model config
-            if args.megatron_checkpoint:
-                model_cfg = ModelPT.restore_from(
-                    restore_path=nemo_in, trainer=trainer, megatron_legacy=True, return_config=True
-                )
-                OmegaConf.set_struct(model_cfg, True)
-                with open_dict(model_cfg):
-                    model_cfg.language_model.lm_checkpoint = args.megatron_checkpoint
-                model = ModelPT.restore_from(
-                    restore_path=nemo_in, trainer=trainer, megatron_legacy=True, override_config_path=model_cfg,
-                )
-            else:
-                logging.error("Megatron checkpoint path must be provided if megatron_legacy is selected")
+        # If the megatron based NLP model was trained on NeMo < 1.5, then we need to update the lm_checkpoint on the model config
+        model_cfg = ModelPT.restore_from(
+            restore_path=nemo_in, trainer=trainer, megatron_legacy=True, return_config=True
+        )
+        OmegaConf.set_struct(model_cfg, True)
+        with open_dict(model_cfg):
+            model_cfg.language_model.lm_checkpoint = args.megatron_checkpoint
+
+        # Passing megatron_legacy=True maps the state dict from older versions (Nemo < 1.5) to the latest version
+        model = ModelPT.restore_from(
+            restore_path=nemo_in, trainer=trainer, megatron_legacy=True, override_config_path=model_cfg,
+        )
+        logging.info("Model {} restored from '{}'".format(model.cfg.target, nemo_in))
+
+        # Save the model
+        model.save_to(out)
+        logging.info("Successfully converted to {}".format(out))
+
+        del model
     except Exception as e:
         logging.error(
             "Failed to restore model from NeMo file : {}. Please make sure you have the latest NeMo package installed with [all] dependencies.".format(
@@ -124,32 +120,6 @@ def nemo_export(argv):
         )
         raise e
 
-    logging.info("Model {} restored from '{}'".format(model.cfg.target, nemo_in))
-
-    if not isinstance(model, Exportable):
-        logging.error("Your NeMo model class ({}) is not Exportable.".format(model.cfg.target))
-        sys.exit(1)
-    typecheck.set_typecheck_enabled(enabled=False)
-
-    try:
-        model = model.to(device=args.device)
-        model.eval()
-        if args.autocast:
-            autocast = torch.cuda.amp.autocast
-        with autocast(), torch.inference_mode():
-            model.save_to(out)
-    except Exception as e:
-        logging.error(
-            "Export failed. Please make sure your NeMo model class ({}) has working export() and that you have the latest NeMo package installed with [all] dependencies.".format(
-                model.cfg.target
-            )
-        )
-        raise e
-
-    logging.info("Successfully exported to {}".format(out))
-
-    del model
-
 
 if __name__ == '__main__':
-    nemo_export(sys.argv[1:])
+    nemo_convert(sys.argv[1:])
