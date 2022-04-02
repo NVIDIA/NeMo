087de544dc6212f993ad805d22683c5c6ccfc89f
Vahid Noroozi <VahidooX@users.noreply.github.com>
2022-04-02 00:04:57 -0700

    Adding RNN encoder for LSTM-Transducer and LSTM-CTC models (#3886)
    * added initial rnn encoder.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added rnn encoder and decoder.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added stackingdownsampling.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added stackingdownsampling.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added stackingdownsampling.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * fixed the bug for bidirectional.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * fixed the bug for bidirectional.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added skip_nan_grad.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added skip_nan_grad.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added rnn tpype.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * cleaned the configs.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * cleaned the configs.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added docs.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added docs.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added docs.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * changed proj_out to proj_size
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * changed proj_out to proj_size
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * changed proj_out to proj_size
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * set default to bpe.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * cleaned.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * addressed comments.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * CHANGED names.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * CHANGED names.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * added types.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * fixed proj_size in configs.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * fixed style.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * pull from main.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * replaced proj_size with pred_hidden.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * replaced proj_size with pred_hidden.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * replaced proj_size with pred_hidden.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    * replaced proj_size with pred_hidden.
    
    Signed-off-by: Vahid <vnoroozi@nvidia.com>
    
    Co-authored-by: Somshubra Majumdar <titu1994@gmail.com>

--- nemo/collections/asr/modules/__init__.py
+++ nemo/collections/asr/modules/__init__.py
@@ -32,4 +32,5 @@ from nemo.collections.asr.modules.conv_asr import (
 )
 from nemo.collections.asr.modules.graph_decoder import ViterbiDecoderWithGraph
 from nemo.collections.asr.modules.lstm_decoder import LSTMDecoder
+from nemo.collections.asr.modules.rnn_encoder import RNNEncoder
 from nemo.collections.asr.modules.rnnt import RNNTDecoder, RNNTJoint
