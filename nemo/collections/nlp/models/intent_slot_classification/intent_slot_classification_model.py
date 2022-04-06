de1eb4823be131796c9c1e2a5e16d3d8db59e619
ericharper <complex451@gmail.com>
2022-04-06 15:54:42 -0600

    manual sort imports
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/intent_slot_classification/intent_slot_classification_model.py
+++ nemo/collections/nlp/models/intent_slot_classification/intent_slot_classification_model.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import ntpath
 import os
 import pathlib
 from typing import Dict, List, Optional
