a157a8aeb62b8e6793a236e5cc304d8a6c0c550a
Yi Dong <doyend@gmail.com>
2022-04-06 23:11:47 +0000

    fix merge error
    Signed-off-by: Yi Dong <doyend@gmail.com>

--- nemo/collections/nlp/modules/common/lm_utils.py
+++ nemo/collections/nlp/modules/common/lm_utils.py
@@ -87,7 +87,7 @@ def get_lm_model(
             f"Both config_dict and config_file were found, defaulting to use config_file: {config_file} will be used."
         )
 
-    if cfg.tokenizer is not None and "megatron" in cfg.tokenizer.get("tokenizer_name", ""):
+    if cfg.tokenizer is not None and cfg.tokenizer.get("library", "") == 'megatron':
         import torch
 
         from nemo.collections.nlp.models.language_modeling.megatron_bert_model import MegatronBertModel
