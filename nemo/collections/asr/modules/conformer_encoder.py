59f5ce95fcc5a41f9983119c8022267a8d1133f9
Somshubra Majumdar <titu1994@gmail.com>
2022-04-06 19:19:59 -0700

    Update conformer encoder adapter docs
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/asr/modules/conformer_encoder.py
+++ nemo/collections/asr/modules/conformer_encoder.py
@@ -314,49 +314,6 @@ class ConformerEncoder(NeuralModule, Exportable):
 
 
 class ConformerEncoderAdapter(ConformerEncoder, AdapterModuleMixin):
-    """
-    The encoder for ASR model of Conformer.
-    Based on this paper:
-    'Conformer: Convolution-augmented Transformer for Speech Recognition' by Anmol Gulati et al.
-    https://arxiv.org/abs/2005.08100
-
-    Args:
-        feat_in (int): the size of feature channels
-        n_layers (int): number of layers of ConformerBlock
-        d_model (int): the hidden size of the model
-        feat_out (int): the size of the output features
-            Defaults to -1 (means feat_out is d_model)
-        subsampling (str): the method of subsampling, choices=['vggnet', 'striding']
-            Defaults to striding.
-        subsampling_factor (int): the subsampling factor which should be power of 2
-            Defaults to 4.
-        subsampling_conv_channels (int): the size of the convolutions in the subsampling module
-            Defaults to -1 which would set it to d_model.
-        ff_expansion_factor (int): the expansion factor in feed forward layers
-            Defaults to 4.
-        self_attention_model (str): type of the attention layer and positional encoding
-            'rel_pos': relative positional embedding and Transformer-XL
-            'abs_pos': absolute positional embedding and Transformer
-            default is rel_pos.
-        pos_emb_max_len (int): the maximum length of positional embeddings
-            Defaulst to 5000
-        n_heads (int): number of heads in multi-headed attention layers
-            Defaults to 4.
-        xscaling (bool): enables scaling the inputs to the multi-headed attention layers by sqrt(d_model)
-            Defaults to True.
-        untie_biases (bool): whether to not share (untie) the bias weights between layers of Transformer-XL
-            Defaults to True.
-        conv_kernel_size (int): the size of the convolutions in the convolutional modules
-            Defaults to 31.
-        conv_norm_type (str): the type of the normalization in the convolutional modules
-            Defaults to 'batch_norm'.
-        dropout (float): the dropout rate used in all layers except the attention layers
-            Defaults to 0.1.
-        dropout_emb (float): the dropout rate used for the positional embeddings
-            Defaults to 0.1.
-        dropout_att (float): the dropout rate used for the attention layer
-            Defaults to 0.0.
-    """
 
     # Higher level forwarding
     def add_adapter(self, name: str, cfg: dict):
