3617e1e0bdec72a6ec9e8ceb2fc328504baaf4c0
Evelina <10428420+ekmb@users.noreply.github.com>
2022-04-14 10:38:43 -0700

    docs fixes (#3987)
    * docs fixes
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * rename files in docs
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * docs improvement
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * arg renamed
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/run_predict.py
+++ nemo_text_processing/inverse_text_normalization/run_predict.py
@@ -56,7 +56,9 @@ def write_file(file_path: str, data: List[str]):
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--input", help="input file path", required=True, type=str)
-    parser.add_argument("--language", help="language", choices=['en', 'de'], default="en", type=str)
+    parser.add_argument(
+        "--lang", help="language", choices=['en', 'de', 'es', 'ru', 'fr', 'vi'], default="en", type=str
+    )
     parser.add_argument("--output", help="output file path", required=True, type=str)
     parser.add_argument("--verbose", help="print denormalization info. For debugging", action='store_true')
     return parser.parse_args()
@@ -65,7 +67,7 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     file_path = args.input
-    inverse_normalizer = InverseNormalizer(lang=args.language)
+    inverse_normalizer = InverseNormalizer(lang=args.lang)
 
     print("Loading data: " + file_path)
     data = load_file(file_path)
