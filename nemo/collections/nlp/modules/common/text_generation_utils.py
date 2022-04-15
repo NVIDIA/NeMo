LogProb calculation performance fix (#3984)
* performance fix for logprob computation

Signed-off-by: Yi Dong <doyend@gmail.com>

* fix redandant assign

Signed-off-by: Yi Dong <doyend@gmail.com>

* fix bug to add gather from TP workers

Signed-off-by: Yi Dong <doyend@gmail.com>

Co-authored-by: Yi Dong <doyend@gmail.com>

nemo/collections/nlp/modules/common/text_generation_utils.py
@@ -23,7 +23,7 @@ from nemo.collections.nlp.modules.common.transformer.text_generation import Leng
 from nemo.utils import AppState
 
 try:
-    from apex.transformer import parallel_state
+    from apex.transformer import parallel_state, tensor_parallel
     from apex.transformer.pipeline_parallel.schedules.fwd_bwd_pipelining_without_interleaving import (
         forward_backward_pipelining_without_interleaving,
     )
@@ -140,8 +140,8 @@ def get_computeprob_response(tokenizer, response, inputs):
         new_token_ids.append(new_token_id)
         new_tokens.append(response['tokens'][batch_id][:token_len])
         new_texts.append(new_text)
-        log_probs.append(response['logprob'][batch_id][: (token_len - 1)])
-        full_logprobs.append(response['full_logprob'][batch_id][: (token_len - 1)])
+        log_probs.append(response['logprob'][batch_id][:token_len])
+        full_logprobs.append(response['full_logprob'][batch_id][:token_len])
         offsets.append(response['offsets'][batch_id][:-1])
     compute_prob_response['sentences'] = new_texts
     compute_prob_response['tokens'] = new_tokens
@@ -516,10 +516,6 @@ def generate(
             else:
                 words = tokenizer.text_to_tokens(sentence)
                 resp_sentences_seg.append(words)
-        output_logits = output_logits.cpu().numpy().tolist()
-        if all_probs:
-            full_logits = full_logits.cpu().numpy().tolist()
-
         # offsets calculation
         all_offsets = []
         for item in resp_sentences_seg:
@@ -620,8 +616,8 @@ def sample_sequence_batch(
         # Generate enough tokens for the longest sequence
         maxlen = tokens_to_generate + context_lengths.max().item()
 
-        if maxlen > model.cfg.encoder_seq_length:
-            maxlen = model.cfg.encoder_seq_length
+        if maxlen > model.cfg.encoder_seq_length + 1:
+            maxlen = model.cfg.encoder_seq_length + 1
 
         lengths = torch.ones([batch_size]).long().cuda() * maxlen
 
@@ -662,6 +658,7 @@ def sample_sequence_batch(
 
             if parallel_state.is_pipeline_last_stage():
                 output = output[0]['logits'].float()
+                output = tensor_parallel.gather_from_tensor_model_parallel_region(output)
                 assert output is not None
                 output = output.float()
                 logits = output[:, -1].view(batch_size, -1).contiguous()
@@ -854,6 +851,7 @@ def tab_sample_sequence_batch(
 
             if parallel_state.is_pipeline_last_stage():
                 output = output[0]['logits'].float()
+                output = tensor_parallel.gather_from_tensor_model_parallel_region(output)
                 assert output is not None
                 output = output.float()
                 logits = output[:, -1].view(batch_size, -1).contiguous()