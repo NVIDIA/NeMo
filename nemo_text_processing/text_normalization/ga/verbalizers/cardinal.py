940257b8027eb11179c36bfc88c568d1ffbc2cc4
Jim O'Regan <joregan@kth.se>
2022-04-19 10:33:31 +0000

    add cardinal verbaliser
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/ga/verbalizers/cardinal.py
+++ nemo_text_processing/text_normalization/ga/verbalizers/cardinal.py
@@ -12,11 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from nemo_text_processing.text_normalization.en.graph_utils import NEMO_NOT_QUOTE, GraphFst
-from nemo_text_processing.text_normalization.es.graph_utils import (
-    add_cardinal_apocope_fem,
-    shift_cardinal_gender,
-    strip_cardinal_apocope,
-)
 
 try:
     import pynini
@@ -31,7 +26,7 @@ except (ModuleNotFoundError, ImportError):
 class CardinalFst(GraphFst):
     """
 	Finite state transducer for verbalizing cardinals
-		e.g. cardinal { integer: "dos" } -> "dos"
+		e.g. cardinal { integer: "a haon" } -> "a haon"
 
 	Args:
 		deterministic: if True will provide a single transduction option,
@@ -40,7 +35,7 @@ class CardinalFst(GraphFst):
 
     def __init__(self, deterministic: bool = True):
         super().__init__(name="cardinal", kind="verbalize", deterministic=deterministic)
-        optional_sign = pynini.closure(pynini.cross("negative: \"true\" ", "menos "), 0, 1)
+        optional_sign = pynini.closure(pynini.cross("negative: \"true\" ", "lúide "), 0, 1)
         self.optional_sign = optional_sign
 
         integer = pynini.closure(NEMO_NOT_QUOTE, 1)
@@ -48,20 +43,7 @@ class CardinalFst(GraphFst):
 
         integer = pynutil.delete("integer:") + self.integer
 
-        graph_masc = optional_sign + integer
-        graph_fem = shift_cardinal_gender(graph_masc)
-
-        self.graph_masc = pynini.optimize(graph_masc)
-        self.graph_fem = pynini.optimize(graph_fem)
-
-        # Adding adjustment for fem gender (choice of gender will be random)
-        graph = graph_masc | graph_fem
-
-        if not deterministic:
-            # For alternate renderings when apocope is omitted (i.e. cardinal stands alone)
-            graph |= strip_cardinal_apocope(graph_masc)
-            # "una" will drop to "un" in unique contexts
-            graph |= add_cardinal_apocope_fem(graph_fem)
+        graph = optional_sign + integer
 
         delete_tokens = self.delete_tokens(graph)
         self.fst = delete_tokens.optimize()
