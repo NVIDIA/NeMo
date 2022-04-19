10e687e3dae79d34b17f8cb09871a71cb02cf5eb
Subhankar Ghosh <subhankar2321@gmail.com>
2022-04-19 13:39:06 -0700

    Removing unused import
    Signed-off-by: Subhankar Ghosh <subhankar2321@gmail.com>

--- nemo/collections/tts/models/fastpitch.py
+++ nemo/collections/tts/models/fastpitch.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import contextlib
-from dataclasses import dataclass, is_dataclass
+from dataclasses import dataclass
 from typing import Optional
 
 import torch
