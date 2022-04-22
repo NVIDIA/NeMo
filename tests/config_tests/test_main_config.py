8264d4756f3856945bbe17a95e43172432571731
markelsanz14 <markelsanz14@gmail.com>
2022-04-21 21:14:31 -0700

    Main config test fixed.

--- tests/config_tests/test_main_config.py
+++ tests/config_tests/test_main_config.py
@@ -40,33 +40,22 @@ class TestConfig:
         wandb_api_key_file: null  # File where the w&B api key is stored. Key must be on the first line.
         nccl_topology_xml_file: null  # This file will be exported as "export NCCL_TOPO_FILE=${nccl_topology_xml_file}"
 
+        # GPU Mapping
+        numa_mapping:
+          enable: True  # Set to False to disable all mapping (performance will suffer).
+          mode: unique_contiguous  # One of: all, single, single_unique, unique_interleaved or unique_contiguous.
+          scope: node  # Either node or socket.
+          cores: all_logical  # Either all_logical or single_logical.
+          balanced: True  # Whether to assing an equal number of physical cores to each process.
+          min_cores: 1  # Minimum number of physical cores per process.
+          max_cores: 8  # Maximum number of physical cores per process. Can be null to use all available cores.
+
         # Do not modify below, use the values above instead.
         data_config: ${hydra:runtime.choices.data_preparation}
         training_config: ${hydra:runtime.choices.training}
         finetuning_config: ${hydra:runtime.choices.finetuning}
         evaluation_config: ${hydra:runtime.choices.evaluation}
         conversion_config: ${hydra:runtime.choices.conversion}
-
-        # GPU Mapping
-        dgxa100_gpu2core:
-          0: '48-51,176-179'
-          1: '60-63,188-191'
-          2: '16-19,144-147'
-          3: '28-31,156-159'
-          4: '112-115,240-243'
-          5: '124-127,252-255'
-          6: '80-83,208-211'
-          7: '92-95,220-223'
-
-        dgxa100_gpu2mem:
-          0: '3'
-          1: '3'
-          2: '1'
-          3: '1'
-          4: '7'
-          5: '7'
-          6: '5'
-          7: '5'
         """
         expected = OmegaConf.create(s)
         assert expected == conf, f"conf/config.yaml must be set to {expected} but it currently is {conf}."
