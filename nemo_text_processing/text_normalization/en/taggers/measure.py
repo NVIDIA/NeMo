efe36d0aebd54a6e99aef9a7c690c191b57bd135
Yang Zhang <yangzhang@nvidia.com>
2022-04-26 16:16:10 -0700

    update grammars
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo_text_processing/text_normalization/en/taggers/measure.py
+++ nemo_text_processing/text_normalization/en/taggers/measure.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
-# Copyright 2015 and onwards Google, Inc.
-#
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
