2c2c28066c840a3e1185d41eb6eebabaee3fde4a
yaoyu-33 <yaoyu.094@gmail.com>
2022-04-27 15:08:23 -0700

    Fix mT5 configs

--- bignlp/finetune_scripts/finetune_t5.py
+++ bignlp/finetune_scripts/finetune_t5.py
@@ -28,14 +28,9 @@ def main(cfg):
     cuda_visible_devices = numa_mapping(local_rank=rank, devices=devices, numa_cfg=numa_cfg)
 
     code_dir = "/opt/bignlp/NeMo"
-    if "mt5" in cfg.get("finetuning_config"):
-        code_path = (
-            f"{code_dir}/examples/nlp/language_modeling/megatron_t5_xnli.py"
-        )
-    else:
-        code_path = (
-            f"{code_dir}/examples/nlp/language_modeling/megatron_t5_glue.py"
-        )
+    code_path = (
+        f"{code_dir}/examples/nlp/language_modeling/megatron_t5_glue.py"
+    )
     
     cmd_prefix = generate_cmd_prefix(cfg, code_dir)
     # Write command to launch training.
