97a1a03a740e65486f0cf0b6055087180e79d395
Micha Livne <mlivne@nvidia.com>
2022-04-29 02:58:48 +0300

    1. Debugging.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/data/language_modeling/text_memmap_dataset.py
+++ nemo/collections/nlp/data/language_modeling/text_memmap_dataset.py
@@ -50,7 +50,7 @@ class TextMemMapDataset(Dataset):
 
     # FIXME: header_lines=0 by default
     def __init__(
-        self, dataset_paths, newline_int=10, header_lines=1, workers=None, tokenizer=None, sort_dataset_paths=True,
+        self, dataset_paths, newline_int=10, header_lines=0, workers=None, tokenizer=None, sort_dataset_paths=True,
     ):
         super().__init__()
 
