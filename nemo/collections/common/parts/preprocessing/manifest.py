038579bb322a223dc63266c2595bfc8d0a538469
Карпов Николай Вячеславович <karpnv@yandex.ru>
2022-05-02 00:50:21 +0300

    allow relative paths in manifest
    Signed-off-by: Карпов Николай Вячеславович <karpnv@yandex.ru>

--- nemo/collections/common/parts/preprocessing/manifest.py
+++ nemo/collections/common/parts/preprocessing/manifest.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import json
-from os.path import expanduser
+from os.path import expanduser, join, split, isabs
 from typing import Any, Callable, Dict, Iterator, List, Optional, Union
 
 
@@ -77,6 +77,7 @@ def item_iter(
 
 def __parse_item(line: str, manifest_file: str) -> Dict[str, Any]:
     item = json.loads(line)
+    manifest_path, _ = split(manifest_file)
 
     # Audio file
     if 'audio_filename' in item:
@@ -88,6 +89,8 @@ def __parse_item(line: str, manifest_file: str) -> Dict[str, Any]:
             f"Manifest file {manifest_file} has invalid json line structure: {line} without proper audio file key."
         )
     item['audio_file'] = expanduser(item['audio_file'])
+    if not isabs(item['audio_file']):
+        item['audio_file'] = join(manifest_path, item['audio_file'])
 
     # Duration.
     if 'duration' not in item:
