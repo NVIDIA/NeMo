01ed689621c02bc3da6b28f2cc28bf1a85a0b29d
Yang Zhang <yangzhang@nvidia.com>
2022-05-05 11:23:59 -0700

    sort imports
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo_text_processing/text_normalization/en/taggers/range.py
+++ nemo_text_processing/text_normalization/en/taggers/range.py
@@ -13,11 +13,7 @@
 # limitations under the License.
 
 
-from nemo_text_processing.text_normalization.en.graph_utils import (
-    NEMO_DIGIT,
-    GraphFst,
-    convert_space,
-)
+from nemo_text_processing.text_normalization.en.graph_utils import NEMO_DIGIT, GraphFst, convert_space
 
 try:
     import pynini
