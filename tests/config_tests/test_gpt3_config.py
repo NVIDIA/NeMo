c387b111a2122f105e143c69219e171bfb97966c
markelsanz14 <markelsanz14@gmail.com>
2022-05-13 11:03:34 -0700

    Update conf/search_config/gpt3/20b.yaml, tests/config_tests/test_gpt3_config.py

--- tests/config_tests/test_gpt3_config.py
+++ tests/config_tests/test_gpt3_config.py
@@ -109,7 +109,7 @@ class TestGPT3Config:
           logs: ${base_results_dir}/${search_config_value}
           tensor_parallel_sizes: [4, 8]
           pipeline_parallel_sizes: [1]
-          max_batch_sizes: [1, 2, 4, 8, 16, 32, 64, 256]
+          max_batch_sizes: [4, 8, 16, 32, 64, 128, 256]
         """
         expected = OmegaConf.create(s)
         assert (
