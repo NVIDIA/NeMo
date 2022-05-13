7d90c291c189fd9b89076f5d676ad13b7b245d62
markelsanz14 <markelsanz14@gmail.com>
2022-05-13 11:05:32 -0700

    Update conf/search_config/gpt3/40b.yaml, tests/config_tests/test_gpt3_config.py

--- tests/config_tests/test_gpt3_config.py
+++ tests/config_tests/test_gpt3_config.py
@@ -145,9 +145,9 @@ class TestGPT3Config:
           max_latency_ms: 5000
           top_n: 10
           logs: ${base_results_dir}/${search_config_value}
-          tensor_parallel_sizes: [4, 8]
-          pipeline_parallel_sizes: [1]
-          max_batch_sizes: [1, 2, 4, 8, 16, 32, 64, 256]
+          tensor_parallel_sizes: [8]
+          pipeline_parallel_sizes: [1, 2, 4]
+          max_batch_sizes: [8, 16, 32, 64, 128, 256]
         """
         expected = OmegaConf.create(s)
         assert (
