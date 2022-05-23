93d8a5d5a31f54a14bd5308c401c64ec7394617d
Virginia Adams <vadams@nvidia.com>
2022-05-23 22:12:48 +0000

    Removed redundent line of code
    Signed-off-by: Virginia Adams <vadams@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_prompt_learning_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_prompt_learning_model.py
@@ -84,7 +84,6 @@ class MegatronGPTPromptLearningModel(MegatronBaseModel, TextGeneration):
 
         # Load pretrained GPT model and tokenizer
         if cfg.get('language_model_path', None):
-            self.language_model_path = cfg.get('language_model_path')
             self.frozen_model = MegatronGPTModel.restore_from(
                 cfg.get('language_model_path'), trainer=trainer, save_restore_connector=NLPSaveRestoreConnector(),
             )
