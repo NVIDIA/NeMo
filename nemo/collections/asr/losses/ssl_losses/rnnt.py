d098fd4dc72d66baee6597a310d19ad9bb0eb7c4
Krishna Puvvada <kpuvvada@nvidia.com>
2022-05-25 10:00:31 -0700

    updating PretrainedModelInfo and benchmark sheet for ASR models
    Signed-off-by: Krishna Puvvada <kpuvvada@nvidia.com>

--- nemo/collections/asr/losses/ssl_losses/rnnt.py
+++ nemo/collections/asr/losses/ssl_losses/rnnt.py
@@ -16,7 +16,6 @@ from nemo.collections.asr.losses.rnnt import RNNTLoss
 from nemo.core import Loss, typecheck
 from nemo.core.neural_types import LabelsType, LengthsType, LogprobsType, LossType, NeuralType, SpectrogramType
 
-
 __all__ = ["RNNTLossForSSL"]
 
 
