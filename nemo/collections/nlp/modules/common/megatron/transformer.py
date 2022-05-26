e20c2cd88fcfa77ecffd680f47c7644ba689e159
Yi Dong <yidong@nvidia.com>
2022-05-26 21:46:34 +0000

    fix no attention issue
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/transformer.py
+++ nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -588,6 +588,13 @@ class ParallelAttention(MegatronModule):
         # attention scores and attention mask [b, np, sq, sk]
         attention_probs = self.scale_mask_softmax(attention_scores, attention_mask)
 
+        # Currently if all key sequences are masked, attention will be uniformly distributed.
+        # E.g. attention_mask last dimension all True, attention prob is 1 / last_dim
+        # # The correct behavior should be paying zero attention to them
+        all_k_masked = attention_mask.all(axis=-1)
+        zero_attention_mask = (1.0 - all_k_masked.float())[:, :, :, None]
+        attention_probs = attention_probs * zero_attention_mask
+
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
         with tensor_parallel.random.get_cuda_rng_tracker().fork():
