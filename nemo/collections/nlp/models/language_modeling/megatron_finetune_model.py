88ad5af093b413dd39f16f7f9536cd2759f9be78
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2022-05-26 11:11:09 -0700

    Style
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- nemo/collections/nlp/models/language_modeling/megatron_finetune_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_finetune_model.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import torch
 import json
+
+import torch
 from omegaconf import DictConfig, ListConfig
 from pytorch_lightning.trainer.trainer import Trainer
 
