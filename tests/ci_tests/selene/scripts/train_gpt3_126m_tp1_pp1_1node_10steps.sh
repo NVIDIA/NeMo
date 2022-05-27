7109011ad393f977153ec9e7d7c4de03f0a59157
markelsanz14 <markelsanz14@gmail.com>
2022-05-27 14:39:46 -0700

    Update tests/ci_tests/selene/scripts/train_gpt3_126m_tp1_pp1_1node_10steps.sh, tests/ci_tests/selene/scripts/train_gpt3_126m_tp2_pp1_1node_10steps.sh

--- tests/ci_tests/selene/scripts/train_gpt3_126m_tp1_pp1_1node_10steps.sh
+++ tests/ci_tests/selene/scripts/train_gpt3_126m_tp1_pp1_1node_10steps.sh
@@ -23,4 +23,4 @@ HYDRA_FULL_ERROR=1 python3 main.py \
     training.trainer.limit_val_batches=10 \
     training.model.tensor_model_parallel_size=1 \
     training.model.pipeline_model_parallel_size=1 \
-    training.model.data.data_prefix=[1.0,/lustre/fsw/joc/yuya/bignlp/bignlp-scripts_ci/data/my-gpt3_00_text_document]
+    training.model.data.data_prefix=[1.0,/lustre/fsw/joc/big_nlp/gpt3/prepare_dataset/the_pile/train/my-gpt3_00_text_document]
