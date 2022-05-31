92488628b98e36cd27afcc8b911afdc8d3cde6dd
Zhilin Wang <zhilinw@nvidia.com>
2022-05-31 11:03:17 -0700

    remove unused imports
    Signed-off-by: Zhilin Wang <zhilinw@nvidia.com>

--- nemo/collections/common/losses/spanning_loss.py
+++ nemo/collections/common/losses/spanning_loss.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
-
-import torch
 from torch import nn
 
 from nemo.core.classes import Loss, typecheck
