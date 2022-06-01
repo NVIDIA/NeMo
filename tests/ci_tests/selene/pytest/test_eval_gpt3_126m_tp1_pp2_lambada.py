f8f74c71876acdb45410ce1ad13e6d361914b157
markelsanz14 <markelsanz14@gmail.com>
2022-06-01 15:24:07 -0700

    Update tests/ci_tests/selene/pytest/test_eval_gpt3_126m_tp1_pp1_lambada.py, tests/ci_tests/selene/pytest/test_eval_gpt3_126m_tp1_pp2_lambada.py, tests/ci_tests/selene/pytest/test_eval_gpt3_126m_tp2_pp1_lambada.py, tests/ci_tests/selene/pytest/test_eval_gpt3_126m_tp2_pp2_lambada.py

--- tests/ci_tests/selene/pytest/test_eval_gpt3_126m_tp1_pp2_lambada.py
+++ tests/ci_tests/selene/pytest/test_eval_gpt3_126m_tp1_pp2_lambada.py
@@ -22,6 +22,8 @@ class TestCIGPT126m:
     def test_ci_eval_gpt3_126m_tp1_pp2_lambada(self):
         p = Path(CI_JOB_RESULTS)
         files = list(p.glob('eval_gpt3_126m_tp1_pp2_lambada*/metrics.json'))
+        print(p)
+        print(files)
         assert len(files) == 1, f"Only one metrics.json file should be present inside {CI_JOB_RESULTS}"
 
         metrics_file = files[0]
@@ -29,7 +31,7 @@ class TestCIGPT126m:
 
         with open(metrics_file) as json_file:
             metrics = json.load(json_file)["lambada"]
-            expected_lambada = expected["lambada"]
+            expected_lambada = self.expected["lambada"]
             assert metrics["ppl"] == pytest.approx(expected=expected_lambada["ppl"], rel=self.margin), f"Lambada PPL should be {expected_lambada['ppl']} but it is {metrics['ppl']}"
             assert metrics["ppl_stderr"] == pytest.approx(expected=expected_lambada["ppl_stderr"], rel=self.margin), f"Lambada PPL StdErr should be {expected_lambada['ppl_stderr']} but it is {metrics['ppl_stderr']}"
             assert metrics["acc"] == pytest.approx(expected=expected_lambada["acc"], rel=self.margin), f"Lambada Accuracy should be {expected_lambada['acc']} but it is {metrics['acc']}"
