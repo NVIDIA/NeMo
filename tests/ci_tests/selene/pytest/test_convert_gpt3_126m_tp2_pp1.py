e5d706ae86a5d63f10bf6a6001695cf13c77f16d
markelsanz14 <markelsanz14@gmail.com>
2022-06-01 21:41:57 -0700

    Update .gitlab-ci.yml, tests/ci_tests/selene/scripts/convert_gpt3_126m_tp1_pp1.sh, tests/ci_tests/selene/scripts/convert_gpt3_126m_tp2_pp1.sh, tests/ci_tests/selene/scripts/convert_gpt3_126m_tp1_pp2.sh, tests/ci_tests/selene/scripts/convert_gpt3_126m_tp2_pp2.sh, tests/ci_tests/selene/pytest/test_convert_gpt3_126m_tp1_pp1.py, tests/ci_tests/selene/pytest/test_convert_gpt3_126m_tp2_pp1.py, tests/ci_tests/selene/pytest/test_convert_gpt3_126m_tp1_pp2.py, tests/ci_tests/selene/pytest/test_convert_gpt3_126m_tp2_pp2.py

---
+++ tests/ci_tests/selene/pytest/test_convert_gpt3_126m_tp2_pp1.py
@@ -0,0 +1,15 @@
+import os
+import json
+from pathlib import Path
+
+import pytest
+
+
+CI_JOB_RESULTS = os.environ.get("RESULTS_DIR")
+
+class TestCIGPT126m:
+
+    def test_ci_convert_gpt3_126m_tp2_pp1(self):
+        pass
+
+
