d1013c02088294f71d551611af6d028a311546e0
markelsanz14 <markelsanz14@gmail.com>
2022-06-02 21:36:31 -0700

    Update tests/ci_tests/selene/scripts/data_prep/gpt3/the_pile_download_shard0.sh, tests/ci_tests/selene/pytest/data_prep/gpt3/test_the_pile_download_shard0.py

--- tests/ci_tests/selene/pytest/data_prep/gpt3/test_the_pile_download_shard0.py
+++ tests/ci_tests/selene/pytest/data_prep/gpt3/test_the_pile_download_shard0.py
@@ -1,10 +1,10 @@
 import os
 
 
-BASE_RESULTS_DIR = os.environ.get("BASE_RESULTS_DIR")
+BASE_RESULTS_DIR = os.environ.get("RESULTS_DIR")
 
 class TestCIGPT126m:
 
     def test_ci_convert_gpt3_126m_tp1_pp1(self):
-        ckpt_path = os.path.join(BASE_RESULTS_DIR, "data/00.jsonl.zst")
+        ckpt_path = os.path.join(BASE_RESULTS_DIR, "data/00.jsonl")
         assert os.path.exists(ckpt_path), f"File not found: {ckpt_path}"
