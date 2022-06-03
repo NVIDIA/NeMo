a76bb588c9842ce40ddf59845fea10b1d2c24e2e
markelsanz14 <markelsanz14@gmail.com>
2022-06-03 13:52:43 -0700

    Update tests/ci_tests/selene/scripts/data_prep/gpt3/the_pile_prepare_shard00.sh, tests/ci_tests/selene/pytest/data_prep/gpt3/test_the_pile_prepare_shard00.py, .gitlab-ci.yml, tests/ci_tests/selene/scripts/data_prep/t5/the_pile_prepare_shard00.sh, tests/ci_tests/selene/pytest/data_prep/t5/test_the_pile_prepare_shard00.py

---
+++ tests/ci_tests/selene/pytest/data_prep/gpt3/test_the_pile_prepare_shard00.py
@@ -0,0 +1,12 @@
+import os
+
+
+BASE_RESULTS_DIR = os.environ.get("RESULTS_DIR")
+
+class TestCIGPT126m:
+
+    def test_ci_data_prep_gpt3_the_pile_shard00(self):
+        idx_path = os.path.join(BASE_RESULTS_DIR, "data/my-gpt3_00_text_document.idx")
+        bin_path = os.path.join(BASE_RESULTS_DIR, "data/my-gpt3_00_text_document.bin")
+        assert os.path.exists(idx_path), f"File not found: {idx_path}"
+        assert os.path.exists(bin_path), f"File not found: {bin_path}"
