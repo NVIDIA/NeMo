679075da801a6b3e32ae824e691c576ddb7311c2
Yang Zhang <yangzhang@nvidia.com>
2022-06-07 14:50:38 -0700

    fix
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo_text_processing/text_normalization/ru/verbalizers/verbalize_final.py
+++ nemo_text_processing/text_normalization/ru/verbalizers/verbalize_final.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 
 import pynini
 from nemo_text_processing.text_normalization.en.graph_utils import (
