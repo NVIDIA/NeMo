624dc65c43aaea4376d2739977e10f4664730f27
markelsanz14 <markelsanz14@gmail.com>
2022-06-07 12:03:48 -0700

    Update tests/ci_tests/selene/scripts/train/gpt3/5b_tp2_pp1_1node_100steps.sh, .gitlab-ci.yml, tests/ci_tests/selene/pytest/train/gpt3/test_5b_tp2_pp1_1node_100steps.py, tests/ci_tests/selene/scripts/train/gpt3/5b_tp2_pp2_2node_100steps.sh, tests/ci_tests/selene/pytest/train/gpt3/test_5b_tp2_pp2_2node_100steps.py, tests/ci_tests/selene/scripts/train/gpt3/20b_tp8_pp2_2node_100steps.sh, conf/training/gpt3/20b.yaml, tests/ci_tests/selene/pytest/train/gpt3/test_20b_tp8_pp2_2node_100steps.py, tests/ci_tests/selene/scripts/train/gpt3/175b_tp8_pp16_16node_50steps.sh, tests/ci_tests/selene/pytest/train/gpt3/test_175b_tp8_pp16_16node_50steps.py, tests/ci_tests/selene/scripts/train/gpt3/40b_tp8_pp4_4node_100steps.sh, tests/ci_tests/selene/pytest/train/gpt3/test_40b_tp8_pp4_4node_100steps.py

---
+++ tests/ci_tests/selene/pytest/train/gpt3/test_40b_tp8_pp4_4node_100steps.py
@@ -0,0 +1,95 @@
+import os
+import json
+
+import pytest
+
+from tensorboard.backend.event_processing import event_accumulator
+
+
+CI_JOB_RESULTS = os.environ.get("RESULTS_DIR")
+
+def _read_tb_logs_as_list(path, summary_name):
+    """Reads a TensorBoard Events file from the input path, and returns the 
+    summary specified as input as a list.
+
+    Arguments:
+        path: str, path to the dir where the events file is located.
+        summary_name: str, name of the summary to read from the TB logs.
+    Output:
+        summary_list: list, the values in the read summary list, formatted as a list.
+    """
+    files = os.listdir(path)
+    for f in files:
+        if f[:6] == "events":
+            event_file = os.path.join(path, f)
+            ea = event_accumulator.EventAccumulator(event_file)
+            ea.Reload()
+            summary = ea.Scalars(summary_name)
+            summary_list = [round(x.value, 5) for x in summary]
+            print(summary_list)
+            return summary_list
+    raise FileNotFoundError(f"File not found matching: {path}/events*")
+
+
+class TestCIGPT126m:
+
+    margin_loss, margin_time = 0.05, 0.1
+    expected_json = \
+    r"""
+    {"reduced_train_loss": {"start_step": 0, "end_step": 100, "step_interval": 5, "values": [10.97303, 10.53661, 9.64785, 9.3502, 8.90369, 8.96251, 8.70788, 8.78277, 8.38711, 8.22158, 7.99938, 7.9085, 7.79335, 7.57561, 7.37079, 7.10037, 7.16389, 6.97836, 6.87647, 6.79842]}, "val_loss": {"start_step": 0, "end_step": 5, "step_interval": 1, "values": [8.40292, 7.87163, 7.22711, 6.64085, 6.27052]}, "train_step_timing_avg": 0.642}
+    """
+    expected = json.loads(expected_json)
+
+    def test_ci_gpt3_126m_train_loss_deterministic(self):
+        # Expected training loss curve at different global steps.
+        expected = self.expected["reduced_train_loss"]
+        expected_vals = expected["values"]
+        train_loss_list = _read_tb_logs_as_list(CI_JOB_RESULTS, "reduced_train_loss")
+
+        assert train_loss_list is not None, f"No TensorBoard events file was found in the logs."
+        assert len(train_loss_list) == 100, f"The events file must have 100 training loss values, one per training iteration."
+        for i, step in enumerate(range(expected["start_step"], expected["end_step"], expected["step_interval"])):
+            assert train_loss_list[step] == expected_vals[i], f"The loss at step {step} should be {expected_vals[i]} but it is {train_loss_list[step]}."
+
+    def test_ci_gpt3_126m_train_loss_approx(self):
+        # Expected training loss curve at different global steps.
+        expected = self.expected["reduced_train_loss"]
+        expected_vals = expected["values"]
+        train_loss_list = _read_tb_logs_as_list(CI_JOB_RESULTS, "reduced_train_loss")
+
+        assert train_loss_list is not None, f"No TensorBoard events file was found in the logs."
+        assert len(train_loss_list) == 100, f"The events file must have 100 training loss values, one per training iteration."
+        for i, step in enumerate(range(expected["start_step"], expected["end_step"], expected["step_interval"])):
+            assert train_loss_list[step] == pytest.approx(expected=expected_vals[i], rel=self.margin_loss), f"The loss at step {step} should be approximately {expected_vals[i]} but it is {train_loss_list[step]}."
+
+    def test_ci_gpt3_126m_val_loss_deterministic(self):
+        # Expected validation loss curve at different global steps.
+        expected = self.expected["val_loss"]
+        expected_vals = expected["values"]
+        val_loss_list = _read_tb_logs_as_list(CI_JOB_RESULTS, "val_loss")
+
+        assert val_loss_list is not None, f"No TensorBoard events file was found in the logs."
+        assert len(val_loss_list) == 5, f"The events file must have 5 validation loss values."
+        for i, step in enumerate(range(expected["start_step"], expected["end_step"], expected["step_interval"])):
+            assert val_loss_list[step] == expected_vals[i], f"The loss at step {step} should be {expected_vals[i]} but it is {val_loss_list[step]}."
+
+    def test_ci_gpt3_126m_val_loss_approx(self):
+        # Expected validation loss curve at different global steps.
+        expected = self.expected["val_loss"]
+        expected_vals = expected["values"]
+        val_loss_list = _read_tb_logs_as_list(CI_JOB_RESULTS, "val_loss")
+
+        assert val_loss_list is not None, f"No TensorBoard events file was found in the logs."
+        assert len(val_loss_list) == 5, f"The events file must have 5 validation loss values."
+        for i, step in enumerate(range(expected["start_step"], expected["end_step"], expected["step_interval"])):
+            assert val_loss_list[step] == pytest.approx(expected=expected_vals[i], rel=self.margin_loss), f"The loss at step {step} should be approximately {expected_vals[i]} but it is {val_loss_list[step]}."
+
+    def test_ci_gpt3_126m_train_step_timing_1node(self):
+        # Expected average training time per global step.
+        expected_avg = self.expected["train_step_timing_avg"]
+        train_time_list = _read_tb_logs_as_list(CI_JOB_RESULTS, "train_step_timing")
+        train_time_list = train_time_list[len(train_time_list)//2:] # Discard the first half..
+        train_time_avg = sum(train_time_list) / len(train_time_list)
+
+        assert train_time_list is not None, f"No TensorBoard events file was found in the logs."
+        assert train_time_avg == pytest.approx(expected=expected_avg, rel=self.margin_time), f"The time per global step must be approximately {expected_avg} but it is {train_time_avg}."
