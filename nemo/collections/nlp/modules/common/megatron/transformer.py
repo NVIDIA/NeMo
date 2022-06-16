0c12ed637ac4e2cc902b5b37d9033086ca273cd5
Xiaowei Ren <xren@nvidia.com>
2022-06-15 18:03:05 -0700

    delete redundant code
    Signed-off-by: Xiaowei Ren <xren@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/transformer.py
+++ nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -1254,9 +1254,6 @@ class ParallelTransformerLayer_(MegatronModule):
                 transformer_block_type=self.transformer_block_type, position_after='attention'
             )
 
-            #if attention_bias is not None:
-            #    attention_bias = attention_bias.expand_as(residual)
-
             layernorm_input = bias_dropout_add_func(attention_output, attention_bias, residual, self.hidden_dropout)
 
             # Post-LN normalization after residual
@@ -1311,9 +1308,6 @@ class ParallelTransformerLayer_(MegatronModule):
 
             residual = layernorm_input
 
-            #if attention_bias is not None:
-            #    attention_bias = attention_bias.expand_as(residual)
-
             bias_dropout_add_func = self._get_bias_droput_add_func(
                 transformer_block_type=self.transformer_block_type, position_after='attention'
             )
@@ -1327,9 +1321,6 @@ class ParallelTransformerLayer_(MegatronModule):
         mlp_output, mlp_bias = self.mlp(normalization_output)
 
         residual = layernorm_input
-
-        #if mlp_bias is not None:
-        #    mlp_bias = mlp_bias.expand_as(residual)
 
         bias_dropout_add_func = self._get_bias_droput_add_func(
             transformer_block_type=self.transformer_block_type, position_after='mlp'
