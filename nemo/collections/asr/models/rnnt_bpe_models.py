250ea646117c971ed8c9f8fd4492e5edadb511a6
Somshubra Majumdar <titu1994@gmail.com>
2022-06-15 21:32:40 -0700

    Fix link
    Signed-off-by: smajumdar <smajumdar@nvidia.com>

--- nemo/collections/asr/models/rnnt_bpe_models.py
+++ nemo/collections/asr/models/rnnt_bpe_models.py
@@ -157,7 +157,7 @@ class EncDecRNNTBPEModel(EncDecRNNTModel, ASRBPEMixin):
 
         model = PretrainedModelInfo(
             pretrained_model_name="stt_fr_conformer_transducer_large",
-            description="For details about this model, please visit https://ngc.nvidia.com/catalog/models/nvidia:nemo:stt_fr_conformer_transducer_large",
+            description="For details about this model, please visit https://ngc.nvidia.com/catalog/modelswget 'https://api.ngc.nvidia.com/v2/models/nvidia/nemo/stt_fr_conformer_transducer_large/versions/1.5/files/stt_fr_conformer_transducer_large.nemo'/nvidia:nemo:stt_fr_conformer_transducer_large",
             location="https://api.ngc.nvidia.com/v2/models/nvidia/nemo/stt_de_conformer_transducer_large/versions/1.5/files/stt_fr_conformer_transducer_large.nemo",
         )
         results.append(model)
