1d2c492f11f083537f0dd593fbd016adb47e0ea0
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2022-06-17 12:27:23 -0700

    Add on epoch start reconfiguration
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- nemo/collections/nlp/models/machine_translation/megatron_nmt_model.py
+++ nemo/collections/nlp/models/machine_translation/megatron_nmt_model.py
@@ -28,14 +28,16 @@ from nemo.collections.nlp.data.common.sequence_to_sequence_dataset import (
     TextMemmapSequenceToSequenceDataset,
 )
 from nemo.collections.nlp.data.language_modeling.megatron.blendable_dataset import BlendableDataset
-from nemo.collections.nlp.data.language_modeling.megatron.megatron_batch_samplers import MegatronPretrainingBatchSampler
+from nemo.collections.nlp.data.language_modeling.megatron.megatron_batch_samplers import (
+    MegatronPretrainingBatchSampler,
+    MegatronPretrainingRandomBatchSampler,
+)
 from nemo.collections.nlp.models.language_modeling.megatron_lm_encoder_decoder_model import (
     MegatronLMEncoderDecoderModel,
 )
 from nemo.collections.nlp.models.machine_translation.mt_enc_dec_model import MTEncDecModel
 from nemo.collections.nlp.parts.nlp_overrides import GlobalBatchDataFetcher
 from nemo.collections.nlp.parts.utils_funcs import get_last_rank
-from nemo.collections.nlp.data.language_modeling.megatron .base_dataset_utils import get_datasets_weights_and_num_samples
 from nemo.utils import AppState, logging, timers
 
 try:
@@ -159,9 +161,8 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
             ) = MTEncDecModel.setup_multilingual_ids_and_processors(
                 src_language=self.src_language,
                 tgt_language=self.tgt_language,
-                encoder_tokenizer=self.encoder_tokenizer,  # Multilingual training requires shared tokenizers.
-                encoder_tokenizer_library=self.encoder_tokenizer_library,
-                decoder_tokenizer_library=self.decoder_tokenizer_library,
+                tokenizer=self.encoder_tokenizer,  # Multilingual training requires shared tokenizers.
+                tokenizer_library=self.encoder_tokenizer_library,
             )
         else:
             # After this call, the model will have  self.source_processor and self.target_processor objects
@@ -572,19 +573,8 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
                     f"concat_sampling_probabilities must be of the same size as src_file_name and tgt_file_name. Provided size {len(cfg.concat_sampling_probabilities)}, number of datasets {len(cfg.src_file_name)}"
                 )
 
-            data_prefix = []
-            for fname, weight in zip(cfg.src_file_name, cfg.concat_sampling_probabilities):
-                data_prefix.append(weight)
-                data_prefix.append(fname)
-
-            total_training_examples = self.trainer.max_steps * self._cfg.train_ds.global_batch_size
-            output = get_datasets_weights_and_num_samples(
-                data_prefix, [total_training_examples] # Typically expects a list of size 3 for train, valid test num samples, but we want train only.
-            )
-            num_samples_per_dataset = output[2]
-            num_samples_per_dataset = [x[0] for x in num_samples_per_dataset] # Again, this is because we want training size only.
             datasets = []
-            for src_file, tgt_fille, num_samples in zip(cfg.src_file_name, cfg.tgt_file_name, num_samples_per_dataset):
+            for src_file, tgt_fille in zip(cfg.src_file_name, cfg.tgt_file_name):
                 if cfg.dataset_type == 'bin_memmap':
                     dataset = BinarizedMemmapSequenceToSequenceDataset(
                         src_dataset_prefix=src_file,
@@ -593,8 +583,10 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
                         tgt_tokenizer=self.decoder_tokenizer,
                         max_src_seq_length=cfg.max_seq_length,
                         max_tgt_seq_length=cfg.max_seq_length,
-                        max_num_samples=num_samples,
-                        seed=self._cfg.seed
+                        start_index=0,
+                        end_index=None,
+                        data_impl="mmap",
+                        skip_warmup=True,
                     )
                 elif cfg.dataset_type == 'text_memmap':
                     dataset = TextMemmapSequenceToSequenceDataset(
@@ -604,11 +596,9 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
                         tgt_tokenizer=self.decoder_tokenizer,
                         max_src_seq_length=cfg.max_seq_length,
                         max_tgt_seq_length=cfg.max_seq_length,
-                        max_num_samples=num_samples,
-                        seed=self._cfg.seed
                     )
                 datasets.append(dataset)
-            dataset = BlendableDataset(datasets=datasets, weights=cfg.concat_sampling_probabilities, size=sum(num_samples_per_dataset))
+            dataset = BlendableDataset(datasets=datasets, weights=cfg.concat_sampling_probabilities)
         else:
             if cfg.dataset_type == 'bin_memmap':
                 dataset = BinarizedMemmapSequenceToSequenceDataset(
@@ -618,7 +608,10 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
                     tgt_tokenizer=self.decoder_tokenizer,
                     max_src_seq_length=cfg.max_seq_length,
                     max_tgt_seq_length=cfg.max_seq_length,
-                    seed=self._cfg.seed
+                    start_index=0,
+                    end_index=None,
+                    data_impl="mmap",
+                    skip_warmup=True,
                 )
             elif cfg.dataset_type == 'text_memmap':
                 dataset = TextMemmapSequenceToSequenceDataset(
@@ -628,7 +621,6 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
                     tgt_tokenizer=self.decoder_tokenizer,
                     max_src_seq_length=cfg.max_seq_length,
                     max_tgt_seq_length=cfg.max_seq_length,
-                    seed=self._cfg.seed
                 )
         return dataset
 
@@ -656,6 +648,16 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
                 micro_batch_size=self._cfg.train_ds.micro_batch_size,
                 data_parallel_size=parallel_state.get_data_parallel_world_size(),
             )
+
+    def on_validation_epoch_start(self):
+        app_state = AppState()
+        _reconfigure_microbatch_calculator(
+            rank=app_state.global_rank,
+            rampup_batch_size=None,
+            global_batch_size=parallel_state.get_data_parallel_world_size(),
+            micro_batch_size=1,
+            data_parallel_size=parallel_state.get_data_parallel_world_size(),
+        )
 
     @torch.no_grad()
     def translate(
