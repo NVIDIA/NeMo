e3ddb4c66c328ef98f764c3518725f536d64a607
yaoyu-33 <yaoyu.094@gmail.com>
2022-06-17 09:32:21 -0700

    Fix prompt metrics

--- tests/ci_tests/selene/pytest/eval/gpt3/test_prompt_126m_tp1_pp1_squad.py
+++ tests/ci_tests/selene/pytest/eval/gpt3/test_prompt_126m_tp1_pp1_squad.py
@@ -11,7 +11,7 @@ class TestCIGPT126m:
 
     margin = 0.05
     expected = {
-        "lambada": {
+        "prompt": {
             "ppl": 896469,
             "ppl_stderr": 66318,
             "acc": 0.0,
@@ -19,7 +19,7 @@ class TestCIGPT126m:
         },
     }
 
-    def test_ci_eval_gpt3_126m_tp1_pp1_lambada(self):
+    def test_ci_eval_gpt3_126m_tp1_pp1_prompt(self):
         p = Path(CI_JOB_RESULTS)
         files = list(p.glob('eval_gpt3_prompt_126m_tp1_pp1_squad*/metrics.json'))
         print(p)
@@ -32,8 +32,8 @@ class TestCIGPT126m:
         with open(metrics_file) as json_file:
             metrics = json.load(json_file)["prompt"]
             print(metrics)
-            expected_lambada = self.expected["prompt"]
-            assert metrics["ppl"] == pytest.approx(expected=expected_lambada["ppl"], rel=self.margin), f"Lambada PPL should be {expected_lambada['ppl']} but it is {metrics['ppl']}"
-            assert metrics["ppl_stderr"] == pytest.approx(expected=expected_lambada["ppl_stderr"], rel=self.margin), f"Lambada PPL StdErr should be {expected_lambada['ppl_stderr']} but it is {metrics['ppl_stderr']}"
-            assert metrics["acc"] == pytest.approx(expected=expected_lambada["acc"], rel=self.margin), f"Lambada Accuracy should be {expected_lambada['acc']} but it is {metrics['acc']}"
-            assert metrics["acc_stderr"] == pytest.approx(expected=expected_lambada["acc_stderr"], rel=self.margin), f"Lambada Accuracy StdErr should be {expected_lambada['acc_stderr']} but it is {metrics['acc_stderr']}"
+            expected_prompt = self.expected["prompt"]
+            assert metrics["ppl"] == pytest.approx(expected=expected_prompt["ppl"], rel=self.margin), f"Lambada PPL should be {expected_prompt['ppl']} but it is {metrics['ppl']}"
+            assert metrics["ppl_stderr"] == pytest.approx(expected=expected_prompt["ppl_stderr"], rel=self.margin), f"Lambada PPL StdErr should be {expected_prompt['ppl_stderr']} but it is {metrics['ppl_stderr']}"
+            assert metrics["acc"] == pytest.approx(expected=expected_prompt["acc"], rel=self.margin), f"Lambada Accuracy should be {expected_prompt['acc']} but it is {metrics['acc']}"
+            assert metrics["acc_stderr"] == pytest.approx(expected=expected_prompt["acc_stderr"], rel=self.margin), f"Lambada Accuracy StdErr should be {expected_prompt['acc_stderr']} but it is {metrics['acc_stderr']}"
