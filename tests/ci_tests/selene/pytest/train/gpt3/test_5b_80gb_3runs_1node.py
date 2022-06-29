62b01418bfc763116c38b1fd36e5b7eb8189e868
markelsanz14 <markelsanz14@gmail.com>
2022-06-29 10:53:54 -0700

    Update tests/ci_tests/selene/pytest/train/gpt3/test_126m_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/gpt3/test_126m_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/gpt3/test_5b_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/gpt3/test_5b_40gb_3runs_2node.py, tests/ci_tests/selene/pytest/train/mt5/test_170m_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/mt5/test_170m_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/mt5/test_3.2b_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/mt5/test_3.2b_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/t5/test_220m_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/t5/test_220m_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/t5/test_3.2b_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/t5/test_3.2b_80gb_3runs_1node.py

--- tests/ci_tests/selene/pytest/train/gpt3/test_5b_80gb_3runs_1node.py
+++ tests/ci_tests/selene/pytest/train/gpt3/test_5b_80gb_3runs_1node.py
@@ -6,12 +6,12 @@ BASE_RESULTS_DIR = os.environ.get("BASE_RESULTS_DIR")
 class TestCIGPT5b:
 
     def test_ci_train_gpt3_5b_80gb_3runs(self):
-        base_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/5b/base_cfg_5b.yaml")
-        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/5b/candidate_configs")
-        training_logs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/5b/training_logs")
-        final_result_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/5b/final_result")
-        final_summary_csv = os.path.join(BASE_RESULTS_DIR, "gpt3/5b/final_result/final_summary_1nodes.csv")
-        optimal_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/5b/final_result/optimal_config_5b_1nodes.yaml")
+        base_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/5b_80gb/base_cfg_5b.yaml")
+        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/5b_80gb/candidate_configs")
+        training_logs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/5b_80gb/training_logs")
+        final_result_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/5b_80gb/final_result")
+        final_summary_csv = os.path.join(BASE_RESULTS_DIR, "gpt3/5b_80gb/final_result/final_summary_1nodes.csv")
+        optimal_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/5b_80gb/final_result/optimal_config_5b_1nodes.yaml")
 
         assert os.path.exists(base_cfg), f"File not found: {base_cfg}"
         assert os.path.exists(candidate_configs_dir), f"Dir not found: {candidate_configs_dir}"
--- tests/ci_tests/selene/pytest/train/mt5/test_170m_40gb_3runs_1node.py
+++ tests/ci_tests/selene/pytest/train/mt5/test_170m_40gb_3runs_1node.py
@@ -6,12 +6,12 @@ BASE_RESULTS_DIR = os.environ.get("BASE_RESULTS_DIR")
 class TestCIGPT126m:
 
     def test_ci_train_gpt3_126m_80gb_3runs(self):
-        base_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b/base_cfg_0.17b.yaml")
-        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b/candidate_configs")
-        training_logs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b/training_logs")
-        final_result_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b/final_result")
-        final_summary_csv = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b/final_result/final_summary_1nodes.csv")
-        optimal_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b/final_result/optimal_config_0.17b_1nodes.yaml")
+        base_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/base_cfg_0.17b.yaml")
+        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/candidate_configs")
+        training_logs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/training_logs")
+        final_result_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/final_result")
+        final_summary_csv = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/final_result/final_summary_1nodes.csv")
+        optimal_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/final_result/optimal_config_0.17b_1nodes.yaml")
 
         assert os.path.exists(base_cfg), f"File not found: {base_cfg}"
         assert os.path.exists(candidate_configs_dir), f"Dir not found: {candidate_configs_dir}"
