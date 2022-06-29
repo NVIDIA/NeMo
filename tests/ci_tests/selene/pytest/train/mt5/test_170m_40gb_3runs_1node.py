9a33243a756e823bb3b84b23728867633af45665
markelsanz14 <markelsanz14@gmail.com>
2022-06-29 13:52:10 -0700

    Update tests/ci_tests/selene/pytest/train/t5/test_220m_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/t5/test_220m_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/t5/test_2.8b_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/t5/test_2.8b_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/mt5/test_170m_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/mt5/test_170m_80gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/mt5/test_3.2b_40gb_3runs_1node.py, tests/ci_tests/selene/pytest/train/mt5/test_3.2b_80gb_3runs_1node.py

--- tests/ci_tests/selene/pytest/train/mt5/test_170m_40gb_3runs_1node.py
+++ tests/ci_tests/selene/pytest/train/mt5/test_170m_40gb_3runs_1node.py
@@ -3,15 +3,15 @@ import os
 
 BASE_RESULTS_DIR = os.environ.get("BASE_RESULTS_DIR")
 
-class TestCIGPT126m:
+class TestCImT5170m:
 
-    def test_ci_train_gpt3_126m_80gb_3runs(self):
-        base_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/base_cfg_0.17b.yaml")
-        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/candidate_configs")
-        training_logs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/training_logs")
-        final_result_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/final_result")
-        final_summary_csv = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/final_result/final_summary_1nodes.csv")
-        optimal_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/0.17b_40gb/final_result/optimal_config_0.17b_1nodes.yaml")
+    def test_ci_train_mt5_170m_80gb_3runs(self):
+        base_cfg = os.path.join(BASE_RESULTS_DIR, "mt5/0.17b_40gb/base_cfg_0.17b.yaml")
+        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, "mt5/0.17b_40gb/candidate_configs")
+        training_logs_dir = os.path.join(BASE_RESULTS_DIR, "mt5/0.17b_40gb/training_logs")
+        final_result_dir = os.path.join(BASE_RESULTS_DIR, "mt5/0.17b_40gb/final_result")
+        final_summary_csv = os.path.join(BASE_RESULTS_DIR, "mt5/0.17b_40gb/final_result/final_summary_1nodes.csv")
+        optimal_cfg = os.path.join(BASE_RESULTS_DIR, "mt5/0.17b_40gb/final_result/optimal_config_0.17b_1nodes.yaml")
 
         assert os.path.exists(base_cfg), f"File not found: {base_cfg}"
         assert os.path.exists(candidate_configs_dir), f"Dir not found: {candidate_configs_dir}"
