47ea78377883fdc8a0a025382cea33a29eee47dc
Yang Zhang <yangzhang@nvidia.com>
2022-06-30 11:41:12 -0700

    fix header
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- scripts/text_normalization/model_utils.py
+++ scripts/text_normalization/model_utils.py
@@ -1,5 +1,4 @@
 # Copyright 2022, NVIDIA CORPORATION & AFFILIATION.
-# Copyright 2020 AWSLABS, AMAZON.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
