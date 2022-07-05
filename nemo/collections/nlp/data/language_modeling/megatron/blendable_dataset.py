90567c94c20cc4faa615816b63def1a657f682be
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2022-07-05 15:34:48 -0700

    Style
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- nemo/collections/nlp/data/language_modeling/megatron/blendable_dataset.py
+++ nemo/collections/nlp/data/language_modeling/megatron/blendable_dataset.py
@@ -33,7 +33,9 @@ class BlendableDataset(torch.utils.data.Dataset):
         self.size = size
         total_dataset_size = sum([len(dataset) for dataset in self.datasets])
         if self.size < total_dataset_size:
-            raise ValueError(f"Provided dataset size argument {size} is smaller than the sum of all dataset sizes {total_dataset_size}.")
+            raise ValueError(
+                f"Provided dataset size argument {size} is smaller than the sum of all dataset sizes {total_dataset_size}."
+            )
 
         # Normalize weights.
         weights = np.array(weights, dtype=np.float64)
