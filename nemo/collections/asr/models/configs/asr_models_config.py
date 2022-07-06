17b9c82368c33ee22b4d3ca503db3d2ae13b8bcc
tbartley94 <tbartley@nvidia.com>
2022-07-06 13:03:46 -0700

    Forgot to add Optional type...
    Signed-off-by: tbartley94 <tbartley@nvidia.com>

--- nemo/collections/asr/models/configs/asr_models_config.py
+++ nemo/collections/asr/models/configs/asr_models_config.py
@@ -60,7 +60,7 @@ class ASRDatasetConfig(nemo.core.classes.dataset.DatasetConfig):
     # bucketing params
     bucketing_strategy: str = "synced_randomized"
     bucketing_batch_size: Optional[Any] = None
-    bucket_weights: List[int] = None
+    bucket_weights: Optional[List[int]] = None
 
 
 @dataclass
