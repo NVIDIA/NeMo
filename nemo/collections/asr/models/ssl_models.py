a15dcdd7ecae5121cf856c308564f18ce3b19216
Somshubra Majumdar <titu1994@gmail.com>
2022-07-08 19:41:58 -0700

    Remove comment
    Signed-off-by: smajumdar <smajumdar@nvidia.com>

--- nemo/collections/asr/models/ssl_models.py
+++ nemo/collections/asr/models/ssl_models.py
@@ -307,8 +307,6 @@ class SpeechEncDecSelfSupervisedModel(ModelPT, ASRModuleMixin, AccessMixin):
             3) The encoded features tensor of shape [B, D, T].
             2) The lengths of the acoustic sequence after propagation through the encoder, of shape [B].
         """
-        # reset module registry from AccessMixin
-        self.reset_registry()
 
         has_input_signal = input_signal is not None and input_signal_length is not None
         has_processed_signal = processed_signal is not None and processed_signal_length is not None
@@ -457,10 +455,15 @@ class SpeechEncDecSelfSupervisedModel(ModelPT, ASRModuleMixin, AccessMixin):
 
     # PTL-specific methods
     def training_step(self, batch, batch_nb):
-        if self.output_from_layer is not None and len(self.output_from_layer) > 0:
-            self.reset_registry()
-            self.access_cfg['save_encoder_tensors'] = True
-            self.set_access_enabled(access_enabled=True)
+        # reset module registry from AccessMixin
+        if self.decoder_losses is not None and self.output_from_layer is not None and len(self.output_from_layer) > 0:
+            layer_names = list(self.output_from_layer.values())
+            register_layer = any([name is not None for name in layer_names])
+
+            if register_layer:
+                self.reset_registry()
+                self.access_cfg['save_encoder_tensors'] = True
+                self.set_access_enabled(access_enabled=True)
 
         signal, signal_len, targets, target_lengths = batch
         spectrograms, spec_masks, encoded, encoded_len = self.forward(
