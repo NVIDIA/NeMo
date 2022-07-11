17b2582ddf9ce4c71619fd5f81996c8e003150c9
yaoyu-33 <yaoyu.094@gmail.com>
2022-07-11 11:54:02 -0700

    add set_gpu_queue_sw in T5 training

--- bignlp/train_scripts/pretrain_t5.py
+++ bignlp/train_scripts/pretrain_t5.py
@@ -46,6 +46,8 @@ def main(cfg):
             hydra_train_args = convert_args_to_hydra_train_args(hydra_args.split())
 
     cuda_visible_devices = numa_mapping(local_rank=rank, devices=devices, numa_cfg=numa_cfg)
+    set_gpu_queue_sw = "CUDA_DEVICE_MAX_CONNECTIONS=1" if \
+        train_cfg.get("model").get("tensor_model_parallel_size", 0) > 1 else ""
 
     code_dir = "/opt/bignlp/NeMo"
     code_path = f"{code_dir}/examples/nlp/language_modeling/megatron_t5_pretraining.py"
@@ -65,10 +67,10 @@ def main(cfg):
         nsys = ""
 
     if cfg.get("cluster_type") == "bcm":
-        cmd = f"{cmd_prefix} {cuda_visible_devices} {nsys} python3 {code_path} {hydra_train_args} {flags}"
+        cmd = f"{cmd_prefix} {cuda_visible_devices} {set_gpu_queue_sw} {nsys} python3 {code_path} {hydra_train_args} {flags}"
     elif cfg.get("cluster_type") == "bcp":
         pause_and_prime_dns_connections()
-        cmd = f'{cmd_prefix} {cuda_visible_devices} python3 {code_path} +cluster_type=BCP +rank={os.environ.get("RANK")}  {hydra_train_args} {flags}'
+        cmd = f'{cmd_prefix} {cuda_visible_devices} {set_gpu_queue_sw} python3 {code_path} +cluster_type=bcp +rank={os.environ.get("RANK")}  {hydra_train_args} {flags}'
     os.system(f"{cmd}")
 
 
