b04a487ab6fc5d19d75a197a6d5043ec9a84fa70
yaoyu-33 <yaoyu.094@gmail.com>
2022-07-11 10:46:27 -0700

    add nsys profiling support in pretrain_t5

--- bignlp/train_scripts/pretrain_t5.py
+++ bignlp/train_scripts/pretrain_t5.py
@@ -24,6 +24,10 @@ def main(cfg):
     devices = trainer_cfg.get("devices")
     numa_cfg = cfg.get("numa_mapping")
 
+    # Run parameters
+    run_cfg = train_cfg.get("run")
+    results_dir = run_cfg.get("results_dir")
+
     rank = int(os.environ.get("LOCAL_RANK"))
 
     training_config = cfg.get("training_config").rsplit("/", 1)[1]
@@ -46,9 +50,22 @@ def main(cfg):
     code_dir = "/opt/bignlp/NeMo"
     code_path = f"{code_dir}/examples/nlp/language_modeling/megatron_t5_pretraining.py"
     cmd_prefix = generate_cmd_prefix(cfg, code_dir)
+
     # Write command to launch training.
+        if cfg.get("profile", False):
+        slurm_node = os.environ.get("SLURM_NODEID", "0")
+        slurm_rank = os.environ.get("SLURM_PROCID", "0")
+        slurm_jobid = os.environ.get("SLURM_JOB_ID", "0")
+        profile_out_path = os.path.join(results_dir, "profile_logs")
+        os.makedirs(profile_out_path, exist_ok=True)
+        nsys = f"nsys profile -s none -o -t " \
+               f"cuda,nvtx {profile_out_path}/profile_{slurm_jobid}_node{slurm_node}_rank{slurm_rank} " \
+               f"--force-overwrite true --capture-range=cudaProfilerApi --capture-range-end=stop"
+    else:
+        nsys = ""
+
     if cfg.get("cluster_type") == "bcm":
-        cmd = f"{cmd_prefix} {cuda_visible_devices} python3 {code_path} {hydra_train_args} {flags}"
+        cmd = f"{cmd_prefix} {cuda_visible_devices} {nsys} python3 {code_path} {hydra_train_args} {flags}"
     elif cfg.get("cluster_type") == "bcp":
         pause_and_prime_dns_connections()
         cmd = f'{cmd_prefix} {cuda_visible_devices} python3 {code_path} +cluster_type=BCP +rank={os.environ.get("RANK")}  {hydra_train_args} {flags}'
