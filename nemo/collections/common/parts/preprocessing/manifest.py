eb5fe33a21e73110251b884f803d5569644227c9
He Huang (Steve) <105218074+stevehuang52@users.noreply.github.com>
2022-07-12 12:15:02 -0400

    update (#4520)
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo/collections/common/parts/preprocessing/manifest.py
+++ nemo/collections/common/parts/preprocessing/manifest.py
@@ -95,7 +95,7 @@ def __parse_item(line: str, manifest_file: str) -> Dict[str, Any]:
     # Assume that the audio path is like "wavs/xxxxxx.wav".
     manifest_dir = Path(manifest_file).parent
     audio_file = Path(item['audio_file'])
-    if not audio_file.is_file() and not audio_file.is_absolute():
+    if (len(str(audio_file)) < 255) and not audio_file.is_file() and not audio_file.is_absolute():
         # assume the "wavs/" dir and manifest are under the same parent dir
         audio_file = manifest_dir / audio_file
         if audio_file.is_file():
