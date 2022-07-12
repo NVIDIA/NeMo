4c0ac7d54a4601d68585f2b3972ee6c2d1d9156d
Abhinav Khattar <aklife97@gmail.com>
2022-07-11 19:10:42 -0700

    rm preproc
    Signed-off-by: Abhinav Khattar <aklife97@gmail.com>

--- examples/nlp/machine_translation/enc_dec_nmt_finetune.py
+++ examples/nlp/machine_translation/enc_dec_nmt_finetune.py
@@ -80,11 +80,6 @@ def main(cfg: MTFineTuneConfig) -> None:
     trainer_cfg.pop('plugins', None)
     trainer = Trainer(plugins=[NLPDDPPlugin()], **trainer_cfg)
 
-    # tokenizers will be trained and and tarred training data will be created if needed
-    # model config is then updated
-    if cfg.model.preproc_out_dir is not None:
-        MTDataPreproc(cfg=cfg.model, trainer=trainer)
-
     # experiment logs, checkpoints, and auto-resume are managed by exp_manager and PyTorch Lightning
     exp_manager(trainer, cfg.exp_manager)
 
