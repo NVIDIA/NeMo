a09531777c86a468fe40dc0b256c59920896dc12
shanmugamr1992 <shanmugamr1992@gmail.com>
2022-07-23 21:33:20 -0700

    Refactor and writing 1 test

--- tests/ci_tests/selene/pytest/convert/test_convert_pipeline.py
+++ tests/ci_tests/selene/pytest/convert/test_convert_pipeline.py
@@ -1,6 +1,6 @@
 import os
 
-CI_JOB_RESULTS = os.environ.get("RESULTS_DIR")  #eg '/home/shanmugamr/bignlp-scripts/results/train_gpt3_126m_tp1_pp1_1node_100steps'
+CI_JOB_RESULTS_DIR = os.environ.get("RESULTS_DIR")  #eg '/home/shanmugamr/bignlp-scripts/results/train_gpt3_126m_tp1_pp1_1node_100steps'
 
 class TestConvertPipeline:
 
@@ -11,5 +11,5 @@ class TestConvertPipeline:
     output_file="megatron" + model + ".nemo"
 
     def test_ci_convert(self):
-        ckpt_path = os.path.join(CI_JOB_RESULTS, self.output_file)
+        ckpt_path = os.path.join(CI_JOB_RESULTS_DIR, self.output_file)
         assert os.path.exists(ckpt_path), f"File not found: {ckpt_path}"
