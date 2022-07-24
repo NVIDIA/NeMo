a80a52618d61124bbe03c789e5fdcc108cef1d17
Yi Dong <yidong@nvidia.com>
2022-07-24 18:24:16 +0000

    init checking
    Signed-off-by: Yi Dong <yidong@nvidia.com>

---
+++ nemo/collections/nlp/modules/common/megatron/fused_kernels/scaled_masked_softmax.h
@@ -0,0 +1,371 @@
+/* coding=utf-8
+ * Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <assert.h>
+#include <cuda_fp16.h>
+#include <cfloat>
+#include <limits>
+#include <stdint.h>
+#include <cuda_fp16.h>
+#include <c10/macros/Macros.h>
+#include <stdio.h>
+
+namespace {
+
+template<typename T>
+struct Add {
+  __device__ __forceinline__ T operator()(T a, T b) const {
+    return a + b;
+  }
+};
+
+template<typename T>
+struct Max {
+  __device__ __forceinline__ T operator()(T a, T b) const {
+    return a < b ? b : a;
+  }
+};
+
+template <typename T>
+__device__ __forceinline__ T WARP_SHFL_DOWN_NATIVE(T value, int laneMask, int width = warpSize, unsigned int mask = 0xffffffff)
+{
+#if CUDA_VERSION >= 9000
+    return __shfl_down_sync(mask, value, laneMask, width);
+#else
+    return __shfl_down(value, laneMask, width);
+#endif
+}
+
+template <typename acc_t, int WARP_SIZE, template<typename> class ReduceOp>
+__device__ __forceinline__ acc_t warp_reduce_new(acc_t val) {
+  ReduceOp<acc_t> r;
+  #pragma unroll
+  for (int offset = WARP_SIZE / 2; offset > 0; offset /= 2)
+  {
+      val = r(val, WARP_SHFL_DOWN_NATIVE(val, offset, WARP_SIZE));
+  }
+  return val;
+}
+
+
+template <typename input_t, typename output_t, typename acc_t, int log2_elements>
+__global__ void scaled_masked_softmax_warp_backward_new(
+    output_t *gradInput, //[batches, attn_heads, q_len, k_len]
+    input_t *grad, 
+    const input_t *output, //[batches, attn_heads, q_len, k_len]
+    acc_t scale, 
+    int element_count)
+{
+    int threads_per_block = blockDim.x; 
+    //the first element_count*2 elements are used for cache, the last 128 is used for reduction
+    extern __shared__ acc_t shared_data[];
+    input_t *local_data = (input_t *)shared_data;
+    input_t *output_data = &local_data[element_count];
+    // maximum shared cached 128, enough for 4096 elements reduction into 4096/32= 128 elements
+    acc_t *shared = (acc_t *)(&(local_data[element_count*2]));
+
+    int num_reductions =  (element_count - 1) / threads_per_block + 1;
+
+    int offset = blockIdx.x * element_count;
+
+    int local_idx = threadIdx.x;
+    int lane = threadIdx.x % C10_WARP_SIZE;
+    int wid = threadIdx.x / C10_WARP_SIZE;
+    int warps_per_thread_block = threads_per_block / C10_WARP_SIZE; 
+
+    // load the data to local data
+    acc_t val = 0.0;
+    for (int i = local_idx; i < element_count; i += threads_per_block)
+    {
+        val = output[offset + i];
+        output_data[i] = val;
+        local_data[i] = val * grad[offset + i];
+    }
+
+    // find the sum 
+    for (int i = local_idx; i < 128; i += threads_per_block){
+        shared[i] = 0.0;
+    }
+    __syncthreads();
+
+    #pragma unroll
+    for (int i = 0; i < num_reductions; i++){
+        if (i*threads_per_block + local_idx < element_count){
+            val = local_data[i*threads_per_block + local_idx];
+        }
+        else{
+            val = 0.0;
+        }
+        val = warp_reduce_new<acc_t, C10_WARP_SIZE, Add>(val);
+        if (lane==0) {
+            shared[wid + warps_per_thread_block*i] = val;
+        }
+        __syncthreads();
+    }
+
+    // final shared reduction
+    if (local_idx < 128) {
+        val = shared[local_idx];
+        val = warp_reduce_new<acc_t, C10_WARP_SIZE, Add>(val);
+        if (lane==0) {
+            shared[wid] = val;
+        }
+    }
+
+    __syncthreads();
+
+    if (local_idx < C10_WARP_SIZE) {
+        val = shared[local_idx];
+        val = warp_reduce_new<acc_t, 4, Add>(val);
+        if (lane==0) {
+            shared[wid] = val;
+        }
+    }
+    __syncthreads();
+    val = shared[0];
+
+    //acc_t reduced_val = (shared[0] + shared[1]) + (shared[2] + shared[3]);
+
+    #pragma unroll
+    for (int i = local_idx; i < element_count; i += threads_per_block){
+        gradInput[offset + i] = (output_t)(scale*(local_data[i] - output_data[i]*val)); 
+    }
+}
+
+} // end of anonymous namespace
+
+template<typename input_t, typename output_t, typename acc_t>
+void dispatch_scaled_masked_softmax_backward_new(
+    output_t *grad_input, 
+    input_t *grad, 
+    const input_t *output, 
+    const acc_t scale, 
+    int query_seq_len, 
+    int key_seq_len, 
+    int batches,
+    int attn_heads)
+{
+    TORCH_INTERNAL_ASSERT(key_seq_len >= 0 && key_seq_len <= 4096);
+    if (key_seq_len == 0)
+    {
+        return;
+    }
+    else
+    {
+        int batch_count = batches * attn_heads * query_seq_len;
+        // use 128 threads per block to maximimize gpu utilization
+        constexpr int threads_per_block = 128;
+        dim3 blocks(batch_count, 1, 1);
+        dim3 threads(threads_per_block, 1, 1);
+
+        scaled_masked_softmax_warp_backward_new<input_t, output_t, acc_t, 12>
+            <<<blocks, threads, sizeof(input_t)*key_seq_len*2 + sizeof(acc_t)*128, at::cuda::getCurrentCUDAStream()>>>(grad_input, grad, output, scale, key_seq_len);
+    }
+}
+
+/*
+ * Extended softmax (from native aten pytorch) with following additional features
+ * 1) input scaling
+ * 2) Explicit masking
+ */	
+template <typename input_t, typename output_t, typename acc_t>
+__global__ void scaled_masked_softmax_warp_forward_new(
+    output_t *dst, 
+    const input_t *src,
+    const uint8_t *mask, 
+    const acc_t scale, 
+    int query_len,          // query_len
+    int attn_heads,
+    int element_count,      // key_len
+    int pad_batches)        // mask batch size 
+{
+    // min threawds_per_block has to be bigger than 128
+    int threads_per_block = blockDim.x; 
+    //  the first element_count is used for cache, the last 128 is used for reduction
+    extern __shared__ acc_t local_data[];
+    // maximum shared cached 128, enough for 4096 elements reduction into 4096/32= 128 elements
+    acc_t *shared = &(local_data[element_count]);
+    // number of 1024 threads reductions 
+    int num_reductions =  (element_count - 1) / threads_per_block + 1;
+
+    int offset = blockIdx.x * element_count;
+    int mask_offset;
+    int query_id = blockIdx.x % query_len; 
+    if (pad_batches == 1){
+        // broadcaste the mask tensor 
+        mask_offset = query_id * element_count; 
+    }
+    else{
+        int mask_batch_id = blockIdx.x / attn_heads / query_len;
+        mask_offset = (mask_batch_id * query_len + query_id) * element_count;
+    }
+
+    int local_idx = threadIdx.x;
+    int lane = threadIdx.x % C10_WARP_SIZE;
+    int wid = threadIdx.x / C10_WARP_SIZE;
+    int warps_per_thread_block = threads_per_block / C10_WARP_SIZE; 
+
+    // load the data to local data
+    for (int i = local_idx; i < element_count; i += threads_per_block)
+    {
+        // TODO, use the copy vector method
+        if (mask[mask_offset + i] == 1)
+        {
+            local_data[i] = -10000.0;
+        }
+        else
+        {
+            local_data[i] = src[offset + i] * scale;
+        }
+    }
+
+    // first find the max value
+    for (int i = local_idx; i < 128; i += threads_per_block){
+        shared[i] = -10000.0;
+    }
+    __syncthreads();
+    acc_t val = -10000.0;
+    #pragma unroll
+    for (int i = 0; i < num_reductions; i++){
+        if (i*threads_per_block + local_idx < element_count){
+            val = local_data[i*threads_per_block + local_idx];
+        }
+        else{
+            val = -10000.0;
+        }
+
+        val = warp_reduce_new<acc_t, C10_WARP_SIZE, Max>(val);
+
+        if (lane==0) {
+            shared[wid + warps_per_thread_block*i] = val;
+        }
+        __syncthreads();
+    }
+    // final shared reduction
+    if (local_idx < 128) {
+        val = shared[local_idx];
+        val = warp_reduce_new<acc_t, C10_WARP_SIZE, Max>(val);
+        if (lane==0) {
+            shared[wid] = val;
+        }
+    }
+    __syncthreads();
+
+    if (local_idx < C10_WARP_SIZE) {
+        val = shared[local_idx];
+        val = warp_reduce_new<acc_t, 4, Max>(val);
+        if (lane==0) {
+            shared[wid] = val;
+        }
+    }
+    __syncthreads();
+    acc_t reduced_val = shared[0];
+
+    if (reduced_val < -10000.0 + 0.1){
+        // if everything is masked, pay attention to nothing
+        #pragma unroll
+        for (int i = local_idx; i < element_count; i += threads_per_block){
+             dst[offset + i] = 0.0;
+        }
+        return;
+    }
+
+    // update the values
+    #pragma unroll
+    for (int i = local_idx; i < element_count; i += threads_per_block){
+        local_data[i] = std::exp(local_data[i] - reduced_val);
+    }
+
+    // find the sum 
+    for (int i = local_idx; i < 128; i += threads_per_block){
+        shared[i] = 0.0;
+    }
+    __syncthreads();
+
+    #pragma unroll
+    for (int i = 0; i < num_reductions; i++){
+        if (i*threads_per_block + local_idx < element_count){
+            val = local_data[i*threads_per_block + local_idx];
+        }
+        else{
+            val = 0.0;
+        }
+        val = warp_reduce_new<acc_t, C10_WARP_SIZE, Add>(val);
+        if (lane==0) {
+            shared[wid + warps_per_thread_block*i] = val;
+        }
+        __syncthreads();
+    }
+    // final shared reduction
+    if (local_idx < 128) {
+        val = shared[local_idx];
+        val = warp_reduce_new<acc_t, C10_WARP_SIZE, Add>(val);
+        if (lane==0) {
+            shared[wid] = val;
+        }
+    }
+
+    __syncthreads();
+
+    if (local_idx < C10_WARP_SIZE) {
+        val = shared[local_idx];
+        val = warp_reduce_new<acc_t, 4, Add>(val);
+        if (lane==0) {
+            shared[wid] = val;
+        }
+    }
+    __syncthreads();
+    reduced_val = shared[0];
+    //if (local_idx<32){
+    //    printf("bid %d, lid %d, offset %d, blocks %d, v: %f, mask_offset %d\n", blockIdx.x, local_idx, offset, gridDim.x, reduced_val, mask_offset);
+    //}
+
+    #pragma unroll
+    for (int i = local_idx; i < element_count; i += threads_per_block){
+         dst[offset + i] = local_data[i] / reduced_val;
+    }
+}
+
+
+template<typename input_t, typename output_t, typename acc_t>
+void dispatch_scaled_masked_softmax_forward_new(
+    output_t *dst, 
+    const input_t *src, 
+    const uint8_t *mask,
+    const input_t scale, 
+    int query_seq_len, 
+    int key_seq_len, 
+    int batches,
+    int attn_heads,
+    int pad_batches)
+{
+    TORCH_INTERNAL_ASSERT(key_seq_len >= 0 && key_seq_len <= 4096 );
+    if (key_seq_len == 0) {
+        return;
+    } else {
+        int batch_count = batches * attn_heads * query_seq_len;
+
+        // use 128 threads per block to maximimize gpu utilization
+        constexpr int threads_per_block = 128;
+
+        dim3 blocks(batch_count, 1, 1);
+        dim3 threads(threads_per_block, 1, 1);
+        scaled_masked_softmax_warp_forward_new<input_t, output_t, acc_t>
+            <<<blocks, threads, sizeof(acc_t) * (key_seq_len + 128), at::cuda::getCurrentCUDAStream()>>>(dst, src, mask, scale, query_seq_len, attn_heads, key_seq_len, pad_batches);
+    }
+}
---
+++ nemo/collections/nlp/modules/common/megatron/fused_softmax.py
@@ -0,0 +1,126 @@
+import torch
+from apex._autocast_utils import _cast_if_autocast_enabled
+# this triggers kernel compiling
+import nemo.collections.nlp.modules.common.megatron.fused_kernels 
+
+
+class ScaledMaskedSoftmax(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, inputs, mask, scale):
+        import scaled_masked_softmax_cuda_new
+        scale_t = torch.tensor([scale])
+        softmax_results = scaled_masked_softmax_cuda_new.forward(inputs, mask, scale_t[0])
+        ctx.save_for_backward(softmax_results, scale_t)
+        return softmax_results
+
+    @staticmethod
+    def backward(ctx, output_grads):
+        import scaled_masked_softmax_cuda_new
+
+        softmax_results, scale_t = ctx.saved_tensors
+
+        input_grads = scaled_masked_softmax_cuda_new.backward(
+            output_grads, softmax_results, scale_t[0]
+        )
+        return input_grads, None, None
+
+
+def scaled_masked_softmax(inputs, mask, scale):
+    # input is 4D tensor (b, np, sq, sk)
+    args = _cast_if_autocast_enabled(inputs, mask, scale)
+    with torch.cuda.amp.autocast(enabled=False):
+        return ScaledMaskedSoftmax.apply(*args)
+
+
+class FusedScaleMaskSoftmax(torch.nn.Module):
+    """
+    Drop-in replacement for apex FusedSacleMaskSoftmax.
+    It removes the seq-len limitations compares with apex one.
+    It handles the case that all tokens are masked, it returns zeros for attention score.
+
+    fused operation: scaling + mask + softmax
+
+    Arguments:
+        input_in_fp16: flag to indicate if input in fp16 data format.
+        input_in_bf16: flag to indicate if input in bf16 data format.
+        scaled_masked_softmax_fusion: flag to indicate user want to use softmax fusion
+        mask_func: mask function to be applied.
+        softmax_in_fp32: if true, softmax in performed at fp32 precision.
+        scale: scaling factor used in input tensor scaling.
+    """
+
+    def __init__(
+        self,
+        input_in_fp16,
+        input_in_bf16,
+        scaled_masked_softmax_fusion,
+        mask_func,
+        softmax_in_fp32,
+        scale,
+    ):
+        super().__init__()
+        self.input_in_fp16 = input_in_fp16
+        self.input_in_bf16 = input_in_bf16
+        if self.input_in_fp16 and self.input_in_bf16:
+            raise RuntimeError(
+                "both fp16 and bf16 flags cannot be active at the same time."
+            )
+        self.input_in_float16 = self.input_in_fp16 or self.input_in_bf16
+        self.mask_func = mask_func
+        self.softmax_in_fp32 = softmax_in_fp32
+        self.scale = scale
+        self.scaled_masked_softmax_fusion = scaled_masked_softmax_fusion
+
+        if not (self.scale is None or softmax_in_fp32):
+            raise RuntimeError("softmax should be in fp32 when scaled")
+        self.fused_softmax_func = scaled_masked_softmax
+
+    def forward(self, input, mask):
+        # [b, np, sq, sk]
+        assert input.dim() == 4
+
+        if self.is_kernel_available(mask, *input.size()):
+            res = self.forward_fused_softmax(input, mask)
+            control = self.forward_torch_softmax(input, mask).detach()
+            all_k_masked = mask.all(axis=-1)
+            zero_attention_mask = (1.0 - all_k_masked.float())[:, :, :, None]
+            control = zero_attention_mask * control
+            if (res - control).abs().max() > 1e-2:
+                torch.save(input, '/results/input.pth')
+                torch.save(mask, '/results/mask.pth')
+                torch.save(self.scale, '/results/scale.pth')
+                import sys
+                sys.exit(0)
+            return self.forward_fused_softmax(input, mask)
+        else:
+            return self.forward_torch_softmax(input, mask)
+
+    def is_kernel_available(self, mask, b, np, sq, sk):
+        if (
+            self.scaled_masked_softmax_fusion  # user want to fuse
+            and 16 < sk <= 4096  # sk must be 16 ~ 2048
+        ):
+            return True
+        return False
+
+    def forward_fused_softmax(self, input, mask):
+        # input.shape = [b, np, sq, sk]
+        scale = self.scale if self.scale is not None else 1.0
+        return self.fused_softmax_func(input, mask, scale)
+
+    def forward_torch_softmax(self, input, mask):
+        if self.input_in_float16 and self.softmax_in_fp32:
+            input = input.float()
+
+        if self.scale is not None:
+            input = input * self.scale
+        mask_output = self.mask_func(input, mask) if mask is not None else input
+        probs = torch.nn.Softmax(dim=-1)(mask_output)
+
+        if self.input_in_float16 and self.softmax_in_fp32:
+            if self.input_in_fp16:
+                probs = probs.half()
+            else:
+                probs = probs.bfloat16()
+
+        return probs
