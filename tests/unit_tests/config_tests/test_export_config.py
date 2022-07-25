8390365600db3131e71aa410a6650b29a97892ac
Pawel Ziecina <pziecina@nvidia.com>
2022-07-25 19:10:53 +0200

    Fix for export configs

--- tests/unit_tests/config_tests/test_export_config.py
+++ tests/unit_tests/config_tests/test_export_config.py
@@ -13,9 +13,8 @@ class TestExportmT5Config:
           finetuning_results_dir: ${base_results_dir}/${.model_train_name}/${.task_name}
           config_summary: tp${export.model.tensor_model_parallel_size}_pp${export.triton_deployment.pipeline_model_parallel_size}_${export.model.weight_data_type}_${export.triton_deployment.data_type}
           results_dir: ${base_results_dir}/${.model_train_name}/${.task_name}_export_${.config_summary}
-          triton_model_dir: ${.results_dir}/model_repo/${.model_train_name}
           model_type: "mt5"
-
+        
         model:
           checkpoint_path: ${export.run.finetuning_results_dir}/checkpoints/megatron_mt5_glue_xnli.nemo
           # FT checkpoint will be saved in ${.triton_model_dir}/1/${.tensor_model_parallel_size}-gpu
@@ -23,15 +22,17 @@ class TestExportmT5Config:
           weight_data_type: fp16   # fp32|fp16
           processes: 16
           load_checkpoints_to_cpu: False
-
+        
         triton_deployment:
+          triton_model_dir: ${export.run.results_dir}/model_repo/${export.run.model_train_name}
           max_batch_size: 1
           pipeline_model_parallel_size: 1
           int8_mode: False
           enable_custom_all_reduce: False
           data_type: fp16  # fp32|fp16|bf16
-
+        
         accuracy:
+          enabled: True  # enable accuracy test
           ntasks_per_node: 8  # usually should be number of available gpus per node
           test_data: ${data_dir}/glue_data/xnli/xnli.test.tsv
           output_path: ${export.run.results_dir}/eval_output.json
@@ -60,9 +61,8 @@ class TestExportT5Config:
           finetuning_results_dir: ${base_results_dir}/${.model_train_name}/${.task_name}
           config_summary: tp${export.model.tensor_model_parallel_size}_pp${export.triton_deployment.pipeline_model_parallel_size}_${export.model.weight_data_type}_${export.triton_deployment.data_type}
           results_dir: ${base_results_dir}/${.model_train_name}/${.task_name}_export_${.config_summary}
-          triton_model_dir: ${.results_dir}/model_repo/${.model_train_name}
           model_type: "t5"
-
+        
         model:
           checkpoint_path: ${export.run.finetuning_results_dir}/checkpoints/megatron_t5_glue.nemo
           # FT checkpoint will be saved in ${.triton_model_dir}/1/${.tensor_model_parallel_size}-gpu
@@ -70,15 +70,17 @@ class TestExportT5Config:
           weight_data_type: fp16   # fp32|fp16
           processes: 16
           load_checkpoints_to_cpu: False
-
+        
         triton_deployment:
+          triton_model_dir: ${export.run.results_dir}/model_repo/${export.run.model_train_name}
           max_batch_size: 1
           pipeline_model_parallel_size: 1
           int8_mode: False
           enable_custom_all_reduce: False
           data_type: fp16  # fp32|fp16|bf16
-
+        
         accuracy:
+          enabled: True  # enable accuracy test
           ntasks_per_node: 8  # usually should be number of available gpus per node
           test_data: ${data_dir}/glue_data/mnli/dev_matched.tsv
           output_path: ${export.run.results_dir}/eval_output.json
@@ -106,9 +108,8 @@ class TestExportGPT3Config:
           training_dir: ${base_results_dir}/${.model_train_name}
           config_summary: tp${export.model.tensor_model_parallel_size}_pp${export.triton_deployment.pipeline_model_parallel_size}_${export.model.weight_data_type}_${export.triton_deployment.data_type}
           results_dir: ${base_results_dir}/${.model_train_name}/export_${.config_summary}
-          triton_model_dir: ${.results_dir}/model_repo/${.model_train_name}
           model_type: "gpt3"
-
+        
         model:
           checkpoint_path: ${export.run.training_dir}/checkpoints
           # FT checkpoint will be saved in ${.triton_model_dir}/1/${.tensor_model_parallel_size}-gpu
@@ -116,15 +117,17 @@ class TestExportGPT3Config:
           weight_data_type: fp16   # fp32|fp16
           processes: 16
           load_checkpoints_to_cpu: False
-
+        
         triton_deployment:
+          triton_model_dir: ${export.run.results_dir}/model_repo/${export.run.model_train_name}
           max_batch_size: 1
           pipeline_model_parallel_size: 1
           int8_mode: False
           enable_custom_all_reduce: False
           data_type: fp16  # fp32|fp16|bf16
-
+        
         accuracy:
+          enabled: True  # enable accuracy test
           ntasks_per_node: 8  # usually should be number of available gpus per node
           runtime_config_ini_path: ${export.run.results_dir}/ft_runtime.ini
           test_data: ${export.run.results_dir}/lambada_test.jsonl
