ec138101b9dcec791dfc74645b498e285e897ea1
Guilherme Steinmann <guist@linse.ufsc.br>
2022-08-02 05:58:29 -0300

    Fix itn pt time (#4630)
    * Fix time rule on ITN pt
    
    Signed-off-by: Guilherme Steinmann <guist@linse.ufsc.br>
    
    * Fix style
    
    Signed-off-by: Guilherme Steinmann <guist@linse.ufsc.br>
    
    Co-authored-by: ekmb <ebakhturina@nvidia.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/pt/taggers/tokenize_and_classify.py
+++ nemo_text_processing/inverse_text_normalization/pt/taggers/tokenize_and_classify.py
@@ -82,7 +82,7 @@ class ClassifyFst(GraphFst):
 
             classify = (
                 pynutil.add_weight(whitelist_graph, 1.01)
-                | pynutil.add_weight(time_graph, 1.1)
+                | pynutil.add_weight(time_graph, 1.09)
                 | pynutil.add_weight(date_graph, 1.09)
                 | pynutil.add_weight(decimal_graph, 1.09)
                 | pynutil.add_weight(measure_graph, 1.1)
