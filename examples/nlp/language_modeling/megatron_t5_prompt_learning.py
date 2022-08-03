d64e3276404c2dc41cea2d96159f6cd615174cd2
HeyyyyyyG <49757268+HeyyyyyyG@users.noreply.github.com>
2022-08-03 18:13:41 -0400

    Add prompt learning for T5 (#4391)
    * prompt learning for T5 model
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * fix a typo
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * Add CI test for T5 prompt learning
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * Modify training config for CI test
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * delete old p-tuning files
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * Update codes according to reviews
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * Delete old files
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * Update init function, write a load_frozen_model function to reduce duplication.
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * Change names of variables, replace float_type with autocast_type
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * update variable's name
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * 1. Add support for data parallelism 2. Use forward_backward_no_pipelining
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * style fix
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * fix bugs
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * update config files
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * style fix
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * remove unused import
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * update based on recent change in megatron_lm_encoder_decoder_model.py
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * fix a bug on enc_input shape
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    * add comments
    
    Signed-off-by: HeyyyyyyG <jiaqizen@andrew.cmu.edu>
    
    Co-authored-by: Virginia Adams <78445382+vadam5@users.noreply.github.com>
    Co-authored-by: Sandeep Subramanian <sandeep.subramanian.1@umontreal.ca>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

---
+++ examples/nlp/language_modeling/megatron_t5_prompt_learning.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from omegaconf.omegaconf import OmegaConf, open_dict
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks.timer import Timer
+from pytorch_lightning.plugins.environments.torchelastic_environment import TorchElasticEnvironment
+
+from nemo.collections.nlp.models.language_modeling.megatron_t5_prompt_learning_model import (
+    MegatronT5PromptLearningModel,
+)
+from nemo.collections.nlp.parts.nlp_overrides import (
+    GradScaler,
+    NLPDDPPlugin,
+    NLPSaveRestoreConnector,
+    PipelineMixedPrecisionPlugin,
+)
+from nemo.core.config import hydra_runner
+from nemo.utils import logging
+from nemo.utils.exp_manager import StatelessTimer, exp_manager
+
+
+"""
+This is an example of how to ptune/prompt-tune a pretrained T5 model.
+Be sure to use a .nemo T5 model with this code. If you've downloaded 
+a model from NGC or are otherwise using a MegatronLM model, please use
+either megatron_ckpt_to_nemo.py or megatron_lm_ckpt_to_nemo.py found
+within this examples directory to convert your model to .nemo format.
+"""
+
+
+@hydra_runner(config_path="conf", config_name="megatron_t5_prompt_learning.yaml")
+def main(cfg) -> None:
+    logging.info("\n\n************** Experiment configuration ***********")
+    logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
+
+    plugins = [NLPDDPPlugin(no_ddp_communication_hook=True, find_unused_parameters=False,)]
+    if cfg.trainer.precision == 16:
+        scaler = GradScaler(
+            init_scale=cfg.model.get('native_amp_init_scale', 2 ** 32),
+            growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
+            hysteresis=cfg.model.get('hysteresis', 2),
+        )
+        plugins.append(PipelineMixedPrecisionPlugin(precision=cfg.trainer.precision, device='cuda', scaler=scaler))
+
+    if cfg.get('cluster_type', None) == 'BCP':
+        plugins.append(TorchElasticEnvironment())
+
+    trainer = Trainer(plugins=plugins, **cfg.trainer)
+    exp_manager(trainer, cfg.exp_manager)
+
+    # Override timer callback to a stateless one
+    for idx, callback in enumerate(trainer.callbacks):
+        if isinstance(callback, Timer):
+            trainer.callbacks[idx] = StatelessTimer(cfg.trainer.max_time,)
+
+    # hydra interpolation does not work here as the interpolation key is lost when PTL saves hparams
+    with open_dict(cfg):
+        cfg.model.precision = cfg.trainer.precision
+
+    # load existing or init new soft prompt T5 model
+    if cfg.model.get("restore_path", None):
+        model = MegatronT5PromptLearningModel.restore_from(
+            cfg.model.restore_path, cfg.model, trainer=trainer, save_restore_connector=NLPSaveRestoreConnector()
+        )
+
+    else:
+        model = MegatronT5PromptLearningModel(cfg.model, trainer=trainer)
+
+    trainer.fit(model)
+
+
+if __name__ == '__main__':
+    main()
