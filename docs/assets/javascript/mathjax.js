973698082f69029b9309ef42fa53965e8138137f
Somshubra Majumdar <titu1994@gmail.com>
2022-08-04 18:33:04 -0700

    Start draft of intro post
    Signed-off-by: smajumdar <smajumdar@nvidia.com>

---
+++ docs/assets/javascript/mathjax.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  document$.subscribe(() => { 
+    MathJax.typesetPromise()
+  })
+  
