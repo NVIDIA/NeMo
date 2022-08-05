74c997a240a001f4d585dcda1f2116b709327b08
yaoyu-33 <yaoyu.094@gmail.com>
2022-08-04 17:30:25 -0700

    Update main pipeline

---
+++ main2.py
@@ -0,0 +1,54 @@
+import sys
+
+import copy
+import math
+import hydra
+import omegaconf
+import subprocess
+
+from bignlp.core.stages import BigNLPStage
+from bignlp.core.stages import Training, FineTuning, PromptLearning
+from bignlp.core.stages import Conversion
+from bignlp.core.stages import EvalHarnessEvaluation, NeMoEvaluation
+
+
+omegaconf.OmegaConf.register_new_resolver("multiply", lambda x, y: x * y, replace=True)
+omegaconf.OmegaConf.register_new_resolver("divide_ceil", lambda x, y: int(math.ceil(x / y)), replace=True)
+omegaconf.OmegaConf.register_new_resolver("divide_floor", lambda x, y: int(math.floor(x / y)), replace=True)
+
+STR2STAGECLASS = {
+    "training": Training,
+    "fine_tuning": FineTuning,
+    "prompt_learning": PromptLearning,
+    "conversion": Conversion,
+    "evaluation": {
+        EvalHarnessEvaluation: ["gpt3", "prompt_gpt3"],
+        NeMoEvaluation: ["t5", "mt5"]
+    },
+}
+
+
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg):
+    requested_stages = cfg.get("stages")
+
+    dependency = None
+    for stage_name in requested_stages:
+        stage_class = STR2STAGECLASS[stage_name]
+        if isinstance(stage_class, dict):
+            stage_config_choice = cfg.get(f"{stage_name}_config")
+            choice_model_type = stage_config_choice.rsplit("/", 1)[0]
+            for cls, model_types in stage_class.items():
+                if choice_model_type in model_types:
+                    stage_class = cls
+                    break
+
+        if dependency is not None:
+            cfg[stage_name]["run"]["dependency"] = dependency
+        stage = stage_class(cfg)
+        job_id = stage.run()
+        if job_id:
+            dependency = f"afterany:{job_id}"
+
+if __name__ == "__main__":
+    main()
