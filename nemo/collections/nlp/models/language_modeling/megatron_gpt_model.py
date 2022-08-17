af60b68dbb54ce40f82797a13afabccccf71966d
ericharper <complex451@gmail.com>
2022-08-17 16:54:25 -0600

    account for virtual when determinining first and last pipeline stages
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -120,21 +120,6 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         # configuration used for inference
         self._inference_config = None
 
-        # At pipeline-parallel training, set the pipeline stage that the current GPU belongs to skip loading inputs
-        # Intemediate stage: doesn't need any inputs
-        # Fist pipeline stage: needs only 'tokens' and 'position_ids'
-        # Last pipeline stage: needs only 'labels' and 'loss_mask'
-        self._is_first_pipe_stage = False
-        self._is_intermediate_pipe_stage = False
-        self._is_last_pipe_stage = False
-        if parallel_state.get_pipeline_model_parallel_world_size() > 1:
-            if parallel_state.is_pipeline_first_stage():
-                self._is_first_pipe_stage = True
-            elif parallel_state.is_pipeline_last_stage():
-                self._is_last_pipe_stage = True
-            else:
-                self._is_intermediate_pipe_stage = True
-
     def set_inference_config(self, inference_config):
         self._inference_config = inference_config
 
@@ -195,13 +180,16 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         # we zero grads here because we also call backward in the apex fwd/bwd functions
         self._optimizer.zero_grad()
 
-        if self._is_intermediate_pipe_stage:
+        if parallel_state.is_pipeline_first_stage(ignore_virtual=True) or parallel_state.is_pipeline_last_stage(
+            ignore_virtual=True
+        ):
+            # we prepare the micro batches for the apex fwd/bwd function
+            batch_for_pipeline = self.process_global_batch(batch)
+        else:
             # The intermediate pipeline stages do not need any inputs from data loader
             # GPT3 uses decoder with AttnMask:causal, thus doesn't need attention_mask
             batch_for_pipeline = None
-        else:
-            # we prepare the micro batches for the apex fwd/bwd function
-            batch_for_pipeline = self.process_global_batch(batch)
+
         tensor_shape = [self.cfg.encoder_seq_length, self.cfg.micro_batch_size, self.cfg.hidden_size]
 
         if self.cfg.get('pipeline_model_parallel_size', 1) > 1:
@@ -360,21 +348,20 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
                 attention_mask = attention_mask[0:1]
             else:
                 # GPT3 uses only causal mask, which doesn't need attention mask
-                if self._is_first_pipe_stage:
+                if parallel_state.is_pipeline_first_stage():
                     # Fist pipeline stage needs only the tokens and position_ids
                     tokens = batch[0].cuda(non_blocking=True)
                     position_ids = batch[4].cuda(non_blocking=True)
                     labels, loss_mask, attention_mask = None, None, None
-                elif self._is_intermediate_pipe_stage:
-                    # Intermediate pipeline stage doesn't need any inputs
-                    tokens, labels, loss_mask, attention_mask, position_ids = None, None, None, None, None
-                elif self._is_last_pipe_stage:
+                elif parallel_state.is_pipeline_last_stage():
                     # Last pipeline stage needs only the labels and loss_mask
                     labels = batch[1].cuda(non_blocking=True)
                     loss_mask = batch[2].cuda(non_blocking=True)
                     tokens, attention_mask, position_ids = None, None, None
                 else:
-                    assert False
+                    # Intermediate pipeline stage doesn't need any inputs
+                    tokens, labels, loss_mask, attention_mask, position_ids = None, None, None, None, None
+
             output_tensor = model(tokens, position_ids, attention_mask, labels)
 
             def loss_func(output_tensor):
