1901f85606ffc58dd8f55e8d281037ca87b8104d
Lily Lee <lilee@nvidia.com>
2022-08-18 16:13:40 -0700

    examples/nlp/dialogue/dialogue.py update
    Signed-off-by: Lily Lee <lilee@nvidia.com>

--- examples/nlp/dialogue/dialogue.py
+++ examples/nlp/dialogue/dialogue.py
@@ -150,7 +150,7 @@ def main(cfg: DictConfig) -> None:
 
     if hasattr(cfg.model, 'test_ds') and cfg.model.test_ds.ds_item is not None:
         eval_device = [cfg.trainer.devices[0]] if isinstance(cfg.trainer.devices, list) else 1
-        trainer = pl.Trainer(devices=eval_device, accelerator=cfg.trainer.accelerator, precision=16, strategy=strategy)
+        trainer = pl.Trainer(devices=eval_device, accelerator=cfg.trainer.accelerator, precision=16)
         model.setup_multiple_test_data(test_data_config=cfg.model.test_ds)
         trainer.test(model)
 
