b4602337dcc73cae095f3bf2caf0da1dc5da78a7
yaoyu-33 <yaoyu.094@gmail.com>
2022-08-24 16:24:11 -0700

    Add prompt learning t5 CI test files

---
+++ tests/ci_tests/selene/scripts/eval/prompt_mt5/eval_prompt_mt5_model.sh
@@ -0,0 +1,26 @@
+params=()
+if [[ "$TEST_TASK" = "squad_real" ]]; then
+  params+=(container_mounts=[/lustre/fsw/joc/big_nlp/bignlp_ci_resources:/lustre/fsw/joc/big_nlp/bignlp_ci_resources,/lustre/fsw/joc/yuya/bignlp/bignlp-scripts_gpt3/data:/lustre/fsw/joc/yuya/bignlp/bignlp-scripts_gpt3/data])
+fi
+
+HYDRA_FULL_ERROR=1 BIGNLP_CI=1 python3 main.py \
+    evaluation=${RUN_MODEL}/squad \
+    stages=["evaluation"] \
+    bignlp_path=${GIT_CLONE_PATH} \
+    data_dir=${BASE_RESULTS_DIR}/data \
+    base_results_dir=${BASE_RESULTS_DIR} \
+    "container='${BUILD_IMAGE_NAME_SRUN}'" \
+    cluster.partition=${SLURM_PARTITION} \
+    cluster.account=${SLURM_ACCOUNT} \
+    cluster.gpus_per_task=null \
+    cluster.gpus_per_node=null \
+    cluster.job_name_prefix="${SLURM_ACCOUNT}-bignlp_ci:" \
+    evaluation.run.name=${RUN_NAME} \
+    evaluation.run.time_limit=${TIME_LIMIT} \
+    evaluation.run.model_train_name=${RUN_NAME} \
+    evaluation.run.results_dir=${BASE_RESULTS_DIR}/${RUN_NAME} \
+    evaluation.virtual_prompt_model_file=${BASE_RESULTS_DIR}/${PROMPT_LEARN_MODEL_DIR}/results/megatron_mt5_prompt.nemo \
+    evaluation.pretrained_language_model_file=${BASE_RESULTS_DIR}/${CONVERSION_MODEL_DIR}/results/megatron_mt5.nemo \
+    evaluation.tensor_model_parallel_size=${TP_SIZE} \
+    evaluation.pipeline_model_parallel_size=${PP_SIZE} \
+    "${params[@]}" ${ADDITIONAL_PARAMS}
---
+++ tests/ci_tests/selene/scripts/prompt_learn/mt5/prompt_learn_mt5_model.sh
@@ -0,0 +1,41 @@
+params=()
+
+
+if [[ "$TEST_TASK" = "squad_real" ]]; then
+  # Should come in here for the test prompt_learn.gpt3.126m_tp1_pp1_1node_squad_real
+  # We need container mounts and LANGUAGE MODEL PATH from the config at gitlab ci yaml file
+  params+=($CONTAINER_MOUNTS)
+else
+  # Should come in here for the test prompt_learn.gpt3.126m_tp1_pp1_1node_100steps_squad
+  LOG_EVERY_N_STEPS=`expr $MAX_STEPS / 100`
+  VAL_CHECK_INTERVAL=`expr $MAX_STEPS / 5`
+  params+=(prompt_learning.trainer.log_every_n_steps=$LOG_EVERY_N_STEPS)
+  params+=(prompt_learning.trainer.val_check_interval=$VAL_CHECK_INTERVAL)
+  params+=(prompt_learning.trainer.max_steps=${MAX_STEPS})
+  UPSTREAM_RUN_NAME=convert_${RUN_MODEL}_${RUN_MODEL_SIZE}_tp${TP_SIZE}_pp${PP_SIZE}
+  LANGUAGE_MODEL_PATH=${BASE_RESULTS_DIR}/${UPSTREAM_RUN_NAME}/results/megatron_mt5.nemo
+fi
+
+set -o xtrace
+
+HYDRA_FULL_ERROR=1 BIGNLP_CI=1 python3 main.py \
+    prompt_learning=${RUN_MODEL}/squad \
+    stages=["prompt_learning"] \
+    bignlp_path=${GIT_CLONE_PATH} \
+    data_dir=${BASE_RESULTS_DIR}/data \
+    base_results_dir=${BASE_RESULTS_DIR} \
+    "container='${BUILD_IMAGE_NAME_SRUN}'" \
+    cluster.partition=${SLURM_PARTITION} \
+    cluster.account=${SLURM_ACCOUNT} \
+    cluster.gpus_per_task=null \
+    cluster.gpus_per_node=null \
+    cluster.job_name_prefix="${SLURM_ACCOUNT}-bignlp_ci:" \
+    prompt_learning.run.name=${RUN_NAME} \
+    prompt_learning.run.time_limit=${TIME_LIMIT} \
+    prompt_learning.run.results_dir=${BASE_RESULTS_DIR}/${RUN_NAME} \
+    prompt_learning.run.model_train_name=${RUN_MODEL}_${RUN_MODEL_SIZE} \
+    prompt_learning.trainer.num_nodes=${NUM_NODES} \
+    prompt_learning.model.language_model_path=${LANGUAGE_MODEL_PATH}\
+    prompt_learning.model.tensor_model_parallel_size=${TP_SIZE} \
+    prompt_learning.model.pipeline_model_parallel_size=${PP_SIZE} \
+     "${params[@]}" ${ADDITIONAL_PARAMS}
