eeffa7a09b2a0ca4b9db013af0160462216cee4f
Piotr Marcinkiewicz <piotrm@nvidia.com>
2022-08-24 18:21:50 +0200

    Further work with refactor

--- hp_tool/inference_sweep.py
+++ hp_tool/inference_sweep.py
@@ -2,8 +2,211 @@ import os
 import shutil
 import subprocess
 import sys
+import typing
 
 import omegaconf
+
+
+def create_slurm_file(
+    new_script_path,
+    steps_cmds,
+    job_name,
+    flags="",
+    dependency=None,
+    time="04:00:00",
+    exclusive=True,
+    mem: typing.Optional[int] = None,
+    overcommit=True,
+    nodes=None,
+    ntasks=None,
+    ntasks_per_node=None,
+    gpus_per_task=None,
+    gpus_per_node=None,
+    partition="batch",
+    account=None,
+    exclude=None,
+):
+    """
+    Creates a slurm file to launch an export job.
+    """
+    with open(new_script_path, "w") as f:
+        f.writelines("#!/usr/bin/env bash\n")
+        if nodes is not None:
+            f.writelines(f"#SBATCH --nodes={nodes}\n")
+        if ntasks is not None:
+            f.writelines(f"#SBATCH --ntasks={ntasks}\n")
+        if ntasks_per_node is not None:
+            f.writelines(f"#SBATCH --ntasks-per-node={ntasks_per_node}\n")
+        if gpus_per_task is not None:
+            f.writelines(f"#SBATCH --gpus-per-task={gpus_per_task}\n")
+        if gpus_per_node is not None:
+            f.writelines(f"#SBATCH --gpus-per-node={gpus_per_node}\n")
+        if dependency is not None:
+            dependency = dependency.strip()
+            if dependency != "singleton":
+                dependency = f"afterany:{dependency}"
+            f.writelines(f"#SBATCH --dependency={dependency}\n")
+        f.writelines(f"#SBATCH -p {partition}\n")
+        if account is not None:
+            f.writelines(f"#SBATCH -A {account}\n")
+        f.writelines(f"#SBATCH --job-name={job_name}\n")
+        if mem is not None:
+            f.writelines(f"#SBATCH --mem={mem}\n")
+        if exclusive:
+            f.writelines("#SBATCH --exclusive\n")
+        if overcommit:
+            f.writelines("#SBATCH --overcommit\n")
+        if exclude:
+            f.writelines(f"#SBATCH --exclude={','.join(exclude)}\n")
+        f.writelines(f"#SBATCH --time={time}\n\n")
+        for cmd in steps_cmds:
+            assert "'" not in cmd
+            f.writelines(f"srun {flags} sh -c '{cmd}'\n\n")
+        f.writelines("set +x\n")
+
+
+
+def run_benchmark(run_cfg,
+                  benchmark_cfg,
+                  cluster_cfg,
+                  dependency,
+                  bignlp_scripts_path,
+                  triton_model_dir,
+                  model_name,
+                  container,
+                  tensor_parallel_size,
+                  pipeline_parallel_size,
+                  nodes_number,
+                  workspace_path,
+                  verbose = True,
+                  ):
+
+    #dt = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    #parser = argparse.ArgumentParser(description="Test BigNLP models")
+    #parser.add_argument("--cluster-config-path", help="Path to cluster configuration file", required=True)
+    #parser.add_argument("--model-config-path", help="Path to model configuration file", required=True)
+    #parser.add_argument("--start-id-path", help="Path to start id csv file", required=True)
+    #parser.add_argument(
+    #    "--workspace-path",
+    #    help="Path to workspace dir where logs and artifacts will be stored",
+    #    default=f"./infer_workspace-{dt}",
+    #)
+    #parser.add_argument("--verbose", "-v", help="Provides verbose output", action="store_true", default=False)
+    #args = parser.parse_args()
+
+    workspace_path_absolute = pathlib.Path(workspace_path).resolve().absolute()
+    config_logger(workspace_path_absolute, verbose)
+
+    LOGGER.info(f"Arguments:")
+    for name, value in vars(args).items():
+        LOGGER.info(f"    {name}: {value}")
+
+    config_path = pathlib.Path(args.cluster_config_path).resolve().absolute()
+    with config_path.open("r") as config_file:
+        cluster_config = yaml.load(config_file, Loader=yaml.SafeLoader)
+
+    cluster_dir_path = workspace_path_absolute / CLUSTER_DIR_NAME
+
+    job_name_prefix = cluster_config["env"]["job_name_prefix"]
+    training_container_image = cluster_config["env"]["training_container_image"]
+
+    # variant = Variant.from_triton_model_repository(triton_model_repository_path)
+    # LOGGER.info(f"Config variant {variant}")
+
+    #executor = ClusterExecutor(cluster_dir_path=cluster_dir_path, cluster_config=cluster_config["cluster"])
+
+    start_id_path = "start_id.csv"
+    output_path = "sweep.out"
+
+
+    commands=[
+        f"export CUDA_VISIBLE_DEVICES={','.join(map(str, range(0, 8)))}",
+        f"/opt/bignlp/FasterTransformer/build/bin/multi_gpu_gpt_sweep ",
+        f"{config_path} ",
+        f"{start_id_path} ",
+        f"{workspace_path}/{output_path} ",
+        f"{tensor_parallel_size} ",
+        f"{pipeline_parallel_size}",
+    ]
+    flags=[
+    ]
+
+
+    cmds = "".join(commands)
+
+    submission_script_path = f"{workspace_path}/cluster_workspace/submission_%j.out"
+
+    cluster_cfg = cfg.cluster
+
+    job_name = "ft_benchmark"
+
+    create_slurm_file(
+        new_script_path=submission_script_path,
+        steps_cmds=cmds,
+        job_name=f"{cluster_cfg.job_name_prefix}{job_name}",
+        flags=flags,
+        dependency=None,
+        exclusive=cluster_cfg.exclusive,
+        mem=None,
+        overcommit=False,
+        #time=time_limit,
+        nodes=nodes,
+        ntasks=ntasks,
+        ntasks_per_node=ntasks_per_node,
+        gpus_per_task=gpus_per_task or cluster_cfg.gpus_per_task,
+        gpus_per_node=gpus_per_node or cluster_cfg.gpus_per_node,
+        partition=cluster_cfg.partition,
+        account=cluster_cfg.account,
+        exclude=cluster_cfg.get("exclude"),
+    )
+
+#f"""
+##!/usr/bin/env bash
+
+## parameters
+##SBATCH --account=joc
+##SBATCH --comment='Triton Inference Server'
+##SBATCH --job-name=joc-bermuda:tritonserver_set_ft_175B_random_io_200_16_vocab_128k-type_GPT-tp_8-pp_1-data_fp16-int8_0-maxseq_216-mbs_1
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=1
+##SBATCH --open-mode=append
+##SBATCH --output={workspace_path}/cluster_workspace/submission_%j.out
+##SBATCH --partition=interactive
+##SBATCH --signal=USR1@90
+##SBATCH --time=0-02:00:00
+
+## command
+#srun --mpi pmix \
+#  --output /lustre/fsw/joc/piotrm/src/megatron/my_helper_scripts/TEST_WORKSPACE_RANDOM_20220623_175b_vocab128k_pp_1/cluster_workspace/submission_%j.out \
+#  --container-image gitlab-master.nvidia.com#dl/dgx/bignlp/infer:main-py3-base \
+#  --container-mounts /lustre/fsw/joc/piotrm/src/megatron/my_helper_scripts/TEST_WORKSPACE_RANDOM_20220623_175b_vocab128k_pp_1:/lustre/fsw/joc/piotrm/src/megatron/my_helper_scripts/TEST_WORKSPACE_RANDOM_20220623_175b_vocab128k_pp_1 \
+#  --unbuffered \
+#  bash -c 'export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 && export NCCL_LAUNCH_MODE=GROUP && echo ${SLURM_PROCID}.${SLURM_LOCALID}@$(hostname) && tritonserver --model-repository /lustre/fsw/joc/piotrm/src/megatron/my_helper_scripts/TEST_WORKSPACE_RANDOM_20220623_175b_vocab128k_pp_1/model_repo_ft_175B_random_io_200_16_vocab_128k-type_GPT-tp_8-pp_1-data_fp16-int8_0-maxseq_216-mbs_1 
+#"""
+
+#    benchmark_set_job_def = JobDefinition(
+#        name=f"{job_name_prefix}gpt benchmark",
+#        description=f"Run gpt benchmark",
+#        max_time_s=DEFAULT_BENCHMARK_TIME_MIN * MIN2S,
+#        container_image=training_container_image,
+#        commands=[
+#            f"export CUDA_VISIBLE_DEVICES={','.join(map(str, range(0, 8)))}",
+#            f"/opt/bignlp/FasterTransformer/build/bin/multi_gpu_gpt_sweep ",
+#            f"{config_path} ",
+#            f"{start_id_path} ",
+#            f"{workspace_path}/{output_path} ",
+#            f"{tensor_parallel_size} ",
+#            f"{pipeline_parallel_size}",
+#        ],
+##         directories_to_mount=[triton_model_repository_path],
+#        ports=[DEFAULT_HTTP_PORT, DEFAULT_GRPC_PORT, DEFAULT_METRIC_PORT],
+#        tasks_number=pipeline_parallel_size,
+#        tasks_number_per_node=1,
+#        gpus_number_per_task=tensor_parallel_size,
+#    )
+    LOGGER.info(f"[-] Submitted job for {benchmark_set_job_def.description}")
+    # benchmark_set_job = executor.submit(benchmark_set_job_def)
+    # benchmark_set_job.wait()
 
 
 def search_inference_config(base_cfg, cfg):
@@ -39,8 +242,6 @@ def search_inference_config(base_cfg, cfg):
     benchmark_cfg = inference_cfg.get("benchmark")
     max_batch_size = max(benchmark_cfg.batch_sizes)
 
-    sys.path.append(bignlp_scripts_path)
-    from bignlp.inference_scripts.benchmark import run_benchmark
 
     # Process container-mounts.
     mounts_str = f"{bignlp_hp_tool_path}:{bignlp_hp_tool_path},{base_results_dir}:{base_results_dir}"
