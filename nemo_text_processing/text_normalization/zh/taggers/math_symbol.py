a7c5c52e43a44730ab575a9f0d139475a12dfb57
Zhenxiang Ma <59506191+mzxcpp@users.noreply.github.com>
2022-08-29 10:21:37 -0700

    add import in math
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-6-91.us-west-1.compute.internal>

--- nemo_text_processing/text_normalization/zh/taggers/math_symbol.py
+++ nemo_text_processing/text_normalization/zh/taggers/math_symbol.py
@@ -14,6 +14,7 @@
 import pynini
 from nemo_text_processing.text_normalization.zh.graph_utils import GraphFst
 from nemo_text_processing.text_normalization.zh.taggers.cardinal import Cardinal
+from nemo_text_processing.text_normalization.zh.utils import get_abs_path
 from pynini.lib import pynutil
 
 
