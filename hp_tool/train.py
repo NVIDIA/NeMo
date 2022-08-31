90ce2e19f0c99ee3444fc140aad2ffee6e96f2c2
markelsanz14 <markelsanz14@gmail.com>
2022-08-31 15:10:21 -0700

    Update conf/config.yaml, hp_tool/train.py, tests/ci_tests/selene/scripts/train/gpt3/126m_40gb_3runs_1node.sh

--- hp_tool/train.py
+++ hp_tool/train.py
@@ -8,10 +8,12 @@ def run_training(file_name, bignlp_hp_tool_path, bignlp_scripts_path, model_name
     """
     training_container = cfg.get("training_container")
     bignlp_path = cfg.get("bignlp_scripts_path")
+    data_dir = cfg.get("data_dir")
+    bignlp_ci = f"BIGNLP_CI=1" if bool(os.getenv('BIGNLP_CI')) else ""
 
     main_path = os.path.join(bignlp_scripts_path, "main.py")
     file_name = file_name.replace('.yaml', '')
-    cmd = f"python3 {main_path} training={model_name}/{file_name} base_results_dir={results_dir} container={training_container} stages=[training] bignlp_path={bignlp_path} training.exp_manager.create_checkpoint_callback=False "
+    cmd = f"HYDRA_FULL_ERROR=1 {bignlp_ci} python3 {main_path} training={model_name}/{file_name} base_results_dir={results_dir} container={training_container} stages=[training] bignlp_path={bignlp_path} data_dir={data_dir} training.exp_manager.create_checkpoint_callback=False "
     job_output = subprocess.check_output([cmd], shell=True).decode("utf-8")
     job_id = job_output.split(" ")[-1]
     print(f"Submitted Training script with job id: {job_id}")
