00bc876b756db162290f154010d64125a2902520
Virginia Adams <vadams@nvidia.com>
2022-08-31 15:08:00 +0000

    Python style fix
    Signed-off-by: Virginia Adams <vadams@nvidia.com>

--- nemo/collections/nlp/modules/common/prompt_encoder.py
+++ nemo/collections/nlp/modules/common/prompt_encoder.py
@@ -24,9 +24,11 @@ from nemo.core.neural_types import ChannelType, NeuralType
 
 __all__ = ['PromptEncoder']
 
+
 class PromptEncoderType(enum.Enum):
     LSTM = 'lstm'
     MLP = 'mlp'
+
 
 class PromptEncoder(NeuralModule, Exportable):
     """
@@ -44,7 +46,15 @@ class PromptEncoder(NeuralModule, Exportable):
     def output_types(self) -> Optional[Dict[str, NeuralType]]:
         return {"output_embeds": NeuralType(('B', 'T', 'C'), ChannelType())}
 
-    def __init__(self, encoder_type: enum, total_virtual_tokens: int, token_dim: int, hidden_size, lstm_dropout: float, num_layers: int):
+    def __init__(
+        self,
+        encoder_type: enum,
+        total_virtual_tokens: int,
+        token_dim: int,
+        hidden_size,
+        lstm_dropout: float,
+        num_layers: int,
+    ):
         """
         Initializes the PromptEncoder module.
         Args:
@@ -54,6 +64,7 @@ class PromptEncoder(NeuralModule, Exportable):
             num_layers: number of layers used in the LSTM
         """
         super().__init__()
+        self.token_dim = token_dim
         self.input_size = token_dim
         self.output_size = token_dim
         self.hidden_size = hidden_size
@@ -64,7 +75,7 @@ class PromptEncoder(NeuralModule, Exportable):
         self.register_buffer('indices', torch.LongTensor(list(range(self.total_virtual_tokens))))
 
         # embedding
-        self.embedding = torch.nn.Embedding(self.total_virtual_tokens, self.hidden_size)
+        self.embedding = torch.nn.Embedding(self.total_virtual_tokens, self.token_dim)
 
         if self.encoder_type == PromptEncoderType.LSTM:
             # LSTM
@@ -78,12 +89,16 @@ class PromptEncoder(NeuralModule, Exportable):
             )
 
             self.mlp_head = nn.Sequential(
-                nn.Linear(self.hidden_size * 2, self.hidden_size * 2), nn.ReLU(), nn.Linear(self.hidden_size * 2, self.output_size)
+                nn.Linear(self.hidden_size * 2, self.hidden_size * 2),
+                nn.ReLU(),
+                nn.Linear(self.hidden_size * 2, self.output_size),
             )
-        
+
         elif self.encoder_type == PromptEncoderType.MLP:
             if num_layers <= 1:
-                raise ValueError("The MLP prompt encoder must have at least 2 layers, and exactly 2 layers is recommended.")
+                raise ValueError(
+                    "The MLP prompt encoder must have at least 2 layers, and exactly 2 layers is recommended."
+                )
 
             layers = [nn.Linear(self.input_size, self.hidden_size), nn.ReLU()]
             for _ in range(num_layers - 2):
@@ -91,7 +106,7 @@ class PromptEncoder(NeuralModule, Exportable):
 
             layers.append(nn.Linear(self.hidden_size, self.output_size))
             self.mlp_head = nn.Sequential(*layers)
-        
+
         else:
             raise ValueError("Prompt encoder type not recognized. Please use one of MLP (recommended) or LSTM.")
 
@@ -99,13 +114,13 @@ class PromptEncoder(NeuralModule, Exportable):
     def forward(self, taskname_embeddings) -> torch.Tensor:
         input_embeds = self.embedding(self.indices).unsqueeze(0)
         batch_size, task_seq_length, _ = taskname_embeddings.shape
-        input_embeds = input_embeds.expand(batch_size, self.total_virtual_tokens, self.hidden_size).clone()
+        input_embeds = input_embeds.expand(batch_size, self.total_virtual_tokens, self.token_dim).clone()
         length = min(task_seq_length, self.total_virtual_tokens)
 
         # Replace general input with task specific embeddings to specify the correct task
         input_embeds[:, 0:length, :] = taskname_embeddings[:, 0:length, :]
-        
-        if self.encoder_type == PromptEncoderType.LSTM: 
+
+        if self.encoder_type == PromptEncoderType.LSTM:
             output_embeds = self.mlp_head(self.lstm_head(input_embeds)[0])
         elif self.encoder_type == PromptEncoderType.MLP:
             output_embeds = self.mlp_head(input_embeds)
