7704f38c3b09f6e42c0d249d4d7e567e5c85f84f
George <37293288+Jorjeous@users.noreply.github.com>
2022-09-01 18:16:47 +0300

    add new models  (#4852)
    * add pretrained Belarusian ASR checkpoints
    
    Signed-off-by: George Zelenfroind <gzelenfroind@nvidia.com>
    Signed-off-by: Matvei Novikov <mattyson.so@gmail.com>

--- nemo/collections/asr/models/rnnt_bpe_models.py
+++ nemo/collections/asr/models/rnnt_bpe_models.py
@@ -210,6 +210,14 @@ class EncDecRNNTBPEModel(EncDecRNNTModel, ASRBPEMixin):
             location="https://api.ngc.nvidia.com/v2/models/nvidia/nemo/stt_kab_conformer_transducer_large/versions/1.12.0/files/stt_kab_conformer_transducer_large.nemo",
         )
         results.append(model)
+
+        model = PretrainedModelInfo(
+            pretrained_model_name="stt_be_conformer_transducer_large",
+            description="For details about this model, please visit https://ngc.nvidia.com/catalog/models/nvidia:nemo:stt_be_conformer_transducer_large",
+            location="https://api.ngc.nvidia.com/v2/models/nvidia/nemo/stt_be_conformer_transducer_large/versions/1.12.0/files/stt_be_conformer_transducer_large.nemo",
+        )
+        results.append(model)
+
         return results
 
     def __init__(self, cfg: DictConfig, trainer: Trainer = None):
