5b601970f5892b2edb42805399cfe30b7ec7741f
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2022-09-06 11:00:27 -0700

    Style
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
+++ examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
@@ -104,10 +104,7 @@ def convert(local_rank, rank, world_size, args):
     app_state.pipeline_model_parallel_size = args.pipeline_model_parallel_size
     app_state.tensor_model_parallel_size = args.tensor_model_parallel_size
     # Auto set split rank for T5, BART, NMT if split rank is None.
-    if (
-        args.pipeline_model_parallel_size > 1
-        and args.model_type in ['t5', 'bart', 'nmt']
-    ):
+    if args.pipeline_model_parallel_size > 1 and args.model_type in ['t5', 'bart', 'nmt']:
         if args.pipeline_model_parallel_split_rank is not None:
             app_state.pipeline_model_parallel_split_rank = args.pipeline_model_parallel_split_rank
         else:
