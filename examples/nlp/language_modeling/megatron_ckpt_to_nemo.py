56b4166edf805a68facb71a5ba4612cce0ff3d72
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2022-09-06 10:09:15 -0700

    Fix nmt and ckpt_to_nemo
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
+++ examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
@@ -106,7 +106,6 @@ def convert(local_rank, rank, world_size, args):
     # Auto set split rank for T5, BART, NMT if split rank is None.
     if (
         args.pipeline_model_parallel_size > 1
-        and args.pipeline_model_parallel_split_rank is None
         and args.model_type in ['t5', 'bart', 'nmt']
     ):
         if args.pipeline_model_parallel_split_rank is not None:
@@ -119,6 +118,9 @@ def convert(local_rank, rank, world_size, args):
             else:
                 # If split rank is not set, then we set it to be pipeline_model_parallel_size // 2 - this is because in most cases we have the same number of enc/dec layers.
                 app_state.pipeline_model_parallel_split_rank = args.pipeline_model_parallel_size // 2
+    else:
+        app_state.pipeline_model_parallel_split_rank = None
+
     app_state.model_parallel_size = app_state.tensor_model_parallel_size * app_state.pipeline_model_parallel_size
 
     parallel_state.initialize_model_parallel(
