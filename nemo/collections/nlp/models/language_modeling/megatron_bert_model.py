a648d51aeb8bb0502eb7e234d8703aaa546c304b
Micha Livne <michalivne@users.noreply.github.com>
2022-09-07 23:08:17 +0300

    Fixed output of BERT to be [batch x seq x hidden] (#4887)
    * 1. Fixed output of BERT to be [batch x seq x hidden]
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Fixed returned dimensions of loss and logits.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Debugging.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Fixed style.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Debugging.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Fixed style.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Debugging.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Fixed internal inconsistency of dimensions.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    * 1. Debugging.
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    
    Signed-off-by: Micha Livne <mlivne@cs.toronto.edu>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_bert_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_bert_model.py
@@ -45,7 +45,8 @@ except (ImportError, ModuleNotFoundError):
 
 class MegatronBertModel(MegatronBaseModel):
     """
-    Megatron Bert pretraining
+    Megatron Bert pretraining.
+    Model returns [batch, seq, hidden] shape
     """
 
     def __init__(self, cfg: DictConfig, trainer: Trainer):
@@ -96,6 +97,19 @@ class MegatronBertModel(MegatronBaseModel):
 
     def forward(self, input_ids, attention_mask, token_type_ids, lm_labels=None):
         output_tensor = self.model(input_ids, attention_mask, token_type_ids=token_type_ids, lm_labels=lm_labels)
+
+        # Return the output tensor of encoder and transpose from [seq_len, batch, hidden] to [batch, seq_len, hidden]
+        if torch.is_tensor(output_tensor):
+            output_tensor = output_tensor.transpose(1, 0).contiguous()
+        else:
+            lm_loss_, sop_logits = output_tensor
+
+            lm_loss_ = lm_loss_.transpose(1, 0).contiguous()
+            if sop_logits is not None:
+                sop_logits = sop_logits.transpose(1, 0).contiguous()
+
+            output_tensor = (lm_loss_, sop_logits)
+
         return output_tensor
 
     def training_step(self, batch, batch_idx):
