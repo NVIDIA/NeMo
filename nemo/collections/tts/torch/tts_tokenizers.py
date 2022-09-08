c28dcce8639c33819d012e9b473b0a1e8bc635c3
Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
2022-09-07 21:15:53 -0700

    add TODO because all ngc checkpoints paths need to update.
    Signed-off-by: Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>

--- nemo/collections/tts/torch/tts_tokenizers.py
+++ nemo/collections/tts/torch/tts_tokenizers.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# TODO (xueyang): deprecate this file since no other places import modules from here anymore. However,
+#  all checkpoints uploaded in ngc used this path. So it requires to update all ngc checkpoints path as well.
 from nemo.collections.common.tokenizers.text_to_speech.tts_tokenizers import (
     BaseCharsTokenizer,
     BaseTokenizer,
