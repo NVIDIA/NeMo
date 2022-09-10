d4ae42cbb588fedfc25b6ae2d5283293d98c7a76
Ryan Langman <rlangman@nvidia.com>
2022-09-10 07:38:07 -0700

    Add mixed-representation config to PhonemizerTokenizer (#4904)
    * Add mixed-representation config to PhonemizerTokenizer
    
    Signed-off-by: Ryan <rlangman@nvidia.com>
    
    * Update FastPitch config, locale string
    
    Signed-off-by: Ryan <rlangman@nvidia.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- tests/nemo_text_processing/g2p/test_modules.py
+++ tests/nemo_text_processing/g2p/test_modules.py
@@ -22,13 +22,64 @@ class TestModules:
 
     PHONEME_DICT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "phoneme_dict", "test_dict.txt")
 
+    @staticmethod
+    def _create_g2p(
+        phoneme_dict=PHONEME_DICT_PATH,
+        apply_to_oov_word=lambda x: x,
+        use_chars=False,
+        phoneme_probability=None,
+        set_graphemes_upper=True,
+    ):
+        return IPAG2P(
+            phoneme_dict,
+            apply_to_oov_word=apply_to_oov_word,
+            use_chars=use_chars,
+            phoneme_probability=phoneme_probability,
+            set_graphemes_upper=set_graphemes_upper,
+        )
+
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_ipa_g2p_parse_dict(self):
+        # fmt: off
+        expected_symbols = {
+            'h', 'ə', 'ˈ', 'ɫ', 'o', 'ʊ', 'ˈ',
+            'w', 'ɝ', 'ɫ', 'd'
+        }
+        # fmt: on
+        g2p = self._create_g2p()
+
+        assert expected_symbols == g2p.symbols
+        assert len(g2p.phoneme_dict["HELLO"]) == 1
+        assert len(g2p.phoneme_dict["WORLD"]) == 1
+        assert g2p.phoneme_dict["HELLO"][0] == [char for char in "həˈɫoʊ"]
+        assert g2p.phoneme_dict["WORLD"][0] == [char for char in "ˈwɝɫd"]
+
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_ipa_g2p_parse_dict_with_chars(self):
+        # fmt: off
+        expected_symbols = {
+            'H', 'E', 'L', 'L', 'O',
+            'W', 'O', 'R', 'L', 'D',
+            'h', 'ə', 'ˈ', 'ɫ', 'o', 'ʊ',
+            'ˈ', 'w', 'ɝ', 'ɫ', 'd'
+        }
+        # fmt: on
+        g2p = self._create_g2p(use_chars=True)
+
+        assert expected_symbols == g2p.symbols
+        assert len(g2p.phoneme_dict["HELLO"]) == 1
+        assert len(g2p.phoneme_dict["WORLD"]) == 1
+        assert g2p.phoneme_dict["HELLO"][0] == [char for char in "həˈɫoʊ"]
+        assert g2p.phoneme_dict["WORLD"][0] == [char for char in "ˈwɝɫd"]
+
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
     def test_ipa_g2p(self):
         input_text = "Hello world."
         expected_output = [char for char in "həˈɫoʊ ˈwɝɫd."]
-
-        g2p = IPAG2P(self.PHONEME_DICT_PATH)
+        g2p = self._create_g2p()
 
         phonemes = g2p(input_text)
         assert phonemes == expected_output
@@ -38,8 +89,37 @@ class TestModules:
     def test_ipa_g2p_with_oov(self):
         input_text = "Hello Kitty!"
         expected_output = [char for char in "həˈɫoʊ KITTY!"]
+        g2p = self._create_g2p()
 
-        g2p = IPAG2P(self.PHONEME_DICT_PATH)
+        phonemes = g2p(input_text)
+        assert phonemes == expected_output
+
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_ipa_g2p_with_oov_func(self):
+        input_text = "Hello Kitty!"
+        expected_output = [char for char in "həˈɫoʊ test!"]
+        g2p = self._create_g2p(apply_to_oov_word=lambda x: "test")
+
+        phonemes = g2p(input_text)
+        assert phonemes == expected_output
+
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_ipa_g2p_graphemes(self):
+        input_text = "Hello world."
+        expected_output = [char for char in input_text.upper()]
+        g2p = self._create_g2p(use_chars=True, phoneme_probability=0.0)
+
+        phonemes = g2p(input_text)
+        assert phonemes == expected_output
+
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_ipa_g2p_graphemes_lower(self):
+        input_text = "Hello world."
+        expected_output = [char for char in input_text.lower()]
+        g2p = self._create_g2p(use_chars=True, phoneme_probability=0.0, set_graphemes_upper=False)
 
         phonemes = g2p(input_text)
         assert phonemes == expected_output
