c3f9a4e3c4ad8b24eb27dcf2170a76da648deb68
Sean Naren <snarenthiran@nvidia.com>
2022-09-12 23:26:57 +0100

    Add support for skipping validation on resume + extend saving last ckpt test (#4922)
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo/collections/nlp/models/machine_translation/megatron_nmt_model.py
+++ nemo/collections/nlp/models/machine_translation/megatron_nmt_model.py
@@ -327,8 +327,6 @@ class MegatronNMTModel(MegatronLMEncoderDecoderModel):
         return self.eval_epoch_end(outputs, 'test')
 
     def eval_epoch_end(self, outputs, mode):
-        if not outputs:
-            return
         if isinstance(outputs[0], dict):
             outputs = [outputs]
 
