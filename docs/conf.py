7a611c063c7e2684aca2b77e5d90ce7bcfb8ee08
markelsanz14 <markelsanz14@gmail.com>
2022-09-19 15:12:10 -0700

    Documentation improved.

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BigNLP HP Tool'
+copyright = '2022, Markel Ausin'
+author = 'Markel Ausin'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- hp_tool/train.py
+++ hp_tool/train.py
@@ -1,3 +1,5 @@
+"""Module to launch training jobs using bignlp-scripts."""
+
 import os
 import shutil
 import subprocess
@@ -5,25 +7,82 @@ import subprocess
 from omegaconf import OmegaConf
 
 
-def run_training(file_name, bignlp_scripts_path, model_name, results_dir, cfg):
+def run_training(file_name: str, model_name: str, results_dir: str, cfg: OmegaConf) -> int:
     """
-    Main function to launch a training job, with the config given in cfg.
-    """
-    training_container = cfg.get("training_container")
-    data_dir = cfg.get("data_dir")
-    cluster_cfg = cfg.get("cluster")
-    bignlp_ci = f"BIGNLP_CI=1" if bool(os.getenv('BIGNLP_CI')) else ""
-    
-    # Copy cluster config to bignlp-scripts.
-    dst = os.path.join(bignlp_scripts_path, "conf/cluster/bcm.yaml")
-    with open(dst, "w") as f:
-        OmegaConf.save(config=cluster_cfg, f=f)
-    print(f"Cluster config copied to {dst}")
+    Launch a training job for the given model name and config file, using bignlp-scripts.
 
+    :param str file_name: name of the file configuration to be selected for training with bignlp-scripts.
+    :param str model_name: model type to be run, usually gpt3, t5 or mt5.
+    :param str results_dir: path to the directory where the results will be stored.
+    :param OmegaConf cfg: OmegaConf object with full configuration for the HP tool.
+    :return: SLURM job_id of the training job that was launched.
+    :rtype: int
+    """
+    # Copy cluster config to bignlp-scripts.
+    bignlp_scripts_path = cfg.get("bignlp_scripts_path")
+    cluster_cfg = cfg.get("cluster")
+    dst = os.path.join(bignlp_scripts_path, "conf/cluster/bcm.yaml")
+    copy_cluster_config()
+    print(f"Copied cluster config to {dst}")
+
+    # Generate string of hydra overrides for bignlp-scripts.
+    overrides_str = generate_overrides_str(cfg)
+
+    bignlp_ci = f"BIGNLP_CI=1" if bool(os.getenv("BIGNLP_CI")) else ""
     main_path = os.path.join(bignlp_scripts_path, "main.py")
-    file_name = file_name.replace('.yaml', '')
-    cmd = f"HYDRA_FULL_ERROR=1 {bignlp_ci} python3 {main_path} training={model_name}/{file_name} base_results_dir={results_dir} \"container='{training_container}'\" stages=[training] bignlp_path={bignlp_scripts_path} data_dir={data_dir} training.exp_manager.create_checkpoint_callback=False "
+    cmd = f"HYDRA_FULL_ERROR=1 {bignlp_ci} python3 {main_path} {overrides_str} "
+
+    # Launch job with command cmd.
     job_output = subprocess.check_output([cmd], shell=True).decode("utf-8")
     job_id = job_output.split(" ")[-1]
     print(f"Submitted Training script with job id: {job_id}")
     return job_id
+
+
+def copy_config(cfg: OmegaConf, dst: str) -> None:
+    """
+    Copies OmegaConf configuration to a dst file.
+
+    :param OmegaConf cfg: OmegaConfg object with the config to be stored in a file.
+    :param str dst: destination path to where the config will be stored. Must be a yaml file.
+    :return: None
+    """
+    with open(dst, "w") as f:
+        OmegaConf.save(config=cfg, f=f)
+
+
+def generate_overrides_str(
+    file_name: str, model_name: str, results_dir: str, cfg: OmegaConf
+) -> str:
+    """
+    Generates string with hydra-like parameter overrides for bignlp-scripts.
+
+    :param str file_name: name of the file configuration to be selected for training with bignlp-scripts.
+    :param str model_name: model type to be run, usually gpt3, t5 or mt5.
+    :param str results_dir: path to the directory where the results will be stored.
+    :param OmegaConf cfg: OmegaConf object with full configuration for the HP tool.
+    :return: string containing all the hydra-like overrides required for the training job.
+    :rtype: str
+    """
+    file_name = file_name.replace(".yaml", "")
+    training_model = f"{model_name}/{file_name}"
+    cluster_type = cfg.get("cluster_type")
+    container = cfg.get("training_container")
+    bignlp_scripts_path = cfg.get("bignlp_scripts_path")
+    data_dir = cfg.get("data_dir")
+    container_mounts = cfg.get("container_mounts")
+    api_key_file = cfg.get("wandb").get("api_key_file")
+
+    overrides_str = (
+        f"training={training_model} "
+        f"stages=[training] "
+        f"cluster_type={cluster_type} "
+        f"base_results_dir={results_dir} "
+        f"\"container='{container}'\" "
+        f"bignlp_path={bignlp_scripts_path} "
+        f"data_dir={data_dir} "
+        f"training.exp_manager.create_checkpoint_callback=False "
+        f"container_mounts={container_mounts} "
+        f"wandb_api_key_file={api_key_file} "
+    )
+    return overrides_str
