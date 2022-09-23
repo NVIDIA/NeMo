24d1fedca6b2133d06490c6234b2daac982f3ff0
markelsanz14 <markelsanz14@gmail.com>
2022-09-23 16:41:58 -0700

    Update tests/ci_tests/selene/pytest/train/gpt3/test_gpt3_training_search.py

--- tests/ci_tests/selene/pytest/train/gpt3/test_gpt3_training_search.py
+++ tests/ci_tests/selene/pytest/train/gpt3/test_gpt3_training_search.py
@@ -10,11 +10,11 @@ class TestCIGPT126m:
 
     def test_ci_train_gpt3_126m_40gb_3runs(self):
         base_cfg = os.path.join(BASE_RESULTS_DIR, f"gpt3/{RUN_SIZE}_{GPU_MEM}gb/base_cfg_{RUN_SIZE}.yaml")
-        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/{RUN_SIZE}_{GPU_MEM}gb/candidate_configs")
-        training_logs_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/{RUN_SIZE}_{GPU_MEM}gb/training_logs")
-        final_result_dir = os.path.join(BASE_RESULTS_DIR, "gpt3/{RUN_SIZE}_{GPU_MEM}gb/final_result")
-        final_summary_csv = os.path.join(BASE_RESULTS_DIR, "gpt3/{RUN_SIZE}_{GPU_MEM}gb/final_result/final_summary_{NODES}nodes.csv")
-        optimal_cfg = os.path.join(BASE_RESULTS_DIR, "gpt3/{RUN_SIZE}_{GPU_MEM}gb/final_result/optimal_config_{RUN_SIZE}_{NODES}nodes.yaml")
+        candidate_configs_dir = os.path.join(BASE_RESULTS_DIR, f"gpt3/{RUN_SIZE}_{GPU_MEM}gb/candidate_configs")
+        training_logs_dir = os.path.join(BASE_RESULTS_DIR, f"gpt3/{RUN_SIZE}_{GPU_MEM}gb/training_logs")
+        final_result_dir = os.path.join(BASE_RESULTS_DIR, f"gpt3/{RUN_SIZE}_{GPU_MEM}gb/final_result")
+        final_summary_csv = os.path.join(BASE_RESULTS_DIR, f"gpt3/{RUN_SIZE}_{GPU_MEM}gb/final_result/final_summary_{NODES}nodes.csv")
+        optimal_cfg = os.path.join(BASE_RESULTS_DIR, f"gpt3/{RUN_SIZE}_{GPU_MEM}gb/final_result/optimal_config_{RUN_SIZE}_{NODES}nodes.yaml")
 
         assert os.path.exists(base_cfg), f"File not found: {base_cfg}"
         assert os.path.exists(candidate_configs_dir), f"Dir not found: {candidate_configs_dir}"
