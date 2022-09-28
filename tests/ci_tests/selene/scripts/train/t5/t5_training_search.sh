009042bede7b361852d5d7766395b461ddbf1a4e
markelsanz14 <markelsanz14@gmail.com>
2022-09-27 18:14:21 -0700

    Update tests/ci_tests/selene/scripts/train/gpt3/gpt3_training_search.sh, tests/ci_tests/selene/scripts/train/mt5/mt5_training_search.sh, tests/ci_tests/selene/scripts/train/t5/t5_training_search.sh

--- tests/ci_tests/selene/scripts/train/t5/t5_training_search.sh
+++ tests/ci_tests/selene/scripts/train/t5/t5_training_search.sh
@@ -11,6 +11,7 @@ HYDRA_FULL_ERROR=1 BIGNLP_CI=1 python3 main.py \
     cluster.job_name_prefix="${SLURM_ACCOUNT}-bignlp_hp_tool:" \
     cluster.gpus_per_task=null \
     cluster.gpus_per_node=null \
+    search_config.train_settings.num_nodes=${NODES} \
     search_config.train_settings.gpu_memory_gb=${GPU_MEM} \
     search_config.train_settings.limit_search_runs=${RUNS} \
     search_config.train_settings.tensor_parallel_sizes=${TP_SIZES} \
