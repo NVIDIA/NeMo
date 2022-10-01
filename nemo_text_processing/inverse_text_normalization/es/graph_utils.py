35ef62f824098612c30200534cc5834d0d1baaec
anand-nv <105917641+anand-nv@users.noreply.github.com>
2022-10-01 09:36:11 +0530

    Fix wrong import
    Fix wrong import

--- nemo_text_processing/inverse_text_normalization/es/graph_utils.py
+++ nemo_text_processing/inverse_text_normalization/es/graph_utils.py
@@ -14,7 +14,7 @@
 
 
 import pynini
-from nemo_text_processing.text_normalization.es.utils import get_abs_path
+from nemo_text_processing.inverse_text_normalization.es.utils import get_abs_path
 from pynini.lib import pynutil
 
 
