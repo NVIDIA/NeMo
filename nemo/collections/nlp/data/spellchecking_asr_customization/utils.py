545598f84816bc8a555989dfad4a120ad0ce0f0d
Alexandra Antonova <aleksandraa@nvidia.com>
2022-10-12 17:38:25 +0300

    bug fixes
    Signed-off-by: Alexandra Antonova <aleksandraa@nvidia.com>

---
+++ nemo/collections/nlp/data/spellchecking_asr_customization/utils.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import re
+from itertools import groupby
+from typing import Dict, List, Tuple
+
+"""Utility functions for Spellchecking ASR Customization."""
+
+
+def get_token_list(text: str) -> List[str]:
+    """Returns a list of tokens.
+
+    This function expects that the tokens in the text are separated by space
+    character(s). Example: "ca n't , touch". This is the case at least for the
+    public DiscoFuse and WikiSplit datasets.
+
+    Args:
+        text: String to be split into tokens.
+    """
+    return text.split()
+
+
+def read_label_map(path: str) -> Dict[str, int]:
+    """Return label map read from the given path."""
+    with open(path, 'r') as f:
+        label_map = {}
+        empty_line_encountered = False
+        for tag in f:
+            tag = tag.strip()
+            if tag:
+                label_map[tag] = len(label_map)
+            else:
+                if empty_line_encountered:
+                    raise ValueError('There should be no empty lines in the middle of the label map ' 'file.')
+                empty_line_encountered = True
+        return label_map
+
+
