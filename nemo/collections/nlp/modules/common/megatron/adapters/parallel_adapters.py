2b15399d387ff78c24ae1b88091a669b236caee4
Somshubra Majumdar <titu1994@gmail.com>
2022-10-14 11:33:13 -0700

    Fix types for ParallelAdapterConfig
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/adapters/parallel_adapters.py
+++ nemo/collections/nlp/modules/common/megatron/adapters/parallel_adapters.py
@@ -17,7 +17,7 @@
 import enum
 import logging
 from dataclasses import dataclass
-from typing import Optional
+from typing import Any, Optional
 
 import torch
 import torch.nn as nn
@@ -167,5 +167,5 @@ class ParallelLinearAdapterConfig:
     column_init_method: str = 'xavier'
     row_init_method: str = 'zero'
     dropout: float = 0.0
-    adapter_strategy: Optional[dict] = adapter_mixin_strategies.ResidualAddAdapterStrategyConfig()
+    adapter_strategy: Optional[Any] = adapter_mixin_strategies.ResidualAddAdapterStrategyConfig()
     _target_: str = "{0}.{1}".format(ParallelLinearAdapter.__module__, ParallelLinearAdapter.__name__)
