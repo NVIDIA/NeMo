8553b9e1a6f211bb5cb27a1e3fa910d5297c89ea
SeanNaren <snarenthiran@nvidia.com>
2022-10-25 12:33:17 +0100

    Fix avg
    Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

--- nemo/collections/asr/metrics/running_avg.py
+++ nemo/collections/asr/metrics/running_avg.py
@@ -5,14 +5,15 @@ from torchmetrics import Metric
 class RunningAverage(Metric):
     """Compute running average of a loss."""
 
-    full_state_update: bool = False
+    full_state_update: bool = True
 
     def __init__(self):
         super().__init__()
-        self.add_state('sum', default=torch.tensor(0, dtype=torch.float), dist_reduce_fx='sum')
-        self.add_state('count', default=torch.tensor(0, dtype=torch.int), dist_reduce_fx='sum')
+        self.add_state('sum', default=torch.tensor(0, dtype=torch.float), dist_reduce_fx='sum', persistent=True)
+        self.add_state('count', default=torch.tensor(0, dtype=torch.int), dist_reduce_fx='sum', persistent=True)
 
     def compute(self):
+        print(f"current state, {self.sum} {self.count}")
         return self.sum / self.count
 
     def update(self, val, n=1) -> None:
