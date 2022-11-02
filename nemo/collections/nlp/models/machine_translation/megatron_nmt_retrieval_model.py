35adc1f67fe6064b104c8a4093c6993a099502d8
soumye <singhalsoumye@gmail.com>
2022-11-02 12:32:08 -0700

    train with no grad-accum working
    Signed-off-by: soumye <singhalsoumye@gmail.com>

--- nemo/collections/nlp/models/machine_translation/megatron_nmt_retrieval_model.py
+++ nemo/collections/nlp/models/machine_translation/megatron_nmt_retrieval_model.py
@@ -12,129 +12,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import itertools
-import json
-import random
-from multiprocessing import Value
-from pathlib import Path
-from typing import Dict, List, Optional, Union
+from typing import List
 
 import numpy as np
 import torch
-import torch.distributed as dist
-import torch.utils.data as pt_data
 from torch.utils.data import Dataset
-from omegaconf import DictConfig, ListConfig, OmegaConf, open_dict
+from omegaconf import DictConfig, OmegaConf, open_dict
 from pytorch_lightning import Trainer
-from pytorch_lightning.utilities import rank_zero_only
-from sacrebleu import corpus_bleu
 
-from nemo.collections.common.data import ConcatDataset
-from nemo.collections.common.losses import NLLLoss
-from nemo.collections.nlp.models.machine_translation.mt_enc_dec_config import MTRetrievalModelConfig
 from nemo.collections.nlp.models.machine_translation.megatron_nmt_model import MegatronNMTModel
 from nemo.collections.nlp.models.language_modeling.megatron_bart_model import MegatronBARTModel
 from nemo.collections.nlp.models.machine_translation.mt_enc_dec_model import MTEncDecModel
-from nemo.collections.nlp.modules.common.transformer import AttentionBridge, TopKSequenceGenerator
-from nemo.collections.nlp.data import RetrievalTranslationDataset, TarredTranslationDataset, TranslationDataset
-from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector, NLPDDPStrategy
-from nemo.core.classes.common import typecheck
-from nemo.utils import logging, model_utils, timers
-from nemo.utils.app_state import AppState
-from nemo.collections.nlp.modules.common.megatron.megatron_init import fake_initialize_model_parallel
+from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector
+from nemo.utils import logging
 
 from nemo.collections.nlp.modules.common.megatron.utils import (
-    ApexGuardDefaults,
     build_position_ids,
-    average_losses_across_data_parallel_group,
-    get_params_for_weight_decay_optimization,
+    average_losses_across_data_parallel_group
 )
-
-try:
-    from apex.transformer.enums import ModelType
-    from apex.transformer import parallel_state, tensor_parallel
-    from apex.transformer.pipeline_parallel.schedules.common import build_model
-    from apex.transformer.pipeline_parallel.schedules.fwd_bwd_no_pipelining import forward_backward_no_pipelining
-    from apex.transformer.pipeline_parallel.schedules.fwd_bwd_pipelining_without_interleaving import (
-        forward_backward_pipelining_without_interleaving,
-    )
-    from apex.transformer.pipeline_parallel.utils import (
-        get_num_microbatches,
-        _reconfigure_microbatch_calculator,
-        get_micro_batch_size,
-    )
-
-    HAVE_APEX = True
-except (ImportError, ModuleNotFoundError):
-    ModelType = ApexGuardDefaults()
-    HAVE_APEX = False
 
 __all__ = ['MegatronNMTRetrievalModel']
 
 class MegatronNMTRetrievalModel(MegatronNMTModel):
     """
     Proposed Retrieval-Model for NMT by conditioning on Perceiver embeddings
-    Things to implement
-    > See what methods to override and nothing more. Maybe make a new encoder class as well?
-    > Add Config file for this?
-    > load perceiver model
-    > compute perceiver embeddings
-    > condition on perceiver embeddings during train/test/val
-    > Not to add positional embeddings
+    Things to implement:
+    > condition on perceiver embeddings during val/test
+    > condition on raw text only
+    > Figure out max_seq_length constraint is satisfied or not
 
     Second step
-    > Implement cross attention styel
-    > Do monolingual stuff
+    > Implement cross attention style
+    > Do monolingual stuff using retro
     """
 
     def __init__(self, cfg: DictConfig, trainer: Trainer):
-        # when to call this? is this right time?
         super().__init__(cfg=cfg, trainer=trainer)
         
-        # TODO: Update from config
-        self.num_neighbors = 1
+        # TODO: Update for val/test set
+        self.num_neighbors = self.cfg.train_ds.num_neighbors
         # TODO: load configs params
-        # load the perceiver
-        # TODO: See if this is correct? 
-        # TODO: Do I need to call apex's build_model here?
-        # TODO: Need to extract encoder from MegatronBARTModel which is perceiver encoder and transformer decoder
+        # TODO: Throw away perceiver decoder to reduce memory?
         if self.cfg.retriever.type == 'perceiver':
-            # app_state = AppState()
-            # if cfg.tensor_model_parallel_size > 1 or cfg.pipeline_model_parallel_size > 1:
-            #     app_state.model_parallel_size = cfg.tensor_model_parallel_size * cfg.pipeline_model_parallel_size
-            #     (
-            #         app_state.tensor_model_parallel_rank,
-            #         app_state.pipeline_model_parallel_rank,
-            #         app_state.model_parallel_size,
-            #         app_state.data_parallel_size,
-            #         app_state.pipeline_model_parallel_split_rank,
-            #         app_state.virtual_pipeline_model_parallel_rank
-            #     ) = fake_initialize_model_parallel(
-            #         world_size=app_state.model_parallel_size,
-            #         rank=trainer.global_rank,
-            #         tensor_model_parallel_size_=cfg.tensor_model_parallel_size,
-            #         pipeline_model_parallel_size_=cfg.pipeline_model_parallel_size,
-            #         pipeline_model_parallel_split_rank_=cfg.pipeline_model_parallel_split_rank,
-            #     )
-            # if parallel_state.is_unitialized():
-            #     def placeholder():
-            #         return
-            #     if trainer.strategy.launcher is not None:
-            #         trainer.strategy.launcher.launch(placeholder, trainer=trainer)
-            #     trainer.strategy.setup_environment()
-
-            # Need to reconfigure micro batch calculator with apex for new p-tuning session
-
+            # load the perceiver
             pretrained_cfg = MegatronBARTModel.restore_from(cfg.retriever.get("encoder_path"), trainer=trainer, return_config=True)
-            # app_state = AppState()
-            # _reconfigure_microbatch_calculator(
-            #     rank=app_state.global_rank,
-            #     rampup_batch_size=None,
-            #     global_batch_size=pretrained_cfg.global_batch_size,
-            #     micro_batch_size=pretrained_cfg.micro_batch_size,
-            #     data_parallel_size=parallel_state.get_data_parallel_world_size(),
-            # )
-
             OmegaConf.set_struct(pretrained_cfg, True)
             with open_dict(pretrained_cfg):
                 pretrained_cfg.masked_softmax_fusion = False
@@ -150,23 +71,6 @@ class MegatronNMTRetrievalModel(MegatronNMTModel):
             self.retrieval_encoder.freeze()
         else:
             self.retrieval_encoder = None
-        # TODO See how to get retrieval tokenizer from the model
-
-
-    def encode_neighbor(self, padded_batch):
-        '''
-        Logic for computing latent tokens from perceiver embeddings
-        padded_batch: List of padded sentences to encode
-        '''
-        batch_size = len(padded_batch)
-        # 64 * 1024 etc
-        hidden_dim = self.retrieval_encoder.cfg.hidden_size * self.retrieval_encoder.cfg.hidden_steps
-        latents = np.zeros((batch_size, hidden_dim)).astype(np.float16)
-        padded_batch = torch.LongTensor(padded_batch).cuda()
-        mask = padded_batch != self.retrieval_encoder.tokenizer.pad_id
-        latent = self.retrieval_encoder.encode(padded_batch, mask)
-        latent = latent.contiguous().view(len(padded_batch), -1).data.cpu().numpy().astype(np.float16)
-        return latents
 
     def build_train_valid_test_datasets(self):
         """Builds the train, validation, and test datasets."""
@@ -242,7 +146,6 @@ class MegatronNMTRetrievalModel(MegatronNMTModel):
                     self._cfg.test_ds.nn_mapping,
                     self._cfg.test_ds.num_neighbors,
                 )
-            
 
     def process_global_batch_for_text_translation_datasets(self, batch):
         #TODO: See how to modify this for retrievals. This is called in eval_step
@@ -261,22 +164,25 @@ class MegatronNMTRetrievalModel(MegatronNMTModel):
 
         # Parent function will pad microbatches to the same length.
         return self._process_global_batch_without_megatron_batch_sampler([batch], tokenizer=self.encoder_tokenizer)
-        
+
     def process_global_batch(self, global_batch):
-        # ! modify for retrieval. This is called in train_step
-        # ! See what the collate function of retrievalseq2seq returns
-        return [
+        output = [
             global_batch["text_enc"],
             global_batch["text_dec"],
             global_batch["loss_mask"],
             global_batch["labels"],
             global_batch["enc_mask"],
-            global_batch["dec_mask"],
-            global_batch.get("nn_src_list", None),
-            global_batch.get("nn_tgt_list", None),
-            global_batch.get("nn_src_mask_list", None),
-            global_batch.get("nn_tgt_mask_list", None),
+            global_batch["dec_mask"]
         ]
+        if global_batch.get("nn_src_list", None) is None:
+            return output
+        else:
+            for idx in range(self.num_neighbors):
+                output.append(global_batch["nn_src_list"][idx])
+                output.append(global_batch["nn_tgt_list"][idx])
+                output.append(global_batch["nn_src_mask_list"][idx])
+                output.append(global_batch["nn_tgt_mask_list"][idx])
+            return output
 
     def get_forward_output_and_loss_func(self, val=False):
         """
@@ -291,12 +197,8 @@ class MegatronNMTRetrievalModel(MegatronNMTModel):
                 loss_mask, 
                 lm_labels, 
                 encoder_attn_mask, 
-                decoder_attn_mask, 
-                nn_src_ids, 
-                nn_tgt_ids,
-                nn_src_mask,
-                nn_tgt_mask, 
-            ) = batch
+                decoder_attn_mask
+            ) = batch[0:6]
             """
             1. Get the embeddings from the embedding matrix and add positional embeddings
             2. Get the perceiver encodings from self.retriever. Embedding dropout? 
@@ -304,24 +206,37 @@ class MegatronNMTRetrievalModel(MegatronNMTModel):
             4. Pass it to the model forward using encoder inputs option
             5. Figure out how to do batching here properly
             """
-
-            # Compute perceiver embeddings. Dim is [B x S x H]
-            latent_src = self.retrieval_encoder.encode(nn_src_ids, nn_src_mask)
-            latent_tgt = self.retrieval_encoder.encode(nn_tgt_ids, nn_tgt_mask)
-
             if model.encoder_cfg.get("position_embedding_type", "learned_absolute") != 'relative':
                 enc_position_ids = build_position_ids(encoder_input_ids)
             else:
                 enc_position_ids = None
 
             enc_input = model.encoder_embedding(encoder_input_ids, enc_position_ids, token_type_ids=None)
+            
+            enc_input_append = []
+            for idx in range(self.num_neighbors):
+                # Iterate over the neighbors and get the embeddings
+
+                nn_src_ids = batch[6 + (idx * 4) + 0]
+                nn_tgt_ids = batch[6 + (idx * 4) + 1]
+                nn_src_mask = batch[6 + (idx * 4) + 2]
+                nn_tgt_mask = batch[6 + (idx * 4) + 3]
+                
+                # Compute perceiver embeddings. Dim is [B x S x H]
+                latent_src = self.retrieval_encoder.encode(nn_src_ids, nn_src_mask)
+                latent_tgt = self.retrieval_encoder.encode(nn_tgt_ids, nn_tgt_mask)
+                enc_input_append.append(latent_src)
+                enc_input_append.append(latent_tgt)
+
+            enc_input_append.append(enc_input.transpose(0,1))
+            # join latents with the encoder input embeddings
+            # Concatenate along the sequence dimension
+            enc_input = torch.cat(enc_input_append, dim=1)
+            
+            # Modify the mask to account for the new sequence length
             prepend_length = 2 * self.num_neighbors * latent_src.shape[1]
             mask_prepend = torch.ones((encoder_attn_mask.shape[0], prepend_length)).to(encoder_attn_mask.device)
             encoder_attn_mask = torch.cat((mask_prepend, encoder_attn_mask), dim=1)
-
-            # join latents with the encoder input embeddings
-            # Concatenate along the sequence dimension
-            enc_input = torch.cat([latent_src, latent_tgt, enc_input.transpose(0,1)], dim=1)
             output = model(
                 enc_input_ids=None,
                 enc_attn_mask=encoder_attn_mask,
@@ -415,16 +330,18 @@ class RetrievalSeq2Seq(Dataset):
         self.train_text_memmap_dataset = train_text_memmap_dataset
         self.retrieval_text_memmap_dataset = retrieval_text_memmap_dataset
         self.num_neighbors = num_neighbors
+
         # Select only the number of nns specified
         self.nn_mapping = np.load(nn_mapping)[:, :num_neighbors]
+
         # len(self.train_text_memmap_dataset.src_indexed_dataset) is oversampled 
         assert len(self.train_text_memmap_dataset.src_indexed_dataset) == len(self.nn_mapping)
+        # TODO: Figure out how to do this for eval set
     
     def __len__(self):
         return len(self.train_text_memmap_dataset)
     
     def __getitem__(self, idx):
-        # ! Convert this to a dictionary i guess
         # Output format is a tuple of ((src, tgt), [list of neighbors in (src,tgt) format]))])
         return {
             'text_enc': self.train_text_memmap_dataset[idx]['text_enc'],
@@ -471,10 +388,9 @@ class RetrievalSeq2Seq(Dataset):
             retrieval_encoder_input_src_mask_list.append(retrieval_input_src_mask)
             retrieval_encoder_input_tgt_mask_list.append(retrieval_input_tgt_mask)
 
-        # TODO: This is a hack, fix this. Figure out how to do for multiple neighbors
-        output['nn_src_list'] = retrieval_encoder_input_src_list[0]
-        output['nn_tgt_list'] = retrieval_encoder_input_tgt_list[0]
-        output['nn_src_mask_list'] = retrieval_encoder_input_src_mask_list[0]
-        output['nn_tgt_mask_list'] = retrieval_encoder_input_tgt_mask_list[0]
+        output['nn_src_list'] = retrieval_encoder_input_src_list
+        output['nn_tgt_list'] = retrieval_encoder_input_tgt_list
+        output['nn_src_mask_list'] = retrieval_encoder_input_src_mask_list
+        output['nn_tgt_mask_list'] = retrieval_encoder_input_tgt_mask_list
 
         return output
