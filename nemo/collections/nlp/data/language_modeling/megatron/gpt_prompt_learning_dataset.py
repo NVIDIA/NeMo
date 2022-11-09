df4c4064419af6dca51c855245ff004e5f38f322
arendu <adithya.r@gmail.com>
2022-11-09 12:44:52 -0800

    style fix
    Signed-off-by: arendu <adithya.r@gmail.com>

--- nemo/collections/nlp/data/language_modeling/megatron/gpt_prompt_learning_dataset.py
+++ nemo/collections/nlp/data/language_modeling/megatron/gpt_prompt_learning_dataset.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import copy
 import json
 import os
 import pickle
-import copy
 
 import torch
 from tqdm.auto import tqdm
