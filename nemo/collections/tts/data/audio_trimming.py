fac97cfcd9c10c1261f57b3606f3683dec338f5e
Ryan <rlangman@nvidia.com>
2022-11-10 11:03:46 -0800

    [TTS] Remove unused import
    Signed-off-by: Ryan <rlangman@nvidia.com>

--- nemo/collections/tts/data/audio_trimming.py
+++ nemo/collections/tts/data/audio_trimming.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
 from abc import ABC, abstractmethod
 from typing import Tuple
 
