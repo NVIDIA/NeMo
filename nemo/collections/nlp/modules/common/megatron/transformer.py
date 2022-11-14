ae1a3cbf4c3399575b7aba3d2d7e074be80237f2
Abhinav Khattar <aklife97@gmail.com>
2022-11-14 12:54:15 -0800

    style
    Signed-off-by: Abhinav Khattar <aklife97@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/transformer.py
+++ nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -298,13 +298,15 @@ class SwitchMLP(MegatronModule):
         self.route_algo = SwitchMLP.sinkhorn
         self.router = torch.nn.Linear(kwargs['hidden_size'], num_experts)
         self.experts = torch.nn.ModuleList([ParallelMLP(**kwargs) for _ in range(num_experts)])
-    
+
     def forward(self, hidden_states):
         hidden_shape = hidden_states.shape
         route = self.router(hidden_states).view(-1, self.num_experts)
-        if self.training: 
+        if self.training:
             with torch.no_grad():
-                norm_route = self.route_algo(route.detach().to(dtype=torch.float32)) # explicit fp32 conversion for stability
+                norm_route = self.route_algo(
+                    route.detach().to(dtype=torch.float32)
+                )  # explicit fp32 conversion for stability
                 _, max_ind = torch.max(norm_route, dim=1)
             route = torch.sigmoid(route)
             max_prob = route[torch.arange(route.size(0)), max_ind]
@@ -316,28 +318,28 @@ class SwitchMLP(MegatronModule):
         hidden_states = hidden_states.view(-1, hidden_shape[-1])
 
         local_indices = (max_ind == 0).nonzero()
-        hidden = hidden_states[local_indices,:]
+        hidden = hidden_states[local_indices, :]
         output, output_bias = self.experts[0](hidden)
         output_bias = output_bias.expand_as(output)
 
         output_total = torch.empty_like(hidden_states, dtype=output.dtype)
         output_bias_total = torch.empty_like(hidden_states, dtype=output_bias.dtype)
 
-        output_total[local_indices,:] = output
-        output_bias_total[local_indices,:] = output_bias
+        output_total[local_indices, :] = output
+        output_bias_total[local_indices, :] = output_bias
 
         for expert_num, expert in enumerate(self.experts):
             if expert_num == 0:
                 continue
             local_indices = (max_ind == expert_num).nonzero()
-            hidden = hidden_states[local_indices,:]
+            hidden = hidden_states[local_indices, :]
             output, output_bias = expert(hidden)
             output_bias = output_bias.expand_as(output)
-            output_total[local_indices,:] = output
-            output_bias_total[local_indices,:] = output_bias
-        
-        output_total = output_total*max_prob
-        output_bias_total = output_bias_total*max_prob
+            output_total[local_indices, :] = output
+            output_bias_total[local_indices, :] = output_bias
+
+        output_total = output_total * max_prob
+        output_bias_total = output_bias_total * max_prob
         output_total = output_total.view(hidden_shape)
         output_bias_total = output_bias_total.view(hidden_shape)
 
@@ -350,16 +352,16 @@ class SwitchMLP(MegatronModule):
         cost = torch.exp(cost)
         d0 = torch.ones(cost.size(0), device=cost.device, dtype=cost.dtype)
         d1 = torch.ones(cost.size(1), device=cost.device, dtype=cost.dtype)
-        
+
         eps = 0.00000001
         error = 1e9
         d1_old = d1
         while error > tol:
-            d0 = (1/d0.size(0))*1/(torch.sum(d1*cost,1) + eps)
-            d1 = (1/d1.size(0))*1/(torch.sum(d0.unsqueeze(1)*cost,0)+eps)
-            error = torch.mean(torch.abs(d1_old-d1))
+            d0 = (1 / d0.size(0)) * 1 / (torch.sum(d1 * cost, 1) + eps)
+            d1 = (1 / d1.size(0)) * 1 / (torch.sum(d0.unsqueeze(1) * cost, 0) + eps)
+            error = torch.mean(torch.abs(d1_old - d1))
             d1_old = d1
-        return d1*cost*d0.unsqueeze(1)
+        return d1 * cost * d0.unsqueeze(1)
 
 
 class CoreAttention(MegatronModule):
