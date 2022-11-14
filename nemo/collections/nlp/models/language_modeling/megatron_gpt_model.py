Fixes bugs with loss averaging with `validation_drop_last=False` for Megatron GPT (#5329)
* fixing bugs

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* bug fix

* bug fix

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

Co-authored-by: Shanmugam Ramasamy <shanmugamr@shanmugamr-mlt.client.nvidia.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Oleksii Kuchaiev <okuchaiev@users.noreply.github.com>
Signed-off-by: andrusenkoau <andrusenkoau@gmail.com>

nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -449,7 +449,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
                     grad = word_embeddings_weight.grad
                 torch.distributed.all_reduce(grad, group=parallel_state.get_embedding_group())
 
-    def get_forward_output_and_loss_func(self):
+    def get_forward_output_and_loss_func(self, validation_step=False):
         def fwd_output_and_loss_func(batch, model, checkpoint_activations_all_layers=None):
             if parallel_state.get_pipeline_model_parallel_world_size() == 1:
                 batch = [x.cuda(non_blocking=True) for x in batch]
@@ -480,9 +480,24 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             )
 
             def loss_func(output_tensor):
-                loss = self.loss_func(loss_mask, output_tensor)
-                reduced_loss = average_losses_across_data_parallel_group([loss])
-                return loss, {'avg': reduced_loss}
+                loss_for_mb = self.loss_func(loss_mask, output_tensor)
+                if validation_step and not self.cfg.data.get('validation_drop_last', True):
+                    num_valid_samples_in_mb = int(loss_mask.sum() / loss_mask.numel() * loss_mask.shape[0])
+                    loss_sum_for_mb = num_valid_samples_in_mb * loss_for_mb
+                    loss_sum_and_mb_size_all_gpu = torch.cat(
+                        [
+                            loss_sum_for_mb.clone().detach().view(1),
+                            torch.tensor([num_valid_samples_in_mb]).cuda().clone().detach(),
+                        ]
+                    )
+                    # Could potentially reduce num_valid_samples_in_microbatch and use that to aggregate instead of len(self._validation_ds)
+                    torch.distributed.all_reduce(
+                        loss_sum_and_mb_size_all_gpu, group=parallel_state.get_data_parallel_group()
+                    )
+                    return loss_for_mb, {'loss_sum_and_mb_size': loss_sum_and_mb_size_all_gpu}
+                else:
+                    reduced_loss = average_losses_across_data_parallel_group([loss_for_mb])
+                    return loss_for_mb, {'avg': reduced_loss}
 
             return output_tensor, loss_func
 
@@ -526,7 +541,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             The list of microbatches is then piped through the pipeline using Apex fwd/bwd functions.
         """
 
-        batch_for_pipeline = self.process_global_batch(batch, self.cfg.global_batch_size)
+        batch_for_pipeline = self.process_global_batch(batch)
         tensor_shape = [self.cfg.encoder_seq_length, self.cfg.micro_batch_size, self.cfg.hidden_size]
 
         # run forward passes for an entire global batch
@@ -534,7 +549,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         fwd_bwd_function = self._get_fwd_bwd_function()
 
         losses_reduced_per_micro_batch = fwd_bwd_function(
-            forward_step_func=self.get_forward_output_and_loss_func(),
+            forward_step_func=self.get_forward_output_and_loss_func(validation_step=True),
             batch=batch_for_pipeline,
             model=self.model,
             forward_only=True,
@@ -546,44 +561,37 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
 
         # only the last stage of the pipeline returns losses
         if losses_reduced_per_micro_batch:
-            actual_batch_size = batch['tokens'].shape[0]  # Might be lesser than global_batch_size if drop_last=False
-            expected_batch_size = self.cfg.global_batch_size // parallel_state.get_data_parallel_world_size()
-            if actual_batch_size == expected_batch_size:
-                loss_with_batch_size_list = [
-                    [loss_reduced['avg'].item(), self.cfg.micro_batch_size]
-                    for loss_reduced in losses_reduced_per_micro_batch
-                ]
+            if self.cfg.data.get('validation_drop_last', True):
+                # average loss across micro batches
+                loss_tensors_list = [loss_reduced['avg'] for loss_reduced in losses_reduced_per_micro_batch]
+                return torch.concat(loss_tensors_list).mean()
             else:
-                loss_with_batch_size_list = []
-                total_samples_remaining = actual_batch_size
-                for loss_reduced in losses_reduced_per_micro_batch:
-                    if total_samples_remaining <= 0:
-                        break
-                    if total_samples_remaining // self.cfg.micro_batch_size >= 1:
-                        loss_with_batch_size_list.append([loss_reduced['avg'].item(), self.cfg.micro_batch_size])
-                    else:
-                        loss_with_batch_size_list.append([loss_reduced['avg'].item(), total_samples_remaining])
-                    total_samples_remaining = total_samples_remaining - self.cfg.micro_batch_size
+                # Get the total loss since micro batches sizes are not uniform
+                loss_sum_tensors_list = [
+                    loss_sum['loss_sum_and_mb_size']
+                    for loss_sum in losses_reduced_per_micro_batch
+                    if loss_sum['loss_sum_and_mb_size'][1] > 0
+                ]
+                loss_sum = (
+                    torch.vstack(loss_sum_tensors_list).sum(axis=0)
+                    if len(loss_sum_tensors_list) > 0
+                    else torch.tensor(0.0)
+                )
+                return loss_sum
         else:
             # we're not on the last pipeline stage so no losses
-            loss_with_batch_size_list = []
-
-        return loss_with_batch_size_list
+            return []
 
     def validation_epoch_end(self, outputs):
         if parallel_state.is_pipeline_last_stage():
             # only the last pipeline parallel stages return loss with their batch size
-            total_num_samples = 0
-            total_loss = 0
-            for loss_with_batch_size in outputs:
-                loss_with_batch_size_array = np.array(loss_with_batch_size).flatten()
-                batch_losses = loss_with_batch_size_array[0::2]
-                batch_sizes = loss_with_batch_size_array[1::2]
-                total_num_samples += sum(batch_sizes)
-                total_loss += np.dot(batch_losses, batch_sizes)
-
-            avg_loss = total_loss / total_num_samples
-            averaged_loss = torch.tensor(avg_loss, dtype=torch.float32).cuda()
+            if self.cfg.data.get('validation_drop_last', True):
+                averaged_loss = torch.stack(outputs).mean()
+            else:
+                # Compute the avg loss by total_loss across all samples / total number of samples
+                total_loss_and_total_samples = torch.vstack(outputs).sum(axis=0)
+                avg_loss = total_loss_and_total_samples[0] / total_loss_and_total_samples[1]
+                averaged_loss = avg_loss.type(torch.float32).cuda()
         else:
             averaged_loss = torch.tensor(0.0, dtype=torch.float32).cuda()
 
@@ -610,39 +618,12 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         """ Prepares the global batch for apex fwd/bwd functions.
             Global batch is a list of micro batches.
         """
-        tokens = global_batch["tokens"]
-        labels = global_batch["labels"]
-        loss_mask = global_batch["loss_mask"]
-        attention_mask = global_batch["attention_mask"]
-        position_ids = global_batch["position_ids"]
-        expected_batch_size = None
-        if global_batch_size is not None:
-            expected_batch_size = global_batch_size // parallel_state.get_data_parallel_world_size()
-        current_batch_size = tokens.shape[0]
-        if expected_batch_size is not None and expected_batch_size > current_batch_size:
-            logging.info(
-                'Got batch size of '
-                + str(current_batch_size)
-                + ' , expected batch size :'
-                + str(expected_batch_size)
-                + '. Appending dummy data.'
-            )
-            pad_length = expected_batch_size - current_batch_size
-            pad_dim = (int(pad_length), tokens.shape[1])
-            attention_mask_shape = list(attention_mask.shape)
-            attention_mask_shape[0] = int(pad_length)
-            tokens = torch.cat((tokens, torch.ones(pad_dim, dtype=tokens.dtype)))
-            labels = torch.cat((labels, torch.ones(pad_dim, dtype=labels.dtype)))
-            attention_mask = torch.cat((attention_mask, torch.zeros(attention_mask_shape, dtype=attention_mask.dtype)))
-            position_ids = torch.cat((position_ids, torch.ones(pad_dim, dtype=position_ids.dtype)))
-            loss_mask = torch.cat((loss_mask, torch.zeros(pad_dim, dtype=loss_mask.dtype)))
-
         return [
-            tokens,
-            labels,
-            loss_mask,
-            attention_mask,
-            position_ids,
+            global_batch["tokens"],
+            global_batch["labels"],
+            global_batch["loss_mask"],
+            global_batch["attention_mask"],
+            global_batch["position_ids"],
         ]
 
     def build_train_valid_test_datasets(self):
@@ -687,7 +668,9 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
 
         return self._train_ds, self._validation_ds, self._test_ds
 
-    def build_pretraining_data_loader(self, dataset, consumed_samples, dataset_type=None, drop_last=True):
+    def build_pretraining_data_loader(
+        self, dataset, consumed_samples, dataset_type=None, drop_last=True, pad_samples_to_global_batch_size=False
+    ):
         """Buld dataloader given an input dataset."""
 
         logging.info(f'Building dataloader with consumed samples: {consumed_samples}')
@@ -702,6 +685,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
                     data_parallel_rank=parallel_state.get_data_parallel_rank(),
                     data_parallel_size=parallel_state.get_data_parallel_world_size(),
                     drop_last=drop_last,
+                    pad_samples_to_global_batch_size=pad_samples_to_global_batch_size,
                 )
             elif self.cfg.data.dataloader_type == 'cyclic':
                 batch_sampler = MegatronPretrainingRandomBatchSampler(
@@ -712,6 +696,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
                     data_parallel_rank=parallel_state.get_data_parallel_rank(),
                     data_parallel_size=parallel_state.get_data_parallel_world_size(),
                     drop_last=self.cfg.get('drop_last', True),
+                    pad_samples_to_global_batch_size=pad_samples_to_global_batch_size,
                 )
             else:
                 raise ValueError('cfg.data.dataloader_type must be "single" or "cyclic"')
@@ -808,12 +793,18 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             logging.info(
                 f'Setting up validation dataloader with len(len(self._validation_ds)): {len(self._validation_ds)} and consumed samples: {consumed_samples}'
             )
+
             drop_last = True
             if not self.cfg.data.get('validation_drop_last', True):
                 logging.info(f'Drop last in validation dataset is set to False')
                 drop_last = False
+            pad_samples_to_global_batch_size = False
+            if self.cfg.data.get('pad_samples_to_global_batch_size', False):
+                logging.info('pad_samples_to_global_batch_size set to True')
+                pad_samples_to_global_batch_size = True
+
             self._validation_dl = self.build_pretraining_data_loader(
-                self._validation_ds, consumed_samples, "validation", drop_last
+                self._validation_ds, consumed_samples, "validation", drop_last, pad_samples_to_global_batch_size
             )
 
     def setup_test_data(self, cfg):