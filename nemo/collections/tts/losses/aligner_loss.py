3a9b0e785300295630dbc7d159dbbba6d314d93b
subhankar-ghosh <subhankar2321@gmail.com>
2022-11-17 13:09:33 -0800

    JOC Optimization init
    Signed-off-by: subhankar-ghosh <subhankar2321@gmail.com>

--- nemo/collections/tts/losses/aligner_loss.py
+++ nemo/collections/tts/losses/aligner_loss.py
@@ -24,7 +24,7 @@ from nemo.core.neural_types.neural_type import NeuralType
 class ForwardSumLoss(Loss):
     def __init__(self, blank_logprob=-1):
         super().__init__()
-        self.log_softmax = torch.nn.LogSoftmax(dim=3)
+        self.log_softmax = torch.nn.LogSoftmax(dim=-1)
         self.ctc_loss = torch.nn.CTCLoss(zero_infinity=True)
         self.blank_logprob = blank_logprob
 
@@ -46,24 +46,31 @@ class ForwardSumLoss(Loss):
     def forward(self, attn_logprob, in_lens, out_lens):
         key_lens = in_lens
         query_lens = out_lens
-        attn_logprob_padded = F.pad(input=attn_logprob, pad=(1, 0), value=self.blank_logprob)
+        max_key_len = attn_logprob.size(-1)
 
-        total_loss = 0.0
-        for bid in range(attn_logprob.shape[0]):
-            target_seq = torch.arange(1, key_lens[bid] + 1).unsqueeze(0)
-            curr_logprob = attn_logprob_padded[bid].permute(1, 0, 2)[: query_lens[bid], :, : key_lens[bid] + 1]
+        # Reorder input to [query_len, batch_size, key_len]
+        attn_logprob = attn_logprob.squeeze(1)
+        attn_logprob = attn_logprob.permute(1, 0, 2)
 
-            curr_logprob = self.log_softmax(curr_logprob[None])[0]
-            loss = self.ctc_loss(
-                curr_logprob,
-                target_seq,
-                input_lengths=query_lens[bid : bid + 1],
-                target_lengths=key_lens[bid : bid + 1],
-            )
-            total_loss += loss
+        # Add blank label
+        attn_logprob = F.pad(input=attn_logprob, pad=(1, 0, 0, 0, 0, 0), value=self.blank_logprob)
 
-        total_loss /= attn_logprob.shape[0]
-        return total_loss
+        # Convert to log probabilities
+        # Note: Mask out probs beyond key_len
+        key_inds = torch.arange(max_key_len + 1, device=attn_logprob.device, dtype=torch.long)
+        attn_logprob.masked_fill_(
+            key_inds.view(1, 1, -1) > key_lens.view(1, -1, 1), -float("inf")  # key_inds >= key_lens+1
+        )
+        attn_logprob = self.log_softmax(attn_logprob)
+
+        # Target sequences
+        target_seqs = key_inds[1:].unsqueeze(0)
+        target_seqs = target_seqs.repeat(key_lens.numel(), 1)
+
+        # Evaluate CTC loss
+        cost = self.ctc_loss(attn_logprob, target_seqs, input_lengths=query_lens, target_lengths=key_lens)
+
+        return cost
 
 
 class BinLoss(Loss):
