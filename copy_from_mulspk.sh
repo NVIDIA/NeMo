da28ff10253d26f52beca448eb301b5cc8acfe11
Taejin Park <tango4j@gmail.com>
2022-11-17 10:33:57 -0800

    Delete unecessary files
    Signed-off-by: Taejin Park <tango4j@gmail.com>

--- copy_from_mulspk.sh
+++
@@ -1,4 +0,0 @@
- cp /home/taejinp/projects/_streaming_mulspk_asr/NeMo/build/lib/nemo/collections/asr/models/online_diarizer.py nemo/collections/asr/models/online_diarizer.py
- cp /home/taejinp/projects/_streaming_mulspk_asr/NeMo/nemo/collections/asr/models/online_diarizer.py nemo/collections/asr/models/online_diarizer.py
- cp /home/taejinp/projects/_streaming_mulspk_asr/NeMo/nemo/collections/asr/parts/utils/speaker_utils.py nemo/collections/asr/parts/utils/speaker_utils.py
- cp /home/taejinp/projects/_streaming_mulspk_asr/NeMo/nemo/collections/asr/parts/utils/nmesc_clustering.py nemo/collections/asr/parts/utils/nmesc_clustering.py
