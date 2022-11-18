e913842b9eb668e9e1ce8ee69a65a4df7c7230d7
Roman Korostik <rkorostik@nvidia.com>
2022-11-18 17:06:48 +0000

    [TTS] add SpectrogramEnhancer based on StyleGAN 2
    Signed-off-by: Roman Korostik <rkorostik@nvidia.com>

---
+++ nemo/collections/tts/models/spectrogram_enhancer.py
@@ -0,0 +1,340 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# MIT License
+#
+# Copyright (c) 2020 Phil Wang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# The following is largely based on code from https://github.com/lucidrains/stylegan2-pytorch
+
+import math
+from random import random, randrange
+from typing import Dict, List, Optional, TypedDict, Union
+
+import torch
+import torch.nn.functional as F
+from einops import rearrange, repeat
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import Trainer
+from torch import Tensor, nn
+from torch.autograd import grad as torch_grad
+
+import nemo
+from nemo.collections.tts.models import FastPitchModel
+from nemo.collections.tts.modules.spectrogram_enhancer import Discriminator, Generator, mask
+from nemo.core import Exportable, ModelPT
+
+
+def type_as_recursive(e, source: Tensor):
+    if isinstance(e, (list, tuple)):
+        return [type_as_recursive(elem, source) for elem in e]
+    elif isinstance(e, dict):
+        return {key: type_as_recursive(value, source) for key, value in e.items()}
+    elif isinstance(e, Tensor):
+        return e.type_as(source)
+    else:
+        return e
+
+
+def gradient_penalty(images, output, weight=10):
+    batch_size = images.shape[0]
+    gradients = torch_grad(
+        outputs=output,
+        inputs=images,
+        grad_outputs=torch.ones(output.size(), device=images.device),
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True,
+    )[0]
+
+    gradients = gradients.reshape(batch_size, -1)
+    return weight * ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+
+
+def calc_pl_lengths(styles, images):
+    device = images.device
+    num_pixels = images.shape[2] * images.shape[3]
+    pl_noise = torch.randn(images.shape, device=device) / math.sqrt(num_pixels)
+    outputs = (images * pl_noise).sum()
+
+    pl_grads = torch_grad(
+        outputs=outputs,
+        inputs=styles,
+        grad_outputs=torch.ones(outputs.shape, device=device),
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True,
+    )[0]
+
+    return (pl_grads ** 2).sum(dim=2).mean(dim=1).sqrt()
+
+
+def gen_hinge_loss(fake):
+    return fake.mean()
+
+
+def hinge_loss(real, fake):
+    return (F.relu(1 + real) + F.relu(1 - fake)).mean()
+
+
+def consistency_loss(condition, output, lengths):
+    *_, w, h = condition.shape
+    w, h = w // 4, h
+
+    condition = torch.nn.functional.interpolate(condition, size=(w, h), mode="bilinear", antialias=True)
+    output = torch.nn.functional.interpolate(output, size=(w, h), mode="bilinear", antialias=True)
+
+    dist = (condition - output).abs()
+    dist = mask(dist, lengths)
+    return (dist / rearrange(lengths, "b 1 -> b 1 1 1")).sum(dim=-1).mean()
+
+
+class ParallelSpectrogramBatch(TypedDict):
+    target: torch.FloatTensor
+    condition: torch.FloatTensor
+    lengths: torch.LongTensor
+
+
+class SpectrogramEnhancerModel(ModelPT, Exportable):
+    def __init__(self, cfg: DictConfig, trainer: Trainer = None, fastpitch: FastPitchModel = None) -> None:
+        self.fastpitch = None
+        super().__init__(cfg=cfg, trainer=trainer)
+
+        if fastpitch:
+            self.set_fastpitch_model(fastpitch)
+
+        self.G = Generator(
+            n_bands=cfg["n_bands"],
+            latent_dim=cfg["latent_dim"],
+            network_capacity=cfg["network_capacity"],
+            style_depth=cfg["style_depth"],
+            channels=1,
+            fmap_max=cfg["fmap_max"],
+        )
+        self.D = Discriminator(
+            n_bands=cfg["n_bands"], network_capacity=cfg["network_capacity"], channels=1, fmap_max=cfg["fmap_max"],
+        )
+
+    def set_fastpitch_model(self, fastpitch: FastPitchModel):
+        self.fastpitch = fastpitch
+        self.fastpitch.freeze()
+        # for param in self.fastpitch.parameters():
+        #    param.requires_grad = False
+
+        self._cfg.train_ds = OmegaConf.merge(self.fastpitch._cfg.train_ds, self._cfg.train_ds)
+        self.setup_training_data(self._cfg.train_ds)
+
+        self._cfg.validation_ds = OmegaConf.merge(self.fastpitch._cfg.validation_ds, self._cfg.validation_ds)
+        self.setup_validation_data(self._cfg.validation_ds)
+
+    def move_to_correct_device(self, e):
+        return type_as_recursive(e, next(iter(self.G.parameters())))
+
+    def normalize_spectrograms(self, spectrogram: Tensor) -> Tensor:
+        spectrogram = spectrogram - self._cfg["spectrogram_min_value"]
+        spectrogram = spectrogram / (self._cfg["spectrogram_max_value"] - self._cfg["spectrogram_min_value"])
+        return spectrogram
+
+    def unnormalize_spectrograms(self, spectrogram: Tensor) -> Tensor:
+        spectrogram = spectrogram * (self._cfg["spectrogram_max_value"] - self._cfg["spectrogram_min_value"])
+        spectrogram = spectrogram + self._cfg["spectrogram_min_value"]
+        return spectrogram
+
+    def generate_zs(self, batch_size: int = 1, mixing: bool = False):
+        if mixing and self._cfg["mixed_prob"] < random():
+            mixing_point = randrange(1, self.G.num_layers)
+            first_part = [torch.randn(batch_size, self._cfg["latent_dim"])] * mixing_point
+            second_part = [torch.randn(batch_size, self._cfg["latent_dim"])] * (self.G.num_layers - mixing_point)
+            zs = [*first_part, *second_part]
+        else:
+            zs = [torch.randn(batch_size, self._cfg["latent_dim"])] * self.G.num_layers
+
+        return self.move_to_correct_device(zs)
+
+    def generate_noise(self, batch_size: int = 1) -> Tensor:
+        noise = torch.rand(batch_size, self._cfg["n_bands"], 4096, 1)
+        return self.move_to_correct_device(noise)
+
+    def pad_spectrogram(self, spectrogram):
+        multiplier = 2 ** sum(1 for block in self.G.blocks if block.upsample)
+        *_, max_length = spectrogram.shape
+        return torch.nn.functional.pad(spectrogram, (0, multiplier - max_length % multiplier))
+
+    def forward(
+        self,
+        condition: Tensor,
+        lengths: Tensor,
+        zs: Optional[List[Tensor]] = None,
+        ws: Optional[List[Tensor]] = None,
+        noise: Optional[Tensor] = None,
+        mixing: bool = False,
+        normalize: bool = True,
+    ):
+        batch_size, *_, max_length = condition.shape
+
+        # generate noise
+        if zs is not None and ws is not None:
+            raise ValueError(
+                "Please specify either zs or ws or neither, but not both. It is not clear which one to use."
+            )
+
+        if zs is None:
+            zs = self.generate_zs(batch_size, mixing)
+        if ws is None:
+            ws = [self.G.style_mapping(z) for z in zs]
+        if noise is None:
+            noise = self.generate_noise(batch_size)
+
+        # normalize if needed, mask and pad appropriately
+        if normalize:
+            condition = self.normalize_spectrograms(condition)
+        condition = self.pad_spectrogram(condition)
+        condition = mask(condition, lengths)
+
+        # the main call
+        enhanced_spectrograms = self.G(condition, lengths, ws, noise)
+
+        # denormalize if needed, mask and remove padding
+        if normalize:
+            enhanced_spectrograms = self.unnormalize_spectrograms(enhanced_spectrograms)
+        enhanced_spectrograms = mask(enhanced_spectrograms, lengths)
+        enhanced_spectrograms = enhanced_spectrograms[:, :, :, :max_length]
+
+        return enhanced_spectrograms
+
+    def resynthesize_tts_batch(self, batch):
+        attn_prior, durs, speaker = None, None, None
+        audio, audio_lens, text, text_lens, attn_prior, pitch, _, speaker = batch  # map(lambda x: x.cuda(), batch)
+        mels, mel_lens = self.fastpitch.preprocessor(input_signal=audio, length=audio_lens)
+
+        mels_pred, *_, pitch = self.fastpitch(
+            text=text,
+            durs=durs,
+            pitch=pitch,
+            speaker=speaker,
+            pace=1.0,
+            spec=mels if self.fastpitch.learn_alignment else None,
+            attn_prior=attn_prior,
+            mel_lens=mel_lens,
+            input_lens=text_lens,
+        )
+
+        mels = mels.cpu()
+        mel_lens = mel_lens.cpu()
+        mels_pred = mels_pred.cpu()
+
+        batch_size, *_ = mels.shape
+
+        targets = []
+        conditions = []
+        lengths = []
+
+        for i in range(batch_size):
+            l = mel_lens[i].item()
+            lengths.append(l)
+
+            targets.append(mels[i, :, :l].T)
+            conditions.append(mels_pred[i, :, :l].T)
+
+        target = nn.utils.rnn.pad_sequence(targets, batch_first=True)
+        condition = nn.utils.rnn.pad_sequence(conditions, batch_first=True)
+        lengths = torch.LongTensor(lengths).unsqueeze(-1)
+
+        return self.move_to_correct_device((target, condition, lengths))
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        target, condition, lengths = self.resynthesize_tts_batch(batch)
+
+        target = rearrange(target, "b l c -> b 1 c l")
+        condition = rearrange(condition, "b l c -> b 1 c l")
+
+        with torch.no_grad():
+            target = self.normalize_spectrograms(target)
+            condition = self.normalize_spectrograms(condition)
+
+            target = mask(target, lengths)
+            condition = mask(condition, lengths)
+
+        # train discriminator
+        if optimizer_idx == 0:
+            generated_images = self.forward(condition, lengths, mixing=True, normalize=False)
+            fake_logits = self.D(generated_images, condition, lengths)
+
+            real_images = target.requires_grad_()
+            real_logits = self.D(real_images, condition, lengths)
+            d_loss = hinge_loss(real=real_logits, fake=fake_logits)
+            self.log("d_loss", d_loss, prog_bar=True)
+
+            if batch_idx % 4 == 0:
+                gp_loss = gradient_penalty(real_images, real_logits)
+                self.log("d_loss_gp", gp_loss, prog_bar=True)
+                return d_loss + gp_loss
+
+            return d_loss
+
+        # train generator
+        if optimizer_idx == 1:
+            generated_images = self.forward(condition, lengths, mixing=True)
+            fake_logits = self.D(generated_images, condition, lengths)
+            g_loss = gen_hinge_loss(fake_logits)
+            c_loss = 10 * consistency_loss(condition, generated_images, lengths)
+
+            self.log("g_loss", g_loss, prog_bar=True)
+            self.log("c_loss", c_loss, prog_bar=True)
+            return g_loss + c_loss
+
+    def configure_optimizers(self):
+        generator_opt = torch.optim.Adam(self.G.parameters(), lr=self._cfg["lr"], betas=(0.5, 0.9),)
+        discriminator_opt = torch.optim.Adam(self.D.parameters(), lr=self._cfg["lr"], betas=(0.5, 0.9))
+        return [discriminator_opt, generator_opt], []
+
+    def setup_training_data(self, train_data_config):
+        if self.fastpitch:
+            self.fastpitch.setup_training_data(train_data_config)
+            self._train_dl = self.fastpitch._train_dl
+
+    def setup_validation_data(self, val_data_config):
+        if self.fastpitch:
+            self.fastpitch.setup_validation_data(val_data_config)
+            self._validation_dl = self.fastpitch._validation_dl
+
+    @classmethod
+    def list_available_models(cls):
+        return None
+
+    def save_to(self, save_path: str):
+        if self.fastpitch:
+            fastpitch = self._modules.pop("fastpitch")
+            super().save_to(save_path)
+            self.fastpitch = fastpitch
+        else:
+            super().save_to(save_path)
---
+++ nemo/collections/tts/modules/spectrogram_enhancer.py
@@ -0,0 +1,351 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# MIT License
+#
+# Copyright (c) 2020 Phil Wang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# The following is largely based on code from https://github.com/lucidrains/stylegan2-pytorch
+
+import math
+from functools import partial
+from math import log2
+from typing import List
+
+import torch
+import torch.nn.functional as F
+from einops import rearrange
+from kornia.filters import filter2d
+from torch import Tensor, nn
+
+
+class Blur(nn.Module):
+    def __init__(self):
+        super().__init__()
+        f = torch.Tensor([1, 2, 1])
+        self.register_buffer("f", f)
+
+    def forward(self, x):
+        f = self.f
+        f = f[None, None, :] * f[None, :, None]
+        return filter2d(x, f, normalized=True)
+
+
+class EqualLinear(nn.Module):
+    def __init__(self, in_dim, out_dim, lr_mul=1, bias=True):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(out_dim, in_dim))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_dim))
+
+        self.lr_mul = lr_mul
+
+    def forward(self, input):
+        return F.linear(input, self.weight * self.lr_mul, bias=self.bias * self.lr_mul)
+
+
+class StyleMapping(nn.Module):
+    def __init__(self, emb, depth, lr_mul=0.1):
+        super().__init__()
+
+        layers = []
+        for _ in range(depth):
+            layers.extend([EqualLinear(emb, emb, lr_mul), nn.LeakyReLU(0.2, inplace=True)])
+
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = F.normalize(x, dim=1)
+        return self.net(x)
+
+
+class RGBBlock(nn.Module):
+    def __init__(self, latent_dim, input_channel, upsample, channels=3):
+        super().__init__()
+        self.input_channel = input_channel
+        self.to_style = nn.Linear(latent_dim, input_channel)
+
+        out_filters = channels
+        self.conv = Conv2DMod(input_channel, out_filters, 1, demod=False)
+
+        self.upsample = (
+            nn.Sequential(nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False), Blur(),)
+            if upsample
+            else None
+        )
+
+    def forward(self, x, prev_rgb, istyle):
+        style = self.to_style(istyle)
+        x = self.conv(x, style)
+
+        if prev_rgb is not None:
+            x = x + prev_rgb
+
+        if self.upsample is not None:
+            x = self.upsample(x)
+
+        return x
+
+
+class Conv2DMod(nn.Module):
+    def __init__(
+        self, in_chan, out_chan, kernel, demod=True, stride=1, dilation=1, eps=1e-8, **kwargs,
+    ):
+        super().__init__()
+        self.filters = out_chan
+        self.demod = demod
+        self.kernel = kernel
+        self.stride = stride
+        self.dilation = dilation
+        self.weight = nn.Parameter(torch.randn((out_chan, in_chan, kernel, kernel)))
+        self.eps = eps
+        nn.init.kaiming_normal_(self.weight, a=0, mode="fan_in", nonlinearity="leaky_relu")
+
+    def _get_same_padding(self, size, kernel, dilation, stride):
+        return ((size - 1) * (stride - 1) + dilation * (kernel - 1)) // 2
+
+    def forward(self, x, y):
+        b, c, h, w = x.shape
+
+        w1 = y[:, None, :, None, None]
+        w2 = self.weight[None, :, :, :, :]
+        weights = w2 * (w1 + 1)
+
+        if self.demod:
+            d = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + self.eps)
+            weights = weights * d
+
+        x = x.reshape(1, -1, h, w)
+
+        _, _, *ws = weights.shape
+        weights = weights.reshape(b * self.filters, *ws)
+
+        padding = self._get_same_padding(h, self.kernel, self.dilation, self.stride)
+        x = F.conv2d(x, weights, padding=padding, groups=b)
+
+        x = x.reshape(-1, self.filters, h, w)
+        return x
+
+
+class GeneratorBlock(nn.Module):
+    def __init__(
+        self, latent_dim, input_channels, filters, upsample=True, upsample_rgb=True, channels=1,
+    ):
+        super().__init__()
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False) if upsample else None
+
+        self.to_style1 = nn.Linear(latent_dim, input_channels)
+        self.to_noise1 = nn.Linear(1, filters)
+        self.conv1 = Conv2DMod(input_channels, filters, 3)
+
+        self.to_style2 = nn.Linear(latent_dim, filters)
+        self.to_noise2 = nn.Linear(1, filters)
+        self.conv2 = Conv2DMod(filters, filters, 3)
+
+        self.activation = nn.LeakyReLU(0.2, inplace=True)
+        self.to_rgb = RGBBlock(latent_dim, filters, upsample_rgb, channels)
+
+    def forward(self, x, prev_rgb, istyle, inoise):
+        if self.upsample is not None:
+            x = self.upsample(x)
+
+        inoise = inoise[:, : x.shape[2], : x.shape[3], :]
+        noise1 = self.to_noise1(inoise).permute((0, 3, 1, 2))
+        noise2 = self.to_noise2(inoise).permute((0, 3, 1, 2))
+
+        style1 = self.to_style1(istyle)
+        x = self.conv1(x, style1)
+        x = self.activation(x + noise1)
+
+        style2 = self.to_style2(istyle)
+        x = self.conv2(x, style2)
+        x = self.activation(x + noise2)
+
+        rgb = self.to_rgb(x, prev_rgb, istyle)
+        return x, rgb
+
+
+def mask(tensor, lengths):
+    batch_size, *_, max_lengths = tensor.shape
+    mask = torch.ones(batch_size, 1, 1, max_lengths).cumsum(dim=-1).type_as(lengths)
+    mask = mask <= rearrange(lengths, "b 1 -> b 1 1 1")
+    return tensor * mask
+
+
+class DiscriminatorBlock(nn.Module):
+    def __init__(self, input_channels, filters, downsample=True):
+        super().__init__()
+        self.conv_res = nn.Conv2d(input_channels, filters, 1, stride=(2 if downsample else 1))
+
+        self.net = nn.Sequential(
+            nn.Conv2d(input_channels, filters, 3, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(filters, filters, 3, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.downsample = (
+            nn.Sequential(Blur(), nn.Conv2d(filters, filters, 3, padding=1, stride=2)) if downsample else None
+        )
+
+    def forward(self, x):
+        res = self.conv_res(x)
+        x = self.net(x)
+        if self.downsample is not None:
+            x = self.downsample(x)
+        x = (x + res) * (1 / math.sqrt(2))
+        return x
+
+
+class Generator(nn.Module):
+    def __init__(
+        self, n_bands, latent_dim, style_depth, network_capacity=16, channels=1, fmap_max=512,
+    ):
+        super().__init__()
+        self.image_size = n_bands
+        self.latent_dim = latent_dim
+        self.num_layers = int(log2(n_bands) - 1)
+        self.style_depth = style_depth
+
+        self.style_mapping = StyleMapping(self.latent_dim, self.style_depth, lr_mul=0.1)
+
+        filters = [network_capacity * (2 ** (i + 1)) for i in range(self.num_layers)][::-1]
+
+        set_fmap_max = partial(min, fmap_max)
+        filters = list(map(set_fmap_max, filters))
+        init_channels = filters[0]
+        filters = [init_channels, *filters]
+
+        in_out_pairs = zip(filters[:-1], filters[1:])
+        initial_block_size = n_bands // 2 ** (self.num_layers - 1), 800
+        self.initial_block = nn.Parameter(torch.randn((1, init_channels, *initial_block_size)), requires_grad=False)
+
+        self.initial_conv = nn.Conv2d(filters[0], filters[0], 3, padding=1)
+        self.blocks = nn.ModuleList([])
+
+        for ind, (in_chan, out_chan) in enumerate(in_out_pairs):
+            not_first = ind != 0
+            not_last = ind != (self.num_layers - 1)
+
+            block = GeneratorBlock(
+                latent_dim, in_chan, out_chan, upsample=not_first, upsample_rgb=not_last, channels=channels,
+            )
+            self.blocks.append(block)
+
+        for m in self.modules():
+            if isinstance(m, (torch.nn.Conv2d, torch.nn.Linear)):
+                torch.nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in", nonlinearity="leaky_relu")
+        for block in self.blocks:
+            torch.nn.init.zeros_(block.to_noise1.weight)
+            torch.nn.init.zeros_(block.to_noise1.bias)
+            torch.nn.init.zeros_(block.to_noise2.weight)
+            torch.nn.init.zeros_(block.to_noise2.bias)
+
+    def forward(self, condition: Tensor, lengths: Tensor, ws: List[Tensor], noise: Tensor):
+        batch_size, _, _, max_length = condition.shape
+
+        x = self.initial_block.expand(batch_size, -1, -1, -1)
+        x = x[:, :, :, : max_length // 16]
+
+        rgb = None
+        x = self.initial_conv(x)
+
+        for style, block in zip(ws, self.blocks):
+            x, rgb = block(x, rgb, style, noise)
+
+            scaled_condition = nn.functional.interpolate(condition, size=x.shape[-2:], mode="bilinear", antialias=True)
+            x = (x + scaled_condition) / 2
+
+            x_scale = condition.shape[-1] // x.shape[-1]
+            x = mask(x, (lengths / x_scale).ceil().long())
+
+            scaled_condition = nn.functional.interpolate(
+                condition, size=rgb.shape[-2:], mode="bilinear", antialias=True
+            )
+            rgb = (rgb + scaled_condition) / 2
+
+            rgb_scale = condition.shape[-1] // rgb.shape[-1]
+            rgb = mask(rgb, (lengths / rgb_scale).ceil().long())
+
+        return rgb
+
+
+class Discriminator(nn.Module):
+    def __init__(
+        self, n_bands, network_capacity=16, channels=1, fmap_max=512,
+    ):
+        super().__init__()
+        num_layers = int(log2(n_bands) - 1)
+        num_init_filters = channels
+
+        blocks = []
+        filters = [num_init_filters] + [(network_capacity * 4) * (2 ** i) for i in range(num_layers + 1)]
+
+        set_fmap_max = partial(min, fmap_max)
+        filters = list(map(set_fmap_max, filters))
+        chan_in_out = list(zip(filters[:-1], filters[1:]))
+
+        blocks = []
+
+        for ind, (in_chan, out_chan) in enumerate(chan_in_out):
+            is_not_last = ind != (len(chan_in_out) - 1)
+
+            block = DiscriminatorBlock(in_chan, out_chan, downsample=is_not_last)
+            blocks.append(block)
+
+        self.blocks = nn.ModuleList(blocks)
+
+        chan_last = filters[-1]
+        latent_dim = chan_last  # 2 * 2 * chan_last
+
+        self.final_conv = nn.Conv2d(chan_last, chan_last, 3, padding=1)
+        self.to_logit = nn.Linear(latent_dim, 1)
+
+        for m in self.modules():
+            if isinstance(m, (torch.nn.Conv2d, torch.nn.Linear)):
+                torch.nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in", nonlinearity="leaky_relu")
+
+    def forward(self, x, condition: Tensor, lengths: Tensor):
+        for block in self.blocks:
+            x = block(x)
+            scale = condition.shape[-1] // x.shape[-1]
+            x = mask(x, (lengths / scale).ceil().long())
+
+        x = self.final_conv(x)
+
+        scale = condition.shape[-1] // x.shape[-1]
+        x = mask(x, (lengths / scale).ceil().long())
+
+        x = x.mean(axis=-2)
+        x = (x / rearrange(lengths / scale, "b 1 -> b 1 1")).sum(axis=-1)
+        x = self.to_logit(x)
+        return x.squeeze()
