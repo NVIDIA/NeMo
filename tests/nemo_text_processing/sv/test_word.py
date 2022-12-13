7929a4917159fdfc42135a3bba956a383adb2e7b
Jim O'Regan <joregan@kth.se>
2022-12-13 14:45:37 +0100

    re-copy from vi
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- tests/nemo_text_processing/sv/test_word.py
+++ tests/nemo_text_processing/sv/test_word.py
@@ -12,53 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import pytest
-from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
-from nemo_text_processing.text_normalization.normalize import Normalizer
-from nemo_text_processing.text_normalization.normalize_with_audio import NormalizerWithAudio
 from parameterized import parameterized
 
-from ..utils import CACHE_DIR, PYNINI_AVAILABLE, parse_test_case_file
+from ..utils import CACHE_DIR, parse_test_case_file
+
+try:
+    from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
+
+    PYNINI_AVAILABLE = True
+except (ImportError, ModuleNotFoundError):
+    PYNINI_AVAILABLE = False
 
 
 class TestWord:
-    inverse_normalizer_sv = (
+    inverse_normalizer = (
         InverseNormalizer(lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False) if PYNINI_AVAILABLE else None
     )
 
     @parameterized.expand(parse_test_case_file('sv/data_inverse_text_normalization/test_cases_word.txt'))
     @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
+        not PYNINI_AVAILABLE,
+        reason="`pynini` not installed, please install via nemo_text_processing/pynini_install.sh",
     )
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
-    def test_denorm_sv(self, test_input, expected):
-        pred = self.inverse_normalizer_sv.inverse_normalize(test_input, verbose=False)
+    def test_denorm(self, test_input, expected):
+        pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
         assert pred == expected
-
-    normalizer_sv = (
-        Normalizer(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
-        if PYNINI_AVAILABLE
-        else None
-    )
-    normalizer_with_audio_sv = (
-        NormalizerWithAudio(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
-        if PYNINI_AVAILABLE and CACHE_DIR
-        else None
-    )
-
-    @parameterized.expand(parse_test_case_file('sv/data_text_normalization/test_cases_word.txt'))
-    @pytest.mark.skipif(
-        not PYNINI_AVAILABLE, reason="`pynini` not installed, please install via nemo_text_processing/setup.sh"
-    )
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm(self, test_input, expected):
-        pred = self.normalizer_sv.normalize(test_input, verbose=False)
-        assert pred == expected, f"input: {test_input}"
-
-        if self.normalizer_with_audio_sv:
-            pred_non_deterministic = self.normalizer_with_audio_sv.normalize(
-                test_input, n_tagged=150, punct_post_process=False
-            )
-            assert expected in pred_non_deterministic, f"input: {test_input}"
