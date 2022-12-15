ada69a3090e6c887dfb9c19d7962a548e89b0d24
Vladimir Bataev <vbataev@nvidia.com>
2022-12-15 21:48:03 +0400

    Fix trainer
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- nemo/collections/asr/models/hybrid_asr_tts_models.py
+++ nemo/collections/asr/models/hybrid_asr_tts_models.py
@@ -133,6 +133,9 @@ class ASRWithTTSModel(ASRModel):
         self.tts_model.freeze()
         self._full_init_guard = True
 
+        if trainer is not None:
+            self.set_trainer(trainer)  # set here for asr model
+
         if optim_cfg:
             self.setup_optimization(optim_config=optim_cfg)
         if train_ds_cfg:
@@ -161,6 +164,11 @@ class ASRWithTTSModel(ASRModel):
         cfg.tts_model_path = tts_model_path
         cfg.asr_model_path = asr_model_path
         return ASRWithTTSModel(cfg, trainer=trainer)
+
+    def set_trainer(self, trainer: Trainer):
+        super().set_trainer(trainer)
+        if self._full_init_guard:
+            self.asr_model.set_trainer(trainer)
 
     # fix trainer, see https://github.com/Lightning-AI/lightning/issues/13146#issuecomment-1137593172
     # @property
