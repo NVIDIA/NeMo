5142e7350424657a284f51270f74abac13ee9122
Alexandra Antonova <antonova_sasha@list.ru>
2022-12-19 12:04:10 +0300

    change sampling of dummy candidates at inference, add candidate info file
    Signed-off-by: Alexandra Antonova <antonova_sasha@list.ru>

--- examples/nlp/spellchecking_asr_customization/evaluation/prepare_input_for_spellchecker_inference.py
+++ examples/nlp/spellchecking_asr_customization/evaluation/prepare_input_for_spellchecker_inference.py
@@ -1,5 +1,5 @@
-import math
 import random
+import sys
 from argparse import ArgumentParser
 from collections import defaultdict
 from os.path import join
@@ -15,12 +15,18 @@ from utils import get_all_candidates_coverage, get_index, load_ngram_mappings, s
 
 
 parser = ArgumentParser(
-    description="Prepare training examples for Bert: insert custom phrases and best candidates into sample sentences"
+    description="Prepare input examples for inference"
 )
 parser.add_argument("--input_file", required=True, type=str, help="Path to input file with asr hypotheses")
 parser.add_argument("--input_vocab", type=str, required=True, help="Path to custom vocabulary")
 parser.add_argument("--ngram_mapping", type=str, required=True, help="Path to ngram mapping vocabulary")
-parser.add_argument("--output_name", type=str, required=True, help="Output file")
+parser.add_argument(
+    "--sub_misspells_file", required=True, type=str, help="File with misspells from which only keys will be used to sample dummy candidates"
+)
+
+parser.add_argument("--output_name", type=str, required=True, help="Output file in format required by the inference")
+parser.add_argument("--output_info_name", type=str, required=True, help="Additional output file with information about candidates")
+
 
 args = parser.parse_args()
 
@@ -45,15 +51,17 @@ with open(args.output_name + ".index", "w", encoding="utf-8") as out_debug:
             phr = phrases[phrase_id]
             out_debug.write(ngram + "\t" + phr + "\t" + str(b) + "\t" + str(size) + "\t" + str(lp) + "\n")
 
-dummy_candidates = [
-    "a g k t t r k n a p r t f",
-    "v w w x y x u r t g p w q",
-    "n t r y t q q r u p t l n t",
-    "p b r t u r e t f v w x u p z",
-    "p p o j j k l n b f q t",
-    "j k y u i t d s e w s r e j h i p p",
-    "q w r e s f c t d r q g g y",
-]
+big_sample_of_phrases = set()
+with open(args.sub_misspells_file, "r", encoding="utf-8") as f:
+    for line in f:
+        phrase, _, _, src_freq, dst_freq = line.strip().split("\t")
+        if int(src_freq) > 50:   # do not want to use frequent phrases as dummy candidates
+            continue
+        if len(phrase) < 6 or len(phrase) > 15:  # do not want to use too short or too long phrases as dummy candidates
+            continue        
+        big_sample_of_phrases.add(phrase)
+
+big_sample_of_phrases = list(big_sample_of_phrases)
 
 path_parts = args.output_name.split("/")
 path_parts2 = path_parts[-1].split(".")
@@ -62,6 +70,7 @@ doc_id = path_parts2[0]
 out_debug = open(args.output_name + ".candidates", "w", encoding="utf-8")
 out_debug2 = open(args.output_name + ".candidates_select", "w", encoding="utf-8")
 out = open(args.output_name, "w", encoding="utf-8")
+out_info = open(args.output_info_name, "w", encoding="utf-8")
 with open(args.input_file, "r", encoding="utf-8") as f:
     for line in f:
         short_sent, _ = line.strip().split("\t")
@@ -101,20 +110,27 @@ with open(args.input_file, "r", encoding="utf-8") as f:
             if real_coverage < 0.8:
                 out_debug.write("\t\t- " + phrases[idx] + "\tcov: " + str(coverage) + "\treal_cov: " + str(real_coverage) + "\n")
                 continue
-            candidates.append(phrases[idx])
+            candidates.append((phrases[idx], begin, phrase_length, coverage, real_coverage))
             out_debug.write(
                 "\t" + str(real_coverage) + "\t" + phrases[idx] + "\n" + " ".join(list(map(str, (map(int, phrases2positions[idx]))))) + "\n"
             )
             out_debug2.write(doc_id + "\t" + phrases[idx].replace(" ", "").replace("_", " ") + "\t" + short_sent + "\n")
 
         while len(candidates) < 10:
-            candidates.append(random.choice(dummy_candidates))
+            dummy = random.choice(big_sample_of_phrases)
+            dummy = " ".join(list(dummy.replace(" ", "_")))
+            candidates.append((dummy, -1, dummy.count(" ") + 1, 0.0, 0.0))
 
         random.shuffle(candidates)
         if len(candidates) != 10:
             print("WARNING: cannot get 10 candidates", candidates)
             continue
-        out.write(" ".join(letters) + "\t" + ";".join(candidates) + "\n")
+        out.write(" ".join(letters) + "\t" + ";".join([x[0] for x in candidates]) + "\n")
+        info = ""
+        for cand, begin, length, cov, real_cov in candidates:
+            info += cand + "|" + str(begin) + "|" + str(length) + "|" + str(cov) + "|" + str(real_cov) + ";"
+        out_info.write(info[:-1] + "\n")
 out.close()
+out_info.close()
 out_debug.close()
 out_debug2.close()
