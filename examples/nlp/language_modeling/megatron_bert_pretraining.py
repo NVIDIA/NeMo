85cc023adc530de59dd13903c4541ca008b260d3
Sandeep Subramanian <sandeep.subramanian.1@umontreal.ca>
2022-12-21 15:19:14 -0800

    Minor fixes (#5691)
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
    
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/language_modeling/megatron_bert_pretraining.py
+++ examples/nlp/language_modeling/megatron_bert_pretraining.py
@@ -15,7 +15,6 @@
 from lightning_lite.plugins.environments import TorchElasticEnvironment
 from omegaconf.omegaconf import OmegaConf, open_dict
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks.timer import Timer
 from pytorch_lightning.trainer.connectors.checkpoint_connector import CheckpointConnector
 
 from nemo.collections.nlp.models.language_modeling.megatron_bert_model import MegatronBertModel
