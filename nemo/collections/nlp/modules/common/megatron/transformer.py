Fix precision check
Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -686,11 +686,11 @@ class ParallelTransformerLayer(ParallelTransformerLayer_):
             moe_dropout=moe_dropout,
         )
 
-        if precision == 32:
+        if precision == 'bf16':
             self.dtype = torch.float32
-        elif precision == 16:
+        elif int(precision) == 16:
             self.dtype = torch.float16
-        elif precision == 'bf16':
+        elif int(precision) == 32:
             self.dtype = torch.bfloat16
         else:
             raise ValueError