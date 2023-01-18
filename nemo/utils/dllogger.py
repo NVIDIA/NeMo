ea4860e3c7a2509df9c712f2a245a5b67beea3c6
SeanNaren <snarenthiran@nvidia.com>
2023-01-18 13:52:34 +0000

    Protect the imports for DLLogger
    Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

--- nemo/utils/dllogger.py
+++ nemo/utils/dllogger.py
@@ -15,7 +15,6 @@
 from argparse import Namespace
 from pathlib import Path
 
-from lightning_fabric.utilities.logger import _convert_params, _flatten_dict, _sanitize_callable_params
 from lightning_utilities.core.apply_func import apply_to_collection
 from omegaconf import DictConfig, ListConfig, OmegaConf
 from pytorch_lightning.loggers import Logger
@@ -30,6 +29,13 @@ try:
     HAVE_DLLOGGER = True
 except (ImportError, ModuleNotFoundError):
     HAVE_DLLOGGER = False
+
+try:
+    from lightning_fabric.utilities.logger import _convert_params, _flatten_dict, _sanitize_callable_params
+
+    PL_LOGGER_UTILITIES = True
+except (ImportError, ModuleNotFoundError):
+    PL_LOGGER_UTILITIES = False
 
 
 class DLLogger(Logger):
@@ -46,6 +52,11 @@ class DLLogger(Logger):
             raise ImportError(
                 "DLLogger was not found. Please see the README for installation instructions: "
                 "https://github.com/NVIDIA/dllogger"
+            )
+        if not PL_LOGGER_UTILITIES:
+            raise ImportError(
+                "DLLogger utilities were not found. You probably need to update PyTorch Lightning>=1.9.0. "
+                "pip install pytorch-lightning -U"
             )
         verbosity = Verbosity.VERBOSE if verbose else Verbosity.DEFAULT
         backends = []
