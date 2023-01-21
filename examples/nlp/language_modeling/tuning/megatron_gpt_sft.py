36ac0b1810a11f43728bc1d1a8d61497ffa20c64
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2023-01-21 11:26:07 -0800

    Add dist opt support
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/language_modeling/tuning/megatron_gpt_sft.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_sft.py
@@ -126,6 +126,7 @@ def main(cfg) -> None:
     logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
 
     megatron_amp_o2 = cfg.model.get('megatron_amp_O2', False)
+    with_distributed_adam = cfg.model.optim.get('name', 'fused_adam') == 'distributed_fused_adam'
     plugins = []
     strategy = NLPDDPStrategy(
         no_ddp_communication_hook=True,
@@ -140,7 +141,7 @@ def main(cfg) -> None:
                 growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
                 hysteresis=cfg.model.get('hysteresis', 2),
             )
-        if megatron_amp_o2:
+        if megatron_amp_o2 and not with_distributed_adam:
             plugins.append(MegatronHalfPrecisionPlugin(precision=cfg.trainer.precision, device='cuda', scaler=scaler))
         else:
             plugins.append(PipelineMixedPrecisionPlugin(precision=cfg.trainer.precision, device='cuda', scaler=scaler))
