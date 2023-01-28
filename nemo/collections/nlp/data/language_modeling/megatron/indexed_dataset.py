291a7f7af30aee70a3ee9c8bd4f5e02c224baceb
Yi Dong <43824965+yidong72@users.noreply.github.com>
2023-01-28 09:45:44 -0500

    RETRO model finetuning (#5800)
    * add save and load dynmaic index
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * add chunk stride feature
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * add chunk stride feature
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * add no pq index
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added megatron lm compatible mode
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * addd config
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix position embedding
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added index factory
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * share neighbors and weights amoung strategies
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix bug
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added metric tto faiss index
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * set default to inner product
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added qa fine tuen dataset
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added fine tuning code
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * trim it
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix data issue
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix style
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added version
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix key error
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * make sure to overwrite the cfg
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * make multiple sentence bert available
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix the document
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix the table
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix transformer
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * make sure to turn off the rope in chunked cross attention layer
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix the security issue
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * style fix
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix codeql issues
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * use -1
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix empty index
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * clean up
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix the lower bound for repetition penalty
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * add retro qa inference strategy
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added new inference logic
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * working inference
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix TP inference
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * revert requirement
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * added file inference
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * use string to prevent collison
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * use NQ test
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix prompt
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix inference
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * set good defaults for demo
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * replicate adlr
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * make sure to turn off attention reset for megatron lm compatible model
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * style fix
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix typo
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix inference error
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * fix logging
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    * address comments
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    
    ---------
    
    Signed-off-by: Yi Dong <yidong@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nemo/collections/nlp/data/language_modeling/megatron/indexed_dataset.py
+++ nemo/collections/nlp/data/language_modeling/megatron/indexed_dataset.py
@@ -73,7 +73,7 @@ def infer_dataset_impl(path):
         return None
 
 
-def make_builder(out_file, impl, vocab_size=None, chunk_size=64, pad_id=0, retrieval_db=False):
+def make_builder(out_file, impl, vocab_size=None, chunk_size=64, pad_id=0, retrieval_db=False, stride=64):
     if impl == 'mmap':
         return MMapIndexedDatasetBuilder(out_file, dtype=__best_fitting_dtype(vocab_size))
     elif impl == 'retmmap':
@@ -83,6 +83,7 @@ def make_builder(out_file, impl, vocab_size=None, chunk_size=64, pad_id=0, retri
             pad_id=pad_id,
             retrieval_db=retrieval_db,
             dtype=__best_fitting_dtype(vocab_size),
+            stride=stride,
         )
     else:
         return IndexedDatasetBuilder(out_file)
