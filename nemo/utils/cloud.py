3ebe0a92e3dc89368c986fde87e83622be945a2a
SeanNaren <snarenthiran@nvidia.com>
2023-01-30 14:21:12 +0000

    Fixes
    Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

--- nemo/utils/cloud.py
+++ nemo/utils/cloud.py
@@ -95,6 +95,7 @@ class SageMakerDDPStrategy(DDPStrategy):
 
     @cluster_environment.setter
     def cluster_environment(self, env):
+        # prevents Lightning from overriding the Environment required for SageMaker
         pass
 
 
@@ -136,5 +137,6 @@ def initialize_sagemaker() -> None:
 
         if dist.get_local_rank():
             _install_system_libraries()
+        print("WORLD SIZE NEMO", dist.get_world_size())
         return dist.barrier()  # wait for main process
     _install_system_libraries()
