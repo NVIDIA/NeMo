2de78f3968257163b979e805b79f826a120a92f9
Tim Moon <tmoon@nvidia.com>
2023-01-31 10:15:44 -0800

    Add distopt support for overlapping param all-gather with forward compute
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- nemo/core/optim/distributed_adam.py
+++ nemo/core/optim/distributed_adam.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from apex.contrib.optimizers.distributed_fused_adam import DistributedFusedAdam
+from apex.contrib.optimizers.distributed_fused_adam import DistributedFusedAdam, _disable_pre_forward_hook
 from apex.transformer import parallel_state
 
 
@@ -26,6 +26,14 @@ class MegatronDistributedFusedAdam(DistributedFusedAdam):
 
     def _make_post_backward_hook(self, param, param_group_id, param_id):
         def hook(*unused):
+            if getattr(param, '_pre_forward_hook_is_enabled', False):
+                raise RuntimeError(
+                    'A parameter called its post-backward hook '
+                    'before its pre-forward hook. '
+                    'Please manually interact with the parameter '
+                    'before the forward pass (e.g. by calling data_ptr) '
+                    'or run DistributedFusedAdam with overlap_param_sync=False.'
+                )
             with self._lock:
                 need_to_initialize = 'fragments' not in self.state[param]
                 if need_to_initialize:
@@ -49,6 +57,5 @@ class MegatronDistributedFusedAdam(DistributedFusedAdam):
         super().zero_grad(*args, **kwargs)
         if self.contiguous_grad_buffer:
             for param in self.parameters():
-                param.main_grad = self.grad_buffer_view(param)
-                if param.dtype == param.main_grad.dtype and param.is_cuda:
-                    param.grad = param.main_grad
+                with _disable_pre_forward_hook(param):
+                    param.main_grad = self.grad_buffer_view(param)
