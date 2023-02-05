e797fe4ba1550a0ecb24bc4e1f234ac669051595
Somshubra Majumdar <titu1994@gmail.com>
2023-02-04 20:09:04 -0800

    Finalize ALSD alignment generation
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- tests/collections/asr/decoding/test_rnnt_decoding.py
+++ tests/collections/asr/decoding/test_rnnt_decoding.py
@@ -66,6 +66,7 @@ def get_rnnt_joint(vocab_size, vocabulary=None, encoder_output_size=4, decoder_o
     return joint
 
 
+@lru_cache(maxsize=1)
 def get_model_encoder_output(data_dir, model_name):
     # Import inside function to avoid issues with dependencies
     import librosa
@@ -74,6 +75,9 @@ def get_model_encoder_output(data_dir, model_name):
 
     with torch.no_grad():
         model = ASRModel.from_pretrained(model_name, map_location='cpu')  # type: ASRModel
+        model.preprocessor.featurizer.dither = 0.0
+        model.preprocessor.featurizer.pad_to = 0
+
         audio, sr = librosa.load(path=audio_filepath, sr=16000, mono=True)
 
         input_signal = torch.tensor(audio, dtype=torch.float32).unsqueeze(0)
@@ -84,11 +88,19 @@ def get_model_encoder_output(data_dir, model_name):
     return model, encoded, encoded_len
 
 
+def decode_text_from_greedy_hypotheses(hyps, decoding):
+    hypotheses = []
+
+    decoded_hyps = decoding.decode_hypothesis(hyps)  # type: List[str]
+
+    return decoded_hyps
+
+
 def decode_text_from_nbest_hypotheses(hyps, decoding):
     hypotheses = []
     all_hypotheses = []
 
-    for nbest_hyp in hyps:  # type: NBestHypotheses
+    for nbest_hyp in hyps:  # type: rnnt_utils.NBestHypotheses
         n_hyps = nbest_hyp.n_best_hypotheses  # Extract all hypotheses for this sample
         decoded_hyps = decoding.decode_hypothesis(n_hyps)  # type: List[str]
 
@@ -122,13 +134,55 @@ class TestRNNTDecoding:
     #     not NUMBA_RNNT_LOSS_AVAILABLE, reason='RNNTLoss has not been compiled with appropriate numba version.',
     # )
     # @pytest.mark.with_downloads
+    # @pytest.mark.unit
+    # def test_greedy_decoding_preserve_alignments(self, test_data_dir):
+    #     model, encoded, encoded_len = get_model_encoder_output(test_data_dir, 'stt_en_conformer_transducer_small')
+    #
+    #     print("Encoded length", encoded_len, encoded.shape)
+    #     beam = greedy_decode.GreedyRNNTInfer(
+    #         model.decoder,
+    #         model.joint,
+    #         blank_index=model.joint.num_classes_with_blank - 1,
+    #         max_symbols_per_step=5,
+    #         preserve_alignments=True,
+    #     )
+    #
+    #     enc_out = encoded
+    #     enc_len = encoded_len
+    #
+    #     with torch.no_grad():
+    #         hyps = beam(encoder_output=enc_out, encoded_lengths=enc_len)[0]  # type: rnnt_utils.Hypothesis
+    #         hyp = decode_text_from_greedy_hypotheses(hyps, model.decoding)
+    #         hyp = hyp[0]
+    #
+    #         assert hyp.alignments is not None
+    #
+    #         # Use the following commented print statements to check
+    #         # the alignment of other algorithms compared to the default
+    #         print("Text", hyp.text)
+    #         for t in range(len(hyp.alignments)):
+    #             t_u = []
+    #             for u in range(len(hyp.alignments[t])):
+    #                 logp, label = hyp.alignments[t][u]
+    #                 assert torch.is_tensor(logp)
+    #                 assert torch.is_tensor(label)
+    #
+    #                 t_u.append(int(label))
+    #
+    #             print(f"Tokens at timestep {t} = {t_u}")
+    #         print()
+
+    # @pytest.mark.skipif(
+    #     not NUMBA_RNNT_LOSS_AVAILABLE, reason='RNNTLoss has not been compiled with appropriate numba version.',
+    # )
+    # @pytest.mark.with_downloads
     @pytest.mark.unit
     @pytest.mark.parametrize(
         "beam_config",
         [
-            {"search_type": "greedy"},
-            {"search_type": "default", "beam_size": 2,},
-            {"search_type": "alsd", "alsd_max_target_len": 20, "beam_size": 2,},
+            # {"search_type": "greedy"},
+            # {"search_type": "default", "beam_size": 2,},
+            {"search_type": "alsd", "alsd_max_target_len": 0.5, "beam_size": 2,},
             # {"search_type": "tsd", "tsd_max_sym_exp_per_step": 3, "beam_size": 2, },
             # {"search_type": "maes", "maes_num_steps": 2, "maes_expansion_beta": 2, "beam_size": 2},
             # {"search_type": "maes", "maes_num_steps": 3, "maes_expansion_beta": 1, "beam_size": 2},
@@ -140,6 +194,9 @@ class TestRNNTDecoding:
         # decoder = get_rnnt_decoder(vocab_size=len(token_list))
         # joint_net = get_rnnt_joint(vocab_size=len(token_list))
         model, encoded, encoded_len = get_model_encoder_output(test_data_dir, 'stt_en_conformer_transducer_small')
+
+        print("beam type", beam_config['search_type'])
+        print("Encoded length", encoded_len, encoded.shape)
 
         beam = beam_decode.BeamRNNTInfer(
             model.decoder,
@@ -161,9 +218,12 @@ class TestRNNTDecoding:
 
             assert hyp.alignments is not None
 
-            # print(hyp)
+            if beam_config['search_type'] == 'alsd':
+                assert len(all_hyps) <= int(beam_config['alsd_max_target_len'] * float(enc_len[0]))
 
-            for idx, hyp_ in enumerate(all_hyps):
+            # Use the following commented print statements to check
+            # the alignment of other algorithms compared to the default
+            for idx, hyp_ in enumerate(all_hyps):  # type: (int, rnnt_utils.Hypothesis)
                 print("Hyp index", idx + 1, "text :", hyp_.text)
                 for t in range(len(hyp_.alignments)):
                     t_u = []
@@ -176,3 +236,5 @@ class TestRNNTDecoding:
 
                     print(f"Tokens at timestep {t} = {t_u}")
                 print()
+
+                print("Timesteps", hyp_.timestep)
