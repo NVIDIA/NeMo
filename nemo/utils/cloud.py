eb547bb14da33abf16a5a58620c04f4e5a4c0c56
SeanNaren <snarenthiran@nvidia.com>
2023-02-08 16:38:06 +0000

    Fix warning
    Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

--- nemo/utils/cloud.py
+++ nemo/utils/cloud.py
@@ -138,4 +138,5 @@ def initialize_sagemaker() -> None:
             _install_system_libraries()
         print("WORLD SIZE NEMO", dist.get_world_size())
         return dist.barrier()  # wait for main process
-    return _install_system_libraries()
+    _install_system_libraries()
+    return
