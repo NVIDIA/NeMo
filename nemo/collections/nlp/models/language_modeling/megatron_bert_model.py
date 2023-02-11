c62f026be3e7c9f3a617071485a6facc49df191c
shanmugamr1992 <shanmugamr1992@gmail.com>
2023-02-10 17:15:41 -0800

    Microbatch loader for bert

--- nemo/collections/nlp/models/language_modeling/megatron_bert_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_bert_model.py
@@ -19,11 +19,12 @@ import torch
 import torch.nn.functional as F
 from omegaconf.dictconfig import DictConfig
 from pytorch_lightning.trainer.trainer import Trainer
+from pytorch_lightning.plugins.precision.native_amp import NativeMixedPrecisionPlugin
 
 from nemo.collections.nlp.data.language_modeling.megatron import dataset_utils
-from nemo.collections.nlp.data.language_modeling.megatron.megatron_batch_samplers import (
-    MegatronPretrainingBatchSampler,
-    MegatronPretrainingRandomBatchSampler,
+from nemo.collections.nlp.data.language_modeling.megatron.data_samplers import (
+    MegatronPretrainingRandomSampler,
+    MegatronPretrainingSampler,
 )
 from nemo.collections.nlp.models.language_modeling.megatron.bert_model import BertModel
 from nemo.collections.nlp.models.language_modeling.megatron_base_model import MegatronBaseModel
@@ -32,6 +33,7 @@ from nemo.collections.nlp.modules.common.megatron.utils import (
     average_losses_across_data_parallel_group,
     get_params_for_weight_decay_optimization,
 )
+from nemo.collections.nlp.parts.nlp_overrides import GradScaler
 from nemo.collections.nlp.parts.utils_funcs import get_last_rank
 from nemo.core.classes.common import PretrainedModelInfo
 from nemo.core.neural_types import ChannelType, MaskType, NeuralType
@@ -117,6 +119,13 @@ class MegatronBertModel(MegatronBaseModel):
                     self.model = converted_model
             else:
                 self.model = Float16Module(module=self.model, precision=cfg.precision)
+            
+        if hasattr(self, '_nsys_profile_enabled'):
+            mp_size = cfg.get('tensor_model_parallel_size', 1) * cfg.get('pipeline_model_parallel_size', 1)
+            data_parallel_world_size = trainer.world_size // mp_size
+            grad_accum_steps = cfg.get('global_batch_size') // (cfg.get('micro_batch_size') * data_parallel_world_size)
+            self._nsys_profile_start_step *= grad_accum_steps
+            self._nsys_profile_end_step *= grad_accum_steps
 
     def model_provider_func(self, pre_process, post_process):
         cfg = self.cfg
@@ -179,26 +188,26 @@ class MegatronBertModel(MegatronBaseModel):
         return fwd_bwd_function
 
     def get_forward_output_and_loss_func(self):
-        def fwd_output_and_loss_func(batch, model, checkpoint_activations_all_layers=None):
+        def fwd_output_and_loss_func(dataloader_iter, model, checkpoint_activations_all_layers=None):
             if parallel_state.get_pipeline_model_parallel_world_size() == 1:
-                batch = [x.cuda(non_blocking=True) for x in batch]
-                tokens, types, sentence_order, loss_mask, lm_labels, padding_mask = batch
+                batch = next(dataloader_iter)
+                tokens, types, sentence_order, loss_mask, lm_labels, padding_mask = batch['tokens'], batch['types'], batch['sentence_order'], batch['loss_mask'], batch['lm_labels'], batch['padding_mask']
             else:
                 if parallel_state.is_pipeline_first_stage():
-                    tokens = batch[0].cuda(non_blocking=True)
-                    types = batch[1].cuda(non_blocking=True)
-                    sentence_order = batch[2].cuda(non_blocking=True)
-                    padding_mask = batch[5].cuda(non_blocking=True)
+                    tokens = batch['tokens'].cuda(non_blocking=True)
+                    types = batch['types'].cuda(non_blocking=True)
+                    sentence_order = batch['sentence_order'].cuda(non_blocking=True)
+                    padding_mask = batch['padding_mask'].cuda(non_blocking=True)
                     loss_mask, lm_labels = None, None
                 elif parallel_state.is_pipeline_last_stage():
-                    loss_mask = batch[3].cuda(non_blocking=True)
-                    lm_labels = batch[4].cuda(non_blocking=True)
-                    sentence_order = batch[2].cuda(non_blocking=True)
-                    padding_mask = batch[5].cuda(non_blocking=True)
+                    loss_mask = batch['loss_mask'].cuda(non_blocking=True)
+                    lm_labels = batch['lm_labels'].cuda(non_blocking=True)
+                    sentence_order = batch['sentence_order'].cuda(non_blocking=True)
+                    padding_mask = batch['padding_mask'].cuda(non_blocking=True)
                     tokens, types = None, None
                 else:
-                    padding_mask = batch[5].cuda(non_blocking=True)
-                    sentence_order = batch[2].cuda(non_blocking=True)
+                    padding_mask = batch['padding_mask'].cuda(non_blocking=True)
+                    sentence_order = batch['sentence_order'].cuda(non_blocking=True)
                     tokens, types, loss_mask, lm_labels = None, None, None, None
 
             if not self.cfg.bert_binary_head:
@@ -262,10 +271,8 @@ class MegatronBertModel(MegatronBaseModel):
 
         return output_tensor
 
-    def training_step(self, batch, batch_idx):
-
+    def training_step(self, dataloader_iter, batch_idx):
         self._optimizer.zero_grad()
-        batch_for_pipeline = self.process_batch(batch)
         tensor_shape = [self.cfg.encoder_seq_length, self.cfg.micro_batch_size, self.cfg.hidden_size]
 
         # handle asynchronous grad reduction
@@ -292,7 +299,7 @@ class MegatronBertModel(MegatronBaseModel):
 
         losses_reduced_per_micro_batch = fwd_bwd_function(
             forward_step_func=self.get_forward_output_and_loss_func(),
-            batch=batch_for_pipeline,
+            batch=dataloader_iter,
             model=self.model,
             forward_only=False,
             tensor_shape=tensor_shape,
@@ -389,15 +396,14 @@ class MegatronBertModel(MegatronBaseModel):
                     grad = word_embeddings_weight.grad
                 torch.distributed.all_reduce(grad, group=parallel_state.get_embedding_group())
 
-    def validation_step(self, batch, batch_idx):
-        batch_for_pipeline = self.process_batch(batch)
+    def validation_step(self, dataloader_iter, batch_idx):
         tensor_shape = [self.cfg.encoder_seq_length, self.cfg.micro_batch_size, self.cfg.hidden_size]
 
         fwd_bwd_function = self._get_fwd_bwd_function()
 
         losses_reduced_per_micro_batch = fwd_bwd_function(
             forward_step_func=self.get_forward_output_and_loss_func(),
-            batch=batch_for_pipeline,
+            batch=dataloader_iter,
             model=self.model,
             forward_only=True,
             tensor_shape=tensor_shape,
@@ -460,17 +466,6 @@ class MegatronBertModel(MegatronBaseModel):
             # averaged_losses = average_losses_across_data_parallel_group(
             #     [lm_loss])
             # return loss, {'lm loss': averaged_losses[0]}
-
-    def process_batch(self, batch):
-        """Build the batch."""
-        # Unpack.
-        tokens = batch['text'].long()
-        types = batch['types'].long()
-        sentence_order = batch['is_random'].long()
-        loss_mask = batch['loss_mask'].float()
-        lm_labels = batch['labels'].long()
-        padding_mask = batch['padding_mask'].long()
-        return [tokens, types, sentence_order, loss_mask, lm_labels, padding_mask]
 
     def build_train_valid_test_datasets(self):
         logging.info('Building Bert datasets.')
@@ -621,7 +616,7 @@ class MegatronBertModel(MegatronBaseModel):
         # Megatron sampler
         if hasattr(self.cfg.data, 'dataloader_type') and self.cfg.data.dataloader_type is not None:
             if self.cfg.data.dataloader_type == 'single':
-                batch_sampler = MegatronPretrainingBatchSampler(
+                batch_sampler = MegatronPretrainingSampler(
                     total_samples=len(dataset),
                     consumed_samples=consumed_samples,
                     micro_batch_size=self.cfg.micro_batch_size,
@@ -631,11 +626,10 @@ class MegatronBertModel(MegatronBaseModel):
                     drop_last=self.cfg.get('drop_last', True),
                 )
             elif self.cfg.data.dataloader_type == 'cyclic':
-                batch_sampler = MegatronPretrainingRandomBatchSampler(
+                batch_sampler = MegatronPretrainingRandomSampler(
                     total_samples=len(dataset),
                     consumed_samples=consumed_samples,
                     micro_batch_size=self.cfg.micro_batch_size,
-                    global_batch_size=self.cfg.global_batch_size,
                     data_parallel_rank=parallel_state.get_data_parallel_rank(),
                     data_parallel_size=parallel_state.get_data_parallel_world_size(),
                     drop_last=self.cfg.get('drop_last', True),
@@ -843,3 +837,41 @@ class MegatronBertModel(MegatronBaseModel):
                 parallel_state.set_virtual_pipeline_model_parallel_rank(i)
                 self.model[i].module.load_state_dict(checkpoint[f'model{i}'], strict=True)
             parallel_state.set_virtual_pipeline_model_parallel_rank(0)
+
+    def on_train_batch_end(self, outputs, dataloader_iter: Any, batch_idx: int, unused: Optional[int] = 0) -> None:
+        super().on_train_batch_end(outputs, dataloader_iter, batch_idx)
+
+        # TODO: Replace with newer override for scheduler.step() instead of
+        # search for plugins for fp16 GradScalar
+        if self.trainer.precision_plugin is not None and isinstance(
+            self.trainer.precision_plugin, NativeMixedPrecisionPlugin
+        ):
+            precision_plugin = self.trainer.precision_plugin
+
+            if (
+                hasattr(precision_plugin, 'scaler')
+                and precision_plugin.scaler is not None
+                and isinstance(precision_plugin.scaler, GradScaler)
+            ):
+                grad_scaler = precision_plugin.scaler
+
+                # If the grad scaler skipped its optimizer step due to infs/nans,
+                # decrement the step of all schedulers.
+                if grad_scaler.optimizer_update_skipped is not None and grad_scaler.optimizer_update_skipped is True:
+                    scheduler_cfgs = self.trainer.lr_scheduler_configs
+
+                    if not scheduler_cfgs or not self.trainer.lightning_module.automatic_optimization:
+                        return
+
+                    for scheduler_cfg in scheduler_cfgs:
+                        # Decrement the counter by 2, then perform a scheduler.step() to perform a no-up
+                        # as well as update the optimizer lr in all param groups
+                        scheduler_cfg.scheduler.last_epoch -= 2
+                        scheduler_cfg.scheduler.step()
+
+                    # Removing the line below because it messes up train_valid_test_num_samples calculation.
+                    # self.trainer.fit_loop.max_steps = self.trainer.fit_loop.max_steps + 1
+
+                    # Reset the optimizer update skipped to `None` - this is to prevent scheduler no-ops during
+                    # accumulated gradient updates.
+                    grad_scaler.optimizer_update_skipped = None
