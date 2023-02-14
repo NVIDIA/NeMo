dd935dfc02700da473eed3906aff8db9948586b5
slym <slym@login-preos02.a51.clusters.nvidia.com>
2023-02-14 10:22:12 -0800

    use a separate communicator for amax reduction

--- nemo/collections/nlp/modules/common/megatron/transformer.py
+++ nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -1420,7 +1420,7 @@ class ParallelTransformer(MegatronModule):
             # fp8_autocast will not do anything if TE or FP8 isn't used
             fp8_group = None
             if parallel_state.model_parallel_is_initialized():
-                fp8_group = parallel_state.get_data_parallel_group()
+                fp8_group = parallel_state.get_amax_reduction_group()
 
             if HAVE_TE:
                 # if TE is installed but fp8 is not available then this will do nothing
