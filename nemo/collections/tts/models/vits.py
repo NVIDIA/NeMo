5b71d4d57caed4f46d56c13c79fbef41b11fd2e3
Evelina <10428420+ekmb@users.noreply.github.com>
2023-02-15 14:22:50 -0800

    [TTS/TN/G2P] Remove Text Processing from NeMo, move G2P to TTS (#5982)
    * remove TN
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix imports
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix import
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add missing init
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix import
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * rename unit test
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix import
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix modules test
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix imports
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * remove whitelist from config
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * delete wordid file
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * remove pynini_install from tutorials
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * update requirements
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add support warning
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * review
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    ---------
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nemo/collections/tts/models/vits.py
+++ nemo/collections/tts/models/vits.py
@@ -97,14 +97,29 @@ class VitsModel(TextToWaveform):
                     'text_normalizer.whitelist', cfg.text_normalizer.whitelist
                 )
 
-            self.normalizer = instantiate(cfg.text_normalizer, **normalizer_kwargs)
-            self.text_normalizer_call = self.normalizer.normalize
+            try:
+                import nemo_text_processing
+
+                self.normalizer = instantiate(cfg.text_normalizer, **normalizer_kwargs)
+                self.text_normalizer_call = self.normalizer.normalize
+            except Exception as e:
+                logging.error(e)
+                raise ImportError(
+                    "`nemo_text_processing` not installed, see https://github.com/NVIDIA/NeMo-text-processing for more details"
+                )
             if "text_normalizer_call_kwargs" in cfg:
                 self.text_normalizer_call_kwargs = cfg.text_normalizer_call_kwargs
 
     def _setup_tokenizer(self, cfg):
         text_tokenizer_kwargs = {}
         if "g2p" in cfg.text_tokenizer and cfg.text_tokenizer.g2p is not None:
+            # for backward compatibility
+            if self._is_model_being_restored() and cfg.text_tokenizer.g2p.get('_target_', None):
+                cfg.text_tokenizer.g2p['_target_'] = cfg.text_tokenizer.g2p['_target_'].replace(
+                    "nemo_text_processing.g2p", "nemo.collections.tts.g2p"
+                )
+                logging.warning("This checkpoint support will be dropped after r1.18.0.")
+
             g2p_kwargs = {}
 
             if "phoneme_dict" in cfg.text_tokenizer.g2p:
