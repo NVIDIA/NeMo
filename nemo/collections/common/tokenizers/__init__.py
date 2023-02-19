dca7ddc65b15457ab651a20cbe3b1080918884f6
Somshubra Majumdar <titu1994@gmail.com>
2023-02-19 01:17:36 -0800

    Force TokenizerSpec to be imported first (to prevent circular dependency) and force isort to ignore it
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/collections/common/tokenizers/__init__.py
+++ nemo/collections/common/tokenizers/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from nemo.collections.common.tokenizers.tokenizer_spec import TokenizerSpec  # isort:skip
 from nemo.collections.common.tokenizers.aggregate_tokenizer import AggregateTokenizer
 from nemo.collections.common.tokenizers.bytelevel_tokenizers import ByteLevelTokenizer
 from nemo.collections.common.tokenizers.char_tokenizer import CharTokenizer
@@ -19,6 +20,5 @@ from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTo
 from nemo.collections.common.tokenizers.regex_tokenizer import RegExTokenizer
 from nemo.collections.common.tokenizers.sentencepiece_tokenizer import SentencePieceTokenizer
 from nemo.collections.common.tokenizers.text_to_speech.tokenizer_wrapper import TextToSpeechTokenizer
-from nemo.collections.common.tokenizers.tokenizer_spec import TokenizerSpec
 from nemo.collections.common.tokenizers.word_tokenizer import WordTokenizer
 from nemo.collections.common.tokenizers.youtokentome_tokenizer import YouTokenToMeTokenizer
