6cbf1d6e58fe42a36c18e5b8a9c3bb4e2e58b4eb
Nikolay Karpov <karpnv@gmail.com>
2023-02-22 08:51:14 -0800

    update kenlm
    Signed-off-by: Nikolay Karpov <karpnv@gmail.com>

--- scripts/asr_language_modeling/ngram_lm/kenlm_utils.py
+++ scripts/asr_language_modeling/ngram_lm/kenlm_utils.py
@@ -19,44 +19,216 @@ Utility methods to be used for training N-gram LM with KenLM in train_kenlm.py
 
 import json
 import os
-
+import gzip
+import re
+import argparse
+import torch
 import numpy as np
 from joblib import Parallel, delayed
 from tqdm.auto import tqdm
 
+import nemo.collections.asr as nemo_asr
+from nemo.utils import logging
+
+TOKEN_OFFSET = 100
 # List of the supported models to be used with N-gram LM and beam search decoding
 SUPPORTED_MODELS = {
-    'EncDecCTCModelBPE': 'subword',
-    'EncDecCTCModel': 'char',
-}
-
+    'EncDecCTCModelBPE': ('subword', True),
+    'EncDecRNNTBPEModel': ('subword', False),
+    'EncDecCTCModel': ('char', True),
+    'EncDecRNNTModel': ('char', False),
+    }
 
 def softmax(x):
     e = np.exp(x - np.max(x))
     return e / e.sum(axis=-1).reshape([x.shape[0], 1])
 
+def get_train_list(args_train_path):
 
-def read_train_file(path, lowercase: bool = False):
-    lines_read = 0
-    text_dataset = []
+    train_path = []
+    for train_item in args_train_path:
+        if os.path.isdir(train_item):
+            file_list = os.listdir(train_item)
+            train_path.extend([os.path.join(train_item, file) for file in file_list])
 
-    with open(path, 'r', encoding='utf-8') as f:
-        reader = tqdm(iter(lambda: f.readline(), ''), desc="Read 0 lines", unit=' lines')
-        for i, line in enumerate(reader):
-            if path.endswith('.json'):
-                line = json.loads(line)['text']
+        elif os.path.isfile(train_item):
+            train_path.append(train_item)
+    return sorted(train_path)
 
-            line = line.replace("\n", "").strip()
+def setup_tokenizer(nemo_model_file):
+    """ TOKENIZER SETUP """
+    logging.info(f"Loading nemo model '{nemo_model_file}' ...")
+
+    if nemo_model_file.endswith('.nemo'):
+        model = nemo_asr.models.ASRModel.restore_from(nemo_model_file, map_location=torch.device('cpu'))
+    else:
+        logging.warning(
+            "nemo_model_file does not end with .nemo, therefore trying to load a pretrained model with this name."
+        )
+        model = nemo_asr.models.ASRModel.from_pretrained(nemo_model_file, map_location=torch.device('cpu'))
+
+    encoding_level, offset_encoding = SUPPORTED_MODELS.get(type(model).__name__, None)
+    if not encoding_level:
+        logging.warning(
+            f"Model type '{type(model).__name__}' may not be supported. Would try to train a char-level LM."
+        )
+        encoding_level = 'char'
+    return model, encoding_level, offset_encoding
+
+def iter_files(train_path, nemo_model_file, do_lowercase, rm_punctuation, clean_text):
+    model, _, offset_encoding = setup_tokenizer(nemo_model_file)
+    for fname in train_path:
+        dataset = read_train_file(fname, do_lowercase, rm_punctuation, clean_text, verbose=0)
+        tokenize_text(dataset, model.tokenizer, path='', chunk_size=8192, 
+                      buffer_size=32, 
+                      token_offset=TOKEN_OFFSET if offset_encoding else -1)
+
+
+def norm_spaces(text):
+    #regex for removing extra whitespaces:
+    #"because  they're say-   we- at    least that's" -> "because they're say- we- at least that's"
+    reg_remove_extra_space = '\s{2,}'
+    text = re.sub(reg_remove_extra_space, ' ', text)
+    
+    #regex for removing whitespaces at the begining and at the end:
+    #" because they're say- we- at least that's " -> "because they're say- we- at least that's"
+    reg_remove_start_end_spaces = '\A\s|\s$'
+    text = re.sub(reg_remove_start_end_spaces, '', text)
+    
+    return text
+
+
+def norm_punctuation(text, punctuation_marks):
+    i = 1
+    
+    while i < len(text):
+        char = text[i]
+        
+        if char in punctuation_marks and text[i - 1] == ' ':
+            text = text[ : i - 1] + text[i : ]
+        else:
+            i += 1
+    
+    return text
+
+
+class TextProcessor():
+    def __init__(self) -> None:
+        self.TARGET_PUNCTUATION_MARKS = ['.', ',', '?']
+        self.rm_TARGET_PUNCTUATION_MARKS = str.maketrans('', '', ''.join(self.TARGET_PUNCTUATION_MARKS))
+
+        self.REGEX_TARGET_CHARS = re.compile("[a-zA-Z\'\ " + '\\'.join(self.TARGET_PUNCTUATION_MARKS) + "]+")
+            
+        self.CHARS_TO_REPLACE = {'!' : '.', 
+                            ';' : ',',
+                            '…' : '.'}
+        
+        self.CHARS_TO_REPLACE_WITH_SPACE = ['"', '-', ':', '“', '”', '—', '–', '(', ')', 
+                                    '\t', '\n', '[', ']', '«', '»', '„', '/', '_']
+        
+        self.APOSTROPHES = ["'", '’', '‘', '´', '`', 'ʻ']
+        
+        self.REGEX_ELLIPSIS_NORMALIZATION = re.compile(r'(\.\.\.)')
+
+        self.regex_apostophe_norm = re.compile("([a-zA-Z]\'[a-z])")
+
+        self.white_space = re.compile(r"\s+", flags=re.UNICODE)
+
+
+    def get_text_pc(self, text):
+        if self.REGEX_TARGET_CHARS.fullmatch(text):
+            return text
+        else:
+            #ellipsis normalization ('...' -> '.')
+            text = self.REGEX_ELLIPSIS_NORMALIZATION.sub('.', text)
+            
+            processed = ''
+            
+            for char in text:
+                if self.REGEX_TARGET_CHARS.match(char):
+                    processed += char   
+                elif char in self.CHARS_TO_REPLACE:
+                    processed += self.CHARS_TO_REPLACE[char]
+                elif char in self.CHARS_TO_REPLACE_WITH_SPACE:
+                    processed += ' '
+                elif char in self.APOSTROPHES:
+                    processed += "'"
+                else:
+                    return None
+            
+            #apostrophe normalization (removing quotations and saving apostrophes) 
+            #ex. ‘Never mind,’ said Wardle's wife --> Never mind, said Wardle's wife
+            
+            if processed[0] == "'":
+                processed = processed[1 : ]
+
+
+            for i in range(1, len(processed) - 1):
+                if processed[i] == "'":
+                    if self.regex_apostophe_norm.match(processed[i - 1 : i + 2]):
+                        continue
+                    else:
+                        processed = processed[: i] + ' ' + processed[i + 1 : ]
+                    
+
+            if processed[-1] == "'":
+                processed = processed[: -1]
+            
+            processed = norm_spaces(processed)
+            processed = norm_punctuation(processed, self.TARGET_PUNCTUATION_MARKS)
+            
+            return processed
+
+    def preprocess(self, line_list, lowercase: bool = False, rm_punctuation: bool = False, clean_text: bool = False):
+        l_list = []
+        for line in line_list:
+            if line and clean_text:
+                line = self.get_text_pc(line)
+                if line is None:
+                    line = ""
+                line = line.replace(",", " ,")
+                line = line.replace(".", " .")
+                line = line.replace("?", " ?")
+                line = self.white_space.sub(' ', line).strip()
             if lowercase:
                 line = line.lower()
+            if rm_punctuation:
+                line = line.translate(self.rm_TARGET_PUNCTUATION_MARKS)
+            if line:
+                l_list.append(line)
+        return " ".join(l_list)
+
+def read_train_file(path, lowercase: bool = False, rm_punctuation: bool = False, clean_text: bool = False, verbose: int=0):
+    lines_read = 0
+    text_dataset = []
+    text_processor = TextProcessor()
+    if path[-8:] == '.json.gz':
+        fin = gzip.open(path, 'r')
+    else:
+        fin = open(path, 'r', encoding='utf-8')
+
+    if verbose>0:
+        reader = tqdm(iter(lambda: fin.readline(), ''), desc="Read 0 lines", unit=' lines')
+    else:
+        reader = fin
+
+    for line in reader:
+        if line:
+            if path[-8:] == '.json.gz':
+                line = json.loads(line.decode('utf-8'))['text']
+            elif path.endswith('.json'):
+                line = json.loads(line)['text']
+            
+            line_list = line.split("\n")
+            line = text_processor.preprocess(line_list, lowercase, rm_punctuation, clean_text)
 
             if line:
                 text_dataset.append(line)
-
                 lines_read += 1
-                if lines_read % 100000 == 0:
+                if verbose>0 and lines_read % 100000 == 0:
                     reader.set_description(f"Read {lines_read} lines")
-
+        else:
+            break
     return text_dataset
 
 
@@ -64,23 +236,23 @@ def tokenize_str(texts, tokenizer, offset):
     tokenized_text = []
     for text in texts:
         tok_text = tokenizer.text_to_ids(text)
-        tok_text = [chr(token + offset) for token in tok_text]
+        if offset < 0:
+            tok_text = [str(token) for token in tok_text]
+        else:
+            tok_text = [chr(token + offset) for token in tok_text]
         tokenized_text.append(tok_text)
     return tokenized_text
 
 
 def tokenize_text(data, tokenizer, path, chunk_size=8192, buffer_size=32, token_offset=100):
     dataset_len = len(data)
-    print(
-        f"Chunking {dataset_len} rows into {dataset_len / float(chunk_size):0.4f} tasks (each chunk contains {chunk_size} elements)"
-    )
+    # print(f"Chunking {dataset_len} rows into {dataset_len / float(chunk_size):0.4f} tasks (each chunk contains {chunk_size} elements)")
 
     current_step = 0
-    if os.path.exists(path):
-        print(f"Deleting previous file : {path}")
+    if path and os.path.exists(path):
         os.remove(path)
 
-    with Parallel(n_jobs=-2, verbose=10) as parallel:
+    with Parallel(n_jobs=-2, verbose=0) as parallel: 
         while True:
             start = current_step * chunk_size
             end = min((current_step + buffer_size) * chunk_size, dataset_len)
@@ -93,20 +265,53 @@ def tokenize_text(data, tokenizer, path, chunk_size=8192, buffer_size=32, token_
             # Write dataset
             write_dataset(tokenized_data, path)
             current_step += len(tokenized_data)
-            print(f"Finished writing {len(tokenized_data)} chunks to {path}. Current chunk index = {current_step}")
+            if path:
+                print(f"Finished writing {len(tokenized_data)} chunks to {path}. Current chunk index = {current_step}")
             del tokenized_data
             if end >= dataset_len:
                 break
 
 
 def write_dataset(chunks, path):
-    basedir = os.path.dirname(path)
-
-    if not os.path.exists(basedir):
-        os.makedirs(basedir, exist_ok=True)
-
-    with open(path, 'a+', encoding='utf-8') as f:
-        for chunk_idx in tqdm(range(len(chunks)), desc='Chunk ', total=len(chunks), unit=' chunks'):
+    if path:
+        basedir = os.path.dirname(path)
+        if not os.path.exists(basedir):
+            os.makedirs(basedir, exist_ok=True)
+        with open(path, 'a+', encoding='utf-8') as f:
+            for chunk_idx in tqdm(range(len(chunks)), desc='Chunk ', total=len(chunks), unit=' chunks'):
+                for text in chunks[chunk_idx]:
+                    line = ' '.join(text)
+                    f.write(f"{line}\n")
+    else:
+        for chunk_idx in range(len(chunks)):
             for text in chunks[chunk_idx]:
                 line = ' '.join(text)
-                f.write(f"{line}\n")
+                print(f"{line}\n")
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description="Avg pytorch weights")
+    parser.add_argument(
+        "--train_path",
+        required=True,
+        nargs="+",
+        type=str,
+        help="Path to the training file or files. it can be a text file or JSON manifest",
+    )
+    parser.add_argument(
+        "--nemo_model_file",
+        required=True,
+        type=str,
+        help="Path to the training file or whitespace separated files. it can be a text file, JSON manifest or .json.gz",
+    )
+    parser.add_argument(
+        "--do_lowercase", action='store_true', help="Whether to apply lower case conversion on the training text"
+    )
+    parser.add_argument(
+        "--rm_punctuation", action='store_true', help="Whether to remove punctuation on the training text"
+    )
+    parser.add_argument("--clean_text", action='store_true', help="Whether to clean the text")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    logging.setLevel(logging.ERROR)
+    iter_files(**vars(_parse_args()))
--- scripts/asr_language_modeling/ngram_lm/train_kenlm.py
+++ scripts/asr_language_modeling/ngram_lm/train_kenlm.py
@@ -24,11 +24,10 @@
 # to 'scripts/ngram_lm/install_beamsearch_decoders.sh' on how to install them.
 #
 # USAGE: python train_kenlm.py --nemo_model_file <path to the .nemo file of the model> \
-#                              --train_file <path to the training text or JSON manifest file \
+#                              --train_path <path to the training text or JSON manifest file \
 #                              --kenlm_bin_path <path to the bin folder of KenLM library> \
 #                              --kenlm_model_file <path to store the binary KenLM model> \
-#                              --ngram_length <order of N-gram model> \
-#                              --preserve_arpa
+#                              --ngram_length <order of N-gram model>
 #
 # After training is done, the binary LM model is stored at the path specified by '--kenlm_model_file'.
 # You may find more info on how to use this script at:
@@ -39,11 +38,9 @@ import logging
 import os
 import subprocess
 import sys
+from glob import glob
 
 import kenlm_utils
-import torch
-
-import nemo.collections.asr as nemo_asr
 from nemo.utils import logging
 
 """
@@ -52,22 +49,24 @@ use a trick to encode the sub-word tokens of the training data as unicode charac
 TOKEN_OFFSET is the offset in the unicode table to be used to encode the BPE sub-words. This encoding scheme reduces 
 the required memory significantly, and the LM and its binary blob format require less storage space.
 """
-TOKEN_OFFSET = 100
-
+TOKEN_OFFSET = kenlm_utils.TOKEN_OFFSET
 CHUNK_SIZE = 8192
 CHUNK_BUFFER_SIZE = 512
 
 
 def main():
+    file_path = os.path.split(os.path.realpath(__file__))[0]
     parser = argparse.ArgumentParser(
         description='Train an N-gram language model with KenLM to be used with beam search decoder of ASR models.'
     )
     parser.add_argument(
-        "--train_file",
+        "--train_path",
         required=True,
+        nargs="+",
         type=str,
-        help="Path to the training file, it can be a text file or JSON manifest",
+        help="Path to the training files or whitespace separated files. Files can be a text file, JSON manifest or .json.gz",
     )
+    
     parser.add_argument(
         "--nemo_model_file",
         required=True,
@@ -78,71 +77,95 @@ def main():
         "--kenlm_model_file", required=True, type=str, help="The path to store the KenLM binary model file"
     )
     parser.add_argument("--ngram_length", required=True, type=int, help="The order of N-gram LM")
+    parser.add_argument("--ngram_prun", default="0", required=False, nargs="+", type=str, help="Whitespace separated digits. Example: 0 0 1")
     parser.add_argument("--kenlm_bin_path", required=True, type=str, help="The path to the bin folder of KenLM")
-    parser.add_argument(
-        "--do_lowercase", action='store_true', help="Whether to apply lower case conversion on the training text"
-    )
-    parser.add_argument(
-        '--preserve_arpa', required=False, action='store_true', help='Whether to preserve the intermediate ARPA file.'
-    )
+    parser.add_argument("--cache_path", required=False, type=str, default=None, help="Cache path")
+    parser.add_argument("--do_lowercase", action='store_true', help="Whether to apply lower case conversion on the training text")
+    parser.add_argument("--rm_punctuation", action='store_true', help="Whether to remove punctuation on the training text")
+    parser.add_argument("--clean_text", action='store_true', help="Whether to clean the text")
+    parser.add_argument("--verbose",type=int, default=1, help="Verbose level from 0. Default is 1 ")
+
     args = parser.parse_args()
+    args.train_path = kenlm_utils.get_train_list(args.train_path)
+    
+    if type(args.ngram_prun) == str: args.ngram_prun = [args.ngram_prun]
 
-    """ TOKENIZER SETUP """
-    logging.info(f"Loading nemo model '{args.nemo_model_file}' ...")
+    model, encoding_level, offset_encoding = kenlm_utils.setup_tokenizer(args.nemo_model_file)
+    encoded_train_files = []
 
-    if args.nemo_model_file.endswith('.nemo'):
-        model = nemo_asr.models.ASRModel.restore_from(args.nemo_model_file, map_location=torch.device('cpu'))
+    if args.cache_path:  
+      """ DATASET SETUP """
+      
+      for file_num, train_file in enumerate(args.train_path):
+        logging.info(f"Encoding the train file '{train_file}' number {file_num+1} out of {len(args.train_path)} ...")
+
+        cached_files = glob(os.path.join(args.cache_path, os.path.split(train_file)[1])+"*")
+        encoded_train_file = os.path.join(args.cache_path, os.path.split(train_file)[1] + f"_{file_num}.tmp.txt")
+        if cached_files:
+            if cached_files[0]!=encoded_train_file:
+                os.rename(cached_files[0], encoded_train_file)
+                logging.info("Rename", cached_files[0], "to", encoded_train_file)
+        else:
+            dataset = kenlm_utils.read_train_file(train_file,
+                        lowercase=args.do_lowercase,
+                        rm_punctuation=args.rm_punctuation,
+                        clean_text=args.clean_text,
+                        verbose=args.verbose)
+
+            if encoding_level == "subword":
+                kenlm_utils.tokenize_text(
+                    dataset,
+                    model.tokenizer,
+                    path=encoded_train_file,
+                    chunk_size=CHUNK_SIZE,
+                    buffer_size=CHUNK_BUFFER_SIZE,
+                    token_offset=TOKEN_OFFSET if offset_encoding else -1,
+                )
+            else:
+                with open(encoded_train_file, 'w', encoding='utf-8') as f:
+                    for line in dataset:
+                        f.write(f"{line}\n")
+
+        encoded_train_files.append(encoded_train_file)
+
+      del model
+      first_process_args = ["cat"] + encoded_train_files
+
     else:
-        logging.warning(
-            "nemo_model_file does not end with .nemo, therefore trying to load a pretrained model with this name."
-        )
-        model = nemo_asr.models.ASRModel.from_pretrained(args.nemo_model_file, map_location=torch.device('cpu'))
+      first_process_args = ["python3", 
+                        os.path.join(file_path, "kenlm_utils.py"),
+                        "--nemo_model_file",
+                        args.nemo_model_file,
+                        "--train_path",
+                        ] + args.train_path
+      if args.clean_text:
+        first_process_args.append("--clean_text")
+      if args.do_lowercase:
+        first_process_args.append("--do_lowercase")
+      if args.rm_punctuation:
+        first_process_args.append("--rm_punctuation")
 
-    encoding_level = kenlm_utils.SUPPORTED_MODELS.get(type(model).__name__, None)
-    if not encoding_level:
-        logging.warning(
-            f"Model type '{type(model).__name__}' may not be supported. Would try to train a char-level LM."
-        )
-        encoding_level = 'char'
-
-    """ DATASET SETUP """
-    logging.info(f"Encoding the train file '{args.train_file}' ...")
-    dataset = kenlm_utils.read_train_file(args.train_file, lowercase=args.do_lowercase)
-    encoded_train_file = f"{args.kenlm_model_file}.tmp.txt"
+    first_process = subprocess.Popen(first_process_args, stdout=subprocess.PIPE)
+    
     if encoding_level == "subword":
-        kenlm_utils.tokenize_text(
-            dataset,
-            model.tokenizer,
-            path=encoded_train_file,
-            chunk_size=CHUNK_SIZE,
-            buffer_size=CHUNK_BUFFER_SIZE,
-            token_offset=TOKEN_OFFSET,
-        )
-        # --discount_fallback is needed for training KenLM for BPE-based models
-        discount_arg = "--discount_fallback"
+        discount_arg = "--discount_fallback" # --discount_fallback is needed for training KenLM for BPE-based models
     else:
-        with open(encoded_train_file, 'w', encoding='utf-8') as f:
-            for line in dataset:
-                f.write(f"{line}\n")
-
         discount_arg = ""
-
-    del model
-
     arpa_file = f"{args.kenlm_model_file}.tmp.arpa"
     """ LMPLZ ARGUMENT SETUP """
     kenlm_args = [
         os.path.join(args.kenlm_bin_path, 'lmplz'),
         "-o",
-        f"{args.ngram_length}",
-        "--text",
-        encoded_train_file,
+        str(args.ngram_length),
         "--arpa",
         arpa_file,
         discount_arg,
-    ]
+        "--prune"
+    ] + args.ngram_prun
 
-    ret = subprocess.run(kenlm_args, capture_output=False, text=True, stdout=sys.stdout, stderr=sys.stderr)
+    ret = subprocess.run(kenlm_args, stdin=first_process.stdout, capture_output=False, text=True, stdout=sys.stdout, stderr=sys.stderr)
+    first_process.wait()
+    logging.setLevel(logging.INFO)
     if ret.returncode != 0:
         raise RuntimeError("Training KenLM was not successful!")
     """ BINARY BUILD """
@@ -157,13 +180,6 @@ def main():
 
     if ret.returncode != 0:
         raise RuntimeError("Training KenLM was not successful!")
-
-    os.remove(encoded_train_file)
-    logging.info(f"Deleted the temporary encoded training file '{encoded_train_file}'.")
-
-    if not args.preserve_arpa:
-        os.remove(arpa_file)
-        logging.info(f"Deleted the arpa file '{arpa_file}'.")
 
 
 if __name__ == '__main__':
