a5113cbda26422289a3138b3f8ce606e30d3a8d2
arendu <adithya.r@gmail.com>
2023-02-25 21:21:06 -0800

    check for nlp model
    Signed-off-by: arendu <adithya.r@gmail.com>

--- nemo/utils/exp_manager.py
+++ nemo/utils/exp_manager.py
@@ -40,6 +40,7 @@ from pytorch_lightning.strategies.ddp import DDPStrategy
 from pytorch_lightning.utilities import rank_zero_info
 
 from nemo.collections.common.callbacks import EMA
+from nemo.collections.nlp.models.nlp_model import NLPModel
 from nemo.constants import NEMO_ENV_VARNAME_TESTING, NEMO_ENV_VARNAME_VERSION
 from nemo.utils import logging, timers
 from nemo.utils.app_state import AppState
@@ -920,7 +921,7 @@ class NeMoModelCheckpoint(ModelCheckpoint):
         self.best_model_path = best_k_models[0]
         self.best_model_score = self.best_k_models[self.best_model_path]
 
-    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
+    def _on_save_nlp_model_checkpoint(self, trainer, pl_module, checkpoint):
         # output = None
         output = super().on_save_checkpoint(trainer, pl_module, checkpoint)
         if not self.always_save_nemo:
@@ -970,6 +971,43 @@ class NeMoModelCheckpoint(ModelCheckpoint):
                 pl_module.save_to(save_path=app_state.model_restore_path)
                 logging.info(f"New .nemo model saved to: {app_state.model_restore_path}")
             return output
+
+    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
+        if isinstance(pl_module, NLPModel):
+            return self._on_save_nlp_model_checkpoint(trainer, pl_module, checkpoint)
+        else:
+            # output = None
+            output = super().on_save_checkpoint(trainer, pl_module, checkpoint)
+            if not self.always_save_nemo:
+                return output
+            else:
+                # Load the best model and then re-save it
+                app_state = AppState()
+                if app_state.model_parallel_size is not None and app_state.model_parallel_size > 1:
+                    raise ValueError(f'always_save_nemo is not implemented for model parallel models.')
+                # since we are creating tarfile artifacts we need to update .nemo path
+                app_state.model_restore_path = os.path.abspath(
+                    os.path.expanduser(os.path.join(self.dirpath, self.prefix + self.postfix))
+                )
+                if self.save_best_model:
+                    if not os.path.exists(self.best_model_path):
+                        return output
+
+                    if self.best_model_path == self.previous_best_path:
+                        return output
+
+                    self.previous_model_path = self.best_model_path
+                    old_state_dict = deepcopy(pl_module.state_dict())
+                    checkpoint = torch.load(self.best_model_path, map_location='cpu')
+                    if 'state_dict' in checkpoint:
+                        checkpoint = checkpoint['state_dict']
+                    # get a new instanace of the model
+                    pl_module.load_state_dict(checkpoint, strict=True)
+                    pl_module.save_to(save_path=app_state.model_restore_path)
+                    pl_module.load_state_dict(old_state_dict, strict=True)
+                else:
+                    pl_module.save_to(save_path=app_state.model_restore_path)
+                return output
 
     def on_train_end(self, trainer, pl_module):
         if trainer.fast_dev_run:
