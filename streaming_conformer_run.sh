e10f8a6d09e0d62131710afc74759f20a42f5182
Greg Clark <grclark@nvidia.com>
2023-02-28 13:59:48 -0500

    Remove test scripts
    Signed-off-by: Greg Clark <grclark@nvidia.com>

--- streaming_conformer_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python examples/asr/asr_cache_aware_streaming/speech_to_text_cache_aware_streaming_infer.py \
-    --asr_model=/models/sel_ngcinit_nemoasrset3.0_d512_adamwlr2.0_wd0_augx_speunigram1024_streaming_104_12_wm10k_ctc_striding4x_400e_clip1_newcode.nemo \
-    --manifest_file=/datasets/ls_test_other/transcripts.local.json \
-    --batch_size=128
-
