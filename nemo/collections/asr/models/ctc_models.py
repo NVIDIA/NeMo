a1da4367bcc3463e7df7443e2fb79d04f8ce9097
Dima Rekesh <drekesh@nvidia.com>
2023-03-01 21:11:44 -0800

    docs and adding this fix to RNNT models as well
    Signed-off-by: Dima Rekesh <drekesh@nvidia.com>

--- nemo/collections/asr/models/ctc_models.py
+++ nemo/collections/asr/models/ctc_models.py
@@ -361,8 +361,10 @@ class EncDecCTCModel(ASRModel, ExportableEncDecModel, ASRModuleMixin, InterCTCMi
         if hasattr(dataset, 'collate_fn'):
             collate_fn = dataset.collate_fn
         elif hasattr(dataset.datasets[0], 'collate_fn'):
+            # support datasets that are lists of entries
             collate_fn = dataset.datasets[0].collate_fn
         else:
+            # support datasets that are lists of lists
             collate_fn = dataset.datasets[0].datasets[0].collate_fn
 
         return torch.utils.data.DataLoader(
