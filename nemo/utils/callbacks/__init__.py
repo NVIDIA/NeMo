a80c4eb4f4e7a88b5645b5833814d02027a944c2
Abhishree <abhishreetm@gmail.com>
2023-03-16 03:35:25 +0000

    Adding the following modifications
    1) Rename nemo/collections/common/callbacks/nemomodelcheckpoint.py to nemo/utils/callbacks/nemo_model_checkpoint.py
    2) Rename nemo/utils/preemption_callback.py to nemo/utils/callbacks/preemption.py
    3) Add docstrings, headers, logging and check for torch distributed
    
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- nemo/utils/callbacks/__init__.py
+++ nemo/utils/callbacks/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.collections.common.callbacks.callbacks import LogEpochTimeCallback
-from nemo.collections.common.callbacks.ema import EMA
+from nemo.utils.callbacks.preemption import PreemptionCallback
+from nemo.utils.callbacks.nemo_model_checkpoint import NeMoModelCheckpoint
--- nemo/utils/callbacks/preemption.py
+++ nemo/utils/callbacks/preemption.py
@@ -1,11 +1,31 @@
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import Callback, ModelCheckpoint
-from nemo.collections.common.callbacks.nemomodelcheckpoint import NeMoModelCheckpoint
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import signal  
 import torch
 import sys
+from pytorch_lightning.callbacks import Callback
+from nemo.utils import logging
 
 class PreemptionCallback(Callback):
+    """
+    PreemptionCallback class creates a callback that checks for preemption during training at the end of every step.
+    Upon preemption the callback provides function to gracefully exit the training immediately and also saves the state of training (to be able to start from the 
+    same step without wasting any compute while resuming the next time).
+
+    PreemptionCallback is always enabled.
+    """
 
     def __init__(self, device, checkpoint_callback, sig=signal.SIGTERM):
         self.sig = sig
@@ -23,6 +43,11 @@ class PreemptionCallback(Callback):
         self._interrupted = False
         self.released = False
         self.original_handler = signal.getsignal(self.sig)
+
+        if pl_module.device.type == 'cuda':
+            assert torch.distributed.is_available() and torch.distributed.is_initialized(), "Preemption requires torch distributed to be initialized"
+        else:
+            logging.info("Preemption is supported only on GPUs")
 
         def master_handler(signum, frame):
             self.release()
@@ -48,13 +73,14 @@ class PreemptionCallback(Callback):
         # distributed broadcast of "_interrupted" flag from rank 0 to all other
         # ranks, to avoid performance overheads it's best to store the result in
         # a regular local variable
-        interrupted = self.interrupted
+        #interrupted = self.interrupted(pl_module.device)
+        interrupted = self.interrupted()
         if interrupted:
-            print("Received SIGTERM, exiting")
+            logging.info("Received SIGTERM, exiting")
             monitor_candidates = self.checkpoint_callback._monitor_candidates(trainer)
             self.checkpoint_callback._save_last_checkpoint(trainer, monitor_candidates)
             sys.exit(0)
-
+            
     def release(self):
         if self.released:
             return False
