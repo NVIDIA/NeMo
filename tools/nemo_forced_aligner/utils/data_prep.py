d916db4edde88234509961d44b2db2b201aafe53
Elena Rastorgueva <erastorgueva@nvidia.com>
2023-03-16 14:37:32 -0700

    preprocess input text by removing any duplicate spaces and converting any newlines to spaces
    Signed-off-by: Elena Rastorgueva <erastorgueva@nvidia.com>

--- tools/nemo_forced_aligner/utils/data_prep.py
+++ tools/nemo_forced_aligner/utils/data_prep.py
@@ -74,13 +74,23 @@ def get_manifest_lines_batch(manifest_filepath, start, end):
     with open(manifest_filepath, "r") as f:
         for line_i, line in enumerate(f):
             if line_i == start and line_i == end:
-                manifest_lines_batch.append(json.loads(line))
+                data = json.loads(line)
+                if "text" in data:
+                    # remove any duplicated spaces and convert any
+                    # newline chars to spaces
+                    data["text"] = " ".join(data["text"].split())
+                manifest_lines_batch.append(data)
                 break
 
             if line_i == end:
                 break
             if line_i >= start:
-                manifest_lines_batch.append(json.loads(line))
+                data = json.loads(line)
+                if "text" in data:
+                    # remove any duplicated spaces and convert any
+                    # newline chars to spaces
+                    data["text"] = " ".join(data["text"].split())
+                manifest_lines_batch.append(data)
     return manifest_lines_batch
 
 
