10eced9b039716acf09ddccbfab7c4441b979077
wdykas <wdykas@nvidia.com>
2023-03-29 07:41:28 -0700

    Moving checkpoint consolidation to cpu

--- examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
+++ examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
@@ -15,21 +15,22 @@
 r"""
 Conversion script to convert PTL checkpoints into nemo checkpoint.
   Example to run this conversion script:
-    python -m torch.distributed.launch --nproc_per_node=<tensor_model_parallel_size> * <pipeline_model_parallel_size> \
-     megatron_ckpt_to_nemo.py \
+     python3 megatron_ckpt_to_nemo.py \
      --checkpoint_folder <path_to_PTL_checkpoints_folder> \
      --checkpoint_name <checkpoint_name> \
      --nemo_file_path <path_to_output_nemo_file> \
-     --tensor_model_parallel_size <tensor_model_parallel_size> \
-     --pipeline_model_parallel_size <pipeline_model_parallel_size>
+     --tensor_model_parallel_size <tensor_model_parallel_size>\
+     --pipeline_model_parallel_size <pipeline_model_parallel_size> \
+     --model_type <model type>
 """
 
 import os
+import tempfile
+import itertools
+import shutil
 from argparse import ArgumentParser
 
 import torch
-from apex.transformer import parallel_state
-from pytorch_lightning.plugins.environments import TorchElasticEnvironment
 from pytorch_lightning.trainer.trainer import Trainer
 
 from nemo.collections.nlp.models.language_modeling.megatron_bart_model import MegatronBARTModel
@@ -38,11 +39,15 @@ from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import Meg
 from nemo.collections.nlp.models.language_modeling.megatron_retrieval_model import MegatronRetrievalModel
 from nemo.collections.nlp.models.language_modeling.megatron_t5_model import MegatronT5Model
 from nemo.collections.nlp.models.machine_translation.megatron_nmt_model import MegatronNMTModel
-from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector
-from nemo.utils import AppState, logging
-from nemo.utils.distributed import initialize_distributed
+from nemo.utils import AppState
 from nemo.utils.model_utils import inject_model_parallel_rank
 
+#Extra Imports
+from nemo.collections.nlp.parts.nlp_overrides import (
+    NEMO_MEGATRON_MODEL_PARALLEL_APPSTATE_OVERRIDE,
+    NLPDDPStrategy,
+    NLPSaveRestoreConnector
+)
 
 def get_args():
     parser = ArgumentParser()
@@ -69,7 +74,6 @@ def get_args():
         help="Path config for restoring. It's created during training and may need to be modified during restore if restore environment is different than training. Ex: /raid/nemo_experiments/megatron_gpt/hparams.yaml",
     )
     parser.add_argument("--nemo_file_path", type=str, default=None, required=True, help="Path to output .nemo file.")
-    parser.add_argument("--gpus_per_node", type=int, required=True, default=None)
     parser.add_argument("--tensor_model_parallel_size", type=int, required=True, default=None)
     parser.add_argument("--pipeline_model_parallel_size", type=int, required=True, default=None)
     parser.add_argument(
@@ -82,91 +86,101 @@ def get_args():
     parser.add_argument(
         "--model_type", type=str, required=True, default="gpt", choices=["gpt", "t5", "bert", "nmt", "bart", "retro"]
     )
-    parser.add_argument("--local_rank", type=int, required=False, default=os.getenv('LOCAL_RANK', -1))
-    parser.add_argument("--bcp", action="store_true", help="Whether on BCP platform")
 
     args = parser.parse_args()
     return args
 
 
-def convert(local_rank, rank, world_size, args):
+def convert(args):
+    os.environ[NEMO_MEGATRON_MODEL_PARALLEL_APPSTATE_OVERRIDE] = "true"
 
     app_state = AppState()
     app_state.data_parallel_rank = 0
-    num_nodes = world_size // args.gpus_per_node
-    if args.bcp:
-        trainer = Trainer(
-            devices=args.gpus_per_node, num_nodes=num_nodes, accelerator='gpu', plugins=[TorchElasticEnvironment()]
-        )
-    else:
-        trainer = Trainer(devices=args.gpus_per_node, num_nodes=num_nodes, accelerator='gpu')
-
+    app_state.local_rank = 0
+    app_state.global_rank = 0
     app_state.pipeline_model_parallel_size = args.pipeline_model_parallel_size
     app_state.tensor_model_parallel_size = args.tensor_model_parallel_size
-    # Auto set split rank for T5, BART, NMT if split rank is None.
-    if args.pipeline_model_parallel_size > 1 and args.model_type in ['t5', 'bart', 'nmt']:
-        if args.pipeline_model_parallel_split_rank is not None:
-            app_state.pipeline_model_parallel_split_rank = args.pipeline_model_parallel_split_rank
-        else:
-            if args.pipeline_model_parallel_size % 2 != 0:
-                raise ValueError(
-                    f"Pipeline model parallel size {args.pipeline_model_parallel_size} must be even if split rank is not specified."
-                )
-            else:
-                # If split rank is not set, then we set it to be pipeline_model_parallel_size // 2 - this is because in most cases we have the same number of enc/dec layers.
-                app_state.pipeline_model_parallel_split_rank = args.pipeline_model_parallel_size // 2
-    else:
-        app_state.pipeline_model_parallel_split_rank = None
-
     app_state.model_parallel_size = app_state.tensor_model_parallel_size * app_state.pipeline_model_parallel_size
+    app_state.world_size = args.tensor_model_parallel_size * args.pipeline_model_parallel_size
+    app_state.tensor_model_parallel_rank = 0
+    app_state.pipeline_model_parallel_rank = 0
 
-    parallel_state.initialize_model_parallel(
-        tensor_model_parallel_size_=app_state.tensor_model_parallel_size,
-        pipeline_model_parallel_size_=app_state.pipeline_model_parallel_size,
-        pipeline_model_parallel_split_rank_=app_state.pipeline_model_parallel_split_rank,
-    )
 
-    app_state.pipeline_model_parallel_rank = parallel_state.get_pipeline_model_parallel_rank()
-    app_state.tensor_model_parallel_rank = parallel_state.get_tensor_model_parallel_rank()
+    save_connector = NLPSaveRestoreConnector()
+    trainer = Trainer(devices=1, strategy=NLPDDPStrategy(), accelerator="cpu")
 
-    # inject model parallel rank
     checkpoint_path = inject_model_parallel_rank(os.path.join(args.checkpoint_folder, args.checkpoint_name))
-
-    logging.info(
-        f'rank: {rank}, local_rank: {local_rank}, is loading checkpoint: {checkpoint_path} for tp_rank: {app_state.tensor_model_parallel_rank} and pp_rank: {app_state.pipeline_model_parallel_rank}'
-    )
-
     if args.model_type == 'gpt':
-        model = MegatronGPTModel.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer)
+        model = MegatronGPTModel.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer).to("cpu")
     elif args.model_type == 'bert':
         model = MegatronBertModel.load_from_checkpoint(
             checkpoint_path, hparams_file=args.hparams_file, trainer=trainer
-        )
+        ).to("cpu")
     elif args.model_type == 't5':
-        model = MegatronT5Model.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer)
+        model = MegatronT5Model.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer).to("cpu")
     elif args.model_type == 'bart':
         model = MegatronBARTModel.load_from_checkpoint(
             checkpoint_path, hparams_file=args.hparams_file, trainer=trainer
-        )
+        ).to("cpu")
     elif args.model_type == 'nmt':
-        model = MegatronNMTModel.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer)
+        model = MegatronNMTModel.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer).to("cpu")
     elif args.model_type == 'retro':
         model = MegatronRetrievalModel.load_from_checkpoint(
             checkpoint_path, hparams_file=args.hparams_file, trainer=trainer
-        )
-    model._save_restore_connector = NLPSaveRestoreConnector()
+        ).to("cpu")
+    
 
-    if torch.distributed.is_initialized():
-        torch.distributed.barrier()
+    checkpoint_dir = args.checkpoint_folder
+    checkpoint_name = args.checkpoint_name
+    save_path = os.path.dirname(args.nemo_file_path)
 
-    model.save_to(args.nemo_file_path)
+    with tempfile.TemporaryDirectory() as tmpdir:
 
-    logging.info(f'NeMo model saved to: {args.nemo_file_path}')
+        if app_state.pipeline_model_parallel_size == 1:
+            # move weights to the tmpdir
+            for tp_rank in range(app_state.tensor_model_parallel_size):
+                os.makedirs(os.path.join(tmpdir, f'mp_rank_{tp_rank:02d}'))
+                mp_model_weights = os.path.join(
+                    checkpoint_dir, f'mp_rank_{tp_rank:02d}', checkpoint_name
+                )
+                #TODO: Change to a move
+                shutil.copy(
+                    mp_model_weights,
+                    os.path.join(tmpdir, f'mp_rank_{tp_rank:02d}', save_connector.model_weights_ckpt),
+                )
+        else:
+            # move weights to the tmpdir
+            for tp_rank, pp_rank in itertools.product(
+                range(app_state.tensor_model_parallel_size),
+                range(app_state.pipeline_model_parallel_size),
+            ):
+                os.makedirs(os.path.join(tmpdir, f'tp_rank_{tp_rank:02d}_pp_rank_{pp_rank:03d}'))
+                mp_model_weights = os.path.join(
+                    checkpoint_dir, f'tp_rank_{tp_rank:02d}_pp_rank_{pp_rank:03d}',checkpoint_name
+                )
+                #TODO: Change to a move
+                shutil.copy(
+                    mp_model_weights,
+                    os.path.join(
+                        tmpdir, f'tp_rank_{tp_rank:02d}_pp_rank_{pp_rank:03d}', save_connector.model_weights_ckpt
+                    ),
+                )
+
+        # create config and artifacts in tmpdir
+        config_yaml = os.path.join(tmpdir, save_connector.model_config_yaml)
+        model.to_config_file(path2yaml_file=config_yaml)
+        if hasattr(model, 'artifacts') and model.artifacts is not None:
+            save_connector._handle_artifacts(model, nemo_file_folder=tmpdir)
+            save_connector._update_artifact_paths(model, path2yaml_file=config_yaml)
+
+        # create tar file
+        save_connector._make_nemo_file_from_folder(filename=save_path,source_dir=tmpdir)
+        
+        os.environ.pop(NEMO_MEGATRON_MODEL_PARALLEL_APPSTATE_OVERRIDE, None)
+
+
 
 
 if __name__ == '__main__':
     args = get_args()
-
-    local_rank, rank, world_size = initialize_distributed(args)
-
-    convert(local_rank, rank, world_size, args)
+    convert(args)
