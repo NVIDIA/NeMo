d3e79992046057990bd74eed423ea4626fd9d6f9
Nikolay Karpov <karpnv@gmail.com>
2023-04-05 06:21:18 -0700

    install kenlm with more than 5gram
    Signed-off-by: Nikolay Karpov <karpnv@gmail.com>

--- scripts/asr_language_modeling/ngram_lm/install_beamsearch_decoders.sh
+++ scripts/asr_language_modeling/ngram_lm/install_beamsearch_decoders.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# install Boost package
-sudo apt-get update
-sudo apt-get install swig build-essential libboost-all-dev cmake zlib1g-dev libbz2-dev liblzma-dev
+cd /workspace/nemo
+apt-get update && apt-get upgrade -y && apt-get install -y liblzma-dev && rm -rf /var/lib/apt/lists/*
+
 git clone https://github.com/NVIDIA/OpenSeq2Seq
 cd OpenSeq2Seq
 git checkout ctc-decoders
@@ -11,5 +11,24 @@ rm -rf OpenSeq2Seq
 cd decoders
 # patch setup code to support the recent distutils
 sed -i 's/, distutils/, distutils\nimport distutils.ccompiler/g' setup.py
+
+cp /workspace/nemo/scripts/installers/setup_os2s_decoders.py ./setup.py
 ./setup.sh
+
+# install Boost package for KenLM
+wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2 --no-check-certificate && tar --bzip2 -xf /workspace/nemo/decoders/boost_1_80_0.tar.bz2 && cd boost_1_80_0 && ./bootstrap.sh && ./b2 --layout=tagged link=static,shared threading=multi,single install -j4 || echo FAILURE
+export BOOST_ROOT=/workspace/nemo/decoders/boost_1_80_0
+
+# install KenLM
+cd /workspace/nemo/decoders/kenlm/build && cmake -DKENLM_MAX_ORDER=10 .. && make -j2
+cd /workspace/nemo/decoders/kenlm
+python setup.py install --max_order=10
+export KENLM_LIB=/workspace/nemo/decoders/kenlm/build/bin
+export KENLM_ROOT=/workspace/nemo/decoders/kenlm
+cd ..
+
+# install Flashlight
+git clone https://github.com/flashlight/text && cd text
+python setup.py bdist_wheel
+pip install dist/*.whl
 cd ..
