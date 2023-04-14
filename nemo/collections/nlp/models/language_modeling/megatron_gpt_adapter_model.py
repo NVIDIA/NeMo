009928f75c04fc6ad1446d5ddd4da3c60ff7ebdb
arendu <adithya.r@gmail.com>
2023-04-13 22:23:57 -0700

    clean up
    Signed-off-by: arendu <adithya.r@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_adapter_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_adapter_model.py
@@ -19,15 +19,19 @@
 
 import os
 from typing import List
+
 import torch
 from omegaconf.dictconfig import DictConfig
 from pytorch_lightning.trainer.trainer import Trainer
 
 from nemo.collections.common.parts.adapter_modules import LinearAdapterConfig
+from nemo.collections.nlp.models.language_modeling.megatron_base_model import MegatronBaseModel
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_prompt_learning_model import (
     MegatronGPTPromptLearningModel,
 )
+from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTSFTModel
+from nemo.collections.nlp.models.nlp_model import NLPModel
 from nemo.collections.nlp.modules.common import VirtualPromptStyle
 from nemo.collections.nlp.modules.common.megatron.adapters.parallel_adapters import (
     AdapterName,
@@ -39,102 +43,51 @@ from nemo.collections.nlp.modules.common.megatron.adapters.parallel_adapters imp
 from nemo.collections.nlp.modules.common.megatron.utils import average_losses_across_data_parallel_group
 from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector
 from nemo.collections.nlp.parts.utils_funcs import get_last_rank
+from nemo.core.classes import ModelPT
 from nemo.core.classes.mixins import adapter_mixins
 from nemo.utils import AppState, logging, model_utils
-from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTSFTModel
-from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
-from nemo.collections.nlp.models.language_modeling.megatron_base_model import MegatronBaseModel
-from nemo.collections.nlp.models.nlp_model import NLPModel
-from nemo.core.classes import ModelPT
 
-
-class AdapterWrapper(ModelPT):
-    def __init__(self, cfg, dummy_params: torch.nn.Linear):
-        super().__init__(cfg, trainer=None)
-        self.cfg = cfg
-        self.dummy_params = dummy_params
-
-    @classmethod
-    def list_available_models(cls):
-        pass
-
-    @classmethod
-    def setup_training_data(cls):
-        pass
-
-    @classmethod
-    def setup_validation_data(cls):
-        pass
-
-    def state_dict(self, base_model, destination=None, prefix=None, keep_vars=False):
-        """
-        Creates a state_dict using only the adapter parameters.
-        This ensures that this wrapper class will only checkpoint the adapter
-        weights and not the rest of the base GPT Model.
-        """
-        state_dict_ = {}
-        for name, module in base_model.named_modules():
-            if isinstance(module, adapter_mixins.AdapterModuleMixin) and module.is_adapter_available():
-                for adapter_key in self.adapter_name_keys:
-                    adapter_module = module.get_adapter_module(adapter_key)
-                    if adapter_module:
-                        state_adapter_key = ':'.join([name, adapter_key])
-                        state_dict_[state_adapter_key] = adapter_module.state_dict()
-
-                module.set_enabled_adapters(enabled=True)
-        return state_dict_
-
-    def load_state_dict(self, base_model, state_dict, strict: bool = True):
-        """
-        Loads a state_dict expecting the state_dict to contain key,values 
-        only for the adapter parameters.
-        """
-        for name, module in base_model.named_modules():
-            if isinstance(module, adapter_mixins.AdapterModuleMixin) and module.is_adapter_available():
-                for adapter_key in self.adapter_name_keys:
-                    adapter_module = module.get_adapter_module(adapter_key)
-                    if adapter_module:
-                        state_adapter_key = ':'.join([name, adapter_key])
-                        adapter_module.load_state_dict(state_dict[state_adapter_key], strict)
-                module.set_enabled_adapters(enabled=True)
 
 class MegatronGPTBaseAdapterModel(MegatronGPTSFTModel):
     def __init__(self, cfg: DictConfig, trainer: Trainer):
         super().__init__(cfg, trainer)
         self.setup_complete = False
-        self.adapter_name_keys = [AdapterName.PRE_ATTN_ADAPTER, AdapterName.POST_ATTN_ADAPTER]
+        self.adapter_name_keys = [
+            AdapterName.PRE_ATTN_ADAPTER,
+            AdapterName.POST_ATTN_ADAPTER,
+        ]
         adapter_tuning_cfg = cfg.adapter_tuning
 
         if adapter_tuning_cfg.type == "parallel_adapter":
             adapter_cfg = ParallelLinearAdapterConfig(
                 in_features=self.cfg.hidden_size,
                 dim=adapter_tuning_cfg.adapter_dim,
-                norm_position=adapter_tuning_cfg.get('norm_position', 'pre'),
-                norm_type=adapter_tuning_cfg.get('norm_type', 'mixedfusedlayernorm'),
-                column_init_method=adapter_tuning_cfg.get('column_init_method', 'xavier'),
-                row_init_method=adapter_tuning_cfg.get('row_init_method', 'zero'),
+                norm_position=adapter_tuning_cfg.get("norm_position", "pre"),
+                norm_type=adapter_tuning_cfg.get("norm_type", "mixedfusedlayernorm"),
+                column_init_method=adapter_tuning_cfg.get("column_init_method", "xavier"),
+                row_init_method=adapter_tuning_cfg.get("row_init_method", "zero"),
                 dropout=adapter_tuning_cfg.adapter_dropout,
             )
         else:
             adapter_cfg = LinearAdapterConfig(
                 in_features=self.cfg.hidden_size,
                 dim=adapter_tuning_cfg.adapter_dim,
-                norm_position=adapter_tuning_cfg.get('norm_position', 'pre'),
+                norm_position=adapter_tuning_cfg.get("norm_position", "pre"),
                 dropout=adapter_tuning_cfg.adapter_dropout,
             )
         self.adapter_cfg = adapter_cfg
         self.base_keys = self.get_all_keys()
         self.init_adapters()
         self.adapter_keys = self.get_all_keys() - self.base_keys
-    
+
     def setup(self, stage=None):
         super().setup(stage)
         self.setup_complete = True
-    
+
     def get_all_keys(self,):
         k = [n for n, p in self.named_parameters()]
         return set(k)
-    
+
     def get_adapter_state_dict(self,):
         """ 
         Gets the keys associated with the adapters only.
@@ -143,24 +96,27 @@ class MegatronGPTBaseAdapterModel(MegatronGPTSFTModel):
         adapter_state_dict = {}
         for k in self.adapter_keys:
             adapter_state_dict[k] = state_dict[k]
-        return adapter_state_dict 
+        return adapter_state_dict
 
     def init_adapters(self):
         """ 
         Randomly initialize the adapter params and add them to the appropriate modules.
         """
-        logging.info(f'Before adding adapters:\n{self.summarize()}')
+        logging.info(f"Before adding adapters:\n{self.summarize()}")
         for _, module in self.named_modules():
             if isinstance(module, adapter_mixins.AdapterModuleMixin):
                 for adapter_key in self.adapter_name_keys:
-                    if model_utils.import_class_by_path(self.adapter_cfg._target_) in module.get_accepted_adapter_types():
+                    if (
+                        model_utils.import_class_by_path(self.adapter_cfg._target_)
+                        in module.get_accepted_adapter_types()
+                    ):
                         module.add_adapter(
                             name=adapter_key, cfg=self.adapter_cfg,
                         )
-        logging.info(f'After adding adapters:\n{self.summarize()}')
+        logging.info(f"After adding adapters:\n{self.summarize()}")
         return True
-    
-    def state_dict(self, destination=None, prefix=None, keep_vars=False): 
+
+    def state_dict(self, destination=None, prefix=None, keep_vars=False):
         if self.setup_complete:
             # Once setup is complete we no longer need to track the frozen part of the model. Only there adapter state dict keeps changing so state_dict only track these.
             return self.get_adapter_state_dict()
@@ -169,7 +125,7 @@ class MegatronGPTBaseAdapterModel(MegatronGPTSFTModel):
             # but we can't call self.state_dict() here as it would be a recursive call.
             # so we call self.model.state_dict(prefix="model.") which will return all the keys and params same as calling self.state_dict()
             return self.model.state_dict(prefix="model.")
-    
+
     def load_state_dict(self, state_dict, strict: bool = True):
         if self.setup_complete:
             # at this stage only adapter params will appear in the state_dict arg, so we only update those while the rest of the model is frozen.
@@ -178,7 +134,7 @@ class MegatronGPTBaseAdapterModel(MegatronGPTSFTModel):
             super().load_state_dict(state_dict, strict=False)
         else:
             super().load_state_dict(state_dict, strict=True)
-                
+
     def setup_optimizer_param_groups(self):
         """
         ModelPT override. Optimizer will get self._optimizer_param_groups. 
@@ -197,19 +153,22 @@ class MegatronGPTBaseAdapterModel(MegatronGPTSFTModel):
                 module.unfreeze_enabled_adapters()  # selectively unfreeze the adapter modules.
                 opt_params += [p for p in module.parameters()]
 
-        self._optimizer_param_groups = ({'params': opt_params},)
-        logging.info(f'Optimizer groups set:\n{self.summarize()}')
-        
-        
+        self._optimizer_param_groups = ({"params": opt_params},)
+        logging.info(f"Optimizer groups set:\n{self.summarize()}")
+
+
 class MegatronNLPAdapter(ModelPT):
     """ 
     Wrapper class that handles loading and saving of adapters via load_state_dict and save_dict. Also handles initialization of restore_from for adapters.
     """
-    def __init__(self, cfg: DictConfig, trainer: Trainer, adapter_name_keys: List[str], base_model: MegatronGPTModel):
+
+    def __init__(
+        self, cfg: DictConfig, trainer: Trainer, adapter_name_keys: List[str], base_model: MegatronGPTModel,
+    ):
         super().__init__(cfg, trainer)
         self.base_model = base_model
         self.adapter_name_keys = adapter_name_keys
-    
+
     @classmethod
     def list_available_models(cls):
         pass
@@ -240,7 +199,7 @@ class MegatronNLPAdapter(ModelPT):
                 for adapter_key in self.adapter_name_keys:
                     adapter_module = module.get_adapter_module(adapter_key)
                     if adapter_module:
-                        state_adapter_key = ':'.join([name, adapter_key])
+                        state_adapter_key = ":".join([name, adapter_key])
                         state_dict_[state_adapter_key] = adapter_module.state_dict()
 
                 module.set_enabled_adapters(enabled=True)
@@ -256,9 +215,10 @@ class MegatronNLPAdapter(ModelPT):
                 for adapter_key in self.adapter_name_keys:
                     adapter_module = module.get_adapter_module(adapter_key)
                     if adapter_module:
-                        state_adapter_key = ':'.join([name, adapter_key])
+                        state_adapter_key = ":".join([name, adapter_key])
                         adapter_module.load_state_dict(state_dict[state_adapter_key], strict)
                 module.set_enabled_adapters(enabled=True)
+
 
 class ___MegatronGPTAdapterLearningModel(MegatronNLPAdapter):
     """
@@ -272,40 +232,42 @@ class ___MegatronGPTAdapterLearningModel(MegatronNLPAdapter):
     and infused into the same GPT Model for inference. 
     """
 
-    def __init__(self, adapter_tuning_cfg: DictConfig, trainer: Trainer, base_model: MegatronGPTModel):
-        adapter_name_keys = [AdapterName.PRE_ATTN_ADAPTER, AdapterName.POST_ATTN_ADAPTER]
+    def __init__(
+        self, adapter_tuning_cfg: DictConfig, trainer: Trainer, base_model: MegatronGPTModel,
+    ):
+        adapter_name_keys = [
+            AdapterName.PRE_ATTN_ADAPTER,
+            AdapterName.POST_ATTN_ADAPTER,
+        ]
         super().__init__(adapter_tuning_cfg, trainer, adapter_name_keys, base_model)
-        assert adapter_tuning_cfg.get('adapter_dim', 0) > 0, "adapter_dim has not been set."
+        assert adapter_tuning_cfg.get("adapter_dim", 0) > 0, "adapter_dim has not been set."
         assert (
             adapter_tuning_cfg.adapter_dim % self.base_model.cfg.tensor_model_parallel_size == 0
         ), "The adapter dim should be divisible by tensor_model_parallel_size."
         assert adapter_tuning_cfg.type in [
-            'linear_adapter',
-            'parallel_adapter',
+            "linear_adapter",
+            "parallel_adapter",
         ], "Adapter type should be 'linear_adapter' or 'parallel_adapter'"
-
 
         if adapter_tuning_cfg.type == "parallel_adapter":
             adapter_cfg = ParallelLinearAdapterConfig(
                 in_features=self.base_model.cfg.hidden_size,
                 dim=adapter_tuning_cfg.adapter_dim,
-                norm_position=adapter_tuning_cfg.get('norm_position', 'pre'),
-                norm_type=adapter_tuning_cfg.get('norm_type', 'mixedfusedlayernorm'),
-                column_init_method=adapter_tuning_cfg.get('column_init_method', 'xavier'),
-                row_init_method=adapter_tuning_cfg.get('row_init_method', 'zero'),
+                norm_position=adapter_tuning_cfg.get("norm_position", "pre"),
+                norm_type=adapter_tuning_cfg.get("norm_type", "mixedfusedlayernorm"),
+                column_init_method=adapter_tuning_cfg.get("column_init_method", "xavier"),
+                row_init_method=adapter_tuning_cfg.get("row_init_method", "zero"),
                 dropout=adapter_tuning_cfg.adapter_dropout,
             )
         else:
             adapter_cfg = LinearAdapterConfig(
                 in_features=self.base_model.cfg.hidden_size,
                 dim=adapter_tuning_cfg.adapter_dim,
-                norm_position=adapter_tuning_cfg.get('norm_position', 'pre'),
+                norm_position=adapter_tuning_cfg.get("norm_position", "pre"),
                 dropout=adapter_tuning_cfg.adapter_dropout,
             )
         self.adapter_cfg = adapter_cfg
         self.init_adapters()
-
-    
 
 
 class MegatronGPTInfusedAdapterModel(MegatronGPTBaseAdapterModel):
@@ -322,29 +284,32 @@ class MegatronGPTInfusedAdapterModel(MegatronGPTBaseAdapterModel):
 
     def __init__(self, cfg: DictConfig, trainer: Trainer):
         super().__init__(cfg, trainer)
-        self.adapter_name_keys = [AdapterName.KEY_INFUSED, AdapterName.VALUE_INFUSED, AdapterName.MLP_INFUSED]
+        self.adapter_name_keys = [
+            AdapterName.KEY_INFUSED,
+            AdapterName.VALUE_INFUSED,
+            AdapterName.MLP_INFUSED,
+        ]
 
-        logging.info(f'Before adding adapters:\n{self.model.summarize()}')
+        logging.info(f"Before adding adapters:\n{self.model.summarize()}")
 
         for _, module in self.model.named_modules():
             if isinstance(module, adapter_mixins.AdapterModuleMixin):
                 for adapter_key in self.adapter_name_keys:
                     if adapter_key == AdapterName.MLP_INFUSED:
                         cfg = MLPInfusedAdapterConfig(
-                            in_features=cfg.ffn_hidden_size
-                            // cfg.tensor_model_parallel_size
+                            in_features=cfg.ffn_hidden_size // cfg.tensor_model_parallel_size
                         )
-                    elif adapter_key in [AdapterName.KEY_INFUSED, AdapterName.VALUE_INFUSED]:
-                        cfg = InfusedAdapterConfig(
-                            in_features=cfg.hidden_size
-                            // cfg.tensor_model_parallel_size
-                        )
+                    elif adapter_key in [
+                        AdapterName.KEY_INFUSED,
+                        AdapterName.VALUE_INFUSED,
+                    ]:
+                        cfg = InfusedAdapterConfig(in_features=cfg.hidden_size // cfg.tensor_model_parallel_size)
                     else:
                         raise ValueError(f"Adapter Key {adapter_key} is unknown.")
                     if model_utils.import_class_by_path(cfg._target_) in module.get_accepted_adapter_types():
                         module.add_adapter(name=adapter_key, cfg=cfg)
 
-        logging.info(f'After adding adapters:\n{self.model.summarize()}')
+        logging.info(f"After adding adapters:\n{self.model.summarize()}")
 
     @classmethod
     def list_available_models(cls):
@@ -360,7 +325,7 @@ class MegatronPTuningAdapterLearningModel(MegatronGPTBaseAdapterModel):
         super().__init__(cfg, trainer)
         self.adapter_name_keys = [AdapterName.PTUNING_ADAPTER]
 
-        logging.info(f'Before adding adapters:\n{self.model.summarize()}')
+        logging.info(f"Before adding adapters:\n{self.model.summarize()}")
 
         self.adapter_cfg = PromptEncoderAdapterConfig(
             cfg.prompt_encoder_adapter.virtual_tokens,
@@ -374,13 +339,15 @@ class MegatronPTuningAdapterLearningModel(MegatronGPTBaseAdapterModel):
         for module_name, module in self.model.named_modules():
             if isinstance(module, adapter_mixins.AdapterModuleMixin):
                 for adapter_key in self.adapter_name_keys:
-                    if model_utils.import_class_by_path(self.adapter_cfg._target_) in module.get_accepted_adapter_types():
+                    if (
+                        model_utils.import_class_by_path(self.adapter_cfg._target_)
+                        in module.get_accepted_adapter_types()
+                    ):
                         module.add_adapter(
                             name=adapter_key, cfg=self.adapter_cfg,
                         )
 
-        logging.info(f'After adding adapters:\n{self.summarize()}')
-
+        logging.info(f"After adding adapters:\n{self.summarize()}")
 
     def state_dict(self, destination=None, prefix=None, keep_vars=False):
         state_dict_ = {}
@@ -398,8 +365,8 @@ class MegatronPTuningAdapterLearningModel(MegatronGPTBaseAdapterModel):
             super().setup_optimizer_param_groups()
         else:
             self.model.freeze()  # Freeze the entire model
-            self._optimizer_param_groups = ({'params': []},)
-        logging.info(f'Optimizer groups set:\n{self.model.summarize()}')
+            self._optimizer_param_groups = ({"params": []},)
+        logging.info(f"Optimizer groups set:\n{self.model.summarize()}")
 
     @classmethod
     def list_available_models(cls):
