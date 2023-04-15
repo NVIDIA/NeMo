55317c7a402da219b4e0a0dfe4a82a6ce34b74cd
hsiehjackson <c2hsieh@ucsd.edu>
2023-04-14 18:02:57 -0700

    Change copyright and random weight test
    Signed-off-by: hsiehjackson <c2hsieh@ucsd.edu>

--- tests/collections/tts/modules/test_submodules.py
+++ tests/collections/tts/modules/test_submodules.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,16 +20,29 @@ from nemo.collections.tts.modules import submodules
 
 @pytest.mark.unit
 def test_conditional_layer_norm():
+    
     # NLP Example
     batch, sentence_length, embedding_dim = 20, 5, 10
     embedding = torch.randn(batch, sentence_length, embedding_dim)
-    lm = torch.nn.LayerNorm(embedding_dim)
-    clm = submodules.ConditionalLayerNorm(embedding_dim)
-    assert torch.all(lm(embedding) == clm(embedding))
+    ln = torch.nn.LayerNorm(embedding_dim)
+    cln = submodules.ConditionalLayerNorm(embedding_dim)
+    assert torch.all(ln(embedding) == cln(embedding))
+    
+    weight = torch.nn.Parameter(torch.randn(embedding_dim))
+    bias = torch.nn.Parameter(torch.randn(embedding_dim))
+    ln.weight, ln.bias = weight, bias
+    cln.weight, cln.bias  = weight, bias
+    assert torch.all(ln(embedding) == cln(embedding)) # Simulate trained weights
 
     # Image Example
     N, C, H, W = 20, 5, 10, 10
     image = torch.randn(N, C, H, W)
-    lm = torch.nn.LayerNorm([C, H, W])
-    clm = submodules.ConditionalLayerNorm([C, H, W])
-    assert torch.all(lm(image) == clm(image))
+    ln = torch.nn.LayerNorm([C, H, W])
+    cln = submodules.ConditionalLayerNorm([C, H, W])
+    assert torch.all(ln(image) == cln(image))
+    
+    weight = torch.nn.Parameter(torch.randn(C, H, W))
+    bias = torch.nn.Parameter(torch.randn(C, H, W))
+    ln.weight, ln.bias = weight, bias
+    cln.weight, cln.bias  = weight, bias
+    assert torch.all(ln(image) == cln(image)) # Simulate trained weights
