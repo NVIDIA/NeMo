7c321ce5955002cd5f713387dc802949895f259e
hsiehjackson <c2hsieh@ucsd.edu>
2023-04-17 10:28:59 -0700

    Rebase and Fix error
    Signed-off-by: hsiehjackson <c2hsieh@ucsd.edu>

--- nemo/collections/tts/modules/fastpitch.py
+++ nemo/collections/tts/modules/fastpitch.py
@@ -148,34 +148,6 @@ class TemporalPredictor(NeuralModule):
         return out.squeeze(-1)
 
 
-class TemporalPredictorAdapter(TemporalPredictor, adapter_mixins.AdapterModuleMixin):
-    """ Inherit from TemporalPredictor and add support for adapter"""
-
-    def add_adapter(self, name: str, cfg: dict):
-        cfg = self._update_adapter_cfg_input_dim(cfg)
-        for conv_layer in self.layers:  # type: adapter_mixins.AdapterModuleMixin
-            conv_layer.add_adapter(name, cfg)
-
-    def is_adapter_available(self) -> bool:
-        return any([conv_layer.is_adapter_available() for conv_layer in self.layers])
-
-    def set_enabled_adapters(self, name: Optional[str] = None, enabled: bool = True):
-        for conv_layer in self.layers:  # type: adapter_mixins.AdapterModuleMixin
-            conv_layer.set_enabled_adapters(name=name, enabled=enabled)
-
-    def get_enabled_adapters(self) -> List[str]:
-        names = set([])
-        for conv_layer in self.layers:  # type: adapter_mixins.AdapterModuleMixin
-            names.update(conv_layer.get_enabled_adapters())
-
-        names = sorted(list(names))
-        return names
-
-    def _update_adapter_cfg_input_dim(self, cfg: DictConfig):
-        cfg = adapter_utils.update_adapter_cfg_input_dim(self, cfg, module_dim=self.filter_size)
-        return cfg
-
-
 class FastPitchModule(NeuralModule, adapter_mixins.AdapterModuleMixin):
     def __init__(
         self,
