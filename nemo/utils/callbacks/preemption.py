91c1566c8af7c56c518f1df7acae01605811de11
Abhishree <abhishreetm@gmail.com>
2023-04-25 18:29:01 +0000

    Add print statement
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- nemo/utils/callbacks/preemption.py
+++ nemo/utils/callbacks/preemption.py
@@ -20,7 +20,7 @@ from pytorch_lightning.callbacks import Callback
 
 from nemo.utils import logging
 
-
+print("--hello---")
 class PreemptionCallback(Callback):
     """
     PreemptionCallback class creates a callback that checks for preemption during training at the end of every step.
