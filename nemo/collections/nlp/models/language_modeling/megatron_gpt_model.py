62cd46a4a1f5e1c20839c890d09300030d6582cc
Abhinav Khattar <aklife97@gmail.com>
2023-05-04 12:22:28 -0700

    scale changes for main
    Signed-off-by: Abhinav Khattar <aklife97@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -346,7 +346,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             forward_only=forward_only,
             tensor_shape=tensor_shape,
             dtype=self.autocast_dtype,
-            grad_scaler=self.trainer.precision_plugin.scaler if self.cfg.precision == 16 else None,
+            grad_scaler=self.trainer.precision_plugin.scaler.scale if self.cfg.precision == 16 else None,
             sequence_parallel=self.cfg.get('sequence_parallel', False),
             enable_autocast=True,
         )
