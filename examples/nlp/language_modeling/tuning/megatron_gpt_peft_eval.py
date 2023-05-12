5562d1f9be92e26915bca944212073e68caddccf
arendu <adithya.r@gmail.com>
2023-05-12 12:27:25 -0700

    eval with peft or sft model
    Signed-off-by: arendu <adithya.r@gmail.com>

--- examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
@@ -23,7 +23,6 @@ from torch.utils.data import DataLoader
 
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_peft_models import MegatronGPTPEFTModel
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTSFTModel
-from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
 from nemo.collections.nlp.models.nlp_model import NLPModel
 from nemo.collections.nlp.parts.nlp_overrides import (
     GradScaler,
