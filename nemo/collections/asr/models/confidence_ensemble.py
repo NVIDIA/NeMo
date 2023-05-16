c648d99987a53325ba2ac48522aa6cf783f27f83
Igor Gitman <igitman@nvidia.com>
2023-05-16 09:50:01 -0700

    Confidence ensembles: fix issues and add tuning functionality (#6657)
    * Implement compute confidence to properly handle blanks
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    
    * Implement proper confidence for transducers
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    
    * Implement tuning logic
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    
    * Add tests for confidence tuning
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    
    * Remove unused imports
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    
    * Add types/docs
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    
    * Add comment about the main conf compute loop
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    
    ---------
    
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    Signed-off-by: hsiehjackson <c2hsieh@ucsd.edu>

--- nemo/collections/asr/models/confidence_ensemble.py
+++ nemo/collections/asr/models/confidence_ensemble.py
@@ -12,25 +12,139 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from dataclasses import dataclass
 from typing import Dict, List, Optional, Union
 
 import joblib
 import numpy as np
 import torch
-from omegaconf import DictConfig, OmegaConf, open_dict
+from omegaconf import DictConfig, open_dict
 from pytorch_lightning import Trainer
 
 from nemo.collections.asr.models.asr_model import ASRModel
 from nemo.collections.asr.models.hybrid_rnnt_ctc_models import EncDecHybridRNNTCTCModel
-from nemo.collections.asr.parts.utils.asr_confidence_utils import ConfidenceConfig, get_confidence_aggregation_bank
+from nemo.collections.asr.parts.utils.asr_confidence_utils import (
+    ConfidenceConfig,
+    ConfidenceMethodConfig,
+    get_confidence_aggregation_bank,
+    get_confidence_measure_bank,
+)
 from nemo.collections.asr.parts.utils.audio_utils import ChannelSelectorType
+from nemo.collections.asr.parts.utils.rnnt_utils import Hypothesis
 from nemo.core.classes import ModelPT
 from nemo.utils import model_utils
 
-__all__ = ['ConfidenceEnsembleModel']
+
+# frozen is required to allow hashing of this class and use it
+# as a dictionary key when running confidence tuning
+@dataclass(frozen=True)
+class ConfidenceSpec:
+    exclude_blank: bool
+    aggregation: str
+    confidence_type: str
+    alpha: float
+
+    def to_confidence_config(self) -> ConfidenceConfig:
+        """Converts confidence spec to the confidence config.
+
+        Internally, the tuning procedure uses this "spec" objects as they
+        are more aligned with how things are implemented. But when it's time
+        to save the models or call transcribe, we need to use the proper
+        object of type ``ConfidenceConfig``.
+        """
+        if self.confidence_type == 'max_prob':
+            name = 'max_prob'
+            entropy_type = 'tsallis'  # can be any
+            entropy_norm = 'lin'  # can be any
+        else:
+            name, entropy_type, entropy_norm = self.confidence_type.split("_")
+        return ConfidenceConfig(
+            exclude_blank=self.exclude_blank,
+            aggregation=self.aggregation,
+            method_cfg=ConfidenceMethodConfig(
+                name=name, entropy_type=entropy_type, temperature=self.alpha, entropy_norm=entropy_norm,
+            ),
+        )
+
+
+def get_filtered_logprobs(hypothesis: Hypothesis, exclude_blank: bool) -> torch.Tensor:
+    """Returns logprobs from the hypothesis object with optional blanks filter.
+
+    This function supports both CTC and Transducer hypotheses. Will place the
+    logprobs on GPU if it's available.
+
+    Args:
+        hypothesis: generated hypothesis as returned from the transcribe
+            method of the ASR model.
+        exclude_blank: whether to filter out all ``<blank>`` tokens.
+
+    Returns:
+        torch.Tensor: of shape [S, V], where S is (filtered) sequence length and
+        V is the vocabulary size.
+    """
+    if isinstance(hypothesis.alignments, list):  # Transducer
+        filtered_logprobs = []
+        for alignment in hypothesis.alignments:
+            for align_elem in alignment:
+                if exclude_blank and align_elem[1].item() != align_elem[0].shape[-1] - 1:
+                    filtered_logprobs.append(align_elem[0])
+                filtered_logprobs.append(align_elem[0])
+        if not filtered_logprobs:  # for the edge-case of all blanks
+            filtered_logprobs.append(align_elem[0])
+        filtered_logprobs = torch.stack(filtered_logprobs)
+        if torch.cuda.is_available():  # by default logprobs are placed on cpu in nemo
+            filtered_logprobs = filtered_logprobs.cuda()
+    else:  # CTC
+        logprobs = hypothesis.y_sequence
+        if torch.cuda.is_available():  # by default logprobs are placed on cpu in nemo
+            logprobs = logprobs.cuda()
+        if exclude_blank:  # filtering blanks
+            labels = logprobs.argmax(dim=-1)
+            filtered_logprobs = logprobs[labels != logprobs.shape[1] - 1]
+        else:
+            filtered_logprobs = logprobs
+    return filtered_logprobs
+
+
+def compute_confidence(hypothesis: Hypothesis, confidence_cfg: ConfidenceConfig) -> float:
+    """Computes confidence score of the full utterance from a given hypothesis.
+
+    This is essentially a re-implementation of the built-in confidence
+    computation in NeMo. The difference is that we aggregate full-utterance
+    scores, while core functionality only supports word and token level
+    aggregations.
+
+    Args:
+        hypothesis: generated hypothesis as returned from the transcribe
+            method of the ASR model.
+        confidence_cfg: confidence config specifying what kind of
+            measure/aggregation should be used.
+
+    Returns:
+        float: confidence score.
+
+    """
+    filtered_logprobs = get_filtered_logprobs(hypothesis, confidence_cfg.exclude_blank)
+    vocab_size = filtered_logprobs.shape[1]
+    aggr_func = get_confidence_aggregation_bank()[confidence_cfg.aggregation]
+    if confidence_cfg.method_cfg.name == "max_prob":
+        conf_type = "max_prob"
+        alpha = 1.0
+    else:
+        conf_type = f"entropy_{confidence_cfg.method_cfg.entropy_type}_{confidence_cfg.method_cfg.entropy_norm}"
+        alpha = confidence_cfg.method_cfg.temperature
+    conf_func = get_confidence_measure_bank()[conf_type]
+
+    conf_value = aggr_func(conf_func(filtered_logprobs, v=vocab_size, t=alpha)).cpu().item()
+    return conf_value
 
 
 class ConfidenceEnsembleModel(ModelPT):
+    """Implementation of the confidence ensemble model.
+
+    See <PAPER TBD> for details.
+    """
+
     def __init__(
         self, cfg: DictConfig, trainer: 'Trainer' = None,
     ):
@@ -79,7 +193,7 @@ class ConfidenceEnsembleModel(ModelPT):
         self.model_selection_block = joblib.load(model_selection_block_path)
         self.confidence_cfg = ConfidenceConfig(**self.cfg.confidence)
 
-        # making sure each model has correct confidence settings in the decoder strategy
+        # making sure each model has correct temperature setting in the decoder strategy
         for model_idx in range(self.num_models):
             model = getattr(self, f"model{model_idx}")
             # for now we assume users are direclty responsible for matching
@@ -94,11 +208,15 @@ class ConfidenceEnsembleModel(ModelPT):
                 self.update_decoding_parameters(model.cfg.decoding)
                 model.change_decoding_strategy(model.cfg.decoding)
 
-    def update_decoding_parameters(self, decoding_cfg):
-        """Updating confidence/temperature parameters of the config."""
+    def update_decoding_parameters(self, decoding_cfg: DictConfig):
+        """Updating temperature/preserve_alignment/preserve_frame_confidence parameters of the config."""
         with open_dict(decoding_cfg):
-            decoding_cfg.confidence_cfg = self.confidence_cfg
             decoding_cfg.temperature = self.cfg.temperature
+            decoding_cfg.preserve_alignments = True
+            if 'confidence_cfg' in decoding_cfg:
+                decoding_cfg.confidence_cfg.preserve_frame_confidence = True
+            else:
+                decoding_cfg.confidence_cfg = ConfidenceConfig(preserve_frame_confidence=True)
 
     def setup_training_data(self, train_data_config: Union[DictConfig, Dict]):
         """Pass-through to the ensemble models.
@@ -122,13 +240,13 @@ class ConfidenceEnsembleModel(ModelPT):
                 self_attention_model, att_context_size, update_config
             )
 
-    def change_decoding_strategy(self, decoding_cfg: DictConfig = None, decoder_type: str = None):
+    def change_decoding_strategy(self, decoding_cfg: Optional[DictConfig] = None, decoder_type: str = None):
         """Pass-through to the ensemble models.
 
-        The only change here is that we always require frame-confidence to
-        be returned.
+        The only change here is that we always require expected temperature
+        to be set as well as ``decoding_cfg.preserve_alignments = True``
         """
-        decoding_cfg.confidence_cfg = self.confidence_cfg
+        self.update_decoding_parameters(decoding_cfg)
         for model_idx in range(self.num_models):
             model = getattr(self, f"model{model_idx}")
             if isinstance(model, EncDecHybridRNNTCTCModel):
@@ -157,8 +275,6 @@ class ConfidenceEnsembleModel(ModelPT):
             3. Use logistic regression to pick the "most confident" model
             4. Return the output of that model
         """
-        # TODO: lots of duplicate code with building ensemble script
-        aggr_func = get_confidence_aggregation_bank()[self.confidence_cfg.aggregation]
         confidences = []
         all_transcriptions = []
         # always requiring to return hypothesis
@@ -181,12 +297,7 @@ class ConfidenceEnsembleModel(ModelPT):
 
             model_confidences = []
             for transcription in transcriptions:
-                if isinstance(transcription.frame_confidence[0], list):
-                    # NeMo Transducer API returns list of lists for confidences
-                    conf_values = [conf_value for confs in transcription.frame_confidence for conf_value in confs]
-                else:
-                    conf_values = transcription.frame_confidence
-                model_confidences.append(aggr_func(conf_values))
+                model_confidences.append(compute_confidence(transcription, self.confidence_cfg))
             confidences.append(model_confidences)
             all_transcriptions.append(transcriptions)
 
