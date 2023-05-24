7847a5403d8e15b16ef5232c450d257532e07082
hsiehjackson <c2hsieh@ucsd.edu>
2023-05-24 15:45:02 -0700

    Uncomment correct bias size
    Signed-off-by: hsiehjackson <c2hsieh@ucsd.edu>

--- nemo/collections/nlp/modules/common/megatron/position_embedding/alibi_relative_position_embedding.py
+++ nemo/collections/nlp/modules/common/megatron/position_embedding/alibi_relative_position_embedding.py
@@ -118,7 +118,7 @@ class ALiBiRelativePositionEmbedding(torch.nn.Module):
         else:
             relative_position = self.relative_position
         # shape (num_attention_heads, query_seq_length, key_seq_length)
-        # relative_position = relative_position[:, :query_seq_length, :key_seq_length]
+        relative_position = relative_position[:, -query_seq_length:, -key_seq_length:]
         # if not bidirectional, mask out the future positions
         if not self.bidirectional:
             relative_position = torch.tril(relative_position)
