241f9fd44a698873a09f37fabc1fbaa7fc4cea87
Yang Zhang <yangzhang@nvidia.com>
2023-05-25 09:11:26 -0700

    made gpt inference with pp=2 work and custom length
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/modules/common/text_generation_utils.py
+++ nemo/collections/nlp/modules/common/text_generation_utils.py
@@ -638,6 +638,7 @@ def sample_sequence_batch(
     # initialize the batch
     with torch.no_grad():
         context_length = context_lengths.min().item()
+        print(context_length)
         inference_strategy.init_batch(context_tokens, context_length)
         # added eos_id to support the function generate_samples_eval that passes
         # eos_id as an argument and needs termination when that id id found.
@@ -662,7 +663,6 @@ def sample_sequence_batch(
                 tokens, maxlen, micro_batch_size, counter, context_length
             )
 
-            print(context_tokens.sha, "context_tokens")
             output = inference_strategy.forward_step(batch, tensor_shape)
 
             if parallel_state.is_pipeline_last_stage():
