d101922f5bbf35d476feed269ac32391d6406c2c
Dan Crankshaw <crankshaw@google.com>
2023-05-26 21:06:48 +0000

    Check for megatron-LM changes in docker build script

--- build_and_push_docker.sh
+++ build_and_push_docker.sh
@@ -5,8 +5,12 @@ set -euo pipefail
 IMAGE="us-central1-docker.pkg.dev/supercomputer-testing/crankshaw-nemo-stagetest/nemo"
 TAG=$(git rev-parse --short HEAD)
 
+cd ../Megatron-LM
+MEGATRON_VER=$(git rev-parse --short HEAD)
+cd -
+
 IMAGE_FULL=$IMAGE:$TAG
 
-DOCKER_BUILDKIT=1 docker build --build-arg FORCE_REBUILD="2" -f Dockerfile -t $IMAGE_FULL .
+DOCKER_BUILDKIT=1 docker build --build-arg FORCE_REBUILD=$MEGATRON_VER -f Dockerfile -t $IMAGE_FULL .
 
 docker push $IMAGE_FULL
