adjusted hot fix that prevents softmax on the entire output embedding,now memory bottlenecked by attention softmax which needs to be solved with FA or long attention
Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

nemo/collections/nlp/modules/common/text_generation_utils.py
@@ -406,7 +406,7 @@ def synced_generate(
     if parallel_state.is_pipeline_last_stage():
         src = parallel_state.get_pipeline_model_parallel_last_rank()
         group = parallel_state.get_embedding_group()
-        torch.distributed.broadcast(output_logits, src, group)
+        # torch.distributed.broadcast(output_logits, src, group)
         if all_probs:
             src = parallel_state.get_pipeline_model_parallel_last_rank()
             group = parallel_state.get_embedding_group()
@@ -416,10 +416,10 @@ def synced_generate(
         if parallel_state.is_pipeline_first_stage():
             src = parallel_state.get_pipeline_model_parallel_last_rank()
             group = parallel_state.get_embedding_group()
-            output_logits = torch.empty(
-                tokens.size(0), context_length - 1, dtype=torch.float32, device=torch.device("cuda")
-            )
-            torch.distributed.broadcast(output_logits, src, group)
+            # output_logits = torch.empty(
+            #     tokens.size(0), context_length - 1, dtype=torch.float32, device=torch.device("cuda")
+            # )
+            # torch.distributed.broadcast(output_logits, src, group)
 
             if all_probs:
                 src = parallel_state.get_pipeline_model_parallel_last_rank()
@@ -671,14 +671,23 @@ def sample_sequence_batch(
                 output = output[0]['logits'].float()
                 if torch.distributed.get_rank() == 0:
                     print("after output.float(), ", torch.cuda.memory_allocated()//(1024 ** 2), "MB")
-                output = tensor_parallel.gather_from_tensor_model_parallel_region(output)
+                logits = output[:, -1] # .view(batch_size, -1).contiguous() # last pos torch.Size([1, 256000])
+                logits = tensor_parallel.gather_from_tensor_model_parallel_region(logits)
 
-        
                 if torch.distributed.get_rank() == 0:
                     print("after output gather, ", torch.cuda.memory_allocated()//(1024 ** 2), "MB")
-                assert output is not None
-                output = output.float()
-                logits = output[:, -1].view(batch_size, -1).contiguous()
+                assert logits is not None
+                logits = logits.view(batch_size, -1).contiguous().float() # torch.Size([1, 16000, 256000])
+
+                # if output_logits is None:
+                #     output = F.log_softmax(output[:, :context_length, :], 2)
+
+                #     if torch.distributed.get_rank() == 0:
+                #         print("after logsoftmax, ", torch.cuda.memory_allocated()//(1024 ** 2), "MB")
+                # else:
+                #     output = F.log_softmax(output, 2)
+                #     if torch.distributed.get_rank() == 0:
+                #         print("after logsoftmax, ", torch.cuda.memory_allocated()//(1024 ** 2), "MB")
 
                 # make sure it will generate at least min_length
                 min_length = extra.get('min_tokens_to_generate', 0)
@@ -690,23 +699,24 @@ def sample_sequence_batch(
                 logits[:, tokenizer.vocab_size :] = -float('Inf')
 
                 # started indicates whether the current token step passes the context_length, so we make sure not to overwrite the context tokens
-                started = context_lengths <= context_length
+
+                started = context_lengths <= context_length # tensor([True])
                 if extra.get('greedy', False):
-                    prev = torch.argmax(logits, dim=-1).view(-1)
-                else:
-                    logits = logits.float()
-                    logits /= temperature
-                    # handle repetition penality
-                    logits = repetition_penalty(logits, extra.get('repetition_penalty', 1.2), all_generated_indices)
-                    logits = top_k_logits(
-                        logits, top_k=extra.get('top_k', 0), top_p=extra.get('top_p', 0.9), started=started
-                    )
-                    probs = F.softmax(logits, dim=-1)
-                    prev = torch.multinomial(probs, num_samples=1).view(-1)
+                    prev = torch.argmax(logits, dim=-1).view(-1) # torch.Size([1]), tensor([291], device='cuda:0')
+                # else:
+                #     logits = logits.float()
+                #     logits /= temperature
+                #     # handle repetition penality
+                #     logits = repetition_penalty(logits, extra.get('repetition_penalty', 1.2), all_generated_indices)
+                #     logits = top_k_logits(
+                #         logits, top_k=extra.get('top_k', 0), top_p=extra.get('top_p', 0.9), started=started
+                #     )
+                #     probs = F.softmax(logits, dim=-1)
+                #     prev = torch.multinomial(probs, num_samples=1).view(-1)
 
                 # Clamp the predicted out of vocabulary tokens
                 prev = torch.clamp(prev, max=tokenizer.vocab_size - 1)
-                new_tokens = switch(tokens[:, context_length].view(-1), prev, started)
+                new_tokens = switch(tokens[:, context_length].view(-1), prev, started) # tokens: torch.Size([1, 16003])
 
                 # Replace sampled tokens w/ done token if EOD has already been sampled
                 new_tokens = switch(new_tokens, eod_id, is_done)
@@ -715,30 +725,23 @@ def sample_sequence_batch(
                 inference_strategy.post_process(tokens, new_tokens, context_length)
 
                 # Insert either new predicted or next prompt token
-                tokens[:, context_length] = new_tokens
+                tokens[:, context_length] = new_tokens # tokens: torch.Size([1, 16003])
 
-                if output_logits is None:
-                    output = F.log_softmax(output[:, :context_length, :], 2)
+                # if output_logits is None:
+                #     indices = torch.unsqueeze(tokens[:, 1 : context_length + 1], 2)
+                #     output_logits = torch.gather(output, 2, indices).squeeze(2)
+                #     all_generated_indices = indices[:, :, 0]
+                #     if all_probs:
+                #         full_logits = output
+                # else:
+                #     indices = torch.unsqueeze(new_tokens, 1).unsqueeze(2)
+                #     new_output_logits = torch.gather(output, 2, indices).squeeze(2)
 
-                    if torch.distributed.get_rank() == 0:
-                        print("after logsoftmax, ", torch.cuda.memory_allocated()//(1024 ** 2), "MB")
-                    indices = torch.unsqueeze(tokens[:, 1 : context_length + 1], 2)
-                    output_logits = torch.gather(output, 2, indices).squeeze(2)
-                    all_generated_indices = indices[:, :, 0]
-                    if all_probs:
-                        full_logits = output
-                else:
-                    output = F.log_softmax(output, 2)
-                    if torch.distributed.get_rank() == 0:
-                        print("after logsoftmax, ", torch.cuda.memory_allocated()//(1024 ** 2), "MB")
-                    indices = torch.unsqueeze(new_tokens, 1).unsqueeze(2)
-                    new_output_logits = torch.gather(output, 2, indices).squeeze(2)
-
-                    # TODO(rprenger) we're copying output_logits every time.  Should pre-allocate
-                    output_logits = torch.cat([output_logits, new_output_logits], 1)
-                    all_generated_indices = torch.cat([all_generated_indices, indices[:, :, 0]], 1)
-                    if all_probs:
-                        full_logits = torch.cat([full_logits, output], 1)
+                #     # TODO(rprenger) we're copying output_logits every time.  Should pre-allocate
+                #     output_logits = torch.cat([output_logits, new_output_logits], 1)
+                #     all_generated_indices = torch.cat([all_generated_indices, indices[:, :, 0]], 1)
+                #     if all_probs:
+                #         full_logits = torch.cat([full_logits, output], 1)
 
                 src = parallel_state.get_pipeline_model_parallel_last_rank()
                 group = parallel_state.get_embedding_group()
@@ -759,9 +762,9 @@ def sample_sequence_batch(
                 group = parallel_state.get_pipeline_model_parallel_group()
                 torch.distributed.broadcast(done, src, group)
                 if all_probs:
-                    yield tokens, lengths, output_logits, full_logits
+                    yield tokens, lengths, None, None
                 else:
-                    yield tokens, lengths, output_logits, None
+                    yield tokens, lengths, None, None
 
             else:
                 if parallel_state.is_pipeline_first_stage():