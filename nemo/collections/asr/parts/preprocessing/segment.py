1353aca1c0c9346f47df97148bc6d2fd325e7393
He Huang (Steve) <105218074+stevehuang52@users.noreply.github.com>
2023-06-01 14:55:57 -0400

    Add Frame-VAD examples and utils (#6463)
    * add model, dataset, necessary utils and tests
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * fix tarred data
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * fix typo
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * add fvad examples and update utils
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * add copyright
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * refactor and add tests
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * update dataset
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * update test
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * refactor
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * refactor
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    * fix typos
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    
    ---------
    
    Signed-off-by: stevehuang52 <heh@nvidia.com>
    Co-authored-by: fayejf <36722593+fayejf@users.noreply.github.com>
    Co-authored-by: Taejin Park <tango4j@gmail.com>
    Signed-off-by: hsiehjackson <c2hsieh@ucsd.edu>

--- nemo/collections/asr/parts/preprocessing/segment.py
+++ nemo/collections/asr/parts/preprocessing/segment.py
@@ -36,6 +36,7 @@
 import math
 import os
 import random
+from typing import Optional
 
 import librosa
 import numpy as np
@@ -78,8 +79,8 @@ class AudioSegment(object):
         trim_hop_length=512,
         orig_sr=None,
         channel_selector=None,
-        normalize_db=False,
-        normalize_db_target=-20.0,
+        normalize_db: Optional[float] = None,
+        ref_channel: Optional[int] = None,
     ):
         """Create audio segment from samples.
         Samples are convert float32 internally, with int scaled to [-1, 1].
@@ -114,8 +115,11 @@ class AudioSegment(object):
         self._samples = samples
         self._sample_rate = sample_rate
         self._orig_sr = orig_sr if orig_sr is not None else sample_rate
-        if normalize_db:
-            self.normalize_db(normalize_db_target)
+        self._ref_channel = ref_channel
+        self._normalize_db = normalize_db
+
+        if normalize_db is not None:
+            self.normalize_db(normalize_db, ref_channel)
 
     def __eq__(self, other):
         """Return whether two objects are equal."""
@@ -185,8 +189,8 @@ class AudioSegment(object):
         trim_hop_length=512,
         orig_sr=None,
         channel_selector=None,
-        normalize_db=False,
-        normalize_db_target=-20.0,
+        normalize_db=None,
+        ref_channel=None,
     ):
         """
         Load a file supported by librosa and return as an AudioSegment.
@@ -207,8 +211,8 @@ class AudioSegment(object):
         :param channel selector: string denoting the downmix mode, an integer denoting the channel to be selected, or an iterable
                                  of integers denoting a subset of channels. Channel selector is using zero-based indexing.
                                  If set to `None`, the original signal will be used.
-        :param normalize_db (bool): if true, normalize the audio signal to a target RMS value
-        :param normalize_db_target (float): the target RMS value in decibels
+        :param normalize_db (Optional[float]): if not None, normalize the audio signal to a target RMS value
+        :param ref_channel (Optional[int]): channel to use as reference for normalizing multi-channel audio, set None to use max RMS across channels
         :return: AudioSegment instance
         """
         samples = None
@@ -226,6 +230,8 @@ class AudioSegment(object):
                 trim_hop_length=trim_hop_length,
                 orig_sr=orig_sr,
                 channel_selector=channel_selector,
+                normalize_db=normalize_db,
+                ref_channel=ref_channel,
             )
 
         if not isinstance(audio_file, str) or os.path.splitext(audio_file)[-1] in sf_supported_formats:
@@ -283,7 +289,7 @@ class AudioSegment(object):
             orig_sr=orig_sr,
             channel_selector=channel_selector,
             normalize_db=normalize_db,
-            normalize_db_target=normalize_db_target,
+            ref_channel=ref_channel,
         )
 
     @classmethod
@@ -464,10 +470,14 @@ class AudioSegment(object):
     def gain_db(self, gain):
         self._samples *= 10.0 ** (gain / 20.0)
 
-    def normalize_db(self, target_db=-20):
+    def normalize_db(self, target_db=-20, ref_channel=None):
         """Normalize the signal to a target RMS value in decibels. 
+        For multi-channel audio, the RMS value is determined by the reference channel (if not None),
+        otherwise it will be the maximum RMS across all channels.
         """
         rms_db = self.rms_db
+        if self.num_channels > 1:
+            rms_db = max(rms_db) if ref_channel is None else rms_db[ref_channel]
         gain = target_db - rms_db
         self.gain_db(gain)
 
