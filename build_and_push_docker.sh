7fa46c1b8e187a3582940b9da851632200c75d2c
Dan Crankshaw <crankshaw@google.com>
2023-06-03 00:57:10 +0000

    Include megatron git hash in docker tag

--- build_and_push_docker.sh
+++ build_and_push_docker.sh
@@ -3,14 +3,17 @@
 set -euo pipefail
 
 IMAGE="us-central1-docker.pkg.dev/supercomputer-testing/crankshaw-nemo-stagetest/nemo"
-TAG=$(git rev-parse --short HEAD)
 
 cd ../Megatron-LM
 MEGATRON_VER=$(git rev-parse --short HEAD)
 cd -
+
+TAG="$(git rev-parse --short HEAD)-$MEGATRON_VER"
 
 IMAGE_FULL=$IMAGE:$TAG
 
 DOCKER_BUILDKIT=1 docker build --build-arg MEGATRON_VER=$MEGATRON_VER -f Dockerfile -t $IMAGE_FULL .
 
 docker push $IMAGE_FULL
+
+echo "New tag: $TAG"
