bug in causal
Signed-off-by: Dima Rekesh <bmwshop@gmail.com>

nemo/collections/asr/parts/submodules/subsampling.py
@@ -385,7 +385,7 @@ class ConvSubsampling(torch.nn.Module):
             step = chunk.size()[1]
 
             if self.is_causal:
-                chunk = nn.functional.pad(chunk, pad=(kernel_size - 1, stride - 1, kernel_size - 1, stride - 1))
+                chunk = nn.functional.pad(chunk, pad=(self._kernel_size - 1, self._stride - 1, self._kernel_size - 1, self._stride - 1))
                 ch_out = nn.functional.conv2d(
                     chunk,
                     conv.weight[ind : ind + step, :, :, :],