db4564071e65c3b9bc7c189a826d85d341536ca1
Dima Rekesh <bmwshop@gmail.com>
2023-06-05 18:13:01 -0700

    imports in test
    Signed-off-by: Dima Rekesh <bmwshop@gmail.com>

--- tests/collections/asr/test_asr_subsampling.py
+++ tests/collections/asr/test_asr_subsampling.py
@@ -11,16 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-import shutil
-import tempfile
-
 import pytest
-import pytorch_lightning as pl
 import torch
-from omegaconf import DictConfig
 
-from nemo.collections.asr.models import ASRModel, EncDecCTCModel
+from nemo.collections.asr.models import ASRModel
 
 
 class TestASRSubsamplingConvChunking:
