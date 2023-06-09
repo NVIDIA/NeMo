e0939a569b52313f76f4e1975a0c94f97e29c1d4
Yu Yao <yuya@nvidia.com>
2023-06-09 13:04:25 -0700

    Add imagen support

--- examples/multimodal/convert_ckpt_to_nemo.py
+++ examples/multimodal/convert_ckpt_to_nemo.py
@@ -33,6 +33,7 @@ from pytorch_lightning.trainer.trainer import Trainer
 
 from nemo.collections.multimodal.models.clip.megatron_clip_models import MegatronCLIPModel
 from nemo.collections.multimodal.models.dreambooth.dreambooth import MegatronDreamBooth
+from nemo.collections.multimodal.models.imagen.imagen import MegatronImagen
 from nemo.collections.multimodal.models.instruct_pix2pix.ldm.ddpm_edit import MegatronLatentDiffusionEdit
 from nemo.collections.multimodal.models.stable_diffusion.ldm.ddpm import MegatronLatentDiffusion
 from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector
@@ -156,6 +157,8 @@ def convert(local_rank, rank, world_size, args):
         model = MegatronLatentDiffusion.load_from_checkpoint(
             checkpoint_path, hparams_file=args.hparams_file, trainer=trainer
         )
+    elif args.model_type == 'imagen':
+        model = MegatronImagen.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer)
     else:
         raise ValueError(f"Unrecognized model_type {args.model_type}.")
 
