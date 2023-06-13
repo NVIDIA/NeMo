2c6e2520cc20c3c1b26b320a505b9590062479bf
arendu <adithya.r@gmail.com>
2023-06-13 10:49:58 -0700

    update
    Signed-off-by: arendu <adithya.r@gmail.com>

--- examples/nlp/language_modeling/tuning/megatron_gpt_sft.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_sft.py
@@ -19,20 +19,54 @@ from omegaconf.omegaconf import OmegaConf, open_dict
 from pytorch_lightning import Trainer
 from pytorch_lightning.plugins.environments import TorchElasticEnvironment
 from pytorch_lightning.trainer.connectors.checkpoint_connector import CheckpointConnector
+from torch.utils.data import DataLoader, Dataset
 
-from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTSFTModel
+from nemo.collections.nlp.models.language_modeling.megatron_gpt_peft_models import (
+    MegatronGPTAdapterModel,
+    MegatronGPTAdapterPTuningModel,
+    MegatronGPTIA3Model,
+    MegatronGPTLoRAModel,
+    MegatronGPTPTuningModel,
+)
+from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTModel, MegatronGPTSFTModel
 from nemo.collections.nlp.modules.common.megatron.megatron_init import fake_initialize_model_parallel
 from nemo.collections.nlp.parts.nlp_overrides import (
     GradScaler,
     MegatronHalfPrecisionPlugin,
     NLPDDPStrategy,
     NLPSaveRestoreConnector,
+    PEFTSaveRestoreConnector,
     PipelineMixedPrecisionPlugin,
 )
 from nemo.core.config import hydra_runner
 from nemo.utils import AppState, logging
 from nemo.utils.exp_manager import exp_manager
 from nemo.utils.model_utils import inject_model_parallel_rank
+
+mp.set_start_method("spawn", force=True)
+
+"""
+TODO: @adithyre should update this docstring.
+This is the script to train an Adapter infused GPT Model for text generation.
+A base GPT Model is required as a starting point. This script will then insert
+Adapters into each Transformer layer and will train/update only these adapters
+during training. The base GPT Model weights will remain frozen.
+
+During training this script will only save the newly trained Adapter weights
+in checkpoints. At the end of training a .nemo file of Adapter weights will 
+be saved.
+
+Usage:
+    Assuming the base model is a 125m GPT Model, with TP=1, PP=1:
+    a. run a training run for a base gpt nemo file:
+        python megatron_gpt_adapter_tuning.py \
+            "model.data.train_ds=[PATH TO TRAINING JSONL FILE]",
+            "model.data.validation_ds=[PATH TO VALIDATION JSONL FILE]",
+            model.language_model_path="PATH TO BASE GPT MODEL .nemo FILE"
+            name="NAME OF TRAINING RUN"
+            exp_manager.exp_dir="DIR TO SAVE CHECKPOINTS and .nemo FILE",
+            trainer.max_epochs=2
+"""
 
 
 def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
@@ -61,7 +95,8 @@ def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
         gpt_cfg.hidden_dropout = cfg.model.get('hidden_dropout', 0.0)
         gpt_cfg.attention_dropout = cfg.model.get('attention_dropout', 0.0)
         gpt_cfg.ffn_dropout = cfg.model.ffn_dropout
-        sft_cls = MegatronGPTSFTModel
+        gpt_cfg.peft = cfg.model.peft
+        sft_cls = _get_peft_scheme(cfg.model)
         gpt_cfg.target = f"{sft_cls.__module__}.{sft_cls.__name__}"
 
         # This is needed when modifying a hparam file directly to load `.ckpt` files.
@@ -71,6 +106,24 @@ def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
             gpt_cfg.cfg = gpt_cfg
 
     return gpt_cfg
+
+
+def _get_peft_scheme(cfg):
+    if cfg.peft.peft_scheme is None:
+        peft_cls = MegatronGPTSFTModel
+    elif cfg.peft.peft_scheme == "adapter":
+        peft_cls = MegatronGPTAdapterModel
+    elif cfg.peft.peft_scheme == "ia3":
+        peft_cls = MegatronGPTIA3Model
+    elif cfg.peft.peft_scheme == "ptuning":
+        peft_cls = MegatronGPTPTuningModel
+    elif cfg.peft.peft_scheme == "adapter_and_ptuning":
+        peft_cls = MegatronGPTAdapterPTuningModel
+    elif cfg.peft.peft_scheme == "lora":
+        peft_cls = MegatronGPTLoRAModel
+    else:
+        raise RuntimeError("Invalid Peft scheme")
+    return peft_cls
 
 
 def load_from_nemo(cls, cfg, trainer, gpt_cfg, modify_confg_fn):
@@ -147,6 +200,9 @@ def main(cfg) -> None:
                 init_scale=cfg.model.get('native_amp_init_scale', 2 ** 32),
                 growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
                 hysteresis=cfg.model.get('hysteresis', 2),
+                enabled=False
+                if cfg.model.pipeline_model_parallel_size > 1
+                else True,  # turn off the grad scale for pipeline parallel LM model
             )
         if megatron_amp_o2 and not with_distributed_adam:
             plugins.append(MegatronHalfPrecisionPlugin(precision=cfg.trainer.precision, device='cuda', scaler=scaler))
@@ -184,7 +240,23 @@ def main(cfg) -> None:
             save_restore_connector=save_restore_connector,
         )
         gpt_cfg = _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False)
-        model = load_from_nemo(MegatronGPTSFTModel, cfg, trainer, gpt_cfg, modify_confg_fn=_modify_config)
+        peft_cls = _get_peft_scheme(cfg.model)
+        if peft_cls == MegatronGPTSFTModel:
+            model = load_from_nemo(MegatronGPTSFTModel, cfg, trainer, gpt_cfg, modify_confg_fn=_modify_config)
+        else:
+            save_restore_connector = PEFTSaveRestoreConnector(
+                peft_model_nemo_path=cfg.model.peft.restore_from_path, peft_model_ckpt_path=resume_from_checkpoint
+            )
+            if os.path.isdir(cfg.model.restore_from_path):
+                save_restore_connector.model_extracted_dir = cfg.model.restore_from_path
+
+        model = peft_cls.restore_from(
+            restore_path=cfg.model.restore_from_path,
+            trainer=trainer,
+            override_config_path=base_model_cfg,
+            save_restore_connector=save_restore_connector,
+        )
+
     else:
         validate_checkpoint_loading_args(cfg.model.pretrained_checkpoint)
         model = load_from_checkpoint_dir(MegatronGPTSFTModel, cfg, trainer, modify_confg_fn=_modify_config)
