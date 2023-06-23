7992ddd57b048d760bcf400096fbcff6387d3341
ericharper <complex451@gmail.com>
2023-06-23 11:46:38 -0600

    set vp size to none if it is 1
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_base_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_base_model.py
@@ -126,18 +126,30 @@ class MegatronBaseModel(NLPModel):
             init_global_rank = trainer.global_rank
             init_local_rank = trainer.local_rank
 
+        # Set virtual pipeline size to None if it is 1 and
+        # confirm that the number of model chunks is the same across all pipeline stages.
+        vp_size = self.cfg.get('virtual_pipeline_model_parallel_size', None)
+
+        if vp_size is not None:
+            if vp_size == 1:
+                self.cfg.virtual_pipeline_model_parallel_size = None
+            else:
+                assert (
+                    self.cfg.num_layers // self.cfg.pipeline_model_parallel_size
+                ) % vp_size == 0, 'Make sure the number of model chunks is the same across all pipeline stages.'
+
         initialize_model_parallel_for_nemo(
             world_size=init_world_size,
             global_rank=init_global_rank,
             local_rank=init_local_rank,
-            tensor_model_parallel_size=cfg.get('tensor_model_parallel_size', 1),
-            pipeline_model_parallel_size=cfg.get('pipeline_model_parallel_size', 1),
-            virtual_pipeline_model_parallel_size=cfg.get('virtual_pipeline_model_parallel_size', None),
-            pipeline_model_parallel_split_rank=cfg.get('pipeline_model_parallel_split_rank', 0),
-            micro_batch_size=cfg.get('micro_batch_size'),
-            global_batch_size=cfg.get('global_batch_size'),
-            rampup_batch_size=cfg.get('rampup_batch_size'),
-            use_fp8=cfg.get('fp8', False),
+            tensor_model_parallel_size=self.cfg.get('tensor_model_parallel_size', 1),
+            pipeline_model_parallel_size=self.cfg.get('pipeline_model_parallel_size', 1),
+            virtual_pipeline_model_parallel_size=self.cfg.get('virtual_pipeline_model_parallel_size', None),
+            pipeline_model_parallel_split_rank=self.cfg.get('pipeline_model_parallel_split_rank', 0),
+            micro_batch_size=self.cfg.get('micro_batch_size'),
+            global_batch_size=self.cfg.get('global_batch_size'),
+            rampup_batch_size=self.cfg.get('rampup_batch_size'),
+            use_fp8=self.cfg.get('fp8', False),
             seed=self.cfg.get('seed', 1234),
             apex_transformer_log_level=self.cfg.get('apex_transformer_log_level', 30),
         )
@@ -558,16 +570,6 @@ class MegatronBaseModel(NLPModel):
 
         if self.cfg.get('use_emha', False):
             raise ValueError('use_emha is not yet supported please set to False')
-
-        vp_size = self.cfg.get('virtual_pipeline_model_parallel_size', None)
-
-        if vp_size is not None:
-            if vp_size == 1:
-                self.cfg['virtual_pipeline_model_parallel_size'] = None
-            else:
-                assert (
-                    self.cfg.num_layers // self.cfg.pipeline_model_parallel_size
-                ) % vp_size == 0, 'Make sure the number of model chunks is the same across all pipeline stages.'
 
     def is_data_parallel_rank_zero(self):
         if is_global_rank_zero():
