alpha computation runing but wrong

nemo/collections/asr/losses/rnnt_pytorch.py
@@ -47,6 +47,9 @@ class RNNTLossPytorch(Loss):
         self.reduction = reduction
 
     def forward(self, acts, labels, act_lens, label_lens):
+        shape_list = [int(x) for x in acts.shape]
+        assert len(shape_list) == 3
+
         normed_acts = torch.log_softmax(acts[:,:,:self.vocab_size + 1], -1)
         duration_acts = acts[:,:,self.vocab_size + 1:]
         forward_logprob = self.compute_forward_prob(normed_acts, duration_acts, labels, act_lens, label_lens)
@@ -69,6 +72,7 @@ class RNNTLossPytorch(Loss):
         return losses
 
     def compute_forward_prob(self, acts, duration_acts, labels, act_lens, label_lens):
+        shape_list = [int(x) for x in acts.shape]
         B, T, V = acts.shape
         D = duration_acts.shape[-1]
         U = torch.max(label_lens).item()
@@ -100,9 +104,6 @@ class RNNTLossPytorch(Loss):
                                     dim=0,
                                 )
 
-
-#                    print('alpha', b, t, u, log_alpha[b, t, u])
-
         log_probs = []
         for b in range(B):
             # here we need to add the final blank emission weights.
@@ -110,6 +111,7 @@ class RNNTLossPytorch(Loss):
             u = label_lens[b]
             to_append = log_alpha[b, t - 1, u + 1] + acts[b, t - 1, -1]
             log_probs.append(to_append)
+
         log_prob = torch.stack(log_probs)
         print('log_prob', log_prob)
         print()
@@ -432,94 +434,3 @@ class MultiblankRNNTLossPytorch(Loss):
 
 
 
-class RNNTLossPytorch(Loss):
-    @property
-    def input_types(self):
-        """Input types definitions for CTCLoss.
-        """
-        return {
-            "acts": NeuralType(('B', 'T', 'T', 'D'), LogprobsType()),
-            "labels": NeuralType(('B', 'T'), LabelsType()),
-            "act_lens": NeuralType(tuple('B'), LengthsType()),
-            "label_lens": NeuralType(tuple('B'), LengthsType()),
-        }
-
-    @property
-    def output_types(self):
-        """Output types definitions for CTCLoss.
-        loss:
-            NeuralType(None)
-        """
-        return {"loss": NeuralType(elements_type=LossType())}
-
-    def __init__(self, blank, reduction):
-        super().__init__()
-        self.blank = blank
-        self.reduction = reduction
-
-    def forward(self, acts, labels, act_lens, label_lens):
-        acts = torch.log_softmax(acts, -1)
-        forward_logprob = self.compute_forward_prob(acts, labels, act_lens, label_lens)
-        losses = -forward_logprob
-        if self.reduction == 'mean_batch':
-            losses = losses.mean()  # global batch size average
-        elif self.reduction == 'mean':
-            losses = torch.div(losses, label_lens).mean()
-        elif self.reduction == 'sum':
-            losses = losses.sum()
-        elif self.reduction == 'mean_volume':
-            losses = losses.sum() / label_lens.sum()  # same as above but longer samples weigh more
-
-        return losses
-
-    def compute_forward_prob(self, acts, labels, act_lens, label_lens):
-        B, T, U, _ = acts.shape
-
-        log_alpha = torch.zeros(B, T, U)
-        log_alpha = log_alpha.to(acts.device)
-
-        for t in range(T):
-            for u in range(U):
-                if u == 0:
-                    if t == 0:
-                        # this is the base case: (t=0, u=0) with log-alpha = 0.
-                        log_alpha[:, t, u] = 0.0
-                    else:
-                        # this is case for (t = 0, u > 0), reached by (t, u - 1)
-                        # emitting a blank symbol.
-                        log_alpha[:, t, u] = log_alpha[:, t - 1, u] + acts[:, t - 1, 0, self.blank]
-                else:
-                    if t == 0:
-                        # in case of (u > 0, t = 0), this is only reached from
-                        # (t, u - 1) with a label emission.
-                        gathered = torch.gather(
-                            acts[:, t, u - 1], dim=1, index=labels[:, u - 1].view(-1, 1).type(torch.int64)
-                        ).reshape(-1)
-                        log_alpha[:, t, u] = log_alpha[:, t, u - 1] + gathered.to(log_alpha.device)
-                    else:
-                        # here both t and u are > 0, this state is reachable
-                        # with two possibilities: (t - 1, u) with a blank emission
-                        # or (t, u - 1) with a label emission.
-                        log_alpha[:, t, u] = torch.logsumexp(
-                            torch.stack(
-                                [
-                                    log_alpha[:, t - 1, u] + acts[:, t - 1, u, self.blank],
-                                    log_alpha[:, t, u - 1]
-                                    + torch.gather(
-                                        acts[:, t, u - 1], dim=1, index=labels[:, u - 1].view(-1, 1).type(torch.int64)
-                                    ).reshape(-1),
-                                ]
-                            ),
-                            dim=0,
-                        )
-
-        log_probs = []
-        for b in range(B):
-            # here we need to add the final blank emission weights.
-            to_append = (
-                log_alpha[b, act_lens[b] - 1, label_lens[b]] + acts[b, act_lens[b] - 1, label_lens[b], self.blank]
-            )
-            log_probs.append(to_append)
-        log_prob = torch.stack(log_probs)
-
-        return log_prob

nemo/collections/asr/parts/numba/rnnt_loss/rnnt.py
@@ -137,13 +137,14 @@ def rnnt_loss_cpu(
 
 def rnnt_loss_gpu(
     acts: torch.Tensor,
+    duration_acts: torch.Tensor,
     labels: torch.Tensor,
     input_lengths: torch.Tensor,
     label_lengths: torch.Tensor,
     costs: torch.Tensor,
     grads: torch.Tensor,
-    blank_label: int,
-    fastemit_lambda: float,
+    duration_grads: torch.Tensor,
+    vocab_size: int,
     clamp: float,
     num_threads: int,
 ):
@@ -166,6 +167,8 @@ def rnnt_loss_gpu(
     minibatch_size = acts.shape[0]
     maxT = acts.shape[1]
     alphabet_size = acts.shape[2]
+    maxU = label_lengths.max().item()
+
 
     if hasattr(cuda, 'external_stream'):
         stream = cuda.external_stream(torch.cuda.current_stream(acts.device).cuda_stream)
@@ -177,7 +180,7 @@ def rnnt_loss_gpu(
 
     num_threads = max(1, num_threads)  # have to use at least 1 thread
 
-    gpu_size, status = rnnt_helper.get_workspace_size(maxT, minibatch_size, gpu=True)
+    gpu_size, status = rnnt_helper.get_workspace_size(maxT, maxU, minibatch_size, gpu=True)
     if status != global_constants.RNNTStatus.RNNT_STATUS_SUCCESS:
         raise RuntimeError("Invalid parameter passed when calculating working space memory")
 
@@ -187,13 +190,15 @@ def rnnt_loss_gpu(
 
     ### VIEW TENSORS AS VECTORS FOR POINTER INDEXING ###
     acts, acts_shape = rnnt_helper.flatten_tensor(acts)
+    duration_acts, duration_acts_shape = rnnt_helper.flatten_tensor(duration_acts)
 
     wrapper = gpu_rnnt.GPURNNT(
         minibatch=minibatch_size,
         maxT=maxT,
+        maxU=maxU,
         alphabet_size=alphabet_size,
         workspace=gpu_workspace,
-        blank=blank_label,
+        vocab_size=vocab_size,
         clamp=clamp,
         num_threads=num_threads,
         stream=stream,
@@ -202,6 +207,7 @@ def rnnt_loss_gpu(
     if grads is None:
         status = wrapper.score_forward(
             acts=acts.data,
+            duration_acts=duration_acts.data,
             costs=costs.data,
             pad_labels=labels.data,
             label_lengths=label_lengths.data,
@@ -214,10 +220,13 @@ def rnnt_loss_gpu(
     else:
         ### FLATTEN GRAD TENSOR ###
         grads, grads_shape = rnnt_helper.flatten_tensor(grads)
+        duration_grads, duration_grads_shape = rnnt_helper.flatten_tensor(duration_grads)
 
         status = wrapper.cost_and_grad(
             acts=acts.data,
+            duration_acts=duration_acts.data,
             grads=grads.data,
+            duration_grads=duration_grads.data,
             costs=costs.data,
             pad_labels=labels.data,
             label_lengths=label_lengths.data,

nemo/collections/asr/parts/numba/rnnt_loss/rnnt_pytorch.py
@@ -39,7 +39,7 @@ __all__ = ['rnnt_loss', 'RNNTLossNumba', 'MultiblankRNNTLossNumba', 'TDTLossNumb
 
 class _RNNTNumba(Function):
     @staticmethod
-    def forward(ctx, acts, labels, act_lens, label_lens, blank, reduction, fastemit_lambda, clamp):
+    def forward(ctx, acts, duration_acts, labels, act_lens, label_lens, vocab_size, reduction, clamp):
         """
         log_probs: Tensor of (batch x seqLength x labelLength x outputDim) containing output from network
         labels: 2 dimensional Tensor containing all the targets of the batch with zero padded
@@ -51,23 +51,26 @@ class _RNNTNumba(Function):
         is_cuda = acts.is_cuda
 
         certify_inputs(acts, labels, act_lens, label_lens)
+        certify_inputs(duration_acts, labels, act_lens, label_lens)
         if clamp < 0:
             raise ValueError("`clamp` must be 0.0 or positive float value.")
 
         loss_func = rnnt.rnnt_loss_gpu if is_cuda else rnnt.rnnt_loss_cpu
-        grads = torch.zeros_like(acts) if acts.requires_grad else None
+        vocab_grads = torch.zeros_like(acts) if acts.requires_grad else None
+        duration_grads = torch.zeros_like(duration_acts) if acts.requires_grad else None
         minibatch_size = acts.size(0)
         costs = torch.zeros(minibatch_size, device=acts.device, dtype=acts.dtype)
 
         loss_func(
             acts,
+            duration_acts,
             labels=labels,
             input_lengths=act_lens,
             label_lengths=label_lens,
             costs=costs,
-            grads=grads,
-            blank_label=blank,
-            fastemit_lambda=fastemit_lambda,
+            grads=vocab_grads,
+            duration_grads=duration_grads,
+            vocab_size=vocab_size,
             clamp=clamp,
             num_threads=0,
         )
@@ -80,15 +83,16 @@ class _RNNTNumba(Function):
                 if grads is not None:
                     grads /= minibatch_size
 
-        ctx.grads = grads
+        ctx.grads = vocab_grads
+        ctx.duration_grads = duration_grads
 
         return costs
 
     @staticmethod
     def backward(ctx, grad_output):
         if grad_output is not None and ctx.grads is not None:
-            grad_output = grad_output.view(-1, 1, 1, 1).to(ctx.grads)
-            return ctx.grads.mul_(grad_output), None, None, None, None, None, None, None
+            grad_output = grad_output.view(-1, 1, 1).to(ctx.grads)
+            return ctx.grads.mul_(grad_output), ctx.duration_grads.mul_(grad_output), None, None, None, None, None, None, None
 
 
 class _TDTNumba(Function):
@@ -391,7 +395,7 @@ def tdt_loss(
 class RNNTLossNumba(Module):
     """
     Parameters:
-        blank (int, optional): blank label. Default: 0.
+        vocab_size (int, optional): 
         reduction (string, optional): Specifies the reduction to apply to the output:
             'none' | 'mean' | 'sum'. 'none': no reduction will be applied,
             'mean': the output losses will be divided by the target lengths and
@@ -401,10 +405,10 @@ class RNNTLossNumba(Module):
         clamp: Float value. When set to value >= 0.0, will clamp the gradient to [-clamp, clamp].
     """
 
-    def __init__(self, blank=0, reduction='mean', fastemit_lambda: float = 0.0, clamp: float = -1):
+    def __init__(self, vocab_size=0, reduction='mean', clamp: float = -1):
         super(RNNTLossNumba, self).__init__()
-        self.blank = blank
-        self.fastemit_lambda = fastemit_lambda
+        self.vocab_size = vocab_size
+        assert vocab_size != 0
         self.clamp = float(clamp) if clamp > 0 else 0.0
         self.reduction = reduction
         self.loss = _RNNTNumba.apply
@@ -416,21 +420,13 @@ class RNNTLossNumba(Module):
         act_lens: Tensor of size (batch) containing size of each output sequence from the network
         label_lens: Tensor of (batch) containing label length of each example
         """
-        if not acts.is_cuda:
-            # Since CPU requires log_softmax to be computed explicitly, we need to perform grad clipping
-            # *after* we have obtained the gradients of loss(logsoftmax()).
-            # This is highly wasteful since it requires a copy of the entire joint tensor which is expensive.
-            # CUDA version is much more efficient since it performs an inplace logsoftmax, and therefore
-            # can inplace clamp the gradient.
-            if self.clamp > 0.0:
-                acts = cpu_rnnt.LogSoftmaxGradModification.apply(acts, self.clamp)
 
-            # NOTE manually done log_softmax for CPU version,
-            # log_softmax is computed within GPU version.
-            acts = torch.nn.functional.log_softmax(acts, -1)
+        vocab_acts = acts[:,:,:self.vocab_size].contiguous()
+        duration_acts = acts[:,:,self.vocab_size:].contiguous()
+        vocab_acts = torch.nn.functional.log_softmax(vocab_acts, -1)
 
         return self.loss(
-            acts, labels, act_lens, label_lens, self.blank, self.reduction, self.fastemit_lambda, self.clamp
+            vocab_acts, duration_acts, labels, act_lens, label_lens, self.vocab_size, self.reduction, self.clamp
         )
 
 
@@ -616,14 +612,12 @@ def certify_inputs(log_probs, labels, lengths, label_lengths):
             f"Log probs dim : {log_probs.shape[0]}"
         )
 
-    check_dim(log_probs, 4, "log_probs")
+    check_dim(log_probs, 3, "log_probs")
     check_dim(labels, 2, "labels")
     check_dim(lengths, 1, "lenghts")
     check_dim(label_lengths, 1, "label_lenghts")
     max_T = torch.max(lengths)
     max_U = torch.max(label_lengths)
-    T, U = log_probs.shape[1:3]
+    T = log_probs.shape[1]
     if T != max_T:
         raise ValueError(f"Input length mismatch! Given T: {T}, Expected max T from input lengths: {max_T}")
-    if U != max_U + 1:
-        raise ValueError(f"Output length mismatch! Given U: {U}, Expected max U from target lengths: {max_U} + 1")