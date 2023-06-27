458a6ee2b168fd16283c935b362bb4a6e13f6de3
stevehuang52 <heh@nvidia.com>
2023-06-27 14:17:31 -0400

    update
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- workspace/train.sh
+++ workspace/train.sh
@@ -6,10 +6,10 @@ data_dir="/media/data/datasets/LibriSpeech"
 train_manifests="[${data_dir}/train_clean_360_cleaned.json,${data_dir}/train_clean_100_cleaned.json,${data_dir}/train_other_500_cleaned.json]"
 # train_manifests="[${data_dir}/test_clean.json]"
 dev_manifests="${data_dir}/dev_clean_cleaned.json"
-batch_size=4
+batch_size=16
 num_workers=8
 
-CUDA_VISIBLE_DEVICES="0" python speech_pretrain.py \
+CUDA_VISIBLE_DEVICES="0,1" python speech_pretrain.py \
     --config-path="configs/" \
     --config-name="conformer_large_ssl_rq" \
     model.train_ds.manifest_filepath=$train_manifests \
@@ -18,7 +18,8 @@ CUDA_VISIBLE_DEVICES="0" python speech_pretrain.py \
     model.validation_ds.batch_size=$batch_size \
     model.train_ds.num_workers=$num_workers \
     model.validation_ds.num_workers=$num_workers \
-    exp_manager.create_wandb_logger=False \
-    exp_manager.wandb_logger_kwargs.name="conformer_ssl_rq_debug_r3" \
-    exp_manager.wandb_logger_kwargs.project="ssl_debug" \
+    exp_manager.create_wandb_logger=True \
+    exp_manager.wandb_logger_kwargs.name="ssl_conformer_large_rq_ls" \
+    exp_manager.wandb_logger_kwargs.project="ssl_ConformerL-RQ_ls" \
+    ++trainer.accumulate_grad_batches=64
 
