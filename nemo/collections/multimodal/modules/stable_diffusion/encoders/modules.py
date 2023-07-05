d2dfabd742cbbad684f7d06ba2a1e23baebbaa64
Bobby Chen <bobchen@nvidia.com>
2023-07-05 11:23:41 -0700

    Fix SD export issue

--- nemo/collections/multimodal/modules/stable_diffusion/encoders/modules.py
+++ nemo/collections/multimodal/modules/stable_diffusion/encoders/modules.py
@@ -11,14 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import kornia
-import open_clip
 import os
 import tempfile
+from functools import partial
+
+import kornia
+import open_clip
 import torch
 import torch.nn as nn
 from einops import rearrange, repeat
-from functools import partial
 from omegaconf import OmegaConf
 from torch.utils.checkpoint import checkpoint
 from transformers import CLIPTextConfig, CLIPTextModel, CLIPTokenizer
@@ -26,10 +27,10 @@ from transformers.models.clip.modeling_clip import CLIPTextTransformer
 
 from nemo.collections.multimodal.data.clip.clip_dataset import get_preprocess_fns
 from nemo.collections.multimodal.models.clip.megatron_clip_models import CLIPModel
-from nemo.collections.multimodal.modules.stable_diffusion.encoders.x_transformer import Encoder
 from nemo.collections.multimodal.modules.stable_diffusion.encoders.x_transformer import (
     TransformerWrapper,  # TODO: can we directly rely on lucidrains code and simply add this as a reuirement? --> test
 )
+from nemo.collections.multimodal.modules.stable_diffusion.encoders.x_transformer import Encoder
 from nemo.collections.nlp.modules.common.megatron.megatron_init import initialize_model_parallel_for_nemo
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
 from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector
@@ -118,14 +119,14 @@ class BERTEmbedder(AbstractEncoder):
     """Uses the BERT tokenizr model and add some transformer encoder layers"""
 
     def __init__(
-            self,
-            n_embed,
-            n_layer,
-            vocab_size=30522,
-            max_seq_len=77,
-            device="cuda",
-            use_tokenizer=True,
-            embedding_dropout=0.0,
+        self,
+        n_embed,
+        n_layer,
+        vocab_size=30522,
+        max_seq_len=77,
+        device="cuda",
+        use_tokenizer=True,
+        embedding_dropout=0.0,
     ):
         super().__init__()
         self.use_tknz_fn = use_tokenizer
@@ -181,8 +182,7 @@ class FrozenCLIPEmbedder(AbstractEncoder):
     """Uses the CLIP transformer encoder for text (from Hugging Face)"""
 
     def __init__(
-            self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77,
-            capture_cudagraph_iters: int = -1
+        self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77, capture_cudagraph_iters: int = -1
     ):
         super().__init__()
         self.tokenizer = CLIPTokenizer.from_pretrained(version)
@@ -263,14 +263,14 @@ class FrozenOpenCLIPEmbedder(AbstractEncoder):
     ]
 
     def __init__(
-            self,
-            arch="ViT-H-14",
-            version="laion2b_s32b_b79k",
-            device="cuda",
-            max_length=77,
-            freeze=True,
-            layer="last",
-            use_fp16=False,
+        self,
+        arch="ViT-H-14",
+        version="laion2b_s32b_b79k",
+        device="cuda",
+        max_length=77,
+        freeze=True,
+        layer="last",
+        use_fp16=False,
     ):
         super().__init__()
         assert layer in self.LAYERS
@@ -384,7 +384,7 @@ class FrozenMegatronCLIPEmbedder(AbstractEncoder):
             legacy=legacy,
         )
 
-        _, self.text_transform = get_preprocess_fns(cfg, self.tokenizer, is_train=False, )
+        _, self.text_transform = get_preprocess_fns(cfg, self.tokenizer, is_train=False,)
         self.max_length = cfg.text.get("max_position_embeddings")
 
     def load_model(self, cfg, state_dict):
