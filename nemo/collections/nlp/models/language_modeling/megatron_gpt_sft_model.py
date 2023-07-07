734b5df285de2fa527434e176624f91b8657e5cb
hsiehjackson <c2hsieh@ucsd.edu>
2023-07-07 14:00:33 -0700

    Fix pad
    Signed-off-by: hsiehjackson <c2hsieh@ucsd.edu>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_sft_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_sft_model.py
@@ -263,7 +263,7 @@ class MegatronGPTSFTModel(MegatronGPTModel):
                 ),
                 answer_only_loss=self.cfg.get('answer_only_loss', True),
                 truncation_field=data_cfg.get('truncation_field', 'context'),
-                pad_to_max_length=False,
+                pad_to_max_length=data_cfg.get('pad_to_max_length', False),
                 index_mapping_dir=data_cfg.get('index_mapping_dir', None),
                 prompt_template=data_cfg.get('prompt_template', None),
                 virtual_tokens=self.virtual_tokens,
