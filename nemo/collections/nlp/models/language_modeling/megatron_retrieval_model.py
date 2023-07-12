Precision fix and validation/test_step_outputs
1) Add fix to account for 16-mixed and bf16-mixed in megatron_retro_mutransfer_pretrain.py, megatron_retro_pretraining.py
2) Reset ckpt_path for test in enc_dec_nmt.py
3) Remove outputs args and add validation/test_step_outputs in megatron_retrieval_model.py
4) Comment Megatron Bert Pretraining and Resume Training with Pipeline Paralleism and add back NMT Training Post-LN

Signed-off-by: Abhishree <abhishreetm@gmail.com>

nemo/collections/nlp/models/language_modeling/megatron_retrieval_model.py
@@ -310,6 +310,7 @@ class MegatronRetrievalModel(MegatronBaseModel, TextGeneration):
         return lm_loss
 
     def validation_step(self, batch, batch_idx):
+        prefix = "test" if self.trainer.testing else "val"
         input_tokens_id = batch['tokens']
         input_attn_mask = batch['tokens_mask']
         loss_mask = batch['loss_mask']
@@ -331,26 +332,32 @@ class MegatronRetrievalModel(MegatronBaseModel, TextGeneration):
         loss_mask = loss_mask.float()
         lm_loss = torch.sum(loss.view(-1) * loss_mask.reshape(-1)) / loss_mask.sum()
         reduced_loss = average_losses_across_data_parallel_group([lm_loss])
+        if prefix == 'val':
+            self.validation_step_outputs.append(reduced_loss)
+        else:
+            self.test_step_outputs.apped(reduced_loss)
         return reduced_loss
 
-    def on_validation_epoch_end(self, outputs):
-        if len(outputs) == 0:
+    def on_validation_epoch_end(self):
+        if len(self.validation_step_outputs) == 0:
             return
-        averaged_loss = torch.stack(outputs).mean()
+        averaged_loss = torch.stack(self.validation_step_outputs).mean()
         self.log('val_loss', averaged_loss, prog_bar=True, batch_size=1)
         # formula to compute the perplexity
         # https://towardsdatascience.com/the-relationship-between-perplexity-and-entropy-in-nlp-f81888775ccc
         self.log('perplexity', torch.exp(averaged_loss), prog_bar=True, batch_size=1)
+        self.validation_step_outputs.clear() #free memory
         return averaged_loss
 
     def test_step(self, batch, batch_idx):
         return self.validation_step(batch, batch_idx)
 
-    def on_test_epoch_end(self, outputs):
-        averaged_loss = torch.stack(outputs).mean()
+    def on_test_epoch_end(self):
+        averaged_loss = torch.stack(self.test_step_outputs).mean()
         self.log('test_loss', averaged_loss, prog_bar=True, batch_size=1)
         logging.info(f'test_loss: {averaged_loss} ')
         self.log('perplexity', torch.exp(averaged_loss), prog_bar=True, batch_size=1)
+        self.test_step_outputs.clear() #free memory
         return averaged_loss
 
     def build_train_valid_test_datasets(self):