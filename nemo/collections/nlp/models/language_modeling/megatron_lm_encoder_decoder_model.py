Precision fix in megatron_nmt_training.py for 16-mixed, bf16-mixed
Signed-off-by: Abhishree <abhishreetm@gmail.com>

nemo/collections/nlp/models/language_modeling/megatron_lm_encoder_decoder_model.py
@@ -128,11 +128,11 @@ class MegatronLMEncoderDecoderModel(MegatronBaseModel):
 
         # Check for first few chars to be '16', '32' or 'bf16' as Precision can also be '16-mixed', '32-true' or 'bf16-mixed' 
         # along with 16, 32 or bf16 in case of PTL >= 2.0 
-        if self.cfg.precision[0:4] == 'bf16':
+        if str(self.cfg.precision)[0:4] == 'bf16':
             self.autocast_dtype = torch.bfloat16
-        elif self.cfg.precision[0:2] == '32':
+        elif str(self.cfg.precision)[0:2] == '32':
             self.autocast_dtype = torch.float
-        elif self.cfg.precision[0:2] == '16':
+        elif str(self.cfg.precision)[0:2] == '16':
             self.autocast_dtype = torch.half
         else:
             raise ValueError('precision must be in [32, "32-true", 16, "16-mixed", "bf16", "bf16-mixed"]')