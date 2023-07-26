b7d114d73e2110c64a7cd80c2757916fd70612fd
hsiehjackson <c2hsieh@ucsd.edu>
2023-07-25 19:10:50 -0700

    Fix mask
    Signed-off-by: hsiehjackson <c2hsieh@ucsd.edu>

--- nemo/collections/nlp/modules/common/megatron/attention.py
+++ nemo/collections/nlp/modules/common/megatron/attention.py
@@ -938,6 +938,11 @@ class CoreAttention(MegatronModule):
         attention_probs = self.scale_mask_softmax(attention_scores, attention_mask)
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
+        
+        if attention_mask is not None:
+            all_k_masked = attention_mask.all(axis=-1).squeeze(0)
+            zero_attention_mask = (1.0 - all_k_masked.type(attention_probs.type()))[:, :, :, None]
+            attention_probs = attention_probs * zero_attention_mask
 
         if not self.sequence_parallel:
             with tensor_parallel.random.get_cuda_rng_tracker().fork():
@@ -947,11 +952,7 @@ class CoreAttention(MegatronModule):
 
         # change view [b * np, sq, sk]
         attention_probs = rearrange(attention_probs, 'b np sq sk -> (b np) sq sk')
-        
-        if attention_mask is not None:
-            all_k_masked = attention_mask.all(axis=-1)
-            zero_attention_mask = (1.0 - all_k_masked.type(attention_probs.type()))[:, :, :, None]
-            attention_probs = attention_probs * zero_attention_mask
+
 
         # matmul: [b * np, sq, hn]
         context_layer = torch.bmm(attention_probs, value_layer)
