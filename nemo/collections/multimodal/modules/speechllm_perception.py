5749ea82478a74fd9d32c8363002371fd836c6b1
Zhehuai Chen <chenzhehuai.sjtu@aispeech.com>
2023-07-27 13:21:57 -0700

    add initial impl of ModularizedSpeechGPTModel and integration test

---
+++ nemo/collections/multimodal/modules/speechllm_perception.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections import OrderedDict
+
+import torch
+import torch.distributed
+import torch.nn as nn
+
+from typing import Optional, Dict, Any
+from nemo.core.classes.common import typecheck
+from nemo.core.classes.exportable import Exportable
+from nemo.core.classes.module import NeuralModule
+from nemo.core.neural_types import (
+    AcousticEncodedRepresentation,
+    AudioSignal,
+    LengthsType,
+    NeuralType,
+    SpectrogramType,
+)
+
+__all__ = ["AudioPerceptionModel"]
+
+
+class AudioPerceptionModel(NeuralModule, Exportable):
+    """Audio perception model with basic matcher (some fc layers)."""
+
+    def input_example(
+        self, max_batch: int = 8, max_dim: int = 32000, min_length: int = 200
+    ):
+        batch_size = torch.randint(low=1, high=max_batch, size=[1]).item()
+        max_length = torch.randint(low=min_length, high=max_dim, size=[1]).item()
+        signals = torch.rand(size=[batch_size, max_length]) * 2 - 1
+        lengths = torch.randint(low=min_length, high=max_dim, size=[batch_size])
+        lengths[0] = max_length
+        return signals, lengths, None, None
+
+    @property
+    def input_types(self):
+        """Returns definitions of module input ports."""
+        return OrderedDict(
+            {
+                "input_signal": NeuralType(
+                    ("B", "T"), AudioSignal(freq=self.preprocessor._sample_rate)
+                ),
+                "input_signal_length": NeuralType(
+                    tuple("B"), LengthsType()
+                ),  # Please note that length should be in samples not seconds.
+                "processed_signal": NeuralType(("B", "D", "T"), SpectrogramType()),
+                "processed_signal_length": NeuralType(tuple("B"), LengthsType()),
+            }
+        )
+
+    @property
+    def output_types(self):
+        """Returns definitions of module output ports."""
+        return OrderedDict(
+            {
+                "encoded": NeuralType(("B", "D", "T"), AcousticEncodedRepresentation()),
+                "encoded_len": NeuralType(tuple("B"), LengthsType()),
+            }
+        )
+
+    def __init__(
+        self,
+        preprocessor: Dict[str, Any],
+        encoder: Dict[str, Any],
+        matcher: Dict[str, Any],
+        d_model: int,
+        spec_augment: Optional[Dict[str, Any]] = None,
+        freeze_encoder: bool = False,
+    ):
+        super().__init__()
+        # Initialize components
+        self.preprocessor = preprocessor
+        self.encoder = encoder
+        self.spec_augmentation = spec_augment
+        self.matcher = matcher
+        if freeze_encoder:
+            for params in self.encoder.parameters():
+                params.requires_grad = False
+
+    def maybe_preprocess_audio(
+        self,
+        input_signal=None,
+        input_signal_length=None,
+        processed_signal=None,
+        processed_signal_length=None,
+    ):
+        has_input_signal = input_signal is not None and input_signal_length is not None
+        has_processed_signal = (
+            processed_signal is not None and processed_signal_length is not None
+        )
+        if (has_input_signal ^ has_processed_signal) is False:
+            raise ValueError(
+                f"{self} Arguments ``input_signal`` and ``input_signal_length`` are mutually exclusive "
+                " with ``processed_signal`` and ``processed_signal_len`` arguments."
+            )
+
+        if not has_processed_signal:
+            processed_signal, processed_signal_length = self.preprocessor(
+                input_signal=input_signal,
+                length=input_signal_length,
+            )
+        return processed_signal, processed_signal_length
+
+    @typecheck()
+    def forward(
+        self,
+        input_signal=None,
+        input_signal_length=None,
+        processed_signal=None,
+        processed_signal_length=None,
+    ):
+        processed_signal, processed_signal_length = self.maybe_preprocess_audio(
+            input_signal, input_signal_length, processed_signal, processed_signal_length
+        )
+
+        # Spec augment is not applied during evaluation/testing
+        if self.spec_augmentation is not None and self.training:
+            processed_signal = self.spec_augmentation(
+                input_spec=processed_signal, length=processed_signal_length
+            )
+
+        encoded, encoded_len = self.encoder(
+            audio_signal=processed_signal, length=processed_signal_length
+        )
+        encoded, encoded_len = self.matcher(
+            audio_signal=processed_signal, length=processed_signal_length
+        )
+
+        return encoded, encoded_len
