46479c6127a4075d462c940d700326f109a530ab
xren <xren@nvidia.com>
2023-08-03 16:00:32 -0700

    typo fix
    Signed-off-by: xren <xren@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/language_model.py
+++ nemo/collections/nlp/modules/common/megatron/language_model.py
@@ -730,7 +730,7 @@ class TransformerLanguageModel(MegatronModule, adapter_mixins.AdapterModuleMixin
 
         self.encoder.set_input_tensor(input_tensor[0])
 
-    def get_position_embedding_on_this_context_parallel_rank(self, position_embedding, seq_dim)
+    def get_position_embedding_on_this_context_parallel_rank(self, position_embedding, seq_dim):
         cp_size = parallel_state.get_context_parallel_world_size()
         cp_rank = parallel_state.get_context_parallel_rank()
         cp_idx = torch.tensor([cp_rank, (2*cp_size-cp_rank-1)], device=position_embedding.device)
