d8c623c54d5c573ef0faa3815e668dfdfd986bba
Mingyuan Ma <mingyuanm@nvidia.com>
2023-08-07 15:07:59 -0700

    Add example training script.

---
+++ examples/multimodal/generative/stable_diffusion/sd_xl_train.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from datetime import timedelta
+
+import torch
+from omegaconf.omegaconf import OmegaConf, open_dict, DictConfig
+from pytorch_lightning import Trainer
+from pytorch_lightning.plugins.environments import TorchElasticEnvironment
+from pytorch_lightning.strategies.ddp import DDPStrategy
+from pytorch_lightning.trainer.connectors.checkpoint_connector import CheckpointConnector
+
+from nemo.collections.multimodal.models.stable_diffusion.diffusion_engine import MegatronDiffusionEngine
+from nemo.collections.nlp.parts.nlp_overrides import (
+    GradScaler,
+    MegatronHalfPrecisionPlugin,
+    NLPDDPStrategy,
+    PipelineMixedPrecisionPlugin,
+)
+from nemo.core.config import hydra_runner
+from nemo.utils import logging
+from nemo.utils.exp_manager import exp_manager
+
+
+@hydra_runner(config_path='conf', config_name='sd_xl_base_train')
+def main(cfg) -> None:
+    logging.info("\n\n************** Experiment configuration ***********")
+    logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
+
+    megatron_amp_O2 = cfg.model.get('megatron_amp_O2', False)
+    with_distributed_adam = cfg.model.optim.get('name') == 'distributed_fused_adam'
+
+    torch.backends.cuda.matmul.allow_tf32 = True
+
+    if cfg.model.capture_cudagraph_iters >= 0:
+        # Required by CUDA graph with DDP
+        os.environ["NCCL_ASYNC_ERROR_HANDLING"] = "0"
+
+        # Hack to avoid CUDA graph issue with AMP, PyTorch Lightning doesn't support
+        # changing autocast arguments for now.
+        # https://github.com/pytorch/pytorch/blob/v1.13.1/torch/cuda/graphs.py#L234
+        def amp_autocast_init(self, *args, **kwargs):
+            if "cache_enabled" not in kwargs:
+                kwargs["cache_enabled"] = False
+            return self.__orig_init__(*args, **kwargs)
+
+        torch.cuda.amp.autocast.__orig_init__ = torch.cuda.amp.autocast.__init__
+        torch.cuda.amp.autocast.__init__ = amp_autocast_init
+        torch.autocast.__orig_init__ = torch.autocast.__init__
+        torch.autocast.__init__ = amp_autocast_init
+
+    plugins = []
+
+    strategy = NLPDDPStrategy(
+        no_ddp_communication_hook=True,  # we don't use DDP for async grad allreduce
+        gradient_as_bucket_view=cfg.model.gradient_as_bucket_view,
+        find_unused_parameters=False,
+    )
+
+    if cfg.trainer.precision in [16, 'bf16']:
+        scaler = None
+        if cfg.trainer.precision == 16:
+            scaler = GradScaler(
+                init_scale=cfg.model.get('native_amp_init_scale', 65536.0),
+                growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
+                hysteresis=cfg.model.get('hysteresis', 2),
+            )
+        if megatron_amp_O2 and not with_distributed_adam:
+            plugins.append(MegatronHalfPrecisionPlugin(precision=cfg.trainer.precision, device='cuda', scaler=scaler))
+        else:
+            plugins.append(PipelineMixedPrecisionPlugin(precision=cfg.trainer.precision, device='cuda', scaler=scaler))
+
+    if cfg.get('cluster_type', None) == 'BCP':
+        plugins.append(TorchElasticEnvironment())
+
+    callbacks = []
+    trainer = Trainer(plugins=plugins, strategy=strategy, callbacks=callbacks, **cfg.trainer)
+
+    exp_manager(trainer, cfg.exp_manager)
+    # update resume from checkpoint found by exp_manager
+    if cfg.model.get("resume_from_checkpoint") is not None:
+        resume_from_checkpoint = cfg.model.resume_from_checkpoint
+    else:
+        resume_from_checkpoint = trainer._checkpoint_connector.resume_from_checkpoint_fit_path
+
+    logging.info(f'Resuming training from checkpoint: {resume_from_checkpoint}')
+
+    trainer._checkpoint_connector = CheckpointConnector(trainer, resume_from_checkpoint=resume_from_checkpoint)
+
+    # hydra interpolation does not work here as the interpolation key is lost when PTL saves hparams
+    with open_dict(cfg):
+        cfg.model.precision = cfg.trainer.precision
+
+
+    model = MegatronDiffusionEngine(cfg.model, trainer)
+
+    trainer.fit(model)
+
+
+if __name__ == '__main__':
+    main()
