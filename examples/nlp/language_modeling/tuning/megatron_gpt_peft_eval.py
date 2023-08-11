9935851de0230f1ff119057dfee3c2381fb2e1f6
Cheng-Ping Hsieh <chsieh@nvidia.com>
2023-08-11 11:56:52 -0700

    Fix config
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>

--- examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
@@ -172,7 +172,7 @@ def main(cfg) -> None:
     )
 
     model.freeze()
-    if not cfg.model.use_flash_attention:
+    if not cfg.model.get('use_flash_attention', False):
         cfg.inference.compute_attention_mask = True
     config = OmegaConf.to_container(cfg.inference, resolve=True)
     model.set_inference_config(config)
