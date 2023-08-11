be2a857891321c4edc1fad424ec6f18eb5ddd34a
Cheng-Ping Hsieh <chsieh@nvidia.com>
2023-08-11 10:06:15 -0700

    Fix attention mask inference
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>

--- examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
@@ -172,6 +172,8 @@ def main(cfg) -> None:
     )
 
     model.freeze()
+    if not cfg.model.use_flash_attention:
+        cfg.inference.compute_attention_mask = True
     config = OmegaConf.to_container(cfg.inference, resolve=True)
     model.set_inference_config(config)
 
