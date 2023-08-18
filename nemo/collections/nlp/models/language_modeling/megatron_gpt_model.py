b51d6f6430cb2c7614ffc6b82498166149dcaf0d
Jason Wang <jasonwan@nvidia.com>
2023-08-18 13:56:40 -0700

    reset and restore transformer config sequence parallel
    Signed-off-by: Jason Wang <jasonwan@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -1348,6 +1348,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
 
         # Reset config values. Needed for calling generate.
         self.cfg.sequence_parallel = False
+        self.transformer_config.sequence_parallel = False
 
         # Reset model parameters.
         for module in self.get_gpt_module_list():
@@ -1362,6 +1363,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         """
         # Restore config values.
         self.cfg.sequence_parallel = self.last_sequence_parallel
+        self.transformer_config.sequence_parallel = self.last_sequence_parallel
 
         # Restore model parameters.
         for module in self.get_gpt_module_list():
