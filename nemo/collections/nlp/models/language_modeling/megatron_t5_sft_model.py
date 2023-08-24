5c2ab84ff0e79523d2e74d2a4a37265a854bb2d3
Chen Cui <chcui@nvidia.com>
2023-08-24 12:40:39 -0400

    Various small changes to make T5 SFT work like GPT SFT

--- nemo/collections/nlp/models/language_modeling/megatron_t5_sft_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_t5_sft_model.py
@@ -26,6 +26,7 @@ from nemo.collections.nlp.data.common.sequence_to_sequence_dataset import Sequen
 from nemo.collections.nlp.data.language_modeling.megatron.t5_sft_dataset import T5SFTDataset
 from nemo.collections.nlp.models.language_modeling.megatron_t5_model import MegatronT5Model, T5Sentinel
 from nemo.collections.nlp.modules.common.megatron.utils import get_iterator_k_split
+from nemo.collections.nlp.parts.mixins.nlp_adapter_mixins import NLPAdapterModelMixin
 from nemo.collections.nlp.parts.utils_funcs import get_last_rank
 from nemo.utils import AppState, logging
 
@@ -54,13 +55,12 @@ except (ImportError, ModuleNotFoundError):
 __all__ = ['MegatronT5SFTModel']
 
 
-class MegatronT5SFTModel(MegatronT5Model):
-    """Finetune Model that Inherits from MegatronT5Model instead."""
-
+class MegatronT5SFTModel(MegatronT5Model, NLPAdapterModelMixin):
+    """ T5 Finetuning model in the same format as MegatronGPTSFTModel """
     def __init__(self, cfg: DictConfig, trainer: Trainer):
         super().__init__(cfg, trainer=trainer)
         self.val_metric, self.val_metric_name = self.setup_metric(self.cfg.data.validation_ds)
-        self.val_metric = torch.nn.ModuleList(self.val_metric)
+        self.val_metric = torch.nn.ModuleList(self.val_metric) if self.val_metric is not None else None
         if hasattr(self.cfg.data, "test_ds"):
             self.test_metric, self.test_metric_name = self.setup_metric(self.cfg.data.test_ds)
             self.test_metric = torch.nn.ModuleList(self.test_metric)
@@ -72,10 +72,12 @@ class MegatronT5SFTModel(MegatronT5Model):
             metric = [ExactStringPerCategoryMatchMetric(self.cfg.eval_languages)]
         else:
             if not hasattr(data_cfg, "metric"):
-                metric = MetricStringToTorchMetric["exact_string_match"]
+                metric_class = MetricStringToTorchMetric["exact_string_match"]
             else:
                 if not hasattr(data_cfg.metric, "name"):
                     raise ValueError("Metric name is not provided in the metric config.")
+                if data_cfg.metric.name == "loss":
+                    return None, "loss"
                 if data_cfg.metric.name not in MetricStringToTorchMetric:
                     raise KeyError(
                         f"{data_cfg.metric.name} is not supported. List of supported metrics: {MetricStringToTorchMetric.keys()}"
@@ -105,9 +107,8 @@ class MegatronT5SFTModel(MegatronT5Model):
                         raise ValueError(
                             f"Number of class labels {len(data_cfg.metric.get('class_labels', None))} does not match `num_classes` : {data_cfg.metric.num_classes}"
                         )
-
-            metric_name = data_cfg.metric.name
-            metric_class = MetricStringToTorchMetric[metric_name]
+                metric_name = data_cfg.metric.name
+                metric_class = MetricStringToTorchMetric[metric_name]
 
             # GLUE will not have a "src_file_name" attribute and will always have only a single metric.
             if hasattr(data_cfg, "src_file_name") or hasattr(data_cfg, "file_names"):
@@ -330,21 +331,22 @@ class MegatronT5SFTModel(MegatronT5Model):
             else:
                 categories = batch['lang']
 
-            metric = self.val_metric[dataloader_idx] if mode == 'validation' else self.test_metric[dataloader_idx]
-            assert len(categories) == len(preds_text) == len(labels_text)
-            for _, (pred, label, category) in enumerate(zip(preds_text, labels_text, categories)):
-                # To compute metrics like pearson or spearman correlation, we need to cast the predicted string and labels to floats.
-                pred, label = self.cast_for_metric(
-                    pred=pred,
-                    label=label,
-                    metric_name=self.val_metric_name if mode == 'validation' else self.test_metric_name,
-                    class_labels=data_cfg.metric.get('class_labels', None),
-                    labels_are_strings=data_cfg.metric.get('labels_are_strings', False),
-                )
-                if batch_has_lang_information:
-                    _ = metric(pred, label, category)
-                else:
-                    _ = metric(pred, label)
+            if self.val_metric is not None:
+                metric = self.val_metric[dataloader_idx] if mode == 'validation' else self.test_metric[dataloader_idx]
+                assert len(categories) == len(preds_text) == len(labels_text)
+                for _, (pred, label, category) in enumerate(zip(preds_text, labels_text, categories)):
+                    # To compute metrics like pearson or spearman correlation, we need to cast the predicted string and labels to floats.
+                    pred, label = self.cast_for_metric(
+                        pred=pred,
+                        label=label,
+                        metric_name=self.val_metric_name if mode == 'validation' else self.test_metric_name,
+                        class_labels=data_cfg.metric.get('class_labels', None),
+                        labels_are_strings=data_cfg.metric.get('labels_are_strings', False),
+                    )
+                    if batch_has_lang_information:
+                        _ = metric(pred, label, category)
+                    else:
+                        _ = metric(pred, label)
 
             outputs = {
                 'preds': preds_text,
@@ -436,40 +438,40 @@ class MegatronT5SFTModel(MegatronT5Model):
             torch.distributed.broadcast(loss, get_last_rank())
             self.log('val_loss', loss, prog_bar=True, rank_zero_only=True, batch_size=1)
             self.log('global_step', self.trainer.global_step, prog_bar=True, rank_zero_only=True, batch_size=1)
+            averaged_loss.append(loss)
 
             # Determine the key used to log the loss based on the user provided name of the dataset or the dataloader index.
             loss_log_key = self._determine_log_key(data_cfg, dataloader_idx, "loss", mode)
-            # Determine the key used to log the eval metric based on the user provided name of the dataset or the dataloader index.
-            metric_log_key = self._determine_log_key(data_cfg, dataloader_idx, metric_name, mode)
-            self.log(loss_log_key, loss, batch_size=1)
-            metric_object = (
-                self.val_metric[dataloader_idx] if mode == 'validation' else self.test_metric[dataloader_idx]
-            )
-            metric = metric_object.compute()
-            if metric_name == 'rouge':
-                metric = metric['rouge1_fmeasure']
-            # Handle logging of GLUE/XNLI separately here. XNLI has a separate metric per language.
-            if isinstance(metric, dict):
-                # GLUE case:
-                if len(metric) == 1 and 'acc' in metric:
-                    metric = metric['acc']
-                    self.log(metric_log_key, metric, batch_size=1)
-                    logging.info(f"{mode} {metric_name}: {metric}")
-                # XNLI case where the metric dictionary contains the language and the computed metric as values.
-                else:
-                    for k, v in metric.items():
-                        if k != 'acc' and 'total' not in k:
-                            self.log(metric_log_key + f'_{k}', v, batch_size=1)
-                            logging.info(f"{mode} {metric_name} lang {k} : {v}")
-                    if metric_name != 'rouge':
+            if metric_name != 'loss':
+                # Determine the key used to log the eval metric based on the user provided name of the dataset or the dataloader index.
+                metric_log_key = self._determine_log_key(data_cfg, dataloader_idx, metric_name, mode)
+                self.log(loss_log_key, loss, batch_size=1)
+                metric_object = (
+                    self.val_metric[dataloader_idx] if mode == 'validation' else self.test_metric[dataloader_idx]
+                )
+                metric = metric_object.compute()
+                if metric_name == 'rouge':
+                    metric = metric['rouge1_fmeasure']
+                # Handle logging of GLUE/XNLI separately here. XNLI has a separate metric per language.
+                if isinstance(metric, dict):
+                    # GLUE case:
+                    if len(metric) == 1 and 'acc' in metric:
                         metric = metric['acc']
-            else:
-                self.log(metric_log_key, metric, batch_size=1)
-                logging.info(f"{metric_log_key}: {metric}")
-            metric_object.reset()
-
-            averaged_loss.append(loss)
-            averaged_metric.append(metric)
+                        self.log(metric_log_key, metric, batch_size=1)
+                        logging.info(f"{mode} {metric_name}: {metric}")
+                    # XNLI case where the metric dictionary contains the language and the computed metric as values.
+                    else:
+                        for k, v in metric.items():
+                            if k != 'acc' and 'total' not in k:
+                                self.log(metric_log_key + f'_{k}', v, batch_size=1)
+                                logging.info(f"{mode} {metric_name} lang {k} : {v}")
+                        if metric_name != 'rouge':
+                            metric = metric['acc']
+                else:
+                    self.log(metric_log_key, metric, batch_size=1)
+                    logging.info(f"{metric_log_key}: {metric}")
+                metric_object.reset()
+                averaged_metric.append(metric)
 
             # Write predictions, labels, and inputs to a file for each validation/test dataset.
             if data_cfg.get("write_predictions_to_file", False):
@@ -480,7 +482,7 @@ class MegatronT5SFTModel(MegatronT5Model):
                         f"Cannot write predictions to file when output_file_path_prefix is not set or present in the yaml config file."
                     )
 
-                # Gather the outputs object from all data parallel ranks since we are using the DistributedSampler which splits data across DDP ranks.
+                # Gather the outputs object from all data parallel ranks since we are using the DistributedSampler which splits data across DDPDDP ranks.
                 gathered_outputs = [None for _ in range(parallel_state.get_data_parallel_world_size())]
                 torch.distributed.all_gather_object(
                     gathered_outputs,
@@ -528,10 +530,10 @@ class MegatronT5SFTModel(MegatronT5Model):
 
         # Logging of the averaged metrics:
         averaged_loss = sum(averaged_loss) / len(averaged_loss)
-        averaged_metric = sum(averaged_metric) / len(averaged_metric)
+        averaged_metric = sum(averaged_metric) / len(averaged_metric)  if len(averaged_metric) > 1 else None
 
         # Handle case where metrics can be nan or inf. This can break checkpoint save/load.
-        if torch.isinf(averaged_metric) or torch.isnan(averaged_metric):
+        if averaged_metric is not None and (torch.isinf(averaged_metric) or torch.isnan(averaged_metric)):
             app_state = AppState()
             monitor_mode = app_state.checkpoint_callback_params.mode
             assert monitor_mode in ['min', 'max']
@@ -539,10 +541,12 @@ class MegatronT5SFTModel(MegatronT5Model):
 
         if mode == 'validation':
             self.log("validation_loss", averaged_loss, batch_size=1)
-            self.log(f"validation_{self.val_metric_name}", averaged_metric, batch_size=1)
+            if averaged_metric is not None:
+                self.log(f"validation_{self.val_metric_name}", averaged_metric, batch_size=1)
         elif mode == 'test':
             self.log("test_loss", averaged_loss, batch_size=1)
-            self.log(f"test_{self.test_metric_name}", averaged_metric, batch_size=1)
+            if averaged_metric is not None:
+                self.log(f"test_{self.test_metric_name}", averaged_metric, batch_size=1)
 
         app_state = AppState()
         if hasattr(self, "_train_ds"):
@@ -694,11 +698,10 @@ class MegatronT5SFTModel(MegatronT5Model):
                     file_path=file_path,
                     src_tokenizer=self.tokenizer,
                     tgt_tokenizer=self.tokenizer,
-                    max_src_seq_length=data_cfg.max_src_seq_length,
-                    max_tgt_seq_length=data_cfg.max_tgt_seq_length,
-                    add_bos_to_input=data_cfg.get('add_bos_to_input', True),
-                    add_eos_to_input=data_cfg.get('add_eos_to_input', True),
-                    replace_bos_with_pad=data_cfg.get('replace_bos_with_pad', False),
+                    max_src_seq_length=data_cfg.max_seq_length,
+                    max_tgt_seq_length=data_cfg.max_seq_length,
+                    add_bos_to_input=data_cfg.get('add_bos', True),
+                    add_eos_to_input=data_cfg.get('add_eos', True),  # review: need domain knowledge to undertand if these args are ok
                     index_mapping_dir=data_cfg.get('index_mapping_dir', None),
                     memmap_workers=data_cfg.get('memmap_workers', None),
                     hf_dataset=data_cfg.get('hf_dataset', False),
@@ -727,41 +730,58 @@ class MegatronT5SFTModel(MegatronT5Model):
                 f'You are trying to use "implicit gradient accumulation" of {data_cfg.global_batch_size // (data_cfg.micro_batch_size * parallel_state.get_data_parallel_world_size())} in your validation/test datasets. This is not supported. Please set global_batch_size equal to micro_batch_size * data_parallel_world_size.'
             )
         datasets = []
-        # Determine if we are using a single dataset or a list of datasets.
-        is_src_list_config = isinstance(data_cfg.src_file_name, ListConfig)
-        is_tgt_list_config = isinstance(data_cfg.tgt_file_name, ListConfig)
-        is_names_list_config = False
-        if hasattr(data_cfg, "names"):
-            if isinstance(data_cfg.names, ListConfig):
-                is_names_list_config = True
+        if hasattr(data_cfg, "src_file_name") and hasattr(data_cfg, "tgt_file_name"):
+            # Determine if we are using a single dataset or a list of datasets.
+            is_src_list_config = isinstance(data_cfg.src_file_name, ListConfig)
+            is_tgt_list_config = isinstance(data_cfg.tgt_file_name, ListConfig)
+            is_names_list_config = False
+            if hasattr(data_cfg, "names"):
+                if isinstance(data_cfg.names, ListConfig):
+                    is_names_list_config = True
 
-        if (is_src_list_config and not is_tgt_list_config) or (is_tgt_list_config and not is_src_list_config):
-            raise ValueError("src_list and tgt_list must both be either a ListConfig or a string. ")
-        if is_src_list_config:
-            if len(data_cfg.src_file_name) != len(data_cfg.tgt_file_name):
-                raise ValueError("src_file_name and tgt_file_name must have the same number of elements. ")
-            if is_names_list_config and len(data_cfg.names) != len(data_cfg.src_file_name):
-                raise ValueError(
-                    "If you are providing names for each src/tgt file, they must have the same number of elements."
+            if (is_src_list_config and not is_tgt_list_config) or (is_tgt_list_config and not is_src_list_config):
+                raise ValueError("src_list and tgt_list must both be either a ListConfig or a string. ")
+            if is_src_list_config:
+                if len(data_cfg.src_file_name) != len(data_cfg.tgt_file_name):
+                    raise ValueError("src_file_name and tgt_file_name must have the same number of elements. ")
+                if is_names_list_config and len(data_cfg.names) != len(data_cfg.src_file_name):
+                    raise ValueError(
+                        "If you are providing names for each src/tgt file, they must have the same number of elements."
+                    )
+            else:
+                data_cfg.src_file_name = [data_cfg.src_file_name]
+                data_cfg.tgt_file_name = [data_cfg.tgt_file_name]
+
+            for src, tgt in zip(data_cfg.src_file_name, data_cfg.tgt_file_name):
+                dataset = SequenceToSequenceDataset(
+                    src_file_name=src,
+                    tgt_file_name=tgt,
+                    src_tokenizer=self.tokenizer,
+                    tgt_tokenizer=self.tokenizer,
+                    max_src_seq_length=data_cfg.max_src_seq_length,
+                    max_tgt_seq_length=data_cfg.max_tgt_seq_length,
+                    add_bos_to_input=data_cfg.get('add_bos_to_input', True),
+                    add_eos_to_input=data_cfg.get('add_eos_to_input', True),
+                    replace_bos_with_pad=data_cfg.get('replace_bos_with_pad', False),
                 )
+                datasets.append(dataset)
+        elif hasattr(data_cfg, "file_names"):
+            for file_path in data_cfg.file_names:
+                dataset = T5SFTDataset(
+                    file_path=file_path,
+                    src_tokenizer=self.tokenizer,
+                    tgt_tokenizer=self.tokenizer,
+                    max_src_seq_length=data_cfg.max_seq_length,
+                    max_tgt_seq_length=data_cfg.max_seq_length,
+                    add_bos_to_input=data_cfg.get('add_bos', True),
+                    add_eos_to_input=data_cfg.get('add_eos', True),
+                    index_mapping_dir=data_cfg.get('index_mapping_dir', None),
+                    memmap_workers=data_cfg.get('memmap_workers', None),
+                    hf_dataset=data_cfg.get('hf_dataset', False),
+                )
+                datasets.append(dataset)
         else:
-            data_cfg.src_file_name = [data_cfg.src_file_name]
-            data_cfg.tgt_file_name = [data_cfg.tgt_file_name]
-
-        for src, tgt in zip(data_cfg.src_file_name, data_cfg.tgt_file_name):
-            dataset = SequenceToSequenceDataset(
-                src_file_name=src,
-                tgt_file_name=tgt,
-                src_tokenizer=self.tokenizer,
-                tgt_tokenizer=self.tokenizer,
-                max_src_seq_length=data_cfg.max_src_seq_length,
-                max_tgt_seq_length=data_cfg.max_tgt_seq_length,
-                add_bos_to_input=data_cfg.get('add_bos_to_input', True),
-                add_eos_to_input=data_cfg.get('add_eos_to_input', True),
-                replace_bos_with_pad=data_cfg.get('replace_bos_with_pad', False),
-            )
-            datasets.append(dataset)
-
+            raise ValueError("You must specify either (src_file_name and tgt_file_name) or file_names in data config")
         return datasets
 
     def build_train_valid_test_datasets(self, stage):
