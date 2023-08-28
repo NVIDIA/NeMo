cfd11053a61716bb85b320b4bd1663572fab6c5b
Chen Cui <chcui@nvidia.com>
2023-08-28 11:18:26 -0700

    Make test script GPT/T5 agnostic

--- examples/nlp/language_modeling/tuning/test_gpt_t5_adapter.py
+++ examples/nlp/language_modeling/tuning/test_gpt_t5_adapter.py
@@ -9,6 +9,7 @@ from pytorch_lightning.trainer.connectors.checkpoint_connector import _Checkpoin
 from torch.utils.data import DataLoader, Dataset
 
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTSFTModel
+from nemo.collections.nlp.models.language_modeling.megatron_t5_sft_model import MegatronT5SFTModel
 from nemo.collections.nlp.modules.common.megatron.adapters.parallel_adapters import (
     AdapterName,
     IA3AdapterConfig,
@@ -25,44 +26,44 @@ from nemo.utils.exp_manager import exp_manager
 mp.set_start_method("spawn", force=True)
 
 
-def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
+def _modify_config(base_model_cfg, cfg, add_cfg_to_tree=False):
     """
-    This function modifies the original gpt pre-training config (gpt_cfg) with attributes from the finetuning config (cfg).
+    This function modifies the original gpt/t5 pre-training config (base_model_cfg) with attributes from the finetuning config (cfg).
     The `add_cfg_to_tree` arg adds `cfg` to the top of the yaml tree which is needed for all `hparams.yaml` files when passed as an arg to `load_from_checkpoint()`.
     """
-    OmegaConf.set_struct(gpt_cfg, True)
+    OmegaConf.set_struct(base_model_cfg, True)
     OmegaConf.resolve(cfg)
-    with open_dict(gpt_cfg):
-        gpt_cfg.megatron_amp_O2 = cfg.model.get('megatron_amp_O2', False)
-        gpt_cfg.micro_batch_size = cfg.model.data.train_ds.micro_batch_size
-        gpt_cfg.global_batch_size = cfg.model.data.train_ds.global_batch_size
-        gpt_cfg.sequence_parallel = cfg.model.get("sequence_parallel", False)
-        gpt_cfg.activations_checkpoint_granularity = cfg.model.get("activations_checkpoint_granularity", None)
-        gpt_cfg.activations_checkpoint_num_layers = cfg.model.get("activations_checkpoint_num_layers", None)
-        gpt_cfg.activations_checkpoint_method = cfg.model.get("activations_checkpoint_method", None)
-        gpt_cfg.data = cfg.model.data
-        gpt_cfg.optim = cfg.model.optim
-        gpt_cfg.precision = cfg.trainer.precision
-        gpt_cfg.answer_only_loss = cfg.model.answer_only_loss
-        gpt_cfg.restore_from_path = cfg.model.restore_from_path
-        gpt_cfg.resume_from_checkpoint = cfg.model.resume_from_checkpoint
-        gpt_cfg.save_nemo_on_validation_end = cfg.model.save_nemo_on_validation_end
-        gpt_cfg.gradient_as_bucket_view = cfg.model.gradient_as_bucket_view
-        gpt_cfg.hidden_dropout = cfg.model.get('hidden_dropout', 0.0)
-        gpt_cfg.attention_dropout = cfg.model.get('attention_dropout', 0.0)
-        gpt_cfg.ffn_dropout = cfg.model.ffn_dropout
-        gpt_cfg.activations_checkpoint_layers_per_pipeline = None
-        gpt_cfg.peft = cfg.model.peft
-        peft_cls = MegatronGPTSFTModel
-        gpt_cfg.target = f"{peft_cls.__module__}.{peft_cls.__name__}"
+    with open_dict(base_model_cfg):
+        base_model_cfg.megatron_amp_O2 = cfg.model.get('megatron_amp_O2', False)
+        base_model_cfg.micro_batch_size = cfg.model.data.train_ds.micro_batch_size
+        base_model_cfg.global_batch_size = cfg.model.data.train_ds.global_batch_size
+        base_model_cfg.sequence_parallel = cfg.model.get("sequence_parallel", False)
+        base_model_cfg.activations_checkpoint_granularity = cfg.model.get("activations_checkpoint_granularity", None)
+        base_model_cfg.activations_checkpoint_num_layers = cfg.model.get("activations_checkpoint_num_layers", None)
+        base_model_cfg.activations_checkpoint_method = cfg.model.get("activations_checkpoint_method", None)
+        base_model_cfg.data = cfg.model.data
+        base_model_cfg.optim = cfg.model.optim
+        base_model_cfg.precision = cfg.trainer.precision
+        base_model_cfg.answer_only_loss = cfg.model.answer_only_loss
+        base_model_cfg.restore_from_path = cfg.model.restore_from_path
+        base_model_cfg.resume_from_checkpoint = cfg.model.resume_from_checkpoint
+        base_model_cfg.save_nemo_on_validation_end = cfg.model.save_nemo_on_validation_end
+        base_model_cfg.gradient_as_bucket_view = cfg.model.gradient_as_bucket_view
+        base_model_cfg.hidden_dropout = cfg.model.get('hidden_dropout', 0.0)
+        base_model_cfg.attention_dropout = cfg.model.get('attention_dropout', 0.0)
+        base_model_cfg.ffn_dropout = cfg.model.ffn_dropout
+        base_model_cfg.activations_checkpoint_layers_per_pipeline = None
+        base_model_cfg.peft = cfg.model.peft
+        peft_cls = MegatronT5SFTModel if "megatron_t5" in cfg.name else MegatronGPTSFTModel
+        base_model_cfg.target = f"{peft_cls.__module__}.{peft_cls.__name__}"
 
         # This is needed when modifying a hparam file directly to load `.ckpt` files.
         # This is not needed to modify the cfg in `.nemo` files.
         if add_cfg_to_tree:
-            OmegaConf.resolve(gpt_cfg)
-            gpt_cfg.cfg = gpt_cfg
+            OmegaConf.resolve(base_model_cfg)
+            base_model_cfg.cfg = base_model_cfg
 
-    return gpt_cfg
+    return base_model_cfg
 
 
 def validate_checkpoint_loading_args(cfg):
@@ -140,15 +141,24 @@ def generate_new_ia3_config(cfg):
 
 @hydra_runner(config_path="conf", config_name="megatron_gpt_peft_tuning_config")
 def main(cfg) -> None:
+    # for T5, use config name `megatron_t5_peft_tuning_config`
+    if "megatron_t5" in cfg.name:
+        FTModel = MegatronT5SFTModel
+    elif "megatron_gpt" in cfg.name:
+        FTModel = MegatronGPTSFTModel
+    else:
+        raise NotImplementedError("Cannot infer model from config")
+
     logging.info("\n\n************** Experiment configuration ***********")
     logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
 
     trainer = MegatronTrainerBuilder(cfg).create_trainer()
     exp_manager(trainer, cfg.exp_manager)
-    # update resume from checkpoint found by exp_manager
-    if cfg.model.resume_from_checkpoint is not None:
-        trainer.ckpt_path = cfg.model.resume_from_checkpoint
-    logging.info(f'Resuming training from checkpoint: {trainer.ckpt_path}')
+
+    ##  In this test script, we never resume from checkpoint
+    # if cfg.model.resume_from_checkpoint is not None:
+    #     trainer.ckpt_path = cfg.model.resume_from_checkpoint
+    # logging.info(f'Resuming training from checkpoint: {trainer.ckpt_path}')
 
     trainer._checkpoint_connector = _CheckpointConnector(trainer)
 
@@ -157,33 +167,33 @@ def main(cfg) -> None:
         cfg.model.precision = cfg.trainer.precision
 
     if cfg.model.restore_from_path:
-        base_model_cfg = MegatronGPTSFTModel.restore_from(
+        base_model_cfg = FTModel.restore_from(
             restore_path=cfg.model.restore_from_path, trainer=trainer, return_config=True,
         )
         base_model_cfg = _modify_config(base_model_cfg, cfg, add_cfg_to_tree=False)
-        model = MegatronGPTSFTModel.restore_from(
+        model = FTModel.restore_from(
             restore_path=cfg.model.restore_from_path, trainer=trainer, override_config_path=base_model_cfg,
         )
 
         peft_name_keys, adapter_cfg = generate_new_ia3_config(base_model_cfg)
-        model.add_adapters(peft_name_keys, adapter_cfg)
+        model.add_adapters(peft_name_keys, adapter_cfg, layer_selection=[1, 12])
         trainer.fit(model)
 
         ckpt_dir = os.path.join(trainer._default_root_dir, "checkpoints")
-        adapter_path = os.path.join(ckpt_dir, "gpt-adapter.ckpt")
+        adapter_path = os.path.join(ckpt_dir, "adapter.ckpt")
 
         model.save_adapters(adapter_path)
 
         ckpt_path = os.path.join(ckpt_dir, f"{cfg.name}.nemo")
-        model2 = MegatronGPTSFTModel.restore_from_nemo_with_adapter(
+        model2 = FTModel.restore_from_nemo_with_adapter(
             restore_path=ckpt_path, adapter_names=peft_name_keys, adapter_cfgs=adapter_cfg, trainer=trainer,
         )
         model2_state = str(model2.state_dict())
 
-        model = MegatronGPTSFTModel.restore_from(
+        model = FTModel.restore_from(
             restore_path=cfg.model.restore_from_path, trainer=trainer, override_config_path=base_model_cfg,
         )
-        model.add_adapters(peft_name_keys, adapter_cfg)
+        model.add_adapters(peft_name_keys, adapter_cfg, layer_selection=[1, 12])
 
         print("Compare state before loading adapter:", str(model.state_dict()) == model2_state)
         model.load_adapters(adapter_path)
