fix precision lookup in a few more places
Signed-off-by: Maanu Grover <maanug@nvidia.com>

nemo/collections/nlp/models/language_modeling/megatron_base_model.py
@@ -475,10 +475,10 @@ class MegatronBaseModel(NLPModel):
 
         # Wrap the baseline optimizer with the optimizer class with master parameters
         if self.megatron_amp_o2 and not self.with_distributed_adam and self._optimizer is not None:
-            if self.cfg.precision == 'bf16':
+            if self.cfg.precision in ['bf16', 'bf16-mixed']:
                 fp32_grad_accum = True
                 contiguous_grad_bucket = True
-            elif self.cfg.precision == 16:
+            elif self.cfg.precision == [16, '16', '16-mixed']:
                 fp32_grad_accum = False
                 # TODO: contiguous grad bucket for fp16 is also planned to be supported
                 contiguous_grad_bucket = False
@@ -774,10 +774,8 @@ class MegatronBaseModel(NLPModel):
             "async_tensor_model_parallel_allreduce": self.cfg.get('tensor_model_parallel_world_size', 1) > 1
             and not self.cfg.get('sequence_parallel', False),
             "pipeline_dtype": pipeline_dtype,
-            "grad_scale_func": self.trainer.precision_plugin.scaler.scale
-            if precision in [16, '16', '16-mixed']
-            else None,
-            "enable_autocast": not megatron_amp_O2 and precision in [16, '16', 'bf16'],
+            "grad_scale_func": self.trainer.precision_plugin.scaler.scale if pipeline_dtype == torch.float16 else None,
+            "enable_autocast": not megatron_amp_O2 and pipeline_dtype in [torch.bfloat16, torch.float16],
             "autocast_dtype": autocast_dtype,
             "variable_seq_lengths": False,  # set dynamically during training
             "num_microbatches_with_partial_activation_checkpoints": self.cfg.get(