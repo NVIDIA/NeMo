219a5bb038f76cd040c90c9dd71edad5bf4ea00a
Chen Cui <chcui@nvidia.com>
2023-08-28 16:01:11 -0700

    update gpt peft tuning script to new API

--- examples/nlp/language_modeling/tuning/megatron_gpt_peft_tuning.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_peft_tuning.py
@@ -12,40 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import os
-import tempfile
-
 import torch.multiprocessing as mp
 from omegaconf.omegaconf import OmegaConf, open_dict
-from pytorch_lightning import Trainer
-from pytorch_lightning.plugins.environments import TorchElasticEnvironment
-from pytorch_lightning.trainer.connectors.checkpoint_connector import _CheckpointConnector
-from torch.utils.data import DataLoader, Dataset
 
-from nemo.collections.nlp.models.language_modeling.megatron_gpt_peft_models import (
-    MegatronGPTAdapterModel,
-    MegatronGPTAdapterModelWeightTying,
-    MegatronGPTAdapterPTuningModel,
-    MegatronGPTIA3Model,
-    MegatronGPTLoRAModel,
-    MegatronGPTLoRAModelWeightTying,
-    MegatronGPTPTuningModel,
-)
-from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTModel
-from nemo.collections.nlp.modules.common.megatron.megatron_init import fake_initialize_model_parallel
-from nemo.collections.nlp.parts.nlp_overrides import (
-    GradScaler,
-    MegatronHalfPrecisionPlugin,
-    NLPDDPStrategy,
-    NLPSaveRestoreConnector,
-    PEFTSaveRestoreConnector,
-    PipelineMixedPrecisionPlugin,
-)
+from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTSFTModel
+from nemo.collections.nlp.modules.common.megatron.adapters.parallel_adapters import IA3AdapterConfig, AttnAdapterConfig, \
+    PtuningAdapterConfig, AttnPtuningAdapterConfig, LoraAdapterConfig
+from nemo.collections.nlp.parts.megatron_trainer_builder import MegatronTrainerBuilder
+
 from nemo.core.config import hydra_runner
-from nemo.utils import AppState, logging
+from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
-from nemo.utils.model_utils import inject_model_parallel_rank
 
 mp.set_start_method("spawn", force=True)
 
@@ -71,6 +48,13 @@ Usage:
             trainer.max_epochs=2
 """
 
+peft_config_map = {
+    "adapter": AttnAdapterConfig,
+    "ia3": IA3AdapterConfig,
+    "ptuning": PtuningAdapterConfig,
+    "adapter_and_ptuning": AttnPtuningAdapterConfig,
+    "lora": LoraAdapterConfig,
+}
 
 def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
     """
@@ -102,8 +86,6 @@ def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
         gpt_cfg.attention_dropout = cfg.model.get('attention_dropout', 0.0)
         gpt_cfg.ffn_dropout = cfg.model.ffn_dropout
         gpt_cfg.peft = cfg.model.peft
-        peft_cls = _get_peft_scheme(cfg.model)
-        gpt_cfg.target = f"{peft_cls.__module__}.{peft_cls.__name__}"
 
         # This is needed when modifying a hparam file directly to load `.ckpt` files.
         # This is not needed to modify the cfg in `.nemo` files.
@@ -114,107 +96,7 @@ def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
     return gpt_cfg
 
 
-def _get_peft_scheme(cfg):
-    if cfg.peft.peft_scheme == "adapter":
-        if cfg.peft.adapter_tuning.weight_tying:
-            peft_cls = MegatronGPTAdapterModelWeightTying
-        else:
-            peft_cls = MegatronGPTAdapterModel
-    elif cfg.peft.peft_scheme == "ia3":
-        peft_cls = MegatronGPTIA3Model
-    elif cfg.peft.peft_scheme == "ptuning":
-        peft_cls = MegatronGPTPTuningModel
-    elif cfg.peft.peft_scheme == "adapter_and_ptuning":
-        peft_cls = MegatronGPTAdapterPTuningModel
-    elif cfg.peft.peft_scheme == "lora":
-        if cfg.peft.lora_tuning.weight_tying:
-            peft_cls = MegatronGPTLoRAModelWeightTying
-        else:
-            peft_cls = MegatronGPTLoRAModel
-    else:
-        raise RuntimeError("Invalid Peft scheme")
-    return peft_cls
-
-
-def load_from_checkpoint_dir(cls, cfg, trainer, modify_confg_fn):
-    app_state = AppState()
-    if cfg.model.tensor_model_parallel_size > 1 or cfg.model.pipeline_model_parallel_size > 1:
-        app_state.model_parallel_size = cfg.model.tensor_model_parallel_size * cfg.model.pipeline_model_parallel_size
-        app_state.tensor_model_parallel_size = cfg.model.tensor_model_parallel_size
-        app_state.pipeline_model_parallel_size = cfg.model.pipeline_model_parallel_size
-        (
-            app_state.tensor_model_parallel_rank,
-            app_state.pipeline_model_parallel_rank,
-            app_state.model_parallel_size,
-            app_state.data_parallel_size,
-            app_state.pipeline_model_parallel_split_rank,
-            app_state.virtual_pipeline_model_parallel_rank,
-        ) = fake_initialize_model_parallel(
-            world_size=app_state.model_parallel_size,
-            rank=trainer.global_rank,
-            tensor_model_parallel_size_=cfg.model.tensor_model_parallel_size,
-            pipeline_model_parallel_size_=cfg.model.pipeline_model_parallel_size,
-            pipeline_model_parallel_split_rank_=cfg.model.pipeline_model_parallel_split_rank,
-        )
-    checkpoint_path = inject_model_parallel_rank(
-        os.path.join(cfg.model.pretrained_checkpoint.checkpoint_dir, cfg.model.pretrained_checkpoint.checkpoint_name)
-    )
-    hparams_file = OmegaConf.load(cfg.model.pretrained_checkpoint.hparams_file)
-    gpt_cfg = modify_confg_fn(hparams_file.cfg, cfg, add_cfg_to_tree=True)
-    with tempfile.NamedTemporaryFile(suffix='.yaml') as f:
-        OmegaConf.save(config=gpt_cfg, f=f.name)
-        model = cls.load_from_checkpoint(checkpoint_path=checkpoint_path, trainer=trainer, hparams_file=f.name,)
-        return model
-
-
-def validate_checkpoint_loading_args(cfg):
-    if cfg.checkpoint_dir is None or not os.path.isdir(cfg.checkpoint_dir):
-        raise ValueError(f'Checkpoint directory {cfg.checkpoint_dir} does not exist or is not a directory.')
-    if cfg.checkpoint_name is None:
-        raise ValueError(f'Checkpoint name {cfg.checkpoint_name} is not valid.')
-    if cfg.hparams_file is None or not os.path.isfile(cfg.hparams_file):
-        raise ValueError(f'Hparams file {cfg.hparams_file} does not exist or is not a file.')
-
-
-@hydra_runner(config_path="conf", config_name="megatron_gpt_peft_tuning_config")
-def main(cfg) -> None:
-    logging.info("\n\n************** Experiment configuration ***********")
-    logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
-
-    megatron_amp_o2 = cfg.model.get('megatron_amp_O2', False)
-    with_distributed_adam = cfg.model.optim.get('name') == 'distributed_fused_adam'
-
-    plugins = []
-    strategy = NLPDDPStrategy(
-        no_ddp_communication_hook=True,  # we don't use DDP for async grad allreduce
-        gradient_as_bucket_view=cfg.model.gradient_as_bucket_view,
-        find_unused_parameters=False,
-    )
-    if cfg.trainer.precision in [16, '16', 'bf16', '16-mixed', 'bf16-mixed']:
-        scaler = None
-        if cfg.trainer.precision in [16, '16', '16-mixed']:
-            scaler = GradScaler(
-                init_scale=cfg.model.get('native_amp_init_scale', 2 ** 32),
-                growth_interval=cfg.model.get('native_amp_growth_interval', 1000),
-                hysteresis=cfg.model.get('hysteresis', 2),
-                enabled=False
-                if cfg.model.pipeline_model_parallel_size > 1
-                else True,  # turn off the grad scale for pipeline parallel LM model
-            )
-            # MixedPrecisionPlugin in PTL >= 2.0 requires precision to be 16-mixed or bf16-mixed
-            plugin_precision = '16-mixed'
-        else:
-            plugin_precision = 'bf16-mixed'
-        if megatron_amp_o2 and not with_distributed_adam:
-            plugins.append(MegatronHalfPrecisionPlugin(precision=plugin_precision, device='cuda', scaler=scaler))
-        else:
-            plugins.append(PipelineMixedPrecisionPlugin(precision=plugin_precision, device='cuda', scaler=scaler))
-
-    if cfg.get('cluster_type', None) == 'BCP':
-        plugins.append(TorchElasticEnvironment())
-
-    trainer = Trainer(plugins=plugins, strategy=strategy, **cfg.trainer)
-    exp_manager(trainer, cfg.exp_manager)
+def build_config(cfg, trainer):
     # update resume from checkpoint found by exp_manager
     if cfg.model.resume_from_checkpoint is not None:
         trainer.ckpt_path = cfg.model.resume_from_checkpoint
@@ -224,31 +106,30 @@ def main(cfg) -> None:
     with open_dict(cfg):
         cfg.model.precision = cfg.trainer.precision
 
-    if cfg.model.restore_from_path:
-        base_model_save_restore_connector = NLPSaveRestoreConnector()
-        if os.path.isdir(cfg.model.restore_from_path):
-            base_model_save_restore_connector.model_extracted_dir = cfg.model.restore_from_path
-        base_model_cfg = MegatronGPTModel.restore_from(
-            restore_path=cfg.model.restore_from_path,
-            trainer=trainer,
-            return_config=True,
-            save_restore_connector=base_model_save_restore_connector,
-        )
-        base_model_cfg = _modify_config(base_model_cfg, cfg, add_cfg_to_tree=False)
-        save_restore_connector = PEFTSaveRestoreConnector(
-            peft_model_nemo_path=cfg.model.peft.restore_from_path, peft_model_ckpt_path=trainer.ckpt_path
-        )
-        if os.path.isdir(cfg.model.restore_from_path):
-            save_restore_connector.model_extracted_dir = cfg.model.restore_from_path
-        peft_cls = _get_peft_scheme(cfg.model)
-        model = peft_cls.restore_from(
-            restore_path=cfg.model.restore_from_path,
-            trainer=trainer,
-            override_config_path=base_model_cfg,
-            save_restore_connector=save_restore_connector,
-        )
-    else:
-        raise RuntimeError("PEFT training needs a trained base model present.")
+    assert cfg.model.restore_from_path, "PEFT training needs a trained base model present."
+
+    base_model_cfg = MegatronGPTSFTModel.restore_from(
+        restore_path=cfg.model.restore_from_path, trainer=trainer, return_config=True,
+    )
+    base_model_cfg = _modify_config(base_model_cfg, cfg, add_cfg_to_tree=False)
+    return base_model_cfg
+
+
+@hydra_runner(config_path="conf", config_name="megatron_gpt_peft_tuning_config")
+def main(cfg) -> None:
+    logging.info("\n\n************** Experiment configuration ***********")
+    logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
+
+    trainer = MegatronTrainerBuilder(cfg).create_trainer()
+    exp_manager(trainer, cfg.exp_manager)
+
+    base_model_cfg = build_config(cfg, trainer)
+    model = MegatronGPTSFTModel.restore_from(
+        restore_path=cfg.model.restore_from_path, trainer=trainer, override_config_path=base_model_cfg,
+    )
+
+    AdapterConfig = peft_config_map[base_model_cfg.peft.peft_scheme]
+    model.add_adapters(AdapterConfig(base_model_cfg))
 
     trainer.fit(model)
 
