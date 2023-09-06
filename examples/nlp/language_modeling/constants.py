03838a4887363b5f20a8bb03b7e638e6a0ac2b60
Evelina <ebakhturina@nvidia.com>
2023-09-06 13:38:34 -0700

    update output num tokens
    Signed-off-by: Evelina <ebakhturina@nvidia.com>

--- examples/nlp/language_modeling/constants.py
+++ examples/nlp/language_modeling/constants.py
@@ -1,6 +1,6 @@
 TASKS = {"qasper":
                 {"response": "Answer:",
-                "tokens_to_generate": 30,
+                "tokens_to_generate": 128,
                 "subset": "Qasper/test.jsonl"},
         "narrative_qa":
                 {"response": "Answer:",
@@ -8,15 +8,15 @@ TASKS = {"qasper":
                 "subset": "NarrativeQA/test.jsonl"},
         "quality":
                 {"response": "Answer:",
-                "tokens_to_generate": 128,
+                "tokens_to_generate": 1,
                 "subset": "QuALITY/validation.jsonl"},
         "musique":
                 {"response": "Answer:",
-                "tokens_to_generate": 128,
+                "tokens_to_generate": 12,
                 "subset": "MuSiQue/validation.jsonl"},
         "space_digest":
                 {"response": "Percentage of Positive Reviews:",
-                "tokens_to_generate": 128,
+                "tokens_to_generate": 8,
                 "subset": "SpaceDigest/test.jsonl"},
         "book_sum_sort":
                 {"response": "Summary IDs in Correct Order:",
@@ -24,19 +24,19 @@ TASKS = {"qasper":
                 "subset": "BookSumSort/validation.jsonl"},
         "gov_report":
                 {"response": "Summary:",
-                "tokens_to_generate": 128,
+                "tokens_to_generate": 1024,
                 "subset": "GovReport/test.jsonl"},
         "summ_screen_fd":
                 {"response": "Summary:",
-                "tokens_to_generate": 128,
+                "tokens_to_generate": 640,
                 "subset": "SummScreenFD/test.jsonl"},
         "qmsum":
                 {"response": "Answer:",
-                "tokens_to_generate": 128,
+                "tokens_to_generate": 256,
                 "subset": "QMSum/test.jsonl"},
         "squality":
                 {"response": "Answer:",
-                "tokens_to_generate": 128,
+                "tokens_to_generate": 640,
                 "subset": "SQuALITY/validation.jsonl"}
         }
 
