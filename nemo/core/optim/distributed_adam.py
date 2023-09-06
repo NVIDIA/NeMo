4456eedc020048b9fd07b17ddca8dc066c480ee5
Tim Moon <tmoon@nvidia.com>
2023-09-06 13:56:10 -0700

    Add distopt logic for int dtypes
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- nemo/core/optim/distributed_adam.py
+++ nemo/core/optim/distributed_adam.py
@@ -32,16 +32,32 @@ def _str_to_dtype(dtype: Union[str, torch.dtype]) -> torch.dtype:
     if isinstance(dtype, torch.dtype):
         return dtype
     name = str(dtype).strip().lower()
-    if name in ('', 'none'):
-        return torch.float32
-    elif name in ('torch.float32', 'float32', 'float', 'fp32', '32'):
-        return torch.float32
-    elif name in ('torch.float16', 'float16', 'half', 'fp16', '16'):
-        return torch.float16
-    elif name in ('torch.bfloat16', 'bfloat16', 'bf16'):
-        return torch.bfloat16
-    else:
-        raise ValueError(f'unsupported dtype ({dtype})')
+    if name.startswith("torch."):
+        name = name.replace("torch.", "", 1)
+    if name.startswith("fp"):
+        name = name.replace("fp", "float", 1)
+    dtype = dict(
+        float32=torch.float32,
+        float=torch.float32,
+        float64=torch.float64,
+        double=torch.float64,
+        float16=torch.float16,
+        half=torch.float16,
+        bfloat16=torch.bfloat16,
+        bf16=torch.bfloat16,
+        uint8=torch.uint8,
+        byte=torch.uint8,
+        int8=torch.int8,
+        char=torch.int8,
+        int16=torch.int16,
+        short=torch.int16,
+        int32=torch.int32,
+        int=torch.int32,
+        int64=torch.int64,
+        long=torch.int64,
+        bool=torch.bool,
+    )[name]
+    return dtype
 
 
 class MegatronDistributedFusedAdam(DistributedFusedAdam):
