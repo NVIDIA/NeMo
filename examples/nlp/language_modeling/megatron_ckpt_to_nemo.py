735e0bee9ae10ecab1296f3db60f648317607035
eharper <eharper@nvidia.com>
2023-09-11 15:29:32 -0600

    update ptl convert script
    Signed-off-by: eharper <eharper@nvidia.com>

--- examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
+++ examples/nlp/language_modeling/megatron_ckpt_to_nemo.py
@@ -40,10 +40,11 @@ from nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import
 from nemo.collections.nlp.models.language_modeling.megatron_retrieval_model import MegatronRetrievalModel
 from nemo.collections.nlp.models.language_modeling.megatron_t5_model import MegatronT5Model
 from nemo.collections.nlp.models.machine_translation.megatron_nmt_model import MegatronNMTModel
-from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector
+from nemo.collections.nlp.parts.nlp_overrides import NLPDDPStrategy, NLPSaveRestoreConnector
 from nemo.utils import AppState, logging
 from nemo.utils.distributed import initialize_distributed
 from nemo.utils.model_utils import inject_model_parallel_rank
+
 
 
 def get_args():
@@ -100,12 +101,15 @@ def convert(local_rank, rank, world_size, args):
     app_state = AppState()
     app_state.data_parallel_rank = 0
     num_nodes = world_size // args.gpus_per_node
+    plugins = []
+    strategy = "auto"
     if args.bcp:
-        trainer = Trainer(
-            devices=args.gpus_per_node, num_nodes=num_nodes, accelerator='gpu', plugins=[TorchElasticEnvironment()]
-        )
-    else:
-        trainer = Trainer(devices=args.gpus_per_node, num_nodes=num_nodes, accelerator='gpu')
+        plugins.append(TorchElasticEnvironment())
+    if args.model_type == 'gpt':
+        strategy = NLPDDPStrategy()
+    
+
+    trainer = Trainer(devices=args.gpus_per_node, num_nodes=num_nodes, accelerator='gpu', plugins=plugins, strategy=strategy)
 
     app_state.pipeline_model_parallel_size = args.pipeline_model_parallel_size
     app_state.tensor_model_parallel_size = args.tensor_model_parallel_size
@@ -143,8 +147,12 @@ def convert(local_rank, rank, world_size, args):
     )
 
     if args.model_type == 'gpt':
+        # with distributed checkpoint
+        checkpoint_path = os.path.join(args.checkpoint_folder, args.checkpoint_name)
         model = MegatronGPTModel.load_from_checkpoint(checkpoint_path, hparams_file=args.hparams_file, trainer=trainer)
     elif args.model_type == 'sft':
+        # with distributed checkpoint
+        checkpoint_path = os.path.join(args.checkpoint_folder, args.checkpoint_name)
         model = MegatronGPTSFTModel.load_from_checkpoint(
             checkpoint_path, hparams_file=args.hparams_file, trainer=trainer
         )
