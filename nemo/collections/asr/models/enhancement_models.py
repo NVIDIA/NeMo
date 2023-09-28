74cf1ed7d44abae945af92ab58ab9357b746fa35
Abhishree Thittenamane <47577437+athitten@users.noreply.github.com>
2023-09-27 17:10:24 -0700

    Append output of val_step to self.validation_step_outputs (#7543)
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- nemo/collections/asr/models/enhancement_models.py
+++ nemo/collections/asr/models/enhancement_models.py
@@ -434,6 +434,10 @@ class EncMaskDecAudioToAudioModel(AudioToAudioModel):
         # Log global step
         self.log('global_step', torch.tensor(self.trainer.global_step, dtype=torch.float32), sync_dist=True)
 
+        if type(self.trainer.val_dataloaders) == list and len(self.trainer.val_dataloaders) > 1:
+            self.validation_step_outputs[dataloader_idx].append(output_dict)
+        else:
+            self.validation_step_outputs.append(output_dict)
         return output_dict
 
     @classmethod
