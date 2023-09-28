168979031db9c2fdddf1eb55cb773c5227aae56b
Li Tao <chntaoli@163.com>
2023-09-29 02:01:51 +0800

    fix a typo (#7496)
    Signed-off-by: BestJuly <chntaoli@163.com>

--- nemo/collections/nlp/data/language_modeling/megatron/helpers.cpp
+++ nemo/collections/nlp/data/language_modeling/megatron/helpers.cpp
@@ -38,7 +38,7 @@ void build_blending_indices(py::array_t<uint8_t>& dataset_index,
 			    const int32_t num_datasets,
 			    const int64_t size, const bool verbose) {
   /* Given multiple datasets and a weighting array, build samples
-   such that it follows those wieghts.*/
+   such that it follows those weights.*/
 
   if (verbose) {
     std::cout << "> building indices for blendable datasets ..." << std::endl;
