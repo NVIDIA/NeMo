06e633136099597712f87d9e166c10f7c40aa0a4
arendu <adithyare@nvidia.com>
2023-10-03 07:02:41 +0000

    mbs update
    Signed-off-by: arendu <adithyare@nvidia.com>

--- examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
@@ -146,6 +146,7 @@ def main(cfg) -> None:
         peft_model_cfg.activations_checkpoint_method = None
         peft_model_cfg.activations_checkpoint_layers_per_pipeline = None
         peft_model_cfg.global_batch_size = cfg.model.data.test_ds.global_batch_size
+        peft_model_cfg.micro_batch_size = cfg.model.data.test_ds.micro_batch_size
         if peft_model_cfg.get("use_flash_attention", False):
             peft_model_cfg.use_flash_attention = cfg.model.use_flash_attention
         if cfg.model.get("seq_len_interpolation_factor", None) is not None:
