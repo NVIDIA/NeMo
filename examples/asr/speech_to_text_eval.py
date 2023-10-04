db6d1cb589a0079f85d775c86a615ea359e1ab1b
Sasha Meister <117230141+ssh-meister@users.noreply.github.com>
2023-10-04 20:16:53 +0400

    Write metrics to "output_filename" if "scores_per_sample=True"
    Signed-off-by: Sasha Meister <117230141+ssh-meister@users.noreply.github.com>
    Signed-off-by: Sasha Meister <sasha.meister.work@gmail.com>

--- examples/asr/speech_to_text_eval.py
+++ examples/asr/speech_to_text_eval.py
@@ -25,7 +25,8 @@ same directory during execution.
 for full list of arguments >>
 
     dataset_manifest: Required - path to dataset JSON manifest file (in NeMo format)
-    output_filename: Optional - output filename where the transcriptions will be written.
+    output_filename: Optional - output filename where the transcriptions will be written. (if scores_per_sample=True, 
+    metrics per sample will be written there too)
 
     use_cer: Bool, whether to compute CER or WER
     use_per: Bool, compute dataset Punctuation Error Rate
@@ -34,7 +35,6 @@ for full list of arguments >>
 
     only_score_manifest: Bool, when set will skip audio transcription and just calculate WER of provided manifest.
     scores_per_sample: Bool, compute metrics for each sample separately
-    output_with_scores_filename: Optional - output filename where the transcriptions with metrics will be written
 
 # Usage
 
@@ -70,19 +70,17 @@ import transcribe_speech
 from omegaconf import MISSING, OmegaConf, open_dict
 
 from nemo.collections.asr.metrics.wer import word_error_rate
-from nemo.collections.asr.parts.utils.transcribe_utils import PunctuationCapitalization, TextProcessingConfig
 from nemo.collections.common.metrics.per import PERData
+from nemo.collections.asr.parts.utils.transcribe_utils import PunctuationCapitalization, TextProcessingConfig
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
 
 try:
     import pandas as pd
     from tabulate import tabulate
-
-    HAVE_TABLUATE_AND_PANDAS = True
+    HAVE_TABLUATE_AND_PANDAS=True
 except (ImportError, ModuleNotFoundError):
-    HAVE_TABLUATE_AND_PANDAS = False
-
+    HAVE_TABLUATE_AND_PANDAS=False
 
 @dataclass
 class EvaluationConfig(transcribe_speech.TranscriptionConfig):
@@ -100,7 +98,6 @@ class EvaluationConfig(transcribe_speech.TranscriptionConfig):
 
     only_score_manifest: bool = False
     scores_per_sample: bool = False
-    output_with_scores_filename: Optional[str] = output_filename.replace(".json", ".metrics.json")
 
     text_processing: Optional[TextProcessingConfig] = TextProcessingConfig(
         punctuation_marks=".,?", separate_punctuation=False, do_lowercase=False, rm_punctuation=False,
@@ -139,7 +136,7 @@ def main(cfg: EvaluationConfig):
 
     if cfg.scores_per_sample:
         samples = []
-
+    
     ground_truth_text = []
     predicted_text = []
     invalid_manifest = False
@@ -150,7 +147,7 @@ def main(cfg: EvaluationConfig):
             if 'pred_text' not in data:
                 invalid_manifest = True
                 break
-
+            
             if cfg.scores_per_sample:
                 samples.append(data)
 
@@ -175,52 +172,43 @@ def main(cfg: EvaluationConfig):
             f"Invalid manifest provided: {transcription_cfg.output_filename} does not "
             f"contain value for `pred_text`."
         )
-
+    
     if cfg.use_per:
-        per_data_obj = PERData(
-            hypotheses=predicted_text,
-            references=ground_truth_text,
-            punctuation_marks=list(cfg.text_processing.punctuation_marks),
-        )
+        per_data_obj = PERData(hypotheses=predicted_text, references=ground_truth_text,
+                               punctuation_marks=list(cfg.text_processing.punctuation_marks))
         per_data_obj.compute()
         per = per_data_obj.per
-
+    
     if cfg.scores_per_sample:
-        with open(cfg.output_with_scores_filename, 'w') as manifest_with_scores:
-            if cfg.use_per:
-                for sample, punct_rates in zip(samples, per_data_obj.rates):
-                    sample_cer = word_error_rate(
-                        hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=True
-                    )
-                    sample_wer = word_error_rate(
-                        hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=False
-                    )
-                    sample["cer"] = round(100 * sample_cer, 2)
-                    sample["wer"] = round(100 * sample_wer, 2)
-                    sample["punct_correct_rate"] = round(100 * punct_rates.correct_rate, 2)
-                    sample["punct_deletions_rate"] = round(100 * punct_rates.deletions_rate, 2)
-                    sample["punct_insertions_rate"] = round(100 * punct_rates.insertions_rate, 2)
-                    sample["punct_substitutions_rate"] = round(100 * punct_rates.substitution_rate, 2)
-                    sample["per"] = round(100 * punct_rates.per, 2)
-
-                    line = json.dumps(sample)
-                    manifest_with_scores.writelines(f'{line}\n')
-            else:
-                for sample in samples:
-                    sample_cer = word_error_rate(
-                        hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=True
-                    )
-                    sample_wer = word_error_rate(
-                        hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=False
-                    )
-                    sample["cer"] = round(100 * sample_cer, 2)
-                    sample["wer"] = round(100 * sample_wer, 2)
-
-                    line = json.dumps(sample)
-                    manifest_with_scores.writelines(f'{line}\n')
-        logging.info(f'Output manifest saved: {cfg.output_with_scores_filename}')
-
-    # Compute the WER
+        samples_with_metrics = []
+        if cfg.use_per:
+            for sample, punct_rates in zip(samples, per_data_obj.rates):
+                sample_cer = word_error_rate(hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=True)
+                sample_wer = word_error_rate(hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=False)
+                sample["cer"] = round(100 * sample_cer, 2)
+                sample["wer"] = round(100 * sample_wer, 2)
+                sample["punct_correct_rate"] = round(100 * punct_rates.correct_rate, 2)
+                sample["punct_deletions_rate"] = round(100 * punct_rates.deletions_rate, 2)
+                sample["punct_insertions_rate"] = round(100 * punct_rates.insertions_rate, 2)
+                sample["punct_substitutions_rate"] = round(100 * punct_rates.substitution_rate, 2)
+                sample["per"] = round(100 * punct_rates.per, 2)
+                samples_with_metrics.append(sample)      
+        else:
+            for sample in samples:
+                sample_cer = word_error_rate(hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=True)
+                sample_wer = word_error_rate(hypotheses=[sample['text']], references=[sample['pred_text']], use_cer=False)
+                sample["cer"] = round(100 * sample_cer, 2)
+                sample["wer"] = round(100 * sample_wer, 2)
+                samples_with_metrics.append(sample)
+        
+        with open(cfg.output_filename, 'w') as manifest_with_scores:            
+            for sample in samples_with_metrics:
+                line = json.dumps(sample)
+                manifest_with_scores.writelines(f'{line}\n')
+                    
+        logging.info(f'Output manifest saved: {cfg.output_filename}')
+    
+    # Compute the WER         
     cer = word_error_rate(hypotheses=predicted_text, references=ground_truth_text, use_cer=True)
     wer = word_error_rate(hypotheses=predicted_text, references=ground_truth_text, use_cer=False)
 
@@ -238,30 +226,28 @@ def main(cfg: EvaluationConfig):
         logging.info(f'Got {metric_name} of {metric_value}. Tolerance was {cfg.tolerance}')
 
     logging.info(f'Dataset WER/CER ' + str(round(100 * wer, 2)) + "%/" + str(round(100 * cer, 2)) + "%")
-
+    
     if cfg.use_per:
         logging.info(f'Dataset PER ' + str(round(100 * per, 2)) + '%')
-
+        
         rates_by_pm_df = pd.DataFrame(per_data_obj.operation_rates) * 100
         substitution_rates_by_pm_df = pd.DataFrame(per_data_obj.substitution_rates) * 100
-
+        
         if HAVE_TABLUATE_AND_PANDAS:
             logging.info(
-                "Rates of punctuation correctness and errors (%):\n"
-                + tabulate(rates_by_pm_df, headers='keys', tablefmt='psql')
+            "Rates of punctuation correctness and errors (%):\n" + tabulate(rates_by_pm_df, headers='keys', tablefmt='psql')
             )
             logging.info(
-                "Substitution rates between puncutation marks (%):\n"
-                + tabulate(substitution_rates_by_pm_df, headers='keys', tablefmt='psql')
+            "Substitution rates between puncutation marks (%):\n"
+            + tabulate(substitution_rates_by_pm_df, headers='keys', tablefmt='psql')
             )
-
+                       
     # Inject the metric name and score into the config, and return the entire config
     with open_dict(cfg):
         cfg.metric_name = metric_name
         cfg.metric_value = metric_value
 
     return cfg
-
 
 if __name__ == '__main__':
     main()  # noqa pylint: disable=no-value-for-parameter
