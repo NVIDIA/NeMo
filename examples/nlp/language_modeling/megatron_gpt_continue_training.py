c989851db2a2d94dcc1684cb417bc5861d785ad6
Eric Harper <complex451@gmail.com>
2023-10-04 13:22:12 -0600

    propagate mp config (#7637)
    Signed-off-by: eharper <eharper@nvidia.com>

--- examples/nlp/language_modeling/megatron_gpt_continue_training.py
+++ examples/nlp/language_modeling/megatron_gpt_continue_training.py
@@ -61,9 +61,10 @@ def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
         gpt_cfg.max_position_embeddings = cfg.model.max_position_embeddings
         gpt_cfg.seq_len_interpolation_factor = cfg.model.seq_len_interpolation_factor
         gpt_cfg.use_flash_attention = cfg.model.use_flash_attention
-        assert (
-            gpt_cfg.encoder_seq_length == gpt_cfg.max_position_embeddings * gpt_cfg.seq_len_interpolation_factor
-        ), 'seq_length should be equal to max_position_embedding * seq_len_interpolation_factor'
+        gpt_cfg.tensor_model_parallel_size = cfg.model.get('tensor_model_parallel_size', 1)
+        gpt_cfg.pipeline_model_parallel_size = cfg.model.get('pipeline_model_parallel_size', 1)
+        gpt_cfg.pipeline_model_parallel_split_rank = cfg.model.get('pipeline_model_parallel_split_rank', 0)
+
         # This is needed when modifying a hparam file directly to load `.ckpt` files.
         # This is not needed to modify the cfg in `.nemo` files.
         if add_cfg_to_tree:
