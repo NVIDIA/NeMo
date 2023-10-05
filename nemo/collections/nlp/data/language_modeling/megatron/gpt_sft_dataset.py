c66de49292ce38db5ccceb52a0c5c7c3d849f476
Yi Dong <yidong@nvidia.com>
2023-10-05 20:19:56 +0000

    fix dataset issues
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- nemo/collections/nlp/data/language_modeling/megatron/gpt_sft_dataset.py
+++ nemo/collections/nlp/data/language_modeling/megatron/gpt_sft_dataset.py
@@ -17,8 +17,10 @@ from typing import List, Optional
 
 import numpy as np
 import torch
+import datasets
+# hack to avoid the "not enough disk space" error in some slurm cluster
+datasets.builder.has_sufficient_disk_space = lambda needed_bytes, directory='.': True
 from datasets import load_dataset
-
 from nemo.collections.common.tokenizers.tokenizer_spec import TokenizerSpec
 from nemo.collections.nlp.data.language_modeling.megatron.dataset_utils import get_samples_mapping
 from nemo.collections.nlp.data.language_modeling.text_memmap_dataset import JSONLMemMapDataset
