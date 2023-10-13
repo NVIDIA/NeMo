53018795cbda2dd0ff4581c2d6d2066c09153cbb
zhehuaichen <dian.chenzhehuai@gmail.com>
2023-10-13 12:06:17 -0700

    move useful tools to example directory
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>

--- examples/multimodel/modular_speechllm/run_sft_audio_lm.py
+++ examples/multimodel/modular_speechllm/run_sft_audio_lm.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: move files under workspace to examples/multimodel/ once mature
-
 import os
 import tempfile
 
