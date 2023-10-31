b0c0ec6c416450e2f6f93ab20fd719f178924515
fedorovgv <ivlog7@gmail.com>
2023-11-01 01:31:40 +0300

    Add semi sorted batch sampler.

--- nemo/collections/asr/models/rnnt_bpe_models.py
+++ nemo/collections/asr/models/rnnt_bpe_models.py
@@ -20,6 +20,8 @@ import torch
 from omegaconf import DictConfig, ListConfig, OmegaConf, open_dict
 from pytorch_lightning import Trainer
 
+from nemo.collections.asr.data.audio_to_text import _TarredAudioToTextDataset
+from nemo.collections.common.data.dataset import ConcatDataset
 from nemo.collections.asr.data import audio_to_text_dataset
 from nemo.collections.asr.data.audio_to_text_dali import AudioToBPEDALIDataset
 from nemo.collections.asr.losses.rnnt import RNNTLoss
@@ -515,6 +517,12 @@ class EncDecRNNTBPEModel(EncDecRNNTModel, ASRBPEMixin):
             collate_fn = dataset.datasets[0].datasets[0].collate_fn
 
         if config.get('use_semi_sorted_batching', False):
+            if (
+                isinstance(dataset, _TarredAudioToTextDataset) or
+                (isinstance(dataset, ConcatDataset) and isinstance(dataset.datasets, _TarredAudioToTextDataset))
+            ):
+                raise RuntimeError('Semi Sorted Batch sampler can\'t be used with tarred datasets.')
+
             batch_sampler = get_semi_sorted_batch_sampler(self, dataset, config)
             # set batch_size and batch_sampler to None to disable automatic batching
             return torch.utils.data.DataLoader(
