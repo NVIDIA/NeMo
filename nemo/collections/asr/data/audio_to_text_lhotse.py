f4f45f722be82b16a2c7f2354f8237396bfa068e
Piotr Żelasko <pzelasko@nvidia.com>
2023-11-02 10:36:16 -0400

    Initial implementation of NeMo tarred and non-tarred manifest -> lhotse.CutSet adaptors

--- nemo/collections/asr/data/audio_to_text_lhotse.py
+++ nemo/collections/asr/data/audio_to_text_lhotse.py
@@ -1,15 +1,20 @@
+import random
+import secrets
+import tarfile
 import warnings
-from typing import Dict, Optional, Tuple
+from pathlib import Path
+from typing import Dict, List, Optional, Sequence, Tuple
 
+import lhotse.lazy
 import torch.utils.data
 from lhotse import CutSet
 from lhotse.dataset import (
     AudioSamples,
+    CutMix,
     DynamicBucketingSampler,
+    DynamicCutSampler,
     IterableDatasetWrapper,
     make_worker_init_fn,
-    CutMix,
-    DynamicCutSampler,
 )
 from lhotse.dataset.collation import collate_vectors
 from omegaconf import DictConfig
@@ -161,3 +166,135 @@ class LhotseSpeechToTextBpeDataset(torch.utils.data.Dataset):
 
 def _identity(x):
     return x
+
+
+class LazyNeMoIterator(lhotse.lazy.ImitatesDict):
+    """
+    ``LazyNeMoIterator`` reads a NeMo (non-tarred) JSON manifest and converts it on the fly to an ``Iterable[Cut]``.
+    It's used to create a ``lhotse.CutSet``.
+
+    Currently, it requires (and exclusively supports) the following keys in NeMo manifests:
+    - "audio_filepath"
+    - "duration"
+    - "text"
+
+    .. caution:: We assume every audio file has the same sampling rate, and it has to be explicitly provided
+        in ``LazyNeMoIterator`` constructor.
+
+    Example::
+
+        >>> cuts = lhotse.CutSet(LazyNeMoIterator("nemo_manifests/train.json", sampling_rate=16000))
+    """
+
+    def __init__(self, path: str | Path, sampling_rate: int = 16000) -> None:
+        self.source = lhotse.lazy.LazyJsonlIterator(path)
+        self.sampling_rate = sampling_rate
+
+    @property
+    def path(self) -> str | Path:
+        return self.source.path
+
+    def __iter__(self):
+        for data in self.source:
+            recording = lhotse.Recording(
+                id=...,
+                sources=[lhotse.audio.AudioSource(type="file", channels=[0], source=data["audio_filepath"],)],
+                sampling_rate=self.sampling_rate,
+                duration=data["duration"],
+                num_samples=lhotse.utils.compute_num_samples(data["duration"], self.sampling_rate),
+            )
+            cut = recording.to_cut()
+            cut.supervisions.append(
+                lhotse.SupervisionSegment(
+                    id=cut.id, recording_id=cut.recording_id, start=0, duration=cut.duration, text=data["text"],
+                )
+            )
+            yield cut
+
+    def __len__(self) -> int:
+        return len(self.source)
+
+    def __add__(self, other) -> "lhotse.lazy.LazyIteratorChain":
+        return lhotse.lazy.LazyIteratorChain(self, other)
+
+
+class LazyNeMoTarredIterator(lhotse.lazy.ImitatesDict):
+    """
+    ``LazyNeMoTarredIterator`` reads a NeMo tarred JSON manifest and converts it on the fly to an ``Iterable[Cut]``.
+    It's used to create a ``lhotse.CutSet``.
+
+
+
+    Currently, it requires (and exclusively supports) the following keys in NeMo manifests:
+    - "audio_filepath"
+    - "duration"
+    - "text"
+    - "shard_id"
+
+    Example of CutSet with inter-shard shuffling enabled::
+
+        >>> cuts = lhotse.CutSet(LazyNeMoTarredIterator(
+        ...     manifest_path="nemo_manifests/train.json",
+        ...     tar_paths=["nemo_manifests/audio_0.tar", ...],
+        ...     shuffle_shards=True,
+        ... ))
+    """
+
+    def __init__(
+        self, manifest_path: str | Path, tar_paths: Sequence[str | Path], shuffle_shards: bool = False,
+    ) -> None:
+        from cytoolz import groupby
+
+        self.source = lhotse.lazy.LazyJsonlIterator(manifest_path)
+        self.shard_id_to_manifest: Dict[int, str] = groupby("shard_id", self.source)
+        self.shard_id_to_tar_path: Dict[int, Path] = {int(p.stem.split("_")[1]): p for p in tar_paths}
+        self.shuffle_shards = shuffle_shards
+        self._validate()
+
+    def _validate(self) -> None:
+        shard_ids_tars = set(self.shard_id_to_tar_path)
+        shard_ids_manifest = set(self.shard_id_to_manifest)
+        assert shard_ids_tars == shard_ids_manifest, (
+            f"Mismatch between shard IDs discovered from tar files ({len(shard_ids_tars)=}) and "
+            f"JSON manifest ({len(shard_ids_manifest)=}): {shard_ids_tars - shard_ids_manifest=}"
+        )
+
+    @property
+    def shard_ids(self) -> List[int]:
+        return sorted(self.shard_id_to_manifest.keys())
+
+    @property
+    def path(self) -> str | Path:
+        return self.source.path
+
+    def __iter__(self):
+        shard_ids = self.shard_ids
+
+        if self.shuffle_shards:
+            # Use TRNG for 100% randomness
+            random.Random(secrets.randbelow(2 ** 32)).shuffle(shard_ids)
+
+        for sid in shard_ids:
+            shard_manifest = self.shard_id_to_manifest[sid]
+            tar_path = self.shard_id_to_tar_path[sid]
+            with tarfile.open(fileobj=lhotse.serialization.open_best(tar_path, mode="rb"), mode="r|*") as tar:
+                for data, tar_info in zip(shard_manifest, tar):
+                    raw_audio = tar.extractfile(tar_info).read()
+                    recording = lhotse.Recording.from_bytes(raw_audio, recording_id=tar_info.path)
+                    cut = recording.to_cut()
+                    cut.supervisions.append(
+                        lhotse.SupervisionSegment(
+                            id=cut.id,
+                            recording_id=cut.recording_id,
+                            start=0,
+                            duration=cut.duration,
+                            text=data["text"],
+                        )
+                    )
+                    yield cut
+
+    def __len__(self) -> int:
+        return len(self.source)
+
+    def __add__(self, other) -> "lhotse.lazy.LazyIteratorChain":
+        return lhotse.lazy.LazyIteratorChain(self, other)
