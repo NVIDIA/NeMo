7ed628388ed001c9f494cae5f8d2da2862d3e294
yaoyu-33 <yaoyu.094@gmail.com>
2023-11-02 11:58:35 -0700

    Revert "hide multimodal"
    This reverts commit e2ccc8850cd0f939f48f10120580697f23e89ca1.

--- nemo/collections/nlp/modules/common/text_generation_strategy.py
+++ nemo/collections/nlp/modules/common/text_generation_strategy.py
@@ -622,6 +622,7 @@ class PromptLearningModelTextGenerationStrategy(TextGenerationStrategy):
 
 
 def model_inference_strategy_dispatcher(model, **args):
+    from nemo.collections.multimodal.models.neva.neva_model import MegatronNevaModel
     from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
     from nemo.collections.nlp.models.language_modeling.megatron_gpt_prompt_learning_model import (
         MegatronGPTPromptLearningModel,
@@ -633,6 +634,8 @@ def model_inference_strategy_dispatcher(model, **args):
         RetroQAModelTextGenerationStrategy,
     )
 
+    if isinstance(model, MegatronNevaModel):
+        return NevaModelTextGenerationStrategy(model)
     if isinstance(model, MegatronGPTPromptLearningModel):
         return PromptLearningModelTextGenerationStrategy(model, **args)
     elif isinstance(model, MegatronGPTModel):
