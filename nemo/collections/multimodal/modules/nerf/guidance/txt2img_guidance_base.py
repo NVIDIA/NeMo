782316f3ac3e4e8704d417dd9aa4f1068ba20bbe
yaoyu-33 <yaoyu.094@gmail.com>
2023-11-07 10:28:46 -0800

    Hide Nerf and text to image

--- nemo/collections/multimodal/modules/nerf/guidance/txt2img_guidance_base.py
+++
@@ -1,6 +0,0 @@
-import torch.nn as nn
-
-
-class Txt2ImgGuidanceBase(nn.Module):
-    def __init__(self):
-        super().__init__()
--- nemo/collections/multimodal/modules/nerf/renderers/base_volume_renderer.py
+++
@@ -1,7 +0,0 @@
-from nemo.collections.multimodal.modules.nerf.geometry.nerf_base import DensityActivationEnum
-from nemo.collections.multimodal.modules.renderer.base_renderer import RendererBase
-
-
-class BaseVolumeRenderer(RendererBase):
-    def __init__(self, bound, update_interval):
-        super().__init__(bound, update_interval)
