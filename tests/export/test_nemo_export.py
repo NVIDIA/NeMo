2db5dfcdf70062593246d23a509cc4080af5de5e
Onur Yilmaz <oyilmaz@nvidia.com>
2023-11-08 15:05:00 -0800

    update pytest

--- tests/export/test_nemo_export.py
+++ tests/export/test_nemo_export.py
@@ -21,83 +21,84 @@ import shutil
 
 
 class TestNemoExport:
-    @pytest.mark.unit
-    def test_trt_llm_export(self):
+
+    # @pytest.mark.unit
+    @pytest.mark.parametrize("model_name", ["NV-GPT-8B-Base-4k", "NV-GPT-8B-QA-4k", "NV-GPT-8B-Chat-4k-SFT", "NV-GPT-8B-Chat-4k-RLHF"])
+    def test_trt_llm_export(self, model_name):
         """Here we test the trt-llm transfer and infer function"""
 
         test_data = get_infer_test_data()
-        test_at_least_one = False
 
-        for model_name, model_info in test_data.items():
-            if model_info["location"] == "HF":
-                download_nemo_checkpoint(
-                    model_info["checkpoint_link"], model_info["checkpoint_dir"], model_info["checkpoint"]
-                )
-
-            print(
-                "Path: {0} and model: {1} is next and test will start if the nemo checkpoint exists ...".format(
-                    model_info["checkpoint"], model_name
-                )
+        model_info = test_data[model_name]
+        if model_info["location"] == "HF":
+            download_nemo_checkpoint(
+                model_info["checkpoint_link"], model_info["checkpoint_dir"], model_info["checkpoint"]
             )
-            if Path(model_info["checkpoint"]).exists():
-                for n_gpu in model_info["total_gpus"]:
-                    if n_gpu > torch.cuda.device_count():
-                        print(
-                            "Path: {0} and model: {1} with {2} gpus won't be tested since available # of gpus = {3}".format(
-                                model_info["checkpoint"], model_name, n_gpu, torch.cuda.device_count()
-                            )
-                        )
-                        continue
 
-                    Path(model_info["trt_llm_model_dir"]).mkdir(parents=True, exist_ok=True)
-
-                    print("")
-                    print("")
-                    print("################################################## NEW MODEL ##################################################")
-                    print("")
-
+        print(
+            "Path: {0} and model: {1} is next and test will start if the nemo checkpoint exists ...".format(
+                model_info["checkpoint"], model_name
+            )
+        )
+        if Path(model_info["checkpoint"]).exists():
+            for n_gpu in model_info["total_gpus"]:
+                if n_gpu > torch.cuda.device_count():
                     print(
-                        "Path: {0} and model: {1} with {2} gpus will be tested".format(
-                            model_info["checkpoint"], model_name, n_gpu
+                        "Path: {0} and model: {1} with {2} gpus won't be tested since available # of gpus = {3}".format(
+                            model_info["checkpoint"], model_name, n_gpu, torch.cuda.device_count()
                         )
                     )
-                    trt_llm_exporter = TensorRTLLM(model_dir=model_info["trt_llm_model_dir"])
-                    trt_llm_exporter.export(
-                        nemo_checkpoint_path=model_info["checkpoint"],
-                        model_type=model_info["model_type"],
-                        n_gpus=n_gpu,
-                    )
-                    output = trt_llm_exporter.forward(
-                        input_texts=["Hi, how are you?", "I am good, thanks, how about you?"],
-                        max_output_token=128,
-                        top_k=1,
-                        top_p=0.0,
-                        temperature=1.0,
-                    )
-                    print("output after export: ", output)
+                    continue
 
-                    if "p_tuning_checkpoint" in model_info.keys():
-                        if Path(model_info["p_tuning_checkpoint"]).exists():
-                            print(
-                                "Path: {0} and model: {1} with {2} gpus will be tested with PTuning checkpoint {3}".format(
-                                    model_info["checkpoint"], model_name, n_gpu, model_info["p_tuning_checkpoint"]
-                                )
-                            )
+                Path(model_info["trt_llm_model_dir"]).mkdir(parents=True, exist_ok=True)
 
-                            trt_llm_exporter.export(
-                                nemo_checkpoint_path=model_info["checkpoint"],
-                                model_type=model_info["model_type"],
-                                n_gpus=n_gpu,
-                                prompt_embeddings_checkpoint_path=model_info["p_tuning_checkpoint"],
+                print("")
+                print("")
+                print("################################################## NEW MODEL ##################################################")
+                print("")
+
+                print(
+                    "Path: {0} and model: {1} with {2} gpus will be tested".format(
+                        model_info["checkpoint"], model_name, n_gpu
+                    )
+                )
+                trt_llm_exporter = TensorRTLLM(model_dir=model_info["trt_llm_model_dir"])
+                trt_llm_exporter.export(
+                    nemo_checkpoint_path=model_info["checkpoint"],
+                    model_type=model_info["model_type"],
+                    n_gpus=n_gpu,
+                )
+                output = trt_llm_exporter.forward(
+                    input_texts=["Hi, how are you?", "I am good, thanks, how about you?"],
+                    max_output_token=128,
+                    top_k=1,
+                    top_p=0.0,
+                    temperature=1.0,
+                )
+                print("output after export: ", output)
+
+                if "p_tuning_checkpoint" in model_info.keys():
+                    if Path(model_info["p_tuning_checkpoint"]).exists():
+                        print(
+                            "Path: {0} and model: {1} with {2} gpus will be tested with PTuning checkpoint {3}".format(
+                                model_info["checkpoint"], model_name, n_gpu, model_info["p_tuning_checkpoint"]
                             )
-                            output = trt_llm_exporter.forward(
-                                input_texts=["Let's see how this works", "Did you get the result yet with ptuning as well?"],
-                                max_output_token = 50,
-                                top_k=1,
-                                top_p=0.3,
-                                temperature=0.5,
-                            )
-                            print("output with export using ptuning: ", output)
+                        )
+
+                        trt_llm_exporter.export(
+                            nemo_checkpoint_path=model_info["checkpoint"],
+                            model_type=model_info["model_type"],
+                            n_gpus=n_gpu,
+                            prompt_embeddings_checkpoint_path=model_info["p_tuning_checkpoint"],
+                        )
+                        output = trt_llm_exporter.forward(
+                            input_texts=["Let's see how this works", "Did you get the result yet with ptuning as well?"],
+                            max_output_token = 50,
+                            top_k=1,
+                            top_p=0.3,
+                            temperature=0.5,
+                        )
+                        print("output with export using ptuning: ", output)
 
                 trt_llm_exporter = None
                 test_at_least_one = True
