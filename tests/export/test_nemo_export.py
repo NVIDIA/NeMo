f203b0b6a9a8b0ca4b3c2b30767d9672533a4b73
Onur Yilmaz <oyilmaz@nvidia.com>
2023-11-08 18:05:55 -0800

    split all the tests

--- tests/export/test_nemo_export.py
+++ tests/export/test_nemo_export.py
@@ -99,25 +99,145 @@ def run_trt_llm_export(model_name, n_gpu):
 
 
 @pytest.mark.parametrize("n_gpus", [1])
-def test_NV_GPT_8B_Base_4k(n_gpus):
+def test_NV_GPT_8B_Base_4k_1gpu(n_gpus):
     """Here we test the trt-llm transfer and infer function"""
+
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Base-4k", n_gpus)
+
+@pytest.mark.parametrize("n_gpus", [2])
+def test_NV_GPT_8B_Base_4k_2gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Base-4k", n_gpus)
+
+@pytest.mark.parametrize("n_gpus", [4])
+def test_NV_GPT_8B_Base_4k_4gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Base-4k", n_gpus)
+
+@pytest.mark.parametrize("n_gpus", [8])
+def test_NV_GPT_8B_Base_4k_8gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
     run_trt_llm_export("NV-GPT-8B-Base-4k", n_gpus)
 
 
 @pytest.mark.parametrize("n_gpus", [1])
-def test_NV_GPT_8B_QA_4k(n_gpus):
+def test_NV_GPT_8B_QA_4k_1gpu(n_gpus):
     """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-QA-4k", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [2])
+def test_NV_GPT_8B_QA_4k_2gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-QA-4k", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [4])
+def test_NV_GPT_8B_QA_4k_4gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-QA-4k", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [8])
+def test_NV_GPT_8B_QA_4k_8gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
     run_trt_llm_export("NV-GPT-8B-QA-4k", n_gpus)
 
 
 @pytest.mark.parametrize("n_gpus", [1])
-def test_NV_GPT_8B_Chat_4k_SFT(n_gpus):
+def test_NV_GPT_8B_Chat_4k_SFT_1gpu(n_gpus):
     """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Chat-4k-SFT", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [2])
+def test_NV_GPT_8B_Chat_4k_SFT_2gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Chat-4k-SFT", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [4])
+def test_NV_GPT_8B_Chat_4k_SFT_4gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Chat-4k-SFT", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [8])
+def test_NV_GPT_8B_Chat_4k_SFT_8gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
     run_trt_llm_export("NV-GPT-8B-Chat-4k-SFT", n_gpus)
         
 
 @pytest.mark.parametrize("n_gpus", [1])
-def test_NV_GPT_8B_Chat_4k_RLHF(n_gpus):
+def test_NV_GPT_8B_Chat_4k_RLHF_1gpu(n_gpus):
     """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
     run_trt_llm_export("NV-GPT-8B-Chat-4k-RLHF", n_gpus)
 
+
+@pytest.mark.parametrize("n_gpus", [2])
+def test_NV_GPT_8B_Chat_4k_RLHF_2gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Chat-4k-RLHF", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [4])
+def test_NV_GPT_8B_Chat_4k_RLHF_4gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Chat-4k-RLHF", n_gpus)
+
+
+@pytest.mark.parametrize("n_gpus", [8])
+def test_NV_GPT_8B_Chat_4k_RLHF_8gpu(n_gpus):
+    """Here we test the trt-llm transfer and infer function"""
+    if n_gpus > torch.cuda.device_count():
+        pytest.skip("Skipping the test due to not enough number of GPUs", allow_module_level=True)
+
+    run_trt_llm_export("NV-GPT-8B-Chat-4k-RLHF", n_gpus)
