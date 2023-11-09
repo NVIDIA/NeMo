3943b17bd7acce975e60b3bc4a36491bff966a22
Onur Yilmaz <oyilmaz@nvidia.com>
2023-11-09 11:45:57 -0800

    print added

--- tests/export/test_nemo_export.py
+++ tests/export/test_nemo_export.py
@@ -114,8 +114,9 @@ def run_trt_llm_export(model_name, n_gpu):
             ew = model_info["expected_keyword"][i] 
             assert (output[i][0].find(ew) > -1 or output[i][0].find(ew.lower()) > -1), "Keyword: {0} couldn't be found in output: {1}".format(ew, output[i][0])
 
+        print("Start model accuracy testing ...")
         trtllm_accuracy, all_trtllm_outputs, all_expected_outputs = get_accuracy_with_lambada(trt_llm_exporter)
-        print("****** model accuracy: ", trtllm_accuracy)
+        print("Model Accuracy: ", trtllm_accuracy)
         assert trtllm_accuracy > 0.5, "Model accuracy is below 0.5"
 
         if "p_tuning_checkpoint" in model_info.keys():
