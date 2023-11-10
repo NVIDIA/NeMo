20f6e89bc07ca73e447dc653497919ac4a3f4caa
Onur Yilmaz <oyilmaz@nvidia.com>
2023-11-09 20:51:11 -0800

    add run script

---
+++ tests/deploy/run.sh
@@ -0,0 +1,8 @@
+
+echo "unset all SLURM_, PMI_, PMIX_ Variables"
+set -x
+for i in $(env | grep ^SLURM_ | cut -d"=" -f 1); do unset -v $i; done
+for i in $(env | grep ^PMI_ | cut -d"=" -f 1); do unset -v $i; done
+for i in $(env | grep ^PMIX_ | cut -d"=" -f 1); do unset -v $i; done
+set +x
+
