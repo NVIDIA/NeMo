4973b6b39e9b0219784cbf4c0e04369656315ca8
Aleksandr Laptev <alaptev@nvidia.com>
2023-11-14 13:51:18 -0800

    remove rebase glitch
    Signed-off-by: Aleksandr Laptev <alaptev@nvidia.com>

--- scripts/installers/install_k2.sh
+++ scripts/installers/install_k2.sh
@@ -23,26 +23,7 @@ LATEST_RELEASE=d12eec7 # Temporary fix for PyTorch 2.1.0
 #    | cut -d '/' -f 3)
 # "cut --delimiter '/' --fields 3" doesn't work on macOS, use "-d ... -f ..." instead
 
-GRAPHVIZ_REPO=https://gitlab.com/graphviz/graphviz.git
-GRAPHVIZ_LATEST_RELEASE=9.0.0
-GRAPHVIZ_PY_REPO=https://github.com/xflr6/graphviz
-GRAPHVIZ_PY_LATEST_RELEASE=448d1a0  # Temporary fix until 0.20.2
 
-K2_MAKE_ARGS="-j" pip install -v "git+${K2_REPO}@${K2_LATEST_RELEASE}#egg=k2" || { echo "k2 could not be installed!"; exit 1; }
+K2_MAKE_ARGS="-j" pip install -v "git+${K2_REPO}@${LATEST_RELEASE}#egg=k2" || { echo "k2 could not be installed!"; exit 1; }
 python3 -m k2.version > /dev/null || { echo "k2 installed with errors! Please check installation manually."; exit 1; }
 echo "k2 installed successfully!"
-
-# reinstalling graphviz
-{
-    apt-get update
-    apt-get remove -y graphviz && pip uninstall -y graphviz
-    apt-get install -y libtool libltdl-dev automake autoconf bison flex tcl \
-        libperl-dev python-dev-is-python3 libpython3-dev dh-python ghostscript libgd-dev fontconfig \
-        libcairo2-dev libpango1.0-dev libgts-dev
-    git clone ${GRAPHVIZ_REPO} -b ${GRAPHVIZ_LATEST_RELEASE} && cd graphviz
-    ./autogen.sh && ./configure --disable-python2 --enable-python3
-    make -j && make install
-    cd .. && rm -rf graphviz
-    pip install -v "git+${GRAPHVIZ_PY_REPO}@${GRAPHVIZ_PY_LATEST_RELEASE}#egg=graphviz"
-} || { echo "graphviz installed with errors! Please check installation manually."; exit 1; }
-echo "graphviz reinstalled successfully!"
