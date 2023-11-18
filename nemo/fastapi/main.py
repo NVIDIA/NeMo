7b3a88a6ae1b7981e7fdea72fada83a343b0a4de
Onur Yilmaz <oyilmaz@nvidia.com>
2023-11-18 11:21:01 -0800

    Add nemo query

--- nemo/fastapi/main.py
+++ nemo/fastapi/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from nemo.deploy import NemoQuery
 
 app = FastAPI()
 
@@ -15,6 +16,28 @@ async def completions_v1(
         stop="None",
         frequency_penalty: float=1.0,
 ):
+
+    print('model: ', model)
+    print('prompt: ', prompt)
+    print('max_tokens: ', max_tokens)
+    print('temperature: ', temperature)
+    print('top_p: ', top_p)
+    print('n: ', n)
+    print('stream: ', stream)
+    print('stop: ', stop)
+    print('frequency_penalty: ', frequency_penalty)
+
+
+    nq = NemoQuery(url="http://localhost", model_name=model)
+    output = nq.query_llm(
+        prompts=[prompt],
+        max_output_token=max_tokens,
+        top_k=n,
+        top_p=top_p,
+        temperature=temperature,
+    )
+
+
     return {
         "model": model,
         "prompt": prompt,
