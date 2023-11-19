bf18d901924e4d8500c38d0509566e6a690d67c4
SungFeng-Huang <f06942045@ntu.edu.tw>
2023-11-19 13:03:10 +0000

    finish template config settings

--- examples/tts/voicebox.py
+++ examples/tts/voicebox.py
@@ -15,7 +15,7 @@
 import pytorch_lightning as pl
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
-from nemo.utils.exp_manager import exp_manager
+from nemo.utils.exp_manager import exp_manager, clean_exp_ckpt
 
 from typing import Any, Dict, List, Optional, Tuple
 
@@ -49,6 +49,7 @@ def instantiate_callbacks(callbacks_cfg: DictConfig) -> List[Callback]:
 
     return callbacks
 
+
 def get_metric_value(metric_dict: Dict[str, Any], metric_name: Optional[str]) -> Optional[float]:
     """Safely retrieves value of the metric logged in LightningModule.
 
@@ -73,16 +74,14 @@ def get_metric_value(metric_dict: Dict[str, Any], metric_name: Optional[str]) ->
     return metric_value
 
 
-def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
-    """Trains the model. Can additionally evaluate on a testset, using best weights obtained during
-    training.
+@hydra_runner(config_path="conf/voicebox", config_name="train.yaml")
+def main(cfg: DictConfig) -> Optional[float]:
+    """Main entry point for training.
 
-    This method is wrapped in optional @task_wrapper decorator, that controls the behavior during
-    failure. Useful for multiruns, saving info about the crash, etc.
-
-    :param cfg: A DictConfig configuration composed by Hydra.
-    :return: A tuple with metrics and dict with all instantiated objects.
+    :param cfg: DictConfig configuration composed by Hydra.
+    :return: Optional[float] with optimized metric value.
     """
+
     # set seed for random number generators in pytorch, numpy and python.random
     if cfg.get("seed"):
         pl.seed_everything(cfg.seed, workers=True)
@@ -96,14 +95,8 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     logging.info(f"Instantiating trainer <{cfg.trainer._target_}>")
     trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks)
 
-    object_dict = {
-        "cfg": cfg,
-        "model": model,
-        "callbacks": callbacks,
-        "trainer": trainer,
-    }
-
     if cfg.get("train"):
+        exp_log_dir = exp_manager(trainer, cfg.get("exp_manager", None))
         logging.info("Starting training!")
         trainer.fit(model=model, ckpt_path=cfg.get("ckpt_path"))
 
@@ -123,24 +116,15 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     # merge train and test metrics
     metric_dict = {**train_metrics, **test_metrics}
 
-    return metric_dict, object_dict
-
-
-@hydra_runner(config_path="conf/voicebox", config_name="train.yaml")
-def main(cfg: DictConfig) -> Optional[float]:
-    """Main entry point for training.
-
-    :param cfg: DictConfig configuration composed by Hydra.
-    :return: Optional[float] with optimized metric value.
-    """
-
-    # train the model
-    metric_dict, _ = train(cfg)
-
     # safely retrieve metric value for hydra-based hyperparameter optimization
     metric_value = get_metric_value(
         metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
     )
+
+    if cfg.get("clean_exp_ckpt", False):
+        # Add following line to end of the training script
+        # Remove PTL ckpt file, and potentially also remove .nemo file to conserve storage space.
+        clean_exp_ckpt(exp_log_dir, remove_ckpt=True, remove_nemo=False)
 
     # return optimized metric
     return metric_value
