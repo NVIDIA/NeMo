a7dcb242eafbc161c4e88b3069ef6aed2f4bdda3
vnoroozi <vnoroozi@nvidia.com>
2023-11-18 18:25:58 -0800

    added sampling for pretraining data

--- scripts/speechlm_sft/sampling_pretraining_data.py
+++ scripts/speechlm_sft/sampling_pretraining_data.py
@@ -9,9 +9,9 @@ MAX_LENGTH = 4096
 
 OUTPUT_FILE = f"pretraining_samples_{'_'.join(SEL_BLENDS)}.json"
 DATA_FOLDER = "/lustre/fsw/adlr/adlr-nlp/adlr-nlp-sharing/nvllm-3.5t/data/text/"
-
 # DATA_FOLDER = "/drive3/datasets/speechlm/pretrainig/"
 
+#NON-ENGLISH DATASETS
 AR2240 = "non-english/AR_shuf.jsonl"
 AZ2240 = "non-english/AZ_shuf.jsonl"
 BG2240 = "non-english/BG_shuf.jsonl"
@@ -68,7 +68,7 @@ ZHMC4 = "non-english/ZH_tdd_shuf.jsonl"
 
 NMT = "non-english/nmt_tdd_shuf.jsonl"
 
-
+#ENGLISH DATASETS
 B3 = "english/MTNLG/Books3_shuf.jsonl"
 OWT2 = "english/MTNLG/OpenWebText2_shuf.jsonl"
 SE = "english/MTNLG/StackExchange_shuf.jsonl"
@@ -99,7 +99,49 @@ CC202314 = "english/CC-MAIN-2023-14_tdd_shuf.jsonl"
 MC4 = "english/mc4-en_shuf.jsonl"
 SEC = "english/sec_tdd_shuf.jsonl"
 
-# english and non-english
+# CODING DATASETS
+ASMB = "starcoder-repo-len-filt/assembly/assembly_repo_shuf.json"
+CPLA = "starcoder-repo-len-filt/c/c_repo_shuf.json"
+CSHA = "starcoder-repo-len-filt/c-sharp/c-sharp_repo_shuf.json"
+CLIS = "starcoder-repo-len-filt/common-lisp/common-lisp_repo_shuf.json"
+CPPP = "starcoder-repo-len-filt/cpp/cpp_repo_shuf.json"
+CSSL = "starcoder-repo-len-filt/css/css_repo_shuf.json"
+CUDA = "starcoder-repo-len-filt/cuda/cuda_repo_shuf.json"
+DART = "starcoder-repo-len-filt/dart/dart_repo_shuf.json"
+DOCK = "starcoder-repo-len-filt/dockerfile/dockerfile_repo_shuf.json"
+FORT = "starcoder-repo-len-filt/fortran/fortran_repo_shuf.json"
+GOPL = "starcoder-repo-len-filt/go/go_repo_shuf.json"
+HASK = "starcoder-repo-len-filt/haskell/haskell_repo_shuf.json"
+HTML = "starcoder-repo-len-filt/html/html_repo_shuf.json"
+JAVA = "starcoder-repo-len-filt/java/java_repo_shuf.json"
+JASC = "starcoder-repo-len-filt/javascript/javascript_repo_shuf.json"
+JSON = "starcoder-repo-len-filt/json/json_repo_shuf.json"
+JULI = "starcoder-repo-len-filt/julia/julia_repo_shuf.json"
+JUPY = "starcoder-repo-len-filt/jupyter-scripts-dedup-filtered/jupyter-scripts-dedup-filtered_repo_shuf.json"
+LUAL = "starcoder-repo-len-filt/lua/lua_repo_shuf.json"
+MAKE = "starcoder-repo-len-filt/makefile/makefile_repo_shuf.json"
+MARD = "starcoder-repo-len-filt/markdown/markdown_repo_shuf.json"
+MATH = "starcoder-repo-len-filt/mathematica/mathematica_repo_shuf.json"
+OMNI = "starcoder-repo-len-filt/python_merged_piiremoval.json"
+PASC = "starcoder-repo-len-filt/pascal/pascal_repo_shuf.json"
+PERL = "starcoder-repo-len-filt/perl/perl_repo_shuf.json"
+PHPL = "starcoder-repo-len-filt/php/php_repo_shuf.json"
+PYTH = "starcoder-repo-len-filt/python/python_repo_shuf.json"
+RSTL = "starcoder-repo-len-filt/restructuredtext/restructuredtext_repo_shuf.json"
+RUBY = "starcoder-repo-len-filt/ruby/ruby_repo_shuf.json"
+RUST = "starcoder-repo-len-filt/rust/rust_repo_shuf.json"
+SCAL = "starcoder-repo-len-filt/scala/scala_repo_shuf.json"
+SHEL = "starcoder-repo-len-filt/shell/shell_repo_shuf.json"
+SQLP = "starcoder-repo-len-filt/sql/sql_repo_shuf.json"
+SWIF = "starcoder-repo-len-filt/swift_shuf.json"
+SYSV = "starcoder-repo-len-filt/systemverilog/systemverilog_repo_shuf.json"
+TEXP = "starcoder-repo-len-filt/tex/tex_repo_shuf.json"
+TYPE = "starcoder-repo-len-filt/typescript/typescript_repo_shuf.json"
+VHDL = "starcoder-repo-len-filt/vhdl/vhdl_repo_shuf.json"
+VISU = "starcoder-repo-len-filt/vidual-basic/visual-basic_repo_shuf.json"
+XMLL = "starcoder-repo-len-filt/xml_shuf.json"
+YAML = "starcoder-repo-len-filt/yaml/yaml_repo_shuf.json"
+
 DATA_BLEND_NONENGLISH = {
     AR2240: 0.0015,
     AZ2240: 0.00005,
@@ -188,8 +230,50 @@ DATA_BLEND_ENGLISH = {
     SEC: 0.00689,
 }
 
-# TODO: add coding datasets
-DATA_BLEND_CODING = {}
+DATA_BLEND_CODING = {
+    ASMB: 0.0002,
+    CPLA: 0.01454,
+    CSHA: 0.00847,
+    CLIS: 0.00005,
+    CPPP: 0.01203,
+    CSSL: 0.00267,
+    CUDA: 0.00006,
+    DART: 0.00037,
+    DOCK: 0.00003,
+    FORT: 0.00019,
+    GOPL: 0.00582,
+    HASK: 0.00022,
+    HTML: 0.00577,
+    JAVA: 0.0162,
+    JASC: 0.01757,
+    JSON: 0.00071,
+    JULI: 0.00014,
+    JUPY: 0.00114,
+    LUAL: 0.00033,
+    MAKE: 0.00015,
+    MARD: 0.01666,
+    MATH: 0.00014,
+    OMNI: 0.00001,
+    PASC: 0.0002,
+    PERL: 0.00026,
+    PHPL: 0.01518,
+    PYTH: 0.01582,
+    RSTL: 0.00032,
+    RUBY: 0.00098,
+    RUST: 0.00128,
+    SCAL: 0.00047,
+    SHEL: 0.00042,
+    SQLP: 0.00292,
+    SWIF: 0.00083,
+    SYSV: 0.00004,
+    TEXP: 0.00069,
+    TYPE: 0.0051,
+    VHDL: 0.00011,
+    VISU: 0.00011,
+    XMLL: 0.00137,
+    YAML: 0.00046,
+}
+
 
 DATA_BLEND = {}
 if "english" in SEL_BLENDS:
