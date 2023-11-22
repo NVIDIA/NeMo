faf6fa5af6997c15c4f47e08bcca29518e60c2ac
Sangkug Lym <slym@nvidia.com>
2023-11-22 14:39:25 -0800

    cleanup
    Signed-off-by: Sangkug Lym <slym@nvidia.com>

--- nemo/collections/nlp/parts/nlp_overrides.py
+++ nemo/collections/nlp/parts/nlp_overrides.py
@@ -161,7 +161,7 @@ class NLPDDPStrategy(DDPStrategy):
             raise ImportError(
                 "megatron-core was not found. Please see the NeMo README for installation instructions: https://github.com/NVIDIA/NeMo#megatron-gpt."
             )
-        DDPStrategy.__init__(self, parallel_devices, cluster_environment, checkpoint_io, **kwargs)
+        super().__init__(parallel_devices, cluster_environment, checkpoint_io, **kwargs)
 
         self.no_ddp_communication_hook = no_ddp_communication_hook
         self.nccl_communicator_config_path = nccl_communicator_config_path
@@ -199,7 +199,7 @@ class NLPDDPStrategy(DDPStrategy):
             Sets find_unused_parameters to False to use activation-checkpoint-recomputation.
         """
 
-        if (hasattr(self.model, 'megatron_amp_o2') and self.model.megatron_amp_o2) or (
+        if (hasattr(self.model, 'megatron_amp_O2') and self.model.megatron_amp_O2) or (
             hasattr(self.model, 'with_distributed_adam') and self.model.with_distributed_adam
         ):
             # do not use DDP if using megatron amp O2 or distributed optimizer
