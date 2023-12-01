fb1544b8462659bd04d4ec47c21986ff9cf3d852
Gerald Shen <geshen@nvidia.com>
2023-11-30 18:30:36 -0800

    fix test by setting TE var
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -1546,9 +1546,12 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         apply_query_key_layer_scaling = self.cfg.get('apply_query_key_layer_scaling', False)
 
         fp16_enabled = self.trainer.precision in [16, '16', '16-mixed']
-        if apply_query_key_layer_scaling and not fp16_enabled:
-            logging.warning("apply_query_key_layer_scaling is only enabled when using FP16, setting it to False")
-            apply_query_key_layer_scaling = False
+        if apply_query_key_layer_scaling:
+            if fp16_enabled:
+                os.environ["NVTE_APPLY_QK_LAYER_SCALING"] = "1"
+            else:
+                logging.warning("apply_query_key_layer_scaling is only enabled when using FP16, setting it to False")
+                apply_query_key_layer_scaling = False
 
         if apply_query_key_layer_scaling:
             attention_softmax_in_fp32 = True
