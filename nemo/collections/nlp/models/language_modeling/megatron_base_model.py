b08c123681fd3cc5390bd6a6246fb53badc48f15
Jan Lasek <janek.lasek@gmail.com>
2023-12-06 13:29:52 +0100

    trainer != None is assured thanks to a previous check
    Signed-off-by: Jan Lasek <janek.lasek@gmail.com>

--- nemo/collections/nlp/models/language_modeling/megatron_base_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_base_model.py
@@ -103,7 +103,7 @@ class MegatronBaseModel(NLPModel):
         self.tokenizer = None
 
         with open_dict(cfg):
-            if cfg.get('precision', None) is None and trainer is not None:
+            if cfg.get('precision', None) is None:
                 cfg.precision = trainer.precision
 
         super().__init__(cfg, trainer=trainer, no_lm_init=no_lm_init)
