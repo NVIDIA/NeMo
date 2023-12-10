fix FSDP mixed precision settings
Signed-off-by: Sangkug Lym <slym@nvidia.com>

nemo/collections/nlp/models/language_modeling/megatron_base_model.py
@@ -856,16 +856,11 @@ class MegatronBaseModel(NLPModel):
     def configure_sharded_model(self):
         if self.use_fsdp:
             """ Top-evel FSDP model sharding """
-            # Cast the full model to initialization precision to match the precision among parameters.
-            params_dtype = utils_funcs.torch_dtype_from_precision(self.cfg.precision, None)
-            self.model = self.model.to(params_dtype)
-            # Shard the top-level model with FSDP. We shard the strategy-unwrapped model not
+            # Shard the top-level model hierarchically. We shard the strategy-unwrapped model not
             # to lose the structure of non-FSDP wrapped parameters (e.g, embedding)
+            # TODO: Currently the main parameter data type is kept in fp32 (when O2=False). This needs to be
+            # extended to support lower precision main parameters.
             self.model = self.trainer.strategy._setup_model(self.model)
-            # Keep the master parameters in fp32.
-            # The prameters can be initialized in half-precision to save memory before sharding
-            self.model = self.model.float()
-            # Move model from CPU to GPU, which is to avoid out-of-memory carash before sharding.
-            # FSDP with `use_cpu_initialization` has the model initialized on CPU then move GPU after sharding.
-            # In case of GPU-initialized model, this is no-op.
+            # Move the CPU-initialized model (with `use_cpu_initialization=True`) to GPU, which is to avoid
+            # out-of-memory carash before sharding. In case of GPU-initialized model, this is no-op.
             self.model = self.model.cuda(torch.cuda.current_device())

nemo/collections/nlp/parts/nlp_overrides.py
@@ -561,16 +561,17 @@ class NLPFSDPStrategy(FSDPStrategy):
     def _set_mixed_precision_recipe(
         self, precision: Union[int, str], grad_reduce_dtype: Union[int, str]
     ) -> MixedPrecision:
-        # Set FSDP mixed precision recipe.
-        if precision == "16-mixed":
-            buffer_dtype = torch.float32
-            param_dtype = reduce_dtype = torch.float16
-        elif precision == "bf16-mixed":
-            buffer_dtype = torch.float32
-            param_dtype = reduce_dtype = torch.bfloat16
-        elif precision in ["16-true", 16]:
+        """
+        Set FSDP mixed precision recipe.
+        `param_dtype` sets the data type for computation in forward and backpropagation, and the parameter
+        data type for optimizer execution is maintained in the full precision.
+        `buffer_dtype` is only valid when a module has buffers by `register_buffer` method, which is not
+        shared by FSDP.
+        `reduce_dtype` sets gradient reduction data type.
+        """
+        if precision in ["16-true", "16-mixed", 16]:
             param_dtype = reduce_dtype = buffer_dtype = torch.float16
-        elif precision in ["bf16-true", "bf16"]:
+        elif precision in ["bf16-true", "bf16-mixed", "bf16"]:
             param_dtype = reduce_dtype = buffer_dtype = torch.bfloat16
         elif precision == 32:
             param_dtype = reduce_dtype = buffer_dtype = torch.float