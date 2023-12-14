23e87be49dd61425bdfb5bf8feadc19591a90dec
Vladimir Bataev <vbataev@nvidia.com>
2023-12-14 23:17:39 +0400

    Clean up tests
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- tests/core/test_torch_jit_script.py
+++ tests/core/test_torch_jit_script.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from inspect import isclass
-from typing import Any, Dict
+from typing import Any, Type
 
 import pytest
 import torch
@@ -21,10 +21,27 @@ import torch.nn as nn
 
 import nemo.core.neural_types.elements as nelements
 from nemo.core.classes import Exportable, NeuralModule, typecheck
+from nemo.core.classes.mixins import AdapterModuleMixin
 from nemo.core.neural_types import FloatType, NeuralType, VoidType
 
 
+def get_all_neural_types() -> list[Type[nelements.ElementType]]:
+    """Get all neural types (elements) by inspecting neural_types.element module"""
+    neural_types = []
+    for neural_type_str in dir(nelements):
+        candidate = getattr(nelements, neural_type_str)
+        if (
+            isclass(candidate)
+            and issubclass(candidate, nelements.ElementType)
+            and candidate is not nelements.ElementType
+        ):
+            neural_types.append(candidate)
+    return neural_types
+
+
 class SimpleLinear(NeuralModule):
+    """Simple linear projection. Test use of NeuralModule instead of nn.Module"""
+
     def __init__(self):
         super().__init__()
         self.linear = nn.Linear(10, 20, bias=False)
@@ -34,6 +51,19 @@ class SimpleLinear(NeuralModule):
 
 
 class SimpleLinearExportable(NeuralModule, Exportable):
+    """Simple linear projection. Test use of NeuralModule with Exportable mixin"""
+
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(10, 20, bias=False)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+class SimpleLinearWithAdapterMixin(NeuralModule, AdapterModuleMixin):
+    """Simple linear projection. Test use of NeuralModule with AdapterModuleMixin mixin"""
+
     def __init__(self):
         super().__init__()
         self.linear = nn.Linear(10, 20, bias=False)
@@ -43,14 +73,16 @@ class SimpleLinearExportable(NeuralModule, Exportable):
 
 
 class SimpleLinearWithTypes(NeuralModule):
+    """Simple linear projection. Test use of NeuralModule with input/output types"""
+
     @property
-    def input_types(self) -> Dict[str, Any]:
+    def input_types(self) -> dict[str, Any]:
         return {
             "x": NeuralType(None, FloatType()),
         }
 
     @property
-    def output_types(self) -> Dict[str, Any]:
+    def output_types(self) -> dict[str, Any]:
         return {
             "output": NeuralType(None, VoidType()),
         }
@@ -65,14 +97,16 @@ class SimpleLinearWithTypes(NeuralModule):
 
 
 class DummyModuleWithIOTypes(NeuralModule):
+    """Identity module. For testing input/output types in a sequence of network calls"""
+
     @property
-    def input_types(self) -> Dict[str, NeuralType]:
+    def input_types(self) -> dict[str, NeuralType]:
         return {
             "x": NeuralType(None, VoidType()),
         }
 
     @property
-    def output_types(self) -> Dict[str, NeuralType]:
+    def output_types(self) -> dict[str, NeuralType]:
         return {
             "output": NeuralType(None, FloatType()),
         }
@@ -84,6 +118,7 @@ class DummyModuleWithIOTypes(NeuralModule):
 class TestTorchJitCompatibility:
     @pytest.mark.unit
     def test_simple_linear(self):
+        """Test basic module derived from NeuralModule"""
         module = torch.jit.script(SimpleLinear())
         x = torch.zeros(2, 10)
         result = module(x)
@@ -92,6 +127,7 @@ class TestTorchJitCompatibility:
 
     @pytest.mark.unit
     def test_simple_linear_exportable(self):
+        """Test basic module derived from NeuralModule and Exportable"""
         module = torch.jit.script(SimpleLinearExportable())
         x = torch.zeros(2, 10)
         result = module(x)
@@ -99,7 +135,17 @@ class TestTorchJitCompatibility:
         assert torch.allclose(result, torch.zeros_like(result))
 
     @pytest.mark.unit
+    def test_simple_linear_with_adapter_mixin(self):
+        """Test basic module derived from NeuralModule and Adapter Mixin"""
+        module = torch.jit.script(SimpleLinearWithAdapterMixin())
+        x = torch.zeros(2, 10)
+        result = module(x)
+        assert result.shape == (2, 20)
+        assert torch.allclose(result, torch.zeros_like(result))
+
+    @pytest.mark.unit
     def test_simple_linear_with_types(self):
+        """Test basic module derived from NeuralModule containing types"""
         module = torch.jit.script(SimpleLinearWithTypes())
         x = torch.zeros(2, 10)
         result = module(x)
@@ -108,18 +154,11 @@ class TestTorchJitCompatibility:
 
     @pytest.mark.unit
     @pytest.mark.parametrize(
-        "neural_type",
-        [
-            getattr(nelements, neural_type_str)
-            for neural_type_str in dir(nelements)
-            if isclass(getattr(nelements, neural_type_str))
-            and issubclass(getattr(nelements, neural_type_str), nelements.ElementType)
-            and not (getattr(nelements, neural_type_str) is nelements.ElementType)
-        ],
+        "neural_type", get_all_neural_types(),
     )
-    def test_element_compilable(self, neural_type):
+    def test_element_compilable(self, neural_type: Type[nelements.ElementType]):
         """
-        Tests that NeuralType elements are compilable by TorchScript.
+        Tests that all NeuralType Elements are compilable by TorchScript (and be used in modules/functions).
         """
 
         @torch.jit.script
@@ -133,6 +172,7 @@ class TestTorchJitCompatibility:
 
     @pytest.mark.unit
     def test_dummy_module_with_io_types(self):
+        """Test module with input/output types"""
         module = torch.jit.script(DummyModuleWithIOTypes())
         x = torch.rand(2, 10)
         result = module(x)
@@ -141,6 +181,7 @@ class TestTorchJitCompatibility:
 
     @pytest.mark.unit
     def test_chain_with_types(self):
+        """Test applying 2 modules consecutively with input/output types"""
         dummy_module = torch.jit.script(DummyModuleWithIOTypes())
         module = torch.jit.script(SimpleLinearWithTypes())
         x = torch.zeros(2, 10)
