004aeb3c3e71d69ba2af5bfbf66cd3168c848241
Onur Yilmaz <oyilmaz@nvidia.com>
2023-12-14 13:32:39 -0800

    Update file setup_export.py

--- setup_export.py
+++ setup_export.py
@@ -43,7 +43,7 @@ def setup_export():
         # Author details
         author="NVIDIA",
         license='Apache2',
-        packages=setuptools.find_packages(where="./nemo/export/"),
+        packages=["nemo.export.trt_llm", "nemo.export.trt_llm.decoder"],
         install_requires=req_file("requirements_export.txt"),
         # Add in any packaged data.
         include_package_data=True,
