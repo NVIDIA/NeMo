9c95d84cbef5dcbc305ddb392ded599596adcfe1
Sasha Meister <117230141+ssh-meister@users.noreply.github.com>
2023-12-28 19:35:44 +0400

    Changed "Amount" to "Count"
    Signed-off-by: Sasha Meister <117230141+ssh-meister@users.noreply.github.com>

--- tools/speech_data_explorer/sde/dataloader/engines/cudf_engine.py
+++ tools/speech_data_explorer/sde/dataloader/engines/cudf_engine.py
@@ -1,10 +1,9 @@
 import cudf.pandas
-
 cudf.pandas.install()
 import pandas as pd
 
 
-class CuDFEngine:
+class cuDF:
     """
     A class representing a data engine utilizing cuDF for processing and analysis.
 
@@ -19,10 +18,10 @@ class CuDFEngine:
         Processes vocabulary information, including word frequencies and match frequencies for hypotheses.
 
     """
-
+    
     def __init__(self):
         pass
-
+    
     def load_samples_chunk(self, samples: list[dict]):
         """
         Loads a chunk of samples into a cuDF DataFrame.
@@ -33,10 +32,10 @@ class CuDFEngine:
         Returns:
         - pd.DataFrame: cuDF DataFrame containing the loaded samples.
         """
-
+        
         chunk = pd.DataFrame(samples)
         return chunk
-
+    
     def concat_samples_chunks(self, samples_chunks: list):
         """
         Concatenates a list of sample chunks into a single cuDF DataFrame.
@@ -47,10 +46,10 @@ class CuDFEngine:
         Returns:
         - pd.DataFrame: Concatenated cuDF DataFrame containing all samples.
         """
-
+        
         samples_datatable = pd.concat(samples_chunks).reset_index(drop=True)
         return samples_datatable
-
+    
     def process_vocabulary(self, words_frequencies: dict, hypotheses_metrics: list[object]):
         """
         Processes vocabulary information, including word frequencies and match frequencies for hypotheses.
@@ -62,30 +61,29 @@ class CuDFEngine:
         Returns:
         - pd.DataFrame: Processed vocabulary datatable containing word and match frequencies.
         """
-
-        vocabulary_datatable = pd.DataFrame(words_frequencies.items(), columns=["Word", "Amount"]).set_index("Word")
-
+        
+        vocabulary_dfs = []
+        
+        words_frequencies_df = pd.DataFrame(words_frequencies.items(), columns=["Word", "Count"]).set_index("Word")
+        vocabulary_dfs.append(words_frequencies_df)
+                
+        for hypothesis_metrics_obj in hypotheses_metrics:
+            label = hypothesis_metrics_obj.hypothesis_label
+            match_words_frequencies = hypothesis_metrics_obj.match_words_frequencies
+            match_words_frequencies_df = pd.DataFrame(match_words_frequencies.items(), columns=["Word", f"Match_{hypothesis_metrics_obj.hypothesis_label}"]).set_index("Word")
+            vocabulary_dfs.append(match_words_frequencies_df)
+        
+        vocabulary_datatable = pd.concat(vocabulary_dfs, axis = 1, join = "outer").reset_index().fillna(0)
+        
         for hypothesis_metrics_obj in hypotheses_metrics:
             label = hypothesis_metrics_obj.hypothesis_label
             postfix = ""
             if label != "":
                 postfix = f"_{label}"
-
-            match_words_frequencies = hypothesis_metrics_obj.match_words_frequencies
-            match_words_frequencies_df = pd.DataFrame(
-                match_words_frequencies.items(), columns=["Word", f"Match_{postfix}"]
-            ).set_index("Word")
-
-            vocabulary_datatable = pd.concat(
-                [vocabulary_datatable, match_words_frequencies_df], axis=1, join="outer"
-            ).fillna(0)
-            vocabulary_datatable[f"Accuracy{postfix}"] = (
-                vocabulary_datatable[f"Match_{label}"] / vocabulary_datatable["Amount"] * 100
-            ).round(2)
-            vocabulary_datatable = vocabulary_datatable.drop(f"Match_{label}", axis=1)
-
+            
+            vocabulary_datatable[f"Accuracy{postfix}"] = vocabulary_datatable[f"Match_{label}"] / vocabulary_datatable["Count"] * 100
+            vocabulary_datatable[f"Accuracy{postfix}"] = vocabulary_datatable[f"Accuracy{postfix}"].round(2)
+            vocabulary_datatable = vocabulary_datatable.drop(f"Match_{label}", axis = 1)
             hypothesis_metrics_obj.mwa = round(vocabulary_datatable[f"Accuracy{postfix}"].mean(), 2)
-
-        vocabulary_datatable = vocabulary_datatable.reset_index()
-
+        
         return vocabulary_datatable
