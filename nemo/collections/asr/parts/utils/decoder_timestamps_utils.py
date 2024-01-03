9c96f383c61dcdaa7f86dc60fd10241a9c24ad74
Travis Bartley <tbartley@nvidia.com>
2024-01-03 12:51:47 -0800

    Got too excited, forgot to style check
    Signed-off-by: Travis Bartley <tbartley@nvidia.com>

--- nemo/collections/asr/parts/utils/decoder_timestamps_utils.py
+++ nemo/collections/asr/parts/utils/decoder_timestamps_utils.py
@@ -21,9 +21,14 @@ import torch
 from omegaconf import OmegaConf
 
 import nemo.collections.asr as nemo_asr
-from nemo.collections.asr.metrics.wer import WER 
-from nemo.collections.asr.parts.submodules.ctc_decoding import CTCDecoding, CTCDecodingConfig, CTCBPEDecoding, CTCBPEDecodingConfig
+from nemo.collections.asr.metrics.wer import WER
 from nemo.collections.asr.models import EncDecCTCModel, EncDecCTCModelBPE
+from nemo.collections.asr.parts.submodules.ctc_decoding import (
+    CTCBPEDecoding,
+    CTCBPEDecodingConfig,
+    CTCDecoding,
+    CTCDecodingConfig,
+)
 from nemo.collections.asr.parts.utils.audio_utils import get_samples
 from nemo.collections.asr.parts.utils.speaker_utils import audio_rttm_map, get_uniqname_from_filepath
 from nemo.collections.asr.parts.utils.streaming_utils import AudioFeatureIterator, FrameBatchASR
