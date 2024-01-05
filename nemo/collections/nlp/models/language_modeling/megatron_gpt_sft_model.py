91f9b8872b704c120b71ded5fc39c9c3ca41d103
Chen Cui <chcui@nvidia.com>
2024-01-05 12:39:04 -0800

    make output (ground truth) field optional for test data
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_sft_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_sft_model.py
@@ -306,6 +306,7 @@ class MegatronGPTSFTModel(NLPAdapterModelMixin, MegatronGPTModel):
                 special_tokens=self.cfg.data.get(
                     'chat_prompt_tokens', None
                 ),  # special tokens for the chat prompts, a dictionary of {token_type: token}. Default: {'system_turn_start': '<extra_id_0>', 'turn_start': '<extra_id_1>', 'label_start': '<extra_id_2>', 'end_of_turn': '\n', "end_of_name": "\n"}
+                is_test=not is_train,
             )
             datasets.append(dataset)
         if is_train:
