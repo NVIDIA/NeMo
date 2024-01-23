26fef85612b96f70bc69909292d7a26af284cb5f
Sangkug Lym <slym@nvidia.com>
2024-01-22 21:50:10 -0800

    Pre-compute max_seqlen and cu_seqlens_argmin in all model-parallel cases
    Signed-off-by: Sangkug Lym <slym@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -882,17 +882,14 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
 
             # Transfer needed data to GPU
             required_keys = set()
-            max_seqlen, cu_seqlens_argmin = None, None
+            max_seqlen = batch.pop('max_seqlen').squeeze() if 'max_seqlen' in batch else None
+            cu_seqlens_argmin = batch.pop('cu_seqlens_argmin') if 'cu_seqlens_argmin' in batch else None
             if parallel_state.get_pipeline_model_parallel_world_size() == 1:
                 required_keys.update(batch.keys())
             else:
                 required_keys.add('attention_mask')
                 if 'cu_seqlens' in batch:
                     required_keys.add('cu_seqlens')
-                    if 'max_seqlen' in batch:
-                        max_seqlen = batch['max_seqlen'].squeeze()
-                    if 'cu_seqlens_argmin' in batch:
-                        cu_seqlens_argmin = batch['cu_seqlens_argmin']
                 if parallel_state.is_pipeline_first_stage():
                     required_keys.update(('tokens', 'position_ids'))
                 if parallel_state.is_pipeline_last_stage():
