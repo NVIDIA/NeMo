4fce638a60e752d96ec0b54c2fed451d50f109e0
Vladimir Bataev <vbataev@nvidia.com>
2024-01-24 19:15:50 +0400

    Clean up
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- nemo/collections/asr/parts/submodules/rnnt_greedy_decoding.py
+++ nemo/collections/asr/parts/submodules/rnnt_greedy_decoding.py
@@ -35,7 +35,6 @@ from omegaconf import DictConfig, OmegaConf
 
 from nemo.collections.asr.modules import rnnt_abstract
 from nemo.collections.asr.parts.submodules.rnnt_greedy_computer import GreedyBatchedRNNTLoopLabelsComputer
-from nemo.collections.asr.parts.submodules.tdt_greedy_computer import GreedyBatchedTDTLoopLabelsComputer
 from nemo.collections.asr.parts.utils import rnnt_utils
 from nemo.collections.asr.parts.utils.asr_confidence_utils import ConfidenceMethodConfig, ConfidenceMethodMixin
 from nemo.collections.common.parts.rnn import label_collate
@@ -2674,7 +2673,6 @@ class GreedyBatchedTDTInfer(_GreedyRNNTInfer):
         preserve_alignments: bool = False,
         preserve_frame_confidence: bool = False,
         confidence_method_cfg: Optional[DictConfig] = None,
-        loop_labels: bool = True,
         allow_jit: bool = True,
     ):
         super().__init__(
@@ -2693,22 +2691,7 @@ class GreedyBatchedTDTInfer(_GreedyRNNTInfer):
         # Depending on availability of `blank_as_pad` support
         # switch between more efficient batch decoding technique
         if self.decoder.blank_as_pad:
-            if loop_labels:
-                self._greedy_decode = self._greedy_decode_blank_as_pad_loop_labels
-                self._decoding_computer = GreedyBatchedTDTLoopLabelsComputer(
-                    decoder=self.decoder,
-                    joint=self.joint,
-                    blank_index=self._blank_index,
-                    max_symbols_per_step=self.max_symbols,
-                    preserve_alignments=preserve_alignments,
-                    preserve_frame_confidence=preserve_frame_confidence,
-                    durations=self.durations,
-                )
-                self._decoding_computer.eval()
-                if allow_jit:
-                    self._decoding_computer = torch.jit.script(self._decoding_computer)
-            else:
-                self._greedy_decode = self._greedy_decode_blank_as_pad_loop_labels
+            self._greedy_decode = self._greedy_decode_blank_as_pad
         else:
             self._greedy_decode = self._greedy_decode_masked
 
@@ -2754,7 +2737,7 @@ class GreedyBatchedTDTInfer(_GreedyRNNTInfer):
 
         return (packed_result,)
 
-    def _greedy_decode_blank_as_pad_loop_frames(
+    def _greedy_decode_blank_as_pad(
         self,
         x: torch.Tensor,
         out_len: torch.Tensor,
@@ -2930,20 +2913,3 @@ class GreedyBatchedTDTInfer(_GreedyRNNTInfer):
         partial_hypotheses: Optional[List[rnnt_utils.Hypothesis]] = None,
     ):
         raise NotImplementedError("masked greedy-batched decode is not supported for TDT models.")
-
-    def _greedy_decode_blank_as_pad_loop_labels(
-        self,
-        x: torch.Tensor,
-        out_len: torch.Tensor,
-        device: torch.device,
-        partial_hypotheses: Optional[List[rnnt_utils.Hypothesis]] = None,
-    ):
-        if partial_hypotheses is not None:
-            raise NotImplementedError("`partial_hypotheses` support is not supported")
-
-        assert self._decoding_computer is not None
-
-        self._decoding_computer.eval()
-        self._decoding_computer.to(x.device)
-        batched_hyps, alignments = self._decoding_computer(x=x, out_len=out_len)
-        return rnnt_utils.batched_hyps_to_hypotheses(batched_hyps, alignments)
