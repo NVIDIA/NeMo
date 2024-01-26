7d92bcc6515f3d457878006452e4cedfc6584ec9
Dmytro Pykhtar <37850217+dimapihtar@users.noreply.github.com>
2024-01-26 22:58:05 +0200

    mcore ds fix (#8253)
    * mcore ds fix
    
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -867,96 +867,25 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         # TODO @tmoon: Use once available in Megatron-LM
         # return DataIteratorList(iters)
 
-    def get_ltor_masks_and_position_ids(
-        self, data, eod_token, reset_position_ids, reset_attention_mask, eod_mask_loss
-    ):
-        """Build masks and position id for left to right model."""
-
-        # Extract batch size and sequence length.
-        micro_batch_size, seq_length = data.size()
-
-        # Attention mask (lower triangular).
-        if reset_attention_mask:
-            att_mask_batch = micro_batch_size
-        else:
-            att_mask_batch = 1
-        attention_mask = torch.tril(torch.ones((att_mask_batch, seq_length, seq_length), device=data.device)).view(
-            att_mask_batch, 1, seq_length, seq_length
-        )
-
-        # Loss mask.
-        loss_mask = torch.ones(data.size(), dtype=torch.float, device=data.device)
-        if eod_mask_loss:
-            loss_mask[data == eod_token] = 0.0
-
-        # Position ids.
-        position_ids = torch.arange(seq_length, dtype=torch.long, device=data.device)
-        position_ids = position_ids.unsqueeze(0).expand_as(data)
-        # We need to clone as the ids will be modifed based on batch index.
-        if reset_position_ids:
-            position_ids = position_ids.clone()
-
-        if reset_position_ids or reset_attention_mask:
-            # Loop through the batches:
-            for b in range(micro_batch_size):
-
-                # Find indecies where EOD token is.
-                eod_index = position_ids[b, data[b] == eod_token]
-                # Detach indecies from positions if going to modify positions.
-                if reset_position_ids:
-                    eod_index = eod_index.clone()
-
-                # Loop through EOD indecies:
-                prev_index = 0
-                for j in range(eod_index.size()[0]):
-                    i = eod_index[j]
-                    # Mask attention loss.
-                    if reset_attention_mask:
-                        attention_mask[b, 0, (i + 1) :, : (i + 1)] = 0
-                    # Reset positions.
-                    if reset_position_ids:
-                        position_ids[b, (i + 1) :] -= i + 1 - prev_index
-                        prev_index = i + 1
-
-        # Convert attention mask to binary:
-        attention_mask = attention_mask < 0.5
-
-        return attention_mask, loss_mask, position_ids
-
     def get_batch(self, data_iterator, tuning):
         """Generate a batch."""
-
-        # return batch for GPT SFT
-        if tuning:
-            return next(data_iterator)
-
-        # Items and their type.
-        keys = ['text']
-        datatype = torch.int64
 
         # Broadcast data.
         if data_iterator is not None:
             data = next(data_iterator)
         else:
             data = None
-        data_b = tensor_parallel.broadcast_data(keys, data, datatype)
 
-        # Unpack.
-        tokens_ = data_b['text'].long()
-        labels = tokens_[:, 1:].contiguous()
-        tokens = tokens_[:, :-1].contiguous()
-
-        # Get the masks and postition ids.
-        attention_mask, loss_mask, position_ids = self.get_ltor_masks_and_position_ids(
-            tokens, self.tokenizer.eos_id, self.reset_position_ids, self.reset_attention_mask, self.eod_mask_loss
-        )
+        # return batch for GPT SFT
+        if tuning:
+            return data
 
         batch = {
-            'tokens': tokens,
-            'labels': labels,
-            'loss_mask': loss_mask,
-            'attention_mask': attention_mask,
-            'position_ids': position_ids,
+            'tokens': data["tokens"],
+            'labels': data["labels"],
+            'loss_mask': data["loss_mask"],
+            'attention_mask': data["attention_mask"],
+            'position_ids': data["position_ids"],
         }
 
         return batch
@@ -1259,6 +1188,10 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             "blend": self.cfg.data.data_prefix,
             "split": self.cfg.data.splits_string,
             "path_to_cache": self.cfg.data.index_mapping_dir,
+            "reset_position_ids": self.reset_position_ids,
+            "reset_attention_mask": self.reset_attention_mask,
+            "eod_mask_loss": self.eod_mask_loss,
+            "eod_id": self.tokenizer.eos_id,
         }
 
         if self.cfg.data.get('add_fim', False):
