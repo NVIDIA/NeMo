aa6d0f6e620179a7baea892de30b63c573d2aa4f
yaoyu-33 <yaoyu.094@gmail.com>
2024-02-15 10:07:22 -0800

    Add HF Gemma converter
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- scripts/nlp_language_modeling/convert_gemma_hf_to_nemo.py
+++ scripts/nlp_language_modeling/convert_gemma_hf_to_nemo.py
@@ -20,26 +20,16 @@ Required to set: `export PYTHONPATH=/path/to/deepmind/models/taurus_pytorch/code
 import contextlib
 import os
 from argparse import ArgumentParser
-
-import torch
-from model.config import get_config_for_2b, get_config_for_7b
-from model.model import CausalLM
-from model.tokenizer import Tokenizer
 from omegaconf import OmegaConf
 
+import torch
+from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
+
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
+from nemo.collections.nlp.modules.common.megatron.utils import get_ltor_masks_and_position_ids
 from nemo.collections.nlp.parts.megatron_trainer_builder import MegatronTrainerBuilder
 from nemo.utils import logging
-
-PAD_TOKEN_ID = -1
-
-
-@contextlib.contextmanager
-def _set_default_tensor_type(dtype: torch.dtype):
-    """Sets the default torch dtype to the given dtype."""
-    torch.set_default_dtype(dtype)
-    yield
-    torch.set_default_dtype(torch.float)
 
 
 def create_rename_keys(num_hidden_layers):
@@ -49,8 +39,12 @@ def create_rename_keys(num_hidden_layers):
         rename_keys.extend([
             (f"model.layers.{i}.self_attn.o_proj.weight",
              f"model.decoder.layers.{i}.self_attention.linear_proj.weight"),
-            (f"model.layers.{i}.self_attn.qkv_proj.weight",
-             f"model.decoder.layers.{i}.self_attention.linear_qkv.weight"),
+            (f"model.layers.{i}.self_attn.q_proj.weight",
+             f"model.decoder.layers.{i}.self_attention.linear_q.weight"),
+            (f"model.layers.{i}.self_attn.k_proj.weight",
+             f"model.decoder.layers.{i}.self_attention.linear_k.weight"),
+            (f"model.layers.{i}.self_attn.v_proj.weight",
+             f"model.decoder.layers.{i}.self_attention.linear_v.weight"),
             # MLP and LayerNorm
             (f"model.layers.{i}.mlp.gate_proj.weight", f"model.decoder.layers.{i}.mlp.linear_fc1_gate.weight"),
             (f"model.layers.{i}.mlp.up_proj.weight", f"model.decoder.layers.{i}.mlp.linear_fc1_proj.weight"),
@@ -63,7 +57,7 @@ def create_rename_keys(num_hidden_layers):
 
     # Non layer dependent keys
     rename_keys.extend([
-        ("embedder.weight", "model.embedding.word_embeddings.weight"),
+        ("model.embed_tokens.weight", "model.embedding.word_embeddings.weight"),
         ("model.norm.weight", "model.decoder.final_layernorm.weight"),
     ])
 
@@ -130,13 +124,15 @@ def adjust_tensor_shapes(model, nemo_state_dict):
             nemo_state_dict[new_key] = torch.cat((gate_weight, proj_weight))
         if 'layernorm.weight' in key_ or 'layer_norm_weight' in key_:
             nemo_state_dict[key_] = nemo_state_dict[key_] + 1.
-        if 'self_attention.linear_qkv.weight' in key_:
-            qkv_weight = nemo_state_dict[key_]
+        if 'self_attention.linear_q.weight' in key_:
+            key_q = key_
+            key_k = key_.replace('linear_q', 'linear_k')
+            key_v = key_.replace('linear_q', 'linear_v')
+            key_qkv = key_.replace('linear_q', 'linear_qkv')
+
             # [(head_num + 2 * num_query_groups) * head_size, hidden_size]
             # -> [head_num, head_size, hidden_size], 2 * [num_query_groups, head_size, hidden_size]
-            q_weight, k_weight, v_weight = qkv_weight.split(
-                [head_num * head_size, num_query_groups * head_size, num_query_groups * head_size], dim=0,
-            )
+            q_weight, k_weight, v_weight = nemo_state_dict[key_q], nemo_state_dict[key_k], nemo_state_dict[key_v]
             q_weight = q_weight.reshape(head_num, head_size, hidden_size)
             k_weight = k_weight.reshape(num_query_groups, head_size, hidden_size)
             v_weight = v_weight.reshape(num_query_groups, head_size, hidden_size)
@@ -147,13 +143,13 @@ def adjust_tensor_shapes(model, nemo_state_dict):
                 qkv_weight = torch.cat((qkv_weight, k_weight[i: i + 1, :, :]))
                 qkv_weight = torch.cat((qkv_weight, v_weight[i: i + 1, :, :]))
             qkv_weight = qkv_weight.reshape([head_size * (head_num + 2 * num_query_groups), hidden_size])
-            nemo_state_dict[key_] = qkv_weight
+            nemo_state_dict[key_qkv] = qkv_weight
+            del nemo_state_dict[key_q], nemo_state_dict[key_k], nemo_state_dict[key_v]
 
     return nemo_state_dict
 
 
 def adjust_nemo_config(model_config, ref_config):
-    model_config.tokenizer["model"] = ref_config["tokenizer"]  # ref_config["_input_name_or_path"]
     model_config["encoder_seq_length"] = ref_config["max_position_embeddings"]
     model_config["num_layers"] = ref_config["num_hidden_layers"]
     model_config["ffn_hidden_size"] = ref_config["intermediate_size"]
@@ -161,8 +157,6 @@ def adjust_nemo_config(model_config, ref_config):
     model_config["num_attention_heads"] = ref_config["num_attention_heads"]
     model_config["num_query_groups"] = ref_config["num_key_value_heads"]
     model_config["kv_channels"] = ref_config["head_dim"]
-    if "model_vocab_size" in ref_config:
-        model_config["override_vocab_size"] = ref_config["model_vocab_size"]
     model_config["layernorm_epsilon"] = ref_config["rms_norm_eps"]
     return model_config
 
@@ -170,7 +164,7 @@ def adjust_nemo_config(model_config, ref_config):
 def get_args():
     parser = ArgumentParser()
     parser.add_argument("--input_name_or_path", type=str)
-    parser.add_argument("--tokenizer_path", default=None, type=str)
+    parser.add_argument("--tokenizer_path", type=str)
     parser.add_argument(
         "--hparams_file",
         type=str,
@@ -189,112 +183,63 @@ def get_args():
 
 
 def convert(args):
-    logging.info(f"Loading checkpoint from PyT Gemma: `{args.input_name_or_path}`")
-    if "2b" in args.input_name_or_path:
-        pyt_config = get_config_for_2b()
-    else:
-        pyt_config = get_config_for_7b()
-    if args.tokenizer_path is not None:
-        pyt_config.tokenizer = args.tokenizer_path
-
-    device = torch.device("cuda")
-    pyt_config.dtype = "bfloat16" if args.precision == "bf16" else "float32"
-
-    with _set_default_tensor_type(pyt_config.get_dtype()):
-        pyt_model = CausalLM(pyt_config)
-        pyt_model.load_weights(args.input_name_or_path)
-        pyt_model = pyt_model.to(device).eval()
-    tokenizer = Tokenizer(pyt_config.tokenizer)
-    logging.info("Model loading done")
+    logging.info(f"Loading checkpoint from HF Gemma: `{args.input_name_or_path}`")
+    hf_tokenizer = AutoTokenizer.from_pretrained(args.input_name_or_path)
+    hf_model = AutoModelForCausalLM.from_pretrained(args.input_name_or_path)
+    logging.info("HF Model loading done.")
 
     nemo_config = OmegaConf.load(args.hparams_file)
-    nemo_config.model = adjust_nemo_config(nemo_config.model, pyt_config.__dict__)
+    nemo_config.model = adjust_nemo_config(nemo_config.model, hf_model.config.__dict__)
+    nemo_config.model.tokenizer["model"] = args.tokenizer_path
 
     nemo_config.trainer["precision"] = args.precision
     trainer = MegatronTrainerBuilder(nemo_config).create_trainer()
     model = MegatronGPTModel(nemo_config.model, trainer)
 
     rename_keys = create_rename_keys(nemo_config.model.num_layers)
-    old_state_dict = pyt_model.state_dict()
+    old_state_dict = hf_model.state_dict()
     new_state_dict = rename_model_keys(model_state_dict=old_state_dict, rename_keys=rename_keys)
 
     nemo_state_dict = adjust_tensor_shapes(model, new_state_dict)
     model.load_state_dict(nemo_state_dict, strict=False)
 
-    logging.info(f'=' * 50)
-
-    # Mock inputs
-    prompts = [
-        "The capital of France is",
+    logging.info(f'=' * 100)
+    # Verifications
+    input_texts = [
+        'query: how much protein should a female eat',
     ]
-    top_ps = [1.0]
-    top_ks = [1]
-    output_len = 1
-    batch_size = len(prompts)
+    logging.info(f"Running verifications {input_texts} ...")
 
-    prompt_tokens = [tokenizer.encode(prompt) for prompt in prompts]
-    min_prompt_len = min(len(p) for p in prompt_tokens)
-    max_prompt_len = max(len(p) for p in prompt_tokens)
-    max_seq_len = max_prompt_len + output_len
+    # Tokenize the input texts
+    hf_tokenizer.pad_token = hf_tokenizer.eos_token
+    batch_dict = hf_tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
+    batch_dict_cuda = {k: v.cuda() for k, v in batch_dict.items()}
+    hf_model = hf_model.cuda().eval()
+    model = model.eval()
 
-    # build KV caches
-    kv_caches = []
-    for _ in range(pyt_config.num_hidden_layers):
-        k_cache = torch.zeros(
-            size=(batch_size, max_seq_len,
-                  pyt_config.num_key_value_heads, pyt_config.head_dim),
-            dtype=pyt_config.get_dtype(),
-            device=device,
-        )
-        v_cache = torch.zeros(
-            size=(batch_size, max_seq_len,
-                  pyt_config.num_key_value_heads, pyt_config.head_dim),
-            dtype=pyt_config.get_dtype(),
-            device=device,
-        )
-        kv_caches.append((k_cache, v_cache))
+    hf_outputs = hf_model(**batch_dict_cuda, output_hidden_states=True)
+    ids = batch_dict_cuda['input_ids']
 
-    # prepare inputs
-    token_ids_tensor = torch.full(
-        (batch_size, max_seq_len), PAD_TOKEN_ID, dtype=torch.int64
-    )
-    input_token_ids_tensor = torch.full(
-        (batch_size, min_prompt_len), PAD_TOKEN_ID, dtype=torch.int64
-    )
-    for i, p in enumerate(prompt_tokens):
-        token_ids_tensor[i, : len(p)] = torch.tensor(p)
-        input_token_ids_tensor[i, :min_prompt_len] = torch.tensor(
-            p[:min_prompt_len]
-        )
-    token_ids_tensor = token_ids_tensor.to(device)
-    prompt_mask_tensor = token_ids_tensor != PAD_TOKEN_ID
-    input_token_ids_tensor = input_token_ids_tensor.to(device)
-    input_positions_tensor = torch.arange(
-        0, min_prompt_len, dtype=torch.int64
-    ).to(device)
-    mask_tensor = torch.full((1, 1, max_seq_len, max_seq_len), -2.3819763e38).to(
-        torch.float
-    )
-    mask_tensor = torch.triu(mask_tensor, diagonal=1).to(device)
-    curr_mask_tensor = mask_tensor.index_select(2, input_positions_tensor)
-    output_positions_tensor = torch.LongTensor([min_prompt_len - 1]).to(device)
-    top_ps_tensor = torch.FloatTensor(top_ps).to(device)
-    top_ks_tensor = torch.LongTensor(top_ks).to(device)
+    id_tensors = [torch.unsqueeze(torch.LongTensor(id_list), dim=0) for id_list in ids.cpu()]
 
-    pyt_outputs = pyt_model(
-        input_token_ids=input_token_ids_tensor,
-        input_positions=input_positions_tensor,
-        kv_write_indices=None,
-        kv_caches=kv_caches,
-        mask=curr_mask_tensor,
-        output_positions=output_positions_tensor,
-        temperatures=None,
-        top_ps=top_ps_tensor,
-        top_ks=top_ks_tensor,
-    )
-    nemo_outputs = model(tokens=input_token_ids_tensor, text_position_ids=input_positions_tensor,
-                         attention_mask=curr_mask_tensor, labels=None)
-    assert torch.argmax(nemo_outputs[0, -1], dim=-1) == pyt_outputs, "Predicted next token not match."
+    masks_and_position_ids = [
+        get_ltor_masks_and_position_ids(id_tensor, hf_tokenizer.eos_token, False, False, False)
+        for id_tensor in id_tensors
+    ]
+    for tokens, attn_mask_and_pos_ids in zip(id_tensors, masks_and_position_ids):
+        attn_mask, _, pos_ids = attn_mask_and_pos_ids
+
+        outputs = model(tokens=tokens, text_position_ids=pos_ids.cuda(), attention_mask=attn_mask.cuda(), labels=None)
+
+    hf_next_token = hf_outputs.logits[0, -1].argmax()
+    next_token = outputs.squeeze()[-1].argmax()
+
+    logging.info(f"HF predicted next token is: '{hf_tokenizer._convert_id_to_token(hf_next_token)}'.")
+    logging.info(f"NeMo predicted next token is: '{hf_tokenizer._convert_id_to_token(next_token)}'.")
+    assert (
+        hf_next_token == next_token
+    ), f'prediction mismatch: {hf_tokenizer.decode(hf_next_token)} != {hf_tokenizer.decode(next_token)}'
+    logging.info(f'=' * 100)
 
     model.save_to(args.output_path)
     logging.info(f'NeMo model saved to: {args.output_path}')
