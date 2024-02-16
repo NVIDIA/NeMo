1ee5b9057cadc50633dea51243bc076d3248671d
Pratyush Muthukumar <pmuthukumar@nvidia.com>
2024-02-16 11:52:04 -0800

    video pretraining

--- nemo/collections/multimodal/data/videoneva/videoneva_dataset.py
+++ nemo/collections/multimodal/data/videoneva/videoneva_dataset.py
@@ -54,68 +54,91 @@ from nemo.collections.multimodal.models.multimodal_llm.videoneva.multimodal_enco
     LanguageBindVideoProcessor,
 )
 from nemo.collections.nlp.modules.common.megatron.utils import get_ltor_masks_and_position_ids
+from nemo.collections.multimodal.data.neva.neva_dataset import TarOrFolderImageLoader
+
 
 MAX_NUM_IMAGES = 1
 MAX_NUM_VIDEOS = 1
 IGNORE_INDEX = -1
 
 
-class TarOrFolderVisualLoader:
+class TarOrFolderVideoLoader:
     """
-    A class for loading images and videos from a tar archive or a regular folder.
+    A class for loading videos from a tar archive or a regular folder.
 
-    This class provides functionality to open and read images and videos from either a tar archive
-    (.tar file) or a standard directory with image or video files. It builds an index of images and
-    videos if the source is a tar archive for efficient access.
+    This class provides functionality to open and read videos from either a tar archive
+    (.tar file) or a standard directory with video files. It builds an index of videos
+    if the source is a tar archive for efficient access.
 
     Attributes:
-        visual_folder (str): The path to the tar archive or video/image folder.
+        video_folder (str): The path to the tar archive or video folder.
         tar_index (dict): A dictionary that maps file names to their tarfile member
-                          objects if the video/image source is a tar archive.
+                          objects if the video source is a tar archive.
+        data_config (dict): Dataset configuration parameters
 
     Methods:
-        __init__(self, image_folder): Initializes the loader with the specified video/image folder.
-        build_index(self): Builds an index of video/image file names and their corresponding
+        __init__(self, video_folder, data_config): Initializes the loader with the specified video folder.
+        build_index(self): Builds an index of video file names and their corresponding
                            tarfile member objects for a tar archive.
-        open_image(self, file_name): Opens and returns an image by its file name. The image
-                                     is returned as an RGB PIL Image object.
         open_video(self, file_name): Opens and returns a video by its file name. The video
                                      is returned as an OpenCV VideoCapture object.
     """
 
-    def __init__(self, visual_folder):
-        self.visual_folder = visual_folder
+    def __init__(self, video_folder, data_config):
+        self.video_folder = video_folder
+        self.data_config = data_config
         self.tar_index = {}
-        if self.visual_folder.endswith('.tar'):
+        if self.video_folder.endswith('.tar'):
             self.build_index()
 
     def build_index(self):
-        with tarfile.open(self.visual_folder, 'r') as tar:
+        with tarfile.open(self.video_folder, 'r') as tar:
             for member in tar.getmembers():
                 self.tar_index[member.name] = member
 
-    def open_image(self, file_name):
-        if self.visual_folder.endswith('.tar'):
-            with tarfile.open(self.visual_folder, 'r') as tar:
-                member = self.tar_index.get(file_name)
-                if member:
-                    f = tar.extractfile(member)
-                    return Image.open(f).convert('RGB')
-        else:
-            return Image.open(os.path.join(self.visual_folder, file_name)).convert('RGB')
-        return None
-
     def open_video(self, file_name):
-        if self.visual_folder.endswith('.tar'):
-            with tarfile.open(self.visual_folder, 'r') as tar:
+        if self.video_folder.endswith('.tar'):
+            with tarfile.open(self.video_folder, 'r') as tar:
                 member = self.tar_index.get(file_name)
                 if member:
                     f = tar.extractfile(member)
                     video_data = np.frombuffer(f.read(), dtype=np.uint8)
                     return cv2.imdecode(video_data, cv2.IMREAD_UNCHANGED)
         else:
-            return cv2.VideoCapture(os.path.join(self.visual_folder, file_name))
+            return cv2.VideoCapture(os.path.join(self.video_folder, file_name))
         return None
+    
+    def flatten_frames(self, cap):
+        num_frames = self.data_config['num_frames']
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        if self.data_config['splice_single_frame'] == 'first': # represent vid as first frame only
+            frame_indices = [0]
+        elif self.data_config['splice_single_frame'] == 'last': # represent vid as last frame only
+            frame_indices = [-1]
+        elif self.data_config['splice_single_frame'] == 'middle': # represent vid as middle frame only
+            frame_indices = total_frames // 2
+        else: # represent vid as num_frames
+            frame_indices = np.linspace(0, total_frames - 1, num_frames, dtype=int)
+        
+        frames = []
+
+        for index in frame_indices:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, index)
+            ret, frame = cap.read()
+
+            if not ret:
+                print(f"Error: Could not read frame at index {index}.")
+                break
+
+            frames.append(frame)
+
+        cap.release()
+        frames_array = np.array(frames) # (F, H, W, C), F = num_frames, H,W,C are img dims
+
+        frames_array = np.expand_dims(np.transpose(frames_array, (0, 1, 4, 2, 3)), axis=0) # (T_img, F, C, H, W), T_img = 1
+
+        return frames_array # will be bundled with batch size to produce (B, T_img, F, C, H, W)
 
 
 def tokenize(
@@ -551,7 +574,7 @@ def preprocess_plain(sources, tokenizer, cfg,) -> Dict:
 class LazySupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
 
-    def __init__(self, data_path: str, tokenizer, multimodal_cfg: dict):
+    def __init__(self, data_path: str, tokenizer, multimodal_cfg: dict, data_cfg: dict):
         super(LazySupervisedDataset, self).__init__()
         logging.warning("Loading data...")
         if data_path is not None:
@@ -565,14 +588,12 @@ class LazySupervisedDataset(Dataset):
         self.tokenizer = tokenizer
         self.list_data_dict = list_data_dict
         self.multimodal_cfg = multimodal_cfg
+        self.data_cfg = data_cfg
         self.conv_template = multimodal_cfg["conv_template"]
-        self.image_folder = multimodal_cfg['image_folder']
         self.video_folder = multimodal_cfg['video_folder']
-        self.image_processor = multimodal_cfg["image_processor"]
-        self.video_processor = multimodal_cfg["video_processor"]
+        self.processor = multimodal_cfg["image_processor"]
 
-        self.image_loader = TarOrFolderVisualLoader(self.image_folder)
-        self.video_loader = TarOrFolderVisualLoader(self.video_folder)
+        self.video_loader = TarOrFolderVideoLoader(self.video_folder)
 
     def __len__(self):
         return len(self.list_data_dict)
@@ -582,23 +603,24 @@ class LazySupervisedDataset(Dataset):
         if isinstance(i, int):
             sources = [sources]
         assert len(sources) == 1, "Don't know why it is wrapped to a list"  # FIXME
-        if 'image' in sources[0] and 'video' not in sources:
+        if 'image' in sources[0]:
             if not isinstance(self.list_data_dict[i]['image'], list):
                 self.list_data_dict[i]['image'] = [self.list_data_dict[i]['image']]
 
             images = []
-            for image_file in self.list_data_dict[i]['image']:
-                image = self.image_loader.open_image(image_file)
+            for video_file in self.list_data_dict[i]['image']:
+                video = self.video_loader.open_video(image_file)
+                image_frames = self.video_loader.flatten_frames(video)
                 if image is None:
                     logging.warning(f"Image {image_file} could not be found!")
-                if isinstance(self.image_processor, CLIPImageProcessor):
+                if isinstance(self.processor, CLIPImageProcessor):
                     # image processor from HF
                     if self.multimodal_cfg['image_aspect_ratio'] == 'keep':
                         max_hw, min_hw = max(image.size), min(image.size)
                         aspect_ratio = max_hw / min_hw
                         max_len, min_len = 448, 224
                         shortest_edge = int(min(max_len / aspect_ratio, min_len))
-                        image = self.image_processor.preprocess(
+                        image = self.processor.preprocess(
                             image, return_tensors='pt', do_center_crop=False, size={"shortest_edge": shortest_edge}
                         )['pixel_values'][0]
                     elif self.multimodal_cfg['image_aspect_ratio'] == 'pad':
@@ -616,127 +638,17 @@ class LazySupervisedDataset(Dataset):
                                 result.paste(pil_img, ((height - width) // 2, 0))
                                 return result
 
-                        image = expand2square(image, tuple(int(x * 255) for x in self.image_processor.image_mean))
-                        image = self.image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+                        image = expand2square(image, tuple(int(x * 255) for x in self.processor.image_mean))
+                        image = self.processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
                     else:
-                        image = self.image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+                        image = self.processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
                 else:
                     assert (
                         self.multimodal_cfg['image_aspect_ratio'] == 'square'
                     ), 'NeMo image transform with setting `image_aspect_ratio` to `square`.'
-                    image = self.image_processor(image)
+                    image = self.processor(image)
                 images.append(image)
             images_tensors = torch.tensor([])
-            if images:
-                images_tensors = torch.stack(images)
-                cur_token_len = (images_tensors[0].shape[1] // 14) * (
-                    images_tensors[0].shape[2] // 14
-                )  # FIXME: 14 is hardcoded patch size
-                sources = preprocess_multimodal(
-                    copy.deepcopy(sources),
-                    self.multimodal_cfg,
-                    cur_token_len,
-                    use_plain=(self.conv_template == "plain"),
-                )
-        elif 'video' in sources[0] and 'image' not in sources[0]:
-            if not isinstance(self.list_data_dict[i]['video'], list):
-                self.list_data_dict[i]['video'] = [self.list_data_dict[i]['video']]
-
-            videos = []
-            for video_file in self.list_data_dict[i]['video']:
-                video = self.video_loader.open_video(video_file)
-                if video is None:
-                    logging.warning(f"Image {video_file} could not be found!")
-                if isinstance(self.video_processor, LanguageBindVideoProcessor):
-                    video = self.image_processor.preprocess(video, return_tensors='pt')['pixel_values'][0]
-                else:
-                    video = self.video_processor(video)
-                videos.append(video)
-            videos_tensor = torch.tensor([])
-            if videos:
-                videos_tensors = torch.stack(videos)
-                cur_token_len = (videos_tensor[0].shape[1] // 14) * (
-                    videos_tensors[0].shape[2] // 14
-                )  # FIXME: 14 is hardcoded patch size
-                sources = preprocess_multimodal(
-                    copy.deepcopy(sources),
-                    self.multimodal_cfg,
-                    cur_token_len,
-                    use_plain=(self.conv_template == "plain"),
-                )
-        elif 'image' in sources[0] and 'video' in sources[0]:
-            if not isinstance(self.list_data_dict[i]['video'], list):
-                self.list_data_dict[i]['video'] = [self.list_data_dict[i]['video']]
-
-            if not isinstance(self.list_data_dict[i]['image'], list):
-                self.list_data_dict[i]['image'] = [self.list_data_dict[i]['image']]
-
-            videos = []
-            for video_file in self.list_data_dict[i]['video']:
-                video = self.video_loader.open_video(video_file)
-                if video is None:
-                    logging.warning(f"Image {video_file} could not be found!")
-                if isinstance(self.video_processor, LanguageBindVideoProcessor):
-                    video = self.image_processor.preprocess(video, return_tensors='pt')['pixel_values'][0]
-                else:
-                    video = self.video_processor(video)
-                videos.append(video)
-            videos_tensor = torch.tensor([])
-
-            images = []
-            for image_file in self.list_data_dict[i]['image']:
-                image = self.image_loader.open_image(image_file)
-                if image is None:
-                    logging.warning(f"Image {image_file} could not be found!")
-                if isinstance(self.image_processor, CLIPImageProcessor):
-                    # image processor from HF
-                    if self.multimodal_cfg['image_aspect_ratio'] == 'keep':
-                        max_hw, min_hw = max(image.size), min(image.size)
-                        aspect_ratio = max_hw / min_hw
-                        max_len, min_len = 448, 224
-                        shortest_edge = int(min(max_len / aspect_ratio, min_len))
-                        image = self.image_processor.preprocess(
-                            image, return_tensors='pt', do_center_crop=False, size={"shortest_edge": shortest_edge}
-                        )['pixel_values'][0]
-                    elif self.multimodal_cfg['image_aspect_ratio'] == 'pad':
-
-                        def expand2square(pil_img, background_color):
-                            width, height = pil_img.size
-                            if width == height:
-                                return pil_img
-                            elif width > height:
-                                result = Image.new(pil_img.mode, (width, width), background_color)
-                                result.paste(pil_img, (0, (width - height) // 2))
-                                return result
-                            else:
-                                result = Image.new(pil_img.mode, (height, height), background_color)
-                                result.paste(pil_img, ((height - width) // 2, 0))
-                                return result
-
-                        image = expand2square(image, tuple(int(x * 255) for x in self.image_processor.image_mean))
-                        image = self.image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
-                    else:
-                        image = self.image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
-                else:
-                    assert (
-                        self.multimodal_cfg['image_aspect_ratio'] == 'square'
-                    ), 'NeMo image transform with setting `image_aspect_ratio` to `square`.'
-                    image = self.image_processor(image)
-                images.append(image)
-            images_tensors = torch.tensor([])
-
-            if videos:
-                videos_tensors = torch.stack(videos)
-                cur_token_len = (videos_tensor[0].shape[1] // 14) * (
-                    videos_tensors[0].shape[2] // 14
-                )  # FIXME: 14 is hardcoded patch size
-                sources = preprocess_multimodal(
-                    copy.deepcopy(sources),
-                    self.multimodal_cfg,
-                    cur_token_len,
-                    use_plain=(self.conv_template == "plain"),
-                )
-
             if images:
                 images_tensors = torch.stack(images)
                 cur_token_len = (images_tensors[0].shape[1] // 14) * (
@@ -750,7 +662,6 @@ class LazySupervisedDataset(Dataset):
                 )
         else:
             images_tensors = torch.tensor([])
-            videos_tensor = torch.tensor([])
             sources = copy.deepcopy(sources)
 
         if self.conv_template == "nvgpt":
@@ -769,19 +680,16 @@ class LazySupervisedDataset(Dataset):
 
         # image exist in the data
         if self.multimodal_cfg['is_multimodal']:
-            if "images" in sources[0]:
-                if isinstance(self.image_processor, CLIPImageProcessor):
-                    crop_size = [self.image_processor.crop_size['height'], self.image_processor.crop_size['width']]
-                else:
-                    crop_size = self.multimodal_cfg['crop_size']
-                # image does not exist in the data, but the model is multimodal
-                zero_padding = torch.zeros(
-                    (MAX_NUM_IMAGES - len(images_tensors), 3, crop_size[0], crop_size[1]), dtype=torch.float
-                )
-                images_tensors = torch.cat((images_tensors, zero_padding), dim=0)
-                data_dict['image'] = images_tensors
-            if "videos" in sources[0]:
-                data_dict['video'] = videos_tensors
+            if isinstance(self.processor, CLIPImageProcessor):
+                crop_size = [self.processor.crop_size['height'], self.processor.crop_size['width']]
+            else:
+                crop_size = self.multimodal_cfg['crop_size']
+            # image does not exist in the data, but the model is multimodal
+            zero_padding = torch.zeros(
+                (data_cfg.num_frames - len(images_tensors), 3, crop_size[0], crop_size[1]), dtype=torch.float
+            ) # data_cfg.num_frames instead of MAX_NUM_IMAGES
+            images_tensors = torch.cat((images_tensors, zero_padding), dim=0)
+            data_dict['image'] = images_tensors
         return data_dict
 
 
@@ -797,7 +705,6 @@ class NevaDataset(LazySupervisedDataset):
             super(NevaDataset, self).__init__(None, tokenizer, multimodal_cfg)
             logging.warning("Loading image inputs from SteerLM Dataset")
             image_folder = multimodal_cfg['image_folder']
-            video_folder = multimodal_cfg['video_folder']
             for line in open(data_path, "r"):
                 record = json.loads(line)
 
@@ -899,7 +806,7 @@ def make_supervised_data_module(tokenizer, model_cfg) -> Dict:
             conv_template=data_cfg.get("conv_template", "nvgpt"),
             crop_size=crop_size,
             image_token_len=data_cfg.image_token_len,
-            image_folder=data_cfg.image_folder,
+            video_folder=data_cfg.video_folder,
             image_aspect_ratio=data_cfg.image_aspect_ratio,
             use_im_start_end=getattr(model_cfg.mm_cfg, 'use_im_start_end', False),
             image_processor=image_processor,
