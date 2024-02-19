855f58f523bf963763cacd99acd82f03c5b97882
Abhishree <abhishreetm@gmail.com>
2024-02-19 14:56:41 -0800

    Change to new dataloader_iter API for MultiModal
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- nemo/collections/multimodal/models/text_to_image/dreambooth/dreambooth.py
+++ nemo/collections/multimodal/models/text_to_image/dreambooth/dreambooth.py
@@ -241,7 +241,7 @@ class MegatronDreamBooth(NLPAdapterModelMixin, MegatronBaseModel):
         output_tensor = self.model(batch)
         return output_tensor
 
-    def fwd_bwd_step(self, dataloader_iter, batch_idx, forward_only):
+    def fwd_bwd_step(self, dataloader_iter, forward_only):
         tensor_shape = None  # Placeholder
 
         # handle asynchronous grad reduction
@@ -290,7 +290,7 @@ class MegatronDreamBooth(NLPAdapterModelMixin, MegatronBaseModel):
 
         return loss_mean, loss_dict
 
-    def training_step(self, dataloader_iter, batch_idx):
+    def training_step(self, dataloader_iter):
         """
             Our dataloaders produce a micro-batch and then we fetch
             a number of microbatches depending on the global batch size and model parallel size
@@ -303,7 +303,7 @@ class MegatronDreamBooth(NLPAdapterModelMixin, MegatronBaseModel):
         # we zero grads here because we also call backward in the apex fwd/bwd functions
         self._optimizer.zero_grad()
 
-        loss_mean, loss_dict = self.fwd_bwd_step(dataloader_iter, batch_idx, False)
+        loss_mean, loss_dict = self.fwd_bwd_step(dataloader_iter, False)
 
         torch.distributed.broadcast(loss_mean, get_last_rank())
 
@@ -344,8 +344,8 @@ class MegatronDreamBooth(NLPAdapterModelMixin, MegatronBaseModel):
         )
         return loss_mean
 
-    def validation_step(self, dataloader_iter, batch_idx):
-        loss, val_loss_dict = self.fwd_bwd_step(dataloader_iter, batch_idx, True)
+    def validation_step(self, dataloader_iter):
+        loss, val_loss_dict = self.fwd_bwd_step(dataloader_iter, True)
 
         self.log_dict(val_loss_dict, prog_bar=False, logger=True, on_step=False, on_epoch=True, batch_size=1)
 
@@ -394,7 +394,7 @@ class MegatronDreamBooth(NLPAdapterModelMixin, MegatronBaseModel):
             return images, cond
 
         def fwd_output_and_loss_func(dataloader_iter, model):
-            batch = next(dataloader_iter)
+            batch, _, _ = next(dataloader_iter)
             batch = process_batch(batch)
             batch = [x.cuda(non_blocking=True) for x in batch]
             loss = model(batch)
