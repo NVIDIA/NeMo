eb8fa3110d419fa8ebaff26c15a92b9176ef7fc3
Agoniii <815244047@qq.com>
2024-02-27 20:03:26 +0800

    support chatglm2&3
    Signed-off-by: Agoniii <815244047@qq.com>

--- nemo/collections/nlp/modules/common/tokenizer_utils.py
+++ nemo/collections/nlp/modules/common/tokenizer_utils.py
@@ -152,6 +152,7 @@ def get_nmt_tokenizer(
     r2l: Optional[bool] = False,
     legacy: Optional[bool] = False,
     delimiter: Optional[str] = None,
+    trust_remote_code: Optional[bool] = False,
 ):
     """
     Args:
@@ -186,6 +187,7 @@ def get_nmt_tokenizer(
             merges_file=merges_file,
             **special_tokens_dict,
             use_fast=use_fast,
+            trust_remote_code=trust_remote_code
         )
     elif library == 'sentencepiece':
         logging.info(f'Getting SentencePiece with model: {tokenizer_model}')
