d14f7cab2a95282a49574324d9d2f6f1d20ce913
Michal Futrega <mfutrega@nvidia.com>
2024-03-01 09:52:58 +0100

    Update attention.py
    Signed-off-by: Michal Futrega <mfutrega@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/attention.py
+++ nemo/collections/nlp/modules/common/megatron/attention.py
@@ -71,7 +71,7 @@ try:
     from flash_attn.flash_attn_triton import flash_attn_func as flash_attn_func_triton
 
     # pinned triton version for flash-attention triton https://github.com/HazyResearch/flash-attention/blob/main/flash_attn/flash_attn_triton.py#L3
-    assert pkg_resources.get_distribution("pytorch-triton").version == '2.0.0.dev20221202'
+    assert pkg_resources.get_distribution("triton").version == '2.0.0.dev20221202'
 
 except (ImportError, ModuleNotFoundError, AssertionError, pkg_resources.DistributionNotFound):
 
