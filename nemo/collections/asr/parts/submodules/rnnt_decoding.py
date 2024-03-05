da8d6a1dbfecea7a9df48507b56c0b22f775e035
Isaac McFadyen <isaac@imcf.me>
2024-03-05 12:48:29 -0500

    Fixed missing copy import in rnnt_decoder.py (#8580)
    * Added copy import to rnnt_decoding.py
    
    Signed-off-by: Isaac McFadyen <isaac@imcf.me>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Isaac McFadyen <isaac@imcf.me>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>

--- nemo/collections/asr/parts/submodules/rnnt_decoding.py
+++ nemo/collections/asr/parts/submodules/rnnt_decoding.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import copy
 import re
 from abc import abstractmethod
 from dataclasses import dataclass, field, is_dataclass
