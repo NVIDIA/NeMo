9c7ee6c59fcdfc2331b3fc854ff1060d143ed31b
Jan Baczek <jbaczek@nvidia.com>
2024-03-22 11:08:41 +0100

    Adjust to recent mcore api change

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -1480,7 +1480,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
     ) -> OutputType:
 
         # check whether the DDP is initialized
-        if parallel_state.is_unitialized():
+        if not parallel_state.is_initialized():
 
             def dummy():
                 return
