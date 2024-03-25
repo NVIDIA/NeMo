aa54375575bf85089ce87706c9806dc6b8a1fe5f
Onur Yilmaz <oyilmaz@nvidia.com>
2024-03-25 12:48:44 -0400

    Added/fixed license headers
    Signed-off-by: Onur Yilmaz <oyilmaz@nvidia.com>

--- nemo/export/trt_llm/decoder/falcon.py
+++ nemo/export/trt_llm/decoder/falcon.py
@@ -1,12 +1,16 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
 #
-# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
-# property and proprietary rights in and to this material, related
-# documentation and any modifications thereto. Any use, reproduction,
-# disclosure or distribution of this material and related documentation
-# without an express license agreement from NVIDIA CORPORATION or
-# its affiliates is strictly prohibited.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """The FALCON decoder implementation."""
 
