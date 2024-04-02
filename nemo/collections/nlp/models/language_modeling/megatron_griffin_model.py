4cffa7217333696b5fd5cdc9e9bce816d06d6eca
ataghibakhsh <ataghibakhsh@nvidia.com>
2024-04-02 07:33:16 -0700

    remove unnecessary imports

--- nemo/collections/nlp/models/language_modeling/megatron_griffin_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_griffin_model.py
@@ -27,13 +27,6 @@ from nemo.collections.nlp.data.language_modeling.megatron.data_samplers import (
     MegatronPretrainingRandomSampler,
     MegatronPretrainingSampler,
 )
-from nemo.collections.nlp.models.language_modeling.megatron.bert.bert_model import (
-    MCoreBertModelWrapperWithPostLNSupport,
-    NeMoBertModel,
-)
-from nemo.collections.nlp.models.language_modeling.megatron.bert.bert_spec import (
-    bert_layer_with_transformer_engine_spec_postln,
-)
 from nemo.collections.nlp.models.language_modeling.megatron_base_model import MegatronBaseModel
 from nemo.collections.nlp.modules.common.megatron.build_model import build_model
 from nemo.collections.nlp.modules.common.megatron.module import Float16Module
@@ -67,7 +60,6 @@ except (ImportError, ModuleNotFoundError):
 
 try:
     from megatron.core import parallel_state
-    from megatron.core.models.bert.bert_layer_specs import bert_layer_with_transformer_engine_spec
     from megatron.core.pipeline_parallel.schedules import get_forward_backward_func
     from megatron.core.transformer.module import Float16Module as MCoreFloat16Module
     from megatron.core.transformer.transformer_config import TransformerConfig
