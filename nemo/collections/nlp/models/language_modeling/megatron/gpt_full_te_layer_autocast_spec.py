e4b2139dab7e745b3321ff1899e73c3d4e38e53c
Vasudevan Rengasamy <vrengasamy@nvidia.com>
2024-04-03 12:27:30 -0700

    Bug fix + cleanup
    Signed-off-by: Vasudevan Rengasamy <vrengasamy@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron/gpt_full_te_layer_autocast_spec.py
+++ nemo/collections/nlp/models/language_modeling/megatron/gpt_full_te_layer_autocast_spec.py
@@ -271,7 +271,7 @@ class TETransformerLayerAutocast(AutocastTransformerLayer, BaseTransformerLayer)
         self.is_first_microbatch = False
         context = None
 
-        return hidden_states  # , context
+        return hidden_states
 
     def _get_layer_offset(self):
 
