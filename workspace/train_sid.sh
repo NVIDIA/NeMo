55f5be071238ff6f2532620dfc7ab7073628aa8f
stevehuang52 <heh@nvidia.com>
2024-04-09 20:17:33 -0400

    update sid
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- workspace/train_sid.sh
+++ workspace/train_sid.sh
@@ -3,7 +3,7 @@ NEMO_BASEPATH="/home/heh/codes/nemo-ssl"
 export PYTHONPATH=$NEMO_BASEPATH:$PYTHONPATH
 
 vox1_dir="/media/data2/datasets/speaker_datasets/voxceleb1"
-vox2_dir="/media/data2/datasets/voxceleb2"
+vox2_dir="/media/data2/datasets/speaker_datasets/voxceleb2"
 train_manifests="[${vox1_dir}/vox1_train_manifest_train_chunk3s.json,${vox2_dir}/vox2_all_manifest_train_chunk3s.json]"
 dev_manifests="[${vox1_dir}/vox1_train_manifest_val_chunk30s.json,${vox2_dir}/vox2_all_manifest_val_chunk30s.json]"
 
@@ -15,10 +15,10 @@ POSTFIX=epoch49
 
 SSL_CKPT="/home/heh/codes/nemo-ssl/workspace/nemo_experiments/pretrained_checkpoints/oci_ll_unlab-60k_bs2048_adamwlr0.004_wd1e-3_warmup25000_epoch1000_mask0.01x40pre_conv_wavLM0.2x0.1_n16_r5--val_loss5.0808-epoch43-last.ckpt"
 
-batch_size=4
+batch_size=128
 num_workers=4
 
-CUDA_VISIBLE_DEVICES="0" python speaker_id_train.py \
+CUDA_VISIBLE_DEVICES="0,1" python speaker_id_train.py \
     --config-path="configs" \
     --config-name="ecapa_tdnn_small_ssl" \
     ++init_from_ptl_ckpt=$SSL_CKPT \
@@ -30,7 +30,7 @@ CUDA_VISIBLE_DEVICES="0" python speaker_id_train.py \
     ++model.validation_ds.min_duration=0.3 \
     model.validation_ds.manifest_filepath=$dev_manifests \
     model.train_ds.batch_size=$batch_size \
-    model.validation_ds.batch_size=$batch_size \
+    model.validation_ds.batch_size=32 \
     model.train_ds.num_workers=$num_workers \
     model.validation_ds.num_workers=$num_workers \
     exp_manager.checkpoint_callback_params.save_top_k=1 \
