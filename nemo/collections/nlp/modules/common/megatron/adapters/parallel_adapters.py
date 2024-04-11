67c5d5f448e7a674e0161a14633104185504e719
Anmol Gupta <14880251+anmolgupt@users.noreply.github.com>
2024-04-11 16:14:49 -0700

    add lora config option for enable a2a
    Signed-off-by: Anmol Gupta <14880251+anmolgupt@users.noreply.github.com>

--- nemo/collections/nlp/modules/common/megatron/adapters/parallel_adapters.py
+++ nemo/collections/nlp/modules/common/megatron/adapters/parallel_adapters.py
@@ -147,6 +147,7 @@ class ParallelLinearAdapter(nn.Module, AdapterModuleUtil):
         model_parallel_config: Optional[ModelParallelConfig] = None,
         alpha: float | None = None,
         dropout_position: str = 'post',
+        a2a_experimental: bool = False,
         **kwargs,
     ):
         super().__init__()
@@ -164,6 +165,7 @@ class ParallelLinearAdapter(nn.Module, AdapterModuleUtil):
         self.dropout_position = dropout_position
         self.tp_world_size = None
         self.tp_group = None
+        self.use_a2a = a2a_experimental
 
         # megatron_gpt_peft_models will provide this arg, but deprecated ones do not.
         # in case this arg is not provided, use the dummy default config.
@@ -206,7 +208,7 @@ class ParallelLinearAdapter(nn.Module, AdapterModuleUtil):
             # (@adithyare) we use this option to mirror the behavior a column parallel layer with two low-rank column parallel layers
             # if the original column parallel layer uses gather_output=False, then we will use the self.liner_out layer defined below.
             lin_out_gather_output = True if input_is_parallel else False
-            if input_is_parallel and self._sequence_parallel:
+            if self.use_a2a and input_is_parallel and self._sequence_parallel:
                 lin_out_gather_output = False
                 self.tp_world_size = get_tensor_model_parallel_world_size()
                 self.tp_group = get_tensor_model_parallel_group()
@@ -298,12 +300,15 @@ class ParallelLinearAdapter(nn.Module, AdapterModuleUtil):
             # layernorm after lora is impacted by sequence parallel,
             # hence seq dim need to be scattered right after lora linear layers
             # this function also handles the backward pass correctly
-            # all2all hidden_size / tp to seq_len / tp
-            send_list = list(x.chunk(self.tp_world_size, dim=0))
-            send_list = [tensor.contiguous() for tensor in send_list]
-            receive_list = [torch.empty_like(send_list[0]) for _ in range(self.tp_world_size)]
-            torch.distributed.all_to_all(receive_list, send_list, group=self.tp_group)
-            x = torch.cat(receive_list, dim=-1)
+            if self.use_a2a:
+                # all2all hidden_size / tp to seq_len / tp
+                send_list = list(x.chunk(self.tp_world_size, dim=0))
+                send_list = [tensor.contiguous() for tensor in send_list]
+                receive_list = [torch.empty_like(send_list[0]) for _ in range(self.tp_world_size)]
+                torch.distributed.all_to_all(receive_list, send_list, group=self.tp_group)
+                x = torch.cat(receive_list, dim=-1)
+            else:
+                x = scatter_to_sequence_parallel_region(x)
 
         if self.norm_position == 'post':
             x = self.layer_norm(x)
@@ -333,6 +338,7 @@ class ParallelLinearAdapterConfig(AdapterConfig):
     dropout_position: str = 'post'
     alpha: float | None = None
     network_alpha: int | None = None
+    a2a_experimental: bool = False
     _target_: str = "{0}.{1}".format(ParallelLinearAdapter.__module__, ParallelLinearAdapter.__name__)
 
 
