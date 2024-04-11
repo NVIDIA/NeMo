2476d4f41def3066af39d1acb95559a358bcdd37
Jaemin Choi <jaeminc@nvidia.com>
2024-04-10 20:41:05 -0700

    Fix TE guard
    Signed-off-by: Jaemin Choi <jaeminc@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/transformer.py
+++ nemo/collections/nlp/modules/common/megatron/transformer.py
@@ -850,7 +850,7 @@ class AutocastTransformerLayer(TransformerLayer):
                     transformer_layer_args[ub_overlap_flag] = kwargs.get(split_gemm_flag, True) or kwargs.get(
                         atomic_gemm_flag, False
                     )
-            if te_version > packaging.version.Version("1.6.0"):
+            if te_version > packaging.version.Version("1.6.0.dev0"):
                 transformer_layer_args["ub_overlap_rs_dgrad"] = kwargs.get("ub_overlap_rs_dgrad", False)
         else:
             transformer_layer_args["ub_split_ag"] = kwargs.get("ub_split_ag", True)
@@ -1121,7 +1121,7 @@ class ParallelTransformer(MegatronModule):
                         if hasattr(config, "tp_comm_overlap_rs")
                         else config.tp_comm_split_rs or config.tp_comm_atomic_rs
                     )
-                    if te_version > packaging.version.Version("1.6.0"):
+                    if te_version > packaging.version.Version("1.6.0.dev0"):
                         transformer_layer_args["ub_overlap_rs_dgrad"] = (
                             config.tp_comm_overlap_rs_dgrad if hasattr(config, "tp_comm_overlap_rs_dgrad") else False
                         )
