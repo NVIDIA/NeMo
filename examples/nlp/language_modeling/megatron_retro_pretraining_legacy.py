685bb1e62b4760ec3fea4a5167c4113c9384f715
Huy Vu2 <huvu@login-eos02.eos.clusters.nvidia.com>
2024-04-12 06:18:09 -0700

    update Jenkins and _legacy.py

--- examples/nlp/language_modeling/megatron_retro_pretraining_legacy.py
+++ examples/nlp/language_modeling/megatron_retro_pretraining_legacy.py
@@ -34,7 +34,7 @@ from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
 
 
-@hydra_runner(config_path="conf", config_name="megatron_retro_config")
+@hydra_runner(config_path="conf", config_name="megatron_retro_config_legacy")
 def main(cfg) -> None:
     logging.info("\n\n************** Experiment configuration ***********")
     logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
