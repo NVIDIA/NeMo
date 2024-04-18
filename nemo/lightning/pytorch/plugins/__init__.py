c23ffc28db17f5a119ad67d60ad5ac6978cbd542
Marc Romeijn <mromeijn@nvidia.com>
2024-04-18 06:40:46 -0700

    Adding mixed-precision to NeMo

--- nemo/lightning/pytorch/plugins/__init__.py
+++ nemo/lightning/pytorch/plugins/__init__.py
@@ -1,5 +1,8 @@
 from nemo.lightning.pytorch.plugins.data_sampler import MegatronDataSampler
+from nemo.lightning.pytorch.plugins.mixed_precision import MegatronMixedPrecision, PipelineMixedPrecision
 
 __all__ = [
-    "MegatronDataSampler"
+    "MegatronDataSampler",
+    "MegatronMixedPrecision",
+    "PipelineMixedPrecision",
 ]
