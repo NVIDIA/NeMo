272233fc7e2ec2616bf5ce421b8d27e867ec63b2
Mikołaj Błaż <mblaz@nvidia.com>
2024-04-23 14:04:37 +0200

    Rename existing ckpts instead of using different name
    Signed-off-by: Mikołaj Błaż <mblaz@nvidia.com>

--- nemo/utils/callbacks/nemo_model_checkpoint.py
+++ nemo/utils/callbacks/nemo_model_checkpoint.py
@@ -188,7 +188,8 @@ class NeMoModelCheckpoint(ModelCheckpoint):
         if app_state.model_parallel_size is not None and app_state.model_parallel_size > 1:
             logging.warning(f'always_save_nemo will slow down training for model_parallel > 1.')
         # since we are creating tarfile artifacts we need to update .nemo path
-        app_state.model_restore_path = self._determine_available_nemo_path(trainer)
+        self._backup_existing_nemo_ckpt(trainer)
+        app_state.model_restore_path = self._format_nemo_checkpoint_name()
         if app_state.model_parallel_size is not None and app_state.model_parallel_size > 1:
             maybe_injected_best_model_path = inject_model_parallel_rank(self.best_model_path)
         else:
@@ -254,18 +255,29 @@ class NeMoModelCheckpoint(ModelCheckpoint):
                 trainer._checkpoint_connector.restore(self.best_model_path)
 
         if self.save_nemo_on_train_end:
-            pl_module.save_to(save_path=self._determine_available_nemo_path(trainer))
+            self._backup_existing_nemo_ckpt(trainer)
+            pl_module.save_to(save_path=self._format_nemo_checkpoint_name())
 
-    def _determine_available_nemo_path(self, trainer) -> str:
-        save_path = os.path.join(self.dirpath, self.prefix + self.postfix)
+    def _backup_existing_nemo_ckpt(self, trainer) -> str:
+        """ Search for an available name with version infix and rename existing checkpoint. """
+        base_path = self._format_nemo_checkpoint_name()
+        available_path = base_path
         if self._enable_version_counter:
             version_cnt = self.STARTING_VERSION
-            while self.file_exists(save_path, trainer, check_dist_ckpt=False):
-                save_path = os.path.join(
-                    self.dirpath, self.prefix + f'{self.CHECKPOINT_JOIN_CHAR}v{version_cnt}' + self.postfix
-                )
+            while self.file_exists(available_path, trainer, check_dist_ckpt=False):
+                available_path = self._format_nemo_checkpoint_name(version_cnt)
                 version_cnt += 1
-        return os.path.abspath(os.path.expanduser(save_path))
+        if available_path != base_path:
+            logging.info(f'{base_path} already exists, moving existing checkpoint to {available_path}')
+            shutil.move(base_path, available_path)
+        return available_path
+
+    def _format_nemo_checkpoint_name(self, ver: Optional[int] = None) -> str:
+        version_infix = '' if ver is None else f'{self.CHECKPOINT_JOIN_CHAR}v{ver}'
+        return os.path.abspath(os.path.expanduser(os.path.join(
+            self.dirpath,
+            self.prefix + version_infix + self.postfix
+        )))
 
     def _del_model_without_trainer(self, filepath: str) -> None:
 
