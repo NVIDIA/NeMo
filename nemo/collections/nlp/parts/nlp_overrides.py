1f62ca034fcc133f7c8dd170b93ab13dc9f7a603
Gao Deng <gdeng@nvidia.com>
2024-05-08 10:11:23 -0700

    condition check
    Signed-off-by: Gao Deng <gdeng@nvidia.com>

--- nemo/collections/nlp/parts/nlp_overrides.py
+++ nemo/collections/nlp/parts/nlp_overrides.py
@@ -239,7 +239,7 @@ class NLPDDPStrategy(DDPStrategy):
             hasattr(self.model, 'with_distributed_adam') and self.model.with_distributed_adam
         ):
             # do not use DDP if using megatron amp O2 or distributed optimizer
-            if self.use_mcore_dist_optim:
+            if self.model.use_mcore_dist_optim:
                 self.model.setup_mcore_distributed_parallel()
             self._model = self.model
         else:
