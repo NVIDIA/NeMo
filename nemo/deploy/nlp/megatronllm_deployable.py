db083959e20b6770d7778b3aea4bf244c3f05c3b
Justin Kim <jukim@nvidia.com>
2024-05-08 19:26:09 -0700

    cleanup, comments, and style guide fixes

--- nemo/deploy/nlp/megatronllm_deployable.py
+++ nemo/deploy/nlp/megatronllm_deployable.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 from enum import IntEnum, auto
 from pathlib import Path
@@ -16,7 +30,7 @@ from nemo.collections.nlp.modules.common.text_generation_utils import (
 from nemo.collections.nlp.modules.common.transformer.text_generation import LengthParam, SamplingParam
 from nemo.collections.nlp.parts.nlp_overrides import NLPDDPStrategy, NLPSaveRestoreConnector
 from nemo.deploy import ITritonDeployable
-from nemo.deploy.utils import cast_output, str_ndarray2list, typedict2tensor
+from nemo.deploy.utils import cast_output, str_ndarray2list
 
 
 @wrapt.decorator
@@ -37,17 +51,24 @@ except Exception:
 
 LOGGER = logging.getLogger("NeMo")
 
-# utility funciton to get Triton Tensor shape from a python value
-# assume that lists are shape -1 and all others are scalars with shape 1
 def GetTensorShape(pyvalue):
+    """
+    utility function to get Triton Tensor shape from a python value
+    assume that lists are shape -1 and all others are scalars with shape 1
+    """
     return (-1 if type(pyvalue) == list else 1,)
 
 
-# utility function to get numpy dtype of a python value
-# e.g. bool -> np.bool_
+
 def GetNumpyDtype(pyvalue):
-    # manually defining the mapping of python type -> numpy type for now
-    # is there a better way to do it?  tried np.array(pyvalue).dtype, but that doesn't seem to work
+    """
+    utility function to get numpy dtype of a python value
+    e.g. bool -> np.bool_
+    """
+    '''
+    manually defining the mapping of python type -> numpy type for now
+    is there a better way to do it?  tried np.array(pyvalue).dtype, but that doesn't seem to work
+    '''
     py_to_numpy_mapping = {str: bytes, bool: np.bool_, float: np.single, int: np.int_}
     python_type = type(pyvalue)
     # for lists, return the type of the internal elements
@@ -58,6 +79,7 @@ def GetNumpyDtype(pyvalue):
 
 
 class ServerSync(IntEnum):
+    """Enum for synchronization messages using torch.distributed"""
     WAIT = auto()
     SIGNAL = auto()
 
@@ -66,6 +88,7 @@ class ServerSync(IntEnum):
 
 
 class MegatronLLMDeployable(ITritonDeployable):
+    """Triton inference server compatible deploy class for a .nemo model file"""
     def __init__(
         self,
         nemo_checkpoint_filepath: str = None,
@@ -73,32 +96,31 @@ class MegatronLLMDeployable(ITritonDeployable):
         num_nodes: int = 1,
         existing_model: MegatronGPTModel = None,
     ):
-        assert (
-            nemo_checkpoint_filepath is not None or existing_model is not None
-        ), "MegatronLLMDeployable requires either a .nemo checkpoint filepath or an existing MegatronGPTModel"
+        if nemo_checkpoint_filepath is None and existing_model is None:
+            raise ValueError("MegatronLLMDeployable requires either a .nemo checkpoint filepath or an existing MegatronGPTModel, but both provided were None")
+
+        # if both existing_model and nemo_checkpoint_filepath are provided, existing_model will take precedence
         if existing_model is not None:
             self.model = existing_model
         else:
-            self._load(nemo_checkpoint_filepath, num_devices, num_nodes)
+            self._load_from_nemo_checkpoint(nemo_checkpoint_filepath, num_devices, num_nodes)
 
         self.model.eval()
+        # helper threads spawned by torch.multiprocessing should loop inside this helper function
         self._helper_thread_evaluation_loop()
 
-    def _load(self, nemo_checkpoint_filepath: str, num_devices: int, num_nodes: int):
+    def _load_from_nemo_checkpoint(self, nemo_checkpoint_filepath: str, num_devices: int, num_nodes: int):
         if Path(nemo_checkpoint_filepath).exists():
             trainer = Trainer(
                 strategy=NLPDDPStrategy(),
-                accelerator="gpu",
-                precision="bf16",
                 devices=num_devices,
                 num_nodes=num_nodes,
             )
-            # self.model = MegatronGPTModel.restore_from(nemo_checkpoint_filepath, trainer=trainer)
 
             custom_config = MegatronGPTModel.restore_from(
                 nemo_checkpoint_filepath, trainer=trainer, return_config=True
             )
-            # transformner_engine should always be true according to EricH
+            # transformer_engine should always be true according to EricH
             custom_config.transformer_engine = True
             # using multi-gpu for tensor parallelism directly for now, could do pipeline parallel instead or a combination
             custom_config.tensor_model_parallel_size = num_devices
@@ -123,8 +145,10 @@ class MegatronLLMDeployable(ITritonDeployable):
         "prompts": (-1, bytes, False),
     }
 
-    # there is no get_default equivalent for OutputType like there is for SamplingParameters and LengthParameters
-    # but we still want to generate output using a real OutputType TypedDict for static type checking
+    '''
+    there is no get_default equivalent for OutputType like there is for SamplingParameters and LengthParameters
+    but we still want to generate output using a real OutputType TypedDict for static type checking
+    '''
     _BLANK_OUTPUTTYPE: OutputType = {
         'sentences': [""],
         'tokens': [[""]],
@@ -140,9 +164,12 @@ class MegatronLLMDeployable(ITritonDeployable):
             Tensor(name=name, shape=(shape,), dtype=dtype, optional=optional)
             for name, (shape, dtype, optional) in self._INPUT_PARAMETER_FIELDS.items()
         )
-        # TODO: in theory, would like to use typedict2tensor() function to generate Tensors, but it purposely ignores 1D arrays
-        # need to find out why that is, asked Jakub Kosek on 2024-04-26, but he doesn't know who owns it
-        # sampling_parameters = typedict2tensor(SamplingParam)
+        '''
+        in theory, would like to use typedict2tensor() function to generate Tensors, but it purposely ignores 1D arrays
+        asked JakubK why on 2024-04-26, but he doesn't know who owns the code
+        sampling_parameters = typedict2tensor(SamplingParam)
+        length_parameters = typedict2tensor(LengthParam)
+        '''
         default_sampling_params: SamplingParam = get_default_sampling_params()
         sampling_parameters = tuple(
             Tensor(
@@ -153,7 +180,6 @@ class MegatronLLMDeployable(ITritonDeployable):
             )
             for parameter_name, parameter_value in default_sampling_params.items()
         )
-        # length_parameters = typedict2tensor(LengthParam)
         default_length_params: LengthParam = get_default_length_params()
         length_parameters = tuple(
             Tensor(
@@ -181,6 +207,7 @@ class MegatronLLMDeployable(ITritonDeployable):
 
     @staticmethod
     def _sampling_params_from_triton_inputs(**inputs: np.ndarray):
+        """Extract SamplingParam fields from triton input dict"""
         sampling_params: SamplingParam = get_default_sampling_params()
         for sampling_param_field in sampling_params.keys():
             if sampling_param_field in inputs:
@@ -189,6 +216,7 @@ class MegatronLLMDeployable(ITritonDeployable):
 
     @staticmethod
     def _length_params_from_triton_inputs(**inputs: np.ndarray):
+        """Extract LengthParam fields from triton input dict"""
         length_params: LengthParam = get_default_length_params()
         for length_param_field in length_params.keys():
             if length_param_field in inputs:
@@ -197,7 +225,10 @@ class MegatronLLMDeployable(ITritonDeployable):
 
     @batch
     def triton_infer_fn(self, **inputs: np.ndarray):
-        assert torch.distributed.get_rank() == 0, "Triton inference should only be called on main thread "
+        """Triton server inference function that actually runs the model"""
+        distributed_rank = torch.distributed.get_rank()
+        if distributed_rank != 0:
+            raise ValueError(f"Triton inference function should not be called on a thread with torch.distributed rank != 0, but this thread is rank {distributed_rank}")
         signal_value = ServerSync.SIGNAL.to_long_tensor()
         torch.distributed.broadcast(signal_value, 0)
 
@@ -208,11 +239,14 @@ class MegatronLLMDeployable(ITritonDeployable):
         model_output = self.model.generate(
             inputs=input_strings, length_params=length_params, sampling_params=sampling_params
         )
-        # sentences will be a list of strings (one per prompts)
-        # other fields will either be a list of lists (tokens, for example)
-        # or a list of pytorch Tensor
-        # we expect all lists to be the same length
-        # TODO: add an error check for when they aren't, like if you feed in prompts of varying lengths to compute logprob
+        '''
+            sentences will be a list of strings (one per prompts)
+            other fields will either be a list of lists (tokens, for example)
+            or a list of pytorch Tensor
+            we expect all lists to be the same length
+            TODO: add an error check for when they aren't, like if you feed in prompts of varying lengths to compute logprob
+            or figure out a way to pad data to make the outputs same length
+        '''
 
         num_prompts = len(input_strings)
         triton_output = {}
@@ -220,8 +254,8 @@ class MegatronLLMDeployable(ITritonDeployable):
             field_dtype = GetNumpyDtype(MegatronLLMDeployable._BLANK_OUTPUTTYPE[model_output_field][0])
             if value is None:
                 # triton does not allow for optional output parameters, so need to populate them if they don't exist
-                # 'sentences' should always have a valid value, so use that for the output shape
                 triton_output[model_output_field] = np.full(
+                    # 'sentences' should always have a valid value, so use that for the output shape
                     np.shape(model_output['sentences']),
                     MegatronLLMDeployable._BLANK_OUTPUTTYPE[model_output_field][0],
                     dtype=field_dtype,
