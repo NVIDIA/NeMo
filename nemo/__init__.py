92e1e6c496829ea676a72e7ef368529f36e142db
akoumpa <akoumpa@users.noreply.github.com>
2024-05-10 22:04:38 +0000

    Apply isort and black reformatting
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>

--- nemo/__init__.py
+++ nemo/__init__.py
@@ -14,17 +14,18 @@
 
 
 import pretty_errors
+
 pretty_errors.configure(
-    separator_character = '*',
-    filename_display    = pretty_errors.FILENAME_FULL,
-    line_number_first   = True,
-    display_link        = True,
-    lines_before        = 5,
-    lines_after         = 2,
-    line_color          = pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
-    code_color          = '  ' + pretty_errors.default_config.line_color,
-    truncate_code       = True,
-    display_locals      = True
+    separator_character='*',
+    filename_display=pretty_errors.FILENAME_FULL,
+    line_number_first=True,
+    display_link=True,
+    lines_before=5,
+    lines_after=2,
+    line_color=pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
+    code_color='  ' + pretty_errors.default_config.line_color,
+    truncate_code=True,
+    display_locals=True,
 )
 
 from nemo.package_info import (
