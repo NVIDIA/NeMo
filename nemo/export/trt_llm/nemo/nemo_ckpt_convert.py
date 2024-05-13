77090d4e5e218261b1fe6b3a931d16f4083f2d53
meatybobby <bobchen@nvidia.com>
2024-05-13 16:14:34 -0700

    TRTLLM new API support (#9003)
    * Add trtllm checkpoint
    
    * Change model config
    
    * fix no query_group
    
    * Using build API
    
    * Change export to new API
    
    * Update generate API
    
    * Fix runtime config
    
    * Fix for llama
    
    * Fix for ptuning
    
    * Fix TP issue
    
    * Change TP rank for building weight dict
    
    * Add lora config
    
    * add prompt embedding table config
    
    * Fix PP isue
    
    * PP layers fix
    
    * Fix no prompt task ids
    
    * Add bos for Gemma
    
    * Add multi block mode
    
    * Embedding and layernorm for PP
    
    * MPI multiprocess support for multinode
    
    * Only output text on first rank
    
    * Change to ModelRunnerCpp
    
    * Add falcon
    
    * Add rotary_pct default value
    
    * Falcon fix
    
    * Add MOE config
    
    * Fix MOE weight dict
    
    * Clean code
    
    * Add rotary_base
    
    * Fix MOE config
    
    * Fix falcon new architecture
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix Gemma 7B
    
    * Add rotary_scaling
    
    * Apply isort and black reformatting
    
    Signed-off-by: oyilmaz-nvidia <oyilmaz-nvidia@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: oyilmaz-nvidia <oyilmaz-nvidia@users.noreply.github.com>
    Co-authored-by: abharwani <abharwani@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Onur Yilmaz <35306097+oyilmaz-nvidia@users.noreply.github.com>
    Co-authored-by: oyilmaz-nvidia <oyilmaz-nvidia@users.noreply.github.com>
    Co-authored-by: Eric Harper <complex451@gmail.com>

--- nemo/export/trt_llm/nemo/nemo_ckpt_convert.py
+++ nemo/export/trt_llm/nemo/nemo_ckpt_convert.py
@@ -27,7 +27,7 @@ import numpy as np
 import tensorstore  # This is important even though not used. Otherwise zarr raises error.
 import torch
 import zarr
-from tensorrt_llm._utils import np_bfloat16, str_dtype_to_torch, torch_to_numpy
+from tensorrt_llm._utils import np_bfloat16, pad_vocab_size, str_dtype_to_torch, torch_to_numpy
 from tqdm import tqdm
 from transformers import AutoTokenizer, GPT2Tokenizer, LlamaConfig
 
@@ -174,6 +174,7 @@ def convert_dist_checkpoint(unpacked_checkpoints_dir: UnpackedNemoCheckpointDir,
     multi_query_mode = nemo_model_config.get("multi_query_mode", False)
     num_attention_heads = nemo_model_config["num_attention_heads"]
     kv_channels = nemo_model_config.get("kv_channels", None)
+    use_parallel_embedding = args.use_parallel_embedding
     if num_kv_heads == 0:
         if multi_query_mode:
             num_kv_heads = 1
@@ -191,6 +192,7 @@ def convert_dist_checkpoint(unpacked_checkpoints_dir: UnpackedNemoCheckpointDir,
         "kv_channels": kv_channels,
         "use_attention_nemo_shape": True,
         "transpose_weights": True,
+        "use_parallel_embedding": use_parallel_embedding,
     }
 
     # split_factor: in how many parts a TP training node is split
@@ -202,22 +204,30 @@ def convert_dist_checkpoint(unpacked_checkpoints_dir: UnpackedNemoCheckpointDir,
             if has_position_embedding:
                 val = model[get_layer_name("position_embedding", prefix)]
                 val = torch_to_numpy(val.to(storage_type).cpu())
-                model_level_weights["model.wpe.bin"].append(val)
+                model_level_weights["transformer.position_embedding.weight"].append(val)
         if pp_idx == 0:
             val = model.get("state_dict", model)[get_layer_name("word_embedding", prefix)]
             if embedding_scaling:
                 val = val * float(math.sqrt(hidden_size))
 
+            vocab_size = val.shape[0]
+            if use_parallel_embedding:
+                # Pad vocab_size first
+                if vocab_size % inference_tp_size != 0:
+                    vocab_size_padded = pad_vocab_size(vocab_size, inference_tp_size)
+                    pad_width = vocab_size_padded - vocab_size
+                    val = torch.nn.functional.pad(val, (0, 0, 0, pad_width), value=0)
+
             val = torch_to_numpy(val.to(storage_type).cpu())
-            model_level_weights["model.wte.bin"].append(val)
+            model_level_weights["transformer.vocab_embedding.weight"].append(val)
             if share_embeddings_and_output:
                 val = model.get("state_dict", model)[get_layer_name("word_embedding", prefix)]
                 val = torch_to_numpy(val.to(storage_type).cpu())
-                model_level_weights["model.lm_head.weight.bin"].append(val)
+                model_level_weights["lm_head.weight"].append(val)
         if has_lm_head and pp_idx == training_pp_size - 1:
             val = model.get("state_dict", model)[get_layer_name("output_layer", prefix)]
             val = torch_to_numpy(val.to(storage_type).cpu())
-            model_level_weights["model.lm_head.weight.bin"].append(val)
+            model_level_weights["lm_head.weight"].append(val)
 
     weights_dict = {}
 
@@ -280,7 +290,6 @@ def convert_dist_checkpoint(unpacked_checkpoints_dir: UnpackedNemoCheckpointDir,
         model_level_weights[key] = np.concatenate(values, axis=0)
 
         weights_dict[key] = model_level_weights[key]
-    vocab_size = model_level_weights["model.wte.bin"].shape[0]
 
     if nemo_model_config["tokenizer"].get("library", None) == "huggingface":
         tokenizer = AutoTokenizer.from_pretrained(
@@ -293,23 +302,7 @@ def convert_dist_checkpoint(unpacked_checkpoints_dir: UnpackedNemoCheckpointDir,
         tokenizer_config["model"] = os.path.join(out_dir, "tokenizer.model")
         tokenizer = build_tokenizer(tokenizer_config)
 
-    llm_config = nemo_to_llm_config(
-        nemo_model_config, vocab_size, tokenizer.eos_token_id, tokenizer.bos_token_id, args.decoder_type,
-    )
-
-    llm_config.is_mcore = is_mcore
-
-    config = configparser.ConfigParser()
-    decoder_name_dict = {"llama": "llama", "falcon": "falcon"}
-    model_name = decoder_name_dict[args.decoder_type] if args.decoder_type in decoder_name_dict else "gpt"
-
-    config[model_name] = {k: str(v) for k, v in vars(llm_config).items()}
-    config[model_name]["storage_dtype"] = args.storage_type
-    config_path = out_dir / "config.ini"
-    with config_path.open("w") as config_file:
-        config.write(config_file)
-
-    return weights_dict, llm_config, tokenizer
+    return weights_dict, nemo_model_config, tokenizer
 
 
 @torch.no_grad()
