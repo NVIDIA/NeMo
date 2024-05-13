dd6538eddaea12019d4354ae9ab823858d95b433
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-05-13 11:53:32 -0700

    wrap with a try statement in case pretty-errors is not installed
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/__init__.py
+++ nemo/__init__.py
@@ -12,21 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+try:
+    import pretty_errors
 
-import pretty_errors
-
-pretty_errors.configure(
-    separator_character='*',
-    filename_display=pretty_errors.FILENAME_FULL,
-    line_number_first=True,
-    display_link=True,
-    lines_before=5,
-    lines_after=2,
-    line_color=pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
-    code_color='  ' + pretty_errors.default_config.line_color,
-    truncate_code=True,
-    display_locals=True,
-)
+    pretty_errors.configure(
+        separator_character='*',
+        filename_display=pretty_errors.FILENAME_FULL,
+        line_number_first=True,
+        display_link=True,
+        lines_before=5,
+        lines_after=2,
+        line_color=pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
+        code_color='  ' + pretty_errors.default_config.line_color,
+        truncate_code=True,
+        display_locals=True,
+    )
+except ImportError:
+    pass
 
 from nemo.package_info import (
     __contact_emails__,
