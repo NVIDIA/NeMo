9b39ac9c6ccbfb06ad0acee3bf9e9c38eb3479c8
Jan Lasek <janek.lasek@gmail.com>
2024-05-16 08:41:00 +0200

    Remove config aligner - no longer needed after TRT-LLM 0.9 update
    Signed-off-by: Jan Lasek <janek.lasek@gmail.com>

--- nemo/export/trt_llm/qnemo/__init__.py
+++ nemo/export/trt_llm/qnemo/__init__.py
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .align_config import align_config
 from .qnemo_to_tensorrt_llm import qnemo_to_tensorrt_llm
