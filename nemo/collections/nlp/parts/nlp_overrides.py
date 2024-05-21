27eb5535a508e0aa8c36dfa80df7f477400fd96b
akoumpa <akoumpa@users.noreply.github.com>
2024-05-21 09:24:44 +0000

    Apply isort and black reformatting
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>

--- nemo/collections/nlp/parts/nlp_overrides.py
+++ nemo/collections/nlp/parts/nlp_overrides.py
@@ -298,8 +298,10 @@ class NLPDDPStrategy(DDPStrategy):
         }
         if isinstance(optimizer, McoreDistributedOptimizer):
             return optimizer.sharded_state_dict(
-                model_sharded_state_dict, unsharded_optim_state, is_loading=is_loading,
-                dist_ckpt_parallel_save=self._dist_ckpt_parallel_save
+                model_sharded_state_dict,
+                unsharded_optim_state,
+                is_loading=is_loading,
+                dist_ckpt_parallel_save=self._dist_ckpt_parallel_save,
             )
         elif isinstance(optimizer, MegatronDistributedFusedAdam):
             return optimizer.sharded_state_dict(model_sharded_state_dict, unsharded_optim_state)
