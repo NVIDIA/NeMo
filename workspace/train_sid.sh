a0820ab516838b6db7e241175c059a4148567b09
stevehuang52 <heh@nvidia.com>
2024-05-21 11:03:02 -0400

    fix ecapa attn dim
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- workspace/train_sid.sh
+++ workspace/train_sid.sh
@@ -12,7 +12,7 @@ noise_manifest="[/media/data3/datasets/noise_data/musan/musan_nonspeech_manifest
 rir_manifest="/media/data2/simulated_data/rir_noise_data/real_rirs_isotropic_noises_1ch.json"
 
 EXP_NAME="titanet_large_WavLM"
-POSTFIX=epoch49_r1
+POSTFIX=debug2
 
 SSL_CKPT="/home/heh/codes/nemo-ssl/workspace/nemo_experiments/pretrained_checkpoints/oci_ll_unlab-60k_bs2048_adamwlr0.004_wd1e-3_warmup25000_epoch1000_mask0.01x40pre_conv_wavLM0.2x0.1_n16_r5--val_loss5.0808-epoch43-last.ckpt"
 
@@ -20,14 +20,16 @@ batch_size=128
 num_workers=8
 
 # model.train_ds.augmentor.impulse.manifest_path=$rir_manifest \
+#     ++init_from_ptl_ckpt=$SSL_CKPT \
+
 
 CUDA_VISIBLE_DEVICES="0" python speaker_id_train.py \
     --config-path="configs" \
-    --config-name="titanet_large_ssl" \
-    ++init_from_ptl_ckpt=$SSL_CKPT \
+    --config-name="ecapa_tdnn_small" \
     trainer.log_every_n_steps=10 \
     model.train_ds.manifest_filepath=$train_manifests \
     model.train_ds.augmentor.noise.manifest_path=$noise_manifest \
+    model.train_ds.augmentor.impulse.manifest_path=$rir_manifest \
     ++model.train_ds.min_duration=0.3 \
     ++model.validation_ds.min_duration=0.3 \
     model.validation_ds.manifest_filepath=$dev_manifests \
@@ -38,6 +40,6 @@ CUDA_VISIBLE_DEVICES="0" python speaker_id_train.py \
     trainer.val_check_interval=1.0 \
     ++exp_manager.checkpoint_callback_params.save_top_k=1 \
     exp_manager.name="$EXP_NAME-${POSTFIX}" \
-    ++exp_manager.create_wandb_logger=True \
+    ++exp_manager.create_wandb_logger=false \
     ++exp_manager.wandb_logger_kwargs.name="$EXP_NAME-${POSTFIX}" \
     ++exp_manager.wandb_logger_kwargs.project="ssl_WavLM_spk_id"
