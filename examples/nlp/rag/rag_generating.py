51e4b53b17ade2183548d3c6f1e7101ce414b28f
Huy Vu2 <huvu@login-eos01.eos.clusters.nvidia.com>
2024-05-21 12:37:00 -0700

    addressing Ali's comments

--- examples/nlp/rag/rag_generating.py
+++ examples/nlp/rag/rag_generating.py
@@ -3,13 +3,14 @@ from llama_index.core import Settings, StorageContext, load_index_from_storage
 from nemo.collections.nlp.models.rag.custom_bert_embedder import NeMoBertEmbeddings
 from nemo.collections.nlp.models.rag.custom_gpt_llm import NeMoGPTLLM
 from nemo.core.config import hydra_runner
+from nemo.utils import logging
 
 
 @hydra_runner(config_path="conf", config_name="rag_generating")
 def main(cfg) -> None:
 
     # load LLM
-    print("Loading LLM.")
+    logging.info("Loading LLM.")
     model_path = cfg.generating.llm.model_path
     if cfg.generating.llm.model_type == "gpt":
         Settings.llm = NeMoGPTLLM(model_path=model_path, cfg=cfg)
@@ -17,7 +18,7 @@ def main(cfg) -> None:
         assert cfg.generating.model_type in ["gpt"], "Currently RAG pipeline supports 'gpt' for LLM models."
 
     # load embedder
-    print("Loading embedder.")
+    logging.info("Loading embedder.")
     model_path = cfg.indexing.embedder.model_path
     if cfg.indexing.embedder.model_type == "bert":
         embed_model = NeMoBertEmbeddings(model_path=model_path, cfg=cfg)
@@ -26,21 +27,21 @@ def main(cfg) -> None:
     Settings.embed_model = embed_model
 
     # load index from disk
-    print("Loading index from disk.")
+    logging.info("Loading index from disk.")
     index_path = cfg.indexing.index_path
     storage_context = StorageContext.from_defaults(persist_dir=index_path)
     index = load_index_from_storage(storage_context)
 
     # set query
-    print("Setting query.")
+    logging.info("Setting query.")
     query = cfg.generating.query
-    print("Query: ", query)
+    logging.info("Query: ", query)
 
     # query and print response
-    print("Responding to query using relevant contexts.")
+    logging.info("Responding to query using relevant contexts.")
     query_engine = index.as_query_engine(similarity_top_k=3)
     response = query_engine.query(query)
-    print(response)
+    logging.info(response)
 
 
 if __name__ == '__main__':
