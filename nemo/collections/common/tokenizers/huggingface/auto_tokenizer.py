894cd6f13b9db9d467e924c4d304543530581380
Ssofja <sofiakostandian@gmail.com>
2024-06-03 12:41:47 +0400

    fix wpe tokenizer function
    Signed-off-by: Ssofja <sofiakostandian@gmail.com>

--- nemo/collections/common/tokenizers/huggingface/auto_tokenizer.py
+++ nemo/collections/common/tokenizers/huggingface/auto_tokenizer.py
@@ -216,9 +216,7 @@ class AutoTokenizer(TokenizerSpec):
         return ids
 
     def ids_to_text(self, ids):
-        tokens = self.ids_to_tokens(ids)
-        tokens_clean = [t for t in tokens if t not in self.tokenizer.all_special_tokens]
-        text = self.tokens_to_text(tokens_clean)
+        text = self.tokenizer.decode(ids, skip_special_tokens=True)
         return text
 
     @property
