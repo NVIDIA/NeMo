e4e78020f44c8bb52e92a8a0a4d839ce52638863
zhehuaichen <dian.chenzhehuai@gmail.com>
2024-06-06 10:59:15 -0700

    nit
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/utils.py
+++ nemo/collections/nlp/modules/common/megatron/utils.py
@@ -444,9 +444,9 @@ def get_iterator_k_split(
 
         # Split tensor items
         items = list(tensor_items.items())
-        split_batch = [torch.tensor_split(item[1], num_microbatches, dim=0) for item in items]
         if enforce_divisible_batch:
             assert items[0][1].shape[0] % num_microbatches == 0, "Issue with batch size configuration!"
+        split_batch = [torch.tensor_split(item[1], num_microbatches, dim=0) for item in items]
         # handle the case where the batch size from dynamic bucketting is not divisible
         if items[0][1].shape[0] % num_microbatches != 0:
             chunk_size = split_batch[0][-1].shape[0]
