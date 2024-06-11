de05dfdfc32ada70b425c15d0de6469a55bc4970
Marc Romeyn <mromeijn@nvidia.com>
2024-06-11 16:39:21 +0200

    Add all-reduces to MegatronOptim

--- nemo/lightning/optim.py
+++ nemo/lightning/optim.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Callable, Optional
 
-from megatron.core.optimizer import OptimizerConfig, get_megatron_optimizer
 from pytorch_lightning.utilities.types import OptimizerLRScheduler
 from torch.optim import Optimizer
+from megatron.core.optimizer import OptimizerConfig, get_megatron_optimizer
+from megatron.core.distributed import finalize_model_grads
+from megatron.core.utils import get_model_config
 
 if TYPE_CHECKING:
     from nemo.lightning.megatron_parallel import MegatronParallel
@@ -12,6 +14,7 @@ if TYPE_CHECKING:
 @dataclass
 class MegatronOptim:
     config: OptimizerConfig
+    finalize_model_grads: Callable = finalize_model_grads
 
     def create_optimizer(
         self,
@@ -21,6 +24,9 @@ class MegatronOptim:
         lr_mult: float = 1.0,
     ) -> Optimizer:
         from nemo.core.optim import McoreDistributedOptimizer
+        
+        # TODO: Where should we put this?
+        get_model_config(megatron_parallel[0]).finalize_model_grads = finalize_model_grads
 
         mcore_opt = get_megatron_optimizer(
             self.config,
