3465ad131c7c5b3f99554578bc0530bd6b5b8992
marcromeyn <marcromeyn@users.noreply.github.com>
2024-06-11 14:40:08 +0000

    Apply isort and black reformatting
    Signed-off-by: marcromeyn <marcromeyn@users.noreply.github.com>

--- nemo/lightning/optim.py
+++ nemo/lightning/optim.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Callable, Optional
 
+from megatron.core.distributed import finalize_model_grads
+from megatron.core.optimizer import OptimizerConfig, get_megatron_optimizer
+from megatron.core.utils import get_model_config
 from pytorch_lightning.utilities.types import OptimizerLRScheduler
 from torch.optim import Optimizer
-from megatron.core.optimizer import OptimizerConfig, get_megatron_optimizer
-from megatron.core.distributed import finalize_model_grads
-from megatron.core.utils import get_model_config
 
 if TYPE_CHECKING:
     from nemo.lightning.megatron_parallel import MegatronParallel
@@ -24,7 +24,7 @@ class MegatronOptim:
         lr_mult: float = 1.0,
     ) -> Optimizer:
         from nemo.core.optim import McoreDistributedOptimizer
-        
+
         # TODO: Where should we put this?
         get_model_config(megatron_parallel[0]).finalize_model_grads = finalize_model_grads
 
