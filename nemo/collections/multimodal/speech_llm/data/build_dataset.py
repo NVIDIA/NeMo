c6aef4e68c5cbab5b9f2a0ffd4eed5dbfd3bb830
He Huang (Steve) <105218074+stevehuang52@users.noreply.github.com>
2024-06-13 15:23:01 -0400

    Update build_dataset.py
    Signed-off-by: He Huang (Steve) <105218074+stevehuang52@users.noreply.github.com>

--- nemo/collections/multimodal/speech_llm/data/build_dataset.py
+++ nemo/collections/multimodal/speech_llm/data/build_dataset.py
@@ -207,7 +207,7 @@ def build_speechllm_dataloader(dataset, data_cfg, consumed_samples=0, is_predict
         )
         return dataloader
 
-     pad_to_global_batch = not data_cfg.drop_last
+    pad_to_global_batch = not data_cfg.drop_last
     if is_eval:
         # don't pad to global batch if in eval mode, unless explicitly set by user (e.g., eval with DDP)
         pad_to_global_batch = (not data_cfg.drop_last) and data_cfg.get("pad_samples_to_global_batch_size", False)
