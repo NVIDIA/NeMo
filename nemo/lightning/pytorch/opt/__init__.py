cd728e16488d047bfc0585dd9e9901194aa246ea
Marc Romeyn <mromeijn@nvidia.com>
2024-06-13 10:16:04 +0200

    Adding lr-schedulers

--- nemo/lightning/pytorch/opt/__init__.py
+++ nemo/lightning/pytorch/opt/__init__.py
@@ -1,4 +1,33 @@
 from nemo.lightning.pytorch.opt.base import LRSchedulerModule, OptimizerModule
 from nemo.lightning.pytorch.opt.megatron import MegatronOptimizerModule
+from nemo.lightning.pytorch.opt.lr_scheduler import (
+    WarmupPolicyScheduler, 
+    WarmupHoldPolicyScheduler, 
+    SquareAnnealingScheduler, 
+    SquareRootAnnealingScheduler, 
+    NoamAnnealingScheduler, 
+    NoamHoldAnnealingScheduler,
+    WarmupAnnealingScheduler,
+    InverseSquareRootAnnealingScheduler,
+    T5InverseSquareRootAnnealingScheduler,
+    PolynomialDecayAnnealingScheduler,
+    PolynomialHoldDecayAnnealingScheduler
+)
 
-__all__ = ["OptimizerModule", "LRSchedulerModule", "MegatronOptimizerModule"]
+
+__all__ = [
+    "OptimizerModule", 
+    "LRSchedulerModule", 
+    "MegatronOptimizerModule",
+    "WarmupPolicyScheduler",
+    "WarmupHoldPolicyScheduler",
+    "SquareAnnealingScheduler",
+    "SquareRootAnnealingScheduler",
+    "NoamAnnealingScheduler",
+    "NoamHoldAnnealingScheduler",
+    "WarmupAnnealingScheduler",
+    "InverseSquareRootAnnealingScheduler",
+    "T5InverseSquareRootAnnealingScheduler",
+    "PolynomialDecayAnnealingScheduler",
+    "PolynomialHoldDecayAnnealingScheduler"
+]
