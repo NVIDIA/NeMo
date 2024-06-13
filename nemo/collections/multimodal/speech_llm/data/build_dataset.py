bd83b7f199ddfe9b0dd97a2ea8f55d1630dcb4db
stevehuang52 <stevehuang52@users.noreply.github.com>
2024-06-13 19:24:39 +0000

    Apply isort and black reformatting
    Signed-off-by: stevehuang52 <stevehuang52@users.noreply.github.com>

--- nemo/collections/multimodal/speech_llm/data/build_dataset.py
+++ nemo/collections/multimodal/speech_llm/data/build_dataset.py
@@ -211,7 +211,7 @@ def build_speechllm_dataloader(dataset, data_cfg, consumed_samples=0, is_predict
     if is_eval:
         # don't pad to global batch if in eval mode, unless explicitly set by user (e.g., eval with DDP)
         pad_to_global_batch = (not data_cfg.drop_last) and data_cfg.get("pad_samples_to_global_batch_size", False)
-       
+
     batch_sampler = MegatronPretrainingBatchSampler(
         total_samples=len(dataset),
         consumed_samples=consumed_samples,
