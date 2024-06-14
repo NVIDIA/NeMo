6d570491a2b388498e1762fca5066d4218a39987
Somshubra Majumdar <smajumdar@nvidia.com>
2024-06-13 19:17:41 -0700

    Finalize code for update module
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/core/classes/mixins/adapter_mixins.py
+++ nemo/core/classes/mixins/adapter_mixins.py
@@ -123,7 +123,8 @@ def _prepare_default_adapter_config(*, global_key: str, meta_key: str, cfg: Dict
     return cfg
 
 
-def update_module_class_with_adapter_class(module: nn.Module, cfg: DictConfig, update_config: bool = True):
+def update_module_class_with_adapter_class(module: nn.Module, cfg: DictConfig, update_config: bool = True,
+                                           verbose: bool = True):
     """
     Recursively walks through the module and its children, checking if the class is registered in the adapter registry.
     If it is, the module's class is swapped with the registered adapter class.
@@ -133,6 +134,7 @@ def update_module_class_with_adapter_class(module: nn.Module, cfg: DictConfig, u
         module: torch.nn.Module to recurse through.
         cfg: DictConfig object or dict that contains the config of the module.
         update_config: Bool, whether to update the config with the adapter classpath.
+        verbose: Bool, whether to log the changes made to the module and config.
     """
     def inplace_recursive_walk_dict(d: Union[dict, DictConfig], base_class_path: str, adapter_class_path: str):
         """
@@ -144,32 +146,40 @@ def update_module_class_with_adapter_class(module: nn.Module, cfg: DictConfig, u
             base_class_path: The str classpath of the base class.
             adapter_class_path: The str classpath of the adapter class.
         """
-        for k, v in d.items():
-            if isinstance(v, dict):
+        for k, v in d.items():  # Loop through all k, v pairs
+            if isinstance(v, (dict, DictConfig)):  # If value is a dict, recurse through it
                 inplace_recursive_walk_dict(v, base_class_path, adapter_class_path)
+
+            # If key is target and value is base class, update the value to adapter class
             elif k in ('target', '_target_') and isinstance(v, str) and v == base_class_path:
-                logging.info(f"Updating config from {v} (base class) to {adapter_class_path} (adapter compatible "
-                             f"class)")
+                if verbose:
+                    logging.info(f"Updating config from {v} (base class) to {adapter_class_path} (adapter compatible "
+                                 f"class)")
+
+                # Update the value inplace
                 d[k] = adapter_class_path
 
-        if not isinstance(module, AdapterModuleMixin):
-            info = get_registered_adapter(mod.__class__)
-            if info is not None:
-                # Swap the registered class with its registered adapter class.
-                # Due to direct inheritance of the Adapter subclass from the original class,
-                # the module's class container will be replaced with the adapter class.
+    if not isinstance(module, AdapterModuleMixin):
+        info = get_registered_adapter(module.__class__)
+        if info is not None:
+            if verbose:
                 logging.info(f"Swapping class {info.base_class_path} with adapter compatible class: "
                              f"{info.adapter_class_path}")
-                adapter_cls = info.adapter_class
-                module.__class__ = adapter_cls
 
-                if update_config:
-                    # Update the adapter config with the registered adapter config
-                    # Find the location where the original module was registered in config
-                    # and replace it with the adapter classpath.
-                    original_classpath = info.base_class_path
-                    adapter_classpath = info.adapter_class_path
-                    inplace_recursive_walk_dict(cfg, original_classpath, adapter_classpath)
+            # Swap the registered class with its registered adapter class.
+            # Due to direct inheritance of the Adapter subclass from the original class,
+            # the module's class container will be replaced with the adapter class.
+
+            adapter_cls = info.adapter_class
+            module.__class__ = adapter_cls
+
+            if update_config:
+                # Update the adapter config with the registered adapter config
+                # Find the location where the original module was registered in config
+                # and replace it with the adapter classpath.
+                original_classpath = info.base_class_path
+                adapter_classpath = info.adapter_class_path
+                inplace_recursive_walk_dict(cfg, original_classpath, adapter_classpath)
 
 
 class AdapterModuleMixin(ABC):
@@ -1038,19 +1048,22 @@ class AdapterModelPTMixin(AdapterModuleMixin):
             if isinstance(module, AdapterModuleMixin):
                 module.adapter_cfg = cfg
 
-    def replace_adapter_compatible_modules(self, module: Optional[nn.Module] = None, update_config: bool = True):
+    def replace_adapter_compatible_modules(self, module: Optional[nn.Module] = None, update_config: bool = True,
+                                           verbose: bool = True):
         """
         Utility method to replace all modules with Adapter variants, if they exist.
 
         Args:
             module: The module to be replaced with an Adapter variant.
             update_config: A flag that determines if the config should be updated or not.
+            verbose: A flag that determines if the method should log the changes made or not.
         """
         if module is None:
             module = self
 
-        for mod in module.modules():
-            update_module_class_with_adapter_class(mod, cfg=self.cfg, update_config=update_config)
+        # Update the given module itself, and then all its children modules
+        for name, mod in module.named_modules():
+            update_module_class_with_adapter_class(mod, cfg=self.cfg, update_config=update_config, verbose=verbose)
 
     @property
     def adapter_module_names(self) -> List[str]:
