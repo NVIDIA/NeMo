54e4988d7da4f12f0f81fcff33604bbcc0de1c84
John St John <jstjohn@nvidia.com>
2024-06-14 22:06:20 +0000

    restore missing args for the older version of megatron

--- nemo/lightning/megatron_parallel.py
+++ nemo/lightning/megatron_parallel.py
@@ -143,6 +143,8 @@ class MegatronParallel(nn.ModuleList):
                     module.config,
                     ddp_config,
                     module,
+                    data_parallel_group=parallel_state.get_data_parallel_group(with_context_parallel=True),
+                    expert_data_parallel_group=parallel_state.get_data_modulo_expert_parallel_group(),
                     # Turn off bucketing for model_chunk 2 onwards, since communication for these
                     # model chunks is overlapped with compute anyway.
                     disable_bucketing=(model_chunk_idx > 0),
