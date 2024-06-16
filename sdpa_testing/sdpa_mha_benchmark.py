066a4d2cd56ee0ef8e1f3bbce7e87eeae1c4406d
WoodieDudy <goshagks@yandex.ru>
2024-06-16 14:14:14 +0000

    benchmark backward
    Signed-off-by: WoodieDudy <goshagks@yandex.ru>

--- sdpa_testing/sdpa_mha_benchmark.py
+++ sdpa_testing/sdpa_mha_benchmark.py
@@ -5,9 +5,10 @@ from nemo.collections.asr.parts.submodules.multi_head_attention import MultiHead
 from old_multi_head_attention import MultiHeadAttention
 from nemo.utils import avoid_float16_autocast_context
 
+
 torch.manual_seed(123)
 
-device = "cpu"
+device = "cuda"
 batch_size = 32
 seq_len = 1024
 d_model = 512
@@ -18,7 +19,7 @@ key = torch.rand(batch_size, seq_len, d_model, device=device, requires_grad=True
 value = torch.rand(batch_size, seq_len, d_model, device=device, requires_grad=True)
 mask = torch.ones(batch_size, seq_len, seq_len, device=device, requires_grad=False)
 mask = torch.triu(mask, diagonal=1).bool()
-# mask = None
+mask = None
 
 attention_sdpa = SDPAMultiHeadAttention(n_head, d_model, 0.0).to(device)
 attention_original = MultiHeadAttention(n_head, d_model, 0.0).to(device)
@@ -27,50 +28,57 @@ for original_param, sdpa_param in zip(attention_original.parameters(), attention
 # attention_sdpa = torch.compile(attention_sdpa)
 # attention_original = torch.compile(attention_original)
 
+
 def measure_time(attention, query, key, value, mask):
     with torch.no_grad():
         timer = benchmark.Timer(
-            stmt='attention(query, key, value, mask)',
+            stmt='attention(query, key, value, mask);torch.cuda.synchronize()',
             setup='torch.cuda.synchronize()',
             globals={'attention': attention, 'query': query, 'key': key, 'value': value, 'mask': mask}
         )
         torch.cuda.synchronize()
         
-        with torch.no_grad(), torch.backends.cuda.sdp_kernel(enable_math=False, enable_mem_efficient = False, enable_flash = True):
+        with torch.no_grad():
             results = timer.blocked_autorange(min_run_time=10)
             forward_time = results.mean
             output = attention(query, key, value, mask)
     return forward_time, output
 
-def measure_backward_time(attention, query, key, value, mask):
+
+def measure_fwd_bwd_time(attention, query, key, value, mask):
     timer = benchmark.Timer(
-        stmt='loss.backward()',
-        setup='''
-torch.cuda.empty_cache()
-output = attention(query, key, value, mask)
-loss = output.sum()
-torch.cuda.synchronize()
-''',
+        stmt='loss=attention(query, key, value, mask).sum();torch.cuda.synchronize();loss.backward();torch.cuda.synchronize()',
+        setup='torch.cuda.synchronize()',
         globals={'attention': attention, 'query': query, 'key': key, 'value': value, 'mask': mask}
     )
     torch.cuda.synchronize()
     results = timer.blocked_autorange(min_run_time=10)
-    backward_time = results.mean
-    return backward_time
+    fwd_bwd_time = results.mean
+    return fwd_bwd_time
 
 
-# time_original, output_original = measure_time(attention_original, query, key, value, mask)
-# time_sdpa, output_sdpa = measure_time(attention_sdpa, query, key, value, mask)
+time_fwd_original, output_original = measure_time(attention_original, query, key, value, mask)
+time_fwd_sdpa, output_sdpa = measure_time(attention_sdpa, query, key, value, mask)
 
-# print(f"Original implementation time: {time_original:.6f} seconds")
-# print(f"SDPA implementation time: {time_sdpa:.6f} seconds")
-# print(f"SDPA boost {(time_original - time_sdpa) / time_original * 100:.3f}%")
+print(f"Original implementation time: {time_fwd_original:.6f} seconds")
+print(f"SDPA implementation time: {time_fwd_sdpa:.6f} seconds")
+print(f"SDPA boost {(time_fwd_original - time_fwd_sdpa) / time_fwd_original * 100:.2f}%")
 
-time_backward_original = measure_backward_time(attention_original, query, key, value, mask)
-time_backward_sdpa = measure_backward_time(attention_sdpa, query, key, value, mask)
+time_fwd_bwd_original = measure_fwd_bwd_time(attention_original, query, key, value, mask)
+time_fwd_bwd_sdpa = measure_fwd_bwd_time(attention_sdpa, query, key, value, mask)
+time_bwd_original = time_fwd_bwd_original - time_fwd_original
+time_bwd_sdpa = time_fwd_bwd_sdpa - time_fwd_sdpa
 
-print(f"Original implementation backward time: {time_backward_original:.6f} seconds")
-print(f"SDPA implementation backward time: {time_backward_sdpa:.6f} seconds")
-print(f"SDPA backward boost {(time_backward_original - time_backward_sdpa) / time_backward_original * 100:.3f}%")
+print(f"Original implementation backward time: {time_bwd_original:.6f} seconds")
+print(f"SDPA implementation backward time: {time_bwd_sdpa:.6f} seconds")
+print(f"SDPA backward boost {(time_bwd_original - time_bwd_sdpa) / time_bwd_original * 100:.2f}%")
 
-# print(f"Outputs are {'the same' if torch.allclose(output_original, output_sdpa, atol=1e-5) else 'different'}")
+print(f"Outputs are {'the same' if torch.allclose(output_original, output_sdpa, atol=1e-5) else 'different'}")
+
+# Original implementation time: 0.017988 seconds
+# SDPA implementation time: 0.012850 seconds
+# SDPA boost 28.562%
+# Original implementation backward time: 0.034470 seconds
+# SDPA implementation backward time: 0.038126 seconds
+# SDPA backward boost -10.608%
+# Outputs are the same
