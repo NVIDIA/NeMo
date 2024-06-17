dbec61dcb28ceff72f1a6d4f0ec2f6a2deba9fee
slyne deng <slyned@nvidia.com>
2024-06-17 10:31:27 -0700

    refactor neva_evaluation.py and improved perf
    Signed-off-by: slyne deng <slyned@nvidia.com>

--- examples/multimodal/multimodal_llm/neva/neva_evaluation.py
+++ examples/multimodal/multimodal_llm/neva/neva_evaluation.py
@@ -15,13 +15,14 @@
 import json
 import os
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 from nemo.collections.multimodal.parts.utils import create_neva_model_and_processor
 from nemo.collections.nlp.modules.common.transformer.text_generation import LengthParam, SamplingParam
 from nemo.core.config import hydra_runner
 from nemo.utils.get_rank import is_global_rank_zero
 
+import multiprocessing
 
 try:
     import ammo.torch.quantization as atq
@@ -36,22 +37,93 @@ if not torch.cuda.is_available():
     raise EnvironmentError("GPU is needed for the inference")
 
 
-class RequestDataSet(Dataset):
-    def __init__(self, sentences):
-        super().__init__()
-        self.sentences = sentences
+class TemporalNevaDataset(Dataset):
+    def __init__(self, prompt_dicts,
+                    media_base_path,
+                    media_token,
+                    insert_media_token = None,
+                    image_processor = None,
+                    video_processor = None):
+        self.prompt_dicts = prompt_dicts
+        self.media_token = media_token
+        self.insert_media_token = insert_media_token
+        self.media_base_path = media_base_path
+        self.image_processor = image_processor
+        self.video_processor = video_processor
+        # [(media_name, [prompt_dict, prompt_dict, ...]), ...}
+        self.media_prompt_list = []
+        self.group_by_media(media_token)
 
-    def __len__(self,):
-        return len(self.sentences)
+    def group_by_media(self, media_token):
+        """
+        This function groups the prompt dicts by the media/video/image file name
+        """
+        media_dict = {}
+        media = media_token.lstrip('<').rstrip('>')
+        for prompt_dict in self.prompt_dicts:
+            media_name = prompt_dict[media] # video or image file name
+            if media_name not in media_dict:
+                media_dict[media_name] = []
+            media_dict[media_name].append(prompt_dict)
+        self.media_prompt_list = list(media_dict.items())
+    
+    def __len__(self) -> int:
+        return len(self.media_prompt_list)
+    
+    def __getitem__(self, idx) -> dict:
+        """
+        Return a list of prompt dicts for the idx-th media
+        For a single media file, only one media feature is returned
+        This would help improve performance as well as save GPU memory
+        """
+        prompt_dict_list = self.media_prompt_list[idx][1]
+        cur_item = []
+        cur_media_feature = None
+        for prompt_dict in prompt_dict_list:
+            if 'prompt' not in prompt_dict:
+                prompt_dict['prompt'] = prompt_dict['text'] if 'text' in prompt_dict else prompt_dict['question']
+            if self.insert_media_token == 'left':
+                prompt_dict['prompt'] = self.media_token + prompt_dict['prompt']
+            elif selfinsert_media_token == 'right':
+                prompt_dict['prompt'] = prompt_dict['prompt'] + self.media_token
+            if 'image' in prompt_dict:
+                prompt_dict['image_path'] = prompt_dict['image']
+                image_path = os.path.join(self.media_base_path, prompt_dict['image'])
+                if cur_media_feature is None:
+                    cur_media_feature = ("image", self.image_processor(image_path))
+            if 'video' in prompt_dict:
+                prompt_dict['video_path'] = prompt_dict['video']
+                video_path = os.path.join(self.media_base_path, prompt_dict['video'])
+                if cur_media_feature is None:
+                    cur_media_feature = ("video", self.video_processor(video_path))
+            cur_item.append(prompt_dict)
+        return cur_media_feature, cur_item
+    
+def collate_function(batch):
+    # do nothing
+    return batch
 
-    def __getitem__(self, idx):
-        return self.sentences[idx]
+def do_inference(dataloader, model, length_params, sampling_params, cfg):
+    responses = []
+    all_prompts = []
+    for idx, batch_media_prompts in enumerate(dataloader):
+        if idx % 10 == 0:
+            print(f"Processed {idx} batch media")
+        for media_media_feature, prompts in batch_media_prompts:
+            media, media_feature = media_media_feature
+            all_prompts.extend(prompts.copy())
+            for prompt in prompts:
+                prompt[media] = media_feature
+            cur_batch_responses = model.generate(
+                input_prompts=prompts, length_params=length_params, sampling_params=sampling_params, inference_config=cfg
+                )
+            responses.extend(cur_batch_responses)
+    return responses, all_prompts
 
 
 @hydra_runner(config_path="conf", config_name="neva_inference")
 def main(cfg) -> None:
     model, image_processor, video_processor = create_neva_model_and_processor(cfg)
-
     length_params: LengthParam = {
         "max_length": cfg.inference.tokens_to_generate,
         "min_length": cfg.inference.min_tokens_to_generate,
@@ -68,35 +140,34 @@ def main(cfg) -> None:
         "compute_logprob": cfg.inference.compute_logprob,
         "end_strings": cfg.inference.end_strings,
     }
-
-    with open(cfg.prompt_file, 'r') as f:
-        lines = f.readlines()
+    
+    prompt_dicts = []
+    if cfg.prompt_file.endswith('.jsonl'):
+        with open(cfg.prompt_file, 'r') as f:
+            prompt_dicts = json.load(f)
+    else:
+        with open(cfg.prompt_file, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            prompt_dicts.append(json.loads(line))
 
     media_type_token = cfg.inference.get("media_type", "image")
     media_token = f"<{media_type_token}>"
 
     insert_media_token = cfg.inference.get("insert_media_token", None)
-    final_prompts = []
-    for line in lines:
-        prompt_dict = json.loads(line)
-        assert 'prompt' in prompt_dict or 'text' in prompt_dict
-        if 'prompt' not in prompt_dict:
-            prompt_dict['prompt'] = prompt_dict['text']
-        if insert_media_token == 'left':
-            prompt_dict['prompt'] = media_token + prompt_dict['prompt']
-        elif insert_media_token == 'right':
-            prompt_dict['prompt'] = prompt_dict['prompt'] + media_token
-        if 'image' in prompt_dict:
-            prompt_dict['image_path'] = prompt_dict['image']
-            prompt_dict['image'] = image_processor(os.path.join(cfg.inference.media_base_path, prompt_dict['image']))
-        if 'video' in prompt_dict:
-            prompt_dict['video_path'] = prompt_dict['video']
-            prompt_dict['video'] = video_processor(os.path.join(cfg.inference.media_base_path, prompt_dict['video']))
-        final_prompts.append(prompt_dict)
-
-    responses = model.generate(
-        input_prompts=final_prompts, length_params=length_params, sampling_params=sampling_params, inference_config=cfg
-    )
+    dataset = TemporalNevaDataset(prompt_dicts, \
+                                  cfg.inference.media_base_path, \
+                                  media_token, insert_media_token, \
+                                  image_processor,
+                                  video_processor)
+    #num_workers = min(multiprocessing.cpu_count()-1, 4)
+    num_workers = 2
+    dataloader = DataLoader(dataset,
+                            batch_size=cfg.inference.get("batch_size", 1),
+                            shuffle=False,
+                            collate_fn=collate_function,
+                            num_workers=num_workers)
+    responses, final_prompts = do_inference(dataloader, model, length_params, sampling_params, cfg)
 
     # =================== Start Quantization ====================
     if HAVE_AMMO and cfg.quantization.enable == True:
@@ -111,21 +182,26 @@ def main(cfg) -> None:
             raise ValueError(f"Unsupported quantization algorithm: {cfg.quantization.algorithm}")
 
         def forward_loop():
-            model.generate(
-                input_prompts=final_prompts,
-                length_params=length_params,
-                sampling_params=sampling_params,
-                inference_config=cfg,
-            )
+            num_samples = cfg.quantization.get("num_samples", 100)
+            if num_samples == -1:
+                cur_prompt_dicts = prompt_dicts
+            else:
+                cur_prompt_dicts = prompt_dicts[:num_samples]
+            cur_dataset = TemporalNevaDataset(cur_prompt_dicts, \
+                                              cfg.inference.media_base_path, \
+                                              media_token, insert_media_token, \
+                                              image_processor,
+                                              video_processor)
+            cur_dataloader = DataLoader(cur_dataset,
+                                        batch_size=cfg.inference.get("batch_size", 1),
+                                        shuffle=False,
+                                        collate_fn=collate_function,
+                                        num_workers=num_workers)
+            _, _= do_inference(cur_dataloader, model, length_params, sampling_params, cfg)
 
         atq.quantize(model, atq_config, forward_loop)
-
-        responses = model.generate(
-            input_prompts=final_prompts,
-            length_params=length_params,
-            sampling_params=sampling_params,
-            inference_config=cfg,
-        )
+        responses, final_prompts = do_inference(dataloader, model, length_params, sampling_params, cfg)
+        
     # ============== Quantization End =========================
 
     # PP middle stages do not yield any responses
@@ -136,7 +212,7 @@ def main(cfg) -> None:
         results = []
         for response, prompt in zip(responses, final_prompts):
             prompt['full_text'] = response["clean_text"]
-            prompt['text'] = response["clean_response"]
+            prompt['pred_answer'] = response["clean_response"]
             prompt['model_id'] = cfg.neva_model_file
             if 'image_path' in prompt:
                 prompt['image'] = prompt.pop('image_path')
