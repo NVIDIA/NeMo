f573e02e50a09b66ef79c22edd8965a7f18a37f6
marcromeyn <marcromeyn@users.noreply.github.com>
2024-06-24 08:08:33 +0000

    Apply isort and black reformatting
    Signed-off-by: marcromeyn <marcromeyn@users.noreply.github.com>

--- nemo/lightning/pytorch/callbacks/megatron_model_checkpoint.py
+++ nemo/lightning/pytorch/callbacks/megatron_model_checkpoint.py
@@ -26,10 +26,10 @@ from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint as PTLM
 from pytorch_lightning.callbacks.model_checkpoint import _is_local_file_protocol
 from pytorch_lightning.utilities import rank_zero_info
 
+from nemo.lightning.io.pl import TrainerCheckpoint
 from nemo.utils import logging
 from nemo.utils.app_state import AppState
 from nemo.utils.model_utils import ckpt_to_dir
-from nemo.lightning.io.pl import TrainerCheckpoint
 
 
 class ModelCheckpoint(PTLModelCheckpoint):
@@ -369,12 +369,12 @@ class ModelCheckpoint(PTLModelCheckpoint):
         # if anything goes wrong during checkpointing, we should be able to detect that data is incomplete.
         self.set_checkpoint_unfinished_marker(filepath, barrier_after=True)
         ema_callback = self._ema_callback(trainer)
-        
+
         from nemo.utils.get_rank import is_global_rank_zero
 
         if self.enable_nemo_ckpt_io and is_global_rank_zero():
             TrainerCheckpoint.from_trainer(trainer).io_dump(ckpt_to_dir(filepath))
-        
+
         if ema_callback is not None:
             with ema_callback.save_original_optimizer_state(trainer):
                 super()._save_checkpoint(trainer, filepath)
