6b0ffe9dc57b262f2649758dda055a8ed0696d43
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-06-24 05:59:40 -0700

    mini docstring
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/llm/gpt/model/mixtral.py
+++ nemo/collections/llm/gpt/model/mixtral.py
@@ -17,6 +17,10 @@ if TYPE_CHECKING:
 
 @dataclass
 class MixtralConfig(GPTConfig):
+    """
+    Config for Mixtral-8x7B model
+    Official announcement: https://mistral.ai/news/mixtral-of-experts/
+    """
     normalization: str = "RMSNorm"
     activation_func: Callable = F.silu
     position_embedding_type: str = "rope"
