41f9aa083821d66a330f5740b5f2d0ac14cdee9d
Abhishree <abhishreetm@gmail.com>
2024-06-25 17:49:49 -0700

    Add NemoQuery and requirements
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- nemo/deploy/__init__.py
+++ nemo/deploy/__init__.py
@@ -15,4 +15,5 @@
 
 from nemo.deploy.deploy_base import DeployBase
 from nemo.deploy.deploy_pytriton import DeployPyTriton
+from nemo.deploy.query import NemoQuery
 from nemo.deploy.triton_deployable import ITritonDeployable
