d809a60aae985597432d0ea309c1126d44033589
Pablo Garay <palenq@gmail.com>
2024-07-09 08:24:01 -0700

    add some more NeMoLogger tests and fix explicit_log_dir in 2.0
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- tests/lightning/test_nemo_logger.py
+++ tests/lightning/test_nemo_logger.py
@@ -1,11 +1,13 @@
+from pathlib import Path
 from unittest.mock import patch
-
+import os
 import pytest
 from pytorch_lightning.callbacks import ModelCheckpoint as PTLModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
 from nemo import lightning as nl
-
+from nemo.constants import NEMO_ENV_VARNAME_VERSION
+from nemo.utils.exp_manager import NotFoundError
 
 class TestNeMoLogger:
     @pytest.fixture
@@ -35,6 +37,14 @@ class TestNeMoLogger:
             logger.setup(trainer)
             mock_check.assert_called_once_with(trainer, explicit_dir, None, "test", None)
 
+    def test_default_log_dir(self, trainer):
+        
+        if os.environ.get(NEMO_ENV_VARNAME_VERSION, None) is not None:
+            del os.environ[NEMO_ENV_VARNAME_VERSION]
+        logger = nl.NeMoLogger(use_datetime_version=False)
+        app_state = logger.setup(trainer) 
+        assert app_state.log_dir == Path(Path.cwd() / "nemo_experiments" / "default")
+
     def test_custom_version(self, trainer):
         custom_version = "v1.0"
         logger = nl.NeMoLogger(name="test", version=custom_version, use_datetime_version=False)
@@ -58,3 +68,93 @@ class TestNeMoLogger:
         ptl_ckpt = next(cb for cb in trainer.callbacks if isinstance(cb, PTLModelCheckpoint))
         assert str(ptl_ckpt.dirpath).endswith("test_ckpt")
         assert ptl_ckpt.filename == "test-{epoch:02d}-{val_loss:.2f}"
+
+    def test_resume(self, trainer, tmp_path):
+        """Tests the resume capabilities of exp_manager"""
+
+        if os.environ.get(NEMO_ENV_VARNAME_VERSION, None) is not None:
+            del os.environ[NEMO_ENV_VARNAME_VERSION]
+
+        # Error because explicit_log_dir does not exist
+        ### TODO: make "model" arg optional
+        with pytest.raises(NotFoundError):
+            nl.AutoResume(
+                dirpath=str(tmp_path / "test_resume"),
+                resume_if_exists=True,
+            ).setup(None, trainer)
+
+        # Error because checkpoints folder does not exist
+        with pytest.raises(NotFoundError):
+            nl.AutoResume(
+                dirpath=str(tmp_path / "test_resume" / "does_not_exist"),
+                path="does_not_exist",
+                resume_if_exists=True,
+            ).setup(None, trainer)
+
+        # No error because we tell autoresume to ignore notfounderror
+        nl.AutoResume(
+            dirpath=str(tmp_path / "test_resume" / "does_not_exist"),
+            resume_if_exists=True,
+            resume_ignore_no_checkpoint=True,
+        ).setup(None, trainer)
+
+
+        path = Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints").mkdir(parents=True)
+        # Error because checkpoints do not exist in folder
+        with pytest.raises(NotFoundError):
+            nl.AutoResume(
+                dirpath=path,
+                resume_if_exists=True,
+            ).setup(None, trainer)
+
+        
+        Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints" / "mymodel--end").mkdir()
+        # Error because *end.ckpt is in folder indicating that training has already finished
+        with pytest.raises(ValueError):
+            nl.AutoResume(
+                dirpath=Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints"),
+                resume_if_exists=True,
+            ).setup(None, trainer)
+
+        Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints" / "mymodel--end").rmdir()
+        Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints" / "mymodel--last").mkdir()
+        Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints" / "mymodel2--last").mkdir()
+        # Error because multiple *last.ckpt is in folder. If more than one, don't know which to restore
+        with pytest.raises(ValueError):
+            nl.AutoResume(
+                dirpath=Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints"),
+                resume_if_exists=True,
+            ).setup(None, trainer)
+
+        # Finally succeed
+        logger = nl.NeMoLogger(
+            name="default",
+            dir=str(tmp_path) + "/test_resume",
+            version="version_0",
+            use_datetime_version=False,
+        )
+        logger.setup(trainer)
+        Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints" / "mymodel2--last").rmdir()
+        nl.AutoResume(
+            resume_if_exists=True,
+        ).setup(None, trainer)
+        checkpoint = Path(tmp_path / "test_resume" / "default" / "version_0" / "checkpoints" / "mymodel--last")
+        assert Path(trainer.ckpt_path).resolve() == checkpoint.resolve()
+
+        trainer = nl.Trainer(accelerator="cpu", logger=False)
+        # Check that model loads from `dirpath` and not <log_dir>/checkpoints
+        dirpath_log_dir = Path(tmp_path / "test_resume" / "dirpath_test" / "logs")
+        dirpath_log_dir.mkdir(parents=True)
+        dirpath_checkpoint_dir = Path(tmp_path / "test_resume" / "dirpath_test" / "ckpts")
+        dirpath_checkpoint = Path(dirpath_checkpoint_dir / "mymodel--last")
+        dirpath_checkpoint.mkdir(parents=True)
+        logger = nl.NeMoLogger(
+            name="default",
+            explicit_log_dir=dirpath_log_dir,
+        )
+        logger.setup(trainer)
+        nl.AutoResume(
+            resume_if_exists=True,
+            dirpath=str(dirpath_checkpoint_dir),
+        ).setup(None, trainer)
+        assert Path(trainer.ckpt_path).resolve() == dirpath_checkpoint.resolve()
