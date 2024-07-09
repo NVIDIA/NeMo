a33a9477215dea84268de9f1d5ac219b5c9c2f38
Chen Cui <chcui@nvidia.com>
2024-07-09 16:27:57 -0700

    hard code model lists
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- nemo/collections/nlp/modules/common/huggingface/huggingface_utils.py
+++ nemo/collections/nlp/modules/common/huggingface/huggingface_utils.py
@@ -25,28 +25,6 @@ from transformers import (
     RobertaConfig,
 )
 
-try:
-    from transformers import (
-        ALBERT_PRETRAINED_MODEL_ARCHIVE_LIST,
-        BERT_PRETRAINED_MODEL_ARCHIVE_LIST,
-        CAMEMBERT_PRETRAINED_MODEL_ARCHIVE_LIST,
-        DISTILBERT_PRETRAINED_MODEL_ARCHIVE_LIST,
-        GPT2_PRETRAINED_MODEL_ARCHIVE_LIST,
-        ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST,
-    )
-
-except (ImportError, ModuleNotFoundError):
-
-    # Guard deprecated imports
-    # Temporary solution
-    # TODO(@dimapihtar): fix current file in respect to transformers >= 4.40
-    ALBERT_PRETRAINED_MODEL_ARCHIVE_LIST = None
-    BERT_PRETRAINED_MODEL_ARCHIVE_LIST = None
-    CAMEMBERT_PRETRAINED_MODEL_ARCHIVE_LIST = None
-    DISTILBERT_PRETRAINED_MODEL_ARCHIVE_LIST = None
-    GPT2_PRETRAINED_MODEL_ARCHIVE_LIST = None
-    ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST = None
-
 from nemo.collections.nlp.modules.common.huggingface.albert import AlbertEncoder
 from nemo.collections.nlp.modules.common.huggingface.bert import BertEncoder
 from nemo.collections.nlp.modules.common.huggingface.camembert import CamembertEncoder
@@ -56,6 +34,70 @@ from nemo.collections.nlp.modules.common.huggingface.roberta import RobertaEncod
 from nemo.utils import logging
 
 __all__ = ["get_huggingface_lm_model", "get_huggingface_pretrained_lm_models_list", "VOCAB_FILE_NAME"]
+
+# Manually specify the model archive lists since these are now removed in HF
+# https://github.com/huggingface/transformers/blob/v4.40-release/src/transformers/models/deprecated/_archive_maps.py
+ALBERT_PRETRAINED_MODEL_ARCHIVE_LIST = [
+        "albert/albert-base-v1",
+        "albert/albert-large-v1",
+        "albert/albert-xlarge-v1",
+        "albert/albert-xxlarge-v1",
+        "albert/albert-base-v2",
+        "albert/albert-large-v2",
+        "albert/albert-xlarge-v2",
+        "albert/albert-xxlarge-v2",
+    ]
+
+BERT_PRETRAINED_MODEL_ARCHIVE_LIST = [
+        "google-bert/bert-base-uncased",
+        "google-bert/bert-large-uncased",
+        "google-bert/bert-base-cased",
+        "google-bert/bert-large-cased",
+        "google-bert/bert-base-multilingual-uncased",
+        "google-bert/bert-base-multilingual-cased",
+        "google-bert/bert-base-chinese",
+        "google-bert/bert-base-german-cased",
+        "google-bert/bert-large-uncased-whole-word-masking",
+        "google-bert/bert-large-cased-whole-word-masking",
+        "google-bert/bert-large-uncased-whole-word-masking-finetuned-squad",
+        "google-bert/bert-large-cased-whole-word-masking-finetuned-squad",
+        "google-bert/bert-base-cased-finetuned-mrpc",
+        "google-bert/bert-base-german-dbmdz-cased",
+        "google-bert/bert-base-german-dbmdz-uncased",
+        "cl-tohoku/bert-base-japanese",
+        "cl-tohoku/bert-base-japanese-whole-word-masking",
+        "cl-tohoku/bert-base-japanese-char",
+        "cl-tohoku/bert-base-japanese-char-whole-word-masking",
+        "TurkuNLP/bert-base-finnish-cased-v1",
+        "TurkuNLP/bert-base-finnish-uncased-v1",
+        "wietsedv/bert-base-dutch-cased",
+    ]
+CAMEMBERT_PRETRAINED_MODEL_ARCHIVE_LIST = ["almanach/camembert-base", "Musixmatch/umberto-commoncrawl-cased-v1", "Musixmatch/umberto-wikipedia-uncased-v1"]
+
+DISTILBERT_PRETRAINED_MODEL_ARCHIVE_LIST = [
+        "distilbert-base-uncased",
+        "distilbert-base-uncased-distilled-squad",
+        "distilbert-base-cased",
+        "distilbert-base-cased-distilled-squad",
+        "distilbert-base-german-cased",
+        "distilbert-base-multilingual-cased",
+        "distilbert-base-uncased-finetuned-sst-2-english",
+    ]
+GPT2_PRETRAINED_MODEL_ARCHIVE_LIST = [
+        "openai-community/gpt2",
+        "openai-community/gpt2-medium",
+        "openai-community/gpt2-large",
+        "openai-community/gpt2-xl",
+        "distilbert/distilgpt2",
+    ]
+ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST = [
+        "FacebookAI/roberta-base",
+        "FacebookAI/roberta-large",
+        "FacebookAI/roberta-large-mnli",
+        "distilbert/distilroberta-base",
+        "openai-community/roberta-base-openai-detector",
+        "openai-community/roberta-large-openai-detector",
+    ]
 
 
 HUGGINGFACE_MODELS = {
