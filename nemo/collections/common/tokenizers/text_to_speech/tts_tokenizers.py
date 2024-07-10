145f3fb9c291408b7958697404957f87a880c2c4
Huu Tuong Tu <83907151+huutuongtu@users.noreply.github.com>
2024-07-10 11:00:09 +0700

    Update tts_tokenizers.py

--- nemo/collections/common/tokenizers/text_to_speech/tts_tokenizers.py
+++ nemo/collections/common/tokenizers/text_to_speech/tts_tokenizers.py
@@ -31,6 +31,7 @@ from nemo.collections.common.tokenizers.text_to_speech.tokenizer_utils import (
     french_text_preprocessing,
     italian_text_preprocessing,
     spanish_text_preprocessing,
+    vietnamese_text_preprocessing,
 )
 from nemo.utils import logging
 from nemo.utils.decorators import experimental
@@ -201,6 +202,46 @@ class EnglishCharsTokenizer(BaseCharsTokenizer):
             text_preprocessing_func=text_preprocessing_func,
         )
 
+
+class VietnameseCharsTokenizer(BaseCharsTokenizer):
+
+    
+    _punctuation = '-();:,.!?¡¿—…"«»“”% '
+    _letters = '0123456789abcdefghijklmnopqrstuvwxyzđáàãảạăắằẵẳặâấầẫẩậóòõỏọôốồỗổộơớờỡởợéèẽẻẹêếềễểệúùũủụưứừữửựíìĩỉịýỳỹỷỵ'
+    _CHARSET_STR = _letters    
+    _PUNCT_LIST = _punctuation
+
+    def __init__(
+        self,
+        chars=_CHARSET_STR,
+        punct=True,
+        apostrophe=True,
+        add_blank_at=None,
+        pad_with_space=False,
+        non_default_punct_list=_PUNCT_LIST,
+        text_preprocessing_func=vietnamese_text_preprocessing,
+    ):
+        """German grapheme-based tokenizer.
+        Args:
+            punct: Whether to reserve grapheme for basic punctuation or not.
+            apostrophe: Whether to use apostrophe or not.
+            add_blank_at: Add blank to labels in the specified order ("last") or after tokens (any non None),
+             if None then no blank in labels.
+            pad_with_space: Whether to pad text with spaces at the beginning and at the end or not.
+            non_default_punct_list: List of punctuation marks which will be used instead default.
+            text_preprocessing_func: Text preprocessing function for correct execution of the tokenizer. By default, it
+            would keep any word unchanged.
+        """
+        super().__init__(
+            chars=chars,
+            punct=punct,
+            apostrophe=apostrophe,
+            add_blank_at=add_blank_at,
+            pad_with_space=pad_with_space,
+            non_default_punct_list=non_default_punct_list,
+            text_preprocessing_func=vietnamese_text_preprocessing,
+        )
+        
 
 class GermanCharsTokenizer(BaseCharsTokenizer):
 
