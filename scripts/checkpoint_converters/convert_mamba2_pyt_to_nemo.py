ec1b75b323f472b27a8aef60ea9ba28bd86fb7e5
Ali Taghibakhshi <71892896+JRD971000@users.noreply.github.com>
2024-07-15 11:56:59 -0500

    Alit/r2.0.0 (#9718)
    * fix TP>1 to TP1
    
    * add inference, update based on latest mcore commits
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * minor fix
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * cherry-picking mamba
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * bug fix, tutorial update
    
    * eval cleanup
    
    * clean up TP convertor
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    Signed-off-by: Ali Taghibakhshi <71892896+JRD971000@users.noreply.github.com>
    Co-authored-by: Ali Taghibakhshi <ataghibakhsh@login-eos01.eos.clusters.nvidia.com>
    Co-authored-by: JRD971000 <JRD971000@users.noreply.github.com>

--- scripts/checkpoint_converters/convert_mamba2_pyt_to_nemo.py
+++ scripts/checkpoint_converters/convert_mamba2_pyt_to_nemo.py
@@ -63,7 +63,7 @@ def get_args():
 
 def convert(args):
 
-    checkpoint_weights = torch.load(args.input_name_or_path, map_location='cpu')
+    checkpoint_weights = torch.load(args.input_name_or_path, map_location='cpu')['model']
     new_state_dict = {}
 
     if 'backbone' in list(checkpoint_weights.keys())[0]:
@@ -118,11 +118,6 @@ def convert(args):
             if '.norm.weight' in key and 'mixer' not in key:
                 key = key[:-11] + 'mixer.in_proj.layer_norm_weight'
             new_state_dict["model." + key] = value
-
-        # Tokenizer settings
-        tokenizer_library = 'megatron'
-        tokenizer_type = 'GPTSentencePieceTokenizer'
-        tokenizer_model = args.tokenizer_model_dir
 
         # Tokenizer settings
         tokenizer_library = 'megatron'
