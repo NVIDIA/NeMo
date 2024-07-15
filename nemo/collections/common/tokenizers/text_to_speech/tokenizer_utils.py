1c9d9cb285dcdb24fc1ad7a321787e9cfc1246fc
BuyuanCui <BuyuanCui@users.noreply.github.com>
2024-07-15 16:41:14 +0000

    Apply isort and black reformatting
    Signed-off-by: BuyuanCui <BuyuanCui@users.noreply.github.com>

--- nemo/collections/common/tokenizers/text_to_speech/tokenizer_utils.py
+++ nemo/collections/common/tokenizers/text_to_speech/tokenizer_utils.py
@@ -203,6 +203,6 @@ def chinese_text_preprocessing(text: str) -> str:
 def french_text_preprocessing(text: str) -> str:
     return text.lower()
 
+
 def japanese_text_preprocessing(text: str) -> str:
     return text.lower()
-
