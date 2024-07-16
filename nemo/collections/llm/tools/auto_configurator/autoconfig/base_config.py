8b4b609038975064e9d32e1c3f75b1acaf3c7b69
dimapihtar <dpihtar@gmail.com>
2024-07-16 08:43:45 -0700

    llama, mixtral and mistral model support for training time estiomation
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- nemo/collections/llm/tools/auto_configurator/autoconfig/base_config.py
+++ nemo/collections/llm/tools/auto_configurator/autoconfig/base_config.py
@@ -131,7 +131,7 @@ def _estimate_training_time(
     :raises NotImplementedError: if the model_name is not one of the supported models.
     """
     model_penalty = 1.15 if model_name == "mt5" else 1.0
-    valid_models = ["gpt3", "t5", "mt5", "bert"]
+    valid_models = ["gpt3", "t5", "mt5", "bert", "llama", "mixtral", "mistral"]
     try:
         if model_name in valid_models:
             return round(
