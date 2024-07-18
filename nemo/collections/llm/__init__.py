20632247e151b13cf82946eb114b520890c07d06
Abhishree <abhishreetm@gmail.com>
2024-07-18 13:53:33 -0700

    Add openai_format_response arg and store_args_to_json method
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- nemo/collections/llm/__init__.py
+++ nemo/collections/llm/__init__.py
@@ -5,7 +5,7 @@ except ImportError:
     pass
 
 from nemo.collections.llm import peft, tokenizer
-from nemo.collections.llm.api import export_ckpt, finetune, import_ckpt, pretrain, train, validate
+from nemo.collections.llm.api import export_ckpt, finetune, import_ckpt, pretrain, train, validate, deploy
 from nemo.collections.llm.gpt.data import (
     DollyDataModule,
     FineTuningDataModule,
@@ -99,6 +99,7 @@ __all__ = [
     "pretrain",
     "validate",
     "finetune",
+    "deploy",
     "tokenizer",
     "mock",
     "squad",
