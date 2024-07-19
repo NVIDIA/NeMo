dc6a28ab04aafe8d4aa5699c8ff6db21529cc4be
Ali Taghibakhshi <ataghibakhsh@login-eos01.eos.clusters.nvidia.com>
2024-07-19 12:17:58 -0700

    add rope scaling

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -441,6 +441,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
                 rotary_percent=self.cfg.get('rotary_percentage', 1.0),
                 seq_len_interpolation_factor=self.cfg.get('seq_len_interpolation_factor', None),
                 rotary_base=self.cfg.get('rotary_base', 10000),
+                scale_positional_embedding=self.cfg.get('scale_positional_embedding', False),
             )
             mcore_model_customize(self.cfg, model)
         else:
