b5d6d409352c230fc74ec7bf4f0245dc136ddb4e
dimapihtar <dimapihtar@users.noreply.github.com>
2024-07-22 14:49:05 +0000

    Apply isort and black reformatting
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>

--- tests/collections/llm/auto_conf/test_utils.py
+++ tests/collections/llm/auto_conf/test_utils.py
@@ -16,9 +16,7 @@ class TestUtils:
             300,
             "gpt3",
         )
-        assert (
-            model_size == 0.28
-        ), f"expected model_size is 0.28 but got {model_size}."
+        assert model_size == 0.28, f"expected model_size is 0.28 but got {model_size}."
 
         # Llama
         model_size = calculate_model_size(
@@ -29,9 +27,7 @@ class TestUtils:
             3000,
             "llama",
         )
-        assert (
-            model_size == 1.38
-        ), f"expected model_size is 1.38 but got {model_size}."
+        assert model_size == 1.38, f"expected model_size is 1.38 but got {model_size}."
 
         # Mixtral
         model_size = calculate_model_size(
@@ -42,9 +38,7 @@ class TestUtils:
             600,
             "mixtral",
         )
-        assert (
-            model_size == 12.9
-        ), f"expected model_size is 12.9 but got {model_size}."
+        assert model_size == 12.9, f"expected model_size is 12.9 but got {model_size}."
 
         # Mistral
         model_size = calculate_model_size(
@@ -55,9 +49,7 @@ class TestUtils:
             100,
             "mistral",
         )
-        assert (
-            model_size == 799.37
-        ), f"expected model_size is 799.37 but got {model_size}."
+        assert model_size == 799.37, f"expected model_size is 799.37 but got {model_size}."
 
     def test_calculate_train_time(self):
         # GPT
@@ -68,9 +60,7 @@ class TestUtils:
             300,
             "gpt3",
         )
-        assert (
-            train_time == 33.91
-        ), f"expected train_time is 33.91 but got {train_time}."
+        assert train_time == 33.91, f"expected train_time is 33.91 but got {train_time}."
 
         # Llama
         train_time = _estimate_training_time(
@@ -80,9 +70,7 @@ class TestUtils:
             3000,
             "llama",
         )
-        assert (
-            train_time == 316.48
-        ), f"expected train_time is 316.48 but got {train_time}."
+        assert train_time == 316.48, f"expected train_time is 316.48 but got {train_time}."
 
         # Mixtral
         train_time = _estimate_training_time(
@@ -92,9 +80,7 @@ class TestUtils:
             1000,
             "mixtral",
         )
-        assert (
-            train_time == 4.13
-        ), f"expected train_time is 4.13 but got {train_time}."
+        assert train_time == 4.13, f"expected train_time is 4.13 but got {train_time}."
 
         # Mistral
         train_time = _estimate_training_time(
@@ -104,9 +90,7 @@ class TestUtils:
             250,
             "mistral",
         )
-        assert (
-            train_time == 176.83
-        ), f"expected train_time is 176.83 but got {train_time}."
+        assert train_time == 176.83, f"expected train_time is 176.83 but got {train_time}."
 
     def test_calculate_model_params(self):
         # GPT
@@ -116,8 +100,13 @@ class TestUtils:
             2048,
             "gpt3",
         )
-        assert (
-            params == (48, 8192, 64, None, None, 8e-05)
+        assert params == (
+            48,
+            8192,
+            64,
+            None,
+            None,
+            8e-05,
         ), f"expected model_params set is (48, 8192, 64, None, None, 8e-05) but got {params}."
 
         # Llama
@@ -127,8 +116,13 @@ class TestUtils:
             8192,
             "llama",
         )
-        assert (
-            params == (56, 10240, 80, None, None, 7e-05)
+        assert params == (
+            56,
+            10240,
+            80,
+            None,
+            None,
+            7e-05,
         ), f"expected model_params set is (56, 10240, 80, None, None, 7e-05) but got {params}."
 
         # Mixtral
@@ -138,8 +132,13 @@ class TestUtils:
             4096,
             "mixtral",
         )
-        assert (
-            params == (36, 8192, 64, None, None, 8e-05)
+        assert params == (
+            36,
+            8192,
+            64,
+            None,
+            None,
+            8e-05,
         ), f"expected model_params set is (36, 8192, 64, None, None, 8e-05) but got {params}."
 
         # Mistral
@@ -149,6 +148,11 @@ class TestUtils:
             4096,
             "mistral",
         )
-        assert (
-            params == (16, 1536, 16, None, None, 0.00025)
+        assert params == (
+            16,
+            1536,
+            16,
+            None,
+            None,
+            0.00025,
         ), f"expected model_params set is (16, 1536, 16, None, None, 0.00025) but got {params}."
