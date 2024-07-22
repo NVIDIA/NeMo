58d21e36f9471087ba56fcbeb6f469181c3de544
Hemil Desai <hemild@nvidia.com>
2024-07-22 14:17:16 -0700

    Save checkpoint on train end if preemption callback is used
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- nemo/lightning/pytorch/callbacks/preemption.py
+++ nemo/lightning/pytorch/callbacks/preemption.py
@@ -42,7 +42,7 @@ class PreemptionCallback(Callback, IOMixin):
         self.sig = sig if sig is not None else signal.SIGTERM
         self._interrupted = False
         self._handler_context = None
-        self._preemption_supported = None
+        self.preemption_supported = None
 
     def on_train_start(self, trainer: Trainer, pl_module) -> None:
         if self.preemption_supported:
@@ -51,7 +51,7 @@ class PreemptionCallback(Callback, IOMixin):
 
     def on_train_batch_start(self, trainer: Trainer, pl_module, batch, batch_idx: int) -> None:
         if not self.preemption_supported:
-            self._preemption_supported = self._check_preemption_support()
+            self.preemption_supported = self._check_preemption_support()
             if self.preemption_supported:
                 self._handler_context = self._preemption_handler()
                 self._handler_context.__enter__()
@@ -96,9 +96,9 @@ class PreemptionCallback(Callback, IOMixin):
 
     @property
     def preemption_supported(self) -> bool:
-        if self._preemption_supported is None:
-            self._preemption_supported = self._check_preemption_support()
-        return self._preemption_supported
+        if self.preemption_supported is None:
+            self.preemption_supported = self._check_preemption_support()
+        return self.preemption_supported
 
     def _check_preemption_support(self) -> bool:
         return torch.distributed.is_available() and torch.distributed.is_initialized()
