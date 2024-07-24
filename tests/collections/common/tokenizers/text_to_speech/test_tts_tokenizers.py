25b47cd3373ac55fed6a80585f302f03001cdadc
Pablo Garay <palenq@gmail.com>
2024-07-24 13:30:07 -0700

    Revert "Jpg2p jun18 (#9538)" (#9874)
    * Revert "Jpg2p jun18 (#9538)"
    
    This reverts commit 53d7a917f7d5fb6f0b01b28170312a5086311384.
    
    * Apply isort and black reformatting
    
    Signed-off-by: pablo-garay <pablo-garay@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: pablo-garay <pablo-garay@users.noreply.github.com>
    Co-authored-by: pablo-garay <pablo-garay@users.noreply.github.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- tests/collections/common/tokenizers/text_to_speech/test_tts_tokenizers.py
+++ tests/collections/common/tokenizers/text_to_speech/test_tts_tokenizers.py
@@ -20,11 +20,9 @@ from nemo.collections.common.tokenizers.text_to_speech.tts_tokenizers import (
     GermanCharsTokenizer,
     IPATokenizer,
     ItalianCharsTokenizer,
-    JapanesePhonemeTokenizer,
     SpanishCharsTokenizer,
 )
 from nemo.collections.tts.g2p.models.i18n_ipa import IpaG2p
-from nemo.collections.tts.g2p.models.ja_jp_ipa.py import JapaneseG2p
 
 
 class TestTTSTokenizers:
@@ -45,10 +43,6 @@ class TestTTSTokenizers:
         "BONJOUR": ["bɔ̃ʒˈuʁ"],
         "LE": ["lˈə-"],
         "MONDE": ["mˈɔ̃d"],
-    }
-    PHONEME_DICT_JA = {
-        "ハロー": ["haɾoː"],
-        "ワールド": ["wa:ɾdo"],
     }
 
     @staticmethod
@@ -257,16 +251,4 @@ class TestTTSTokenizers:
 
         chars, tokens = self._parse_text(tokenizer, "Hello, wound")
         expected_output = "HELLO, ˈwund"
-        assert chars == expected_output
-
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_japanese_phoneme_tokenizer(self):
-        input_text = "ハロー ワールド."
-        expected_output = "haɾoː wa:ɾdo."
-        g2p = JapaneseG2p(phoneme_dict=self.PHONEME_DICT_JA)
-
-        tokenizer = JapanesePhonemeTokenizer(g2p=g2p)
-        chars, tokens = self._parse_text(tokenizer, input_text)
-
         assert chars == expected_output
