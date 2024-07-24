61091c7463a0437dbcb61234e87deafc9fa73ffe
XuesongYang <XuesongYang@users.noreply.github.com>
2024-07-24 00:37:40 +0000

    Apply isort and black reformatting
    Signed-off-by: XuesongYang <XuesongYang@users.noreply.github.com>

--- nemo/collections/common/tokenizers/text_to_speech/tokenizer_utils.py
+++ nemo/collections/common/tokenizers/text_to_speech/tokenizer_utils.py
@@ -207,7 +207,7 @@ def french_text_preprocessing(text: str) -> str:
 
 def vietnamese_text_preprocessing(text: str) -> str:
     return text.lower()
-  
+
 
 def japanese_text_preprocessing(text: str) -> str:
     return text.lower()
