3d9e47266baf5b3d473c52cca158e519b3a3fecd
Selvaraj Anandaraj <anandaraj@wisc.edu>
2024-07-25 16:32:59 -0700

    Added defer wgrad support with mcore optim (#9896)
    * Added defer wgrad support with mcore optim
    
    Signed-off-by: Selvaraj Anandaraj <selvaraja@login-eos02.eos.clusters.nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: sanandaraj5597 <sanandaraj5597@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Selvaraj Anandaraj <selvaraja@login-eos02.eos.clusters.nvidia.com>
    Signed-off-by: sanandaraj5597 <sanandaraj5597@users.noreply.github.com>
    Co-authored-by: Selvaraj Anandaraj <selvaraja@login-eos02.eos.clusters.nvidia.com>
    Co-authored-by: sanandaraj5597 <sanandaraj5597@users.noreply.github.com>
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -826,7 +826,9 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             ignore_virtual=True
         ):
             if (
-                self.cfg.get('defer_embedding_wgrad_compute', False) and self.mcore_gpt
+                self.cfg.get('defer_embedding_wgrad_compute', False)
+                and self.mcore_gpt
+                and not self.use_mcore_dist_optim
             ):  # Silently ignore the optimization if MCORE is not used
                 module_list = self.get_model_module_list()
                 if len(module_list) > 1:
@@ -849,7 +851,9 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             ignore_virtual=True
         ):
             if (
-                self.cfg.get('defer_embedding_wgrad_compute', False) and self.mcore_gpt
+                self.cfg.get('defer_embedding_wgrad_compute', False)
+                and self.mcore_gpt
+                and not self.use_mcore_dist_optim
             ):  # Silently ignore the optimization if MCORE is not used
                 module_list = self.get_model_module_list()
                 if len(module_list) > 1:
