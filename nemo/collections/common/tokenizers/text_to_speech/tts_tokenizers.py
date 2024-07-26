251d3343bc188e5525af5659afff0eb1588942af
Huu Tuong Tu <83907151+huutuongtu@users.noreply.github.com>
2024-07-26 15:24:48 +0700

    [TTS][Vietnamese] Add VietnameseCharsTokenizer (#9665)
    * Update tts_tokenizers.py
    * Update tokenizer_utils.py
    * Update test_tts_tokenizers.py
    * Apply isort and black reformatting
    
    Signed-off-by: huutuongtu <huutuongtu@users.noreply.github.com>
    
    * Signed-off-by: Tu [huutu12312vn@gmail.com](mailto:huutu12312vn@gmail.com)
    
    * Update ipa_lexicon.py - Signed-off-by: Tu [huutu12312vn@gmail.com](mailto:huutu12312vn@gmail.com)
    
    Signed-off-by: XuesongYang <XuesongYang@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: huutuongtu <huutuongtu@users.noreply.github.com>
    Signed-off-by: Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
    Signed-off-by: XuesongYang <XuesongYang@users.noreply.github.com>
    Co-authored-by: huutuongtu <huutuongtu@users.noreply.github.com>
    Co-authored-by: Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
    Co-authored-by: XuesongYang <XuesongYang@users.noreply.github.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo/collections/common/tokenizers/text_to_speech/tts_tokenizers.py
+++ nemo/collections/common/tokenizers/text_to_speech/tts_tokenizers.py
@@ -31,6 +31,7 @@ from nemo.collections.common.tokenizers.text_to_speech.tokenizer_utils import (
     french_text_preprocessing,
     italian_text_preprocessing,
     spanish_text_preprocessing,
+    vietnamese_text_preprocessing,
 )
 from nemo.utils import logging
 from nemo.utils.decorators import experimental
@@ -199,6 +200,43 @@ class EnglishCharsTokenizer(BaseCharsTokenizer):
             pad_with_space=pad_with_space,
             non_default_punct_list=non_default_punct_list,
             text_preprocessing_func=text_preprocessing_func,
+        )
+
+
+class VietnameseCharsTokenizer(BaseCharsTokenizer):
+
+    _LOCALE = "vi-VN"
+    _CHARSET_STR = get_grapheme_character_set(locale=_LOCALE, case="mixed")
+
+    def __init__(
+        self,
+        chars=_CHARSET_STR,
+        punct=True,
+        apostrophe=True,
+        add_blank_at=None,
+        pad_with_space=False,
+        non_default_punct_list=None,
+        text_preprocessing_func=vietnamese_text_preprocessing,
+    ):
+        """Vietnamese grapheme tokenizer.
+        Args:
+            punct: Whether to reserve grapheme for basic punctuation or not.
+            apostrophe: Whether to use apostrophe or not.
+            add_blank_at: Add blank to labels in the specified order ("last") or after tokens (any non None),
+            if None then no blank in labels.
+            pad_with_space: Whether to pad text with spaces at the beginning and at the end or not.
+            non_default_punct_list: List of punctuation marks which will be used instead default.
+            text_preprocessing_func: Text preprocessing function for correct execution of the tokenizer. By default, it
+            would keep any word lowercase.
+        """
+        super().__init__(
+            chars=chars,
+            punct=punct,
+            apostrophe=apostrophe,
+            add_blank_at=add_blank_at,
+            pad_with_space=pad_with_space,
+            non_default_punct_list=non_default_punct_list,
+            text_preprocessing_func=vietnamese_text_preprocessing,
         )
 
 
