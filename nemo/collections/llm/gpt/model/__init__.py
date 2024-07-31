404588f354720eca725948e94ab30d1633ed297d
akoumpa <akoumpa@users.noreply.github.com>
2024-07-31 09:15:56 +0000

    Apply isort and black reformatting
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>

--- nemo/collections/llm/gpt/model/__init__.py
+++ nemo/collections/llm/gpt/model/__init__.py
@@ -29,11 +29,7 @@ from nemo.collections.llm.gpt.model.llama import (
     LlamaModel,
 )
 from nemo.collections.llm.gpt.model.mistral import MistralConfig7B, MistralModel
-from nemo.collections.llm.gpt.model.mixtral import (
-    MixtralConfig8x7B,
-    MixtralConfig8x22B,
-    MixtralModel,
-)
+from nemo.collections.llm.gpt.model.mixtral import MixtralConfig8x7B, MixtralConfig8x22B, MixtralModel
 
 __all__ = [
     "GPTConfig",
