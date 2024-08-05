60dae9fbcd51a73ca38bd83e52c8beae86e68443
Piotr Żelasko <petezor@gmail.com>
2024-08-05 10:59:20 -0400

    relieve heavy CPU memory usage for super-long tarred recordings
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>

--- nemo/collections/common/data/lhotse/nemo_adapters.py
+++ nemo/collections/common/data/lhotse/nemo_adapters.py
@@ -362,11 +362,17 @@ class LazyNeMoTarredIterator:
                         num_samples=meta.frames,
                         duration=meta.duration,
                     )
+                    cuts_for_recording = []
                     for data in sorted(shard_manifest[tar_info.name], key=lambda d: d["audio_filepath"]):
-                        cut = recording.to_cut().truncate(
-                            offset=data.get("offset", 0.0),
-                            duration=data.get("duration"),
-                            preserve_id=True,
+                        # Cut the recording into corresponding segment and discard audio data outside the segment.
+                        cut = (
+                            recording.to_cut()
+                            .truncate(
+                                offset=data.get("offset", 0.0),
+                                duration=data.get("duration"),
+                                preserve_id=True,
+                            )
+                            .move_to_memory(audio_format="wav")
                         )
                         cut.supervisions.append(
                             SupervisionSegment(
@@ -381,7 +387,10 @@ class LazyNeMoTarredIterator:
                         cut.custom = _to_custom_attr_dict(data)
                         for extra_field in extra_fields:
                             extra_field.attach_to(cut)
-                        yield cut
+                        cuts_for_recording.append(cut)
+                    del recording  # free the memory - helps with very large audio files
+                    del raw_audio
+                    yield from cuts_for_recording
 
     def __len__(self) -> int:
         return len(self.source)
