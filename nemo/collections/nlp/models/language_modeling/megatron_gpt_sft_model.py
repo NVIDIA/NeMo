4f79c60ffcc8ef7b6b275a5f6550d6f8a5994700
Michal Futrega <mfutrega@nvidia.com>
2024-08-05 17:44:57 +0200

    Add finalize_model_grads

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_sft_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_sft_model.py
@@ -41,6 +41,7 @@ from nemo.utils import AppState, logging
 try:
     from megatron.core import parallel_state
     from megatron.core.pipeline_parallel.schedules import get_forward_backward_func
+    from megatron.core.distributed import finalize_model_grads
 
     HAVE_MEGATRON_CORE = True
 
@@ -397,6 +398,8 @@ class MegatronGPTSFTModel(NLPAdapterModelMixin, MegatronGPTModel):
             module.config.no_sync_func = no_sync_func
             module.config.grad_sync_func = grad_sync_func
             module.config.param_sync_func = param_sync_func
+            if self.use_mcore_dist_optim:
+                module.config.finalize_model_grads_func = finalize_model_grads
 
         fwd_bwd_function = get_forward_backward_func()
 
