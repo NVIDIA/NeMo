c3b95388c6de720f81c2355bd3bcefdbc2c4cc01
Maanu Grover <maanug@nvidia.com>
2024-08-06 16:26:08 -0700

    always broadcast val loss in case of early stop
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- nemo/lightning/pytorch/strategies.py
+++ nemo/lightning/pytorch/strategies.py
@@ -482,9 +482,8 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
 
         with self.precision_plugin.val_step_context():  # TODO: Do we need this?
             out = self.model(dataloader_iter, forward_only=True, *args, **kwargs)
-            _strategy_lib._sync_from_last_pipeline_stage(out, broadcast=False)  # p2p now, broadcast later at ckpt
-            if torch.distributed.get_rank() == 0:
-                self.lightning_module.log('val_loss', out, prog_bar=True, rank_zero_only=True, batch_size=1)
+            _strategy_lib._sync_from_last_pipeline_stage(out, broadcast=True)
+            self.lightning_module.log('val_loss', out, prog_bar=True, batch_size=1)
             return out
 
     @override
