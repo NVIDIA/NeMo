52b371d06bba3a5d226b47660f8d14b6b1b23da3
tonyjie <jl4257@cornell.edu>
2024-08-06 15:55:35 -0700

    add copyright; remove unused commented line
    Signed-off-by: tonyjie <jl4257@cornell.edu>

--- nemo/utils/memory_profile_analyzer.py
+++ nemo/utils/memory_profile_analyzer.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pickle
 import os
 import csv
@@ -6,7 +20,6 @@ import numpy as np
 from scipy.signal import find_peaks, peak_prominences
 from nemo.utils import logging
 
-# __all__ = ['peak_memory_analysis_weight', 'peak_memory_analysis_activation', 'peak_memory_analysis_oom']
 _all__ = ['peak_memory_analysis']
 
 GB_SIZE = 1024*1024*1024
