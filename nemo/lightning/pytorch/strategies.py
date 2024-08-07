e846789a16d753d6a98866090d861698777b3d71
Maanu Grover <maanug@nvidia.com>
2024-08-07 15:56:05 -0700

    use simpler average
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- nemo/lightning/pytorch/strategies.py
+++ nemo/lightning/pytorch/strategies.py
@@ -511,9 +511,7 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
         from megatron.core import parallel_state
 
         if parallel_state.is_pipeline_last_stage():
-            total_loss_and_total_samples = torch.vstack(self.validation_step_outputs).sum(axis=0)
-            avg_loss = total_loss_and_total_samples[0] / total_loss_and_total_samples[1]
-            averaged_loss = avg_loss.type(torch.float32).cuda()
+            averaged_loss = torch.stack(self.validation_step_outputs).mean()
         else:
             averaged_loss = torch.tensor(0.0, dtype=torch.float32).cuda()
 
