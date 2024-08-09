b3f92e95bdbcade901ce84b82a6a8ddc62db7dbf
zhehuaichen <dian.chenzhehuai@gmail.com>
2024-08-09 07:36:07 -0700

    for llama3.1 Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>

--- nemo/collections/multimodal/speech_llm/parts/utils/data_utils.py
+++ nemo/collections/multimodal/speech_llm/parts/utils/data_utils.py
@@ -312,7 +312,7 @@ class TextProcessing:
         else:
             pre_pad = []
         answer_text = text[len(context) :]
-        answer_ids = pre_pad + self.tokenizer.text_to_ids(answer_text, self.sample_alpha)
+        answer_ids = pre_pad + self.tokenizer.text_to_ids(answer_text)
         if self.end_string:
             answer_ids += self.tokenizer.text_to_ids(self.end_string)
 
