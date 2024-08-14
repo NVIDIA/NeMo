3250e60d67407870f8c50fe953fcbf1ef17a7e6a
Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
2024-08-13 19:24:35 -0700

    nemo-ux: run checkpoint conversion only on rank=0 if invoked with tor… (#10082)
    * nemo-ux: run checkpoint conversion only on rank=0 if invoked with torchrun
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Use is_global_rank_zero
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    ---------
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo/lightning/io/mixin.py
+++ nemo/lightning/io/mixin.py
@@ -293,9 +293,12 @@ class ConnectorMixin:
         """
         connector = self._get_connector(path)
         ckpt_path: Path = connector.local_path(base_path=base_path)
-        ckpt_path = connector(ckpt_path, overwrite=overwrite)
+        # If already in multiproc environment (e.g. due to torchrun invocation) run only on RANK = 0
+        from nemo.utils.get_rank import is_global_rank_zero
 
-        connector.on_import_ckpt(self)
+        if is_global_rank_zero():
+            ckpt_path = connector(ckpt_path, overwrite=overwrite)
+            connector.on_import_ckpt(self)
 
         return ckpt_path
 
