f96d1c205911c4ba21af632885989d5bb87d4d3b
dimapihtar <dimapihtar@users.noreply.github.com>
2024-08-15 13:07:27 +0000

    Apply isort and black reformatting
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>

--- nemo/collections/nlp/parts/nlp_overrides.py
+++ nemo/collections/nlp/parts/nlp_overrides.py
@@ -414,7 +414,7 @@ class NLPDDPStrategy(DDPStrategy):
 
         if self.save_last_n_optim_states >= 0 and '-last' in filepath:
             self._drop_optimizer_states(filepath)
-        #print(f"SAVE: {self.dropped_optim_states}")
+        # print(f"SAVE: {self.dropped_optim_states}")
 
     # PTL 2.2 supports non strict loading of the ckpt with the strict arg (https://github.com/Lightning-AI/pytorch-lightning/pull/19404)
     def load_model_state_dict(self, checkpoint: Mapping[str, Any], strict: bool = True) -> None:
@@ -598,7 +598,7 @@ class NLPDDPStrategy(DDPStrategy):
         checkpoints = self._get_checkpoints_list(filepath)
 
         # Drop optimizer states
-        checkpoint_index = (len(checkpoints) - self.save_last_n_optim_states - 1)
+        checkpoint_index = len(checkpoints) - self.save_last_n_optim_states - 1
         if len(checkpoints) > self.save_last_n_optim_states:
             checkpoint_path = checkpoints[checkpoint_index]
 
@@ -628,7 +628,9 @@ class NLPDDPStrategy(DDPStrategy):
     def _get_checkpoints_list(self, filepath: Union[str, Path]):
         checkpoint_dir = os.path.dirname(filepath)
         checkpoints = [
-            d for d in os.listdir(checkpoint_dir) if os.path.isdir(os.path.join(checkpoint_dir, d)) and '-last' not in d
+            d
+            for d in os.listdir(checkpoint_dir)
+            if os.path.isdir(os.path.join(checkpoint_dir, d)) and '-last' not in d
         ]
         checkpoints = sorted(checkpoints, key=lambda x: int(x.split('-step=')[1].split('-')[0]))
         checkpoints = [os.path.join(checkpoint_dir, checkpoint) for checkpoint in checkpoints]
