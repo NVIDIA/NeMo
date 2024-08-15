de5d87bbe4fae589bc5114b9d73489386488e26b
Ao Tang <aot@nvidia.com>
2024-08-15 07:28:46 -0700

    add more config

--- nemo/collections/llm/gpt/model/nemotron.py
+++ nemo/collections/llm/gpt/model/nemotron.py
@@ -3,13 +3,12 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Annotated, Callable, Optional
 
 import torch
-import torch.nn.functional as F
 from torch import nn
 
 from nemo.collections.llm.gpt.model.base import GPTConfig, GPTModel
 from nemo.collections.llm.utils import Config
-from nemo.collections.nlp.modules.common.megatron.utils import squared_relu
 from nemo.lightning import OptimizerModule, io, teardown
+from nemo.collections.llm.fn.activation import squared_relu
 
 if TYPE_CHECKING:
     from transformers import NemotronConfig as HFNemotronConfig
@@ -17,6 +16,7 @@ if TYPE_CHECKING:
 
     from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTokenizer
     from nemo.collections.common.tokenizers.tokenizer_spec import TokenizerSpec
+
 
 
 @dataclass
@@ -30,11 +30,22 @@ class NemotronConfig(GPTConfig):
     rotary_percent: float = 0.5
     hidden_dropout: float = 0.0
     attention_dropout: float = 0.0
-    layernorm_zero_centered_gamma: bool = True  # layernorm1p
+    layernorm_zero_centered_gamma: bool = True # layernorm1p
     init_method_std: float = 0.01
-    # apply_query_key_layer_scaling: bool = True
     share_embeddings_and_output_weights: bool = False
     kv_channels: int = None
+    num_query_groups: int = None
+
+
+@dataclass
+class Nemotron3Config4B(NemotronConfig):
+    num_layers: int = 32
+    hidden_size: int = 3072
+    ffn_hidden_size: int = 9216
+    kv_channels: int = 128
+    num_query_groups: int = 8
+    num_attention_heads: int = 24
+    init_method_std: float = 0.0134
 
 
 @dataclass
@@ -43,6 +54,25 @@ class Nemotron3Config8B(NemotronConfig):
     hidden_size: int = 4096
     ffn_hidden_size: int = 16384
     num_attention_heads: int = 32
+
+@dataclass
+class Nemotron4Config15B(NemotronConfig):
+    num_layers: int = 32
+    hidden_size: int = 6144
+    ffn_hidden_size: int = 24576
+    num_attention_heads: int = 48
+    num_query_groups: int = 8
+    init_method_std: float = 0.0134
+
+
+@dataclass
+class Nemotron4Config340B(NemotronConfig):
+    num_layers: int = 96
+    hidden_size: int = 18432
+    ffn_hidden_size: int = 73728
+    num_attention_heads: int = 96
+    num_query_groups: int = 8
+    init_method_std: float = 0.0063
 
 
 class NemotronModel(GPTModel):
@@ -54,7 +84,6 @@ class NemotronModel(GPTModel):
         model_transform: Optional[Callable[[nn.Module], nn.Module]] = None,
     ):
         super().__init__(config or NemotronConfig(), optim=optim, tokenizer=tokenizer, model_transform=model_transform)
-
 
 @io.model_importer(NemotronModel, "hf")
 class HFNemotronImporter(io.ModelConnector["NemotronForCausalLM", NemotronModel]):
@@ -96,12 +125,6 @@ class HFNemotronImporter(io.ModelConnector["NemotronForCausalLM", NemotronModel]
 
     @property
     def tokenizer(self) -> "AutoTokenizer":
-        from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
-
-        return get_nmt_tokenizer(
-            model_name='/aot/checkpoints/nemotron/nemotron3-8b/tokenizer.model',
-            tokenizer_model='/aot/checkpoints/nemotron/nemotron3-8b/tokenizer.model',
-        )
         from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTokenizer
 
         return AutoTokenizer(str(self))
@@ -134,7 +157,6 @@ class HFNemotronImporter(io.ModelConnector["NemotronForCausalLM", NemotronModel]
         )
 
         return output
-
 
 @io.model_exporter(NemotronModel, "hf")
 class HFNemotronExporter(io.ModelConnector[NemotronModel, "NemotronForCausalLM"]):
@@ -186,11 +208,7 @@ class HFNemotronExporter(io.ModelConnector[NemotronModel, "NemotronForCausalLM"]
             hidden_size=source.hidden_size,
             intermediate_size=source.ffn_hidden_size,
             num_attention_heads=source.num_attention_heads,
-            head_dim=(
-                source.kv_channels
-                if source.kv_channels is not None
-                else source.hidden_size // source.num_attention_heads
-            ),
+            head_dim=source.kv_channels if source.kv_channels is not None else source.hidden_size // source.num_attention_heads,
             tie_word_embeddings=source.share_embeddings_and_output_weights,
             max_position_embeddings=source.seq_length,
             initializer_range=source.init_method_std,
@@ -200,7 +218,6 @@ class HFNemotronExporter(io.ModelConnector[NemotronModel, "NemotronForCausalLM"]
             partial_rotary_factor=source.rotary_percent,
             vocab_size=self.tokenizer.vocab_size,
         )
-
 
 @io.state_transform(
     source_key=(
@@ -243,7 +260,6 @@ def _import_qkv(ctx: io.TransformCTX, q, k, v):
 
     return qkv_weights
 
-
 @io.state_transform(
     source_key="decoder.layers.*.self_attention.linear_qkv.weight",
     target_key=(
@@ -279,9 +295,11 @@ def _export_qkv(ctx: io.TransformCTX, linear_qkv):
 
     return q_proj, k_proj, v_proj
 
-
 __all__ = [
     "NemotronConfig",
+    "Nemotron3Config4B",
     "Nemotron3Config8B",
+    "Nemotron4Config15B",
+    "Nemotron4Config340B",
     "NemotronModel",
 ]
