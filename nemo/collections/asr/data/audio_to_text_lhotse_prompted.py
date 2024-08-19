4c8a8199f34dfa01f402a0662b70cfcc3b572c30
pzelasko <pzelasko@users.noreply.github.com>
2024-08-19 17:49:52 +0000

    Apply isort and black reformatting
    Signed-off-by: pzelasko <pzelasko@users.noreply.github.com>

--- nemo/collections/asr/data/audio_to_text_lhotse_prompted.py
+++ nemo/collections/asr/data/audio_to_text_lhotse_prompted.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from dataclasses import dataclass
-from typing import Callable, Union, Optional
+from typing import Callable, Optional, Union
 
 import torch.utils.data
 from lhotse import CutSet
