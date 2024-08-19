376f463ec4be09bf8d8c8920ce8f51669a9b065b
Boxiang Wang <boxiangw@nvidia.com>
2024-08-19 14:37:26 -0700

    Change recipe
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>

--- nemo/collections/llm/recipes/mixtral_8x7b_16k.py
+++ nemo/collections/llm/recipes/mixtral_8x7b_16k.py
@@ -18,10 +18,10 @@ def pretrain_recipe(
 
     trainer = mixtral_8x7b.trainer(
         tensor_parallelism=2,
-        pipeline_parallelism=2,
+        pipeline_parallelism=4,
         pipeline_parallelism_type=torch.bfloat16,
         virtual_pipeline_parallelism=8,
-        context_parallelism=2,
+        context_parallelism=4,
         sequence_parallelism=True,
         expert_parallelism=8,
         num_nodes=num_nodes,
@@ -43,10 +43,10 @@ def finetune_recipe(name: str, ckpt_dir: str, num_nodes: int, num_gpus_per_node:
 
     trainer = mixtral_8x7b.trainer(
         tensor_parallelism=2,
-        pipeline_parallelism=4,
+        pipeline_parallelism=2,
         pipeline_parallelism_type=torch.bfloat16,
         virtual_pipeline_parallelism=8,
-        context_parallelism=4,
+        context_parallelism=2,
         sequence_parallelism=True,
         expert_parallelism=8,
         num_nodes=num_nodes,
