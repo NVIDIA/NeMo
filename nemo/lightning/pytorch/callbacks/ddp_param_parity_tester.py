490d25d3cb4c64ab278ec410304e568c5522625e
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-08-19 16:47:24 -0700

    Improve messaging
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/callbacks/ddp_param_parity_tester.py
+++ nemo/lightning/pytorch/callbacks/ddp_param_parity_tester.py
@@ -27,15 +27,15 @@ def pl_check_param_hashes_across_dp_replicas(trainer):
             opt.disable_pre_hook()
 
     torch.distributed.barrier()
-    assert check_param_hashes_across_dp_replicas(
+    res = check_param_hashes_across_dp_replicas(
         [trainer.strategy.model]
-    ), "Parameter hashes not matching across DP replicas"
+    )
     torch.distributed.barrier()
 
     if pl_has_dist_opt_with_ovelap(trainer):
         for opt in self.optimizers:
             opt.disable_pre_hook()
-
+    return res
 
 class DdpParamParityChecker(Callback, io.IOMixin):
     """
@@ -61,8 +61,10 @@ class DdpParamParityChecker(Callback, io.IOMixin):
 
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx, unused=0) -> None:
         if self.step == self.interval - 1:
-            pl_check_param_hashes_across_dp_replicas(trainer)
-            logging.info(f"DDP Param parity check passed for batch-id= {batch_idx}")
+            if pl_check_param_hashes_across_dp_replicas(trainer):
+                logging.info(f"DDP Param parity check passed for batch-id= {batch_idx}")
+            else:
+                raise RuntimeError(f"DDP Param parity check FAILED for batch-id= {batch_idx}")
         self.step = (self.step + 1) % self.interval
 
     def on_train_end(self, trainer, pl_module) -> None:
