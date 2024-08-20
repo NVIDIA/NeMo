e85b94e9ef77de50287ddeb876cc3ce8184ac5d5
BoxiangW <BoxiangW@users.noreply.github.com>
2024-08-20 21:47:32 +0000

    Apply isort and black reformatting
    Signed-off-by: BoxiangW <BoxiangW@users.noreply.github.com>

--- nemo/collections/llm/recipes/mixtral_8x7b.py
+++ nemo/collections/llm/recipes/mixtral_8x7b.py
@@ -111,6 +111,6 @@ def finetune_recipe(name: str, ckpt_dir: str, num_nodes: int, num_gpus_per_node:
         name=name, ckpt_dir=ckpt_dir, num_nodes=num_nodes, num_gpus_per_node=num_gpus_per_node, fn=finetune
     )
     recipe.resume = hf_resume()
-    recipe.peft = Config(LoRA, target_modules=['linear_qkv', 'linear_proj'], dim = 32)
+    recipe.peft = Config(LoRA, target_modules=['linear_qkv', 'linear_proj'], dim=32)
     recipe.data = Config(SquadDataModule, seq_length=8192, global_batch_size=512, micro_batch_size=1)
     return recipe
