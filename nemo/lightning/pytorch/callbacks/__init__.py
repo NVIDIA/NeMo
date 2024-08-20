cdf1f3bcb36da9950be28685d383d0b0c6d74f86
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-08-19 21:58:21 -0700

    rename to ddp_parity_checker
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/callbacks/__init__.py
+++ nemo/lightning/pytorch/callbacks/__init__.py
@@ -1,4 +1,4 @@
-from nemo.lightning.pytorch.callbacks.ddp_parity_tester import DdpParityChecker
+from nemo.lightning.pytorch.callbacks.ddp_parity_checker import DdpParityChecker
 from nemo.lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from nemo.lightning.pytorch.callbacks.model_transform import ModelTransform
 from nemo.lightning.pytorch.callbacks.nsys import NsysCallback
