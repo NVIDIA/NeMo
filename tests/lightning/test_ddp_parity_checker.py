65aad24b7e8922f13d5d820bdd84f915d4bb50cb
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-08-20 18:51:43 -0700

    another missing import
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/lightning/test_ddp_parity_checker.py
+++ tests/lightning/test_ddp_parity_checker.py
@@ -9,7 +9,7 @@ from nemo import lightning as nl
 from nemo.collections import llm
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
 from nemo.lightning.pytorch.callbacks import DdpParityChecker
-
+from nemo.collections.llm.gpt.data import PreTrainingDataModule
 
 def make_parser():
     parser = argparse.ArgumentParser(description='Train a small GPT model using NeMo 2.0')
