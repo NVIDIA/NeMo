Fix train loss broadcasting (#10212)
* log train loss on all ranks

Signed-off-by: Maanu Grover <maanug@nvidia.com>

* clarity and doc choice

Signed-off-by: Maanu Grover <maanug@nvidia.com>

---------

Signed-off-by: Maanu Grover <maanug@nvidia.com>
Signed-off-by: adityavavre <aditya.vavre@gmail.com>

nemo/lightning/pytorch/strategies.py
@@ -484,12 +484,10 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
                 )
 
             if self.log_train_loss:
-                # p2p now, broadcast later at ckpt
+                # p2p now, broadcast later at ckpt. only with pp, some ranks will log 0.0
+                # WHICH IS OK because we broadcast later at checkpoint time
                 _strategy_lib._sync_from_last_pipeline_stage(out, broadcast=False)
-                if torch.distributed.get_rank() == 0:
-                    self.lightning_module.log(
-                        'reduced_train_loss', out, prog_bar=True, rank_zero_only=True, batch_size=1
-                    )
+                self.lightning_module.log('reduced_train_loss', out, prog_bar=True, batch_size=1, sync_dist=False)
 
             return out
 