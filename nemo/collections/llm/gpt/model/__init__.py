b3561b44bd340d57888cd0c2cf064b273e3c2571
Ao Tang <aot@nvidia.com>
2024-08-26 09:27:41 -0700

    fix import

--- nemo/collections/llm/gpt/model/__init__.py
+++ nemo/collections/llm/gpt/model/__init__.py
@@ -30,6 +30,7 @@ from nemo.collections.llm.gpt.model.llama import (
     LlamaConfig,
     LlamaModel,
 )
+from nemo.collections.llm.gpt.model.mistral import MistralConfig7B, MistralModel
 from nemo.collections.llm.gpt.model.mixtral import (
     MixtralConfig8x3B,
     MixtralConfig8x7B,
