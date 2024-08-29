b9702ce9772a7998f7cb0da687b17dd77b3500d9
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-08-29 15:13:24 -0700

    Add CI tests for validate_dataset_asset_accessibility
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/llm/gpt/data/test_pre_training_data.py
+++ tests/collections/llm/gpt/data/test_pre_training_data.py
@@ -78,3 +78,37 @@ def test_multiple_data_distributions(tokenizer, trainer):
 
     ## this should succeed
     data.setup(stage="dummy")
+
+
+def test_validate_dataset_asset_accessibility_file_does_not_exist(tokenizer, trainer):
+    raised_exception = False
+    try:
+        data = PreTrainingDataModule(
+            paths=["/this/path/should/not/exist/"],
+            seq_length=512,
+            micro_batch_size=2,
+            global_batch_size=2,
+            tokenizer=tokenizer,
+        )
+        data.trainer = trainer
+    except FileNotFoundError:
+        raised_exception = True
+
+    assert raised_exception == True, "Expected to raise a FileNotFoundError"
+
+
+def test_validate_dataset_asset_accessibility_file_is_none(tokenizer, trainer):
+    raised_exception = False
+    try:
+        data = PreTrainingDataModule(
+            paths=None,
+            seq_length=512,
+            micro_batch_size=2,
+            global_batch_size=2,
+            tokenizer=tokenizer,
+        )
+        data.trainer = trainer
+    except ValueError:
+        raised_exception = True
+
+    assert raised_exception == True, "Expected to raise a ValueError"
