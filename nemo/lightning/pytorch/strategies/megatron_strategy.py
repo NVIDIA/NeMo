cda1c55a8280d21c8f2d727fdc07f0f1a763ce20
Hemil Desai <hemild@nvidia.com>
2024-09-04 10:26:11 -0700

    Fix async checkpointing in nemo.lightning (#10324)
    * Fix async checkpointing in nemo.lightning
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    * PR feedback
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    * Add unit tests
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: hemildesai <hemildesai@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    Signed-off-by: hemildesai <hemildesai@users.noreply.github.com>
    Co-authored-by: hemildesai <hemildesai@users.noreply.github.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo/lightning/pytorch/strategies/megatron_strategy.py
+++ nemo/lightning/pytorch/strategies/megatron_strategy.py
@@ -48,8 +48,8 @@ from nemo.lightning.megatron_parallel import CallbackConnector, MegatronParallel
 from nemo.lightning.pytorch.callbacks import ModelTransform
 from nemo.lightning.pytorch.strategies.utils import (
     ckpt_to_dir,
+    create_checkpoint_io,
     fix_progress_bar,
-    get_checkpoint_io,
     init_model_parallel,
     setup_data_sampler,
     setup_parallel_ranks,
@@ -652,17 +652,19 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
     @property
     @override
     def checkpoint_io(self) -> CheckpointIO:
-        return get_checkpoint_io(
-            self._checkpoint_io,
-            save_ckpt_format=self.save_ckpt_format,
-            async_save=self.async_save,
-            torch_dist_multiproc=self.torch_dist_multiproc,
-            assume_constant_structure=self.assume_constant_structure,
-            parallel_save=self.parallel_save,
-            parallel_save_within_dp=self.parallel_save_within_dp,
-            parallel_load=self.parallel_load,
-            load_directly_on_device=self.load_directly_on_device,
-        )
+        if not self._checkpoint_io:
+            self._checkpoint_io = create_checkpoint_io(
+                save_ckpt_format=self.save_ckpt_format,
+                async_save=self.async_save,
+                torch_dist_multiproc=self.torch_dist_multiproc,
+                assume_constant_structure=self.assume_constant_structure,
+                parallel_save=self.parallel_save,
+                parallel_save_within_dp=self.parallel_save_within_dp,
+                parallel_load=self.parallel_load,
+                load_directly_on_device=self.load_directly_on_device,
+            )
+
+        return self._checkpoint_io
 
     @checkpoint_io.setter
     def checkpoint_io(self, io: CheckpointIO) -> None:
