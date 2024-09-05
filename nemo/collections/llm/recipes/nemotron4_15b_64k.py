e992a04b0ba25e0842f969a353a206aa4be05c08
Boxiang Wang <boxiangw@nvidia.com>
2024-09-05 14:35:26 -0700

    Change nemotron recipe style to new CLI
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>

--- nemo/collections/llm/recipes/nemotron4_15b_64k.py
+++ nemo/collections/llm/recipes/nemotron4_15b_64k.py
@@ -1,28 +1,44 @@
-from typing import Callable
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+
+from typing import Optional
+
+import nemo_run as run
+import pytorch_lightning as pl
 import torch
 
-from nemo.collections.llm.api import pretrain
+from nemo.collections.llm.api import finetune, pretrain
 from nemo.collections.llm.gpt.data.mock import MockDataModule
 from nemo.collections.llm.gpt.data.squad import SquadDataModule
 from nemo.collections.llm.recipes import nemotron4_15b
-from nemo.collections.llm.utils import Config, Partial
-from nemo.utils.exp_manager import TimingCallback
 
-NAME = "nemotron4_15b_64k"
+NAME = "nemotron4_15b_16k"
 
 
-def pretrain_recipe(
-    name: str, ckpt_dir: str, num_nodes: int, num_gpus_per_node: int, fn: Callable = pretrain
-) -> Partial:
-    recipe = nemotron4_15b.pretrain_recipe(
-        name=name, ckpt_dir=ckpt_dir, num_nodes=num_nodes, num_gpus_per_node=num_gpus_per_node, fn=fn
-    )
+@run.cli.factory(name=NAME)
+def model() -> run.Config[pl.LightningModule]:
+    model_config = nemotron4_15b.model()
+    model_config.config.seq_length = 16384
+    return model_config
 
-    model = nemotron4_15b.model()
-    model.config.seq_length = 65536
 
-    trainer = nemotron4_15b.trainer(
+def trainer(
+    num_nodes: int = 4,
+    num_gpus_per_node: int = 8,
+) -> run.Config:
+    return nemotron4_15b.trainer(
         tensor_parallelism=4,
         pipeline_parallelism=2,
         pipeline_parallelism_type=torch.bfloat16,
@@ -31,42 +47,40 @@ def pretrain_recipe(
         sequence_parallelism=True,
         num_nodes=num_nodes,
         num_gpus_per_node=num_gpus_per_node,
-        callbacks=[Config(TimingCallback)],
     )
 
-    data = Config(MockDataModule, seq_length=65536, global_batch_size=512, micro_batch_size=1)
 
-    recipe.model = model
-    recipe.trainer = trainer
-    recipe.data = data
+@run.cli.factory(target=pretrain, name=NAME)
+def pretrain_recipe(
+    dir: Optional[str] = None,
+    name: str = "default",
+    num_nodes: int = 4,
+    num_gpus_per_node: int = 8,
+) -> run.Partial:
+    recipe = nemotron4_15b.pretrain_recipe(
+        name=name, dir=dir, num_nodes=num_nodes, num_gpus_per_node=num_gpus_per_node
+    )
+
+    recipe.model = model()
+    recipe.trainer = trainer(num_nodes=num_nodes, num_gpus_per_node=num_gpus_per_node)
+    recipe.data = run.Config(MockDataModule, seq_length=16384, global_batch_size=512, micro_batch_size=1)
 
     return recipe
 
 
-def finetune_recipe(name: str, ckpt_dir: str, num_nodes: int, num_gpus_per_node: int) -> Partial:
+@run.cli.factory(target=finetune, name=NAME)
+def finetune_recipe(
+    dir: Optional[str] = None,
+    name: str = "default",
+    num_nodes: int = 4,
+    num_gpus_per_node: int = 8,
+) -> run.Partial:
     recipe = nemotron4_15b.finetune_recipe(
-        name=name, ckpt_dir=ckpt_dir, num_nodes=num_nodes, num_gpus_per_node=num_gpus_per_node
+        name=name, dir=dir, num_nodes=num_nodes, num_gpus_per_node=num_gpus_per_node
     )
 
-    model = nemotron4_15b.model()
-    model.config.seq_length = 65536
-
-    trainer = nemotron4_15b.trainer(
-        tensor_parallelism=2,
-        pipeline_parallelism=2,
-        pipeline_parallelism_type=torch.bfloat16,
-        virtual_pipeline_parallelism=None,
-        context_parallelism=1,
-        sequence_parallelism=True,
-        num_nodes=num_nodes,
-        num_gpus_per_node=num_gpus_per_node,
-        callbacks=[Config(TimingCallback)],
-    )
-
-    data = Config(SquadDataModule, seq_length=65536, global_batch_size=512, micro_batch_size=1)
-
-    recipe.model = model
-    recipe.trainer = trainer
-    recipe.data = data
+    recipe.model = model()
+    recipe.trainer = trainer(num_nodes=num_nodes, num_gpus_per_node=num_gpus_per_node)
+    recipe.data = run.Config(SquadDataModule, seq_length=16384, global_batch_size=512, micro_batch_size=1)
 
     return recipe
