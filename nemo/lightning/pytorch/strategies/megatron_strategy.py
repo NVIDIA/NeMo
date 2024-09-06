00114ea3ebf376b134121d5638051f14b67a3d2d
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-09-06 12:59:56 -0700

    Add dummy optimizer_states key in checkpoint to bypass PTL validation.
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/strategies/megatron_strategy.py
+++ nemo/lightning/pytorch/strategies/megatron_strategy.py
@@ -634,7 +634,8 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
             and self.trainer.state.fn == TrainerFn.FITTING
             and self.ckpt_save_optimizer
         ):
-            del checkpoint["optimizer_states"]
+            # Add dummy optimizer states to bypass PTL validation.
+            checkpoint["optimizer_states"] = {}
             checkpoint["optimizer"] = [self.optimizer_sharded_state_dict()]
 
         self.checkpoint_io.save_checkpoint(checkpoint, filepath, storage_options=storage_options)
