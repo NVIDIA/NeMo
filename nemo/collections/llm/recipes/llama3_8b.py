6a5228822c482a3fe54db77fd67901aaa9b6410f
marcromeyn <marcromeyn@users.noreply.github.com>
2024-09-06 07:46:40 +0000

    Apply isort and black reformatting
    Signed-off-by: marcromeyn <marcromeyn@users.noreply.github.com>

--- nemo/collections/llm/recipes/llama3_8b.py
+++ nemo/collections/llm/recipes/llama3_8b.py
@@ -28,9 +28,8 @@ from nemo.collections.llm.gpt.model.llama import Llama3Config8B, LlamaModel
 from nemo.collections.llm.peft.lora import LoRA
 from nemo.collections.llm.recipes.log.default import default_log, default_resume, tensorboard_logger
 from nemo.collections.llm.recipes.optim.adam import distributed_fused_adam_with_cosine_annealing
-from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed_plugin
+from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed, bf16_mixed_plugin
 from nemo.lightning.pytorch.callbacks.megatron_comm_overlap import MegatronCommOverlapCallback
-from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed
 from nemo.utils.exp_manager import TimingCallback
 
 NAME = "llama3_8b"
