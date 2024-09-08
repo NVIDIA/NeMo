7b280c6bf45c77b75e9b702c95fb4f06e8cb4afb
ashors1 <ashors@nvidia.com>
2024-09-08 11:03:15 -0700

    update resume_from_path and context_path
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- nemo/lightning/resume.py
+++ nemo/lightning/resume.py
@@ -244,15 +244,15 @@ class AutoResume:
             checkpoint = self._find_trainer_ckpt_path()
 
         if checkpoint:
-            maybe_model_weights_path = Path(checkpoint) / "context"
-            if os.path.isdir(maybe_model_weights_path):
-                checkpoint = maybe_model_weights_path
+            maybe_context_path = Path(checkpoint) / "context"
+            if maybe_context_path.is_dir():
+                checkpoint = maybe_context_path
         return checkpoint
 
     def get_trainer_ckpt_path(self, model: Optional[io.ConnectorMixin] = None) -> Optional[Path]:
         if self.resume_from_path:
-            maybe_model_weights_path = self.get_model_weights_path(self.resume_from_path)
-            return maybe_model_weights_path if os.path.isdir(maybe_model_weights_path) else self.resume_from_path
+            maybe_weights_path = self.get_weights_path(self.resume_from_path)
+            return maybe_weights_path if os.path.isdir(maybe_weights_path) else self.resume_from_path
 
         checkpoint = None
         app_state = AppState()
