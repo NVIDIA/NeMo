faa2519028e772c26e50db85684937ae472bf5f5
oliver könig <okoenig@nvidia.com>
2024-09-11 12:07:40 -0700

    Bump `Dockerfile.ci` (2024-09-09) (#10423)
    * [🤠]: Howdy folks, let's bump `Dockerfile.ci` to 8307fcd !
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * update TE import paths
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    
    * Update parallelisms.rst
    
    fix sed typo.
    
    Signed-off-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
    
    * fix for mcore dist opt refactor: move overlap_grad_reduce/overlap_param_gather to ddp config
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    
    * remove overlap_grad_reduce overlap_param_gather from autoconfig
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * subclass TransformerConfig because megatronmodule expects it to have fp8 attr
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    
    * fix
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * revert change; Use ModelParallelConfig & add fp8
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * fix, set NVTE_APPLY_QK_LAYER_SCALIN=1
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    Signed-off-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
    Co-authored-by: pablo-garay <7166088+pablo-garay@users.noreply.github.com>
    Co-authored-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Co-authored-by: akoumpa <akoumpa@users.noreply.github.com>
    Co-authored-by: Pablo Garay <palenq@gmail.com>
    Co-authored-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>

--- nemo/collections/nlp/models/language_modeling/megatron/gpt_layer_modelopt_spec.py
+++ nemo/collections/nlp/models/language_modeling/megatron/gpt_layer_modelopt_spec.py
@@ -17,6 +17,7 @@ try:
     from megatron.core.fusions.fused_bias_dropout import get_bias_dropout_add
     from megatron.core.tensor_parallel.layers import ColumnParallelLinear, RowParallelLinear
     from megatron.core.transformer.attention import SelfAttention, SelfAttentionSubmodules
+    from megatron.core.transformer.dot_product_attention import DotProductAttention
     from megatron.core.transformer.enums import AttnMaskType
     from megatron.core.transformer.identity_op import IdentityOp
     from megatron.core.transformer.mlp import MLP, MLPSubmodules
