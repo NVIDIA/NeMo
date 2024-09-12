efcc01623df3d772e356b1c9da9b8b9b979bc745
ashors1 <ashors@nvidia.com>
2024-09-11 17:00:29 -0700

    fix lora without TE
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- nemo/collections/llm/peft/lora.py
+++ nemo/collections/llm/peft/lora.py
@@ -122,17 +122,17 @@ class LoRA(PEFT):
 
         tp_size = parallel_state.get_tensor_model_parallel_world_size()
         if name in self.target_modules:
-            # m.in_features and m.out_features are divided by tp_size already,
-            # but in_features and out_features passed to ParallelLinearAdapter are not.
             if name in ['linear_qkv', 'linear_fc1']:
                 # Column Parallel Linear
                 input_is_parallel = False
                 if HAVE_TE:
+                    # m.in_features and m.out_features are divided by tp_size already,
+                    # but in_features and out_features passed to ParallelLinearAdapter are not.
                     in_features = m.in_features
                     out_features = m.out_features * tp_size
                 else:
                     in_features = m.input_size
-                    out_features = m.output_size * tp_size
+                    out_features = m.output_size
                 # LoRA is applied after layernorm, so layernorm output must be returned
                 m.return_layernorm_output = True
                 # perf optimization for LoRA + SP
@@ -145,7 +145,7 @@ class LoRA(PEFT):
                     in_features = m.in_features * tp_size
                     out_features = m.out_features
                 else:
-                    in_features = m.input_size * tp_size
+                    in_features = m.input_size
                     out_features = m.output_size
 
             logging.info(f"Adding lora to: {prefix}.{name}")
