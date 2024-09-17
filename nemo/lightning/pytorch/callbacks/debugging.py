ae6d4f89053ece54c7909ca430bd553ddc0feb83
Maanu Grover <maanug@nvidia.com>
2024-09-17 16:57:04 -0700

    support running on any ptl hook
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- nemo/lightning/pytorch/callbacks/debugging.py
+++ nemo/lightning/pytorch/callbacks/debugging.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, List, Optional, Union
 
 import torch
 from prettytable import PrettyTable
@@ -21,11 +21,60 @@ class ParameterDebugger(Callback):
         self,
         param_attr_fn: Optional[Callable[[torch.Tensor], Dict[str, str]]] = collect_precision_and_shape,
         grad_attr_fn: Optional[Callable[[torch.Tensor], Dict[str, str]]] = collect_precision,
+        log_on_hooks: Union[List[str], str] = "on_train_start",
     ):
         self.param_attr_fn = param_attr_fn
         self.grad_attr_fn = grad_attr_fn
 
-    def on_train_start(self, trainer, pl_module):
+        valid_hooks = set(
+            [
+                "teardown",
+                "on_fit_end",
+                "on_sanity_check_start",
+                "on_sanity_check_end",
+                "on_train_batch_start",
+                "on_train_batch_end",
+                "on_train_epoch_start",
+                "on_train_epoch_end",
+                "on_validation_epoch_start",
+                "on_validation_epoch_end",
+                "on_test_epoch_start",
+                "on_test_epoch_end",
+                "on_predict_epoch_start",
+                "on_predict_epoch_end",
+                "on_validation_batch_start",
+                "on_validation_batch_end",
+                "on_test_batch_start",
+                "on_test_batch_end",
+                "on_predict_batch_start",
+                "on_predict_batch_end",
+                "on_train_start",
+                "on_train_end",
+                "on_validation_start",
+                "on_validation_end",
+                "on_test_start",
+                "on_test_end",
+                "on_predict_start",
+                "on_predict_end",
+                "on_exception",
+                "on_save_checkpoint",
+                "on_load_checkpoint",
+                "on_before_backward",
+                "on_after_backward",
+                "on_before_optimizer_step",
+                "on_before_zero_grad",
+            ]
+        )
+
+        if isinstance(log_on_hooks, str):
+            log_on_hooks = [log_on_hooks]
+        for hook_name in log_on_hooks:
+            assert (
+                hook_name in valid_hooks
+            ), f"Hook {hook_name} supplied to log_on_hooks is not valid or can not be used. Valid hooks are {valid_hooks}"
+            setattr(self, hook_name, self._log_param_and_grad_attrs)
+
+    def _log_param_and_grad_attrs(self, trainer, pl_module):
 
         def find_grad_tensor(param):
             """If using MCore optimizer, search the grad buckets for param's grad tensor."""
@@ -36,9 +85,9 @@ class ParameterDebugger(Callback):
                 if param in buf.param_to_bucket:
                     return buf.param_to_bucket[param].grad_data
 
+        # create table and get table column headers
         debug_table = PrettyTable()
         debug_table.align = "l"
-
         param_keys = self.param_attr_fn(torch.zeros(0)).keys() if self.param_attr_fn else []
         grad_keys = self.grad_attr_fn(torch.zeros(0)).keys() if self.grad_attr_fn else []
         debug_table.field_names = ["Parameter"] + ["Param " + k for k in param_keys] + ["Grad " + k for k in grad_keys]
