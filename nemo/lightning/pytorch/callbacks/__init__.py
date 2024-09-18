3f55f0bdace4b26dcc62f94674bad7519b635455
maanug-nv <maanug-nv@users.noreply.github.com>
2024-09-18 00:14:13 +0000

    Apply isort and black reformatting
    Signed-off-by: maanug-nv <maanug-nv@users.noreply.github.com>

--- nemo/lightning/pytorch/callbacks/__init__.py
+++ nemo/lightning/pytorch/callbacks/__init__.py
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 from nemo.lightning.pytorch.callbacks.ddp_parity_checker import DdpParityChecker
+from nemo.lightning.pytorch.callbacks.debugging import ParameterDebugger
 from nemo.lightning.pytorch.callbacks.garbage_collection import GarbageCollectionCallback
 from nemo.lightning.pytorch.callbacks.memory_profiler import MemoryProfileCallback
-from nemo.lightning.pytorch.callbacks.debugging import ParameterDebugger
 from nemo.lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from nemo.lightning.pytorch.callbacks.model_transform import ModelTransform
 from nemo.lightning.pytorch.callbacks.nsys import NsysCallback
