db5d12abf12b88ace8f07629cdc6811f706df2dc
artbataev <artbataev@users.noreply.github.com>
2024-09-18 16:43:29 +0000

    Apply isort and black reformatting
    Signed-off-by: artbataev <artbataev@users.noreply.github.com>
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/llm/recipes/mixtral_8x3b.py
+++ nemo/collections/llm/recipes/mixtral_8x3b.py
@@ -13,9 +13,8 @@ from nemo.collections.llm.gpt.model.mixtral import MixtralConfig8x3B, MixtralMod
 from nemo.collections.llm.peft.lora import LoRA
 from nemo.collections.llm.recipes.log.default import default_log, default_resume, tensorboard_logger
 from nemo.collections.llm.recipes.optim.adam import distributed_fused_adam_with_cosine_annealing
-from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed_plugin
+from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed, bf16_mixed_plugin
 from nemo.collections.llm.utils import Config, Partial
-from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed
 from nemo.lightning.pytorch.callbacks.megatron_comm_overlap import MegatronCommOverlapCallback
 from nemo.lightning.pytorch.callbacks.moe_token_drop import MegatronTokenDropCallback
 from nemo.utils.exp_manager import TimingCallback
