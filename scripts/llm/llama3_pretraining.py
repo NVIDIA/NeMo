80d71adb5a3005992e9fc23f20a050cef65dbf03
Hemil Desai <hemild@nvidia.com>
2024-09-19 12:24:12 -0700

    Add copyright headers to nemo llm examples
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- scripts/llm/llama3_pretraining.py
+++ scripts/llm/llama3_pretraining.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# NOTE: This script is only an example of using NeMo with NeMo-Run's APIs and is subject to change without notice.
 # This script is used for pretraining a Llama3 model, specifically for the 8b or 70b model variants, on local and slurm executors.
 # It uses NeMo 2.0 recipes (https://github.com/NVIDIA/NeMo/blob/main/nemo/collections/llm/recipes/llama3_8b.py#L74) and NeMo-Run (https://github.com/NVIDIA/NeMo-Run) to configure and execute the runs.
 
