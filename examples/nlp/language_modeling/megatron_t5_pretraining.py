542b26dfe227d92d4af3a456704e0d481da4ad27
Huy Vu <86480512+huvunvidia@users.noreply.github.com>
2024-09-22 08:50:13 -0700

    Update megatron_t5_pretraining.py
    Signed-off-by: Huy Vu <86480512+huvunvidia@users.noreply.github.com>

--- examples/nlp/language_modeling/megatron_t5_pretraining.py
+++ examples/nlp/language_modeling/megatron_t5_pretraining.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import torch._dynamo
 from omegaconf.omegaconf import OmegaConf, open_dict
 
 from nemo.collections.nlp.models.language_modeling.megatron_t5_model import MegatronT5Model
@@ -21,6 +21,7 @@ from nemo.core.config import hydra_runner
 from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
 
+torch._dynamo.config.suppress_errors = True
 
 @hydra_runner(config_path="conf", config_name="megatron_t5_config")
 def main(cfg) -> None:
