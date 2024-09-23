6e5ee34d9c04c4cc7df6339c703978b45f0e14ec
leejinho610 <43155972+leejinho610@users.noreply.github.com>
2024-09-23 10:06:04 +0900

    Update convert_llava_nemo_to_hf.py
    Signed-off-by: leejinho610 <43155972+leejinho610@users.noreply.github.com>

--- scripts/checkpoint_converters/convert_llava_nemo_to_hf.py
+++ scripts/checkpoint_converters/convert_llava_nemo_to_hf.py
@@ -283,7 +283,7 @@ def convert(args):
         os.path.join(os.path.dirname(__file__), '../../examples/multimodal/multimodal_llm/neva/conf/llava_config.yaml')
     )
     trainer = MegatronTrainerBuilder(nemo_config).create_trainer()
-    model_config = model_config = MegatronNevaModel.restore_from(
+    model_config = MegatronNevaModel.restore_from(
         restore_path=args.input_name_or_path, trainer=trainer, return_config=True
     )
     model_config.tensor_model_parallel_size = 1
@@ -299,6 +299,7 @@ def convert(args):
         restore_path=args.input_name_or_path,
         trainer=trainer,
         override_config_path=model_config,
+        map_location=map_location,
         save_restore_connector=NLPSaveRestoreConnector(),
     )
 
