a6bcb839d3affa9fded086b505f52eb884fd437f
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-09-24 00:37:18 -0700

    import fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/nlp/modules/common/tokenizer_utils.py
+++ nemo/collections/nlp/modules/common/tokenizer_utils.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from .lm_utils import get_pretrained_lm_models_list, get_huggingface_pretrained_lm_models_list
 
 import os.path
 from dataclasses import MISSING, dataclass
