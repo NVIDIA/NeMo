5387b098d751f319bd7cde50e130fbfe24dff1bc
Piotr Żelasko <pzelasko@nvidia.com>
2024-09-26 07:17:17 -0700

    cleanup
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>

--- tests/collections/multimodal/test_speechllm_dataset.py
+++ tests/collections/multimodal/test_speechllm_dataset.py
@@ -10,39 +10,31 @@ from nemo.collections.multimodal.speech_llm.data.lhotse_dataset import LhotseAud
 from nemo.collections.multimodal.speech_llm.parts.utils.data_utils import TextProcessing
 
 
-# @pytest.fixture
-# def tokenizer():
-#     return SentencePieceTokenizer(model_path="tests/.data/tokenizer.model")
-
-# Note: We don't really define special tokens for this test so every 'special token'
-#       will be represented as a number of regular tokens.
-TOKENIZER_TRAIN_TEXT = """
-Example system message.
-Example user message.
-Example assistant message.
-TEST
-[INST]
-[/INST]
-<s>
-</s>
-<<SYS>>
-<</SYS>>
-User: Assistant:
-user model
-Instruct Output 
-\n\n
-<start_of_turn> <end_of_turn>
-<|
-|>
-<|en|> <|de|> <|fr|> <|es|> <|transcribe|> <|translate|> <|pnc|> <|nopnc|> <|startoftranscript|> <|endoftext|>
-Feel free to add new tokens for your own tests!?
-But know that if you do so, you may need to update the token IDs in the existing tests! 
-So, it might be a good idea to create a new tokenizer instead when adding new prompt formats.
-"""
-
-
 @pytest.fixture
 def tokenizer(capsys, tmp_path_factory):
+    TOKENIZER_TRAIN_TEXT = """
+    Example system message.
+    Example user message.
+    Example assistant message.
+    TEST
+    [INST]
+    [/INST]
+    <s>
+    </s>
+    <<SYS>>
+    <</SYS>>
+    User: Assistant:
+    user model
+    Instruct Output 
+    \n\n
+    <start_of_turn> <end_of_turn>
+    <|
+    |>
+    <|en|> <|de|> <|fr|> <|es|> <|transcribe|> <|translate|> <|pnc|> <|nopnc|> <|startoftranscript|> <|endoftext|>
+    Feel free to add new tokens for your own tests!?
+    But know that if you do so, you may need to update the token IDs in the existing tests! 
+    So, it might be a good idea to create a new tokenizer instead when adding new prompt formats.
+    """
     tmpdir = tmp_path_factory.mktemp("bpe_tokenizer")
     text_path = tmpdir / "text.txt"
     text_path.write_text(TOKENIZER_TRAIN_TEXT)
