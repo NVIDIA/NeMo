24a88bb6bcf955e76249107bf22643988cc0e72a
Piotr Żelasko <pzelasko@nvidia.com>
2024-09-27 14:04:59 -0700

    Fix tokenize_with_prompt
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>

--- nemo/collections/common/data/lhotse/dataloader.py
+++ nemo/collections/common/data/lhotse/dataloader.py
@@ -730,12 +730,18 @@ def tokenize_with_prompt(example: Example, tokenizer, prompt_format: str) -> Exa
     #   We intend to extend it for text modality in follow-up work.
     if isinstance(example, Cut):
         prompt_format_fn = get_prompt_format_fn(prompt_format)
-        (tokenized_prompted_transcript,), (tokenized_prompt,), (tokenized_transcript,) = prompt_format_fn(
-            CutSet([example]), tokenizer
-        )
-        example.tokenized_prompted_transcript = tokenized_prompted_transcript
-        example.tokenized_prompt = tokenized_prompt
-        example.tokenized_transcript = tokenized_transcript
+        ans = prompt_format_fn(CutSet([example]), tokenizer)
+        if isinstance(ans, tuple):
+            (tokenized_prompted_transcript,), (tokenized_prompt,), (tokenized_transcript,) = ans
+            example.tokenized_prompted_transcript = tokenized_prompted_transcript
+            example.tokenized_prompt = tokenized_prompt
+            example.tokenized_transcript = tokenized_transcript
+        elif isinstance(ans, dict):
+            example.tokenized_prompted_transcript = ans["input_ids"][0]
+            example.tokenized_prompt = ans["context_ids"][0]
+            example.tokenized_transcript = ans["answer_ids"][0]
+        else:
+            raise RuntimeError(f"Unexpected return type from prompt_format_fn (must be dict or tuple): {ans}")
     else:
         raise RuntimeError(f"Currently we only support tokenization + prompting during sampling for audio modality.")
     return example
