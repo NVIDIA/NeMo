7fd5f2059a455c004d6f3fc16bb1880d2a06ca26
thomasdhc <thomasdhc@users.noreply.github.com>
2024-09-27 14:08:31 +0000

    Apply isort and black reformatting
    Signed-off-by: thomasdhc <thomasdhc@users.noreply.github.com>

--- nemo/collections/nlp/models/language_modeling/megatron/gpt_full_te_layer_autocast_spec.py
+++ nemo/collections/nlp/models/language_modeling/megatron/gpt_full_te_layer_autocast_spec.py
@@ -15,8 +15,8 @@
 from importlib.metadata import version
 from typing import Any, Callable, Optional
 
-import torch
 import packaging
+import torch
 
 from nemo.collections.nlp.modules.common.megatron.utils import ApexGuardDefaults
 from nemo.collections.nlp.parts import utils_funcs
