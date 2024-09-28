fc0581edab8e3ca36ea7f8941237354168593c79
zhehuaichen <dian.chenzhehuai@gmail.com>
2024-09-27 17:49:24 -0700

    support gemma
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>

--- nemo/collections/common/prompts/gemma.py
+++ nemo/collections/common/prompts/gemma.py
@@ -2,7 +2,14 @@
 Implemented following the guide at https://www.promptingguide.ai/models/gemma#gemma-7b-prompt-format
 """
 
+from collections import defaultdict
+
+from lhotse import CutSet
+from lhotse.cut import MixedCut
+
+from nemo.collections.common.prompts import registered_prompt_format_fn
 from nemo.collections.common.prompts.formatter import Modality, PromptFormatter
+from nemo.collections.common.tokenizers import TokenizerSpec
 
 GEMMA_BOS = "<start_of_turn>"
 GEMMA_END_OF_TURN = "<end_of_turn>"
@@ -27,3 +34,31 @@ class GemmaPromptFormatter(PromptFormatter):
             },
         },
     }
+
+
+@registered_prompt_format_fn
+def gemma1(cuts: CutSet, tokenizer: TokenizerSpec):
+    prompt = GemmaPromptFormatter(tokenizer)
+    ans = defaultdict(list)
+    for cut in cuts:
+        if isinstance(cut, MixedCut):
+            cut = cut.first_non_padding_cut
+        if cut.has_custom("context"):
+            context = cut.context
+        elif cut.has_custom("question"):
+            context = cut.question
+        else:
+            context = cut.default_context
+
+        turns = []
+        if cut.has_custom("system_prompt"):
+            turns.append({"role": "system_and_user", "slots": {"system": cut.system_prompt, "message": context}})
+        else:
+            turns.append({"role": "user", "slots": {"message": context}})
+        if (answer := cut.supervisions[0].text) is not None:
+            turns.append({"role": "assistant", "slots": {"message": answer}})
+
+        for k, v in prompt.encode_dialog(turns).items():
+            ans[k].append(v)
+
+    return ans
