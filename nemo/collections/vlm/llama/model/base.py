428403e9a8c2583c7c8760dfc202fdff7c60d64f
yaoyu-33 <yaoyu.094@gmail.com>
2024-09-30 14:13:39 -0700

    rename back to language.py
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- nemo/collections/vlm/llama/model/base.py
+++ nemo/collections/vlm/llama/model/base.py
@@ -33,7 +33,7 @@ from megatron.core.enums import ModelType
 from megatron.core.transformer.transformer_config import TransformerConfig
 from torch import nn, Tensor
 
-from nemo.collections.vlm.llama.model.text import CrossAttentionTextModel, CrossAttentionTransformerLayer
+from nemo.collections.vlm.llama.model.language import CrossAttentionTextModel, CrossAttentionTransformerLayer
 from megatron.core.transformer.spec_utils import ModuleSpec
 from nemo.lightning import get_vocab_size, MegatronStrategy, Trainer
 from nemo.collections.llm.gpt.model.llama import Llama31Config, apply_rope_scaling
