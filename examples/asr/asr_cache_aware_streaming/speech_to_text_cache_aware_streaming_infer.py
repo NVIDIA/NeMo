02df5eda639289fa1caa282ae3248a183e5b1f1d
Vladimir Bataev <vbataev@nvidia.com>
2024-10-01 20:08:18 +0400

    Add todo
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- examples/asr/asr_cache_aware_streaming/speech_to_text_cache_aware_streaming_infer.py
+++ examples/asr/asr_cache_aware_streaming/speech_to_text_cache_aware_streaming_infer.py
@@ -322,7 +322,7 @@ def main():
         decoding_cfg.preserve_alignments = False
         if hasattr(asr_model, 'joint'):  # if an RNNT model
             decoding_cfg.greedy.max_symbols = 10
-            decoding_cfg.greedy.use_cuda_graph_decoder = False
+            decoding_cfg.greedy.use_cuda_graph_decoder = False  # TODO: remove
             decoding_cfg.fused_batch_size = -1
         asr_model.change_decoding_strategy(decoding_cfg)
 
