caa3b276fa414d9c38dc9a653d3828a4a3878d01
Chen Cui <chcui@nvidia.com>
2024-10-01 20:12:19 -0400

    remove interleaved

--- nemo/collections/vlm/llama/model/base.py
+++ nemo/collections/vlm/llama/model/base.py
@@ -130,7 +130,6 @@ class CrossAttentionTextModelConfig(Llama31Config):
     num_cross_attention_layers: int = 8
     vocab_size: int = 128256
     apply_rope_fusion: bool = False
-    rotary_interleaved: bool = True
 
     def _init_fusion_schedule(self, num_layers: int) -> List[int]:
         llama_layers = list(range(self.num_layers))
