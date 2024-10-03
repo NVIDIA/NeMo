bd562ce8165f599c95012ef215bc834c658ba385
Chen Cui <chcui@nvidia.com>
2024-10-03 16:49:24 -0400

    fix predefined config classes

--- nemo/collections/vlm/llama/model/mllama.py
+++ nemo/collections/vlm/llama/model/mllama.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import re
+from dataclasses import field, dataclass
 from pathlib import Path
 from typing import Dict, Optional
 
@@ -26,24 +27,26 @@ from nemo.lightning import MegatronStrategy, Trainer
 from nemo.lightning import io, teardown
 from nemo.lightning.pytorch.utils import dtype_from_hf
 
-
+@dataclass
 class MLlamaConfig11B(MLlamaModelConfig):
-    language_model_config: Optional[TransformerConfig] = CrossAttentionTextConfig(),
-    vision_model_config: Optional[TransformerConfig] = CrossAttentionVisionConfig(vision_chunk_size=448)
+    language_model_config: Optional[TransformerConfig] = field(default_factory=lambda: CrossAttentionTextConfig())
+    vision_model_config: Optional[TransformerConfig] = field(default_factory=lambda: CrossAttentionVisionConfig(vision_chunk_size=448))
 
 
+@dataclass
 class MLlamaConfig11BInstruct(MLlamaModelConfig):
-    language_model_config: Optional[TransformerConfig] = CrossAttentionTextConfig(),
-    vision_model_config: Optional[TransformerConfig] = CrossAttentionVisionConfig(vision_chunk_size=560)
+    language_model_config: Optional[TransformerConfig] = field(default_factory=lambda: CrossAttentionTextConfig())
+    vision_model_config: Optional[TransformerConfig] = field(default_factory=lambda: CrossAttentionVisionConfig(vision_chunk_size=560))
 
-
+@dataclass
 class MLlamaConfig90B(MLlamaModelConfig):
-    language_model_config: Optional[TransformerConfig] = CrossAttentionTextConfig(
+    language_model_config: Optional[TransformerConfig] = field(default_factory=lambda: CrossAttentionTextConfig(
         hidden_size=8192, ffn_hidden_size=28672, num_attention_heads=64, num_layers=80,
-    ),
-    vision_model_config: Optional[TransformerConfig] = CrossAttentionVisionConfig(vision_chunk_size=560)
+    ))
+    vision_model_config: Optional[TransformerConfig] = field(default_factory=lambda: CrossAttentionVisionConfig(vision_chunk_size=560))
 
 
+@dataclass
 class MLlamaConfig90BInstruct(MLlamaConfig90B):
     pass
 
