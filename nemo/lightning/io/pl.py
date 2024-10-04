9a6f8bae47cfa591aad1f56192440f7d91e89b92
akoumpa <akoumpa@users.noreply.github.com>
2024-10-04 20:59:36 +0000

    Apply isort and black reformatting
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>

--- nemo/lightning/io/pl.py
+++ nemo/lightning/io/pl.py
@@ -23,9 +23,9 @@ from megatron.core.parallel_state import get_data_parallel_group
 from torch import nn
 from typing_extensions import Self, override
 
+from nemo.lightning.ckpt_utils import ckpt_to_dir
 from nemo.lightning.io.capture import IOProtocol
 from nemo.lightning.io.mixin import IOMixin
-from nemo.lightning.ckpt_utils import ckpt_to_dir
 
 try:
     from nemo.utils.callbacks.dist_ckpt_io import AsyncCompatibleCheckpointIO
