ae3c5bef5acf5f18223cd716e238c308628f55f5
stevehuang52 <heh@nvidia.com>
2024-10-08 14:43:12 -0400

    update datamodule
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- nemo/collections/slm/data/audio_to_text_data.py
+++ nemo/collections/slm/data/audio_to_text_data.py
@@ -39,11 +39,12 @@ from nemo.collections.nlp.data.language_modeling.megatron.blendable_dataset impo
 from nemo.collections.nlp.data.language_modeling.megatron.megatron_batch_samplers import (
     MegatronPretrainingBatchSampler,
 )
+from nemo.lightning.io.mixin import IOMixin
 from nemo.lightning.pytorch.plugins import MegatronDataSampler
 from nemo.utils import logging, model_utils
 
 
-class AudioToTextDataModule(pl.LightningDataModule):
+class AudioToTextDataModule(pl.LightningDataModule, IOMixin):
     def __init__(self, config: Union[DictConfig, Dict], tokenizer: TokenizerSpec):
         super().__init__()
         self.cfg = OmegaConf.create(config) if not isinstance(config, DictConfig) else config
@@ -314,36 +315,90 @@ class AudioToTextDataModule(pl.LightningDataModule):
             else:
                 return self._create_nemo_dataloader(self._test_ds, 'predict')
 
-    # def state_dict(self) -> Dict[str, Any]:
-    #     """Called when saving a checkpoint, implement to generate and save datamodule state.
+    def state_dict(self) -> Dict[str, Any]:
+        """Called when saving a checkpoint, implement to generate and save datamodule state.
 
-    #     Returns:
-    #         A dictionary containing datamodule state.
+        Returns:
+            A dictionary containing datamodule state.
 
-    #     """
-    #     consumed_samples = self.data_sampler.compute_consumed_samples(self.trainer.global_step - self.init_global_step)
-    #     return {'consumed_samples': consumed_samples}
+        """
+        consumed_samples = self.data_sampler.compute_consumed_samples(self.trainer.global_step - self.init_global_step)
+        return {'consumed_samples': consumed_samples}
 
-    # def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
-    #     """Called when loading a checkpoint, implement to reload datamodule state given datamodule stat
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        """Called when loading a checkpoint, implement to reload datamodule state given datamodule stat
 
-    #     Args:
-    #         state_dict: the datamodule state returned by ``state_dict``.
+        Args:
+            state_dict: the datamodule state returned by ``state_dict``.
 
-    #     """
-    #     try:
-    #         from apex.transformer.pipeline_parallel.utils import _GLOBAL_NUM_MICROBATCHES_CALCULATOR
-    #     except ModuleNotFoundError:
-    #         from nemo.lightning.apex_utils import _GLOBAL_NUM_MICROBATCHES_CALCULATOR
-    #     consumed_samples = state_dict['consumed_samples']
-    #     self.data_sampler.init_consumed_samples = consumed_samples
-    #     self.data_sampler.prev_consumed_samples = consumed_samples
-    #     self.if_first_step = 1
+        """
+        try:
+            from megatron.core.num_microbatches_calculator import update_num_microbatches
 
-    #     if _GLOBAL_NUM_MICROBATCHES_CALCULATOR is not None:
-    #         num_microbatch_calculator = _GLOBAL_NUM_MICROBATCHES_CALCULATOR  # noqa: SLF001
+        except (ImportError, ModuleNotFoundError):
+            logging.warning("Megatron num_microbatches_calculator not found, using Apex version.")
+            from apex.transformer.pipeline_parallel.utils import update_num_microbatches
 
-    #         num_microbatch_calculator.update(
-    #             consumed_samples=consumed_samples,
-    #             consistency_check=False,
-    #         )
+        consumed_samples = state_dict['consumed_samples']
+        self.data_sampler.init_consumed_samples = consumed_samples
+        self.data_sampler.prev_consumed_samples = consumed_samples
+
+        update_num_microbatches(
+            consumed_samples=consumed_samples,
+            consistency_check=False,
+        )
+        self.data_sampler.if_first_step = 1
+
+    def reconfigure_limit_batches(self):
+        # Override limit_train_batches in terms of num of microbatches
+        self._reconfigure_limit_batches(self.trainer.limit_train_batches, self._train_ds, 'train')
+        # Override limit_val_batches to be a multiple of num microbatches to prevent val_step from exiting in between a step
+        self._reconfigure_limit_batches(self.trainer.limit_val_batches, self._validation_ds, 'val')
+
+    def _reconfigure_limit_batches(self, limit_batches, dataloader, mode):
+        """
+        Reconfigure trainer.limit_val_batches for pretraining
+        """
+        # Override limit_batches in terms of num microbatches and so there are limit_batches//num_micro_batches num of global batches
+        try:
+            from megatron.core.num_microbatches_calculator import get_num_microbatches
+
+        except (ImportError, ModuleNotFoundError):
+            logging.warning("Megatron num_microbatches_calculator not found, using Apex version.")
+            from apex.transformer.pipeline_parallel.utils import get_num_microbatches
+
+        if isinstance(limit_batches, int):
+            limit_batches *= get_num_microbatches()
+        else:
+            assert isinstance(limit_batches, float)
+            # Don't reconfigure if limit_batches is 0.0 or if there's no dataloader
+            if limit_batches == 0.0 or dataloader is None:
+                return
+            # len(dataloader) returns len as num of microbatches
+            dl_len_in_micro_batches = len(dataloader)
+            if len(dataloader) != float("inf"):
+                if limit_batches == 1.0:
+                    limit_batches = dl_len_in_micro_batches
+                else:
+                    limit_micro_batches = int(dl_len_in_micro_batches * limit_batches)
+                    if limit_micro_batches == 0 and limit_batches > 0.0:
+                        min_percentage = 1.0 / len(dataloader)
+                        raise RuntimeError(
+                            f"You requested to check {limit_batches} of the val_dataloader but"
+                            f" {limit_batches} * {len(dataloader)} < 1. Please increase the"
+                            f" `limit_val_batches` argument. Try at least"
+                            f" `limit_val_batches={min_percentage}`"
+                        )
+                    # Make sure trainer.limit_val_batches is a multiple of num of microbatches
+                    if limit_micro_batches < get_num_microbatches():
+                        limit_batches = get_num_microbatches()
+                    else:
+                        limit_batches = limit_batches - limit_batches % get_num_microbatches()
+
+        if mode == 'train':
+            self.trainer.limit_train_batches = limit_batches
+        else:
+            self.trainer.limit_val_batches = limit_batches
+
+        # Override num sanity steps to be a multiple of num of microbatches
+        self.trainer.num_sanity_val_steps *= get_num_microbatches()
