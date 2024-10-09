ce35c11d77b3cc63c0fd9d28b20d6b6b3f7de33e
akoumpa <akoumpa@users.noreply.github.com>
2024-10-09 21:19:36 +0000

    Apply isort and black reformatting
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>

--- nemo/collections/llm/api.py
+++ nemo/collections/llm/api.py
@@ -40,12 +40,16 @@ def cli_entrypoint(*args0, **kwargs0):
         def wrapper(*args, **kwargs):
             try:
                 import nemo_run as run
+
                 return run.cli.entrypoint(*args0, **kwargs0)(func, *args, **kwargs)
             except (ImportError, ModuleNotFoundError) as error:
                 logging.warning(f"Failed to import nemo.collections.llm.[api,recipes]: {error}")
                 return func(*args, **kwargs)
+
         return wrapper
+
     return decorator
+
 
 @cli_entrypoint(namespace="llm")
 def train(
