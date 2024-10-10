c16cdd2d303fc67eeb5690f6eaeda34f2a8505dc
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-10 10:59:18 -0700

    path fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/llm/bitexact/mixtral/run.sh
+++ tests/collections/llm/bitexact/mixtral/run.sh
@@ -42,4 +42,5 @@ python3 /workspace/tests/collections/llm/bitexact/mixtral/pretrain_mini_mixtral.
     --exp-dir="$NEMO_OUTPUT_PATH"
 
 # Compare outputs
-python3 compare_ckpts.py "$NEMO_OUTPUT_PATH/checkpoints/--None=0.0000-epoch=0/" "$MCORE_OUTPUT_PATH/iter_0000010/"
+python3 /workspace/tests/collections/llm/bitexact/mixtral/compare_ckpts.py \
+  "$NEMO_OUTPUT_PATH/checkpoints/--None=0.0000-epoch=0/" "$MCORE_OUTPUT_PATH/iter_0000010/"
