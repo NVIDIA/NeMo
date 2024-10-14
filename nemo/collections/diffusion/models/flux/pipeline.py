aec7a13fa69af5cd0cf524f9723eed885e42f437
Victor49152 <Victor49152@users.noreply.github.com>
2024-10-14 22:35:54 +0000

    Apply isort and black reformatting
    Signed-off-by: Victor49152 <Victor49152@users.noreply.github.com>

--- nemo/collections/diffusion/models/flux/pipeline.py
+++ nemo/collections/diffusion/models/flux/pipeline.py
@@ -25,8 +25,8 @@ from tqdm import tqdm
 
 from nemo.collections.diffusion.encoders.conditioner import FrozenCLIPEmbedder, FrozenT5Embedder
 from nemo.collections.diffusion.models.flux.model import Flux, FluxParams
-from nemo.collections.diffusion.utils.flux_ckpt_converter import flux_transformer_converter
 from nemo.collections.diffusion.sampler.flow_matching.flow_match_euler_discrete import FlowMatchEulerDiscreteScheduler
+from nemo.collections.diffusion.utils.flux_ckpt_converter import flux_transformer_converter
 from nemo.collections.diffusion.utils.flux_pipeline_utils import FluxModelParams
 from nemo.collections.diffusion.vae.autoencoder import AutoEncoder, AutoEncoderParams
 
