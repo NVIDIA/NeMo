f324f2a04ef0509f5d391a044253e2acd44e89d5
Maanu Grover <maanug@nvidia.com>
2024-10-15 12:00:16 -0700

    extract to common
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

---
+++ tests/collections/llm/common.py
@@ -0,0 +1,99 @@
+from nemo import lightning as nl
+from nemo.collections import llm
+
+
+def train_data(
+    data_path: str, tokenizer_path: str, index_mapping_dir: str, seq_length: int
+) -> llm.PreTrainingDataModule:
+    """Single shard dataset tokenized by SentencePiece"""
+    tokenizer = SentencePieceTokenizer(model_path=tokenizer_path)
+    return llm.PreTrainingDataModule(
+        paths=data_path,
+        tokenizer=tokenizer,
+        seq_length=seq_length,
+        micro_batch_size=4,
+        global_batch_size=32,
+        seed=1234,
+        index_mapping_dir=index_mapping_dir,
+    )
+
+
+def small_llama_cfg(seq_length: int) -> llm.GPTConfig:
+    """Small 145m model"""
+    return llm.Llama3Config8B(
+        rotary_base=500_000,
+        seq_length=seq_length,
+        num_layers=12,
+        hidden_size=768,
+        ffn_hidden_size=2688,
+        num_attention_heads=16,
+        init_method_std=0.023,
+    )
+
+
+class MCoreModelAttributeValidator(pl.Callback):
+    """Walk through submodules and verify user-specified attributes like parallelisms."""
+
+    def __init__(self, attr_dict: dict):
+        super().__init__()
+        self.attr_dict = attr_dict
+
+    def _check_attrs(self, target):
+        for k, v in self.attr_dict.items():
+            if hasattr(target, k):
+                model_val = getattr(target, k)
+                assert (
+                    model_val == v
+                ), f"Key {k} for model ({model_val}) does not match {v} from provided attribute mapping."
+
+    def on_train_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        def walk_fn(module: torch.nn.Module) -> torch.nn.Module:
+            self._check_attrs(module)
+            if hasattr(module, "config"):
+                self._check_attrs(module.config)
+
+            return module
+
+        trainer.model.walk(walk_fn)
+
+
+def verify_distcp_dir(ckpt_path: str) -> None:
+    pass
+
+
+def verify_ckpt_dir(
+    model_ckpt: nl.ModelCheckpoint, max_steps: int, val_check_interval: int, exp_dir: str, dist_ckpts: bool = True
+) -> None:
+    """Ensures a checkpoint directory has the correct number of checkpoints, followed top-k, a checkpoint
+    for the last step exists, and the checkpoints are the correct format.
+    """
+
+    import os
+
+    ckpts = os.listdir(os.path.join(exp_dir, 'checkpoints'))
+
+    expected_ckpts = (max_steps // val_check_interval) + model_ckpt.save_last
+    if model_ckpt.save_last:
+        assert any([c.endswith('-last') for c in ckpts]), "No -last checkpoint found after training"
+    if model_ckpt.save_top_k > 0:
+        assert (
+            len(ckpts) == expected_ckpts or len(ckpts) == model_ckpt.save_top_k + model_ckpt.save_last
+        ), f"Expected {expected_ckpts} checkpoints, or at most top {model_ckpt.save_top_k}"
+    else:
+        assert len(ckpts) == expected_ckpts, f"Expected {expected_ckpts} checkpoints"
+
+    for ckpt_path in ckpts:
+        assert os.path.isdir(ckpt_path) == dist_ckpts, "Checkpoint is not correct type"
+
+        if ckpt_path.endswith('-last') and 'step' in model_ckpt.filename:
+            assert f'step={max_steps}' in ckpt_path
+
+        if dist_ckpts:
+            verify_distcp_dir(ckpt_path)
+
+
+def create_verify_precision(precision: torch.dtype):
+    def verify_precision(tensor: torch.Tensor) -> None:
+        assert tensor.dtype == precision
+
+    return verify_precision
--- tests/collections/llm/llama3_pretraining.py
+++ tests/collections/llm/llama3_pretraining.py
@@ -13,6 +13,8 @@ from nemo.collections import llm
 from nemo.collections.common.tokenizers import SentencePieceTokenizer
 from nemo.lightning.pytorch.callbacks.debugging import ParameterDebugger
 
+from .common import MCoreModelAttributeValidator, create_verify_precision, small_llama_cfg, train_data, verify_ckpt_dir
+
 
 def get_args():
     parser = argparse.ArgumentParser(prog="", description="")
@@ -45,103 +47,6 @@ def get_args():
     return parser.parse_args()
 
 
-def train_data(
-    data_path: str, tokenizer_path: str, index_mapping_dir: str, seq_length: int
-) -> llm.PreTrainingDataModule:
-    """Single shard dataset tokenized by SentencePiece"""
-    tokenizer = SentencePieceTokenizer(model_path=tokenizer_path)
-    return llm.PreTrainingDataModule(
-        paths=data_path,
-        tokenizer=tokenizer,
-        seq_length=seq_length,
-        micro_batch_size=4,
-        global_batch_size=32,
-        seed=1234,
-        index_mapping_dir=index_mapping_dir,
-    )
-
-
-def small_model_cfg(seq_length: int) -> llm.GPTConfig:
-    """Small 145m model"""
-    return llm.Llama3Config8B(
-        rotary_base=500_000,
-        seq_length=seq_length,
-        num_layers=12,
-        hidden_size=768,
-        ffn_hidden_size=2688,
-        num_attention_heads=16,
-        init_method_std=0.023,
-    )
-
-
-def verify_distcp_dir(ckpt_path: str) -> None:
-    pass
-
-
-def verify_ckpt_dir(
-    model_ckpt: nl.ModelCheckpoint, max_steps: int, val_check_interval: int, exp_dir: str, dist_ckpts: bool = True
-) -> None:
-    """Ensures a checkpoint directory has the correct number of checkpoints, followed top-k, a checkpoint
-    for the last step exists, and the checkpoints are the correct format.
-    """
-
-    import os
-
-    ckpts = os.listdir(os.path.join(exp_dir, 'checkpoints'))
-
-    expected_ckpts = (max_steps // val_check_interval) + model_ckpt.save_last
-    if model_ckpt.save_last:
-        assert any([c.endswith('-last') for c in ckpts]), "No -last checkpoint found after training"
-    if model_ckpt.save_top_k > 0:
-        assert (
-            len(ckpts) == expected_ckpts or len(ckpts) == model_ckpt.save_top_k + model_ckpt.save_last
-        ), f"Expected {expected_ckpts} checkpoints, or at most top {model_ckpt.save_top_k}"
-    else:
-        assert len(ckpts) == expected_ckpts, f"Expected {expected_ckpts} checkpoints"
-
-    for ckpt_path in ckpts:
-        assert os.path.isdir(ckpt_path) == dist_ckpts, "Checkpoint is not correct type"
-
-        if ckpt_path.endswith('-last') and 'step' in model_ckpt.filename:
-            assert f'step={max_steps}' in ckpt_path
-
-        if dist_ckpts:
-            verify_distcp_dir(ckpt_path)
-
-
-def _create_verify_precision(precision: torch.dtype):
-    def verify_precision(tensor: torch.Tensor) -> None:
-        assert tensor.dtype == precision
-
-    return verify_precision
-
-
-class MCoreModelAttributeValidator(pl.Callback):
-    """Walk through submodules and verify user-specified attributes like parallelisms."""
-
-    def __init__(self, attr_dict: dict):
-        super().__init__()
-        self.attr_dict = attr_dict
-
-    def _check_attrs(self, target):
-        for k, v in self.attr_dict.items():
-            if hasattr(target, k):
-                model_val = getattr(target, k)
-                assert (
-                    model_val == v
-                ), f"Key {k} for model ({model_val}) does not match {v} from provided attribute mapping."
-
-    def on_train_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
-        def walk_fn(module: torch.nn.Module) -> torch.nn.Module:
-            self._check_attrs(module)
-            if hasattr(module, "config"):
-                self._check_attrs(module.config)
-
-            return module
-
-        trainer.model.walk(walk_fn)
-
-
 def main():
     args = get_args()
 
@@ -149,7 +54,7 @@ def main():
         dir=args.experiment_dir, name="L2_llama3_small_pretrain_test", num_gpus_per_node=args.devices
     )
 
-    pretrain_recipe.model = llm.LlamaModel(small_model_cfg(args.seq_length))
+    pretrain_recipe.model = llm.LlamaModel(small_llama_cfg(args.seq_length))
 
     if args.data_path and args.tokenizer_path:
         pretrain_recipe.data = train_data(
@@ -178,8 +83,8 @@ def main():
         pretrain_recipe.plugins = precision_recipe()
     dtype_map = {"bf16": torch.bfloat16, "fp16": torch.float16, "fp32": torch.float32}
     debugger_callback = ParameterDebugger(
-        param_fn=_create_verify_precision(dtype_map[args.precision]),
-        grad_fn=_create_verify_precision(torch.float32),
+        param_fn=create_verify_precision(dtype_map[args.precision]),
+        grad_fn=create_verify_precision(torch.float32),
         log_on_hooks=["on_train_start", "on_train_end"],
     )
     pretrain_recipe.trainer.callbacks.append(debugger_callback)
