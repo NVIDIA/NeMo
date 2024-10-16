b2bd221587ec526a5282a3b74603c7ffd2ccc374
yashaswikarnati <yashaswikarnati@users.noreply.github.com>
2024-10-16 19:10:04 +0000

    Apply isort and black reformatting
    Signed-off-by: yashaswikarnati <yashaswikarnati@users.noreply.github.com>

--- nemo/collections/multimodal/data/energon/base.py
+++ nemo/collections/multimodal/data/energon/base.py
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from copy import deepcopy
-from typing import Any, Dict, Literal, Optional
+from typing import TYPE_CHECKING, Any, Dict, Literal, Optional
 
 import fiddle as fdl
 import pytorch_lightning as pl
