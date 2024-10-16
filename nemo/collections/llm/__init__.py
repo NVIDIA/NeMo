0f67666dab60c9081f06ee17e6482cf6880dc8cf
Hemil Desai <hemild@nvidia.com>
2024-10-16 02:41:35 -0700

    Add registry to register all needed classes with artifacts in nemo.lightning.io (#10861)
    * Add registry to register all needed classes with artifacts in nemo.lightning.io
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: hemildesai <hemildesai@users.noreply.github.com>
    
    * Fixes
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: hemildesai <hemildesai@users.noreply.github.com>
    
    * Fix
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    * comments
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: hemildesai <hemildesai@users.noreply.github.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: artbataev <artbataev@users.noreply.github.com>
    
    * Remove cyclic import
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    
    ---------
    
    Signed-off-by: Hemil Desai <hemild@nvidia.com>
    Signed-off-by: hemildesai <hemildesai@users.noreply.github.com>
    Signed-off-by: artbataev <artbataev@users.noreply.github.com>
    Co-authored-by: hemildesai <hemildesai@users.noreply.github.com>
    Co-authored-by: artbataev <artbataev@users.noreply.github.com>

--- nemo/collections/llm/__init__.py
+++ nemo/collections/llm/__init__.py
@@ -17,7 +17,7 @@ from nemo.utils.import_utils import safe_import
 
 safe_import("transformer_engine")
 
-from nemo.collections.llm import peft, tokenizer
+from nemo.collections.llm import peft
 from nemo.collections.llm.gpt.data import (
     DollyDataModule,
     FineTuningDataModule,
--- nemo/collections/llm/t5/model/t5.py
+++ nemo/collections/llm/t5/model/t5.py
@@ -29,8 +29,6 @@ if TYPE_CHECKING:
 
 def t5_data_step(dataloader_iter) -> Dict[str, torch.Tensor]:
     from megatron.core import parallel_state
-    from nemo.collections.nlp.modules.common.megatron.token_level_encoder_decoder import AttnMaskType
-    from nemo.collections.nlp.modules.common.megatron.utils import build_attention_mask_3d
 
     from nemo.collections.nlp.modules.common.megatron.token_level_encoder_decoder import AttnMaskType
     from nemo.collections.nlp.modules.common.megatron.utils import build_attention_mask_3d
