351484ad25e413b5a8f83227b4b8eab2a55b956c
ataghibakhsh <ataghibakhsh@nvidia.com>
2024-10-16 10:00:01 -0700

    minor fix

--- nemo/collections/llm/gpt/data/mock.py
+++ nemo/collections/llm/gpt/data/mock.py
@@ -60,7 +60,8 @@ class MockDataModule(pl.LightningDataModule):
 
         # self.tokenizer = tokenizer or get_nmt_tokenizer("megatron", "GPTSentencePieceTokenizer", 
         #                                                 tokenizer_model="/lustre/fsw/coreai_dlalgo_genai/ataghibakhsh/adlr_mamba2/mamba2-hybrid-8b-3t-4k/mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model")
-        self.tokenizer = get_nmt_tokenizer(library='huggingface', model_name="EleutherAI/gpt-neox-20b", tokenizer_model=None, use_fast=True)
+        # self.tokenizer = get_nmt_tokenizer(library='huggingface', model_name="EleutherAI/gpt-neox-20b", tokenizer_model=None, use_fast=True)
+        self.tokenizer = tokenizer or get_nmt_tokenizer("megatron", "GPT2BPETokenizer")
         self.data_sampler = MegatronDataSampler(
             seq_len=self.seq_length,
             micro_batch_size=micro_batch_size,
