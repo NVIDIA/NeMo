e032352097b51fe1879d00cc36bf5b6cad3a309c
Ao Tang <aot@nvidia.com>
2024-10-21 09:58:51 -0700

    Add qwen recipe

--- nemo/collections/llm/recipes/__init__.py
+++ nemo/collections/llm/recipes/__init__.py
@@ -37,6 +37,11 @@ from nemo.collections.llm.recipes import (
     nemotron4_22b_16k,
     nemotron4_22b_64k,
     nemotron4_340b,
+    qwen2,
+    qwen2_500m,
+    qwen2_1p5b,
+    qwen2_7b,
+    qwen2_72b,
 )
 from nemo.collections.llm.recipes.log.default import default_log, default_resume
 from nemo.collections.llm.recipes.optim import adam
@@ -65,6 +70,11 @@ __all__ = [
     "nemotron4_22b_16k",
     "nemotron4_22b_64k",
     "nemotron4_340b",
+    "qwen2",
+    "qwen2_500m",
+    "qwen2_1p5b",
+    "qwen2_7b",
+    "qwen2_72b",
     "adam",
     "default_log",
     "default_resume",
