b1cd3742e9d98b3cd6669fd0239ed581015d8c8c
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-21 14:07:21 -0700

    add hfAutoModel pretrain nemorun recipe
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/llm/recipes/optim/adam.py
+++ nemo/collections/llm/recipes/optim/adam.py
@@ -17,7 +17,7 @@ from typing import Optional
 import nemo_run as run
 from megatron.core.optimizer import OptimizerConfig
 
-from nemo.lightning.pytorch.optim import CosineAnnealingScheduler, MegatronOptimizerModule, OptimizerModule
+from nemo.lightning.pytorch.optim import CosineAnnealingScheduler, MegatronOptimizerModule, OptimizerModule, PytorchOptimizerModule
 
 
 @run.cli.factory
@@ -56,6 +56,41 @@ def distributed_fused_adam_with_cosine_annealing(
 
     return run.Config(
         MegatronOptimizerModule,
+        config=opt_cfg,
+        lr_scheduler=sched,
+    )
+
+
+
+@run.cli.factory
+def pytorch_adam(
+    precision: str = "bf16-mixed",  # or "16-mixed"
+    warmup_steps: int = 2000,
+    constant_steps: int = 0,
+    max_lr: float = 1e-4,
+    min_lr: Optional[float] = None,
+    clip_grad: float = 1.0,
+) -> run.Config[OptimizerModule]:
+
+    opt_cfg = dict(
+        lr=max_lr,
+        weight_decay=0.1,
+        betas=(0.9, 0.95),
+        eps=1e-8,
+    )
+
+    min_lr = min_lr or (0.1 * max_lr)
+    sched = run.Config(
+        CosineAnnealingScheduler,
+        warmup_steps=warmup_steps,
+        constant_steps=constant_steps,
+        min_lr=min_lr,
+    )
+    from torch.optim import Adam
+
+    return run.Config(
+        PytorchOptimizerModule,
+        optim_cls=Adam,
         config=opt_cfg,
         lr_scheduler=sched,
     )
