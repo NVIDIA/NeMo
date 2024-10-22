a6a15db1e815cbd11dc87e2b70334d9ac8ce2095
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-22 12:22:31 -0700

    add option to start with random weights
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/llm/gpt/model/hf_auto_model_for_causal_lm.py
+++ nemo/collections/llm/gpt/model/hf_auto_model_for_causal_lm.py
@@ -20,13 +20,14 @@ def masked_cross_entropy(logits, targets, mask=None):
 
 
 class HfAutoModelForCausalLM(pl.LightningModule, io.IOMixin):
-    def __init__(self, model_name='gpt2', tokenizer=None, loss_fn=masked_cross_entropy):
+    def __init__(self, model_name='gpt2', load_pretrained_weights=True, tokenizer=None, loss_fn=masked_cross_entropy):
         super().__init__()
         self.save_hyperparameters()
         self.model_name = model_name
         self._tokenizer = None
         self.model = None
         self.loss_fn = loss_fn
+        self.load_pretrained_weights = load_pretrained_weights
 
     @property
     def tokenizer(self):
@@ -45,7 +46,12 @@ class HfAutoModelForCausalLM(pl.LightningModule, io.IOMixin):
 
     def configure_model(self):
         # create all your layers here
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype='auto')
+        if self.load_pretrained_weights:
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype='auto')
+        else:
+            from transformers import AutoConfig
+            config = AutoConfig.from_pretained(self.model_name)
+            self.model = AutoModelForCausalLM.from_config(config)
         self.model.train()
 
     def forward(self, input_ids, attention_mask=None, labels=None, loss_mask=None):
