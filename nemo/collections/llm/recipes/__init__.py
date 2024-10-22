ae3d348bd0d3d1e31924160055eca31e2490c1f2
Malay Nagda <malayn@nvidia.com>
2024-10-22 15:10:25 +0530

    gpt3 175b cli
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- nemo/collections/llm/recipes/__init__.py
+++ nemo/collections/llm/recipes/__init__.py
@@ -44,6 +44,7 @@ from nemo.collections.llm.recipes import (
     nemotron4_22b_16k,
     nemotron4_22b_64k,
     nemotron4_340b,
+    gpt3_175b,
 )
 from nemo.collections.llm.recipes.log.default import default_log, default_resume
 from nemo.collections.llm.recipes.optim import adam
@@ -79,6 +80,7 @@ __all__ = [
     "nemotron4_22b_16k",
     "nemotron4_22b_64k",
     "nemotron4_340b",
+    "gpt3_175b",
     "adam",
     "default_log",
     "default_resume",
