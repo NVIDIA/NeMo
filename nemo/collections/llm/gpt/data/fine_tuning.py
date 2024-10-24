8a5d7bf1fe21f82dffef6600d8c41c36bd293583
Marc Romeyn <mromeijn@nvidia.com>
2024-10-24 16:03:39 +0200

    Move AutoTokenizer inline
    Signed-off-by: Marc Romeyn <mromeijn@nvidia.com>

--- nemo/collections/llm/gpt/data/fine_tuning.py
+++ nemo/collections/llm/gpt/data/fine_tuning.py
@@ -20,7 +20,6 @@ from typing import TYPE_CHECKING, List, Optional, Union
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
-from nemo.collections.common.tokenizers import AutoTokenizer
 from nemo.collections.llm.gpt.data.core import create_sft_dataset
 from nemo.lightning.pytorch.plugins import MegatronDataSampler
 from nemo.utils import logging
@@ -208,6 +207,8 @@ class FineTuningDataModule(pl.LightningDataModule):
         return self.dataset_root / "test.jsonl"
 
     def _extract_tokenizer_model_name(self) -> str:
+        from nemo.collections.common.tokenizers import AutoTokenizer
+
         if self.packed_sequence_specs.tokenizer_model_name is not None:
             tokenizer_model_name = self.packed_sequence_specs.tokenizer_model_name
         elif isinstance(self.tokenizer, AutoTokenizer):
