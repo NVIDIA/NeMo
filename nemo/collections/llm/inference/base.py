53d9e517b6e5e50ace7268b0d3ddd3017cdd8a89
Chen Cui <chcui@nvidia.com>
2024-10-24 14:46:53 -0700

    add copyright header
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- nemo/collections/llm/inference/base.py
+++ nemo/collections/llm/inference/base.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 from pathlib import Path
 from typing import Optional, Union
@@ -119,7 +133,8 @@ def generate(
     prompts: list[str],
     max_batch_size: int = 4,
     random_seed: Optional[int] = None,
-    inference_params: Optional[CommonInferenceParams] = None,
+    add_BOS: bool = False,
+        inference_params: Optional[CommonInferenceParams] = None,
 ) -> dict:
     text_generation_controller = SimpleTextGenerationController(inference_wrapped_model=model, tokenizer=tokenizer)
     mcore_engine = MCoreEngine(
@@ -131,6 +146,7 @@ def generate(
     results = mcore_engine.generate(
         prompts=prompts,
         common_inference_params=common_inference_params,
+        add_BOS=add_BOS,
     )
 
     return results
