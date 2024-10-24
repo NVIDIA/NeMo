06002b4900d91bddcce5655239be0a7f5473520e
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-23 17:56:31 -0700

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/strategies/megatron_strategy.py
+++ nemo/lightning/pytorch/strategies/megatron_strategy.py
@@ -100,13 +100,6 @@ class ParallelismConfig:
     pipeline_dtype: torch.dtype
 
 
-def unwrap_module(model):
-    tmp = model
-    while hasattr(model, 'module'):
-        tmp = model.module
-    return tmp
-
-
 class MegatronStrategy(DDPStrategy, io.IOMixin):
     """Megatron plugin for Pytorch Lightning.
 
@@ -274,9 +267,7 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
     def connect(self, model: pl.LightningModule) -> None:
         super().connect(model)
 
-        assert not hasattr(
-            unwrap_module(model), 'is_hf_model'
-        ), "Cannot use HfAutoModelForCausalLM with MegatronParallel"
+        assert not 'is_hf_model' in model.__dict__, "Cannot use HfAutoModelForCausalLM with MegatronParallel"
 
         _maybe_mcore_config = _strategy_lib.set_model_parallel_attributes(model, self.parallelism)
         if _maybe_mcore_config:
