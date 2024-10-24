161907e88a134a0c8b1b565abe4ada0e53068c77
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-23 17:08:26 -0700

    check is_hf_model in leaf module
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/strategies/megatron_strategy.py
+++ nemo/lightning/pytorch/strategies/megatron_strategy.py
@@ -100,6 +100,12 @@ class ParallelismConfig:
     pipeline_dtype: torch.dtype
 
 
+def unwrap_module(model):
+    tmp = model
+    while hasattr(model, 'module'):
+          tmp = model.module
+    return tmp
+
 class MegatronStrategy(DDPStrategy, io.IOMixin):
     """Megatron plugin for Pytorch Lightning.
 
@@ -267,7 +273,7 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
     def connect(self, model: pl.LightningModule) -> None:
         super().connect(model)
 
-        assert not hasattr(model, 'is_hf_model'), "Cannot use HfAutoModelForCausalLM with MegatronParallel"
+        assert not hasattr(unwrap_module(model), 'is_hf_model'), "Cannot use HfAutoModelForCausalLM with MegatronParallel"
 
         _maybe_mcore_config = _strategy_lib.set_model_parallel_attributes(model, self.parallelism)
         if _maybe_mcore_config:
