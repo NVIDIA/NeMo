5089d3f1d08a8a7f2328324ac0b2c2129b4dea3f
suiyoubi <suiyoubi@users.noreply.github.com>
2024-10-28 13:22:53 +0000

    Apply isort and black reformatting
    Signed-off-by: suiyoubi <suiyoubi@users.noreply.github.com>

--- nemo/collections/llm/recipes/__init__.py
+++ nemo/collections/llm/recipes/__init__.py
@@ -50,16 +50,16 @@ from nemo.collections.llm.recipes import (
     nemotron4_22b_16k,
     nemotron4_22b_64k,
     nemotron4_340b,
-    starcoder,
-    starcoder2,
-    starcoder2_3b,
-    starcoder2_7b,
-    starcoder2_15b,
     qwen2,
     qwen2_1p5b,
     qwen2_7b,
     qwen2_72b,
     qwen2_500m,
+    starcoder,
+    starcoder2,
+    starcoder2_3b,
+    starcoder2_7b,
+    starcoder2_15b,
 )
 from nemo.collections.llm.recipes.log.default import default_log, default_resume
 from nemo.collections.llm.recipes.optim import adam
