e59c7fec96136f5582c42a9628d159dd973ff7b3
yaoyu-33 <yaoyu.094@gmail.com>
2024-10-28 14:57:19 -0700

    try fix test
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- tests/lightning/test_strategy_lib.py
+++ tests/lightning/test_strategy_lib.py
@@ -139,6 +139,8 @@ def test_init_model_parallel(mock_mpu, *args):
         pipeline_model_parallel_size=1,
         virtual_pipeline_model_parallel_size=None,
         pipeline_model_parallel_split_rank=None,
+        encoder_pipeline_model_parallel_size=None,
+        encoder_tensor_model_parallel_size=None,
         context_parallel_size=2,
         expert_model_parallel_size=2,
     )
