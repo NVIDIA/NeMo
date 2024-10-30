41f4d1a1ccb9492e7952df455405b5166791aa69
Hemil Desai <hemild@nvidia.com>
2024-10-30 16:40:46 -0700

    Add scripts for importing a ckpt and running a forward step on it for nemo.collections.llm
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

---
+++ examples/llm/import_and_forward/import_ckpt.py
@@ -0,0 +1,11 @@
+from nemo.collections.llm import import_ckpt
+from nemo.collections.llm.gpt.model.starcoder2 import Starcoder2Model, Starcoder2Config3B
+from pathlib import Path
+
+if __name__ == "__main__":
+    import_ckpt(
+        model=Starcoder2Model(Starcoder2Config3B()), 
+        source='hf://bigcode/starcoder2-3b',
+        output_path=Path('/workspace/starcoder2_3b_nemo2'),
+        overwrite=True
+    )
