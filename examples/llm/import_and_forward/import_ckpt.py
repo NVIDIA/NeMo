73abedc906bf24676f27f24835a6d86a50e71ec8
hemildesai <hemildesai@users.noreply.github.com>
2024-10-30 23:43:51 +0000

    Apply isort and black reformatting
    Signed-off-by: hemildesai <hemildesai@users.noreply.github.com>

--- examples/llm/import_and_forward/import_ckpt.py
+++ examples/llm/import_and_forward/import_ckpt.py
@@ -1,11 +1,12 @@
-from nemo.collections.llm import import_ckpt
-from nemo.collections.llm.gpt.model.starcoder2 import Starcoder2Model, Starcoder2Config3B
 from pathlib import Path
+
+from nemo.collections.llm import import_ckpt
+from nemo.collections.llm.gpt.model.starcoder2 import Starcoder2Config3B, Starcoder2Model
 
 if __name__ == "__main__":
     import_ckpt(
-        model=Starcoder2Model(Starcoder2Config3B()), 
+        model=Starcoder2Model(Starcoder2Config3B()),
         source='hf://bigcode/starcoder2-3b',
         output_path=Path('/workspace/starcoder2_3b_nemo2'),
-        overwrite=True
+        overwrite=True,
     )
