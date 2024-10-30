ce781950e73db1acd90a64fc13bbed643989106b
Marc Romeyn <mromeijn@nvidia.com>
2024-10-30 21:26:47 +0100

    Fix datasets in CLI (#11097)
    * Fix datasets in CLI
    
    Signed-off-by: Marc Romeyn <mromeijn@nvidia.com>
    
    * Fix wrong import
    
    Signed-off-by: Marc Romeyn <mromeijn@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: marcromeyn <marcromeyn@users.noreply.github.com>
    
    * Fix wrong import
    
    Signed-off-by: Marc Romeyn <mromeijn@nvidia.com>
    
    ---------
    
    Signed-off-by: Marc Romeyn <mromeijn@nvidia.com>
    Signed-off-by: marcromeyn <marcromeyn@users.noreply.github.com>
    Co-authored-by: marcromeyn <marcromeyn@users.noreply.github.com>
    Signed-off-by: Hainan Xu <hainanx@nvidia.com>

--- nemo/collections/llm/__init__.py
+++ nemo/collections/llm/__init__.py
@@ -26,7 +26,7 @@ from nemo.collections.llm.gpt.data import (
     PreTrainingDataModule,
     SquadDataModule,
 )
-from nemo.collections.llm.gpt.data.api import dolly, mock, squad
+from nemo.collections.llm.gpt.data.api import dolly, hf_dataset, mock, squad
 from nemo.collections.llm.gpt.model import (
     Baichuan2Config,
     Baichuan2Config7B,
@@ -197,6 +197,7 @@ __all__ = [
     "squad",
     "dolly",
     "peft",
+    "hf_dataset",
     "HfAutoModelForCausalLM",
 ]
 
