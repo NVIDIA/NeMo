cb791ec6fa06f155523169e10668283e00b3e74a
Hemil Desai <hemild@nvidia.com>
2024-10-29 22:42:44 -0700

    fix
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- nemo/collections/llm/gpt/data/pre_training.py
+++ nemo/collections/llm/gpt/data/pre_training.py
@@ -462,6 +462,8 @@ def build_pretraining_datamodule(
     # The indices in Megatron are built on rank 0, so we set the world size to 1 here.
     torch.distributed.init_process_group(world_size=1, rank=0)
 
+    from nemo.utils import logging
+    logging.info(f"Building {datamodule}")
     datamodule.build(
         trainer_max_steps=trainer_max_steps,
         trainer_val_check_interval=trainer_val_check_interval,
