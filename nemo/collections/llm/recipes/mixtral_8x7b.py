b1f969a7546bfc98898f6cdba8a8143ac9d111c4
JimmyZhang12 <67203904+JimmyZhang12@users.noreply.github.com>
2024-10-30 23:14:15 -0400

    disable overlap_param_gather_with_optimizer_step (#11102)
    * disable overlap_param_gather_with_optimizer_step
    
    Signed-off-by: Jimmy Zhang <jiemingz@nvidia.com>
    
    * fix comment
    
    Signed-off-by: Jieming Zhang <jiemingz@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: JimmyZhang12 <JimmyZhang12@users.noreply.github.com>
    
    * fix typo again
    
    Signed-off-by: Jieming Zhang <jiemingz@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: JimmyZhang12 <JimmyZhang12@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jimmy Zhang <jiemingz@nvidia.com>
    Signed-off-by: Jieming Zhang <jiemingz@nvidia.com>
    Signed-off-by: JimmyZhang12 <JimmyZhang12@users.noreply.github.com>
    Co-authored-by: Jimmy Zhang <jiemingz@nvidia.com>
    Co-authored-by: JimmyZhang12 <JimmyZhang12@users.noreply.github.com>

--- nemo/collections/llm/recipes/mixtral_8x7b.py
+++ nemo/collections/llm/recipes/mixtral_8x7b.py
@@ -222,7 +222,7 @@ def pretrain_performance_optimizations(recipe: run.Partial) -> run.Partial:
             run.Config(MegatronTokenDropCallback),
             run.Config(
                 MegatronCommOverlapCallback,
-                overlap_param_gather_with_optimizer_step=True,
+                overlap_param_gather_with_optimizer_step=False,  # Currently disabled due to an issue with checkpointing.
                 align_param_gather=True,
             ),
         ]
