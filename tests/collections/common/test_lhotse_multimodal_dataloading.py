61170a4331d14ba1d1ef8dd689b9dab1059ce87c
Piotr Żelasko <petezor@gmail.com>
2024-11-01 11:17:28 -0400

    Feature/conversations tarred (#11086)
    * Multimodal conversation tarring script
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    * Fix sharding logic
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    * Fix dir creation
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    ---------
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>

--- tests/collections/common/test_lhotse_multimodal_dataloading.py
+++ tests/collections/common/test_lhotse_multimodal_dataloading.py
@@ -2,13 +2,20 @@ import json
 from itertools import islice
 
 import lhotse
+import numpy as np
 import pytest
 import torch
 from lhotse.testing.dummies import dummy_cut, dummy_recording
 from omegaconf import OmegaConf
 
 from nemo.collections.common.data.lhotse import get_lhotse_dataloader_from_config
-from nemo.collections.common.data.lhotse.text_adapters import AudioTurn, NeMoMultimodalConversation, TextTurn
+from nemo.collections.common.data.lhotse.text_adapters import (
+    AudioTurn,
+    NeMoMultimodalConversation,
+    NeMoMultimodalConversationJsonlAdapter,
+    NeMoMultimodalConversationTarWriter,
+    TextTurn,
+)
 from nemo.collections.common.tokenizers.aggregate_tokenizer import TokenizerWrapper
 from nemo.collections.common.tokenizers.sentencepiece_tokenizer import SentencePieceTokenizer, create_spt_model
 
@@ -212,3 +219,46 @@ def test_multimodal_conversation_input_with_prompt(multimodal_conversations_path
     assert (ex.mask[30:72] == True).all()  # assistant turn
     assert (ex.mask[72:95] == False).all()  # user turn
     assert (ex.mask[95:] == True).all()  # assistant turn
+
+
+def test_multimodal_conversation_tarred_format(multimodal_conversations_path, tmp_path_factory):
+    (conversation,) = list(NeMoMultimodalConversationJsonlAdapter(multimodal_conversations_path, "[audio]"))
+    tar_dir = tmp_path_factory.mktemp("multi_convo_tarred")
+    with NeMoMultimodalConversationTarWriter(tar_dir) as writer:
+        writer.write(conversation)
+
+    (restored_conversation,) = list(
+        NeMoMultimodalConversationJsonlAdapter(
+            manifest_filepath=tar_dir / "manifest_0.jsonl",
+            audio_locator_tag="[audio]",
+            tarred_audio_filepaths=tar_dir / "audio_0.tar",
+        )
+    )
+    assert conversation.id == restored_conversation.id
+    assert len(conversation.turns) == len(restored_conversation.turns)
+    for lhs, rhs in zip(conversation.turns, restored_conversation.turns):
+        assert type(lhs) == type(rhs)
+        assert lhs.role == lhs.role
+        if isinstance(lhs, TextTurn):
+            assert lhs.value == rhs.value
+        else:
+            assert lhs.audio_locator_tag == rhs.audio_locator_tag
+            assert lhs.cut.id == rhs.cut.id
+            np.testing.assert_allclose(lhs.cut.load_audio(), rhs.cut.load_audio())
+
+
+def test_multimodal_conversation_tarred_format_sharding_works(multimodal_conversations_path, tmp_path_factory):
+    (conversation,) = list(NeMoMultimodalConversationJsonlAdapter(multimodal_conversations_path, "[audio]"))
+    tar_dir = tmp_path_factory.mktemp("multi_convo_tarred")
+    with NeMoMultimodalConversationTarWriter(tar_dir, shard_size=10) as writer:
+        for i in range(30):
+            writer.write(conversation)
+
+    loader = NeMoMultimodalConversationJsonlAdapter(
+        manifest_filepath=tar_dir / "manifest_{0..2}.jsonl",
+        audio_locator_tag="[audio]",
+        tarred_audio_filepaths=tar_dir / "audio_{0..2}.tar",
+    )
+    restored = list(loader)
+    assert len(restored) == 30
+    assert all(c == restored[0] for c in restored[1:])
