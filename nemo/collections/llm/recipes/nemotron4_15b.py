8f4a22bfba9b8d46fdc02dfed99c74b77166b841
ShriyaPalsamudram <ShriyaPalsamudram@users.noreply.github.com>
2024-11-04 19:54:11 +0000

    Apply isort and black reformatting
    Signed-off-by: ShriyaPalsamudram <ShriyaPalsamudram@users.noreply.github.com>

--- nemo/collections/llm/recipes/nemotron4_15b.py
+++ nemo/collections/llm/recipes/nemotron4_15b.py
@@ -21,8 +21,8 @@ import torch
 from nemo.collections.llm.api import finetune, pretrain
 from nemo.collections.llm.gpt.data.mock import MockDataModule
 from nemo.collections.llm.peft.lora import LoRA
-from nemo.collections.llm.recipes.finetune_default import default_finetune_recipe
 from nemo.collections.llm.recipes.callbacks.default import straggler_det_callback
+from nemo.collections.llm.recipes.finetune_default import default_finetune_recipe
 from nemo.collections.llm.recipes.log.default import default_log, default_resume, tensorboard_logger
 from nemo.collections.llm.recipes.nemotron import nemotron_model, nemotron_trainer
 from nemo.collections.llm.recipes.optim.adam import distributed_fused_adam_with_cosine_annealing
