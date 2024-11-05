b0dac1af3790971b57e6a3f99e09bd2d33d20a2c
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-11-05 05:54:13 -0800

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/llm/test_nemo_jit_cb.py
+++ tests/collections/llm/test_nemo_jit_cb.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional
 
 import fiddle as fdl
 import pytorch_lightning as pl
