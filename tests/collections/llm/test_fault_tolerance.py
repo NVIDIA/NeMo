8508edb0c093b355b2ed4c6063095316f52436cc
Shriya Balaji Palsamudram <spalsamudram@nvidia.com>
2024-11-07 22:06:37 +0000

    draft crash simulation
    Signed-off-by: Shriya Balaji Palsamudram <spalsamudram@nvidia.com>

--- tests/collections/llm/test_fault_tolerance.py
+++ tests/collections/llm/test_fault_tolerance.py
@@ -18,9 +18,12 @@ Test fault tolerance with LLaMA3 recipe and a smaller model.
 
 import argparse
 import os
+from datetime import datetime, timedelta
 
 import nemo_run as run
 import torch
+
+from pytorch_lightning.callbacks import Callback
 
 from nemo.collections import llm
 from nemo.collections.llm.recipes.callbacks.default import straggler_det_callback
@@ -29,16 +32,25 @@ from nemo.utils.exp_manager import TimingCallback
 from tests.collections.llm.common import create_verify_precision, small_llama_cfg, train_data, verify_ckpt_dir
 
 
+class CrashCallback(Callback):
+    def __init__(self, crash_step=3, crash_time=None):
+        self.crash_step = crash_step
+        self.crash_time = crash_time
+        print(f"Setup to simulate a crash if step time > {self.crash_step} before {self.crash_time}")
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        #if self.crash_step and trainer.global_step == self.crash_step:
+        #    raise Exception(f"Simulating a crash at step {self.crash_step}!")
+
+        if datetime.now() <= datetime.strptime(self.crash_time, "%Y-%m-%d %H:%M:%S") and trainer.global_step >= self.crash_step:
+            raise Exception("Simulating a crash!")
+
+
 def get_args():
     parser = argparse.ArgumentParser(prog="", description="")
     parser.add_argument('--devices', type=int, required=True, help="Number of devices to use for training")
     parser.add_argument('--max-steps', type=int, required=True, help="Number of steps to train for")
-    parser.add_argument(
-        '--simulated-fault',
-        action="store_true",
-        default=False,
-        help="Add a simulated fault to stop the training run (for testing nvidia-resiliency-ext's fault tolerance plugin)",
-    )
+    parser.add_argument('--crash-time', type=str, required=True, help="Datetime string which indicates when to simulate a crash. Set this to a few minutes after the training starts to ensure a successful crash.")
     parser.add_argument(
         '--experiment-dir', type=str, required=True, help="directory to write results and checkpoints to"
     )
@@ -79,12 +91,12 @@ def main():
     pretrain_recipe.trainer.log_every_n_steps = 1
     pretrain_recipe.log.ckpt.every_n_train_steps = None
     pretrain_recipe.log.ckpt.train_time_interval = None
-    pretrain_recipe.trainer.val_check_interval = 2
+    pretrain_recipe.trainer.val_check_interval = 4
     pretrain_recipe.trainer.limit_val_batches = 2
 
     executor: run.SlurmExecutor = run.LocalExecutor(ntasks_per_node=args.devices, launcher="ft")
-    run_plugins: list[run.Plugin] = [FaultTolerancePlugin()]
-    pretrain_recipe.trainer.callbacks = [run.Config(TimingCallback), straggler_det_callback()]
+    run_plugins: list[run.Plugin] = [FaultTolerancePlugin(num_in_process_restarts=1, num_job_retries_on_failure=0)]
+    pretrain_recipe.trainer.callbacks = [run.Config(TimingCallback), straggler_det_callback(), run.Config(CrashCallback, crash_step=6, crash_time=args.crash_time)]
 
     run.run(pretrain_recipe, plugins=run_plugins, executor=executor)
 
