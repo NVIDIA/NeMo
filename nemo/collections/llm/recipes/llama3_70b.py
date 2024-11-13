3625d78ad53cd702e09a215dbe7c989a44fbdc61
Chen Cui <chcui@nvidia.com>
2024-11-13 16:50:22 -0500

    Add llama 3.1 recipes (#11273)
    * add llama 3.1 recipes
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: cuichenx <cuichenx@users.noreply.github.com>
    
    * fix pylint
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    * Fix llama3.1 wrong config in io.json
    
    ---------
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    Signed-off-by: cuichenx <cuichenx@users.noreply.github.com>
    Co-authored-by: cuichenx <cuichenx@users.noreply.github.com>
    Co-authored-by: Ao Tang <aot@nvidia.com>

--- nemo/collections/llm/recipes/llama3_70b.py
+++ nemo/collections/llm/recipes/llama3_70b.py
@@ -263,9 +263,10 @@ def finetune_recipe(
         name (str): Name of the fine-tuning run.
         num_nodes (int): Number of compute nodes to use.
         num_gpus_per_node (int): Number of GPUs per node.
-        peft_scheme (Optional[str]): Name of the peft scheme to use for fine-tuning. Allowed values: 'lora', 'none'/None.
+        peft_scheme (Optional[str]): Name of the peft scheme to use for finetuning. Allowed values: 'lora'/'none'/None.
         seq_length (int): Maximum number of tokens per microbatch.
-        packed_sequence (Optional[bool]): If true, fine-tuning sequences will be packed into batches up to the given maximum seq_length for better efficiency. By default, this value equals performance_mode.
+        packed_sequence (Optional[bool]): If true, fine-tuning sequences will be packed into batches up to the given
+            maximum seq_length for better efficiency. By default, this value equals performance_mode.
         performance_mode (bool): If true, enables optimizations for maximum performance.
 
     Returns:
@@ -325,7 +326,7 @@ def finetune_recipe(
     recipe.model.config.seq_length = seq_length
     recipe.data.seq_length = seq_length
     if packed_sequence:
-        recipe.data.pad_to_max_length = True
+        recipe.data.dataset_kwargs = {'pad_to_max_length': True}
         recipe.data.packed_sequence_specs = run.Config(PackedSequenceSpecs, packed_sequence_size=seq_length)
 
     if performance_mode:
