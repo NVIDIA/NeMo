fec35a6e5239b660321df2220ce15a78dc5e91ec
maanug-nv <maanug-nv@users.noreply.github.com>
2024-11-14 06:44:47 +0000

    Apply isort and black reformatting
    Signed-off-by: maanug-nv <maanug-nv@users.noreply.github.com>

--- nemo/utils/callbacks/nemo_model_checkpoint.py
+++ nemo/utils/callbacks/nemo_model_checkpoint.py
@@ -22,10 +22,10 @@ from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
 import lightning.pytorch
 import torch
 from _weakref import proxy
+from lightning.fabric.utilities.cloud_io import get_filesystem
 from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint, _is_local_file_protocol
 from lightning.pytorch.trainer import call
 from lightning.pytorch.utilities import rank_zero_info
-from lightning.fabric.utilities.cloud_io import get_filesystem
 
 from nemo.collections.common.callbacks import EMA
 from nemo.utils import logging
