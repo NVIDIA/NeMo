5120769fc2eda21ca2a98cd9ea20aff48864b27c
Yu Yao <54727607+yaoyu-33@users.noreply.github.com>
2024-11-18 12:45:30 -0800

    Add `attention_bias` argument in transformer block and transformer layer modules, addressing change in MCore (#11289)
    * fix api
    
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>
    
    * fix ci
    
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>
    
    * add docstring
    
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: yaoyu-33 <yaoyu-33@users.noreply.github.com>
    
    * fix docstring2
    
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: yaoyu-33 <yaoyu-33@users.noreply.github.com>
    
    * fix line too long
    
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>
    
    ---------
    
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>
    Signed-off-by: yaoyu-33 <yaoyu-33@users.noreply.github.com>
    Co-authored-by: yaoyu-33 <yaoyu-33@users.noreply.github.com>
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/adapters/mcore_mixins.py
+++ nemo/collections/nlp/modules/common/megatron/adapters/mcore_mixins.py
@@ -82,19 +82,20 @@ class MCoreTransformerBlockMixin(TransformerBlock, MCoreAdapterModuleMixin):
         rotary_pos_emb: Tensor = None,
         rotary_pos_cos: Tensor = None,
         rotary_pos_sin: Tensor = None,
+        attention_bias: Tensor = None,
         inference_params: InferenceParams = None,
         packed_seq_params: PackedSeqParams = None,
     ):
         hidden_states = super().forward(
-            hidden_states,
-            attention_mask,
-            context,
-            context_mask,
-            rotary_pos_emb,
-            rotary_pos_cos,
-            rotary_pos_sin,
-            inference_params,
-            packed_seq_params,
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            context=context,
+            context_mask=context_mask,
+            rotary_pos_emb=rotary_pos_emb,
+            rotary_pos_cos=rotary_pos_cos,
+            rotary_pos_sin=rotary_pos_sin,
+            inference_params=inference_params,
+            packed_seq_params=packed_seq_params,
         )
 
         mlp_head_adapter = self.get_adapter_module(AdapterName.MLP_HEAD_ADAPTER)
@@ -232,6 +233,7 @@ class MCoreSelfAttentionMixin(SelfAttention, MCoreAdapterModuleMixin):
         packed_seq_params=None,
         rotary_pos_cos=None,
         rotary_pos_sin=None,
+        attention_bias=None,
     ):
         # hidden_states: [sq, b, h]
 
