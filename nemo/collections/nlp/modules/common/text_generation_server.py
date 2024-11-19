c4c7de684d296bbafb7adf0673b6631fd5b4c88a
Jiaqi Zeng <jiaqiz@nvidia.com>
2024-11-19 07:13:59 -0800

    remove end_strings
    Signed-off-by: Jiaqi Zeng <jiaqiz@nvidia.com>

--- nemo/collections/nlp/modules/common/text_generation_server.py
+++ nemo/collections/nlp/modules/common/text_generation_server.py
@@ -220,6 +220,11 @@ class MegatronGenerate(Resource):
                     output[k] = output[k].tolist()
 
         output_sentence = output['sentences'][0][len(conversation) :]
+        # remove end_strings
+        for e in end_strings:
+            if output_sentence.endswith(e):
+                output_sentence = output_sentence[:-len(e)]
+
         tokens = output['tokens'][0]
         tokens = [t.decode('utf-8', errors='replace') if isinstance(t, bytes) else t for t in tokens]
         logprobs = output['logprob'][0] if output['logprob'] is not None else None
