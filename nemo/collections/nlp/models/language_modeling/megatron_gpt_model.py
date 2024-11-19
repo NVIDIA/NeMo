094d69f2abfd4df0ff02f1ac39e21e41a3f9c7cd
guyueh1 <guyueh1@users.noreply.github.com>
2024-11-19 19:46:28 +0000

    Apply isort and black reformatting
    Signed-off-by: guyueh1 <guyueh1@users.noreply.github.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -2158,9 +2158,11 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
                 'use_te_rng_tracker', False
             ), "Transformer engine's RNG tracker is required for cudagraphs, this can be enabled with \
                 'use_te_rng_tracker=True'."
-        
+
         if self.cfg.get('use_te_activation_func', False):
-            assert HAVE_TE, "Transformer Engine is required for using TransformerEngine activation \
+            assert (
+                HAVE_TE
+            ), "Transformer Engine is required for using TransformerEngine activation \
                 function."
 
         # any configs that are not in the nemo model config will be added here
