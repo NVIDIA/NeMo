f7b79a576a5a0a493651a2d276e5d123fe8df049
Ananth Subramaniam <ansubramania@nvidia.com>
2024-11-20 11:06:41 -0800

    import optional

--- nemo/collections/diffusion/encoders/conditioner.py
+++ nemo/collections/diffusion/encoders/conditioner.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Union
+from typing import List, Optional, Union
 
 import torch
 import torch.nn as nn
