4f64a1c49cb9ec7c6d75b467a23897d5fe7778ca
Marc Romeijn <mromeijn@nvidia.com>
2024-11-20 07:14:43 -0800

    Add torchrun local executor to recipes
    Signed-off-by: Marc Romeijn <mromeijn@nvidia.com>

---
+++ nemo/collections/llm/recipes/run/executor.py
@@ -0,0 +1,16 @@
+import nemo_run as run
+
+
+@run.cli.factory
+def torchrun(devices: int = 8) -> run.LocalExecutor:
+    env_vars = {
+        "TRANSFORMERS_OFFLINE": "1",
+        "TORCH_NCCL_AVOID_RECORD_STREAMS": "1",
+        "NCCL_NVLS_ENABLE": "0",
+        "NVTE_DP_AMAX_REDUCE_INTERVAL": "0",
+        "NVTE_ASYNC_AMAX_REDUCTION": "1",
+    }
+
+    executor = run.LocalExecutor(ntasks_per_node=devices, launcher="torchrun", env_vars=env_vars)
+
+    return executor
