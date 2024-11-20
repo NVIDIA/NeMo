3ee32b0089cc717e4735dfcc1fd3c6608896d6ba
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-11-20 07:07:59 -0800

    remove stale imports
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/llm/gpt/model/auto_model.py
+++ nemo/collections/llm/gpt/model/auto_model.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Callable, Optional
+from typing import Callable, Optional
 
 from torch import nn
-from typing_extensions import Annotated
 from nemo.lightning.pytorch.optim import OptimizerModule
 from pathlib import Path
 
