a773ecd0a80f69e27d9c5047dc5df2886eaf80e5
Nithin Rao <nithinrao.koluguri@gmail.com>
2024-11-21 16:03:50 -0500

    update hypothesis when passed through cfg (#11366)
    Signed-off-by: Nithin Rao Koluguri <nithinraok>
    Co-authored-by: Nithin Rao Koluguri <nithinraok>
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- examples/asr/transcribe_speech.py
+++ examples/asr/transcribe_speech.py
@@ -275,6 +275,8 @@ def main(cfg: TranscriptionConfig) -> Union[TranscriptionConfig, List[Hypothesis
 
     # we will adjust this flag if the model does not support it
     compute_langs = cfg.compute_langs
+    if cfg.timestamps:
+        cfg.return_hypotheses = True
 
     # Check whether model and decoder type match
     if isinstance(asr_model, EncDecCTCModel):
