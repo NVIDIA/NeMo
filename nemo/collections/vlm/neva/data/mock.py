0e9241a600ce3f4d032ffaa9deffbc4432b78d1b
ykarnati <ykarnati@nvidia.com>
2024-11-22 12:32:41 -0800

    change mock data module to support llava-next

--- nemo/collections/vlm/neva/data/mock.py
+++ nemo/collections/vlm/neva/data/mock.py
@@ -31,6 +31,7 @@ class MockDataModule(pl.LightningDataModule):
         seq_length: int = 2048,
         tokenizer: Optional = None,
         image_processor: Optional = None,
+        is_llava_next=False,
         micro_batch_size: int = 4,
         global_batch_size: int = 8,
         rampup_batch_size: Optional[List[int]] = None,
@@ -49,6 +50,7 @@ class MockDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
         self.pin_memory = pin_memory
         self.persistent_workers = persistent_workers
+        self.is_llava_next = is_llava_next
 
         if tokenizer is None or image_processor is None:
             from transformers import AutoProcessor
@@ -56,6 +58,9 @@ class MockDataModule(pl.LightningDataModule):
             processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
             self.tokenizer = tokenizer or processor.tokenizer
             self.image_processor = image_processor or processor.image_processor
+        else:
+            self.tokenizer = tokenizer
+            self.image_processor = image_processor
         self.data_sampler = MegatronDataSampler(
             seq_len=self.seq_length,
             micro_batch_size=micro_batch_size,
@@ -65,13 +70,13 @@ class MockDataModule(pl.LightningDataModule):
 
     def setup(self, stage: str = "") -> None:
         self._train_ds = _MockNevaDataset(
-            self.tokenizer, self.image_processor, "train", self.num_train_samples, self.seq_length
+            self.tokenizer, self.image_processor, self.is_llava_next, "train", self.num_train_samples, self.seq_length
         )
         self._validation_ds = _MockNevaDataset(
-            self.tokenizer, self.image_processor, "valid", self.num_val_samples, self.seq_length
+            self.tokenizer, self.image_processor, self.is_llava_next, "valid", self.num_val_samples, self.seq_length
         )
         self._test_ds = _MockNevaDataset(
-            self.tokenizer, self.image_processor, "test", self.num_test_samples, self.seq_length
+            self.tokenizer, self.image_processor, self.is_llava_next, "test", self.num_test_samples, self.seq_length
         )
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
@@ -105,6 +110,7 @@ class _MockNevaDataset(Dataset):
         self,
         tokenizer,
         image_processor,
+        is_llava_next,
         name: str,
         num_samples: int,
         seq_length: int,
@@ -124,6 +130,9 @@ class _MockNevaDataset(Dataset):
 
         self.loss_mask = torch.ones(self.seq_length, dtype=torch.float)
         self.position_ids = torch.arange(self.seq_length, dtype=torch.int64)
+        self.tokenizer = tokenizer
+        self.image_processor = image_processor
+        self.is_llava_next = is_llava_next
 
     def __len__(self) -> int:
         return self.length
@@ -141,13 +150,32 @@ class _MockNevaDataset(Dataset):
         images = torch.from_numpy(np_gen.random(size=[3, self.image_height, self.image_width], dtype=np.float32))
         tokens = tokens[:-1]
         labels = labels[1:]
-        return {
-            "media": images,
-            "tokens": tokens,
-            "labels": labels,
-            "loss_mask": self.loss_mask,
-            "position_ids": self.position_ids,
-        }
+        if not self.is_llava_next:
+            return {
+                "media": images,
+                "tokens": tokens,
+                "labels": labels,
+                "loss_mask": self.loss_mask,
+                "position_ids": self.position_ids,
+            }
+        else:
+            #  attention_mask, image_sizes, num_media_tiles required for llava-next. Neva model will ignore these
+            attention_mask = torch.ones(len(tokens), dtype=torch.long)
+            image_sizes = torch.tensor([[self.image_height, self.image_width]], dtype=torch.long)
+            image_array = self.image_processor.preprocess(images, return_tensors='pt', do_rescale=False)[
+                'pixel_values'
+            ][0]
+            num_media_tiles = image_array.shape[0]
+            return {
+                "media": image_array,
+                "tokens": tokens,
+                "labels": labels,
+                "loss_mask": self.loss_mask,
+                "position_ids": self.position_ids,
+                "image_sizes": image_sizes,
+                "num_media_tiles": num_media_tiles,
+                "attention_mask": attention_mask,
+            }
 
     def _collate_fn(self, batch):
         """
@@ -155,7 +183,13 @@ class _MockNevaDataset(Dataset):
         Users should override this method to define custom data loaders.
         """
         collated_batch = data.dataloader.default_collate(batch)
-        collated_batch["attention_mask"] = None
+        if not self.is_llava_next:
+            collated_batch["attention_mask"] = None
+        else:
+            collated_batch['media'] = collated_batch['media'].contiguous().view(-1, *collated_batch['media'].shape[2:])
+            collated_batch['image_sizes'] = (
+                collated_batch['image_sizes'].contiguous().view(-1, *collated_batch['image_sizes'].shape[2:])
+            )
         return collated_batch
 
     def collate_fn(self, batch):
