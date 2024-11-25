8749e5f8addcf1b2333bec4f608d99c759f35fea
Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>
2024-11-25 13:24:15 +0100

    Add sample generate to PTQ for NeMo 2.0 (#11339)
    * Initial commit
    
    Signed-off-by: Piotr Kaminski <pikaminski@nvidia.com>
    
    * Remove leftover print
    
    Signed-off-by: Piotr Kaminski <pikaminski@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: Laplasjan107 <Laplasjan107@users.noreply.github.com>
    
    * Fix docs and type annotations
    
    Signed-off-by: Piotr Kaminski <pikaminski@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: Laplasjan107 <Laplasjan107@users.noreply.github.com>
    
    * Applied code review suggestions
    
    Signed-off-by: Piotr Kaminski <pikaminski@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: Laplasjan107 <Laplasjan107@users.noreply.github.com>
    
    * Fix _get_decoder_type parameter
    
    Signed-off-by: Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Piotr Kaminski <pikaminski@nvidia.com>
    Signed-off-by: Laplasjan107 <Laplasjan107@users.noreply.github.com>
    Signed-off-by: Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>
    Co-authored-by: Piotr Kaminski <pikaminski@nvidia.com>
    Co-authored-by: Laplasjan107 <Laplasjan107@users.noreply.github.com>
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- nemo/collections/llm/quantization/utils.py
+++ nemo/collections/llm/quantization/utils.py
@@ -23,8 +23,33 @@ from nemo.lightning.ckpt_utils import ckpt_to_context_subdir
 from nemo.utils import logging
 
 
+def get_modelopt_decoder_type(model: llm.GPTModel) -> str:
+    """Infers the modelopt decoder type from GPTModel subclass."""
+    mapping = [
+        (llm.Baichuan2Model, "baichuan"),
+        (llm.ChatGLMModel, "chatglm"),
+        (llm.Gemma2Model, "gemma2"),
+        (llm.GemmaModel, "gemma"),
+        (llm.LlamaModel, "llama"),
+        (llm.MistralModel, "llama"),
+        (llm.MixtralModel, "llama"),
+        (llm.NemotronModel, "gptnext"),
+        (llm.Qwen2Model, "qwen"),
+        (llm.StarcoderModel, "gptnext"),
+        (llm.Starcoder2Model, "gptnext"),
+        (llm.Phi3Model, "phi3"),
+    ]
+
+    for config_class, decoder_type in mapping:
+        if isinstance(model, config_class):
+            return decoder_type
+
+    logging.warning("Could not infer the decoder type")
+    return None
+
+
 def quantizable_model_config(model_cfg: llm.GPTConfig) -> llm.GPTConfig:
-    """Modify model config for TensorRT Model Optimizer"""
+    """Modify model config for TensorRT-Model-Optimizer quantization"""
 
     from nemo.collections.nlp.models.language_modeling.megatron.gpt_layer_modelopt_spec import (
         get_gpt_layer_modelopt_spec,
@@ -46,7 +71,9 @@ def quantizable_model_config(model_cfg: llm.GPTConfig) -> llm.GPTConfig:
 def load_with_modelopt_layer_spec(
     nemo_checkpoint_path: str, calib_tp: int = 1, calib_pp: int = 1, inference_only: bool = True
 ):
-    # TODO: setting ddp="pytorch" with manually deleting model.optim is a hackish way to disable DDP initialization. Needs a systematic solution.
+    """Loads a model from a NeMo 2.0 checkpoint using modelopt layer spec."""
+    # TODO: setting ddp="pytorch" and deleting model.optim is a hackish way to disable DDP initialization.
+    # Needs a systematic solution.
     if inference_only:
         strategy = nl.MegatronStrategy(
             tensor_model_parallel_size=calib_tp,
@@ -81,6 +108,7 @@ def load_with_modelopt_layer_spec(
 
 
 def get_unwrapped_mcore_model(model):
+    """Unwraps NeMo 2.0 to base MCore model."""
     from megatron.core.models.gpt import GPTModel as MCoreGPTModel
 
     unwrapped_model = model
