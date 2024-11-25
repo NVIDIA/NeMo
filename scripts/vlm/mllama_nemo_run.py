d4604f6efdcf2522f013439479f4d290c52fdecc
yaoyu-33 <yaoyu-33@users.noreply.github.com>
2024-11-25 18:14:25 +0000

    Apply isort and black reformatting
    Signed-off-by: yaoyu-33 <yaoyu-33@users.noreply.github.com>

--- scripts/vlm/mllama_nemo_run.py
+++ scripts/vlm/mllama_nemo_run.py
@@ -5,7 +5,7 @@ from nemo.collections import llm, vlm
 
 def configure_recipe(nodes: int = 1, gpus_per_node: int = 1):
     recipe = vlm.mllama_11b.finetune_recipe(
-        dir="/checkpoints/mllama", # Path to store checkpoints
+        dir="/checkpoints/mllama",  # Path to store checkpoints
         name="mllama",
         num_nodes=nodes,
         num_gpus_per_node=gpus_per_node,
@@ -15,6 +15,7 @@ def configure_recipe(nodes: int = 1, gpus_per_node: int = 1):
     recipe.trainer.val_check_interval = 100
     return recipe
 
+
 def local_executor_torchrun(nodes: int = 1, devices: int = 1) -> run.LocalExecutor:
     # Env vars for jobs are configured here
     env_vars = {}
@@ -23,11 +24,13 @@ def local_executor_torchrun(nodes: int = 1, devices: int = 1) -> run.LocalExecut
 
     return executor
 
+
 def run_training():
     recipe = configure_recipe()
     executor = local_executor_torchrun(nodes=recipe.trainer.num_nodes, devices=recipe.trainer.devices)
 
     run.run(recipe, executor=executor)
+
 
 # This condition is necessary for the script to be compatible with Python's multiprocessing module.
 if __name__ == "__main__":
--- scripts/vlm/neva_nemo_run.py
+++ scripts/vlm/neva_nemo_run.py
@@ -5,7 +5,7 @@ from nemo.collections import llm, vlm
 
 def configure_recipe(nodes: int = 1, gpus_per_node: int = 8):
     recipe = vlm.llava15_7b.finetune_recipe(
-        dir="/checkpoints/llava", # Path to store checkpoints
+        dir="/checkpoints/llava",  # Path to store checkpoints
         name="llava_ft",
         num_nodes=nodes,
         num_gpus_per_node=gpus_per_node,
@@ -16,6 +16,7 @@ def configure_recipe(nodes: int = 1, gpus_per_node: int = 8):
     recipe.model.config.freeze_vision_model = True
     return recipe
 
+
 def local_executor_torchrun(nodes: int = 1, devices: int = 8) -> run.LocalExecutor:
     # Env vars for jobs are configured here
     env_vars = {}
@@ -24,11 +25,13 @@ def local_executor_torchrun(nodes: int = 1, devices: int = 8) -> run.LocalExecut
 
     return executor
 
+
 def run_training():
     recipe = configure_recipe()
     executor = local_executor_torchrun(nodes=recipe.trainer.num_nodes, devices=recipe.trainer.devices)
 
     run.run(recipe, executor=executor)
+
 
 # This condition is necessary for the script to be compatible with Python's multiprocessing module.
 if __name__ == "__main__":
