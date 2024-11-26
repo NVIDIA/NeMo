2bd5ce786a9377517cb60b7e7905dd28ba8beb4f
stevehuang52 <heh@nvidia.com>
2024-11-26 16:29:23 -0500

    update prompt format fn
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- nemo/collections/common/prompts/llama.py
+++ nemo/collections/common/prompts/llama.py
@@ -12,7 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+from collections import defaultdict
+
+from lhotse import CutSet
+from lhotse.cut import MixedCut
+from lhotse.utils import ifnone
+
+from nemo.collections.common.prompts import registered_prompt_format_fn
 from nemo.collections.common.prompts.formatter import BOS_SLOT, EOS_SLOT, Modality, PromptFormatter
+from nemo.collections.common.tokenizers import TokenizerSpec
 
 
 class Llama2PromptFormatter(PromptFormatter):
@@ -32,7 +41,7 @@ class Llama2PromptFormatter(PromptFormatter):
             },
         },
         "user": {
-            "template": "|bos|[INST] |message| [/INST]",
+            "template": f"{BOS_SLOT}[INST] |message| [/INST]",
             "slots": {
                 "message": Modality.Text,
             },
@@ -44,6 +53,34 @@ class Llama2PromptFormatter(PromptFormatter):
             },
         },
     }
+
+
+@registered_prompt_format_fn
+def llama2(cuts: CutSet, tokenizer: TokenizerSpec):
+    prompt = Llama2PromptFormatter(tokenizer)
+    ans = defaultdict(list)
+    for cut in cuts:
+        if isinstance(cut, MixedCut):
+            cut = cut.first_non_padding_cut
+        if cut.has_custom("context"):
+            context = cut.context
+        elif cut.has_custom("question"):
+            context = cut.question
+        else:
+            context = cut.default_context
+
+        turns = []
+        if cut.has_custom("system_prompt"):
+            turns.append({"role": "system_and_user", "slots": {"system": cut.system_prompt, "message": context}})
+        else:
+            turns.append({"role": "user", "slots": {"message": context}})
+        if (answer := cut.supervisions[0].text) is not None:
+            turns.append({"role": "assistant", "slots": {"message": answer}})
+
+        for k, v in prompt.encode_dialog(turns).items():
+            ans[k].append(v)
+
+    return ans
 
 
 LLAMA3_BOS = "<|begin_of_text|>"
