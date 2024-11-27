b60b48201ac96d76e78157a8d776ee5243c1a3e4
Yashaswi Karnati <144376261+yashaswikarnati@users.noreply.github.com>
2024-11-26 17:02:36 -0800

    data modules for llava_next (#11400)
    * data modules for llava_next
    * added doc strings for mock dataset
    * remove llava next energon dataloader from neva folder
    Signed-off-by: ykarnati <ykarnati@nvidia.com>
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- nemo/collections/vlm/llava_next/data/__init__.py
+++ nemo/collections/vlm/llava_next/data/__init__.py
@@ -11,19 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from nemo.collections.vlm.neva.data.config import DataConfig, ImageDataConfig, VideoDataConfig
-from nemo.collections.vlm.neva.data.lazy import NevaLazyDataModule
-from nemo.collections.vlm.neva.data.mock import MockDataModule as NevaMockDataModule
-from nemo.collections.vlm.neva.data.multimodal_tokens import ImageToken, MultiModalToken, VideoToken
-
-__all__ = [
-    "NevaLazyDataModule",
-    "NevaMockDataModule",
-    "DataConfig",
-    "ImageDataConfig",
-    "VideoDataConfig",
-    "MultiModalToken",
-    "ImageToken",
-    "VideoToken",
-]
