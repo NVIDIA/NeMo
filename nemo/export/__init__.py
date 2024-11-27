bf41530ba3a092eeafca3c17d7ace4beb5754c0f
Boris Fomitchev <borisfom@users.noreply.github.com>
2024-11-27 02:21:30 -0800

    Introducing TensorRT lazy export and caching option with trt_compile()  (#11266)
    * Introducing trt_compile()
    
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>
    
    * Adding tests for dynamic axes
    
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>
    
    * Fixed folding constants and style check
    
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>
    
    ---------
    
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- nemo/export/__init__.py
+++ nemo/export/__init__.py
@@ -11,3 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from nemo.export.tensorrt_lazy_compiler import trt_compile
