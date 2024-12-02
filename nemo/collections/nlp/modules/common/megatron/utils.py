1bc0210efe1f1761701093bf28cbab66252a8aa0
stevehuang52 <heh@nvidia.com>
2024-12-02 16:45:33 -0500

    add debug info
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/utils.py
+++ nemo/collections/nlp/modules/common/megatron/utils.py
@@ -436,6 +436,7 @@ def get_iterator_k_split(
                 f"Only support splitting torch.Tensor and List[torch.Tensor]. Discarding the following keys from the batch: {discard_items}",
                 mode=logging_mode.ONCE,
             )
+        batch_origin = batch
 
         batch = {k: v for k, v in batch.items() if isinstance(v, (torch.Tensor, list))}
         tensor_items = {k: v for k, v in batch.items() if isinstance(v, torch.Tensor)}
@@ -443,6 +444,12 @@ def get_iterator_k_split(
 
         # Split tensor items
         items = list(tensor_items.items())
+
+        if len(items) == 0:
+            raise RuntimeError(
+                f"No torch.Tensor or list in the batch: {[(k,type(v)) for k,v in batch_origin.items()]}"
+            )
+
         if enforce_divisible_batch:
             if items[0][1].shape[0] % num_microbatches != 0:
                 raise ValueError(
