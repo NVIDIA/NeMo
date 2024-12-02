0217ba50e455618207514ce1db4aadb68fa1d0c7
ashors1 <ashors@nvidia.com>
2024-12-02 10:30:45 -0800

    add missing import
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- tests/lightning/test_strategy_lib.py
+++ tests/lightning/test_strategy_lib.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pytest
 from unittest.mock import ANY, MagicMock, patch
 
 import torch
