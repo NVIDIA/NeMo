2580848045ee5f365151006405b321cfd54fa021
ashors1 <ashors1@users.noreply.github.com>
2024-12-02 18:32:10 +0000

    Apply isort and black reformatting
    Signed-off-by: ashors1 <ashors1@users.noreply.github.com>

--- tests/lightning/test_strategy_lib.py
+++ tests/lightning/test_strategy_lib.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 from unittest.mock import ANY, MagicMock, patch
 
+import pytest
 import torch
 from torch import nn
 
