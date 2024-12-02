14e1ca4a36d57ed5e033ffab2057a8fbb62b5dca
Hemil Desai <hemild@nvidia.com>
2024-12-02 14:14:22 -0800

    Add option to provide prior NeMo 2 ckpt path to convert_nemo1_to_nemo2 script
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- scripts/llm/generate.py
+++ scripts/llm/generate.py
@@ -14,6 +14,7 @@
 
 # NOTE: This script is just an example of using NeMo checkpoints for generating outputs and is subject to change without notice.
 
+from argparse import ArgumentParser
 import os
 
 import torch
@@ -23,10 +24,71 @@ from megatron.core.inference.common_inference_params import CommonInferenceParam
 import nemo.lightning as nl
 from nemo.collections.llm import api
 
+def get_args():
+    """
+    Parse the command line arguments.
+    """
+    parser = ArgumentParser(
+        description="""Run generation on a few sample prompts given the checkpoint path."""
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        required=True,
+        help="""Path to NeMo 2 checkpoint""",
+    )
+    parser.add_argument(
+        "--tp",
+        type=int,
+        default=1,
+        help="""Tensor parallel size""",
+    )
+    parser.add_argument(
+        "--pp",
+        type=int,
+        default=1,
+        help="""Pipeline parallel size""",
+    )
+    parser.add_argument(
+        "--devices",
+        type=int,
+        default=1,
+        help="""Number of GPUs to use on a single node""",
+    )
+    parser.add_argument(
+        "--nodes",
+        type=int,
+        default=1,
+        help="""Number of nodes to use""",
+    )
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=1.0,
+        help="""Temperature to be used in megatron.core.inference.common_inference_params.CommonInferenceParams""",
+    )
+    parser.add_argument(
+        "--top_p",
+        type=float,
+        default=0.95,
+        help="""top_p to be used in megatron.core.inference.common_inference_params.CommonInferenceParams""",
+    )
+    parser.add_argument(
+        "--num_tokens_to_generate",
+        type=int,
+        default=4,
+        help="""Number of tokens to generate per prompt""",
+    )
+
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
+    args = get_args()
+
     strategy = nl.MegatronStrategy(
-        tensor_model_parallel_size=2,
-        pipeline_model_parallel_size=1,
+        tensor_model_parallel_size=args.tp,
+        pipeline_model_parallel_size=args.pp,
         context_parallel_size=1,
         sequence_parallel=False,
         setup_optimizers=False,
@@ -35,8 +97,8 @@ if __name__ == "__main__":
 
     trainer = nl.Trainer(
         accelerator="gpu",
-        devices=2,
-        num_nodes=1,
+        devices=args.devices,
+        num_nodes=args.nodes,
         strategy=strategy,
         plugins=nl.MegatronMixedPrecision(
             precision="bf16-mixed",
@@ -52,10 +114,10 @@ if __name__ == "__main__":
         "Which number is bigger? 10.119 or 10.19?",
     ]
     results = api.generate(
-        path=os.path.join(os.environ["NEMO_HOME"], "models", "meta-llama/Meta-Llama-3-8B"),
+        path=args.model_path,
         prompts=prompts,
         trainer=trainer,
-        inference_params=CommonInferenceParams(temperature=0.1, top_k=10, num_tokens_to_generate=512),
+        inference_params=CommonInferenceParams(temperature=args.temperature, top_p=args.top_p, num_tokens_to_generate=args.num_tokens_to_generate),
         text_only=True,
     )
     if torch.distributed.get_rank() == 0:
