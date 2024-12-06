5879f9c98db6ea583037566d0f35d5ae4378b896
ashors1 <ashors@nvidia.com>
2024-12-06 11:46:17 -0800

    fix
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- nemo/lightning/pytorch/strategies/megatron_strategy.py
+++ nemo/lightning/pytorch/strategies/megatron_strategy.py
@@ -812,7 +812,9 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
     @override
     def checkpoint_io(self) -> CheckpointIO:
         """Creates & returns checkpoint io"""
-        use_optim_separation_hint = getattr(self.trainer.checkpoint_callback, "save_last_n_optim_states", -1) > 0
+        use_optim_separation_hint = False
+        if hasattr(self, trainer):
+            use_optim_separation_hint = getattr(self.trainer.checkpoint_callback, "save_last_n_optim_states", -1) > 0
 
         if not self._checkpoint_io:
             self._checkpoint_io = create_checkpoint_io(
