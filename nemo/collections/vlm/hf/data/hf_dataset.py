802c4f6973d8aad091969ae7dce9a3c23538a1be
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-12-06 17:05:10 -0800

    vlm.HFDatasetDataModule pass collate_fn as argument
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/vlm/hf/data/hf_dataset.py
+++ nemo/collections/vlm/hf/data/hf_dataset.py
@@ -97,6 +97,7 @@ class HFDatasetDataModule(pl.LightningDataModule):
     def __init__(
         self,
         path,
+        collate_fn,
         split=None,
         num_workers=2,
         pin_memory=True,
@@ -129,6 +130,8 @@ class HFDatasetDataModule(pl.LightningDataModule):
         # self.dataset_splits will hold the actual dataset for each split.
         self.dataset_splits = make_dataset_splits(path, split, split_aliases, kwargs)
 
+        self._collate_fn = collate_fn
+
         self.num_workers = num_workers
         self.pin_memory = pin_memory
         self.persistent_workers = persistent_workers
@@ -140,40 +143,9 @@ class HFDatasetDataModule(pl.LightningDataModule):
         self.use_mcore_sampler = use_mcore_sampler
         self.mcore_dataloader_type = mcore_dataloader_type
 
-
-    @staticmethod
-    def collate_fn(batch, pad_token_id=0):
-        def batchify(tensor):
-            if tensor.ndim == 1:
-                return tensor.unsqueeze_(0)
-            return tensor
-
-        def extract_key_from_dicts(batch, key):
-            return list(map(lambda x: x[key], batch))
-
-        def pad_within_micro(batch, pad_token_id):
-            max_len = max(map(len, batch))
-            return [item + [pad_token_id] * (max_len - len(item)) for item in batch]
-
-        keys = list(filter(lambda x: x in batch[0], ['tokens', 'labels', 'position_ids', 'loss_mask']))
-        return {
-            key: batchify(
-                torch.LongTensor(
-                    pad_within_micro(
-                        extract_key_from_dicts(batch, key),
-                        pad_token_id,
-                    )
-                )
-            )
-            for key in keys
-        }
-
-    def _make_dataloader(self, dataset, collate_fn=None):
+    def _make_dataloader(self, dataset, collate_fn):
         assert dataset is not None
-
-        if collate_fn is None:
-            collate_fn = lambda x: HFDatasetDataModule.collate_fn(x, pad_token_id=self.pad_token_id)
-
+        assert collate_fn is not None
         return DataLoader(
             dataset,
             num_workers=self.num_workers,
@@ -184,12 +156,18 @@ class HFDatasetDataModule(pl.LightningDataModule):
         )
 
     def train_dataloader(self, collate_fn=None):
+        if collate_fn is None:
+            collate_fn = self._collate_fn
         return self._make_dataloader(self.dataset_splits['train'], collate_fn)
 
     def val_dataloader(self, collate_fn=None):
+        if collate_fn is None:
+            collate_fn = self._collate_fn
         return self._make_dataloader(self.dataset_splits['val'], collate_fn)
 
     def test_dataloader(self, collate_fn=None):
+        if collate_fn is None:
+            collate_fn = self._collate_fn
         return self._make_dataloader(self.dataset_splits['test'], collate_fn)
 
     def map(self, function=None, split_names=None, **kwargs):
