19c4da4d3d8db12975165eb9a466a791ea1449b6
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-12-10 13:07:24 -0800

    bot happy2
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/llm/gpt/data/test_hf_datamodule.py
+++ tests/collections/llm/gpt/data/test_hf_datamodule.py
@@ -12,11 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
-import nemo.lightning as nl
 from nemo.collections import llm
-
 
 DATA_PATH = "/home/TestData/lite/hf_cache/squad/"
 
