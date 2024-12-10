93b7997985afb44c2b70d7725a828183ee50b86f
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-12-10 11:19:51 -0800

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/llm/hf/peft.py
+++ tests/collections/llm/hf/peft.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     llm.api.finetune(
         model=llm.HFAutoModelForCausalLM(args.model),
         data=llm.HFDatasetDataModule(
-            mk_hf_dataset(tokenizer.tokenizer), pad_token_id=tokenizer.tokenizer.eos_token_id
+            make_dummy_dataset(tokenizer.tokenizer), pad_token_id=tokenizer.tokenizer.eos_token_id
         ),
         trainer=nl.Trainer(
             devices=args.devices,
