5c8c036f75f9c837d26300d1df525e285863ce58
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-12-12 13:31:58 -0800

    update hf_dataset
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/vlm/hf/data/hf_dataset.py
+++ nemo/collections/vlm/hf/data/hf_dataset.py
@@ -12,54 +12,65 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import datasets.dataset_dict
 import lightning.pytorch as pl
-from datasets import load_dataset
+import torch
+from datasets import Dataset, DatasetDict, load_dataset
 from torch.utils.data import DataLoader
 
+from nemo.lightning.pytorch.plugins import MegatronDataSampler
 from nemo.utils import logging
 
 
-def make_dataset_splits(path, split, split_aliases, kwargs):
+def clean_split(name):
+    if '[' in name:
+        return name.split('[')[0]
+    return name
+
+
+def make_dataset_splits(dataset, split, split_aliases):
     """
-    Loads a dataset with datasets.load_dataset and
-    returns a dictionary containing all dataset splits.
+    Given a dataset (e.g. from datasets.load_dataset or datasets.Dataset.from_dict) it
+    returns a dictionary containing the corresponding dataset splits.
 
     For example:
 
-    ans = make_dataset_splits("dataset-id")
-        $ ds = load_dataset("dataset-id")
-        $ print(ds)
-        > DatasetDict({
-        >    train: Dataset({
-        >        features: ['id', 'title', 'context', 'question', 'answers'],
-        >        num_rows: 87599
-        >    })
-        >    validation: Dataset({
-        >        features: ['id', 'title', 'context', 'question', 'answers'],
-        >        num_rows: 10570
-        >    })
-        > })
+    $ ds = load_dataset("dataset-id")
+    $ ans = make_dataset_splits(ds)
 
-    In this case the value of `ans` (returned value) will be:
+    # `ds` contains the following
+    $ print(ds)
+    > DatasetDict({
+    >    train: Dataset({
+    >        features: ['id', 'title', 'context', 'question', 'answers'],
+    >        num_rows: 87599
+    >    })
+    >    validation: Dataset({
+    >        features: ['id', 'title', 'context', 'question', 'answers'],
+    >        num_rows: 10570
+    >    })
+    > })
+
+    # In this case the value of `ans` (returned value) will be:
     $ print(ans)
     > {
     >    "train": Dataset .. (with 87599 rows),
     >    "val": Dataset .. (with 10570 rows),
     > }
     """
-    dataset = load_dataset(path, split=split, **kwargs)
-
-    split_names = ['train', 'test', 'val']
-    dataset_splits = {split: None for split in split_names}
+    valid_split_names = ['train', 'test', 'val']
+    dataset_splits = {_split: None for _split in valid_split_names}
 
     alias_to_split = {}
     for split_name, _split_aliases in split_aliases.items():
-        assert split_name in split_names
+        assert split_name in valid_split_names
         for alias in _split_aliases:
             alias_to_split[alias] = split_name
 
-    if isinstance(dataset, datasets.dataset_dict.DatasetDict):
+    if isinstance(dataset, Dataset):
+        assert isinstance(split, str), "Expected split to be a string, but got " + str(type(split))
+        split = clean_split(split)
+        dataset_splits[split] = dataset
+    elif isinstance(dataset, DatasetDict):
         dataset_split_names = dataset.keys()
         logging.info(f"HF dataset has the following splits: {dataset_split_names}")
         for alias_split_name, split in dataset.items():
@@ -69,7 +80,7 @@ def make_dataset_splits(path, split, split_aliases, kwargs):
     elif isinstance(split, list):
         logging.info(f"Loaded HF dataset will use " + str(split) + " splits.")
         assert isinstance(dataset, list)
-        for i, alias_split_name in enumerate(split):
+        for i, alias_split_name in enumerate(map(clean_split, split)):
             split_name = alias_to_split[alias_split_name]
             assert dataset_splits[split_name] is None
             dataset_splits[split_name] = dataset[i]
@@ -87,18 +98,31 @@ def make_dataset_splits(path, split, split_aliases, kwargs):
     else:
         raise ValueError("Expected split name to be None, str or a list")
 
-    assert (
-        sum(map(lambda x: x is not None, dataset_splits.values())) > 0
-    ), "Expected at least one dataset to have been initialized"
+    assert set(valid_split_names) == set(dataset_splits.keys()), dataset_splits.keys()
+    num_init_splits = sum(map(lambda x: x is not None, dataset_splits.values()))
+    assert num_init_splits > 0, f"Expected at least one split to have been initialized {num_init_splits}"
     return dataset_splits
 
 
 class HFDatasetDataModule(pl.LightningDataModule):
+    """HFDatasetDataModule wraps HF's load_dataset (datasets library)
+    so that it can be used within NeMo.
+    Users can select whether to use an mcore-sampler via use_mcore_sampler arg.
+
+    Usage examples:
+
+    - loading a single split (train) from a dataset
+    llm.HFDatasetDataModule("rajpurkar/squad", split="train")
+
+    - loading multiple splits (train, validation) from a dataset
+    llm.HFDatasetDataModule("rajpurkar/squad", split=["train", "validation"])
+    """
+
     def __init__(
         self,
-        path,
-        collate_fn,
+        path_or_dataset,
         split=None,
+        collate_fn=None,
         num_workers=2,
         pin_memory=True,
         persistent_workers=True,
@@ -115,18 +139,29 @@ class HFDatasetDataModule(pl.LightningDataModule):
     ) -> None:
         super().__init__()
         assert pad_token_id is not None
-
-        logging.info(f"Loading HF dataset from {path}")
-
         # A dataset usually will have several splits (e.g. train, val, test, etc).
         # We map synonym names to canonical names (train, test, val).
         # A synonym can be a prefix/suffixed word e.g. train <> training.
         split_aliases = {'train': train_aliases, 'test': test_aliases, 'val': val_aliases}
 
         # self.dataset_splits will hold the actual dataset for each split.
-        self.dataset_splits = make_dataset_splits(path, split, split_aliases, kwargs)
+        if isinstance(path_or_dataset, str):
+            logging.info(f"Loading HF dataset from {path_or_dataset}")
+            dataset = load_dataset(path_or_dataset, split=split, **kwargs)
+        elif isinstance(path_or_dataset, Dataset) or isinstance(path_or_dataset, DatasetDict):
+            logging.info(f"Using passed HF dataset {str(path_or_dataset)}")
+            dataset = path_or_dataset
+        else:
+            raise ValueError(
+                "Expected `path_or_dataset` to be str, Dataset, DatasetDict, but got " + str(type(path_or_dataset))
+            )
 
-        self._collate_fn = collate_fn
+        self.dataset_splits = make_dataset_splits(dataset, split, split_aliases)
+
+        if collate_fn is None:
+            self._collate_fn = lambda x: HFDatasetDataModule.collate_fn(x, pad_token_id=self.pad_token_id)
+        else:
+            self._collate_fn = collate_fn
 
         self.num_workers = num_workers
         self.pin_memory = pin_memory
@@ -139,9 +174,53 @@ class HFDatasetDataModule(pl.LightningDataModule):
         self.use_mcore_sampler = use_mcore_sampler
         self.mcore_dataloader_type = mcore_dataloader_type
 
-    def _make_dataloader(self, dataset, collate_fn):
+    @staticmethod
+    def from_dict(dataset_dict, split, **kwargs):
+        dataset = Dataset.from_dict(dataset_dict)
+        return HFDatasetDataModule(path_or_dataset=dataset, split=split, **kwargs)
+
+    @staticmethod
+    def collate_fn(batch, pad_token_id=0):
+        def batchify(tensor):
+            if tensor.ndim == 1:
+                return tensor.unsqueeze_(0)
+            return tensor
+
+        def extract_key_from_dicts(batch, key):
+            return list(map(lambda x: x[key], batch))
+
+        def pad_within_micro(batch, pad_token_id):
+            max_len = max(map(len, batch))
+            return [item + [pad_token_id] * (max_len - len(item)) for item in batch]
+
+        return {
+            key: batchify(
+                torch.LongTensor(
+                    pad_within_micro(
+                        extract_key_from_dicts(batch, key),
+                        pad_token_id,
+                    )
+                )
+            )
+            for key in batch[0].keys()
+        }
+
+    def setup(self, stage: str):
+        if not self.use_mcore_sampler:
+            return
+        self.data_sampler = MegatronDataSampler(
+            seq_len=self.seq_length,
+            micro_batch_size=self.micro_batch_size,
+            global_batch_size=self.global_batch_size,
+            dataloader_type=self.mcore_dataloader_type,
+        )
+
+    def _make_dataloader(self, dataset, collate_fn=None):
         assert dataset is not None
-        assert collate_fn is not None
+
+        if collate_fn is None:
+            collate_fn = lambda x: HFDatasetDataModule.collate_fn(x, pad_token_id=self.pad_token_id)
+
         return DataLoader(
             dataset,
             num_workers=self.num_workers,
@@ -151,20 +230,26 @@ class HFDatasetDataModule(pl.LightningDataModule):
             batch_size=self.micro_batch_size,
         )
 
-    def train_dataloader(self, collate_fn=None):
-        if collate_fn is None:
-            collate_fn = self._collate_fn
-        return self._make_dataloader(self.dataset_splits['train'], collate_fn)
+    @property
+    def train(self):
+        return self.dataset_splits['train']
 
-    def val_dataloader(self, collate_fn=None):
-        if collate_fn is None:
-            collate_fn = self._collate_fn
-        return self._make_dataloader(self.dataset_splits['val'], collate_fn)
+    @property
+    def val(self):
+        return self.dataset_splits['val']
 
-    def test_dataloader(self, collate_fn=None):
-        if collate_fn is None:
-            collate_fn = self._collate_fn
-        return self._make_dataloader(self.dataset_splits['test'], collate_fn)
+    @property
+    def test(self):
+        return self.dataset_splits['test']
+
+    def train_dataloader(self):
+        return self._make_dataloader(self.train, self._collate_fn)
+
+    def val_dataloader(self):
+        return self._make_dataloader(self.val, self._collate_fn)
+
+    def test_dataloader(self):
+        return self._make_dataloader(self.test, self._collate_fn)
 
     def map(self, function=None, split_names=None, **kwargs):
         if isinstance(split_names, str):
