0347d7189ae70aabb2286a64d7ca36c9cd4e8647
artbataev <artbataev@users.noreply.github.com>
2024-12-13 16:44:23 +0000

    Apply isort and black reformatting
    Signed-off-by: artbataev <artbataev@users.noreply.github.com>

--- nemo/core/utils/optional_libs.py
+++ nemo/core/utils/optional_libs.py
@@ -28,6 +28,7 @@ TRITON_AVAILABLE = is_lib_available("triton")
 
 try:
     from nemo.core.utils.k2_guard import k2
+
     K2_AVAILABLE = True
 except (ImportError, ModuleNotFoundError):
     K2_AVAILABLE = False
