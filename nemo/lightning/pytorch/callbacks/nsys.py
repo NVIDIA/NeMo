5aa99046938552d7f599d939d96c72cbb6e13cdf
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-12-13 00:32:53 -0800

    fix for SingleDeviceStrategy
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/callbacks/nsys.py
+++ nemo/lightning/pytorch/callbacks/nsys.py
@@ -67,11 +67,22 @@ class NsysCallback(Callback):
             f'and end_step: {self._nsys_profile_end_step}'
         )
 
+    def _rank_is_active(self, trainer):
+        # TODO(@akoumparouli): investigate whether it can be cached.
+        from lightning.pytorch.strategies import SingleDeviceStrategy
+        if isinstance(trainer.strategy, SingleDeviceStrategy):
+            return True
+        if not torch.distributed.is_initialized():
+            return True
+        return get_rank() in self._nsys_profile_ranks
+
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx: int) -> Optional[int]:
         """PyTorch Lightning hook:
         https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#on-train-batch-start
         We use it here to enable nsys profiling.
         """
+        if not self._rank_is_active(trainer)
+            return
 
         device = trainer.strategy.root_device
         try:
@@ -80,8 +91,9 @@ class NsysCallback(Callback):
             current_step = trainer.strategy.current_epoch_step
         except AttributeError:
             current_step = self._nsys_profile_start_step
+
         if device.type == 'cuda':
-            if current_step == self._nsys_profile_start_step and get_rank() in self._nsys_profile_ranks:
+            if current_step == self._nsys_profile_start_step:
                 torch.cuda.cudart().cudaProfilerStart()
                 if self._nsys_profile_gen_shape:
                     torch.autograd.profiler.emit_nvtx(record_shapes=True).__enter__()
@@ -93,6 +105,8 @@ class NsysCallback(Callback):
         https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#on-train-batch-end
         We use it here to enable nsys profiling.
         """
+        if not self._rank_is_active(trainer)
+            return
 
         device = trainer.strategy.root_device
         try:
@@ -100,6 +114,6 @@ class NsysCallback(Callback):
         except AttributeError:
             current_step = self._nsys_profile_end_step
         if device.type == 'cuda':
-            if current_step == self._nsys_profile_end_step and get_rank() in self._nsys_profile_ranks:
+            if current_step == self._nsys_profile_end_step:
                 torch.cuda.cudart().cudaProfilerStop()
                 torch.autograd.profiler.emit_nvtx().__exit__(None, None, None)
