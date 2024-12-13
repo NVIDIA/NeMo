fbeaa336fd1a686effb015f5d9ff4bd92d54b8c5
linnan wang <linnanw@nvidia.com>
2024-12-12 16:15:35 -0800

    udpate

--- nemo/collections/diffusion/vae/train_vae.py
+++ nemo/collections/diffusion/vae/train_vae.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from typing import Any, Callable, Dict, Optional, Sequence, Tuple
 
 import nemo_run as run
