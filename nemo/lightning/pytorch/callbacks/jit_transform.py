38f3aa194691385d5ad28eea800534dc5411c827
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-12-12 18:14:20 -0800

    remove optional
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/callbacks/jit_transform.py
+++ nemo/lightning/pytorch/callbacks/jit_transform.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional
-
 import torch
 from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.trainer.trainer import Trainer
