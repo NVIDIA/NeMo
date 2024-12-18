0c5d512c8c3657d908284a5182b4f156abb08681
Marc Romeyn <mromeijn@nvidia.com>
2024-12-18 13:24:52 +0100

    Adding docs
    Signed-off-by: Marc Romeyn <mromeijn@nvidia.com>

--- nemo/lightning/io/artifact/hf_auto.py
+++ nemo/lightning/io/artifact/hf_auto.py
@@ -1,5 +1,43 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""HuggingFace model serialization support for NeMo's configuration system.
+
+This module provides integration between NeMo's configuration system and HuggingFace's
+pretrained models. It enables automatic serialization and deserialization of HuggingFace
+models within NeMo's configuration framework.
+
+The integration works by:
+1. Detecting HuggingFace models through their characteristic methods (save_pretrained/from_pretrained)
+2. Converting them to Fiddle configurations that preserve the model's class and path
+3. Providing an artifact handler (HFAutoArtifact) that manages the actual model files
+
+Example:
+    ```python
+    from transformers import AutoModel
+    
+    # This model will be automatically handled by the HFAutoArtifact system
+    model = AutoModel.from_pretrained("bert-base-uncased")
+    
+    # When serialized, the model files will be saved to the artifacts directory
+    # When deserialized, the model will be loaded from the saved files
+    ```
+"""
+
 import inspect
-from typing import Path
+from pathlib import Path
 
 import fiddle as fdl
 
