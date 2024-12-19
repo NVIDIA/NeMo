3f4212cd0fbcb104335e0e84f1c68fca686547d6
Chen Cui <chcui@nvidia.com>
2024-12-19 10:49:13 -0800

    remove nemo1 docs
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- nemo/collections/llm/gpt/data/fine_tuning.py
+++ nemo/collections/llm/gpt/data/fine_tuning.py
@@ -110,8 +110,8 @@ class FineTuningDataModule(pl.LightningDataModule):
                 f"Set packed sequence length to {self.packed_sequence_size*self.micro_batch_size} "
                 f"(currently {self.packed_sequence_size}) \n"
                 f"For details please visit "
-                f"https://docs.nvidia.com/nemo-framework/user-guide/latest/nemotoolkit/features/optimizations/"
-                f"sequence_packing.html"
+                f"https://nemo-framework-tme.gitlab-master-pages.nvidia.com/documentation/user-guide/latest/"
+                f"sft_peft/packed_sequence.html"
             )
 
     def prepare_data(self) -> None:
