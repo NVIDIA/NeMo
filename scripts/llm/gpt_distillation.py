4b426db5c50d181a9cd909d74526f3c9579eb294
Asha Anoosheh <aanoosheh@nvidia.com>
2024-12-25 08:33:02 -0800

    First draft of distill script port to 2.0
    Signed-off-by: Asha Anoosheh <aanoosheh@nvidia.com>

---
+++ scripts/llm/gpt_distillation.py
@@ -0,0 +1,491 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import types
+from abc import ABCMeta
+from contextlib import contextmanager
+from dataclasses import dataclass
+from typing import Any, Dict, Optional, Tuple
+
+import modelopt.torch.distill as mtd
+import modelopt.torch.opt as mto
+import torch
+import torch.nn.functional as F
+from megatron.core.dist_checkpointing.mapping import ShardedStateDict
+from megatron.core.models.gpt.gpt_model import GPTModel as MCoreGPTModel
+from megatron.core.optimizer import OptimizerConfig
+from megatron.core.parallel_state import (
+    get_context_parallel_group,
+    get_context_parallel_world_size,
+    get_tensor_model_parallel_group,
+)
+from megatron.core.transformer.transformer_config import TransformerConfig
+from torch import Tensor
+from torch.nn.modules.loss import _Loss
+
+from nemo import lightning as nl
+from nemo.collections import llm
+from nemo.collections.llm.quantization import load_with_modelopt_layer_spec
+from nemo.collections.nlp.models.language_modeling.megatron.gpt_layer_modelopt_spec import get_gpt_layer_modelopt_spec
+from nemo.collections.nlp.modules.common.megatron.utils import average_losses_across_data_parallel_group
+from nemo.lightning.megatron_parallel import MaskedTokenLossReduction
+from nemo.utils import logging
+
+
+@dataclass
+class DistillationGPTConfig(llm.GPTConfig):
+    kd_teacher_restore_from_path: str = ""  # default set only for dataclass inheritance
+
+    def configure_model(self, *args, **kwargs) -> MCoreGPTModel:
+        if not self.kd_teacher_restore_from_path:
+            raise ValueError("Config attribute `kd_teacher_restore_from_path` must be set.")
+
+        model = super().configure_model(*args, **kwargs)
+
+        # [ModelOpt] Intialize DistillationModel.
+        distill_cfg = load_distillation_config(self)
+        kd_config = {
+            "teacher_model": (_teacher_provider, [], {"cfg": self}),
+            "criterion": distill_cfg["criterion"],
+            "loss_balancer": distill_cfg["loss_balancer"],
+        }
+        model = mtd.convert(model, mode=[("kd_loss", kd_config)])
+
+        # Additional MCore-specific tweaks needed.
+        adjust_distillation_model_for_mcore(model, model_cfg=self, distill_cfg=distill_cfg)
+
+        return model
+
+
+class _DistillationLossReduction(MaskedTokenLossReduction):
+    """Custom masking and reduction callable used only in training mode."""
+
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._distillation_model: mtd.DistillationModel = model.module
+        self._cp_size = get_context_parallel_world_size()
+
+    def forward(self, batch: Dict[str, Tensor], forward_out: Tensor) -> Tuple[Tensor, Dict[str, Tensor]]:
+        if isinstance(forward_out, tuple):
+            # neva returns (logits, loss_mask)
+            forward_out, batch["loss_mask"] = forward_out
+
+        # [ModelOpt]: KD loss calculation.
+        loss_for_ub = self._distillation_model.compute_kd_loss(
+            loss_reduction_fn=lambda x: self._masked_token_loss(x, batch["loss_mask"], batch['num_valid_tokens_in_ub'])
+        )
+
+        reduced_loss = average_losses_across_data_parallel_group([loss_for_ub])
+        return loss_for_ub * self._cp_size, {"avg": reduced_loss}
+
+    def _masked_token_loss(self, loss_output: Tensor, mask: Tensor, num_valid_tokens_in_ub: Optional[int] = None):
+        """
+        The function takes as input per-token loss and masks non-required values.
+        """
+        if isinstance(loss_output, tuple):
+            # [ModelOpt]: Losses can return extra flag to indicate additional TP-reduction (often required)
+            loss_output, tp_reduce = loss_output
+        losses = loss_output.float()
+        loss_mask = mask.view(-1).float()
+
+        if self._cp_size > 1:
+            if num_valid_tokens_in_ub is None:
+                num_valid_tokens_in_ub = loss_mask.sum()
+            if num_valid_tokens_in_ub < 0.5:  # no valid tokens
+                num_valid_tokens_in_ub += 1.0
+            loss = torch.sum(losses.view(-1) * loss_mask) / num_valid_tokens_in_ub  # sequence level nll
+            torch.distributed.all_reduce(loss, group=get_context_parallel_group())
+        else:
+            loss = torch.sum(losses.view(-1) * loss_mask) / loss_mask.sum()  # sequence level nll
+
+        if tp_reduce is True:
+            torch.distributed.all_reduce(loss, group=get_tensor_model_parallel_group())
+
+        return loss
+
+
+class DistillationGPTModel(llm.GPTModel):
+    """Custom GPT subclass for distillation-related modifications."""
+
+    @property
+    def training_loss_reduction(self) -> _DistillationLossReduction:
+        if not self._training_loss_reduction:
+            self._training_loss_reduction = _DistillationLossReduction()
+
+        return self._training_loss_reduction
+
+
+########################################################
+
+
+class BaseLoss(_Loss, metaclass=ABCMeta):
+    """Abstract base class for Megatron distillation losses."""
+
+    def __init__(self, model_config: TransformerConfig):
+        """
+        Constructor.
+
+        Args:
+            model_config: MCore transformer config.
+        """
+        super().__init__()
+        self._config = model_config
+
+    def pre_forward(self, predictions: Tensor, targets: Tensor) -> Tuple[Tensor, Tensor]:
+        """Prepares inputs safely for loss computation."""
+        if isinstance(predictions, tuple):
+            # `ColumnParallelLinear` returns bias too
+            predictions, targets = predictions[0], targets[0]
+        targets = targets.detach()
+
+        return predictions, targets
+
+    def post_forward(self, loss: Tensor, tp_reduce: bool = False) -> Tensor:
+        """Reshapes tensor from [s, b] to [b, s] for upcoming loss masking."""
+        loss = loss.transpose(0, 1).contiguous()
+        return loss, tp_reduce
+
+
+class LogitsKLLoss(BaseLoss):
+    """Calculates KL-Divergence loss between two logits tensors without reducing the sequence dim."""
+
+    def __init__(self, model_config: TransformerConfig, temperature: float = 1.0, reverse: bool = False):
+        """
+        Constructor.
+
+        Args:
+            model_config: MCore transformer config.
+            temperature: Divide tensors by this value prior to calculating loss.
+            reverse: Whether to reverse the loss as KLD(teacher, student) instead of KLD(student, teacher)
+        """
+        super().__init__(model_config)
+        self._temperature = temperature
+        self._reverse = reverse
+
+    def forward(self, predictions: Tensor, targets: Tensor) -> Tensor:
+        """
+        Forward function.
+
+        Args:
+            predictions: Student model tensors (size [s, b, h])
+            targets: Teacher model tensors (size [s, b, h])
+
+        Returns:
+            KLD loss of tensors (size [b, s])
+        """
+        predictions, targets = self.pre_forward(predictions, targets)
+
+        # Division by temp should happen prior to finding max for both student and teacher.
+        # Currently we don't use temperature in any of ours runs (temp=1.0)
+        output_teacher = targets.float() / self._temperature
+        output_student = predictions.float() / self._temperature
+
+        # Compute local softmax, and the reweight to compute global softmax.
+        if self._config.tensor_model_parallel_size > 1:
+
+            # Maximum value along vocab dimension across all GPUs.
+            teacher_logits_max, _ = torch.max(output_teacher, dim=-1)
+            torch.distributed.all_reduce(
+                teacher_logits_max,
+                op=torch.distributed.ReduceOp.MAX,
+                group=get_tensor_model_parallel_group(),
+            )
+            output_teacher = output_teacher - teacher_logits_max.unsqueeze(dim=-1)
+
+            denom_teacher = torch.sum(torch.exp(output_teacher), dim=-1)
+            # We can't use standard reduction function here since the computation
+            # that follows it isn't identical across TP ranks.
+            denom_teacher = all_reduce_autograd(denom_teacher, group=get_tensor_model_parallel_group())
+
+            # Maximum value along vocab dimension across all GPUs.
+            student_logits_max, _ = torch.max(output_student, dim=-1)
+            torch.distributed.all_reduce(
+                student_logits_max,
+                op=torch.distributed.ReduceOp.MAX,
+                group=get_tensor_model_parallel_group(),
+            )
+            output_student = output_student - student_logits_max.unsqueeze(dim=-1).detach()
+
+            denom_student = torch.sum(torch.exp(output_student), dim=-1)
+            denom_student = all_reduce_autograd(denom_student, group=get_tensor_model_parallel_group())
+
+            slen, bsz, sharded_vocab_size = output_student.shape
+            student_log_prob = output_student - torch.log(denom_student).view(slen, bsz, 1).expand(
+                slen, bsz, sharded_vocab_size
+            )
+            teacher_log_prob = output_teacher - torch.log(denom_teacher).view(slen, bsz, 1).expand(
+                slen, bsz, sharded_vocab_size
+            )
+
+            if self._reverse:
+                loss = torch.sum(
+                    F.kl_div(teacher_log_prob, student_log_prob, reduction="none", log_target=True),
+                    dim=-1,
+                )
+            else:
+                loss = torch.sum(
+                    F.kl_div(student_log_prob, teacher_log_prob, reduction="none", log_target=True),
+                    dim=-1,
+                )
+
+        else:
+            if self._reverse:
+                loss = torch.sum(
+                    F.kl_div(
+                        F.log_softmax(output_teacher, dim=-1),
+                        F.softmax(output_student, dim=-1),
+                        reduction="none",
+                    ),
+                    dim=-1,
+                )
+            else:
+                loss = torch.sum(
+                    F.kl_div(
+                        F.log_softmax(output_student, dim=-1),
+                        F.softmax(output_teacher, dim=-1),
+                        reduction="none",
+                    ),
+                    dim=-1,
+                )
+
+        return self.post_forward(loss, tp_reduce=True)
+
+
+class _AllReduce(torch.autograd.Function):
+    """Implementation from old PyTorch `torch.distributed.nn.parallel`."""
+
+    @staticmethod
+    def forward(ctx, op, group, tensor):
+        ctx.group, ctx.op = group, op
+        tensor = tensor.clone()
+        torch.distributed.all_reduce(tensor, op=op, group=group)
+        return tensor
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return (None, None, _AllReduce.apply(ctx.op, ctx.group, grad_output))
+
+
+def all_reduce_autograd(tensor, op=torch.distributed.ReduceOp.SUM, group=torch.distributed.group.WORLD):
+    """Custom all-reduce function.
+
+    Needed instead of other all-reduce functions available when the computation following
+    the all-reduce call differs per rank. In KL loss, this corresponds to the different numerators.
+    """
+    return _AllReduce.apply(op, group, tensor)
+
+
+########################################################
+
+
+def load_distillation_config(cfg: TransformerConfig) -> Dict[str, Any]:
+    """Create a default distillation config for MCore GPT Models.
+
+    Args:
+        student_cfg: Model config for student model.
+    """
+    logit_pair = ("output_layer", "output_layer")  # logit module names for MCoreGPTModel
+    cfg = {
+        "criterion": {tuple(logit_pair): LogitsKLLoss(cfg)},
+        "loss_balancer": None,
+        "skip_lm_loss": True,
+    }
+    return cfg
+
+
+def _adjust_layer_index_for_pp(submodule_name, model_cfg):
+    """
+    Adjust any sequence-based layer indices found in a submodule name for Pipeline Parallelism.
+
+    For example, on PP=2, layer called `"decoder.layers.17.input_layernorm"` in a model with 32 layers
+    will be assumed to be evely distributed among PP ranks and now be referenced on the final rank as
+    `"decoder.layers.2.input_layernorm"`.
+
+    NOTE: Operates under assumption of being final PP rank and only one numerical index per layer name.
+    """
+    ...
+    # TODO
+
+
+def _teacher_provider(cfg: TransformerConfig) -> MCoreGPTModel:
+    """Teacher model factory (must be a non-local function to pickle)."""
+
+    logging.info("Distillation: Loading teacher weights...")
+    model = load_with_modelopt_layer_spec(
+        cfg.kd_teacher_restore_from_path,
+        tensor_model_parallel_size=cfg.tensor_model_parallel_size,
+        pipeline_model_parallel_size=cfg.pipeline_model_parallel_size,
+        inference_only=True,
+    )
+    logging.info("Distillation: ... teacher weights loaded.")
+    return model.module
+
+
+def adjust_distillation_model_for_mcore(
+    model: mtd.DistillationModel, model_cfg: TransformerConfig, distill_cfg: Dict[str, Any]
+):
+    """Extra modifcations to ``mtd.DistillationModel`` requried for Megatron-Core."""
+
+    # HACK: Get rid of ModelOpt Distillation state
+    # NOTE: If re-placed, above losses need modifcation as `TransformerConfig` has non-pickleable elements.
+    mto.ModeloptStateManager(model)._state.pop()
+
+    # HACK: Hide teacher during `sharded_state_dict` method.
+    def _sharded_state_dict(self, *args, **kwargs) -> ShardedStateDict:
+        with self.hide_teacher_model():
+            return self._sharded_state_dict(*args, **kwargs)
+
+    model._sharded_state_dict = model.sharded_state_dict
+    model.sharded_state_dict = types.MethodType(_sharded_state_dict, model)
+
+    # HACK: Skip `lm_loss` bypassing it when training if not needed for backprop.
+    def _compute_language_model_loss(self, labels, logits) -> Tensor:
+        if self.training:
+            return torch.zeros_like(labels)
+        return self._compute_language_model_loss(labels, logits)
+
+    if distill_cfg["skip_lm_loss"]:
+        model._compute_language_model_loss = model.compute_language_model_loss
+        model.compute_language_model_loss = types.MethodType(_compute_language_model_loss, model)
+
+    # HACK: Skip `lm_loss` always for teacher.
+    def _compute_language_model_loss(self, labels, logits) -> Tensor:
+        return torch.zeros_like(labels)
+
+    model.teacher_model.compute_language_model_loss = types.MethodType(
+        _compute_language_model_loss, model.teacher_model
+    )
+
+    if model_cfg.pipeline_model_parallel_size > 1:
+
+        def _set_input_tensor(self, input_tensor: Tensor):
+            obj = self.teacher_model if self._only_teacher_fwd else self
+            return type(self).set_input_tensor(obj, input_tensor)
+
+        # HACK: Pipeline-parallel Distillation requires a way to cache input batches for subsequent
+        # forward calls, as well as a way to pass through output tensors to teacher model.
+        model.set_input_tensor = types.MethodType(_set_input_tensor, model)
+
+        @contextmanager
+        def _swap_teacher_config(self, model_wrapper):
+            try:
+                if hasattr(model_wrapper, "config"):
+                    model_wrapper._config = model_wrapper.config
+                model_wrapper.config = self.teacher_model.config
+                yield
+            finally:
+                del model_wrapper.config
+                if hasattr(model_wrapper, "_config"):
+                    model_wrapper.config = model_wrapper._config
+                    del model_wrapper._config
+
+        # HACK: Pipeline-parallel forward function relies on the config in the model to know what
+        # hidden size of tensor to communicate to next stage.
+        model.swap_teacher_config = types.MethodType(_swap_teacher_config, model)
+
+
+########################################################
+
+
+if __name__ == "__main__":
+    logging.info("Distillation enabled.")
+
+    seq_length = 2048
+    global_batch_size = 16
+    tp = 1
+    pp = 1
+
+    # TODO: setup the dummy dataset
+    data = llm.MockDataModule(seq_length=seq_length, global_batch_size=global_batch_size)
+
+    TEACHER_PATH = "./test_teacher/"
+    #
+    import os
+    import sys
+
+    from megatron.core import dist_checkpointing
+
+    from nemo.lightning.io.pl import ckpt_to_weights_subdir
+
+    if not os.path.exists(TEACHER_PATH):
+        gpt_config = llm.GPTConfig(
+            num_layers=9,
+            hidden_size=384,
+            ffn_hidden_size=1536,
+            num_attention_heads=6,
+            seq_length=seq_length,
+            init_method_std=0.023,
+            hidden_dropout=0.0,
+            attention_dropout=0.0,
+            layernorm_epsilon=1e-5,
+            make_vocab_size_divisible_by=128,
+            transformer_layer_spec=get_gpt_layer_modelopt_spec(),
+        )
+        model = llm.GPTModel(gpt_config, tokenizer=data.tokenizer)
+        dist_checkpointing.save(model.sharded_state_dict(), str(ckpt_to_weights_subdir(TEACHER_PATH, is_saving=True)))
+        sys.exit(0)
+    #
+
+    ## initialize a small GPT model
+    gpt_config = DistillationGPTConfig(
+        num_layers=6,
+        hidden_size=384,
+        ffn_hidden_size=1536,
+        num_attention_heads=6,
+        seq_length=seq_length,
+        init_method_std=0.023,
+        hidden_dropout=0.0,
+        attention_dropout=0.0,
+        layernorm_epsilon=1e-5,
+        make_vocab_size_divisible_by=128,
+        transformer_layer_spec=get_gpt_layer_modelopt_spec(),
+        kd_teacher_restore_from_path=TEACHER_PATH,
+    )
+    model = DistillationGPTModel(gpt_config, tokenizer=data.tokenizer)
+
+    ## initialize the strategy
+    strategy = nl.MegatronStrategy(
+        tensor_model_parallel_size=tp,
+        pipeline_model_parallel_size=pp,
+        pipeline_dtype=torch.bfloat16,
+    )
+
+    ## setup the optimizer
+    opt_config = OptimizerConfig(
+        optimizer='adam',
+        lr=3e-5,
+        bf16=True,
+    )
+    opt = nl.MegatronOptimizerModule(config=opt_config)
+
+    trainer = nl.Trainer(
+        devices=1,  ## you can change the number of devices to suit your setup
+        max_steps=50,
+        accelerator="gpu",
+        strategy=strategy,
+        plugins=nl.MegatronMixedPrecision(precision="bf16-mixed"),
+    )
+
+    nemo_logger = nl.NeMoLogger(
+        log_dir="test_logdir",  ## logs and checkpoints will be written here
+    )
+
+    llm.train(
+        model=model,
+        data=data,
+        trainer=trainer,
+        log=nemo_logger,
+        tokenizer='data',
+        optim=opt,
+    )
