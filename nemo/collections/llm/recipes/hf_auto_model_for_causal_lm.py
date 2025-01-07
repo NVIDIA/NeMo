15f4af8e51519cf0dfab3a404c601e8588c7c099
Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
2025-01-07 09:46:14 -0800

    add pretrain tests for hf (#11673)
    * add pretrain tests
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    
    * fix
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * add max_steps
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    ---------
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    Co-authored-by: akoumpa <akoumpa@users.noreply.github.com>
    Signed-off-by: Lawrence Lane <llane@nvidia.com>

--- nemo/collections/llm/recipes/hf_auto_model_for_causal_lm.py
+++ nemo/collections/llm/recipes/hf_auto_model_for_causal_lm.py
@@ -127,6 +127,7 @@ def pretrain_recipe(
     num_gpus_per_node: int = 8,
     fn=pretrain,
     model_name: str = '',
+    max_steps: int = 100,
 ) -> run.Partial:
     """
     Create a pre-training recipe for a HFAutoModelForCausalLM model.
@@ -159,6 +160,7 @@ def pretrain_recipe(
             num_nodes=num_nodes,
             num_gpus_per_node=num_gpus_per_node,
             callbacks=[run.Config(TimingCallback)],
+            max_steps=max_steps,
         ),
         data=run.Config(MockDataModule, seq_length=4096, global_batch_size=512, micro_batch_size=1),
         log=default_log(dir=dir, name=name, tensorboard_logger=tensorboard_logger(name=name)),
