b677c6203d938b53ad07de3492b1a4bb63003539
oliver könig <okoenig@nvidia.com>
2025-01-08 20:23:02 +0100

    build: Move dependencies
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -26,15 +26,23 @@ else
   fi
 fi
 
+export MAMBA_FORCE_BUILD=TRUE
+export CAUSAL_CONV1D_FORCE_BUILD=TRUE
+${PIP} install \
+  "transformer-engine @ git+https://github.com/NVIDIA/TransformerEngine.git@${TE_TAG}" \
+  "apex @ git+https://github.com/NVIDIA/apex.git@${APEX_TAG}" \
+  "git+https://github.com/Dao-AILab/causal-conv1d.git@${CAUSAL_CONV_TAG}" \
+  "git+https://github.com/state-spaces/mamba.git@${MAMBA_TAG}"
+
 echo 'Installing nemo'
 if [[ "$INSTALL_OPTION" == "dev" ]]; then
-    ${PIP} install --editable ".[all]"
+  ${PIP} install --editable ".[all]"
 else
-    rm -rf dist/
-    ${PIP} install build pytest-runner
-    python -m build --no-isolation --wheel
-    DIST_FILE=$(find ./dist -name "*.whl" | head -n 1)
-    ${PIP} install "${DIST_FILE}[all]"
+  rm -rf dist/
+  ${PIP} install build pytest-runner
+  python -m build --no-isolation --wheel
+  DIST_FILE=$(find ./dist -name "*.whl" | head -n 1)
+  ${PIP} install "${DIST_FILE}[all]"
 fi
 
 echo 'All done!'
