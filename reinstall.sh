23a74332f37c06308a967fbe7c455117382cb4c4
oliver könig <okoenig@nvidia.com>
2025-01-08 20:40:36 +0100

    fix
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -28,11 +28,23 @@ fi
 
 export MAMBA_FORCE_BUILD=TRUE
 export CAUSAL_CONV1D_FORCE_BUILD=TRUE
+export TE_TAG=7d576ed25266a17a7b651f2c12e8498f67e0baea
+export NEMO_RUN_TAG=34259bd3e752fef94045a9a019e4aaf62bd11ce2
+export APEX_TAG=810ffae374a2b9cb4b5c5e28eaeca7d7998fca0c
+export CAUSAL_CONV_TAG=v1.2.2.post1
+export MAMBA_TAG=v2.2.0
+export MCORE_TAG=4dc8977167d71f86bdec47a60a98e85c4cfa0031
+export NV_RESILIENCY_EXT_TAG=97aad77609d2e25ed38ac5c99f0c13f93c48464e
+
 ${PIP} install \
+  "nemo_run@git+https://github.com/NVIDIA/NeMo-Run.git@${NEMO_RUN_TAG}" \
   "transformer-engine @ git+https://github.com/NVIDIA/TransformerEngine.git@${TE_TAG}" \
   "apex @ git+https://github.com/NVIDIA/apex.git@${APEX_TAG}" \
   "git+https://github.com/Dao-AILab/causal-conv1d.git@${CAUSAL_CONV_TAG}" \
-  "git+https://github.com/state-spaces/mamba.git@${MAMBA_TAG}"
+  "git+https://github.com/state-spaces/mamba.git@${MAMBA_TAG}" \
+  "git+https://github.com/NVIDIA/Megatron-LM.git@{MCORE_TAG}" \
+  "git+https://github.com/NVIDIA/nvidia-resiliency-ext.git@{NV_RESILIENCY_EXT_TAG}" \
+  "onnxscript @ git+https://github.com/microsoft/onnxscript"
 
 echo 'Installing nemo'
 if [[ "$INSTALL_OPTION" == "dev" ]]; then
