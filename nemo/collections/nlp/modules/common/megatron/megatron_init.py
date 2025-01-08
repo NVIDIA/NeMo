9c09f44b1778c13faf6434a3beddf603ac0d65a8
BoxiangW <45734921+BoxiangW@users.noreply.github.com>
2025-01-08 00:10:30 -0800

    Add option to change batch size if needed (#11268)
    * Add option to change batch size if needed
    
    * Apply isort and black reformatting
    
    Signed-off-by: BoxiangW <BoxiangW@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: BoxiangW <BoxiangW@users.noreply.github.com>
    Co-authored-by: BoxiangW <BoxiangW@users.noreply.github.com>
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- nemo/collections/nlp/modules/common/megatron/megatron_init.py
+++ nemo/collections/nlp/modules/common/megatron/megatron_init.py
@@ -183,6 +183,7 @@ def initialize_model_parallel_for_nemo(
                     micro_batch_size=micro_batch_size,
                     data_parallel_size=app_state.data_parallel_size,
                     rampup_batch_size=rampup_batch_size,
+                    decrease_batch_size_if_needed=False,
                 )
             else:
                 if isinstance(_GLOBAL_NUM_MICROBATCHES_CALCULATOR, ConstantNumMicroBatchesCalculator):
@@ -203,6 +204,7 @@ def initialize_model_parallel_for_nemo(
                     micro_batch_size=micro_batch_size,
                     data_parallel_size=app_state.data_parallel_size,
                     rampup_batch_size=rampup_batch_size,
+                    decrease_batch_size_if_needed=False,
                 )
             else:
                 if isinstance(_GLOBAL_NUM_MICROBATCHES_CALCULATOR, ConstantNumMicroBatchesCalculator):
