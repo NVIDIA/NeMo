3f78a5946b8a524046c57b82c6900add895e8d89
Malay Nagda <malayn@nvidia.com>
2025-01-10 17:13:31 +0530

    8x22b seq len tests
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- tests/collections/llm/recipes/test_mixtral_8x22b.py
+++ tests/collections/llm/recipes/test_mixtral_8x22b.py
@@ -74,7 +74,10 @@ class TestMixtral8x22B:
         assert isinstance(recipe.data, run.Config)
         assert recipe.data.__fn_or_cls__ == MockDataModule
         assert isinstance(recipe.model.config, run.Config)
-        assert recipe.data.seq_length == 65536 if recipe.model.config.__fn_or_cls__ == MixtralConfig8x22B else 4096
+        if recipe.model.config.__fn_or_cls__ == MixtralConfig8x22B:
+            assert recipe.data.seq_length == 65536  
+        else:
+            assert recipe.data.seq_length == 4096
         assert recipe.data.global_batch_size == 512
         assert recipe.data.micro_batch_size == 1
 
@@ -123,5 +126,8 @@ class TestMixtral8x22B:
         assert mixtral_config.num_layers == 56
         assert mixtral_config.hidden_size == 6144
         assert mixtral_config.num_attention_heads == 48
-        assert mixtral_config.seq_length == 65536 if mixtral_config.__fn_or_cls__ == MixtralConfig8x22B else 4096
+        if mixtral_config.__fn_or_cls__ == MixtralConfig8x22B:
+            assert mixtral_config.seq_length == 65536  
+        else:
+            assert mixtral_config.seq_length == 4096
         assert mixtral_config.num_moe_experts == 8
