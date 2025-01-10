1f9e6f2b53e2c751584c4b841b23211475fce074
Yuanzhe Dong <yudong@nvidia.com>
2025-01-10 19:27:05 +0000

    use fsdp2

--- examples/llm/peft/hf.py
+++ examples/llm/peft/hf.py
@@ -37,25 +37,27 @@ def make_squad_hf_dataset(tokenizer):
     {}"""
         instruction = examples["context"]
         input = examples["question"]
-        output = examples["answers"]['text']
+        output = examples["answers"]["text"]
         if isinstance(output, list):
             output = output[0]
         text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
         ans = tokenizer(text)
         # 'input_ids' is a list, we want to remove EOS_TOKEN from input_ids and the first token from
         # labels to align the two:
-        ans['labels'] = list(ans['input_ids'][1:])
-        ans['input_ids'] = ans['input_ids'][:-1]
-        ans['attention_mask'] = ans['attention_mask'][:-1]
+        ans["labels"] = list(ans["input_ids"][1:])
+        ans["input_ids"] = ans["input_ids"][:-1]
+        ans["attention_mask"] = ans["attention_mask"][:-1]
         return ans
 
-    tokenizer = getattr(tokenizer, 'tokenizer', tokenizer)
-    datamodule = llm.HFDatasetDataModule("rajpurkar/squad", split="train[:100]", pad_token_id=tokenizer.eos_token_id)
+    tokenizer = getattr(tokenizer, "tokenizer", tokenizer)
+    datamodule = llm.HFDatasetDataModule(
+        "rajpurkar/squad", split="train[:100]", pad_token_id=tokenizer.eos_token_id
+    )
     datamodule.map(
         formatting_prompts_func,
         batched=False,
         batch_size=2,
-        remove_columns=["id", "title", "context", "question", 'answers'],
+        remove_columns=["id", "title", "context", "question", "answers"],
     )
     return datamodule
 
@@ -64,32 +66,37 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', default='meta-llama/Llama-3.2-1B')
-    parser.add_argument('--strategy', type=str, default='auto', choices=['auto', 'ddp', 'fsdp', 'fsdp2'])
-    parser.add_argument('--devices', default=1)
-    parser.add_argument('--accelerator', default='gpu', choices=['gpu'])
-    parser.add_argument('--max-steps', type=int, default=100)
-    parser.add_argument('--wandb-project', type=str, default=None)
-    parser.add_argument('--use-torch-jit', action='store_true')
-    parser.add_argument('--ckpt-folder', type=str, default=None)
+    parser.add_argument("--model", default="meta-llama/Llama-3.2-1B")
+    parser.add_argument(
+        "--strategy", type=str, default="auto", choices=["auto", "ddp", "fsdp", "fsdp2"]
+    )
+    parser.add_argument("--devices", default=1)
+    parser.add_argument("--accelerator", default="gpu", choices=["gpu"])
+    parser.add_argument("--max-steps", type=int, default=100)
+    parser.add_argument("--wandb-project", type=str, default=None)
+    parser.add_argument("--use-torch-jit", action="store_true")
+    parser.add_argument("--ckpt-folder", type=str, default=None)
     args = parser.parse_args()
 
     wandb = None
     if args.wandb_project is not None:
-        model = '_'.join(args.model.split('/')[-2:])
+        model = "_".join(args.model.split("/")[-2:])
         wandb = WandbLogger(
             project=args.wandb_project,
-            name=f'{model}_dev{args.devices}_strat_{args.strategy}',
+            name=f"{model}_dev{args.devices}_strat_{args.strategy}",
         )
     grad_clip = 0.5
-    if args.strategy == 'fsdp':
+    if args.strategy == "fsdp":
         # See:
         # https://github.com/Lightning-AI/pytorch-lightning/blob/8ad3e29816a63d8ce5c00ac104b14729a4176f4f/src/lightning/pytorch/plugins/precision/fsdp.py#L81
         grad_clip = None
 
-    if args.strategy == 'fsdp2':
+    if args.strategy == "fsdp2":
         from nemo.lightning.pytorch.strategies import FSDP2Strategy
-        args.strategy = FSDP2Strategy()
+
+        args.strategy = FSDP2Strategy(
+            data_parallel_size=args.devices, tensor_parallel_size=1
+        )
 
     use_dist_samp = False
 
@@ -103,7 +110,9 @@ def main():
 
     callbacks = []
     if args.use_torch_jit:
-        jit_config = JitConfig(use_torch=True, torch_kwargs={'dynamic': True}, use_thunder=False)
+        jit_config = JitConfig(
+            use_torch=True, torch_kwargs={"dynamic": True}, use_thunder=False
+        )
         callbacks = [JitTransform(jit_config)]
 
     llm.api.finetune(
@@ -127,11 +136,11 @@ def main():
         optim=fdl.build(llm.adam.pytorch_adam_with_flat_lr(lr=1e-5)),
         log=NeMoLogger(log_dir=args.ckpt_folder, use_datetime_version=False),
         peft=llm.peft.LoRA(
-            target_modules=['*_proj'],
+            target_modules=["*_proj"],
             dim=8,
         ),
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
