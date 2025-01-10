36b40e97d1afeb560fd4d33e85048078d00d6fac
Malay Nagda <malayn@nvidia.com>
2025-01-10 16:53:00 +0530

    8x22b seq len tests
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- nemo/collections/llm/gpt/model/mixtral.py
+++ nemo/collections/llm/gpt/model/mixtral.py
@@ -118,7 +118,7 @@ class MixtralConfig8x22B(MixtralConfig):
     hidden_size: int = 6144
     num_attention_heads: int = 48
     ffn_hidden_size: int = 16384
-    max_position_embeddings: int = 4096
+    max_position_embeddings: int = 65536
     seq_length: int = 65536
 
 
