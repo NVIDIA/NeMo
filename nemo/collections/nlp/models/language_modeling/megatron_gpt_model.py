490964a0124abc48ad4e88af0c6e81c2d17268c3
BoxiangW <45734921+BoxiangW@users.noreply.github.com>
2025-01-10 11:39:36 -0800

    Add rope scaling configs for NeMo 1 (#11807)
    * Add rope scaling configs
    
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: BoxiangW <BoxiangW@users.noreply.github.com>
    
    * Fix bug
    
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>
    
    ---------
    
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>
    Signed-off-by: BoxiangW <BoxiangW@users.noreply.github.com>
    Co-authored-by: BoxiangW <BoxiangW@users.noreply.github.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -199,7 +199,13 @@ def mcore_model_customize(cfg, model):
     if cfg.get("apply_embedding_scaling", False) and parallel_state.is_pipeline_first_stage():
         extend_instance(model.embedding, EmbeddingScalingMixin)
     if cfg.get("scale_positional_embedding", False):
-        model.rotary_pos_emb.inv_freq = apply_rope_scaling(model.rotary_pos_emb.inv_freq)
+        model.rotary_pos_emb.inv_freq = apply_rope_scaling(
+            model.rotary_pos_emb.inv_freq,
+            scale_factor=cfg.get('scale_factor', 8),
+            low_freq_factor=cfg.get('low_freq_factor', 1),
+            high_freq_factor=cfg.get('high_freq_factor', 4),
+            old_context_len=cfg.get('old_context_len', 8192),
+        )
     if cfg.get("mcore_customization_config", {}).get("final_logit_softcapping", 0):
         from nemo.collections.nlp.models.language_modeling.megatron.gemma2.gemma2_modules import Gemma2OutputLayer
 
