2c9840ccb614abfc74cd5461ff5a46f54804dd90
yuanzhedong <yuanzhedong@users.noreply.github.com>
2025-01-13 19:50:46 +0000

    Apply isort and black reformatting
    Signed-off-by: yuanzhedong <yuanzhedong@users.noreply.github.com>

--- examples/llm/sft/hf.py
+++ examples/llm/sft/hf.py
@@ -114,9 +114,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="meta-llama/Llama-3.2-1B")
-    parser.add_argument(
-        "--strategy", type=str, default="auto", choices=["auto", "ddp", "fsdp", "fsdp2"]
-    )
+    parser.add_argument("--strategy", type=str, default="auto", choices=["auto", "ddp", "fsdp", "fsdp2"])
     parser.add_argument("--devices", type=int, default=1)
     parser.add_argument("--accelerator", default="gpu", choices=["gpu"])
     parser.add_argument("--model-accelerator", default=None, choices=["te"])
@@ -143,9 +141,7 @@ def main():
         from nemo.lightning.pytorch.strategies import FSDP2Strategy
 
         grad_clip = None
-        args.strategy = FSDP2Strategy(
-            data_parallel_size=args.devices, tensor_parallel_size=1
-        )
+        args.strategy = FSDP2Strategy(data_parallel_size=args.devices, tensor_parallel_size=1)
 
     use_dist_samp = False
 
@@ -158,16 +154,12 @@ def main():
 
     from nemo.lightning.pytorch.accelerate.transformer_engine import te_accelerate
 
-    model = llm.HFAutoModelForCausalLM(
-        model_name=args.model, model_accelerator=model_accelerator
-    )
+    model = llm.HFAutoModelForCausalLM(model_name=args.model, model_accelerator=model_accelerator)
     tokenizer = model.tokenizer
 
     callbacks = []
     if args.use_torch_jit:
-        jit_config = JitConfig(
-            use_torch=True, torch_kwargs={"dynamic": False}, use_thunder=False
-        )
+        jit_config = JitConfig(use_torch=True, torch_kwargs={"dynamic": False}, use_thunder=False)
         callbacks = [JitTransform(jit_config)]
 
     llm.api.finetune(
