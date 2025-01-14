e99c3812e15907ea3681788fce81a9ef06704b5c
ashors1 <ashors@nvidia.com>
2025-01-14 15:36:56 -0800

    formatting
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- nemo/lightning/pytorch/callbacks/flops_callback.py
+++ nemo/lightning/pytorch/callbacks/flops_callback.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, List
+from typing import List
 
 import lightning.pytorch as pl
 import numpy as np
