c1a25db23cfed64c15d3a0ac6c37dc424a7db994
Malay Nagda <malayn@nvidia.com>
2025-01-18 21:43:50 +0530

    mem usage, 8b tp4
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- nemo/lightning/pytorch/strategies/megatron_strategy.py
+++ nemo/lightning/pytorch/strategies/megatron_strategy.py
@@ -563,7 +563,7 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
 
             if self.log_memory_usage:
                 max_memory_reserved = torch.cuda.max_memory_reserved()
-                _memory_allocated = (torch.cuda.mem_get_info()[1] - torch.cuda.mem_get_info()[0]) / 1024 / 1024 / 1024
+                memory_allocated = torch.cuda.memory_allocated()
                 self.lightning_module.log(
                     "peak_memory_usage",
                     max_memory_reserved,
@@ -571,8 +571,8 @@ class MegatronStrategy(DDPStrategy, io.IOMixin):
                     batch_size=1,
                 )
                 self.lightning_module.log(
-                    "_memory_allocated",
-                    _memory_allocated,
+                    "memory_allocated",
+                    memory_allocated,
                     prog_bar=True,
                     batch_size=1,
                 )
