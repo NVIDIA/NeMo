41f92b553d00ec48a69cf1c3a3e960335f0c22ad
zhehuaichen <dian.chenzhehuai@gmail.com>
2025-01-21 13:28:51 -0800

    fixed .nemo saving code. after that, can do .nemo inference
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>

--- examples/multimodal/speech_llm/modular_audio_gpt_eval_s2s.py
+++ examples/multimodal/speech_llm/modular_audio_gpt_eval_s2s.py
@@ -94,13 +94,13 @@ def main(cfg) -> None:
     if hasattr(cfg.model, "restore_from_hparams_path"):  # not a .nemo model we expect a hparams.yaml file
         # .ckpt requires the following
         model = imported_cls.restore_from_pretrained_models(cfg, trainer=trainer)
+    trainer.validate(model)
+
     if cfg.get("save_as_nemo", None):
         model.setup("predict")  # need to call setup() to load adapters and prepare for saving
         model.save_to(cfg.save_as_nemo)
         logging.info(f"Model saved to {Path(cfg.save_as_nemo).absolute()}, exiting...")
         exit(0)
-
-    trainer.validate(model)
 
 
 if __name__ == "__main__":
