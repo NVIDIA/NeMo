cd54ffaeac368fe555843588ead53120e6124240
oliver könig <okoenig@nvidia.com>
2025-01-23 09:37:20 +0100

    constraint triton
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -89,7 +89,7 @@ if [[ "$HEAVY_DEPS" == "TRUE" ]]; then
 fi
 
 echo 'Installing dependencies of nemo'
-${PIP} install --no-cache-dir --extra-index-url https://pypi.nvidia.com "${DEPS[@]}"
+${PIP} install --no-cache-dir --extra-index-url https://pypi.nvidia.com "${DEPS[@]}" -c "triton==3.1.0"
 
 echo 'Installing nemo'
 if [[ "$INSTALL_OPTION" == "dev" ]]; then
