7ca6df84e185e24e50880afa5e46ca5cc73b2e91
ykarnati <ykarnati@nvidia.com>
2025-01-26 19:34:41 +0000

    add import ckpt script

---
+++ scripts/llm/import_ckpt.py
@@ -0,0 +1,11 @@
+from nemo.collections.llm import Llama2Config7B, LlamaModel, import_ckpt
+
+if __name__ == "__main__":
+    # Specify the Hugging Face model ID
+    hf_model_id = "lmsys/vicuna-7b-v1.5"
+
+    # Import the model and convert to NeMo 2.0 format
+    import_ckpt(
+        model=LlamaModel(Llama2Config7B()),
+        source=f"hf://{hf_model_id}",
+    )
