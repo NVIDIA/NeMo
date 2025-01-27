bc09c257c082665aaec496d794846f2910ac8496
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-01-27 12:52:24 -0800

    fix?
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/io/artifact/file.py
+++ nemo/lightning/io/artifact/file.py
@@ -58,7 +58,7 @@ def copy_file(src: Union[Path, str], path: Union[Path, str], relative_dst: Union
 
 
 class DirArtifact(Artifact[str]):
-    def dump(self, value: str, absolute_dir: Path, relative_dir: Path) -> str:
+    def dump(self, instance, value: str, absolute_dir: Path, relative_dir: Path) -> str:
         value = pathize(value)
         absolute_dir = pathize(absolute_dir)
         relative_dir = pathize(relative_dir)
@@ -76,11 +76,11 @@ class DirArtifact(Artifact[str]):
 
 
 class DirOrStringArtifact(DirArtifact):
-    def dump(self, value: str, absolute_dir: Path, relative_dir: Path) -> str:
+    def dump(self, instance, value: str, absolute_dir: Path, relative_dir: Path) -> str:
         if not pathize(value).exists():
             # This is Artifact is just a string.
             return fdl.Config(DirOrStringArtifact, attr=value, skip=True)
-        return super().dump(value, absolute_dir, relative_dir)
+        return super().dump(instance, value, absolute_dir, relative_dir)
 
     def load(self, path: str) -> str:
         return path
