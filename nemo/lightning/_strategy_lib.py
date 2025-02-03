Fix Loss broadcast when ETP<(D)TP when using EPP
Signed-off-by: Parth Mannan <pmannan@nvidia.com>

nemo/lightning/_strategy_lib.py
@@ -596,16 +596,20 @@ def _sync_from_last_pipeline_stage(value: torch.Tensor, broadcast: bool = False)
     if parallel_state.get_pipeline_model_parallel_world_size() > 1:
         src_rank = parallel_state.get_pipeline_model_parallel_last_rank()
 
+        if not isinstance(src_rank, list):
+            src_rank = [src_rank]
+        
         if not broadcast:
             group = parallel_state.get_pipeline_model_parallel_group()
             if isinstance(group, list):
                 group = group[-1]
             pp_ranks = torch.distributed.get_process_group_ranks(group)
 
-            if torch.distributed.get_rank() == src_rank and 0 in pp_ranks:
-                torch.distributed.send(value, 0)
-            elif torch.distributed.get_rank() == 0:
-                torch.distributed.recv(value, src_rank)
+            for src_rank_idx in src_rank:
+                if torch.distributed.get_rank() == src_rank_idx and 0 in pp_ranks:
+                    torch.distributed.send(value, 0)
+                elif torch.distributed.get_rank() == 0:
+                    torch.distributed.recv(value, src_rank_idx)
         else:
             torch.distributed.broadcast(
                 value,