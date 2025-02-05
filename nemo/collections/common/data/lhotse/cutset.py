314fb472940352e29796312b7b26c2b182f67e1f
Taejin Park <tango4j@gmail.com>
2025-02-04 17:43:19 -0800

    Fixed the additional errors in cutset.py
    Signed-off-by: Taejin Park <tango4j@gmail.com>

--- nemo/collections/common/data/lhotse/cutset.py
+++ nemo/collections/common/data/lhotse/cutset.py
@@ -544,8 +544,8 @@ def read_nemo_manifest(config) -> tuple[CutSet, bool]:
         #   i.e., NeMo concatenated dataset
         #   Assume it's [path1, path2, ...] (while tarred_audio_filepaths in the same format).
         logging.info(
-            f"Initializing Lhotse CutSet from multiple tarred NeMo manifest sources with a weighted multiplexer. "
-            f"We found the following sources and weights: "
+            "Initializing Lhotse CutSet from multiple tarred NeMo manifest sources with a weighted multiplexer. " \
+            "We found the following sources and weights: "
         )
         cutsets = []
         weights = []
