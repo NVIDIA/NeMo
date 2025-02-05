e731bac55a06ef889803cb24411c64e52b8e7a9b
Piotr Żelasko <petezor@gmail.com>
2025-02-05 14:45:47 -0500

    test for rnnt-ctc hybrid model
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>

--- tests/collections/asr/test_asr_ctc_encoder_model_bpe_ngpt.py
+++ tests/collections/asr/test_asr_ctc_encoder_model_bpe_ngpt.py
@@ -43,7 +43,7 @@ def asr_model(test_data_dir):
             'feat_in': 64,
             'n_layers': 1,
             'd_model': 64,
-            'subsampling': 'dw_striding',
+            'subsampling': 'ngpt-frame-stack',
             'subsampling_factor': 2,
             'n_heads': 4,
             'use_nGPT': True,
