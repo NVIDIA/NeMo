d3406b06661f685e46294ccb34f41ce547ae7bf8
Ao Tang <aot@nvidia.com>
2025-02-06 10:33:31 -0500

    Support data_root as list, support separate val/test file

--- nemo/collections/llm/gpt/data/retrieval.py
+++ nemo/collections/llm/gpt/data/retrieval.py
@@ -14,9 +14,9 @@
 
 import json
 import os.path
-from typing import TYPE_CHECKING, Any, Dict, List, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
-from datasets import Dataset
+from datasets import Dataset, concatenate_datasets
 
 from nemo.collections.llm.bert.data.fine_tuning import FineTuningDataModule
 from nemo.collections.llm.gpt.data.core import get_dataset_root
@@ -33,7 +33,11 @@ class CustomRetrievalDataModule(FineTuningDataModule):
 
     def __init__(
         self,
-        data_root: str,
+        data_root: Union[str, List[str]],
+        val_root: Optional[str] = None,
+        test_root: Optional[str] = None,
+        val_ratio: Optional[float] = 0.04,
+        test_ratio: Optional[float] = 0.01,
         dataset_identifier: str = "custom_retrieval_dataset",
         seq_length: int = 2048,
         tokenizer: Optional["TokenizerSpec"] = None,
@@ -57,11 +61,38 @@ class CustomRetrievalDataModule(FineTuningDataModule):
         self.delete_raw = delete_raw
 
         assert packed_sequence_specs is None, "RetrievalDataModule does not support packed sequences."
-        assert os.path.exists(data_root), "Data root does not exist."
+        if isinstance(data_root, str):
+            data_root = [data_root]
+        for directory in data_root:
+            assert os.path.exists(directory), f"Data root {directory} does not exist."
+
+        if val_root is not None:
+            assert os.path.exists(val_root), f"Validation root {val_root} does not exist."
+            self.val_ratio = 0 # Use val_root for all validation
+        else:
+            self.val_ratio = val_ratio
+
+        if test_root is not None:
+            assert os.path.exists(test_root), f"Test root {test_root} does not exist."
+            self.test_ratio = 0
+        else:
+            self.test_ratio = test_ratio
+
+        self.train_ratio = 1 - self.val_ratio - self.test_ratio
+        self.val_root = val_root
+        self.test_root = test_root
         self.query_key = query_key
         self.pos_doc_key = pos_doc_key
         self.neg_doc_key = neg_doc_key
         self.unprocessed_root = data_root
+
+        log_info = f"data_root: {data_root} will be split to {self.train_ratio}:{self.val_ratio}:{self.test_ratio} used for train/val/test"
+        if self.val_root is not None:
+            log_info += f", separate validation root: {self.val_root}"
+        if self.test_root is not None:
+            log_info += f", separate test root: {self.test_root}"
+        logging.info(log_info)
+
         super().__init__(
             dataset_root=get_dataset_root(dataset_identifier),
             seq_length=seq_length,
@@ -83,19 +114,44 @@ class CustomRetrievalDataModule(FineTuningDataModule):
             self._preprocess_and_split_data()
         super().prepare_data()
 
-    def _preprocess_and_split_data(self, train_ratio: float = 0.95, val_ratio: float = 0.04):
+    def _preprocess_and_split_data(self):
         logging.info(f"Preprocessing {self.__class__.__name__} to jsonl format and splitting...")
 
-        test_ratio = 1 - train_ratio - val_ratio
         save_splits = {}
-        dataset = Dataset.from_list(json.load(open(self.unprocessed_root, 'r')))
-        split_dataset = dataset.train_test_split(test_size=val_ratio + test_ratio, seed=self.seed)
-        split_dataset2 = split_dataset['test'].train_test_split(
-            test_size=test_ratio / (val_ratio + test_ratio), seed=self.seed
-        )
-        save_splits['training'] = split_dataset['train']
-        save_splits['validation'] = split_dataset2['train']
-        save_splits['test'] = split_dataset2['test']
+
+        train_datasets = []
+        for data_dir in self.unprocessed_root:
+            train_datasets.append(Dataset.from_list(json.load(open(data_dir, 'r'))))
+        train_dataset = concatenate_datasets(train_datasets)
+
+        if self.val_ratio + self.test_ratio != 0:
+            split_dataset = train_dataset.train_test_split(test_size=self.val_ratio + self.test_ratio, seed=self.seed)
+            save_splits['train'] = split_dataset['train']
+        else:
+            # No split
+            save_splits['train'] = train_dataset
+
+        split_dataset2 = {}
+        if self.val_ratio !=0 and self.test_ratio != 0:
+            # split_dataset2['train'] is the actual validation set
+            # split_dataset2['test'] is the actual test set
+            split_dataset2 = split_dataset['test'].train_test_split(
+                test_size=self.test_ratio / (self.val_ratio + self.test_ratio), seed=self.seed
+            )
+        elif self.val_ratio == 0 and self.test_ratio != 0:
+            split_dataset2['test'] = split_dataset['test']
+        elif self.test_ratio == 0 and self.val_ratio != 0:
+            split_dataset2['train'] = split_dataset['test']
+
+        if self.val_root is not None:
+            save_splits['validation'] = Dataset.from_list(json.load(open(self.val_root, 'r')))
+        else:
+            save_splits['validation'] = split_dataset2['train']
+
+        if self.test_root is not None:
+            save_splits['test'] = Dataset.from_list(json.load(open(self.test_root, 'r')))
+        else:
+            save_splits['test'] = split_dataset2['test']
 
         for split_name, dataset in save_splits.items():
             output_file = self.dataset_root / f"{split_name}.jsonl"
