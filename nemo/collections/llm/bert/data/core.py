c08f5933d182f37c1f71691e4d494cd22c45de99
dimapihtar <dpihtar@gmail.com>
2025-02-07 13:48:58 -0800

    fix imports
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- nemo/collections/llm/bert/data/core.py
+++ nemo/collections/llm/bert/data/core.py
@@ -21,8 +21,7 @@ import numpy as np
 import torch
 
 from nemo.collections.common.tokenizers import TokenizerSpec
-from nemo.collections.llm.gpt.data.core import JSONLMemMapDataset
-from nemo.collections.llm.gpt.data.utils import get_samples_mapping
+from nemo.collections.llm.gpt.data.utils import _JSONLMemMapDataset, _get_samples_mapping
 from nemo.core.classes import Dataset
 from nemo.lightning.base import NEMO_DATASETS_CACHE
 from nemo.utils import logging
