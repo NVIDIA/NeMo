a7cf130a3b47c5045a41ef702987f7ce3cfcff99
Abhishree <abhishreetm@gmail.com>
2025-02-07 19:01:25 -0800

    Add FastAPI v1/completions/ endpoint
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

---
+++ nemo/collections/llm/deploy/fastapi_interface_to_pytriton.py
@@ -0,0 +1,106 @@
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from pathlib import Path
+import requests
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+from nemo.deploy.nlp import NemoQueryLLMPyTorch
+from nemo.utils import logging
+
+
+class TritonSettings(BaseSettings):
+    _triton_service_port: int
+    _triton_service_ip: str
+
+    def __init__(self):
+        super(TritonSettings, self).__init__()
+        try:
+            self._triton_service_port = int(os.environ.get('TRITON_PORT', 8000))
+            self._triton_service_ip = os.environ.get('TRITON_HTTP_ADDRESS', '0.0.0.0')
+        except Exception as error:
+            logging.error("An exception occurred trying to retrieve set args in TritonSettings class. Error:", error)
+            return
+
+    @property
+    def triton_service_port(self):
+        return self._triton_service_port
+
+    @property
+    def triton_service_ip(self):
+        return self._triton_service_ip
+
+
+app = FastAPI()
+triton_settings = TritonSettings()
+
+
+class CompletionRequest(BaseModel):
+    model: str
+    prompt: str
+    max_tokens: int = 512
+    temperature: float = 1.0
+    top_p: float = 0.0
+    top_k: int = 1
+    logProb: bool = True
+
+
+@app.get("/v1/health")
+def health_check():
+    return {"status": "ok"}
+
+
+@app.get("/v1/triton_health")
+async def check_triton_health():
+    """
+    This method exposes endpoint "/triton_health" which can be used to verify if Triton server is accessible while running the REST or FastAPI application.
+    Verify by running: curl http://service_http_address:service_port/v1/triton_health and the returned status should inform if the server is accessible.
+    """
+    triton_url = (
+        f"http://{triton_settings.triton_service_ip}:{str(triton_settings.triton_service_port)}/v2/health/ready"
+    )
+    logging.info(f"Attempting to connect to Triton server at: {triton_url}")
+    try:
+        response = requests.get(triton_url, timeout=5)
+        if response.status_code == 200:
+            return {"status": "Triton server is reachable and ready"}
+        else:
+            raise HTTPException(status_code=503, detail="Triton server is not ready")
+    except requests.RequestException as e:
+        raise HTTPException(status_code=503, detail=f"Cannot reach Triton server: {str(e)}")
+
+
+@app.post("/v1/completions/")
+def completions_v1(request: CompletionRequest):
+    try:
+        print("---hello----")
+        url = triton_settings.triton_service_ip + ":" + str(triton_settings.triton_service_port)
+        nq = NemoQueryLLMPyTorch(url=url, model_name=request.model)
+        print("---request----", request)
+        output = nq.query_llm(
+            prompts=[request.prompt],
+            temperature=request.temperature,
+            top_k=request.top_k,
+            top_p=request.top_p,
+            compute_logprob=request.logProb,
+            max_length=request.max_tokens,
+        )
+        print("---output----", output)
+
+        return {
+            "output": output[0][0],
+        }
+    except Exception as error:
+        logging.error("An exception occurred with the post request to /v1/completions/ endpoint:", error)
+        return {"error": "An exception occurred"}
