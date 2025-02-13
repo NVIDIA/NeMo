990f4bd9f0cdd85f7ac5601808aba9b90b1d3e97
Charlie Truong <chtruong@nvidia.com>
2025-02-13 16:30:28 -0600

    Use pip --no-deps --force-reinstall when building the test container
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -141,7 +141,7 @@ nemo() {
   )
 
   echo 'Installing dependencies of nemo'
-  ${PIP} install --no-cache-dir --extra-index-url https://pypi.nvidia.com "${DEPS[@]}"
+  ${PIP} install --no-deps --force-reinstall --no-cache-dir --extra-index-url https://pypi.nvidia.com "${DEPS[@]}"
 
   echo 'Installing nemo itself'
   pip install --no-cache-dir --no-build-isolation $NEMO_DIR/.[all]
