f30372f9eac1893a69b19536b868d4fe5a9d86d7
Charlie Truong <chtruong@nvidia.com>
2025-02-14 17:39:03 -0600

    Fix more locations where weights_only=False needs to be passed to torch.load
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- nemo/utils/callbacks/nemo_model_checkpoint.py
+++ nemo/utils/callbacks/nemo_model_checkpoint.py
@@ -224,7 +224,7 @@ class NeMoModelCheckpoint(ModelCheckpoint):
 
             self.previous_best_path = self.best_model_path
             old_state_dict = deepcopy(pl_module.state_dict())
-            checkpoint = torch.load(maybe_injected_best_model_path, map_location='cpu')
+            checkpoint = torch.load(maybe_injected_best_model_path, map_location='cpu', weights_only=False)
             if 'state_dict' in checkpoint:
                 checkpoint = checkpoint['state_dict']
             # get a new instanace of the model
