842c2307e12a8231a2fe76fe12256988f0130970
Charlie Truong <chtruong@nvidia.com>
2025-02-14 16:08:00 -0600

    Remove unnecessary import of __futures__ in adapter_mixins
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- nemo/core/classes/mixins/adapter_mixins.py
+++ nemo/core/classes/mixins/adapter_mixins.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
-
 import inspect
 from abc import ABC
 from dataclasses import dataclass, is_dataclass
