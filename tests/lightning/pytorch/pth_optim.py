4ceacd986475a50114ba77164a6d9fe348c499c2
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-02-16 00:32:23 -0800

    copyright
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/lightning/pytorch/pth_optim.py
+++ tests/lightning/pytorch/pth_optim.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytest
 from unittest.mock import Mock
 import torch
@@ -10,7 +24,7 @@ class DummyModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.layer = torch.nn.Linear(10, 2)
-    
+
     def forward(self, x):
         return self.layer(x)
 
