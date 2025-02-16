b25cc82afaef11271dd1b2edc31cc4838b4d54ed
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-02-15 17:23:00 -0800

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/llm/hf/sft.py
+++ tests/collections/llm/hf/sft.py
@@ -70,6 +70,7 @@ def make_squad_hf_dataset(data_path, tokenizer):
 def make_strategy(strategy, model, devices, num_nodes, adapter_only=False):
     if strategy == 'auto':
         return pl.strategies.SingleDeviceStrategy(
+            device='cuda:0',
             checkpoint_io=model.make_checkpoint_io(adapter_only=adapter_only),
         )
     elif strategy == 'ddp':
