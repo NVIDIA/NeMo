527d19c983c01feb595f0b1b70aeeaa26e47480a
oliver könig <okoenig@nvidia.com>
2025-02-17 12:26:49 +0100

    f
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -136,7 +136,7 @@ nemo() {
   DEPS=(
     "nvidia-modelopt[torch]~=0.21.0; sys_platform == 'linux'"
     "nemo_run@git+https://github.com/NVIDIA/NeMo-Run.git@34259bd3e752fef94045a9a019e4aaf62bd11ce2"
-    "git+https://github.com/NVIDIA/nvidia-resiliency-ext.git@97aad77609d2e25ed38ac5c99f0c13f93c48464e"
+    "git+https://github.com/NVIDIA/nvidia-resiliency-ext.git@97aad77609d2e25ed38ac5c99f0c13f93c48464e ; platform_machine == 'x86_64'"
     "onnxscript @ git+https://github.com/microsoft/onnxscript"
     "llama-index==0.10.43"
     "unstructured==0.14.9"
