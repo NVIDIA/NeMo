bafefa8600087b758b1c1c8a7e2495fd298425d5
oliver könig <okoenig@nvidia.com>
2025-02-17 10:03:23 +0100

    build: `force-reinstall`
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -140,7 +140,7 @@ nemo() {
   )
 
   echo 'Installing dependencies of nemo'
-  ${PIP} install --no-cache-dir --extra-index-url https://pypi.nvidia.com "${DEPS[@]}"
+  ${PIP} install --force-reinstall --no-cache-dir --extra-index-url https://pypi.nvidia.com "${DEPS[@]}"
 
   echo 'Installing nemo itself'
   pip install --no-cache-dir --no-build-isolation $NEMO_DIR/.[all]
