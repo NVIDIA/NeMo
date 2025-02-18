d0aa90952698a728846ce7324e3d02b24c6992f9
Hemil Desai <hemild@nvidia.com>
2025-02-18 22:08:16 +0000

    fix
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- nemo/tron/optim.py
+++ nemo/tron/optim.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from megatron.core.optimizer import MegatronOptimizer, get_megatron_optimizer
 from megatron.core.optimizer_param_scheduler import OptimizerParamScheduler
 
