26122398b68e78605d91dfb21548042963bc1397
oliver könig <okoenig@nvidia.com>
2025-02-18 17:36:59 +0100

    f
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -131,7 +131,7 @@ nemo() {
   if [[ "$PLATFORM_MACHINE" == "x86_64" ]]; then
     ${PIP} install --no-cache-dir virtualenv &&
       virtualenv $INSTALL_DIR/venv &&
-      $INSTALL_DIR/venv/bin/pip install --no-cache-dir setuptools &&
+      $INSTALL_DIR/venv/bin/pip install --no-cache-dir setuptools coverage &&
       $INSTALL_DIR/venv/bin/pip install --no-cache-dir --no-build-isolation \
         -r $NEMO_DIR/requirements/requirements_vllm.txt \
         -r $NEMO_DIR/requirements/requirements_deploy.txt
