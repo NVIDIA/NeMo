1a5082516d832e5ff34c09583b1e36d331ca8df2
Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>
2025-02-21 10:31:28 +0100

    Mark test GPU-only
    Signed-off-by: Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>

--- tests/export/test_model_loading.py
+++ tests/export/test_model_loading.py
@@ -26,6 +26,7 @@ dummy_module = MagicMock()
 dummy_module.torch_to_numpy = lambda torch_tensor: torch_tensor.detach().cpu().numpy()
 
 
+@pytest.mark.run_only_on('GPU')
 @pytest.mark.unit
 def test_model_loading() -> None:
     """
