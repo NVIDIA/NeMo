79f2c36d7258d4b60bd32de7cb9c1ffb628f9e7c
Yuanhang Su <yuanhangsu@yuanhangs.nvidia.com>
2025-02-21 23:18:49 -0800

    add avlm module in collections.

---
+++ nemo/collections/avlm/data/audio_visual_to_text_module.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nemo.collections.asr.parts.preprocessing.perturb import process_augmentations as audio_process_augmentations 
+from nemo.collections.speechlm.data.audio_to_text_module import AudioToTextDataModule
+from nemo.collections.speechlm.data.dataset.audio_text_dataset import (
+    get_tarred_audio_visual_text_dataset_from_config,
+)
+
+from nemo.utils import logging
+
+
+class AudioVisualToTextDataModule(AudioToTextDataModule):
+    """
+    Data module for speech-image/video-to-text LLM.
+    """
+
+    def __init__(self, config: Union[DictConfig, Dict], tokenizer: TokenizerSpec):
+        super().__init__(config, tokenizer)
+        # image/video processor
+        self.visual_processor = None
+
+    def get_vision_processor():
+        # TODO: implement vision processor
+        return None
+
+    def setup(self, stage=None):
+        """
+        build datasets, data sample processor and data sampler
+        """
+        self.visual_processor = self.get_vision_processor()
+        super().setup(stage)
+        
+    @lru_cache
+    def _create_dataset(self, mode: str):
+        """
+        Create the datasets for each of train/validation/test/predict mode
+        """
+        data_cfg = self.cfg.get(f"{mode}_ds", None)
+        if data_cfg is None:
+            logging.info(f"Skipping {mode} dataset creation as it is not specified in the config: {self.cfg}")
+            return None
+
+        audio_augmentor = None
+        if 'audio' in data_cfg and 'augmentor' in data_cfg.get('audio'):
+            audio_augmentor = audio_process_augmentations(
+                data_cfg['audio']['augmentor'],
+                global_rank=parallel_state.get_data_parallel_rank(),
+                world_size=parallel_state.get_data_parallel_world_size(),
+            )
+
+        # TODO: implement the image/video augmentor
+        visual_augmentor = None            
+
+        # Notably, the data weights are controlled by either bucketing_weights
+        # or concat_sampling_probabilities depending on the dataset type.
+        if data_cfg.get("use_lhotse"):
+            logging.info(f"lhotse dataset is not yet supported. Skipping {mode} dataset creation .")
+            return None
+
+        setattr(self, f"_{mode}_names", data_cfg.get('name', None))
+
+        # Notably, the data weights are controlled by either bucketing_weights
+        # or concat_sampling_probabilities depending on the dataset type.
+        if data_cfg.get('is_tarred', False):
+            dataset = get_tarred_audio_visual_text_dataset_from_config(
+                config=data_cfg,
+                text_processor=self.text_processor,
+                visual_processor=self.visual_processor,
+                audio_augmentor=audio_augmentor,
+                visual_augmentor=visual_augmentor,
+                global_rank=parallel_state.get_data_parallel_rank(),
+                world_size=parallel_state.get_data_parallel_world_size(),
+            )
+        else:
+            # TODO: implement get_audio_text_dataset_from_config for non wds compliant dataset
+            logging.info(f"None tarred data is not yet supported. Skipping {mode} dataset creation.")
+            return None
+
+        if mode != 'train':
+            num_ds = len(dataset) if isinstance(dataset, list) else 1
+            setattr(self, f"_num_{mode}_dl", num_ds)
+        return dataset
