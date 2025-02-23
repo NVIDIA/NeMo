b05ff5cda5d847e5c841dbcb824044bcb6e77b9a
Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
2025-02-23 10:01:30 -0800

    [automodel] re-enable FSDP2 tests (#12325)
    * use /tmp for HF_HOME
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * re-enable tests
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * fix
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * fix fsdp2 strat
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * use _init_model_parallel & _setup_optimizers to bypass PTL init for PEFT
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * guard setup_optim with trainer.state
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * improve logs
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * use fsdp2 in vlm test
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    
    * Update hf_auto_model_for_image_text_to_text.py
    
    Signed-off-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
    
    * Update fsdp2_strategy.py
    
    Signed-off-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
    
    * add missing import
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * add auto-resume for peft test
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * update save_pretraned
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * make test mandatory
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * reduce accumulate_grad_batches 10 -> 2
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * add missing import
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * fix test
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Signed-off-by: akoumpa <akoumpa@users.noreply.github.com>
    Signed-off-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
    Co-authored-by: akoumpa <akoumpa@users.noreply.github.com>

--- nemo/collections/vlm/hf/model/hf_auto_model_for_image_text_to_text.py
+++ nemo/collections/vlm/hf/model/hf_auto_model_for_image_text_to_text.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import _io
 import lightning.pytorch as pl
 import torch
-from torch.distributed._composable.fsdp import MixedPrecisionPolicy
 from transformers import AutoConfig, AutoModelForImageTextToText, AutoProcessor
 
 from nemo.collections.llm import fn
@@ -133,10 +133,47 @@ class HFAutoModelForImageTextToText(pl.LightningModule, io.IOMixin, fn.FNMixin):
 
         self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
 
-    def save_pretrained(self, path):
-        """Saves checkpoint using HF"""
+    def save_pretrained(self, path, state_dict):
+        """
+        Save the pretrained model and tokenizer to a specified path.
+
+        This method ensures that the model state is gathered (especially in distributed settings)
+        and then saves the state dict and tokenizer. Only rank 0 or the appropriate FSDP module saves
+        the files to avoid race conditions.
+
+        Args:
+            path (str): The directory path where the model and tokenizer should be saved.
+
+        Raises:
+            AssertionError: If the model has not been created prior to saving.
+        """
         assert self.model is not None, "Model has to be created first."
-        self.model.save_pretrained(path)
+
+        def pop_fqn_prefix(fqn, expected_prefix='model'):
+            """pops prefix from FQN"""
+            parts = fqn.split('.')
+            assert parts[0] == expected_prefix
+            return '.'.join(parts[1:])
+
+        # Remove the "model." prefix from FQNs.
+        # Context: calling state_dict on an HFAutoModelForCausalLM, will prepend "model." in the
+        # state-dict keys. One solution would be to override HFAutoModelForCausalLM's state_dict
+        # and `return self.model.state_dict()`, however FSDP2 uses FQNs to acecss modules, therefore
+        # removing "model." at the state_dict function level will cause issues with FSDP2.
+        keys = list(state_dict.keys())
+        io_bytes_state = {}
+        for key, new_key in map(lambda x: (x, pop_fqn_prefix(x)), keys):
+            val = state_dict.pop(key)
+            if isinstance(val, _io.BytesIO):
+                io_bytes_state[new_key] = val
+            else:
+                state_dict[new_key] = val
+
+        if len(io_bytes_state) > 0:
+            logging.warning("State-dict contains _io.BytesIO, those will be saved separately to `io_bytes.pt`.")
+            torch.save(io_bytes_state, path / 'io_bytes.pt')
+
+        self.model.save_pretrained(path, state_dict=state_dict)
         if self._processor is not None:
             self._processor.save_pretrained(path)
         else:
@@ -190,3 +227,14 @@ class HFAutoModelForImageTextToText(pl.LightningModule, io.IOMixin, fn.FNMixin):
     @property
     def _has_lora_adapter(self):
         return any(map(lambda x: 'lora' in x[0].lower(), self.named_modules()))
+
+    def make_checkpoint_io(self, adapter_only=False):
+        """
+        Creates a checkpoint_io object for this model;
+        the main issue is passing self to the HFCheckpointIO, because it needs
+        to call save_pretrained within.
+        TODO(@akoumparouli): refactor ^
+        """
+        from nemo.lightning.io.hf import HFCheckpointIO
+
+        return HFCheckpointIO(model=self, adapter_only=adapter_only)
