0ce235d34f8ed1da411e1c7093d5957f54c8cf16
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-02-24 10:49:29 -0800

    make lr scheduler
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/lightning/pytorch/optim/pytorch.py
+++ nemo/lightning/pytorch/optim/pytorch.py
@@ -118,10 +118,14 @@ class PytorchOptimizerModule(OptimizerModule):
             raise ValueError("Model cannot be an instance of MegatronParallel")
 
         wd = self.optimizer_fn.keywords.get('weight_decay', 0)
-        ans = self.optimizer_fn(_extract_model_params_for_optim(model, wd, self.no_weight_decay_cond))
-        if not isinstance(ans, list):
-            ans = [ans]
-        return ans
+        optim = self.optimizer_fn(_extract_model_params_for_optim(model, wd, self.no_weight_decay_cond))
+        self._optimizers = optim
+        if not isinstance(optim, list):
+            optim = [optim]
+        if self.lr_scheduler is None:
+            return optim
+        else:
+            return [self.lr_scheduler.scheduler(model, opt) for opt in optim]
 
     def connect(self, model: L.LightningModule) -> None:
         """Connects the optimizer module to the model.
