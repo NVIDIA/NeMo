2ceb6c994df571af09122924dbf83151a217c11c
Youngeun Kwon <youngeunk@nvidia.com>
2025-02-26 15:11:01 -0800

    modify the comments and assertion text
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- nemo/lightning/megatron_parallel.py
+++ nemo/lightning/megatron_parallel.py
@@ -689,11 +689,11 @@ class MegatronParallel(nn.ModuleList, Generic[ModelT]):
             )  # We need to do this explicitly since this is a attr pytorch uses
             model_chunk.__class__.__getattr__ = getattr_proxy  # type: ignore
 
-            # Ensure that if using custom FSDP, gradient_accumulation_fusion is disabled on the model config.
+            # Ensure that if using FSDP, gradient_accumulation_fusion is disabled on the model config.
             if self.ddp_config.use_custom_fsdp:
                 assert (
                     module.config.gradient_accumulation_fusion == False
-                ), "gradient_accumulation_fusion must be False when using custom FSDP"
+                ), "gradient_accumulation_fusion cannot be used with FSDP"
 
         # param_sync_func is set in nemo.lightning.pytorch.optim.megatron
         no_sync_func, grad_sync_func = extract_ddp_funcs(self.ddp_config, self)
