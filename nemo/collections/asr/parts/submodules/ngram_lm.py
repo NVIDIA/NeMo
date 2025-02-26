b8bb31b5048e073cf03af0115953428bb9d73c0e
artbataev <artbataev@users.noreply.github.com>
2025-02-26 19:02:23 +0000

    Apply isort and black reformatting
    Signed-off-by: artbataev <artbataev@users.noreply.github.com>

--- nemo/collections/asr/parts/submodules/ngram_lm.py
+++ nemo/collections/asr/parts/submodules/ngram_lm.py
@@ -775,7 +775,9 @@ class FastNGramLM(ModelPT):
         for i in range(max_length):
             # TODO(vbataev): support differentiable implementation with Triton
             step_scores, states = self._advance_pytorch(states)
-            scores[:, i] = step_scores.gather(dim=1, index=labels[:, i].unsqueeze(-1)).squeeze(-1) * (i < labels_lengths)
+            scores[:, i] = step_scores.gather(dim=1, index=labels[:, i].unsqueeze(-1)).squeeze(-1) * (
+                i < labels_lengths
+            )
             states = states.gather(dim=1, index=labels[:, i].unsqueeze(-1)).squeeze(-1)
         return scores
 
