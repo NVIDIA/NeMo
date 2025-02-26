a0ea8f16a23b6a8be5456bd4befe36b28084f93b
Malay Nagda <malayn@nvidia.com>
2025-02-26 22:39:27 +0530

    updated cfgs
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/llm/finetune_llama31_405b.py
+++ scripts/performance/llm/finetune_llama31_405b.py
@@ -78,7 +78,7 @@ def override_recipe_configs(
         recipe.trainer.plugins = bf16_with_fp8_mixed()
         recipe.trainer.plugins.grad_reduce_in_fp32 = False
 
-    enable_cuda_graph = bool(args.gpu.lower() in ["b200", "gb200"])
+    enable_cuda_graph = bool(args.gpu.lower() in ["gb200"])
     recipe.model.config.enable_cuda_graph = enable_cuda_graph
     recipe.trainer.strategy.use_te_rng_tracker = enable_cuda_graph
     recipe.data.packed_sequence_specs.pad_cu_seqlens = enable_cuda_graph
