081ae40d639931e5f9b0202a51a4dd575e298fa7
JRD971000 <JRD971000@users.noreply.github.com>
2025-02-26 18:04:57 +0000

    Apply isort and black reformatting
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>

--- tests/lightning/pytorch/callbacks/test_flops_callback.py
+++ tests/lightning/pytorch/callbacks/test_flops_callback.py
@@ -1,16 +1,17 @@
+import lightning.pytorch as pl
 import pytest
 import torch
-import lightning.pytorch as pl
-from nemo.lightning.pytorch.callbacks.flops_callback import FLOPsMeasurementCallback
+
 from nemo.collections.llm.gpt.model.base import GPTConfig
 from nemo.collections.llm.gpt.model.hyena import HyenaConfig
+from nemo.lightning.pytorch.callbacks.flops_callback import FLOPsMeasurementCallback
+
 
 class MockDataModule:
     def __init__(self, global_batch_size, vocab_size):
         self.global_batch_size = global_batch_size
         self.tokenizer = self
         self.vocab_size = vocab_size
-
 
 
 def test_flops_measurement_callback_bert():
@@ -21,10 +22,11 @@ def test_flops_measurement_callback_bert():
         ffn_hidden_size=3072,
         num_attention_heads=12,
         moe_router_topk=0,
-        num_query_groups=12)
+        num_query_groups=12,
+    )
 
     train_step_time = 1.23
-    global_batch_size=1
+    global_batch_size = 1
     num_devices = torch.distributed.get_world_size() if torch.distributed.is_initialized() else 1
     model_name = "bert"
     data_module = MockDataModule(global_batch_size=global_batch_size, vocab_size=100)
@@ -35,7 +37,7 @@ def test_flops_measurement_callback_bert():
     expected_flops_per_gpu = expected_total_flops / num_devices
 
     assert total_flops == expected_total_flops
-    assert flops_per_gpu ==  expected_flops_per_gpu
+    assert flops_per_gpu == expected_flops_per_gpu
 
     tflops_per_sec_per_gpu = callback.eval_tflops_per_sec_per_gpu(train_step_time)
     expected_tflops_per_sec_per_gpu = expected_flops_per_gpu / (1e12 * train_step_time)
