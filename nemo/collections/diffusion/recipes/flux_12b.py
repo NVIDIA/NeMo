e2b0ae0e59d72bde9c61e1337317b3d5d58274d0
Lifu Zhang <tomzhanglf@gmail.com>
2025-03-04 14:48:15 -0800

    fix linting issue
    Signed-off-by: Lifu Zhang <tomzhanglf@gmail.com>

--- nemo/collections/diffusion/recipes/flux_12b.py
+++ nemo/collections/diffusion/recipes/flux_12b.py
@@ -22,10 +22,8 @@ from megatron.core.distributed import DistributedDataParallelConfig
 from megatron.core.optimizer import OptimizerConfig
 
 from nemo import lightning as nl
-from nemo.collections import llm
 from nemo.collections.diffusion.data.diffusion_mock_datamodule import MockDataModule
 from nemo.collections.diffusion.models.flux.model import FluxModelParams, MegatronFluxModel
-from nemo.collections.diffusion.vae.autoencoder import AutoEncoderConfig
 from nemo.collections.llm.api import pretrain
 from nemo.collections.llm.recipes.log.default import default_log, tensorboard_logger
 from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed
