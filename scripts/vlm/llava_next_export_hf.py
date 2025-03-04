ffc8c68d7e63ca86528d337a0bf25f05faaf1289
abhinavg4 <abhinavg4@users.noreply.github.com>
2025-03-04 23:52:32 +0000

    Apply isort and black reformatting
    Signed-off-by: abhinavg4 <abhinavg4@users.noreply.github.com>

--- scripts/vlm/llava_next_export_hf.py
+++ scripts/vlm/llava_next_export_hf.py
@@ -2,19 +2,20 @@ import argparse
 from pathlib import Path
 from nemo.collections import llm
 
+
 def main():
     parser = argparse.ArgumentParser(description="Export NeMo checkpoint to Hugging Face format.")
     parser.add_argument(
         "--path",
         type=str,
         default="/root/.cache/nemo/models/llava-hf/llava-v1.6-vicuna-7b-hf",
-        help="Path to the NeMo checkpoint directory. (Default: /root/.cache/nemo/models/llava-hf/llava-v1.6-vicuna-7b-hf)"
+        help="Path to the NeMo checkpoint directory. (Default: /root/.cache/nemo/models/llava-hf/llava-v1.6-vicuna-7b-hf)",
     )
     parser.add_argument(
         "--output_path",
         type=str,
         default="path/to/converted/hf/ckpt",
-        help="Path to save the converted Hugging Face checkpoint. (Default: path/to/converted/hf/ckpt)"
+        help="Path to save the converted Hugging Face checkpoint. (Default: path/to/converted/hf/ckpt)",
     )
 
     args = parser.parse_args()
@@ -24,6 +25,7 @@ def main():
         target='hf',
         output_path=Path(args.output_path),
     )
+
 
 if __name__ == '__main__':
     main()
