d45aec5b95b85eafeb5d23943a0338e39a9ee7a1
Onur Yilmaz <35306097+oyilmaz-nvidia@users.noreply.github.com>
2025-03-07 08:49:27 -0500

    Oyilmaz nvidia/chore/cherry pick 12242 (#12523)
    * fix merge conflicts
    
    Signed-off-by: Onur Yilmaz <oyilmaz@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: oyilmaz-nvidia <oyilmaz-nvidia@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Onur Yilmaz <oyilmaz@nvidia.com>
    Signed-off-by: oyilmaz-nvidia <oyilmaz-nvidia@users.noreply.github.com>
    Co-authored-by: oyilmaz-nvidia <oyilmaz-nvidia@users.noreply.github.com>

--- nemo/collections/llm/gpt/model/__init__.py
+++ nemo/collections/llm/gpt/model/__init__.py
@@ -47,6 +47,8 @@ from nemo.collections.llm.gpt.model.gemma2 import (
     Gemma2Model,
 )
 from nemo.collections.llm.gpt.model.hf_auto_model_for_causal_lm import HFAutoModelForCausalLM
+from nemo.collections.llm.gpt.model.hf_llama_embedding import get_llama_bidirectional_hf_model
+
 from nemo.collections.llm.gpt.model.llama import (
     CodeLlamaConfig7B,
     CodeLlamaConfig13B,
@@ -208,4 +210,5 @@ __all__ = [
     "transformer_engine_full_layer_spec",
     "local_layer_spec",
     "HFAutoModelForCausalLM",
+    "get_llama_bidirectional_hf_model",
 ]
