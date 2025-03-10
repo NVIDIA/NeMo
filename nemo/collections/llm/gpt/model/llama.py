76435cef23da968476a6618bffb302b8d10af984
Ao Tang <aot@nvidia.com>
2025-03-10 16:39:18 -0400

    FIx Llama3.1/Llama3.2 Exporter (#12497)
    * FIx Exporter
    
    Signed-off-by: Ao Tang <aot@nvidia.com>
    
    * token id update
    
    Signed-off-by: Ao Tang <aot@nvidia.com>
    
    ---------
    
    Signed-off-by: Ao Tang <aot@nvidia.com>

--- nemo/collections/llm/gpt/model/llama.py
+++ nemo/collections/llm/gpt/model/llama.py
@@ -531,6 +531,7 @@ class HFLlamaImporter(io.ModelConnector["LlamaForCausalLM", LlamaModel]):
             init_method_std=source.initializer_range,
             layernorm_epsilon=source.rms_norm_eps,
             num_query_groups=source.num_key_value_heads,
+            seq_length=source.max_position_embeddings,
             rotary_base=source.rope_theta,
             gated_linear_unit=True,
             make_vocab_size_divisible_by=make_vocab_size_divisible_by(source.vocab_size),
@@ -643,6 +644,16 @@ class HFLlamaExporter(io.ModelConnector[LlamaModel, "LlamaForCausalLM"]):
 
         from transformers import LlamaConfig as HFLlamaConfig
 
+        rope_scaling = None
+        # For Llama 3.1 and Llama 3.2, rope_scaling is used and thus needed to parsed to the config
+        if isinstance(source, Llama31Config):
+            rope_scaling = {
+                'factor': source.scale_factor,
+                'low_freq_factor': source.low_freq_factor,
+                'high_freq_factor': source.high_freq_factor,
+                'original_max_position_embeddings': source.old_context_len,
+                'rope_type': 'llama3',
+            }
         return HFLlamaConfig(
             num_hidden_layers=source.num_layers,
             hidden_size=source.hidden_size,
@@ -655,6 +666,9 @@ class HFLlamaExporter(io.ModelConnector[LlamaModel, "LlamaForCausalLM"]):
             rope_theta=source.rotary_base,
             vocab_size=self.tokenizer.vocab_size,
             tie_word_embeddings=source.share_embeddings_and_output_weights,
+            rope_scaling=rope_scaling,
+            bos_token_id=self.tokenizer.bos_id,
+            eos_token_id=self.tokenizer.eos_id,
         )
 
 
