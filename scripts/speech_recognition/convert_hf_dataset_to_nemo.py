0f3c1de4b73e7da92b79f0a4a266e7d993587347
Charlie Truong <chtruong@nvidia.com>
2025-03-12 14:22:24 -0500

    Publish r2.2.0 (#12583)
    * Revert "ASR_CTC_Language_Finetuning.ipynb bugfix (#12538)"
    
    This reverts commit ccdfb81a8bb429177624903c4e383228a72676c1.
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>
    
    * Revert "update readme to include latest pytorch version (#12539) (#12577)"
    
    This reverts commit f6b58bccedffd3b13c13731bd3fee5593ebd20ce.
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>
    
    * Remove pre-release from package_info
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>
    
    ---------
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- scripts/speech_recognition/convert_hf_dataset_to_nemo.py
+++ scripts/speech_recognition/convert_hf_dataset_to_nemo.py
@@ -84,13 +84,14 @@ python convert_hf_dataset_to_nemo.py \
 import json
 import os
 import traceback
-from dataclasses import dataclass, field, is_dataclass
+from dataclasses import dataclass, is_dataclass
 from typing import Optional
 
 import hydra
 import librosa
 import soundfile
 import tqdm
+from datasets import Audio, Dataset, IterableDataset, load_dataset
 from hydra.conf import HydraConf, RunDir
 from hydra.core.config_store import ConfigStore
 from omegaconf import OmegaConf
@@ -117,7 +118,7 @@ class HFDatasetConversionConfig:
     resolved_output_dir: str = ''
     split_output_dir: Optional[str] = None
 
-    hydra: HydraConf = field(default_factory=lambda: HydraConf(run=RunDir(dir=".")))
+    hydra: HydraConf = HydraConf(run=RunDir(dir="."))
 
 
 def prepare_output_dirs(cfg: HFDatasetConversionConfig):
