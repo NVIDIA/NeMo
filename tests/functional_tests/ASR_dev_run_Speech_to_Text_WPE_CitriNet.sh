eb3751ac996452a103b394e82a79ee803d457070
oliver könig <okoenig@nvidia.com>
2025-03-13 12:49:15 +0100

    f
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/functional_tests/ASR_dev_run_Speech_to_Text_WPE_CitriNet.sh
+++ tests/functional_tests/ASR_dev_run_Speech_to_Text_WPE_CitriNet.sh
@@ -1,1 +1,12 @@
-bash tests/functional_tests/ASR_dev_run_Speech_to_Text_WPE_CitriNet.sh
+#!/bin/bash
+
+python examples/asr/asr_ctc/speech_to_text_ctc_bpe.py \
+    --config-path="../conf/citrinet/" --config-name="config_bpe" \
+    model.train_ds.manifest_filepath=/home/TestData/an4_dataset/an4_train.json \
+    model.validation_ds.manifest_filepath=/home/TestData/an4_dataset/an4_val.json \
+    model.tokenizer.dir="/home/TestData/asr_tokenizers/an4_wpe_128/" \
+    model.tokenizer.type="wpe" \
+    trainer.devices=1 \
+    trainer.accelerator="gpu" \
+    +trainer.fast_dev_run=True \
+    exp_manager.exp_dir=/tmp/speech_to_text_wpe_results
--- tests/functional_tests/L2_Community_vita_Checkpoints_tests_Llama3.sh
+++ tests/functional_tests/L2_Community_vita_Checkpoints_tests_Llama3.sh
@@ -1,1 +1,14 @@
-bash tests/functional_tests/L2_Community_vita_Checkpoints_tests_Llama3.sh
+#!/bin/bash
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+mkdir /tmp/${UUID}
+export PYTHONPATH=/home/TestData/multimodal/video_neva/LLaVA:$PYTHONPATH
+CUDA_VISIBLE_DEVICES=0 python examples/multimodal/multimodal_llm/neva/convert_llava_to_neva.py \
+    --in-file /home/TestData/multimodal/video_neva/Llama-3-VILA1.5-8B/llm \
+    --mm-projector-ckpt-dir /home/TestData/multimodal/video_neva/Llama-3-VILA1.5-8B/mm_projector \
+    --mm-vision-tower /home/TestData/multimodal/video_neva/Llama-3-VILA1.5-8B/vision_tower \
+    --tokenizer-model /home/TestData/multimodal/video_neva/vita-tokenizer/ \
+    --config-file vita_config.yaml \
+    --out-file=/tmp/${UUID}/llama3_ci.nemo \
+    --model-type VITA \
+    --conv-template llama_3
