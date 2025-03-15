4b6e63e9688a60a12b08186b6a50293689dde80b
oliver könig <okoenig@nvidia.com>
2025-03-15 16:07:38 +0100

    f
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/functional_tests/L2_G2P_Models_G2P_Conformer_training_evaluation_and_inference.sh
+++ tests/functional_tests/L2_G2P_Models_G2P_Conformer_training_evaluation_and_inference.sh
@@ -1,3 +1,16 @@
+# Copyright (c) 2020-2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 cd examples/tts/g2p &&
     TIME=$(date +"%Y-%m-%d-%T") && OUTPUT_DIR_CONFORMER=output_ctc_${TIME} &&
     coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo g2p_train_and_evaluate.py \
