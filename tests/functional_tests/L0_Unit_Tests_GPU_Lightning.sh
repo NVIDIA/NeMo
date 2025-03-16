c5196450825cc9c2be1cb173e8caff32d1ecd99d
Pablo Garay <pagaray@nvidia.com>
2025-03-15 21:42:37 -0700

    Update CR notice

--- tests/functional_tests/L0_Unit_Tests_GPU_Lightning.sh
+++ tests/functional_tests/L0_Unit_Tests_GPU_Lightning.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2021, NVIDIA CORPORATION.
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
