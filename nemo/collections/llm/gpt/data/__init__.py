715e242e236a5236fc45ad21f6b7f9ceac334b71
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-03-17 12:35:51 -0700

    surface HFMockDataModuel
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/llm/gpt/data/__init__.py
+++ nemo/collections/llm/gpt/data/__init__.py
@@ -16,7 +16,7 @@ from nemo.collections.llm.gpt.data.alpaca import AlpacaDataModule
 from nemo.collections.llm.gpt.data.chat import ChatDataModule
 from nemo.collections.llm.gpt.data.dolly import DollyDataModule
 from nemo.collections.llm.gpt.data.fine_tuning import FineTuningDataModule
-from nemo.collections.llm.gpt.data.hf_dataset import HFDatasetDataModule
+from nemo.collections.llm.gpt.data.hf_dataset import HFDatasetDataModule, HFMockDataModule
 from nemo.collections.llm.gpt.data.mlperf_govreport import MLPerfGovReportDataModule
 from nemo.collections.llm.gpt.data.mock import MockDataModule
 from nemo.collections.llm.gpt.data.pre_training import PreTrainingDataModule, build_pretraining_datamodule
@@ -29,6 +29,7 @@ __all__ = [
     "DollyDataModule",
     "FineTuningDataModule",
     "HFDatasetDataModule",
+    "HFMockDataModule",
     "MLPerfGovReportDataModule",
     "MockDataModule",
     "PreTrainingDataModule",
