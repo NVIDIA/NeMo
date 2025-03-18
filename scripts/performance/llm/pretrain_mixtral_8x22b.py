f9d752a4dc0cd0c9ee91bac4acf095a0032be592
Malay Nagda <malayn@nvidia.com>
2025-03-18 20:28:24 +0530

    mixtral kwargs extra arg
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/llm/pretrain_mixtral_8x22b.py
+++ scripts/performance/llm/pretrain_mixtral_8x22b.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     args_sanity_check(args)
 
     kwargs = get_user_configs(args.gpu.lower(), "pre_train", "mixtral", "8x22b", args)
-    num_nodes, mbs, gbs, tp_size, pp_size, cp_size, vp_size, ep_size, etp_size, _, enable_cuda_graphs = kwargs
+    num_nodes, mbs, gbs, tp_size, pp_size, cp_size, vp_size, ep_size, etp_size, enable_cuda_graphs = kwargs
 
     recipe = override_recipe_configs(
         args, num_nodes, mbs, gbs, tp_size, pp_size, cp_size, vp_size, ep_size, etp_size, enable_cuda_graphs
