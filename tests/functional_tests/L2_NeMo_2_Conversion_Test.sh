8f77a09f7e39ee957f9fcd0e4ed50dd7cff76d31
Ao Tang <aot@nvidia.com>
2025-03-19 12:32:14 -0400

    refactor
    Signed-off-by: Ao Tang <aot@nvidia.com>

--- tests/functional_tests/L2_NeMo_2_Conversion_Test.sh
+++ tests/functional_tests/L2_NeMo_2_Conversion_Test.sh
@@ -11,11 +11,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-HF_ORI_PATH=/home/TestData/llm/models/starcoder_toy
-NEMO_MODEL_TYPE=StarcoderModel
-NEMO_MODEL_CONFIG=StarcoderConfig15B
-NEMO_OUTPUT_PATH=/tmp/output_nemo2_ckpt
-HF_OUTPUT_PATH=/tmp/output_hf_ckpt
+# Parse passed arguments
+for i in "$@"; do
+  case $i in
+    HF_ORI_PATH=*)
+      HF_ORI_PATH="${i#*=}"
+      ;;
+    NEMO_MODEL_TYPE=*)
+      NEMO_MODEL_TYPE="${i#*=}"
+      ;;
+    NEMO_MODEL_CONFIG=*)
+      NEMO_MODEL_CONFIG="${i#*=}"
+      ;;
+    NEMO_OUTPUT_PATH=*)
+      NEMO_OUTPUT_PATH="${i#*=}"
+      ;;
+    HF_OUTPUT_PATH=*)
+      HF_OUTPUT_PATH="${i#*=}"
+      ;;
+    add-model-name)
+      ADD_MODEL_NAME="--add-model-name"
+      ;;
+    *)
+      # Handle other arguments or errors here
+      echo "Unknown argument: $i"
+      exit 1
+      ;;
+  esac
+done
 
 coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/conversion/test_import_from_hf.py --hf-path=${HF_ORI_PATH} --model-type=${NEMO_MODEL_TYPE} --model-config=${NEMO_MODEL_CONFIG} --output-path=${NEMO_OUTPUT_PATH}
-coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/conversion/test_export_to_hf.py --nemo-path=${NEMO_OUTPUT_PATH} --original-hf-path=${HF_ORI_PATH} --output-path=${HF_OUTPUT_PATH}
+coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/conversion/test_export_to_hf.py --nemo-path=${NEMO_OUTPUT_PATH} --original-hf-path=${HF_ORI_PATH} --output-path=${HF_OUTPUT_PATH} $ADD_MODEL_NAME
