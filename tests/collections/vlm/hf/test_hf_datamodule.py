8af874a06bad0601879c990830469032bde04504
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-03-20 23:07:10 -0700

    a
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/vlm/hf/test_hf_datamodule.py
+++ tests/collections/vlm/hf/test_hf_datamodule.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest.mock import MagicMock
 import pytest
 import torch
 from datasets import Dataset, DatasetDict
