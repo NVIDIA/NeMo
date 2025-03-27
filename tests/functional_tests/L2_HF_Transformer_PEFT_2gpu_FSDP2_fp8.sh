4212d1a54d336d9c9bc501b3e8364d0d8196b3e2
oliver könig <okoenig@nvidia.com>
2025-03-27 01:02:59 +0100

    f
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/functional_tests/L2_HF_Transformer_PEFT_2gpu_FSDP2_fp8.sh
+++ tests/functional_tests/L2_HF_Transformer_PEFT_2gpu_FSDP2_fp8.sh
@@ -1,4 +1,6 @@
-TRANSFORMERS_OFFLINE=1 HF_HOME=/home/TestData/automodel/hf_home python examples/llm/peft/automodel.py \
+export TRANSFORMERS_OFFLINE=1
+export HF_HOME=/home/TestData/automodel/hf_home
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo examples/llm/peft/automodel.py \
     --model /home/TestData/akoumparouli/hf_mixtral_2l/ \
     --max-steps 3 \
     --devices 2 \
