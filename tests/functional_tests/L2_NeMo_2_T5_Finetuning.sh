c7c2c3679688d2d2f7e418e60f3247e8474d3f5a
Huy Vu2 <huvu@login-eos01.eos.clusters.nvidia.com>
2025-03-28 10:57:25 -0700

    test

--- tests/functional_tests/L2_NeMo_2_T5_Finetuning.sh
+++ tests/functional_tests/L2_NeMo_2_T5_Finetuning.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/megatron_t5_finetuning.py \
-    --devices=2 \
+    --devices=1 \
     --max-steps=250 \
     --experiment-dir=tests/collections/llm/t5_finetune_results/$RUN_ID \
     --checkpoint-path=/home/TestData/nlp/megatron_t5/220m/nemo2.0_t5_220m_padding_attnmasktype_150steps
