4655a35b81cbec85adeeb09c9b273460d44b5d7e
Abhishree <abhishreetm@gmail.com>
2025-03-31 10:01:42 -0700

    Remove --save_dir L0_Unit_Tests_Eval.sh
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- tests/functional_tests/L0_Unit_Tests_Eval.sh
+++ tests/functional_tests/L0_Unit_Tests_Eval.sh
@@ -11,5 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo -m tests.evaluation.eval_unittest \
- --save_dir /home/TestData/eval
+coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo -m tests.evaluation.eval_unittest
