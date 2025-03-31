24670e48114316c9fe8c25526e42351ec0588d71
Hemil Desai <hemild@nvidia.com>
2025-03-30 19:52:37 -0700

    qwen
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- nemo/tron/converter/llama.py
+++ nemo/tron/converter/llama.py
@@ -1,8 +1,5 @@
 import logging
-from functools import partial
 from typing import TYPE_CHECKING
-
-import torch
 
 from nemo.collections.llm.gpt.model.llama import HFLlamaExporter as _NeMo2HFLlamaExporter
 from nemo.collections.llm.gpt.model.llama import HFLlamaImporter as _NeMo2HFLlamaImporter
@@ -13,7 +10,6 @@ from nemo.collections.llm.gpt.model.llama import (
 from nemo.tron.converter.common import (
     BaseExporter,
     BaseImporter,
-    dtype_from_hf,
 )
 
 if TYPE_CHECKING:
@@ -104,40 +100,5 @@ class HFLlamaImporter(BaseImporter):
         if self._tron_config is not None:
             return self._tron_config
 
-        from transformers import GenerationConfig
-
-        source = self.hf_config
-        generation_config = GenerationConfig.from_pretrained(str(self.input_path))
-
-        def make_vocab_size_divisible_by(vocab_size):
-            base = 128
-            while vocab_size % base != 0:
-                base //= 2
-            return base
-
-        if getattr(source, "rope_scaling", None) is not None and source.rope_scaling.get("rope_type") == "llama3":
-            # Apply Llama3.1 customize rope scaling
-            cls = partial(Llama31Config, scale_factor=source.rope_scaling.get("factor", 8.0))
-        else:
-            cls = LlamaConfig
-        output = cls(
-            num_layers=source.num_hidden_layers,
-            hidden_size=source.hidden_size,
-            ffn_hidden_size=source.intermediate_size,
-            num_attention_heads=source.num_attention_heads,
-            init_method_std=source.initializer_range,
-            layernorm_epsilon=source.rms_norm_eps,
-            num_query_groups=source.num_key_value_heads,
-            seq_length=source.max_position_embeddings,
-            rotary_base=source.rope_theta,
-            gated_linear_unit=True,
-            make_vocab_size_divisible_by=make_vocab_size_divisible_by(source.vocab_size),
-            share_embeddings_and_output_weights=getattr(source, "tie_word_embeddings", False),
-            fp16=(dtype_from_hf(source) == torch.float16),
-            bf16=(dtype_from_hf(source) == torch.bfloat16),
-            params_dtype=dtype_from_hf(source),
-            generation_config=generation_config,
-            vocab_size=self.tokenizer.vocab_size,
-        )
-        self._tron_config = output
+        self._tron_config = _NeMo2HFLlamaImporter.config.fget(self.input_path)
         return self._tron_config
