8420e498fc99a1892908840a72c00a97db0772ec
Jay Yang <jayya@nvidia.com>
2025-04-02 15:37:18 -0700

    refactor: reorder function, minior fix

--- nemo/core/connectors/save_restore_connector.py
+++ nemo/core/connectors/save_restore_connector.py
@@ -603,6 +603,17 @@ class SaveRestoreConnector:
             os.makedirs(dirname, exist_ok=True)
             with tarfile.open(filename, "w:") as tar:
                 tar.add(source_dir, arcname=".")
+    
+    @staticmethod
+    def _make_nemo_file_from_folder_with_multistorageclient(filename, source_dir):
+        filename_with_extension = filename.split("/")[-1]  # get the filename and extension
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tar_file = os.path.join(tmpdir, filename_with_extension)
+            with tarfile.open(tar_file, "w:") as tar:
+                tar.add(source_dir, arcname=".")
+                start_time = time.time()
+                multistorageclient.upload_file(filename, tar_file)
+                logging.debug(f"time spent for multistorageclient.upload from {tar_file} to {filename}: {time.time() - start_time:.4f}")
 
     @staticmethod
     def _is_safe_path(member, extract_to):
@@ -696,6 +707,7 @@ class SaveRestoreConnector:
         return out_folder
 
     @staticmethod
+
     def _save_state_dict_to_disk(state_dict, filepath):
         torch.save(state_dict, filepath)
 
@@ -748,6 +760,14 @@ class SaveRestoreConnector:
                 SaveRestoreConnector._safe_extract(tar, out_folder, members)
             tar.close()
         return out_folder
+
+    @staticmethod
+    def _save_state_dict_to_disk(state_dict, filepath):
+        torch.save(state_dict, filepath)
+
+    @staticmethod
+    def _load_state_dict_from_disk(model_weights, map_location=None):
+        return torch.load(model_weights, map_location='cpu', weights_only=False)
 
     @property
     def model_config_yaml(self) -> str:
