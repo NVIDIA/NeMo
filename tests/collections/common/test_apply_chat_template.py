c83de0fb019a9c4c91dd30f9f94e1a938ffa2e38
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-04-04 15:50:37 -0700

    skip test if transformers not installed
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/collections/common/test_apply_chat_template.py
+++ tests/collections/common/test_apply_chat_template.py
@@ -13,14 +13,13 @@
 # limitations under the License.
 
 import pytest
-from transfomers import AutoTokenizer
-
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_tokenizer
 
 
 def test_chat_template():
+    transformers = pytest.importorskip("transformers")
     path = "/home/TestData/akoumparouli/tokenizer_with_chat_template/"
-    tokenizers = [get_tokenizer(path), AutoTokenizer.from_pretrained(path)]
+    tokenizers = [get_tokenizer(path), transformers.AutoTokenizer.from_pretrained(path)]
     prompt = "Give me a short introduction to pytest."
     messages = [{"role": "system", "content": "You are a helpful CI assistant."}, {"role": "user", "content": prompt}]
     texts = [
