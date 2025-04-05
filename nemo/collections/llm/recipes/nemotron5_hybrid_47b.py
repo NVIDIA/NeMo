7020c3c1f0f813ce0c6a07776d4cb6b1b15fa867
Ali Taghibakhshi <71892896+JRD971000@users.noreply.github.com>
2025-04-05 15:20:02 -0500

    add nemotron5 (#12660)
    * add nemotron5
    
    * add recipes
    
    * address copilot comments
    
    * Add guard to quantizer import
    
    * modify recipes for nm5
    
    * modify test and ssm
    
    * fix HF and CS
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: artbataev <artbataev@users.noreply.github.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * Update quantizer.py
    
    Signed-off-by: Ali Taghibakhshi <71892896+JRD971000@users.noreply.github.com>
    
    * fix tests
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * fix generate
    
    * Add docstring and linting fixes
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    * fix linting
    
    * change mcore hash and add tests
    
    * Apply isort and black reformatting
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: JRD971000 <JRD971000@users.noreply.github.com>
    Signed-off-by: artbataev <artbataev@users.noreply.github.com>
    Signed-off-by: Ali Taghibakhshi <71892896+JRD971000@users.noreply.github.com>
    Co-authored-by: Ali Taghibakhshi <ataghibakhsh@cw-dfw-cs-001-vscode-01.cm.cluster>
    Co-authored-by: JRD971000 <JRD971000@users.noreply.github.com>
    Co-authored-by: artbataev <artbataev@users.noreply.github.com>

--- nemo/collections/llm/recipes/nemotron5_hybrid_47b.py
+++ nemo/collections/llm/recipes/nemotron5_hybrid_47b.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,59 +27,57 @@ from nemo.collections.llm.api import finetune, pretrain
 from nemo.collections.llm.gpt.data.mock import MockDataModule
 from nemo.collections.llm.recipes.log.default import default_log, default_resume, tensorboard_logger
 from nemo.collections.llm.recipes.optim.adam import distributed_fused_adam_with_cosine_annealing
-from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed
+from nemo.collections.llm.recipes.precision.mixed_precision import bf16_with_fp8_mixed_current_scaling
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
+from nemo.lightning.pytorch.callbacks.megatron_comm_overlap import MegatronCommOverlapCallback
 from nemo.utils.exp_manager import TimingCallback
 
-NAME = "mamba2_2_7b"
+NAME = "nemotron5_hybrid_47b"
 
 
 @run.cli.factory(name=NAME)
-def tokenizer(tokenizer_model: str = None) -> run.Config[pl.LightningModule]:
+def tokenizer(vocab_file: str = None) -> run.Config[pl.LightningModule]:
     """
-    Factory function to create a tokenizer configuration.
+    Factory function to create a tokenizer configuration for Nemotron5 Hybrid model.
     """
     return run.Config(
         get_nmt_tokenizer,
-        library='huggingface',
-        model_name="EleutherAI/gpt-neox-20b",
-        tokenizer_model=tokenizer_model,
+        library='tiktoken',
+        model_name="TiktokenTokenizer",
+        vocab_file=vocab_file,
         use_fast=True,
     )
 
 
 @run.cli.factory(name=NAME)
-def model(tokenizer_model: str = None) -> run.Config[pl.LightningModule]:
+def model(vocab_file: str = None) -> run.Config[pl.LightningModule]:
     """
-    Factory function to create a Mamba2 2.7B model configuration.
-
+    Factory function to create a Nemotron5 Hybrid 47B model configuration.
     Returns:
-        run.Config[pl.LightningModule]: Configuration for the Mamba2 2.7B model.
-
+        run.Config[pl.LightningModule]: Configuration for the Nemotron5 Hybrid 47B model.
     Examples:
         CLI usage:
-            $ nemo llm pretrain model=mamba2_2_7B ...
-
+            $ nemo llm pretrain model=nemotron5_hybrid_47b ...
         Python API usage:
             >>> model_config = model()
             >>> print(model_config)
     """
     return run.Config(
         llm.MambaModel,
-        config=run.Config(llm.BaseMambaConfig2_7B),
-        tokenizer=tokenizer(tokenizer_model=tokenizer_model),
+        config=run.Config(llm.Nemotron5HybridConfig47B),
+        tokenizer=tokenizer(vocab_file=vocab_file),
     )
 
 
 @run.cli.factory(target=finetune, name=NAME)
 def trainer(
-    tensor_parallelism: int = 1,
+    tensor_parallelism: int = 8,
     pipeline_parallelism: int = 1,
     pipeline_parallelism_type: Optional[torch.dtype] = None,
     virtual_pipeline_parallelism: Optional[int] = None,
     context_parallelism: int = 1,
-    sequence_parallelism: bool = False,
-    num_nodes: int = 1,
+    sequence_parallelism: bool = True,
+    num_nodes: int = 32,
     num_gpus_per_node: int = 8,
     max_steps: int = 100,
     val_check_interval: int = 100,
@@ -89,10 +87,8 @@ def trainer(
     callbacks: Optional[list[run.Config[Callback]]] = None,
 ) -> run.Config[nl.Trainer]:
     """
-    Configure the NeMo Lightning Trainer for Mamba2 2.7B model.
-
+    Configure the NeMo Lightning Trainer for Nemotron5 Hybrid 47B model.
     This function sets up the distributed training strategy and other training parameters.
-
     Args:
         tensor_parallelism (int): Degree of tensor model parallelism.
         pipeline_parallelism (int): Degree of pipeline model parallelism.
@@ -104,18 +100,14 @@ def trainer(
         num_gpus_per_node (int): Number of GPUs per node.
         max_steps (int): Maximum number of training steps.
         callbacks (Optional[list[run.Config[Callback]]]): List of callback configurations.
-
     Returns:
         run.Config[nl.Trainer]: Configuration for the NeMo Lightning Trainer.
-
     Examples:
         CLI usage:
-            $ nemo llm pretrain trainer=mamba2_2_7b ...
-
+            $ nemo llm pretrain trainer=nemotron5_hybrid_47b ...
         Python API usage:
-            >>> trainer_config = trainer(num_nodes=1, num_gpus_per_node=1)
+            >>> trainer_config = trainer(num_nodes=32, num_gpus_per_node=1)
             >>> print(trainer_config)
-
     Note:
         For more information on distributed training strategies, refer to the
         NeMo documentation on multi-GPU and multi-node training.
@@ -148,7 +140,7 @@ def trainer(
         devices=num_gpus_per_node,
         max_steps=max_steps,
         num_nodes=num_nodes,
-        plugins=bf16_mixed(),
+        plugins=bf16_with_fp8_mixed_current_scaling(),
         strategy=strategy,
         use_distributed_sampler=False,
         val_check_interval=val_check_interval,
@@ -164,58 +156,56 @@ def trainer(
 def pretrain_recipe(
     dir: Optional[str] = None,
     name: str = "default",
-    tokenizer_model: str = None,
-    num_nodes: int = 1,
+    vocab_file: str = None,
+    num_nodes: int = 32,
     num_gpus_per_node: int = 8,
-    tensor_parallelism: int = 1,
+    tensor_parallelism: int = 8,
+    sequence_parallelism: bool = True,
     pipeline_parallelism: int = 1,
     max_steps: int = 100,
     val_check_interval: int = 100,
     limit_test_batches: int = 50,
     limit_val_batches: int = 32,
     log_every_n_steps: int = 10,
-    seq_length: int = 4096,
-    gbs: int = 8,
+    seq_length: int = 8192,
+    gbs: int = 32,
     mbs: int = 1,
+    performance_mode: bool = False,
     fn=pretrain,
 ) -> run.Partial:
     """
-    Create a pre-training recipe for Mamba2 2.7B model.
-
+    Create a pre-training recipe for Nemotron5 Hybrid 47B model.
     This function sets up a complete configuration for pre-training, including
     model, trainer, data, logging, optimization, and resumption settings.
-
     Args:
         dir (Optional[str]): Directory for saving logs and checkpoints.
         name (str): Name of the pre-training run.
         num_nodes (int): Number of compute nodes to use.
         num_gpus_per_node (int): Number of GPUs per node.
         fn (Callable): The pre-training function to use.
-
     Returns:
         run.Partial: Partial configuration for pre-training.
-
     Examples:
         CLI usage:
-            $ nemo llm pretrain --factory mamba2_2_7b
-            $ nemo llm pretrain --factory "mamba2_2_7b(num_nodes=1, name='my_pretrain')"
-
+            $ nemo llm pretrain --factory nemotron5_hybrid_47b
+            $ nemo llm pretrain --factory "nemotron5_hybrid_47b(num_nodes=32, name='my_pretrain')"
         Python API usage:
-            >>> recipe = pretrain_recipe(name="mamba2_2_7b_pretrain", num_nodes=1)
+            >>> recipe = pretrain_recipe(name="nemotron5_hybrid_47b_pretrain", num_nodes=32)
             >>> print(recipe)
-
     Note:
         For more details on pre-training LLMs with NeMo, see the pre-training
         guide in the `examples/llm/pretrain/` directory.
     """
-    return run.Partial(
+
+    recipe = run.Partial(
         fn,
-        model=model(),
+        model=model(vocab_file=vocab_file),
         trainer=trainer(
             max_steps=max_steps,
             num_nodes=num_nodes,
             tensor_parallelism=tensor_parallelism,
             pipeline_parallelism=pipeline_parallelism,
+            sequence_parallelism=sequence_parallelism,
             num_gpus_per_node=num_gpus_per_node,
             val_check_interval=val_check_interval,
             limit_test_batches=limit_test_batches,
@@ -228,128 +218,128 @@ def pretrain_recipe(
             seq_length=seq_length,
             global_batch_size=gbs,
             micro_batch_size=mbs,
-            tokenizer=tokenizer(),
+            tokenizer=tokenizer(vocab_file=vocab_file),
         ),
         log=default_log(dir=dir, name=name, tensorboard_logger=tensorboard_logger(name=name)),
         optim=distributed_fused_adam_with_cosine_annealing(max_lr=3e-4),
         resume=default_resume(),
     )
+    if performance_mode:
+        recipe = performance_optimizations(recipe)
+    return recipe
 
 
 @run.cli.factory(target=finetune, name=NAME)
 def finetune_recipe(
+    resume_path,
+    vocab_file,
     dir: Optional[str] = None,
     name: str = "default",
-    resume_path: str = None,
-    tokenizer_model: str = None,
-    num_nodes: int = 1,
+    num_nodes: int = 32,
     num_gpus_per_node: int = 8,
-    tensor_model_parallel_size: int = 1,
-    pipeline_model_parallel_size: int = 1,
-    seq_length: int = 4096,
+    tensor_parallelism: int = 8,
+    sequence_parallelism: bool = True,
+    pipeline_parallelism: int = 1,
+    seq_length: int = 8192,
     max_steps: int = 100,
     val_check_interval: int = 100,
     limit_test_batches: int = 50,
     limit_val_batches: int = 32,
     log_every_n_steps: int = 10,
-    gbs: int = 8,
+    gbs: int = 32,
     mbs: int = 1,
+    performance_mode: bool = False,
     peft_scheme: Optional[str] = 'none',
 ) -> run.Partial:
     """
-    Create a fine-tuning recipe for Mamba2 2.7B model.
-
+    Create a fine-tuning recipe for Nemotron5 Hybrid 47B model.
     This function sets up a complete configuration for fine-tuning, including
     model, trainer, data, logging, optimization, and resumption settings.
-
     Args:
         dir (Optional[str]): Directory for saving logs and checkpoints.
         name (str): Name of the fine-tuning run.
         resume_path (str): Path to the NeMo checkpoint (refer to notes below
                             on how to convert a pytorch checkpoint to NeMo)
-        tokenizer_model (str): Path to tokenizer model (defaults to None)
+        vocab_file (str): Path to vocab file (defaults to None)
         num_nodes (int): Number of compute nodes to use.
         num_gpus_per_node (int): Number of GPUs per node.
     Returns:
         run.Partial: Partial configuration for fine-tuning.
-
     Examples:
         CLI usage:
-            $ nemo llm finetune --factory mamba2_2_7b
-
+            $ nemo llm finetune --factory nemotron5_hybrid_47b
         Python API usage:
-            >>> recipe = finetune_recipe(name="mamba2_2_7b_finetune", num_nodes=1)
+            >>> recipe = finetune_recipe(name="nemotron5_hybrid_47b_finetune", num_nodes=32)
             >>> print(recipe)
-
     Note:
         This recipe uses the SQuAD dataset for fine-tuning. For more information
         on fine-tuning LLMs with NeMo, see the fine-tuning guide in the
         `examples/llm/finetune/` directory.
         For converting an SSM pytorch checkpoint, use the following line of python code:
-
-        llm.MambaModel(llm.BaseMambaConfig2_7B(), tokenizer=tokenizer()).import_ckpt(
+        llm.MambaModel(llm.Nemotron5HybridConfig47B(), tokenizer=tokenizer(vocab_file=vocab_file)).import_ckpt(
             path="pytorch://ABSOLUTE_PATH_TO_CKPT/your_pytorch_state_dict_file",
-            model_config=llm.BaseMambaConfig2_7B())
+            model_config=llm.Nemotron5HybridConfig47B())
         This line will cache the nemo checkpoint to following directory:
             /root/.cache/nemo/models/your_pytorch_state_dict_file
-
     """
     nemo_resume = run.Config(
         nl.AutoResume,
         restore_config=run.Config(nl.RestoreConfig, path=resume_path),
     )
-    strategy = run.Config(
-        nl.MegatronStrategy,
-        tensor_model_parallel_size=tensor_model_parallel_size,
-        pipeline_model_parallel_size=pipeline_model_parallel_size,
-        gradient_as_bucket_view=True,
-        ckpt_load_optimizer=False,
-        ckpt_save_optimizer=False,
-        ckpt_async_save=False,
-    )
-    checkpoint_callback = run.Config(
-        nl.ModelCheckpoint,
-        every_n_train_steps=10,
-        dirpath=dir,
-    )
-    trainer = run.Config(
-        nl.Trainer,
-        accelerator="gpu",
-        accumulate_grad_batches=1,
-        devices=num_gpus_per_node,
-        max_steps=max_steps,
-        val_check_interval=val_check_interval,
-        limit_test_batches=limit_test_batches,
-        limit_val_batches=limit_val_batches,
-        log_every_n_steps=log_every_n_steps,
-        num_nodes=num_nodes,
-        plugins=run.Config(
-            nl.MegatronMixedPrecision,
-            precision="bf16-mixed",
-            params_dtype=torch.bfloat16,
-        ),
-        callbacks=[checkpoint_callback],
-        strategy=strategy,
-        use_distributed_sampler=False,
-    )
     recipe = run.Partial(
         llm.finetune,
-        model=model(tokenizer_model=tokenizer_model),
-        trainer=trainer,
+        model=model(vocab_file=vocab_file),
+        trainer=trainer(
+            max_steps=max_steps,
+            num_nodes=num_nodes,
+            tensor_parallelism=tensor_parallelism,
+            pipeline_parallelism=pipeline_parallelism,
+            sequence_parallelism=sequence_parallelism,
+            num_gpus_per_node=num_gpus_per_node,
+            val_check_interval=val_check_interval,
+            limit_test_batches=limit_test_batches,
+            limit_val_batches=limit_val_batches,
+            log_every_n_steps=log_every_n_steps,
+            callbacks=[run.Config(TimingCallback)],
+        ),
         data=run.Config(
-            llm.SquadDataModule,
+            MockDataModule,
             seq_length=seq_length,
             global_batch_size=gbs,
             micro_batch_size=mbs,
-            tokenizer=tokenizer(tokenizer_model=tokenizer_model),
+            tokenizer=tokenizer(vocab_file=vocab_file),
         ),
         log=llm.default_log(dir=dir, name=name, tensorboard_logger=tensorboard_logger(name=name)),
         optim=distributed_fused_adam_with_cosine_annealing(max_lr=1e-4, min_lr=0, warmup_steps=50),
         resume=nemo_resume,
     )
     if peft_scheme is None or peft_scheme.lower() == 'none':
-        recipe.trainer.strategy.tensor_model_parallel_size = 1
+        recipe.trainer.strategy.tensor_model_parallel_size = 8
         recipe.optim.config.lr = 5e-6
     else:
         raise ValueError(f"Unrecognized peft scheme: {peft_scheme}")
+    if performance_mode:
+        recipe = performance_optimizations(recipe)
+    return recipe
+
+
+def performance_optimizations(recipe: run.Partial) -> run.Partial:
+    """
+    Create a performance-optimized pre-training recipe for Nemotron5 Hybrid 47B model.
+    This method enables performance optimizations that may not be suitable for all use cases.
+    It builds upon the standard pre-training recipe and adds additional performance enhancements.
+    Args:
+        recipe (run.Partial): Base pre-train recipe to which performance optimizations will be added
+    Returns:
+        run.Partial: Partial configuration for performance-optimized pre-training.
+    Note:
+        Use this method with caution and only when you need maximum performance.
+        It may not be suitable for all hardware configurations or use cases.
+    """
+    recipe.trainer.callbacks.append(
+        run.Config(
+            MegatronCommOverlapCallback,
+            tp_comm_overlap=True,
+        )
+    )
     return recipe
