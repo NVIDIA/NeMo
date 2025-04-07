153d36f8897c46e03c941b5c2ef3815b7374c155
martas <martas@nvidia.com>
2025-04-07 11:43:10 +0200

    change sampling defaults to match what's typically in the request
    Signed-off-by: martas <martas@nvidia.com>

--- nemo/deploy/service/fastapi_interface_to_pytriton.py
+++ nemo/deploy/service/fastapi_interface_to_pytriton.py
@@ -77,8 +77,8 @@ class CompletionRequest(BaseModel):
     messages: list[dict] = [{}]
     max_tokens: int = 512
     temperature: float = 1.0
-    top_p: float = 0.0
-    top_k: int = 1
+    top_p: float = 1.0
+    top_k: int = 0
     logprobs: int = None
 
 
