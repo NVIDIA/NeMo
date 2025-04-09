034b17b3620b11a0f958989ebe52cd6791e4aa89
Guyue Huang <guyueh@nvidia.com>
2025-04-09 10:49:10 -0700

    Add dsv3 pretrain script, support flops calculation
    Signed-off-by: Guyue Huang <guyueh@nvidia.com>

---
+++ scripts/performance/llm/pretrain_deepseek_v3.py
@@ -0,0 +1,170 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os.path import basename, splitext
+
+import fiddle as fdl
+import fiddle._src.experimental.dataclasses as fdl_dc
+import nemo_run as run
+
+from nemo.collections.llm.recipes.deepseek_v3 import pretrain_recipe
+from nemo.lightning.run.plugins import NsysPlugin, PerfEnvPlugin
+
+from ..argument_parser import parse_cli_args
+from ..utils import (
+    args_sanity_check,
+    get_user_configs,
+    hf_tokenizer,
+    set_exp_logging_configs,
+    set_primary_perf_configs,
+    slurm_executor,
+)
+
+
+def override_recipe_configs(
+    args: str,
+    num_nodes: int,
+    mbs: int,
+    gbs: int,
+    tp_size: int,
+    pp_size: int,
+    cp_size: int,
+    vp_size: int,
+    ep_size: int,
+    enable_cuda_graphs: bool,
+    use_mcore_fsdp: bool,
+    recompute_layers: int,
+    activation_offload_layers: int,
+):
+    """
+    DeepSeek V3 pre-train recipe aimed at achieving best possible performance.
+    """
+    recipe = pretrain_recipe()
+    recipe = set_primary_perf_configs(
+        recipe,
+        "pre_train",
+        num_nodes,
+        args.gpus_per_node,
+        mbs,
+        gbs,
+        args.max_steps,
+        tp_size,
+        pp_size,
+        cp_size,
+        vp_size,
+        ep_size,
+        enable_cuda_graphs=enable_cuda_graphs,
+        use_mcore_fsdp=use_mcore_fsdp,
+        recompute_layers=recompute_layers,
+        activation_offload_layers=activation_offload_layers,
+    )
+    recipe = set_exp_logging_configs(
+        recipe, "pre_train", "llm", "deepseekv3", args.tensorboard, args.wandb, args.wandb_prj_name, args.wandb_job_name
+    )
+    
+    # modify model to proxy
+    if args.proxy_num_layers is not None:
+        recipe.model.config.num_layers = args.proxy_num_layers
+        recipe.model.config.moe_layer_freq = [0] * 3 + [1] * (args.proxy_num_layers - 3)
+    if args.proxy_num_experts is not None:
+        recipe.model.config.num_moe_experts = args.proxy_num_experts
+
+    # data module configs
+    recipe.data.tokenizer = hf_tokenizer("deepseek-ai/DeepSeek-V3")
+    recipe.model.tokenizer = recipe.data.tokenizer
+
+    # compute dtype configs
+    if args.compute_dtype.lower() == "fp8":
+        raise ValueError("Deepseek FP8 recipe requires subchannel scaling which will be supported soon.")
+
+    return recipe
+
+
+if __name__ == "__main__":
+    args = parse_cli_args().parse_args()
+    args_sanity_check(args)
+
+    kwargs = get_user_configs(args.gpu.lower(), "pre_train", "deepseek", "v3", args)
+    (
+        num_nodes,
+        mbs,
+        gbs,
+        tp_size,
+        pp_size,
+        cp_size,
+        vp_size,
+        ep_size,
+        _,
+        enable_cuda_graphs,
+        use_mcore_fsdp,
+        recompute_layers,
+        activation_offload_layers,
+    ) = kwargs
+
+    recipe = override_recipe_configs(
+        args,
+        num_nodes,
+        mbs,
+        gbs,
+        tp_size,
+        pp_size,
+        cp_size,
+        vp_size,
+        ep_size,
+        enable_cuda_graphs,
+        use_mcore_fsdp,
+        recompute_layers,
+        activation_offload_layers,
+    )
+
+    exp_config = f"{num_nodes}nodes_tp{tp_size}_pp{pp_size}_cp{cp_size}_vp{vp_size}_ep{ep_size}_{mbs}mbs_{gbs}gbs"
+    exp_name = f"{splitext(basename(__file__))[0]}_{args.compute_dtype}_{exp_config}"
+
+    executor = slurm_executor(
+        args.account,
+        args.partition,
+        args.log_dir,
+        num_nodes,
+        args.gpus_per_node,
+        args.time_limit,
+        args.container_image,
+        custom_mounts=args.custom_mounts,
+        custom_env_vars={},
+        hf_token=args.hf_token,
+        nemo_home=args.nemo_home,
+        wandb_key=args.wandb_key,
+    )
+
+    plugins = [
+        PerfEnvPlugin(
+            enable_vboost=True,
+            nccl_pp_comm_chunksize=2097152 if pp_size > 1 else None,
+            gpu_sm100_or_newer=(args.gpu.lower() in ['b200', 'gb200']),
+        )
+    ]
+    if args.enable_nsys:
+        plugins.append(NsysPlugin(start_step=5, end_step=6))
+
+    with run.Experiment(exp_name) as exp:
+        exp.add(
+            recipe,
+            executor=executor,
+            name=exp_name,
+            plugins=plugins,
+        )
+
+        if not args.dryrun:
+            exp.run(sequential=True, detach=True)
+        else:
+            exp.dryrun()
