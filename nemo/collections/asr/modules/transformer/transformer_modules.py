1d8e7bb2f6936429fe1d02af88413d4cd90c3afb
Vladimir Bataev <vbataev@nvidia.com>
2025-04-15 01:05:44 +0400

    Add customizable start position across batch
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- nemo/collections/asr/modules/transformer/transformer_modules.py
+++ nemo/collections/asr/modules/transformer/transformer_modules.py
@@ -101,7 +101,7 @@ class TransformerEmbedding(nn.Module):
         self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-5)
         self.dropout = nn.Dropout(embedding_dropout)
 
-    def forward(self, input_ids, token_type_ids=None, start_pos=0):
+    def forward(self, input_ids, token_type_ids=None, start_pos: int | torch.Tensor=0):
         seq_length = input_ids.size(1)
         # we fail here only with parametric positional embedding. FixedPositionalEncoding automatically extends.
         if self.learn_positional_encodings and (seq_length > self.max_sequence_length):
@@ -109,10 +109,12 @@ class TransformerEmbedding(nn.Module):
                 f"Input sequence is longer than maximum allowed sequence length for positional encoding. "
                 f"Got {seq_length} and {self.max_sequence_length}"
             )
-        position_ids = torch.arange(
-            start=start_pos, end=start_pos + seq_length, dtype=torch.long, device=input_ids.device
-        )
+        position_ids = torch.arange(start=0, end=seq_length, dtype=torch.long, device=input_ids.device)
         position_ids = position_ids.unsqueeze(0).repeat(input_ids.size(0), 1)
+        if isinstance(start_pos, torch.Tensor):
+            position_ids += start_pos.unsqueeze(1)
+        else:
+            position_ids += start_pos
 
         token_embeddings = self.token_embedding(input_ids)
         position_embeddings = self.position_embedding(position_ids)
