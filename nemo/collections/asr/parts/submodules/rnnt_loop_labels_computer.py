Fix decoding: avoid mixed precision
Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

nemo/collections/asr/parts/submodules/rnnt_loop_labels_computer.py
@@ -1023,8 +1023,8 @@ class GreedyBatchedRNNTLoopLabelsComputer(WithOptionalCudaGraphs, ConfidenceMeth
         out_len: torch.Tensor,
     ) -> Tuple[rnnt_utils.BatchedHyps, Optional[rnnt_utils.BatchedAlignments], Any]:
         if self.cuda_graphs_mode is not None and x.device.type == "cuda":
-            return self.loop_labels_cuda_graphs(encoder_output=x, encoder_output_length=out_len)
+            with torch.amp.autocast(device_type="cuda", enabled=False):
+                # TODO(dgalvez): fix issue with DDP+mixed precision, remove this restriction
+                return self.loop_labels_cuda_graphs(encoder_output=x, encoder_output_length=out_len)
 
-        with torch.amp.autocast(device_type="cuda", enabled=False):
-            # TODO(dgalvez): fix issue with DDP+mixed precision, remove this restriction
-            return self.loop_labels_torch(encoder_output=x, encoder_output_length=out_len)
+        return self.loop_labels_torch(encoder_output=x, encoder_output_length=out_len)