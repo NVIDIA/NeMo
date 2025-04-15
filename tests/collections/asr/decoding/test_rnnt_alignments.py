52de63846f02915ad19362a9b9b81137035c7ef7
lilithgrigoryan <lgrigoryan@nvidia.com>
2025-04-15 13:39:20 +0400

    clean test
    Signed-off-by: lilithgrigoryan <lgrigoryan@nvidia.com>

--- tests/collections/asr/decoding/test_rnnt_alignments.py
+++ tests/collections/asr/decoding/test_rnnt_alignments.py
@@ -38,6 +38,23 @@ def stt_en_conformer_transducer_small_model():
     return model
 
 
+@pytest.fixture(scope="module")
+def an4_val_manifest_corrected(tmp_path_factory, test_data_dir):
+    """
+    Correct an4_val manifest audio filepaths, e.g.,
+    "tests/data/asr/test/an4/wav/an440-mjgm-b.wav" -> test_data_dir / "test/an4/wav/an440-mjgm-b.wav"
+    """
+    an4_val_manifest_orig_path = Path(test_data_dir) / "asr/an4_val.json"
+    an4_val_manifest_corrected_path = tmp_path_factory.mktemp("manifests") / "an4_val_corrected.json"
+    an4_val_records = read_manifest(an4_val_manifest_orig_path)
+    for record in an4_val_records:
+        record["audio_filepath"] = record["audio_filepath"].replace(
+            "tests/data/asr", str(an4_val_manifest_orig_path.resolve().parent)
+        )
+    write_manifest(an4_val_manifest_corrected_path, an4_val_records)
+    return an4_val_manifest_corrected_path
+
+
 def get_rnnt_alignments(
     strategy: str,
     manifest_path: Union[Path, str],
