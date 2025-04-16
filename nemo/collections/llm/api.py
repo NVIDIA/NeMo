f734bd237bcd575845ee7cdd4402942a793d0301
Chen Cui <chcui@nvidia.com>
2025-04-16 10:57:58 -0700

    allocate only as much kv cache as needed
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- nemo/collections/llm/api.py
+++ nemo/collections/llm/api.py
@@ -1080,8 +1080,8 @@ def generate(
     )
 
     max_seq_length = inference_params.num_tokens_to_generate + max(len(mcore_tokenizer.tokenize(p)) for p in inputs)
-    if max_seq_length > inference_wrapped_model.inference_wrapper_config.inference_max_seq_length:
-        inference_wrapped_model.inference_wrapper_config.inference_max_seq_length = max_seq_length
+    # set kv cache allocation to only num tokens in prompt + max tokens to generate
+    inference_wrapped_model.inference_wrapper_config.inference_max_seq_length = max_seq_length
 
     dp_size = trainer.strategy.distributed_sampler_kwargs['num_replicas']
     dp_rank = trainer.strategy.distributed_sampler_kwargs['rank']
