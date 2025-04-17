ec7f1f1d36ea9d2a538451dd61b68bd0928b5143
yaoyu-33 <yaoyu.094@gmail.com>
2025-04-17 10:41:59 -0700

    fix base importing
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- nemo/collections/llm/inference/base.py
+++ nemo/collections/llm/inference/base.py
@@ -29,7 +29,6 @@ from megatron.core.inference.text_generation_controllers.text_generation_control
 from megatron.core.transformer.module import MegatronModule
 
 import nemo.lightning as nl
-from examples.nlp.text_normalization_as_tagging.dataset_preparation.sample_each_label import vocab
 from nemo.lightning import io
 from nemo.lightning.ckpt_utils import ADAPTER_META_FILENAME, ckpt_to_context_subdir
 from nemo.lightning.io.pl import ckpt_to_weights_subdir
