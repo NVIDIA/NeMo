ecfd957539fad8fc8c27f6bb9a30e18c2b79fab8
Onur Yilmaz <oyilmaz@nvidia.com>
2025-04-18 12:31:06 -0400

    removed --branch from tests
    Signed-off-by: Onur Yilmaz <oyilmaz@nvidia.com>

--- tests/functional_tests/L2_NeMo_2_Export_Deploy_Query_In_Framework.sh
+++ tests/functional_tests/L2_NeMo_2_Export_Deploy_Query_In_Framework.sh
@@ -11,17 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/test_hf_import.py \
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/test_hf_import.py \
   --hf_model /home/TestData/nlp/megatron_llama/llama-ci-hf \
   --output_path /tmp/nemo2_ckpt
 
-coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo scripts/deploy/nlp/deploy_inframework_triton.py \
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo scripts/deploy/nlp/deploy_inframework_triton.py \
   --nemo_checkpoint /tmp/nemo2_ckpt \
   --triton_model_name llama \
   --num_gpus 2 \
   --tensor_parallelism_size 2 &
 
-coverage run --branch -a --data-file=/workspace/.coverage --source=/workspace/nemo scripts/deploy/nlp/query_inframework.py \
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo scripts/deploy/nlp/query_inframework.py \
   --model_name llama \
   --prompt "What is the color of a banana?" \
   --max_output_len 20
