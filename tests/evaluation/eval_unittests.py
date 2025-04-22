e80e93bd9cf8676f71d61fe72d8b3b3653c29a46
martas <martas@nvidia.com>
2025-04-22 15:50:40 +0200

    use mock server (we need smth that works with code running in separate process)
    Signed-off-by: martas <martas@nvidia.com>

--- tests/evaluation/eval_unittests.py
+++ tests/evaluation/eval_unittests.py
@@ -1,9 +1,13 @@
-from unittest.mock import patch
-
 import pytest
+from pytest_httpserver import HTTPServer
 
 from nemo.collections.llm.api import evaluate
-from nemo.collections.llm.evaluation.api import ApiEndpoint, ConfigParams, EvaluationConfig, EvaluationTarget
+from nemo.collections.llm.evaluation.api import ConfigParams, EvaluationConfig, EvaluationTarget
+
+
+@pytest.fixture(scope="session")
+def httpserver_listen_address():
+    return ("127.0.0.1", 8000)
 
 
 @pytest.mark.parametrize(
@@ -35,30 +39,38 @@ def test_default_none_tokenizer():
     assert eval_config.params.extra["num_fewshot"] == 5
 
 
-def test_evaluation():
-    target_config = EvaluationTarget(api_endpoint={"url": "http://my_endpoint/v1/completions/", "type": "completions"})
-    eval_config = EvaluationConfig(
-        type="gsm8k",
-        params=ConfigParams(
-            extra={"tokenizer": "Qwen/Qwen2.5-0.5B", "num_fewshot": 13}, limit_samples=1, parallelism=1
-        ),
+def test_evaluation(httpserver: HTTPServer):
+    httpserver.expect_request("/v1/triton_health").respond_with_json(
+        {"status": "Triton server is reachable and ready"}
     )
-    with patch("requests.get") as mock_get, patch("requests.post") as mock_post:
-        mock_get.return_value.status_code = 200
-        mock_response = mock_post.return_value
-        mock_response.status_code = 200
-        mock_response.raise_for_status.return_value = None
-        mock_response.json.return_value = {
+    httpserver.expect_request("/v1/completions/", method="POST").respond_with_json(
+        {
             'id': 'cmpl-123456',
             'object': 'text_completion',
             'created': 1234567,
             'model': 'triton_model',
             'choices': [
                 {
-                    'text': ' Janet eats 3 eggs and bakes 4 eggs, so she has 16 - 3 - 4 = <<16-3-4=9>>9 eggs left.\nShe sells 9 eggs for $2 each, so she makes 9 x 2 = <<9*2=18>>18 dollars.\n#### 18'
+                    'text': ' Janet eats 3 eggs and bakes 4 eggs, so she has 16 - 3 - 4 = <<16-3-4=9>>9 eggs left.\n'
+                    'She sells 9 eggs for $2 each, so she makes 9 x 2 = <<9*2=18>>18 dollars.\n#### 18'
                 }
             ],
-        }
+        },
+    )
+    target_config = EvaluationTarget(
+        api_endpoint={"url": "http://localhost:8000/v1/completions/", "type": "completions"}
+    )
+    eval_config = EvaluationConfig(
+        type="gsm8k",
+        params=ConfigParams(
+            extra={"tokenizer": "Qwen/Qwen2.5-0.5B", "num_fewshot": 13}, limit_samples=1, parallelism=1
+        ),
+    )
 
-        results = evaluate(target_cfg=target_config, eval_cfg=eval_config)
-        assert results is not None
+    results = evaluate(target_cfg=target_config, eval_cfg=eval_config)
+    assert (
+        results['tasks']['gsm8k']['metrics']['exact_match__strict-match']['scores']['exact_match__strict-match'][
+            'value'
+        ]
+        == 1.0
+    )
