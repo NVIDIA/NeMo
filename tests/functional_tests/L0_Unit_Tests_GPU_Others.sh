ff0ad46f2aece5e768f14e006430b070cb62edd6
oliver könig <okoenig@nvidia.com>
2025-04-23 01:52:45 +0200

    f
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/functional_tests/L0_Unit_Tests_GPU_Others.sh
+++ tests/functional_tests/L0_Unit_Tests_GPU_Others.sh
@@ -23,4 +23,6 @@ NEMO_NUMBA_MINVER=0.53 CUDA_VISIBLE_DEVICES=0 coverage run -a --data-file=/works
     --ignore=tests/core_ptl \
     --ignore=tests/hydra \
     --ignore=tests/lightning \
+    --ignore=tests/export \
+    --ignore=tests/deploy \
     --ignore=tests/utils
