27493a5ede6c42e2a12c04f75edcef71500c3768
jenchen13 <jennifchen@nvidia.com>
2025-04-23 06:55:35 -0700

    revert test to per tensor
    Signed-off-by: jenchen13 <jennifchen@nvidia.com>

--- tests/functional_tests/L2_NeMo_2_PTQ_Llama2_FP8_nemo.sh
+++ tests/functional_tests/L2_NeMo_2_PTQ_Llama2_FP8_nemo.sh
@@ -13,4 +13,4 @@
 # limitations under the License.
 coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/test_hf_import.py --hf_model /home/TestData/nlp/megatron_llama/llama-ci-hf --output_path /tmp/nemo2_ckpt
 
-coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo scripts/llm/ptq.py -nc /tmp/nemo2_ckpt -algo block_fp8 -out /tmp/nemo2_ptq_ckpt --export_format nemo --legacy_ckpt
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo scripts/llm/ptq.py -nc /tmp/nemo2_ckpt -algo fp8 -out /tmp/nemo2_ptq_ckpt --export_format nemo --legacy_ckpt
