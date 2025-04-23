40f8887143540aaaa72e93d4a8dba53b6baed6ff
Vladimir Bataev <vbataev@nvidia.com>
2025-04-23 18:27:43 +0400

    Add mps support
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- examples/asr/asr_chunked_inference/rnnt/speech_to_text_buffered_infer_rnnt.py
+++ examples/asr/asr_chunked_inference/rnnt/speech_to_text_buffered_infer_rnnt.py
@@ -122,6 +122,7 @@ class TranscriptionConfig:
     # device anyway, and do inference on CPU only if CUDA device is not found.
     # If `cuda` is a negative number, inference will be on CPU only.
     cuda: Optional[int] = None
+    allow_mps: bool = False  # allow to select MPS device (Apple Silicon M-series GPU)
     audio_type: str = "wav"
 
     # Recompute model transcription, even if the output folder exists with scores.
@@ -173,16 +174,23 @@ def main(cfg: TranscriptionConfig) -> TranscriptionConfig:
     # setup GPU
     if cfg.cuda is None:
         if torch.cuda.is_available():
-            device = [0]  # use 0th CUDA device
             accelerator = 'gpu'
+            map_location = torch.device('cuda:0')  # use 0th CUDA device
+        elif cfg.allow_mps and hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+            logging.warning(
+                "MPS device (Apple Silicon M-series GPU) support is experimental."
+                " Env variable `PYTORCH_ENABLE_MPS_FALLBACK=1` should be set in most cases to avoid failures."
+            )
+            accelerator = 'mps'
+            map_location = torch.device('mps')
         else:
-            device = 1
             accelerator = 'cpu'
+            map_location = torch.device('cpu')
     else:
-        device = [cfg.cuda]
         accelerator = 'gpu'
-    map_location = torch.device('cuda:{}'.format(device[0]) if accelerator == 'gpu' else 'cpu')
-    logging.info(f"Inference will be done on device : {device}")
+        map_location = torch.device(f'cuda:{cfg.cuda}')
+
+    logging.info(f"Inference will be done on device : {map_location}")
 
     asr_model, model_name = setup_model(cfg, map_location)
 
