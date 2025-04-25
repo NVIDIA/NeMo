fde9e63c30640da814a7eb1b7c1f84a2473f7ae4
yaoyu-33 <yaoyu-33@users.noreply.github.com>
2025-04-25 17:30:36 +0000

    Apply isort and black reformatting
    Signed-off-by: yaoyu-33 <yaoyu-33@users.noreply.github.com>

--- nemo/collections/vlm/neva/model/base.py
+++ nemo/collections/vlm/neva/model/base.py
@@ -441,7 +441,11 @@ class MCoreNevaModel(MCoreLLaVAModel):
         )  # [combined_seq_len, b, h_language], [b, combined_seq_len], [b, combined_seq_len]
 
         if self.context_parallel_lm > 1 or self.sequence_parallel_lm:
-            if self.context_parallel_lm > 1 and packed_seq_params is not None and packed_seq_params.qkv_format == "thd":
+            if (
+                self.context_parallel_lm > 1
+                and packed_seq_params is not None
+                and packed_seq_params.qkv_format == "thd"
+            ):
                 # _process_embedding_token_parallel expects input in shape bshd for cp + thd
                 combined_embeddings = combined_embeddings.transpose(1, 0).contiguous()
 
