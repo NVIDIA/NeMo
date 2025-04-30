c40425a5377307c40e19a91ac958ad0a22727fc1
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-04-30 02:15:29 -0700

    switch to gemma
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/functional_tests/L2_HF_Transformer_PEFT_2gpu_FSDP2_fp8.sh
+++ tests/functional_tests/L2_HF_Transformer_PEFT_2gpu_FSDP2_fp8.sh
@@ -1,7 +1,7 @@
 export TRANSFORMERS_OFFLINE=1
 export HF_HOME=/home/TestData/automodel/hf_home
 coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo examples/llm/peft/automodel.py \
-    --model /home/TestData/akoumparouli/hf_mixtral_2l/ \
+    --model /home/TestData/akoumparouli/hf_gemma_38m/ \
     --max-steps 3 \
     --devices 2 \
     --strategy fsdp2 --fp8
