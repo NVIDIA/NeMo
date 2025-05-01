c8c8369dd52a3e09c3be0ff279a3a3663cf20b86
jenchen13 <jennifchen@nvidia.com>
2025-05-01 13:21:03 -0700

    fix constructor
    Signed-off-by: jenchen13 <jennifchen@nvidia.com>

--- nemo/collections/llm/gpt/data/chat.py
+++ nemo/collections/llm/gpt/data/chat.py
@@ -13,9 +13,15 @@
 # limitations under the License.
 
 from functools import lru_cache
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
 from nemo.collections.llm.gpt.data.core import create_sft_dataset
 from nemo.collections.llm.gpt.data.fine_tuning import FineTuningDataModule
+
+if TYPE_CHECKING:
+    from nemo.collections.common.tokenizers import TokenizerSpec
+    from nemo.collections.llm.gpt.data.packed_sequence import PackedSequenceSpecs
 
 
 class ChatDataModule(FineTuningDataModule):
@@ -28,9 +34,20 @@ class ChatDataModule(FineTuningDataModule):
 
     def __init__(
         self,
+        dataset_root: Union[str, Path],
+        seq_length: int = 2048,
+        tokenizer: Optional["TokenizerSpec"] = None,
+        micro_batch_size: int = 4,
+        global_batch_size: int = 8,
+        rampup_batch_size: Optional[List[int]] = None,
+        seed: int = 1234,
+        memmap_workers: int = 1,
+        num_workers: int = 8,
+        pin_memory: bool = True,
+        persistent_workers: bool = False,
+        packed_sequence_specs: Optional["PackedSequenceSpecs"] = None,
+        dataset_kwargs: Optional[Dict[str, Any]] = None,
         use_hf_tokenizer_chat_template: bool = False,
-        *args,
-        **kwargs,
     ):
         """Data module for finetuning on chat datasets.
         See base class `FineTuningDataModule` for more details of the arguments.
@@ -39,10 +56,20 @@ class ChatDataModule(FineTuningDataModule):
             use_hf_tokenizer_chat_template: Whether to use the chat template from the HuggingFace tokenizer. If True, uses the tokenizer's
                 built-in chat template. If False, uses default chat template from GPTSFTChatDataset. Defaults to False.
         """
-
         super().__init__(
-            *args,
-            **kwargs,
+            dataset_root,
+            seq_length,
+            tokenizer,
+            micro_batch_size,
+            global_batch_size,
+            rampup_batch_size,
+            seed,
+            memmap_workers,
+            num_workers,
+            pin_memory,
+            persistent_workers,
+            packed_sequence_specs,
+            dataset_kwargs,
         )
         self.use_hf_tokenizer_chat_template = use_hf_tokenizer_chat_template
 
