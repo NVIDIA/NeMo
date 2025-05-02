bd23c300d1f7419d3d5f374b2d328f5fd1d17f3d
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-05-02 07:09:31 -0700

    no darwin
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -269,9 +269,11 @@ nemo() {
   pip install --no-deps --no-cache-dir "liger-kernel==0.5.8; (platform_machine == 'x86_64' and platform_system != 'Darwin')"
   pip install --no-deps "cut-cross-entropy @ git+https://github.com/apple/ml-cross-entropy.git@87a86aba72cfd2f0d8abecaf81c13c4528ea07d8; (platform_machine == 'x86_64' and platform_system != 'Darwin')"
   # FA3
-  git clone --depth 1 https://github.com/Dao-AILab/flash-attention.git; \
-  cd flash-attention/hopper; \
-  python3 setup.py install
+  if [[ "$OSTYPE" ~= "darwin"* ]]; then
+     git clone --depth 1 https://github.com/Dao-AILab/flash-attention.git; \
+     cd flash-attention/hopper; \
+     python3 setup.py install
+  fi
   echo 'Installing nemo itself'
   pip install --no-cache-dir -e $NEMO_DIR/.[all]
 }
