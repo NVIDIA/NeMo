fc80af91e0a98d62227c7fd9081061d7f3c756d0
Xiaowei Ren <xren@nvidia.com>
2025-05-02 14:15:04 -0700

    remove unused import
    Signed-off-by: Xiaowei Ren <xren@nvidia.com>

--- nemo/collections/llm/bert/loss.py
+++ nemo/collections/llm/bert/loss.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, List, Literal, Tuple
+from typing import Dict, Literal, Tuple
 
 import torch
 import torch.nn.functional as F
