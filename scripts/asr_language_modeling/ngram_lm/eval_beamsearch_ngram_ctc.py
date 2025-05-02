9dfef666b4cd7092bebc8d01242e99a76b6783a8
lilithgrigoryan <lgrigoryan@nvidia.com>
2025-05-02 11:34:34 +0400

    minor fix
    Signed-off-by: lilithgrigoryan <lgrigoryan@nvidia.com>

--- scripts/asr_language_modeling/ngram_lm/eval_beamsearch_ngram_ctc.py
+++ scripts/asr_language_modeling/ngram_lm/eval_beamsearch_ngram_ctc.py
@@ -97,8 +97,7 @@ class EvalBeamSearchNGramConfig:
     hyps_cache_file: Optional[str] = None  # The cache file for storing the logprobs of the model
 
     # Parameters for inference
-    acoustic_batch_size: int = 16  # The batch size to calculate log probabilities
-    beam_batch_size: int = 128  # The batch size to be used for beam search decoding
+    batch_size: int = 16  # The batch size
     device: str = "cuda"  # The device to load the model onto to calculate log probabilities
     use_amp: bool = False  # Whether to use AMP if available to calculate log probabilities
 
