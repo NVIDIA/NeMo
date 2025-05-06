8186f115c5a381b49e084d79bed9a187ba777115
martas <martas@nvidia.com>
2025-05-06 11:01:58 +0200

    fix cmd for launching unit test
    Signed-off-by: martas <martas@nvidia.com>

--- tests/functional_tests/L0_Unit_Tests_Eval.sh
+++ tests/functional_tests/L0_Unit_Tests_Eval.sh
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo -m tests.evaluation.eval_unittest
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo -m pytest tests/evaluation/eval_unittest.py
