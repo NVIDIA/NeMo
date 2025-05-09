17ec7c4ff86fdbb163ba0d01c9825b3cc27a6125
cspades <cspades@users.noreply.github.com>
2025-05-09 03:17:49 +0000

    Apply isort and black reformatting
    Signed-off-by: cspades <cspades@users.noreply.github.com>

--- nemo/automodel/dist_utils.py
+++ nemo/automodel/dist_utils.py
@@ -13,9 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import datetime
+import os
 from contextlib import ContextDecorator
+
 import torch.distributed as dist
 
 
