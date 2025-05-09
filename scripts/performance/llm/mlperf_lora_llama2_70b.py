8c7beab288e5d43457dd3c0929d5fe814c349234
Youngeun Kwon <youngeunk@nvidia.com>
2025-05-09 13:55:06 -0700

    add use sharp and use ubr args
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- scripts/performance/llm/mlperf_lora_llama2_70b.py
+++ scripts/performance/llm/mlperf_lora_llama2_70b.py
@@ -316,6 +316,7 @@ if __name__ == "__main__":
         hf_token=args.hf_token,
         nemo_home=args.nemo_home,
         wandb_key=args.wandb_key,
+        network='sharp' if args.use_sharp else None,
     )
 
     recipe = mlperf_lora_llama2_70b_recipe(
