7a8a358af36b7ac7513d58729fe5413a8da1f16f
lilithgrigoryan <lilithgrigoryan@users.noreply.github.com>
2025-05-12 11:55:35 +0000

    Apply isort and black reformatting
    Signed-off-by: lilithgrigoryan <lilithgrigoryan@users.noreply.github.com>

--- nemo/collections/asr/parts/mixins/transcription.py
+++ nemo/collections/asr/parts/mixins/transcription.py
@@ -28,8 +28,8 @@ from tqdm import tqdm
 
 from nemo.collections.asr.parts.preprocessing.perturb import process_augmentations
 from nemo.collections.asr.parts.preprocessing.segment import AudioSegment, ChannelSelectorType
-from nemo.collections.asr.parts.utils.rnnt_utils import Hypothesis
 from nemo.collections.asr.parts.utils import manifest_utils
+from nemo.collections.asr.parts.utils.rnnt_utils import Hypothesis
 from nemo.collections.common.data.utils import move_data_to_device
 from nemo.utils import logging, logging_mode
 
