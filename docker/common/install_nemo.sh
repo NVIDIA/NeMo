8f2423bab9acfe7212708ccfdc2b0160c920f80c
Dong Hyuk Chang <donghyukc@nvidia.com>
2025-05-12 12:05:51 -0700

    Remove conditional dep install on nemo install script
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>

--- docker/common/install_nemo.sh
+++ docker/common/install_nemo.sh
@@ -44,6 +44,7 @@ fi
 DEPS=(
   "llama-index==0.10.43"                                                                     # incompatible with nvidia-pytriton
   "ctc_segmentation==1.7.1 ; (platform_machine == 'x86_64' and platform_system != 'Darwin')" # requires numpy<2.0.0 to be installed before
+  "nemo_run"
   "nvidia-modelopt[torch]==0.27.1 ; platform_system != 'Darwin'"                             # We want a specific version of nvidia-modelopt
 )
 
@@ -53,15 +54,13 @@ if [[ "${NVIDIA_PYTORCH_VERSION}" != "" ]]; then
   )
 fi
 
-if [[ $MODULE_ARG == "all" ]]; then
-  ${PIP} uninstall -y nemo_toolkit sacrebleu nemo_asr nemo_nlp nemo_tts
-  echo 'Installing dependencies of nemo'
-  pip install --force-reinstall --no-deps --no-cache-dir "${DEPS[@]}"
-  pip install --no-cache-dir "${DEPS[@]}"
-  # needs no-deps to avoid installing triton on top of pytorch-triton.
-  pip install --no-deps --no-cache-dir "liger-kernel==0.5.8; (platform_machine == 'x86_64' and platform_system != 'Darwin')"
-  pip install --no-deps "cut-cross-entropy @ git+https://github.com/apple/ml-cross-entropy.git@87a86aba72cfd2f0d8abecaf81c13c4528ea07d8; (platform_machine == 'x86_64' and platform_system != 'Darwin')"
-fi
+${PIP} uninstall -y nemo_toolkit sacrebleu nemo_asr nemo_nlp nemo_tts
+echo 'Installing dependencies of nemo'
+pip install --force-reinstall --no-deps --no-cache-dir "${DEPS[@]}"
+pip install --no-cache-dir "${DEPS[@]}"
+# needs no-deps to avoid installing triton on top of pytorch-triton.
+pip install --no-deps --no-cache-dir "liger-kernel==0.5.8; (platform_machine == 'x86_64' and platform_system != 'Darwin')"
+pip install --no-deps "cut-cross-entropy @ git+https://github.com/apple/ml-cross-entropy.git@87a86aba72cfd2f0d8abecaf81c13c4528ea07d8; (platform_machine == 'x86_64' and platform_system != 'Darwin')"
 
 echo 'Installing nemo itself'
 pip install --no-cache-dir -e $NEMO_DIR/.["$MODULE_ARG"]
