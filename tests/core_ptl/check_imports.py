115e388908044c5838367c6003a222927436f855
Dong Hyuk Chang <donghyukc@nvidia.com>
2025-05-13 14:18:22 -0700

    Print path and class
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>

--- tests/core_ptl/check_imports.py
+++ tests/core_ptl/check_imports.py
@@ -48,7 +48,6 @@ def _build_import_path(domain, subdomains: list, imp):
 
 def _get_class_from_path(domain, subdomains, imp):
     path = _build_import_path(domain, subdomains, imp)
-    print(path)
     class_ = None
     result = None
 
@@ -61,6 +60,7 @@ def _get_class_from_path(domain, subdomains, imp):
                 class_ = class_.__wrapped__
 
             # Subclass tests
+            print(f"path: {path}, class: {class_}")
             if issubclass(class_, (Model, torch.nn.Module)):
                 result = class_
         else:
