d77ef9925ab6f4538078381033db2b8ccd8be707
Asha Anoosheh <aanoosheh@nvidia.com>
2025-05-15 05:37:28 -0700

    More review suggestions
    Signed-off-by: Asha Anoosheh <aanoosheh@nvidia.com>

--- tests/functional_tests/L2_NeMo_2_GPT_Speculative_Llama3_TP2PP1.sh
+++ tests/functional_tests/L2_NeMo_2_GPT_Speculative_Llama3_TP2PP1.sh
@@ -11,11 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/test_hf_import.py --hf_model /home/TestData/nlp/megatron_llama/llama-ci-hf --output_path /tmp/nemo2_ckpt
+
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo scripts/llm/gpt_convert_speculative.py \
+    --model_path /home/TestData/nemo2_ckpt/llama_68M_v4 \
+    --export_dir /tmp/speculative_ckpt \
+    --tp_size 2 \
+    --pp_size 1 \
+    --devices 2 \
+    --legacy_ckpt
 
 coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo tests/collections/llm/modelopt/test_speculative_training.py \
     --name 'test_speculative_training' \
-    --model_path /tmp/nemo2_ckpt \
+    --model_path /tmp/speculative_ckpt \
     --tp_size 2 \
     --pp_size 1 \
     --devices 2 \
