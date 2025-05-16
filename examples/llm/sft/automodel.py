3cfe1f955340f028ad51f5ecfc74931d0f1b7bc2
Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
2025-05-16 11:34:24 -0700

    Update automodel.py
    Signed-off-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>

--- examples/llm/sft/automodel.py
+++ examples/llm/sft/automodel.py
@@ -78,7 +78,7 @@ def make_squad_hf_dataset(
         input_ids = context_ids + answer_ids
         return dict(
             input_ids=input_ids,
-            labels=input_ids[1:] + [eos_token_id] or [input_ids[-1]],
+            labels=input_ids[1:] + [eos_token_id or input_ids[-1]],
             loss_mask=[0] * len(context_ids) + [1] * len(answer_ids),
         )
 
