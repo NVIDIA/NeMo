d6fa0cc7f1c1419f2009b5751dfb872f8025de51
Jan Lasek <janek.lasek@gmail.com>
2025-05-19 11:11:49 +0200

    Improve load_quant_cfg plus unit test
    Signed-off-by: Jan Lasek <janek.lasek@gmail.com>

--- nemo/collections/llm/modelopt/quantization/utils.py
+++ nemo/collections/llm/modelopt/quantization/utils.py
@@ -1,35 +1,24 @@
 import json
-from typing import Any, Dict
+from pathlib import Path
+from typing import Any, Dict, Union
 
-
-def maybe_cast_to_int(x: Any):
-    """Try to cast a value to int, if it fails, return the original value.
-
-    Args:
-        x (Any): The value to be casted.
-    Returns:
-        Any: The casted value or the original value if casting fails.
-    """
-    try:
-        return int(x)
-    except Exception:
-        return x
+from nemo.utils.cast_utils import maybe_cast_to_type
 
 
 def standardize_json_config(quant_cfg: Dict[str, Any]):
-    """Standardize the quantization configuration JSON file to ensure
-    compatibility with modelopt. Modifiy the input dictionary in place.
+    """Standardize the quantization configuration loaded from a JSON file to
+    ensure compatibility with modelopt. Modifiy the input dictionary in place.
 
     Args:
         quant_cfg (Dict[str, Any]): The quantization config dictionary to standardize.
     """
     for key, value in quant_cfg.items():
         if key == "block_sizes":
-            value = {maybe_cast_to_int(k): v for k, v in value.items()}
+            value = {maybe_cast_to_type(k, int): v for k, v in value.items()}
             quant_cfg[key] = value
         elif key in {"num_bits", "scale_bits"} and isinstance(value, list):
-            value = tuple(value)
-            quant_cfg[key] = value
+            quant_cfg[key] = tuple(value)
+            continue  # No further processing needed
         if isinstance(value, dict):
             standardize_json_config(value)
         elif isinstance(value, list):
@@ -38,9 +27,9 @@ def standardize_json_config(quant_cfg: Dict[str, Any]):
                     standardize_json_config(x)
 
 
-def load_quant_cfg(cfg_path: str):
-    """Load quantization configuration from a JSON file and adjust for modelopt
-    standard if necessary.
+def load_quant_cfg(cfg_path: Union[str, Path]) -> Dict[str, Any]:
+    """Load quantization configuration from a JSON file and adjust for
+    modelopt standards if necessary.
 
     Args:
         cfg_path (str): Path to the quantization config JSON file.
@@ -49,7 +38,7 @@ def load_quant_cfg(cfg_path: str):
         dict: The loaded quantization configuration.
     """
     with open(cfg_path, "r") as f:
-        cfg = json.load(f)
+        quant_cfg = json.load(f)
 
-    standardize_json_config(cfg)
-    return cfg
+    standardize_json_config(quant_cfg)
+    return quant_cfg
---
+++ tests/collections/llm/modelopt/quantization/test_load_quant_cfg.py
@@ -0,0 +1,21 @@
+import json
+import tempfile
+
+import pytest
+from nemo.collections.llm.modelopt.quantization.utils import load_quant_cfg
+from nemo.collections.llm.modelopt.quantization.quant_cfg_choices import get_quant_cfg_choices
+
+QUANT_CFG_CHOICES = get_quant_cfg_choices()
+
+
+@pytest.mark.parametrize("cfg_name", ["nvfp4", "fp8"])
+def test_load_quant_cfg(cfg_name):
+    """Test loading a quantization config from a JSON file."""
+
+    quant_cfg_org = QUANT_CFG_CHOICES[cfg_name]
+
+    with tempfile.NamedTemporaryFile(mode="w") as temp_file:
+        json.dump(quant_cfg_org, temp_file)
+        temp_file.flush()
+        quant_cfg_loaded = load_quant_cfg(temp_file.name)
+        assert quant_cfg_loaded == quant_cfg_org
