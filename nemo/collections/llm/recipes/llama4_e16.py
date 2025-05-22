45e4eb64178f65e80fcef9ccbef4b7a039c9f1d9
suiyoubi <suiyoubi@users.noreply.github.com>
2025-05-22 13:28:10 +0000

    Apply isort and black reformatting
    Signed-off-by: suiyoubi <suiyoubi@users.noreply.github.com>

--- nemo/collections/llm/recipes/llama4_e16.py
+++ nemo/collections/llm/recipes/llama4_e16.py
@@ -310,9 +310,7 @@ def finetune_recipe(
     if seq_length is None:
         seq_length = 4096 if packed_sequence else 2048
 
-    recipe = default_finetune_recipe(
-        model(), resume_path, dir, name, num_nodes, num_gpus_per_node, packed_sequence
-    )
+    recipe = default_finetune_recipe(model(), resume_path, dir, name, num_nodes, num_gpus_per_node, packed_sequence)
     if peft_scheme is None or peft_scheme.lower() == 'none':
         recipe.trainer.strategy.sequence_parallel = True
         recipe.trainer.strategy.tensor_model_parallel_size = 8
