db1f20143fee06aacdf52279b08ad56e75e4549f
Boxiang Wang <boxiangw@nvidia.com>
2025-05-27 16:26:41 -0700

    Add notes for cfsdp

--- nemo/lightning/pytorch/strategies/fsdp2_strategy.py
+++ nemo/lightning/pytorch/strategies/fsdp2_strategy.py
@@ -166,6 +166,7 @@ class FSDP2Strategy(PLModelParallelStrategy, io.IOMixin):
                 "model.layers.*.mlp.down_proj": RowwiseParallel(),
             }
 
+            #TODO(boxiangw): add this support for cFSDP2
             fsdp2_model_tp_plan = {
                 "model.embed_tokens": RowwiseParallel(input_layouts=Replicate()),
                 "lm_head": ColwiseParallel(output_layouts=Replicate()),
@@ -183,10 +184,13 @@ class FSDP2Strategy(PLModelParallelStrategy, io.IOMixin):
 
             if not self.cfsdp2:
                 base_model_tp_plan.update(fsdp2_model_tp_plan)
-
-            if self.sequence_parallel:
-                # Enable sequence parallelism only if TP size > 1
-                base_model_tp_plan.update(base_model_sp_plan)
+                if self.sequence_parallel:
+                    # Enable sequence parallelism only if TP size > 1
+                    base_model_tp_plan.update(base_model_sp_plan)
+            elif self.sequence_parallel:
+                logging.info(
+                    "Sequence parallelism is disabled. It is not compatible with cFSDP2. "
+                )
 
             self.tp_shard_plan = base_model_tp_plan
             logging.info(
