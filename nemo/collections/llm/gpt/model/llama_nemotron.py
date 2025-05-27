b0c4fc42fca1be00379299572e7eb46c2e0a59fd
Ao Tang <aot@nvidia.com>
2025-05-27 16:47:08 -0400

    update model_transform load_context as well

--- nemo/collections/llm/gpt/model/llama_nemotron.py
+++ nemo/collections/llm/gpt/model/llama_nemotron.py
@@ -564,7 +564,7 @@ class HFLlamaNemotronPEFTExporter(HFLlamaNemotronExporter):
         """
         from nemo.collections.llm.peft import CanonicalLoRA, DoRA, LoRA
 
-        self.peft_obj: Union[LoRA, DoRA, CanonicalLoRA] = io.load_context(str(self)).model.model_transform
+        self.peft_obj: Union[LoRA, DoRA, CanonicalLoRA] = io.load_context(str(self), subpath="model.model_transform")
 
         source, _ = self.nemo_load(str(self))
         target = self.init(torch_dtype_from_mcore_config(source.config))
