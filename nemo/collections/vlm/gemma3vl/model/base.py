11dbe6fe8a7f6476f29906b6cbc5b93517389472
Mayank Sharan <mayanksharan96@gmail.com>
2025-06-02 06:30:16 -0700

    Fix Gemma3VL training bugs (#13766)
    * fix gemma3 export
    
    * Add megatron dataset batch loading key backwards compatibility for gemma3vl
    
    * Removing export test file
    
    ---------
    
    Co-authored-by: Xiang Xu <xiangxu@google.com>

--- nemo/collections/vlm/gemma3vl/model/base.py
+++ nemo/collections/vlm/gemma3vl/model/base.py
@@ -63,7 +63,7 @@ def gemma3vl_data_step(dataloader_iter) -> Dict[str, torch.Tensor]:
         _batch = batch
 
     required_keys = set()
-    required_keys.update(("input_ids", "position_ids"))
+    required_keys.update(("input_ids", "position_ids", "tokens"))
     if ps.is_pipeline_first_stage():
         required_keys.update(("pixel_values",))
     if ps.is_pipeline_last_stage():
@@ -84,7 +84,7 @@ def gemma3vl_data_step(dataloader_iter) -> Dict[str, torch.Tensor]:
 def gemma3vl_forward_step(model, batch) -> torch.Tensor:
     """Gemma3 VL model forward step"""
     forward_args = {
-        "input_ids": batch["input_ids"],
+        "input_ids": batch.get("input_ids", batch["tokens"]),
         "position_ids": batch["position_ids"],
         "pixel_values": batch.get("pixel_values", None),
         "loss_mask": batch.get("loss_mask", None),
