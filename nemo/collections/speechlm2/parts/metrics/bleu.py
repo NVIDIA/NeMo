362757db3b5ea2108a48129e587affbd9585b0f7
Ankita Pasad <apasad@nvidia.com>
2025-06-03 18:51:54 -0700

    Rectify speechlm2 BLEU evaluation (#13762)
    Rectify BLEU and ASR-BLEU evaluation. The previous implementation had an incorrect input format for sacrebleu, leading to incorrect (possibly inflated) results. 
    
    ---------
    
    Signed-off-by: Ankita Pasad <apasad@nvidia.com>
    Co-authored-by: Charlie Truong <chtruong@nvidia.com>

--- nemo/collections/speechlm2/parts/metrics/bleu.py
+++ nemo/collections/speechlm2/parts/metrics/bleu.py
@@ -44,7 +44,7 @@ class BLEU:
 
     def update(self, name: str, refs: list[str], hyps: list[str]) -> None:
         for ref, hyp in zip(refs, hyps):
-            self._refs[name].append([self.normalizer(ref)])
+            self._refs[name].append(self.normalizer(ref))
             self._hyps[name].append(self.normalizer(hyp))
             if self.verbose:
                 asrb = sacrebleu.sentence_bleu(hyp, [ref]).score
@@ -53,7 +53,7 @@ class BLEU:
     def compute(self) -> dict[str, torch.Tensor]:
         corpus_metric = {}
         for name in self._refs.keys():
-            metric = torch.tensor(sacrebleu.corpus_bleu(self._hyps[name], self._refs[name]).score)
+            metric = torch.tensor(sacrebleu.corpus_bleu(self._hyps[name], [self._refs[name]]).score)
             corpus_metric[f"txt_bleu_{name}"] = metric
         corpus_metric["txt_bleu"] = torch.stack(list(corpus_metric.values())).mean()
         self._refs.clear()
