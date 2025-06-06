aa539251469dbf7b5e1b59768d00775a7cf02ff7
Arthur <atte.book@gmail.com>
2025-06-06 13:26:44 -0700

    fix(asr): handle missing multistorageclient gracefully
    Signed-off-by: Arthur <atte.book@gmail.com>

--- nemo/utils/msc_utils.py
+++ nemo/utils/msc_utils.py
@@ -21,6 +21,7 @@ try:
     import multistorageclient as msc
 
     HAVE_MSC = True
+
 except (ImportError, ModuleNotFoundError):
     msc = None
 
@@ -40,7 +41,7 @@ def is_multistorageclient_url(path: Union[str, Path]):
     if isinstance(path, Path):
         return False
 
-    has_msc_prefix = path and path.startswith(msc.types.MSC_PROTOCOL)
+    has_msc_prefix = path and path.startswith("msc://")
 
     if HAVE_MSC:
         return has_msc_prefix
@@ -57,7 +58,5 @@ def is_multistorageclient_url(path: Union[str, Path]):
 def import_multistorageclient():
     """Import multistorageclient if it is installed."""
     if not HAVE_MSC:
-        raise ValueError(
-            "Multi-Storage Client is not installed. Please install it with " '"pip install multi-storage-client".'
-        )
+        raise ValueError('Multi-Storage Client is not installed. Please install it with "pip install multi-storage-client".')
     return msc
