57378db9e8303d5cc730973b92c69f7cb93bb9e8
Chen Cui <chcui@nvidia.com>
2025-06-10 00:34:10 -0700

    revert llama 3 system prompt
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- nemo/collections/vlm/neva/data/conversation.py
+++ nemo/collections/vlm/neva/data/conversation.py
@@ -454,7 +454,9 @@ conv_llava_llama_2 = Conversation(
 )
 
 conv_llava_llama_3 = Conversation(
-    system=None,
+    system="You are a helpful language and vision assistant. "
+           "You are able to understand the visual content that the user provides, "
+           "and assist the user with a variety of tasks using natural language.",
     roles=("user", "assistant"),
     version="llama_v3",
     messages=[],
