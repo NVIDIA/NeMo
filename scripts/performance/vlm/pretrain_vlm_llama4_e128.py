93dbf9774596e10489b6a86bfc69f89e741dc433
gdengk <gdengk@users.noreply.github.com>
2025-06-11 04:54:49 +0000

    Apply isort and black reformatting
    Signed-off-by: gdengk <gdengk@users.noreply.github.com>

--- scripts/performance/vlm/pretrain_vlm_llama4_e128.py
+++ scripts/performance/vlm/pretrain_vlm_llama4_e128.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
         nemo_home=args.nemo_home,
         wandb_key=args.wandb_key,
     )
-    
+
     if args.gpu.lower() in ['gb200'] and "PYTORCH_CUDA_ALLOC_CONF" in executor.env_vars:
         del executor.env_vars["PYTORCH_CUDA_ALLOC_CONF"]
 
