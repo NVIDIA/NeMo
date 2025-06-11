2feec25af95120476c427302cb8e9b3a376ddea7
ashbhandare <abhandare@nvidia.com>
2025-06-11 14:59:07 -0700

    specify nemo run commit
    Signed-off-by: ashbhandare <abhandare@nvidia.com>

--- nemo/lightning/run/plugins.py
+++ nemo/lightning/run/plugins.py
@@ -191,7 +191,7 @@ class NsysPlugin(run.Plugin):
             if hasattr(launcher, "nsys_gpu_metrics"):
                 launcher.nsys_gpu_metrics = self.nsys_gpu_metrics
             else:
-                logging.warning(f"Unable to enable nsys gpu metrics collection. Please upgrade Nemo-Run.")
+                logging.warning(f"Unable to enable nsys gpu metrics collection. Please upgrade Nemo-Run to include commit 70a0df4.")
 
 
 @dataclass(kw_only=True)
