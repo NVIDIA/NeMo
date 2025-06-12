a03ce85e4f183c2c24f255640af493e42b9efce6
Alexey Gronskiy <alexey@gronskiy.com>
2025-06-12 14:02:41 +0200

    feat(eval): adds benchmark adapters that allow specisal reasoning models (#13709)
    * feat(adapters): adds adapter config
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * feat(adapters): adds adapters and server
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * feat(adapters): adds hooking logic
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: agronskiy <agronskiy@users.noreply.github.com>
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(adapters): small refactor
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * tests(eval/adapters): adds evaluation adapters tests
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * tests(eval/adapters): register the tests in nemo CI tests machinery
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: pre-commit-ci[bot] <pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(self-review): copyright info
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(lint): linting
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: agronskiy <agronskiy@users.noreply.github.com>
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(lint): linting cont'd
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore: stop server
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * feat: choice of a server port
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * fix: bind to localhost port
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * docs(llm/evals): adds documentation for evals
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * fix(docs): some typos in docs
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(review): comments addressed
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(review): comments addressed -- II
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * feat: refactor subprocess
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * docs: append the api_url
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore: logging noisiness
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * Update docs/source/evaluation/evaluation-adapters.md
    
    Co-authored-by: jgerh <163925524+jgerh@users.noreply.github.com>
    Signed-off-by: Alexey Gronskiy <alexey@gronskiy.com>
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * Update docs/source/evaluation/evaluation-adapters.md
    
    Co-authored-by: jgerh <163925524+jgerh@users.noreply.github.com>
    Signed-off-by: Alexey Gronskiy <alexey@gronskiy.com>
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(review): addressing review comments
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    * chore(review): addressing review comments - 2
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    
    ---------
    
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>
    Signed-off-by: agronskiy <agronskiy@users.noreply.github.com>
    Signed-off-by: pre-commit-ci[bot] <pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Alexey Gronskiy <alexey@gronskiy.com>
    Co-authored-by: agronskiy <agronskiy@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: jgerh <163925524+jgerh@users.noreply.github.com>
    Co-authored-by: oliver könig <okoenig@nvidia.com>

--- tests/evaluation/adapters/conftest.py
+++ tests/evaluation/adapters/conftest.py
@@ -11,3 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+from typing import Any, Generator
+
+import pytest
+
+from .test_utils import create_fake_endpoint_process
+
+
+@pytest.fixture
+def fake_openai_endpoint() -> Generator[Any, Any, Any]:
+    """Fixture to create a Flask app with an OpenAI response.
+
+    Being a "proper" fake endpoint, it responds with a payload which can be
+    set via app.config.response.
+    """
+    # Create and run the fake endpoint server
+    p = create_fake_endpoint_process()
+
+    yield p  # We only need the process reference for cleanup
+
+    p.terminate()
