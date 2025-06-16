ae49fb2952b5afd5df3107fbedf0ac702feed2c2
JRD971000 <ataghibakhsh@nvidia.com>
2025-06-16 16:28:49 -0700

    add license header

--- tests/collections/llm/conversion/test_nmh_conversion.py
+++ tests/collections/llm/conversion/test_nmh_conversion.py
@@ -1,8 +1,21 @@
-import argparse
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from nemo.collections import llm
-from nemo.collections.llm.gpt.model.ssm import HFNemotronHExporter, HFNemotronHImporter
-
+from nemo.collections.llm.gpt.model.ssm import HFNemotronHExporter
+from nemo.collections.llm.gpt.model.ssm import HFNemotronHImporter
+import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -12,12 +25,13 @@ def get_args():
     return parser.parse_args()
 
 
+
 if __name__ == "__main__":
 
     args = get_args()
     nmh_config = llm.NemotronHConfig4B()
     if args.conversion_type == "NEMO2_TO_HF":
-
+            
         exporter = HFNemotronHExporter(args.source_ckpt, model_config=nmh_config)
         exporter.apply(args.target_ckpt)
 
@@ -25,6 +39,6 @@ if __name__ == "__main__":
 
         exporter = HFNemotronHImporter(args.source_ckpt)
         exporter.apply(args.target_ckpt)
-
+    
     else:
         raise ValueError(f"Invalid conversion type: {args.conversion_type}")
