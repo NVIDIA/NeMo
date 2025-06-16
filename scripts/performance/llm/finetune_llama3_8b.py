d133fd85d3086617a74957df8d08bb666091a075
Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
2025-06-16 16:42:05 +0530

    Fix merge conflicts
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>

--- scripts/performance/llm/finetune_llama3_8b.py
+++ scripts/performance/llm/finetune_llama3_8b.py
@@ -24,7 +24,7 @@ from nemo.lightning.run.plugins import MemoryProfilePlugin, NsysPlugin, PerfEnvP
 from ..argument_parser import parse_cli_args
 from ..executors import slurm_executor
 from ..helpers import args_sanity_check, get_user_configs, set_exp_logging_configs, set_primary_perf_configs
-from ..utils import hf_tokenizer, import_ckpt_experiment, isfile_train_pack_metadata,prepare_squad_dataset_experiment
+from ..utils import get_comm_overlap_callback_idx, hf_tokenizer, import_ckpt_experiment, isfile_train_pack_metadata,prepare_squad_dataset_experiment
 
 HF_MODEL_URI = "meta-llama/Meta-Llama-3-8B"
 
