117e4f12225213a95cc47960c699585fefdb857e
DavitGrigoryan132 <DavitGrigoryan132@users.noreply.github.com>
2025-06-17 15:52:01 +0000

    Apply isort and black reformatting
    Signed-off-by: DavitGrigoryan132 <DavitGrigoryan132@users.noreply.github.com>
    Signed-off-by: David Grigoryan <dgrigoryan132@gmail.com>

--- examples/asr/distillation/distill.py
+++ examples/asr/distillation/distill.py
@@ -1,24 +1,23 @@
 import logging
-import torch
+
 import lightning.pytorch as pl
+import torch
 from omegaconf import DictConfig, OmegaConf
-
-
 from torch.nn import MSELoss
 
-from nemo.core.config import hydra_runner
-from nemo.core.classes.mixins import AccessMixin
-from nemo.collections.asr.models import ASRModel, EncDecCTCModel
-from nemo.utils.trainer_utils import resolve_trainer_cfg
-from nemo.utils.exp_manager import exp_manager
 from nemo.collections.asr.data.audio_to_text_dali import DALIOutputs
+from nemo.collections.asr.models import ASRModel, EncDecCTCModel
+from nemo.core.classes.mixins import AccessMixin
+from nemo.core.config import hydra_runner
+from nemo.utils.exp_manager import exp_manager
+from nemo.utils.trainer_utils import resolve_trainer_cfg
 
 
 class KDAdapter(torch.nn.Module):
-    """Adapter for knowledge distillation. This module adapts the teacher's features to match the student's features.
-    """
+    """Adapter for knowledge distillation. This module adapts the teacher's features to match the student's features."""
+
     def __init__(self, in_channels, out_channels, kernel_size=1):
-        """ 
+        """
 
         Args:
             in_channels: the number of input channels from the teacher model.
@@ -26,69 +25,60 @@ class KDAdapter(torch.nn.Module):
             kernel_size: the size of the convolution kernel to use for adaptation.
         """
         super().__init__()
-        self.adapter = torch.nn.Conv1d(in_channels=in_channels,
-                                       out_channels=out_channels,
-                                       kernel_size=kernel_size)
+        self.adapter = torch.nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
         self.kd_loss_fn = MSELoss(reduction="mean")
 
     def forward(self, t_feats, s_feats):
         x = t_feats.permute(0, 2, 1)
         x = self.adapter(x)
-        x = torch.nn.functional.interpolate(x, size=s_feats.size(1),
-                          mode='linear',
-                          align_corners=False)
+        x = torch.nn.functional.interpolate(x, size=s_feats.size(1), mode='linear', align_corners=False)
         proj_t = x.permute(0, 2, 1)
         return self.kd_loss_fn(proj_t, s_feats)
 
 
-class KnowledgeDistillationModule(EncDecCTCModel):
+class KnowledgeDistillationCTCModule(EncDecCTCModel):
     def __init__(self, cfg: DictConfig, trainer=None):
-        super(KnowledgeDistillationModule, self).__init__(cfg, trainer)
-        
+        super(KnowledgeDistillationCTCModule, self).__init__(cfg, trainer)
+
         if not hasattr(cfg, "student_model_path"):
-            raise ValueError("Knowledge distillation config file must have `student_model_path`")        
+            raise ValueError("Knowledge distillation config file must have `student_model_path`")
         if not hasattr(cfg, "teacher_model_path"):
             raise ValueError("Knowledge distillation config file must have `teacher_model_path`")
 
-        student_model_path = cfg.student_model_path
+        student_model_path = cfg.get("student_model_path", None)
         if student_model_path is None:
             raise ValueError("`student_model_path` cannot be None")
 
-        student_model = ASRModel.restore_from(
-            student_model_path, strict=cfg.get("init_strict", True)
-        )
+        student_model = ASRModel.restore_from(student_model_path)
 
         # Restore checkpoint into current model
         self.load_state_dict(student_model.state_dict(), strict=False)
         logging.info(f'Student model checkpoint restored from nemo file with path : `{student_model_path}`')
         del student_model
 
-        teacher_model_path = cfg.teacher_model_path
+        teacher_model_path = cfg.get("teacher_model_path", None)
         if teacher_model_path is None:
             raise ValueError("`teacher_model_path` cannot be None")
-        
+
         self.teacher = ASRModel.restore_from(restore_path=teacher_model_path)
         self.teacher.eval()
         if not hasattr(cfg, "kd_pairs"):
             raise ValueError("Knowledge distillation config file must have `kd_pairs`")
-        
+
         # Freeze teacher parameters
         for param in self.teacher.parameters():
             param.requires_grad = False
-        
-        self.kd_pairs = cfg.kd_pairs
+
+        self.kd_pairs = cfg.get("kd_pairs", None)
         if self.kd_pairs is None:
             raise ValueError("`kd_pairs` cannot be None")
-        
-        self.kd_loss_weight = getattr(cfg, "kd_loss_weight", 1.0)
-        
+
+        self.kd_loss_weight = cfg.get("kd_loss_weight", 1.0)
+
         # Dictionaries to store intermediate outputs from hooks
         self.teacher_outputs = {}
         self.student_outputs = {}
-        
-        self.temperature = cfg.get("kd_temperature", 1.0)
-        self.kd_logits_loss_fn = torch.nn.KLDivLoss(reduction="batchmean")
-        
+
         # Register forward hooks for specified layers
         t_modules = dict(self.teacher.named_modules())
         s_modules = dict(self.named_modules())
@@ -101,17 +91,19 @@ class KnowledgeDistillationModule(EncDecCTCModel):
                 raise ValueError(f"Student layer '{s_name}' not found.")
             t_modules[t_name].register_forward_hook(self._get_hook(self.teacher_outputs, t_name))
             s_modules[s_name].register_forward_hook(self._get_hook(self.student_outputs, s_name))
-        
+
         self.adapters = torch.nn.ModuleDict()
         for pair in self.kd_pairs:
             self.adapters[pair["student"].replace(".", "_")] = KDAdapter(pair["t_dim"], pair["s_dim"])
 
     def _get_hook(self, output_dict, name):
         """Returns a hook that stores the layer's output in output_dict."""
+
         def hook(module, input, output):
             output_dict[name] = output
+
         return hook
-    
+
     def training_step(self, batch, batch_nb):
         # Reset access registry
         if AccessMixin.is_access_enabled(self.model_guid):
@@ -136,16 +128,14 @@ class KnowledgeDistillationModule(EncDecCTCModel):
                 t_log_probs, t_encoded_len, t_predictions = self.teacher.forward(
                     input_signal=signal, input_signal_length=signal_len
                 )
-        
+
         # student forward-pass
         if is_dali:
             log_probs, encoded_len, predictions = super().forward(
                 processed_signal=signal, processed_signal_length=signal_len
             )
         else:
-            log_probs, encoded_len, predictions = super().forward(
-                input_signal=signal, input_signal_length=signal_len
-            )
+            log_probs, encoded_len, predictions = super().forward(input_signal=signal, input_signal_length=signal_len)
 
         loss_value = self.loss(
             log_probs=log_probs,
@@ -159,7 +149,7 @@ class KnowledgeDistillationModule(EncDecCTCModel):
             t_feat = self.teacher_outputs[pair["teacher"]]
             s_feat = self.student_outputs[pair["student"]]
             kd_loss += self.adapters[pair["student"].replace(".", "_")](t_feat, s_feat)
-        
+
         kd_loss = kd_loss * self.kd_loss_weight
 
         loss_value = loss_value + kd_loss
@@ -178,12 +168,14 @@ class KnowledgeDistillationModule(EncDecCTCModel):
             AccessMixin.reset_registry(self)
 
         # 7) logging
-        tb_logs.update({
-            "train_loss": loss_value,
-            "kd_loss": kd_loss,
-            "learning_rate": self._optimizer.param_groups[0]["lr"],
-            "global_step": torch.tensor(self.trainer.global_step, dtype=torch.float32),
-        })
+        tb_logs.update(
+            {
+                "train_loss": loss_value,
+                "kd_loss": kd_loss,
+                "learning_rate": self._optimizer.param_groups[0]["lr"],
+                "global_step": torch.tensor(self.trainer.global_step, dtype=torch.float32),
+            }
+        )
         if (batch_nb + 1) % log_every_n == 0:
             self.wer.update(
                 predictions=log_probs,
@@ -197,14 +189,15 @@ class KnowledgeDistillationModule(EncDecCTCModel):
 
         return {"loss": loss_value, "log": tb_logs}
 
+
 @hydra_runner(config_path="conf/asr_distillation", config_name="distill")
 def main(cfg):
     logging.info(f"Hydra config: {OmegaConf.to_yaml(cfg)}")
 
     trainer = pl.Trainer(**resolve_trainer_cfg(cfg.trainer))
     exp_manager(trainer, cfg.get("exp_manager", None))
-    
-    kd_model = KnowledgeDistillationModule(cfg=cfg.model, trainer=trainer)
+
+    kd_model = KnowledgeDistillationCTCModule(cfg=cfg.model, trainer=trainer)
 
     kd_model.setup_training_data(cfg.model.train_ds)
     kd_model.setup_multiple_validation_data(cfg.model.validation_ds)
@@ -212,6 +205,7 @@ def main(cfg):
     kd_model.setup_optimization(cfg.model.optim)
 
     trainer.fit(kd_model)
+
 
 if __name__ == "__main__":
     main()
