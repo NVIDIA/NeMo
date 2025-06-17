1d221c14e8e707bc11d2b50339d947ac101ee096
rhmukundan <102543536+rhmukundan@users.noreply.github.com>
2025-06-17 21:56:43 +0530

    Fix for Squad Dataset Download (#13893)
    * Fix for Squad Dataset Download
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Giving the option to pass the sequence length from the finetune script
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Rebase Pushing llama4 finetuning e128 script and llama3 70b finetuning to include the dataset download fix
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Finetune Llama4 Recipe with dataset download fix
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Address PR comments
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Tweaks to finetune_llama4_e128
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Addressing PR comments
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Giving an option to have either AutoTokenizer or NullTokenizer for preparing the dataset
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Fix kwargs
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * User passing vocab_size while using the NullTokenizer for downloading dataset
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Adding model configs for finetune llama4
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Rebase Introducing the fix to llama3 finetuning recipes as well
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Setting default vocab_size to None in prepare_squad_dataset_experiment function
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Fix merge conflicts
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Fixing the search condition for the dataset
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: rhmukundan <rhmukundan@users.noreply.github.com>
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Removing NullTokenizer from Finetuning scripts
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Import cleanup
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: rhmukundan <rhmukundan@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Raghav Hrishikeshan Mukundan <rmukundan@nvidia.com>
    Signed-off-by: rhmukundan <rhmukundan@users.noreply.github.com>
    Co-authored-by: rhmukundan <rhmukundan@users.noreply.github.com>

--- scripts/performance/llm/finetune_llama4_e128.py
+++ scripts/performance/llm/finetune_llama4_e128.py
@@ -14,27 +14,25 @@
 
 from os.path import basename, splitext
 
-import fiddle as fdl
-import fiddle._src.experimental.dataclasses as fdl_dc
 import nemo_run as run
 
-from nemo.collections.llm.recipes.llama31_405b import finetune_recipe, model
-from nemo.collections.llm.recipes.tp_overlap_configs.userbuffers import (
-    userbuffers_fp8_h100_h16384_tp4_mbs1_seqlen2048_lora,
-)
+from nemo.collections.llm.recipes.llama4_e128 import finetune_recipe, model
+from nemo.collections.llm.recipes.precision.mixed_precision import bf16_with_fp8_mixed
 from nemo.lightning.run.plugins import MemoryProfilePlugin, NsysPlugin, PerfEnvPlugin
 
 from ..argument_parser import parse_cli_args
-from ..executors import slurm_executor
-from ..helpers import args_sanity_check, get_user_configs, set_exp_logging_configs, set_primary_perf_configs
 from ..utils import (
-    get_comm_overlap_callback_idx,
+    args_sanity_check,
+    get_user_configs,
     hf_tokenizer,
     import_ckpt_experiment,
     prepare_squad_dataset_experiment,
+    set_exp_logging_configs,
+    set_primary_perf_configs,
+    slurm_executor,
 )
 
-HF_MODEL_URI = "meta-llama/Llama-3.1-405B"
+HF_MODEL_URI = "meta-llama/Llama-4-Maverick-17B-128E-Instruct"
 
 # Set this to True if checkpoint is available at 'NEMO_HOME'. If set to False,
 # extra Slurm job will be scheduled. In this case, if checkpoint is available
@@ -57,18 +55,20 @@ def override_recipe_configs(
     cp_size: int,
     vp_size: int,
     ep_size: int,
+    etp_size: int,
     enable_cuda_graphs: bool,
     use_mcore_fsdp: bool,
     recompute_layers: int,
     activation_offload_layers: int,
 ):
     """
-    llama3.1 405b pre-train recipe aimed at achieving best possible performance.
+    Llama4 e128 fine-tuning recipe aimed at achieving best possible performance.
 
     NOTE: Use fp8 precision training with caution. It might not give desirable results.
     """
     finetuning_scheme = "none" if args.finetuning == "sft" else args.finetuning
-    recipe = finetune_recipe(peft_scheme=finetuning_scheme, performance_mode=True)
+
+    recipe = finetune_recipe(peft_scheme=finetuning_scheme, performance_mode=True, packed_sequence=True)
 
     recipe = set_primary_perf_configs(
         recipe,
@@ -83,21 +83,20 @@ def override_recipe_configs(
         cp_size,
         vp_size,
         ep_size,
+        etp_size,
         enable_cuda_graphs=enable_cuda_graphs,
         use_mcore_fsdp=use_mcore_fsdp,
         recompute_layers=recompute_layers,
         activation_offload_layers=activation_offload_layers,
         compute_dtype=args.compute_dtype,
         fp8_recipe=args.fp8_recipe,
-        nccl_communicator_config_path=args.nccl_communicator_config_path,
-        use_user_buffer_registration=args.use_user_buffer_registration,
-        use_sharp=args.use_sharp,
     )
+
     recipe = set_exp_logging_configs(
         recipe,
         finetuning_scheme,
         "llm",
-        "llama3",
+        "llama4",
         args.tensorboard,
         args.wandb,
         args.wandb_prj_name,
@@ -107,36 +106,15 @@ def override_recipe_configs(
     # data module configs
     recipe.data.tokenizer = hf_tokenizer(HF_MODEL_URI)
 
-    comm_overlap_callback_idx = get_comm_overlap_callback_idx(recipe.trainer.callbacks)
-    assert comm_overlap_callback_idx is not None, "MegatronCommOverlapCallback missing. Required for performance."
+    # Compute dtype configs
+    if args.compute_dtype.lower() == "fp8":
+        recipe.trainer.plugins = bf16_with_fp8_mixed()
+        recipe.trainer.plugins.grad_reduce_in_fp32 = False
 
-    if (
-        finetuning_scheme == "lora"
-        and tp_size > 1
-        and args.compute_dtype.lower() == "fp8"
-        and args.fp8_recipe.lower() != "mxfp8"
-    ):
-        tp_comm_overlap_cfg = userbuffers_fp8_h100_h16384_tp4_mbs1_seqlen2048_lora if tp_size == 4 else None
-        if tp_comm_overlap_cfg:
-            # Enable TP comm overlap with the given config
-            recipe.trainer.callbacks[comm_overlap_callback_idx].tp_comm_overlap = True
-            tp_comm_overlap_cfg = fdl.cast(run.Config, fdl_dc.convert_dataclasses_to_configs(tp_comm_overlap_cfg))
-            recipe.trainer.callbacks[comm_overlap_callback_idx].tp_comm_overlap_cfg = tp_comm_overlap_cfg
-
-            # Disable this overlap to allow skipping an all-gather which is redundant for LoRA
-            recipe.model.config.tp_comm_overlap_disable_qkv = True
-
-            # Allow overlapping of dgrad reduce-scatter with dgrad GEMMs
-            # (instead of wgrad GEMMs which are not done when using LoRA)
-            recipe.model.config.tp_comm_bulk_dgrad = False
-            recipe.model.config.tp_comm_overlap_rs_dgrad = True
-    if args.compute_dtype.lower() == "fp8" and args.fp8_recipe.lower() == "mxfp8":
-        recipe.trainer.callbacks[comm_overlap_callback_idx].tp_comm_overlap_cfg = None
-        recipe.trainer.callbacks[comm_overlap_callback_idx].tp_comm_overlap = False
-
-    recipe.optim.config.use_distributed_optimizer = True
-    recipe.model.config.disable_parameter_transpose_cache = True
-
+    recipe.model.config.cross_entropy_fusion_impl = "te"
+    recipe.model.config.cross_entropy_loss_fusion = True
+    recipe.model.config.apply_rope_fusion = True
+    recipe.model.config.moe_permute_fusion = True
     return recipe
 
 
@@ -144,7 +122,7 @@ if __name__ == "__main__":
     args = parse_cli_args().parse_args()
     args_sanity_check(args)
 
-    kwargs = get_user_configs(args.gpu.lower(), args.finetuning, "llama31", "405b", args)
+    kwargs = get_user_configs(args.gpu.lower(), "sft", "llama4", "e128", args)
     (
         num_nodes,
         mbs,
@@ -154,12 +132,12 @@ if __name__ == "__main__":
         cp_size,
         vp_size,
         ep_size,
-        _,
+        etp_size,
         enable_cuda_graphs,
         use_mcore_fsdp,
         recompute_layers,
         activation_offload_layers,
-    ) = kwargs[:13]
+    ) = kwargs[0:13]
 
     recipe = override_recipe_configs(
         args,
@@ -171,17 +149,32 @@ if __name__ == "__main__":
         cp_size,
         vp_size,
         ep_size,
+        etp_size,
         enable_cuda_graphs,
         use_mcore_fsdp,
         recompute_layers,
         activation_offload_layers,
     )
+    exp_config = (
+        f"{num_nodes}nodes_tp{tp_size}_pp{pp_size}_cp{cp_size}_vp{vp_size}_ep{ep_size}_etp{etp_size}_{mbs}mbs_{gbs}gbs"
+    )
+    exp_name = f"{splitext(basename(__file__))[0]}_{args.compute_dtype}_{exp_config}"
 
-    exp_config = f"{num_nodes}nodes_tp{tp_size}_pp{pp_size}_cp{cp_size}_vp{vp_size}_{mbs}mbs_{gbs}gbs"
-    exp_name = f"{args.finetuning}_{splitext(basename(__file__))[0]}_{args.compute_dtype}_{exp_config}"
+    plugins = [
+        PerfEnvPlugin(
+            enable_vboost=True,
+            nccl_pp_comm_chunksize=2097152 if pp_size > 1 else None,
+            gpu_sm100_or_newer=(args.gpu.lower() in ['b200', 'gb200']),
+        )
+    ]
+
+    if args.enable_nsys:
+        plugins.append(NsysPlugin(start_step=5, end_step=6))
+    if args.enable_memory_profile:
+        assert args.memory_profile_out_path is not None
+        plugins.append(MemoryProfilePlugin(dir=args.memory_profile_out_path))
 
     executor = slurm_executor(
-        args.gpu.lower(),
         args.account,
         args.partition,
         args.log_dir,
@@ -194,21 +187,7 @@ if __name__ == "__main__":
         hf_token=args.hf_token,
         nemo_home=args.nemo_home,
         wandb_key=args.wandb_key,
-        network='sharp' if args.use_sharp else None,
     )
-
-    plugins = [
-        PerfEnvPlugin(
-            enable_vboost=True,
-            nccl_pp_comm_chunksize=2097152 if pp_size > 1 else None,
-            gpu_sm100_or_newer=(args.gpu.lower() in ['b200', 'gb200']),
-        )
-    ]
-    if args.enable_nsys:
-        plugins.append(NsysPlugin(start_step=5, end_step=6))
-    if args.enable_memory_profile:
-        assert args.memory_profile_out_path is not None
-        plugins.append(MemoryProfilePlugin(dir=args.memory_profile_out_path))
 
     with run.Experiment(exp_name) as exp:
         if not SKIP_IMPORT:
@@ -224,7 +203,6 @@ if __name__ == "__main__":
             name=exp_name,
             plugins=plugins,
         )
-
         if not args.dryrun:
             exp.run(sequential=True, detach=True)
         else:
--- scripts/performance/utils.py
+++ scripts/performance/utils.py
@@ -73,6 +73,96 @@ def import_ckpt_experiment(executor: run.SlurmExecutor, model: run.Config[GPTMod
     return run.Partial(import_ckpt, model=model, source=source, overwrite=False), import_executor, "import_ckpt_exp"
 
 
+def get_nemo_home(nemo_home=None):
+    """
+    Get NEMO_HOME path. Checks for both nemo_home argument and NEMO_HOME environment variable.
+    """
+    arg_nemo_set = nemo_home is True
+    env_nemo_set = "NEMO_HOME" in os.environ
+
+    if arg_nemo_set and env_nemo_set:
+        if os.environ["NEMO_HOME"] != nemo_home:
+            logging.warning(f"Using nemo_home ({nemo_home}) instead of NEMO_HOME ({os.environ['NEMO_HOME']})")
+        return nemo_home
+
+    if arg_nemo_set:
+        return nemo_home
+
+    if env_nemo_set:
+        return os.environ["NEMO_HOME"]
+
+    raise ValueError("Neither -nh/--nemo_home argument nor NEMO_HOME environment variable is set")
+
+
+def prepare_squad_dataset(model_name: str, seq_length: int = 2048, nemo_home=None):
+    """Prepare the SQuAD dataset for fine-tuning.
+
+    Args:
+        model_name (str): The name of the model
+        seq_length (int): The sequence length to use for packing. Defaults to 2048.
+        nemo_home: Optional path to NEMO home directory set via args.nemo_home
+    """
+    from pathlib import Path
+
+    from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTokenizer
+    from nemo.collections.llm.gpt.data.packed_sequence import PackedSequenceSpecs
+    from nemo.collections.llm.gpt.data.squad import SquadDataModule
+
+    nemo_home_path = Path(get_nemo_home(nemo_home))
+    dataset_root = nemo_home_path / "datasets" / "squad"
+    dataset_root.mkdir(parents=True, exist_ok=True)
+
+    tokenizer = AutoTokenizer(pretrained_model_name=model_name)
+
+    # Configure SquadDataModule with packing specs
+    datamodule = SquadDataModule(
+        dataset_root=dataset_root,
+        seq_length=seq_length,
+        global_batch_size=8,
+        micro_batch_size=1,
+        packed_sequence_specs=PackedSequenceSpecs(packed_sequence_size=seq_length),
+        tokenizer=tokenizer,
+        force_redownload=True,
+        delete_raw=False,
+        seed=1234,
+    )
+
+    # This will generate both JSONL and packed .bin files
+    datamodule.prepare_data()
+
+    # Verify the output
+    packed_dir = dataset_root / "packed" / model_name.replace("/", "--")
+    print(f"Packed files should be in: {packed_dir}")
+    if packed_dir.exists():
+        print("Files found:", list(packed_dir.glob("*")))
+    else:
+        raise FileNotFoundError(f"Packed dataset dir not found at {packed_dir}. Dataset download failed")
+
+
+def prepare_squad_dataset_experiment(
+    executor: run.SlurmExecutor, model_name: str, seq_length: int = 2048, nemo_home=None
+):
+    """
+    Downloads and prepares the SQuAD dataset for fine-tuning.
+    """
+    from copy import deepcopy
+
+    dataset_executor = deepcopy(executor)
+    dataset_executor.ntasks_per_node = 1
+    dataset_executor.nodes = 1
+
+    return (
+        run.Partial(
+            prepare_squad_dataset,
+            model_name=model_name,
+            seq_length=seq_length,
+            nemo_home=nemo_home,
+        ),
+        dataset_executor,
+        "prepare_squad_dataset_exp",
+    )
+
+
 def isfile_train_pack_metadata(hf_model_uri: str, data_config: run.Config[SquadDataModule]) -> bool:
     """
     This method is used for fine-tuning. It checks if packed train data for a partiular
