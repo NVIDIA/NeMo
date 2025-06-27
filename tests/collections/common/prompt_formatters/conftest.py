b0b5151bea59e122d268d2ea9b6c191ffb0b8a85
Piotr Żelasko <petezor@gmail.com>
2025-06-27 13:33:40 -0400

    Add Nemotron-H prompt format, fix cut-to-conversation custom attr propagation (#13963)
    * Add Nemotron-H prompt format
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    * Fix propagation of custom attr in cut_to_conversation
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    * Fix CI
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    * Unit test for the conversion fix
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    ---------
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    Co-authored-by: Charlie Truong <chtruong@nvidia.com>
    Signed-off-by: Amir Hussein <amhussein@nvidia.com>

--- tests/collections/common/prompt_formatters/conftest.py
+++ tests/collections/common/prompt_formatters/conftest.py
@@ -41,6 +41,7 @@ Instruct Output
 Feel free to add new tokens for your own tests!?
 But know that if you do so, you may need to update the token IDs in the existing tests! 
 So, it might be a good idea to create a new tokenizer instead when adding new prompt formats.
+SYSTEM
 """
 
 
@@ -58,7 +59,7 @@ def bpe_tokenizer(tmp_path_factory):
         remove_extra_whitespaces=True,
         bos=True,
         eos=True,
-        user_defined_symbols=['\n', '<|im_start|>', '<|im_end|>'],
+        user_defined_symbols=['\n', '<|im_start|>', '<|im_end|>', '<SPECIAL_10>', '<SPECIAL_11>'],
     )
     return SentencePieceTokenizer(str(tmpdir / "tokenizer.model"))
 
