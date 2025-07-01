9c60e0576c71ca3e50d55a556d27fd5a05008d95
yaoyu-33 <yaoyu.094@gmail.com>
2025-07-01 13:36:00 -0700

    remove nemo1 multimodal and vision
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- nemo/collections/multimodal/modules/stable_diffusion/fast_geglu/geglu.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-
-void geglu_cuda(intptr_t out, intptr_t x_and_gate, int64_t n, int dim_last, intptr_t stream);
-
-void geglu_bwd_cuda(intptr_t grad_x_and_gate, intptr_t grad_out, intptr_t x_and_gate, int64_t n, int dim_last, intptr_t stream);
