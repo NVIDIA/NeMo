91d5e0aa971d31903425513648f7ef0b02582cc8
Piotr Żelasko <petezor@gmail.com>
2025-07-01 15:02:24 -0400

    Higher-level API for SALM.generate (#14034)
    * Higher-level API for SALM.generate
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    * fix
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>
    
    ---------
    
    Signed-off-by: Piotr Żelasko <petezor@gmail.com>

--- nemo/collections/asr/parts/preprocessing/features.py
+++ nemo/collections/asr/parts/preprocessing/features.py
@@ -309,6 +309,7 @@ class FilterbankFeatures(nn.Module):
         self.n_fft = n_fft or 2 ** math.ceil(math.log2(self.win_length))
         self.stft_pad_amount = (self.n_fft - self.hop_length) // 2 if exact_pad else None
         self.exact_pad = exact_pad
+        self.sample_rate = sample_rate
 
         if exact_pad:
             logging.info("STFT using exact pad")
