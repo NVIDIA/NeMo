0843c0dc76e93172a16a3933cec443e589eccd03
Youngeun Kwon <youngeunk@nvidia.com>
2025-07-02 15:53:03 -0700

    Interface to enable fsdp-double-buffer without enabling NCCL-UB (#14076)
    * interface draft
    
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>
    
    * minor change
    
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>
    
    * fix the script
    
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: youngeunkwon0405 <youngeunkwon0405@users.noreply.github.com>
    
    * vlm
    
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>
    
    * diffusion
    
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>
    
    * argument validation
    
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: youngeunkwon0405 <youngeunkwon0405@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>
    Signed-off-by: youngeunkwon0405 <youngeunkwon0405@users.noreply.github.com>
    Co-authored-by: youngeunkwon0405 <youngeunkwon0405@users.noreply.github.com>
    Signed-off-by: Amir Hussein <amhussein@nvidia.com>

--- scripts/performance/llm/finetune_llama3_70b.py
+++ scripts/performance/llm/finetune_llama3_70b.py
@@ -92,12 +92,13 @@ def override_recipe_configs(
         ep_size,
         enable_cuda_graphs=enable_cuda_graphs,
         use_mcore_fsdp=use_mcore_fsdp,
+        use_fsdp_double_buffer=args.use_fsdp_double_buffer,
+        use_user_buffer_registration=args.use_user_buffer_registration,
         recompute_layers=recompute_layers,
         activation_offload_layers=activation_offload_layers,
         compute_dtype=args.compute_dtype,
         fp8_recipe=args.fp8_recipe,
         nccl_communicator_config_path=args.nccl_communicator_config_path,
-        use_user_buffer_registration=args.use_user_buffer_registration,
         use_sharp=args.use_sharp,
     )
     recipe = set_exp_logging_configs(
--- scripts/performance/llm/finetune_llama3_8b.py
+++ scripts/performance/llm/finetune_llama3_8b.py
@@ -78,8 +78,10 @@ def override_recipe_configs(
         enable_cuda_graphs=enable_cuda_graphs,
         compute_dtype=args.compute_dtype,
         fp8_recipe=args.fp8_recipe,
-        nccl_communicator_config_path=args.nccl_communicator_config_path,
+        use_mcore_fsdp=args.use_mcore_fsdp,
+        use_fsdp_double_buffer=args.use_fsdp_double_buffer,
         use_user_buffer_registration=args.use_user_buffer_registration,
+        nccl_communicator_config_path=args.nccl_communicator_config_path,
         use_sharp=args.use_sharp,
     )
     recipe = set_exp_logging_configs(
