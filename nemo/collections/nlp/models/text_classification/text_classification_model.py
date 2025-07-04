b920fe2820cd6995be9649b4145fe2cef7e8299d
dimapihtar <dpihtar@gmail.com>
2025-07-04 15:09:04 -0700

    fix style
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- nemo/collections/nlp/models/text_classification/text_classification_model.py
+++ nemo/collections/nlp/models/text_classification/text_classification_model.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# flake8: noqa
+# pylint: skip-file
 
 import os
 from typing import Dict, List, Optional
