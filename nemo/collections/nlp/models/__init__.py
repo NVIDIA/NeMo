9867060408f7bd6a34890da9967a70822d02f77f
Dmytro Pykhtar <37850217+dimapihtar@users.noreply.github.com>
2025-07-08 19:40:54 +0300

    remove text nlp collection (#14110)
    * remove text nlp collection
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>
    
    * fix style
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>
    
    * remove duplex_text
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * fix imports
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>
    
    * remove spellchecking
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * fix import
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>
    
    * fix style
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * fix import
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>
    
    * remove examples
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    ---------
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>
    Signed-off-by: Dmytro Pykhtar <37850217+dimapihtar@users.noreply.github.com>
    Co-authored-by: dimapihtar <dimapihtar@users.noreply.github.com>
    Signed-off-by: Amir Hussein <amhussein@nvidia.com>

--- nemo/collections/nlp/models/__init__.py
+++ nemo/collections/nlp/models/__init__.py
@@ -13,11 +13,6 @@
 # limitations under the License.
 
 
-from nemo.collections.nlp.models.duplex_text_normalization import (  # noqa: F401
-    DuplexDecoderModel,
-    DuplexTaggerModel,
-    DuplexTextNormalizationModel,
-)
 from nemo.collections.nlp.models.entity_linking.entity_linking_model import EntityLinkingModel  # noqa: F401
 from nemo.collections.nlp.models.glue_benchmark.glue_benchmark_model import GLUEModel  # noqa: F401
 from nemo.collections.nlp.models.information_retrieval import BertDPRModel, BertJointIRModel  # noqa: F401
@@ -29,12 +24,6 @@ from nemo.collections.nlp.models.language_modeling import MegatronGPTPromptLearn
 from nemo.collections.nlp.models.language_modeling.bert_lm_model import BERTLMModel  # noqa: F401
 from nemo.collections.nlp.models.language_modeling.transformer_lm_model import TransformerLMModel  # noqa: F401
 from nemo.collections.nlp.models.machine_translation import MTEncDecModel  # noqa: F401
-from nemo.collections.nlp.models.spellchecking_asr_customization import (  # noqa: F401
-    SpellcheckingAsrCustomizationModel,
-)
-from nemo.collections.nlp.models.text2sparql.text2sparql_model import Text2SparqlModel  # noqa: F401
-from nemo.collections.nlp.models.text_classification import TextClassificationModel  # noqa: F401
-from nemo.collections.nlp.models.text_normalization_as_tagging import ThutmoseTaggerModel  # noqa: F401
 from nemo.collections.nlp.models.token_classification import (  # noqa: F401
     PunctuationCapitalizationLexicalAudioModel,
     PunctuationCapitalizationModel,
