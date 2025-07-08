a8df61210697451c5c21b1db5f3c96628095b60e
dimapihtar <dpihtar@gmail.com>
2025-07-08 10:15:46 -0700

    fix importso
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- nemo/collections/nlp/__init__.py
+++ nemo/collections/nlp/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.collections.nlp import data, losses, models, modules
+from nemo.collections.nlp import data, models, modules
 from nemo.package_info import __version__
 
 # Set collection version equal to NeMo version.
