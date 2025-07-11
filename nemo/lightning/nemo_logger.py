afa8f28c8d2f90272a855f8a0fcaeec97ad9ed2c
Charlie Truong <chtruong@nvidia.com>
2025-07-11 11:12:07 -0500

    Fix "Safely import optional python packages (#13936)" (#14198)
    * Revert "Revert "Safely import optional python packages (#13936)" (#14197)"
    
    This reverts commit 808845b548e6ebf73bef24cf1e654f298fae5b76.
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>
    
    * Fix LocalCheckpointCallback safe import
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>
    
    ---------
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>
    Signed-off-by: Amir Hussein <amhussein@nvidia.com>

--- nemo/lightning/nemo_logger.py
+++ nemo/lightning/nemo_logger.py
@@ -22,12 +22,14 @@ import lightning.fabric as fl
 import lightning.pytorch as pl
 from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint as PTLModelCheckpoint
 from lightning.pytorch.loggers import Logger, TensorBoardLogger, WandbLogger
-from nvidia_resiliency_ext.ptl_resiliency.local_checkpoint_callback import LocalCheckpointCallback
 
 from nemo.lightning.io.mixin import IOMixin
 from nemo.lightning.pytorch.callbacks import ModelCheckpoint
 from nemo.utils import logging
 from nemo.utils.app_state import AppState
+from nemo.utils.import_utils import safe_import
+
+lcp, HAVE_RES = safe_import('nvidia_resiliency_ext.ptl_resiliency.local_checkpoint_callback')
 
 
 @dataclass
@@ -203,7 +205,7 @@ class NeMoLogger(IOMixin):
         if ckpt:
             _overwrite_i = None
             for i, callback in enumerate(trainer.callbacks):
-                if isinstance(callback, PTLModelCheckpoint) and not isinstance(callback, LocalCheckpointCallback):
+                if isinstance(callback, PTLModelCheckpoint) and not isinstance(callback, lcp.LocalCheckpointCallback):
                     logging.warning(
                         "The Trainer already contains a ModelCheckpoint callback. " "This will be overwritten."
                     )
@@ -248,7 +250,7 @@ class NeMoLogger(IOMixin):
         from nemo.lightning import MegatronStrategy
 
         for callback in trainer.callbacks:
-            if isinstance(callback, PTLModelCheckpoint) and not isinstance(callback, LocalCheckpointCallback):
+            if isinstance(callback, PTLModelCheckpoint) and not isinstance(callback, lcp.LocalCheckpointCallback):
                 if callback.dirpath is None:
                     callback.dirpath = Path(log_dir / "checkpoints")
                 if callback.filename is None:
