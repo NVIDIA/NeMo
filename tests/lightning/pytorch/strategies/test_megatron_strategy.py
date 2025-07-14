5be0f55df33c4fb211340d38ceab67c9d244367c
ko3n1g <ko3n1g@users.noreply.github.com>
2025-07-14 08:40:37 +0000

    Apply isort and black reformatting
    Signed-off-by: ko3n1g <ko3n1g@users.noreply.github.com>

--- tests/lightning/pytorch/strategies/test_megatron_strategy.py
+++ tests/lightning/pytorch/strategies/test_megatron_strategy.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 from unittest.mock import MagicMock, patch
+
+import pytest
 
 from nemo.lightning.pytorch.strategies import MegatronStrategy
 
