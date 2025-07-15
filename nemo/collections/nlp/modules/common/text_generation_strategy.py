f652d96b05f991a7068a795a7de594caa9d1dd60
dimapihtar <dpihtar@gmail.com>
2025-07-15 07:19:54 -0700

    fix style
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- nemo/collections/nlp/modules/common/text_generation_strategy.py
+++ nemo/collections/nlp/modules/common/text_generation_strategy.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# flake8: noqa
+# pylint: skip-file
+
 import abc
 import copy
 import os
