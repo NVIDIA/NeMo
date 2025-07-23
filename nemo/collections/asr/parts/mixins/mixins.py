12f72f2d4d1bd142da298f883d8c21c0a8697230
stevehuang52 <heh@nvidia.com>
2025-07-23 11:41:35 -0400

    update streaming ASR
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- nemo/collections/asr/parts/mixins/mixins.py
+++ nemo/collections/asr/parts/mixins/mixins.py
@@ -18,7 +18,7 @@ import shutil
 import tarfile
 import unicodedata
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 
 import torch
 from omegaconf import DictConfig, OmegaConf, open_dict
@@ -602,6 +602,7 @@ class ASRModuleMixin(ASRAdapterModelMixin):
         drop_extra_pre_encoded: int = None,
         return_transcription: bool = True,
         return_log_probs: bool = False,
+        valid_out_len: Optional[int] = None,
     ):
         """
         It simulates a forward step with caching for streaming purposes.
@@ -658,6 +659,11 @@ class ASRModuleMixin(ASRAdapterModelMixin):
             keep_all_outputs=keep_all_outputs,
             drop_extra_pre_encoded=drop_extra_pre_encoded,
         )
+
+        if valid_out_len and not keep_all_outputs:
+            # drop right context if any
+            encoded = encoded[:, :, :valid_out_len]
+            encoded_len = torch.ones_like(encoded_len) * valid_out_len
 
         if isinstance(self, asr_models.EncDecCTCModel) or (
             isinstance(self, asr_models.EncDecHybridRNNTCTCModel) and self.cur_decoder == "ctc"
