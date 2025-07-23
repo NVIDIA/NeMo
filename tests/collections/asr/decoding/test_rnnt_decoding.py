703b0fdd317a192725e3376ddde97050b628b94e
andrusenkoau <andrusenkoau@users.noreply.github.com>
2025-07-23 07:26:15 +0000

    Apply isort and black reformatting
    Signed-off-by: andrusenkoau <andrusenkoau@users.noreply.github.com>

--- tests/collections/asr/decoding/test_rnnt_decoding.py
+++ tests/collections/asr/decoding/test_rnnt_decoding.py
@@ -28,8 +28,8 @@ from nemo.collections.asr.parts.mixins import mixins
 from nemo.collections.asr.parts.submodules import rnnt_beam_decoding
 from nemo.collections.asr.parts.submodules import rnnt_greedy_decoding as greedy_decode
 from nemo.collections.asr.parts.submodules import tdt_beam_decoding
-from nemo.collections.asr.parts.submodules.rnnt_decoding import RNNTBPEDecoding, RNNTDecoding, RNNTDecodingConfig
 from nemo.collections.asr.parts.submodules.ngram_lm import NGramGPULanguageModel
+from nemo.collections.asr.parts.submodules.rnnt_decoding import RNNTBPEDecoding, RNNTDecoding, RNNTDecodingConfig
 from nemo.collections.asr.parts.utils import rnnt_utils
 from nemo.core.utils import numba_utils
 from nemo.core.utils.numba_utils import __NUMBA_MINIMUM_VERSION__
@@ -162,7 +162,11 @@ def check_tdt_greedy_decoding(test_data_dir, use_cuda_graph_decoder: bool, lm_pa
 
     model_config = model.to_config_dict()
 
-    lm_model = NGramGPULanguageModel.from_file(lm_path=lm_path, vocab_size=model.decoder.blank_idx) if lm_path is not None else None
+    lm_model = (
+        NGramGPULanguageModel.from_file(lm_path=lm_path, vocab_size=model.decoder.blank_idx)
+        if lm_path is not None
+        else None
+    )
 
     decoding_algo = greedy_decode.GreedyBatchedTDTInfer(
         model.decoder,
