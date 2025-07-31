2ddbaba41dbadfdbfdf8478b5f1a105e69611f0e
Youngeun Kwon <youngeunk@nvidia.com>
2025-07-31 14:50:07 -0700

    a
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- scripts/performance/helpers.py
+++ scripts/performance/helpers.py
@@ -229,11 +229,11 @@ def set_precision_configs(recipe, compute_dtype: str, fp8_recipe: str | None = N
         recipe.trainer.strategy.ddp.reuse_grad_buf_for_mxfp8_param_ag = True
         recipe.trainer.strategy.ddp.fp8_param_gather = True
         recipe.optim.config.reuse_grad_buf_for_mxfp8_param_ag = True
-        recipe.optim.config.fp8_param_gather = True
         comm_overlap_callback_idx = get_comm_overlap_callback_idx(recipe.trainer.callbacks)
         # TODO: remove this one, after the test
         if comm_overlap_callback_idx is not None:
             recipe.trainer.callbacks[comm_overlap_callback_idx].overlap_param_gather = False
+            # recipe.optim.config.overlap_param_gather = True
         logging.warning(
             "When using MXFP8, to reduce memory usage, we use reuse_grad_buf_for_mxfp8_param_ag. "
             "Disabling AG overlap because it is not supported with reuse_grad_buf_for_mxfp8_param_ag."
