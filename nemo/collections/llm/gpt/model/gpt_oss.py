5e411eac429443c0c4ec379fd9a393fe4ba5d680
Chen Cui <chcui@nvidia.com>
2025-08-04 22:51:38 -0700

    update importer exporter
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- nemo/collections/llm/gpt/model/gpt_oss.py
+++ nemo/collections/llm/gpt/model/gpt_oss.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 import json
 import os
-import re
 from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
@@ -25,7 +24,10 @@ from safetensors import safe_open
 from torch import nn
 import math
 
-from nemo.collections.llm.gpt.model.base import GPTModel, GPTConfig
+from transformers import GenerationConfig
+
+from nemo.collections.common.tokenizers import AutoTokenizer
+from nemo.collections.llm.gpt.model.base import GPTModel, GPTConfig, torch_dtype_from_mcore_config
 from nemo.collections.llm.utils import Config
 from nemo.lightning import OptimizerModule, io, teardown
 from nemo.lightning.io.state import TransformFns, _ModelState
@@ -103,9 +105,7 @@ class GPTOSSModel(GPTModel):
     ):
         super().__init__(config or GPTOSSConfig(), optim=optim, tokenizer=tokenizer, model_transform=model_transform)
 
-
-@io.model_importer(GPTOSSModel, "hf")
-class HFGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
+class _BaseGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
     # pylint: disable=C0115,C0116
     def init(self) -> GPTOSSModel:
         return GPTOSSModel(self.config, tokenizer=self.tokenizer)
@@ -130,7 +130,7 @@ class HFGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
                 try:
                     with safe_open(file_path, framework="pt", device="cpu") as f:
                         for key in f.keys():
-                            if key.endswith("weight.blocks") or key.endswith("weight.scales"):
+                            if key.endswith("blocks") or key.endswith("scales"):
                                 loaded_mxfp4_data[key] = f.get_tensor(key)
                             else:
                                 loaded_bf16_data[key] = f.get_tensor(key)
@@ -154,12 +154,12 @@ class HFGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
         return loaded_bf16_data
 
     def _dequantize_mxfp4(
-        self,
-        blocks: torch.Tensor,
-        scales: torch.Tensor,
-        *,
-        dtype: torch.dtype = torch.bfloat16,
-        rows_per_chunk: int = 32768 * 1024,
+            self,
+            blocks: torch.Tensor,
+            scales: torch.Tensor,
+            *,
+            dtype: torch.dtype = torch.bfloat16,
+            rows_per_chunk: int = 32768 * 1024,
     ) -> torch.Tensor:
         assert blocks.shape[:-1] == scales.shape, (
             f"{blocks.shape=} does not match {scales.shape=}"
@@ -171,7 +171,7 @@ class HFGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
         lut = torch.tensor(FP4_VALUES, dtype=dtype, device=blocks.device)
 
         *prefix_shape, G, B = blocks.shape
-        rows_total   = math.prod(prefix_shape) * G
+        rows_total = math.prod(prefix_shape) * G
 
         blocks = blocks.reshape(rows_total, B)
         scales = scales.reshape(rows_total, 1)
@@ -197,20 +197,106 @@ class HFGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
 
         return out.reshape(*prefix_shape, G, B * 2).view(*prefix_shape, G * B * 2)
 
+    @cached_property
+    def config(self) -> GPTOSSConfig:
+        with open(self / "config.json") as f:
+            ckpt_config = json.load(f)
+        generation_config = GenerationConfig.from_pretrained(str(self))
+        try:
+            num_moe_experts = ckpt_config['num_experts']
+        except KeyError:
+            num_moe_experts = ckpt_config['num_local_experts']
+        return GPTOSSConfig(
+            num_layers=ckpt_config['num_hidden_layers'],
+            num_moe_experts=num_moe_experts,
+            bf16=True,
+            params_dtype=torch.bfloat16,
+            generation_config=generation_config,
+        )
+
+@io.model_importer(GPTOSSModel, "hf")
+class HFGPTOSSImporter(_BaseGPTOSSImporter):
     def apply(self, output_path: Path) -> Path:
-        logging.setLevel(logging.DEBUG)
+        # logging.setLevel(logging.DEBUG)
         source_state = self.hf_ckpt_load()
 
-        if self.config.num_layers == 2:
-            # TEMP only keep first two layers
-            to_pop = []
-            for k, v in source_state.items():
-                if re.match(r"block\..*", k) and not re.match(r"block\.[0-1]\..*", k):
-                    to_pop.append(k)
-            [source_state.pop(k) for k in to_pop]
+        source = _ModelState(source_state)
+        target = self.init()
+        trainer = self.nemo_setup(target)
+        self.convert_state(source, target)
+        self.nemo_save(output_path, trainer)
+
+        print(f"Converted GPT-OSS model to Nemo, model saved to {output_path}")
+
+        teardown(trainer, target)
+        del trainer, target
+
+        return output_path
+
+    def convert_state(self, source, target):
+        mapping = {
+            "model.embed_tokens.weight": "embedding.word_embeddings.weight",
+            "model.norm.weight": "decoder.final_layernorm.weight",
+            "lm_head.weight": "output_layer.weight",
+            "**.input_layernorm.weight": "**.self_attention.linear_qkv.layer_norm_weight",
+            "**.self_attn.o_proj.bias": "**.self_attention.linear_proj.bias",
+            "**.self_attn.o_proj.weight": "**.self_attention.linear_proj.weight",
+            "**.self_attn.sinks": "**.self_attention.core_attention.softmax_offset",
+            "**.post_attention_layernorm.weight": "**.pre_mlp_layernorm.weight",
+            "**.mlp.router.bias": "**.mlp.router.bias",
+            "**.mlp.router.weight": "**.mlp.router.weight",
+        }
+
+        transforms = [
+            io.state_transform(
+                source_key=(
+                    "**.self_attn.q_proj.bias",
+                    "**.self_attn.k_proj.bias",
+                    "**.self_attn.v_proj.bias",
+                ),
+                target_key="**.self_attention.linear_qkv.bias",
+                fn=TransformFns.merge_qkv_bias,
+            ),
+            io.state_transform(
+                source_key=(
+                    "**.self_attn.q_proj.weight",
+                    "**.self_attn.k_proj.weight",
+                    "**.self_attn.v_proj.weight",
+                ),
+                target_key="**.self_attention.linear_qkv.weight",
+                fn=TransformFns.merge_qkv,
+            ),
+        ]
+
+        # moe names for TEGroupedMLP
+        for source_key, target_key in (
+            ("**.mlp.experts.gate_up_proj_bias", "**.mlp.experts.linear_fc1.bias*"),
+            ("**.mlp.experts.gate_up_proj_blocks", "**.mlp.experts.linear_fc1.weight*"),
+        ):
+            transforms.append(io.state_transform(source_key, target_key, lambda x: [interleave(e) for e in [*x]]))
+
+        for source_key, target_key in (
+            ("**.mlp.experts.down_proj_bias", "**.mlp.experts.linear_fc2.bias*"),
+            ("**.mlp.experts.down_proj_blocks", "**.mlp.experts.linear_fc2.weight*"),
+        ):
+            transforms.append(io.state_transform(source_key, target_key, lambda x: [*x]))
+
+        io.apply_transforms(source, target, mapping=mapping, transforms=transforms, cast_dtype=torch.bfloat16)
+        return target
+
+    @cached_property
+    def tokenizer(self) -> "AutoTokenizer":
+        from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTokenizer
+
+        return AutoTokenizer(self.save_hf_tokenizer_assets(str(self)), trust_remote_code=True)
+
+
+@io.model_importer(GPTOSSModel, "openai")
+class OpenAIGPTOSSImporter(_BaseGPTOSSImporter):
+    def apply(self, output_path: Path) -> Path:
+        source_state = self.hf_ckpt_load()
 
         source = _ModelState(source_state)
-        # breakpoint()
         target = self.init()
         trainer = self.nemo_setup(target)
         self.convert_state(source, target)
@@ -251,9 +337,6 @@ class HFGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
         ]
 
         # moe names for TEGroupedMLP
-        def interleave(elem):
-            return torch.cat((elem[::2, ...], elem[1::2, ...]), dim=0)
-
         for source_key, target_key in (
             ("**.mlp.mlp1_weight", "**.mlp.experts.linear_fc1.weight*"),
             ("**.mlp.mlp1_bias", "**.mlp.experts.linear_fc1.bias*"),
@@ -274,149 +357,115 @@ class HFGPTOSSImporter(io.ModelConnector["AutoModelForCausalLM", GPTOSSModel]):
 
         return TiktokenTokenizer(encoding_name="o200k_base")
 
-    @cached_property
-    def config(self) -> GPTOSSConfig:
-        with open(self / "config.json") as f:
-            ckpt_config = json.load(f)
-        return GPTOSSConfig(
-            num_layers=ckpt_config['num_hidden_layers'],
-            num_moe_experts=ckpt_config['num_experts'],
+
+
+def interleave(elem):
+    return torch.cat((elem[::2, ...], elem[1::2, ...]), dim=0)
+
+def uninterleave(elem):
+    gate, up = torch.chunk(elem, 2, dim=0)
+    output = torch.empty_like(elem)
+    output[::2, ...] = gate
+    output[1::2, ...] = up
+    return output
+
+@io.model_exporter(GPTOSSModel, "hf")
+class HFGPTOSSExporter(io.ModelConnector[GPTOSSModel, "AutoModelForCausalLM"]):
+    # pylint: disable=C0115,C0116
+    def init(self, dtype=torch.bfloat16) -> "AutoModelForCausalLM":
+        from transformers import AutoModelForCausalLM
+        from transformers.modeling_utils import no_init_weights
+
+        with no_init_weights():
+            return AutoModelForCausalLM.from_config(self.config, trust_remote_code=True, torch_dtype=dtype)
+
+    def apply(self, output_path: Path) -> Path:
+        source, _ = self.nemo_load(str(self))
+        target = self.init(torch_dtype_from_mcore_config(source.config))
+        target = self.convert_state(source, target)
+
+        target = target.cpu()
+        target.save_pretrained(output_path)
+        self.tokenizer.save_pretrained(output_path)
+
+        return output_path
+
+    def convert_state(self, source, target):
+        mapping = {
+            "embedding.word_embeddings.weight": "model.embed_tokens.weight",
+            "decoder.final_layernorm.weight": "model.norm.weight",
+            "output_layer.weight": "lm_head.weight",
+            "**.self_attention.linear_qkv.layer_norm_weight": "**.input_layernorm.weight",
+            "**.self_attention.linear_proj.bias": "**.self_attn.o_proj.bias",
+            "**.self_attention.linear_proj.weight": "**.self_attn.o_proj.weight",
+            "**.self_attention.core_attention.softmax_offset": "**.self_attn.sinks",
+            "**.pre_mlp_layernorm.weight": "**.post_attention_layernorm.weight",
+            "**.mlp.router.bias": "**.mlp.router.bias",
+            "**.mlp.router.weight": "**.mlp.router.weight",
+        }
+        transforms = [
+            io.state_transform(
+                source_key="**.self_attention.linear_qkv.bias",
+                target_key=(
+                    "**.self_attn.q_proj.bias",
+                    "**.self_attn.k_proj.bias",
+                    "**.self_attn.v_proj.bias",
+                ),
+                fn=TransformFns.split_qkv_bias,
+            ),
+            io.state_transform(
+                source_key="**.self_attention.linear_qkv.weight",
+                target_key=(
+                    "**.self_attn.q_proj.weight",
+                    "**.self_attn.k_proj.weight",
+                    "**.self_attn.v_proj.weight",
+                ),
+                fn=TransformFns.split_qkv,
+            ),
+        ]
+        def stack_experts(*args):
+            t = torch.stack(args)
+            if len(t.shape) == 3:
+                t = t.transpose(-1, -2)
+            return t
+
+        def stack_uninterleave_experts(*args):
+            t = torch.stack([uninterleave(e) for e in args])
+            if len(t.shape) == 3:
+                t = t.transpose(-1, -2)
+            return t
+
+        for source_key, target_key in (
+            ("**.mlp.experts.linear_fc2.bias*", "**.mlp.experts.down_proj_bias"),
+            ("**.mlp.experts.linear_fc2.weight*", "**.mlp.experts.down_proj"),
+        ):
+            transforms.append(io.state_transform(source_key, target_key, stack_experts))
+        for source_key, target_key in (
+            ("**.mlp.experts.linear_fc1.bias*", "**.mlp.experts.gate_up_proj_bias"),
+            ("**.mlp.experts.linear_fc1.weight*", "**.mlp.experts.gate_up_proj"),
+        ):
+            transforms.append(io.state_transform(source_key, target_key, stack_uninterleave_experts))
+
+        return io.apply_transforms(
+            source,
+            target,
+            mapping=mapping,
+            transforms=transforms,
         )
 
+    @cached_property
+    def tokenizer(self):
+        return io.load_context(str(self), subpath="model.tokenizer")
 
-# @io.model_exporter(Qwen3Model, "hf")
-# class HFQwen3Exporter(io.ModelConnector[Qwen3Model, "AutoModelForCausalLM"]):
-#     # pylint: disable=C0115,C0116
-#     def init(self, dtype=torch.bfloat16) -> "AutoModelForCausalLM":
-#         from transformers import AutoModelForCausalLM
-#         from transformers.modeling_utils import no_init_weights
-#
-#         with no_init_weights():
-#             return AutoModelForCausalLM.from_config(self.config, trust_remote_code=True, torch_dtype=dtype)
-#
-#     def apply(self, output_path: Path) -> Path:
-#         source, _ = self.nemo_load(str(self))
-#         target = self.init(torch_dtype_from_mcore_config(source.config))
-#         target = self.convert_state(source, target)
-#
-#         target = target.cpu()
-#         target.save_pretrained(output_path)
-#         self.tokenizer.save_pretrained(output_path)
-#
-#         return output_path
-#
-#     def convert_state(self, source, target):
-#         mapping = {
-#             "**.self_attention.linear_proj.weight": "**.self_attn.o_proj.weight",
-#             "**.self_attention.linear_qkv.layer_norm_weight": "**.input_layernorm.weight",
-#             "**.self_attention.q_layernorm.weight": "**.self_attn.q_norm.weight",
-#             "**.self_attention.k_layernorm.weight": "**.self_attn.k_norm.weight",
-#             "decoder.final_layernorm.weight": "model.norm.weight",
-#         }
-#         is_moe = getattr(self.config, "num_experts", 0) > 0
-#         if is_moe:
-#             mapping.update(
-#                 {
-#                     "**.mlp.experts.linear_fc2.weight*": "**.mlp.experts.*.down_proj.weight",
-#                     "**.mlp.router.weight": "**.mlp.gate.weight",
-#                     "**.pre_mlp_layernorm.weight": "**.post_attention_layernorm.weight",
-#                 }
-#             )
-#         else:
-#             mapping.update(
-#                 {
-#                     "**.mlp.linear_fc2.weight": "**.mlp.down_proj.weight",
-#                     "**.mlp.linear_fc1.layer_norm_weight": "**.post_attention_layernorm.weight",
-#                 }
-#             )
-#         transforms = [
-#             io.state_transform(
-#                 source_key="**.self_attention.linear_qkv.weight",
-#                 target_key=(
-#                     "**.self_attn.q_proj.weight",
-#                     "**.self_attn.k_proj.weight",
-#                     "**.self_attn.v_proj.weight",
-#                 ),
-#                 fn=TransformFns.split_qkv,
-#             ),
-#             (
-#                 io.state_transform(
-#                     source_key="**.mlp.linear_fc1.weight",
-#                     target_key=("**.mlp.gate_proj.weight", "**.mlp.up_proj.weight"),
-#                     fn=TransformFns.split_fc1,
-#                 )
-#                 if not is_moe
-#                 else io.state_transform(
-#                     source_key="**.mlp.experts.linear_fc1.weight*",
-#                     target_key=("**.mlp.experts.*.gate_proj.weight", "**.mlp.experts.*.up_proj.weight"),
-#                     fn=TransformFns.split_fc1,
-#                 )
-#             ),
-#             io.state_transform(
-#                 source_key="embedding.word_embeddings.weight",
-#                 target_key="model.embed_tokens.weight",
-#                 fn=TransformFns.prune_padding,
-#             ),
-#         ]
-#         if not self.config.tie_word_embeddings:
-#             transforms.append(
-#                 io.state_transform(
-#                     source_key="output_layer.weight",
-#                     target_key="lm_head.weight",
-#                     fn=TransformFns.prune_padding,
-#                 )
-#             )
-#
-#         return io.apply_transforms(
-#             source,
-#             target,
-#             mapping=mapping,
-#             transforms=transforms,
-#         )
-#
-#     @property
-#     def tokenizer(self):
-#         return io.load_context(str(self)).model.tokenizer.tokenizer
-#
-#     @property
-#     def config(self) -> "HFQwen3Config":
-#         from transformers import Qwen3Config as HFQwen3Config
-#         from transformers import Qwen3MoeConfig as HFQwen3MoeConfig
-#
-#         source: Qwen3Config = io.load_context(str(self), subpath="model.config")
-#         is_moe = source.num_moe_experts is not None
-#         hf_config_cls = (
-#             partial(
-#                 HFQwen3MoeConfig,
-#                 moe_intermediate_size=source.moe_ffn_hidden_size,
-#                 num_experts=source.num_moe_experts,
-#                 num_experts_per_tok=source.moe_router_topk,
-#                 router_aux_loss_coef=source.moe_aux_loss_coeff,
-#                 norm_topk_prob=True,
-#             )
-#             if is_moe
-#             else HFQwen3Config
-#         )
-#
-#         return hf_config_cls(
-#             architectures=["Qwen3ForCausalLM"],
-#             num_hidden_layers=source.num_layers,
-#             hidden_size=source.hidden_size,
-#             intermediate_size=source.ffn_hidden_size,
-#             num_attention_heads=source.num_attention_heads,
-#             head_dim=source.kv_channels,
-#             max_position_embeddings=source.max_position_embeddings,
-#             initializer_range=source.init_method_std,
-#             rms_norm_eps=source.layernorm_epsilon,
-#             num_key_value_heads=source.num_query_groups,
-#             rope_theta=source.rotary_base,
-#             vocab_size=getattr(source, 'vocab_size', self.tokenizer.vocab_size),
-#             sliding_window=None,
-#             tie_word_embeddings=source.share_embeddings_and_output_weights,
-#             max_window_layers=source.num_layers,
-#             bos_token_id=151643,
-#             eos_token_id=151645,
-#         )
+    @cached_property
+    def config(self) -> "HFGptOssConfig":
+        from transformers import GptOssConfig as HFGptOssConfig
+
+        source: GPTOSSConfig = io.load_context(str(self), subpath="model.config")
+        return HFGptOssConfig(
+            num_hidden_layers=source.num_layers,
+            num_local_experts=source.num_moe_experts,
+        )
 
 
 __all__ = [
