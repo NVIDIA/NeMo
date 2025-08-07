cc08b4c626c3ef4c160834f595955b5d3d1dcbeb
Jason <jasoli@nvidia.com>
2025-08-07 09:52:59 -0700

    disable broken tests on dev branch
    Signed-off-by: Jason <jasoli@nvidia.com>

--- tests/collections/common/test_lhotse_dataloading.py
+++ tests/collections/common/test_lhotse_dataloading.py
@@ -344,7 +344,7 @@ def test_dataloader_from_lhotse_cuts_cut_into_windows(cutset_path: Path):
     assert batches[4]["audio"].shape == (4, 8000)
     # exactly 20 cuts were used because we cut 10x 1s cuts into 20x 0.5s cuts
 
-
+@pytest.mark.pleasefixme
 def test_dataloader_from_lhotse_cuts_pad_min_duration(cutset_path: Path):
     config = OmegaConf.create(
         {
@@ -1938,7 +1938,7 @@ def test_multimodal_text_audio_dataloading_randomized_round_robin_strategy(
         assert torch.is_tensor(ex.answer_ids)
         assert torch.is_tensor(ex.mask)
 
-
+@pytest.mark.pleasefixme
 def test_dataloader_with_noise_nemo_json(cutset_path: Path, nemo_manifest_path: Path):
     config = OmegaConf.create(
         {
@@ -1996,7 +1996,7 @@ def test_dataloader_with_noise_nemo_json(cutset_path: Path, nemo_manifest_path: 
     assert isinstance(cut, MixedCut)
     assert -5.0 < cut.tracks[1].snr < 5.0
 
-
+@pytest.mark.pleasefixme
 def test_dataloader_with_noise_lhotse_jsonl(cutset_path: Path):
     config = OmegaConf.create(
         {
@@ -2025,7 +2025,7 @@ def test_dataloader_with_noise_lhotse_jsonl(cutset_path: Path):
     assert isinstance(cut, MixedCut)
     assert -5.0 < cut.tracks[1].snr < 5.0
 
-
+@pytest.mark.pleasefixme
 def test_dataloader_with_noise_nemo_tar(cutset_path: Path, nemo_tarred_manifest_path_multi: Path):
     noise_json, noise_tar = nemo_tarred_manifest_path_multi
     config = OmegaConf.create(
