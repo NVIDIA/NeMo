e9d0b04bea05bc846246dc34ebe8154a8e6806e6
nasretdinovr <uniform97@gmail.com>
2025-08-08 19:00:08 +0400

    removed unused imports
    Signed-off-by: nasretdinovr <uniform97@gmail.com>

--- nemo/collections/audio/parts/submodules/conformer_unet.py
+++ nemo/collections/audio/parts/submodules/conformer_unet.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import random
-from typing import Dict, List, Optional, Tuple
+from typing import Dict
 
 import einops
 import torch
