5f52487c5f2e9c00ffef46a5254e47b2955c96fe
bdubauski <80418713+bdubauski@users.noreply.github.com>
2025-08-12 14:47:27 -0700

    Make VBoost activation conditional (#14458)
    Signed-off-by: Guyue Huang <guyueh@nvidia.com>

--- scripts/performance/llm/pretrain_nemotronh_56b.py
+++ scripts/performance/llm/pretrain_nemotronh_56b.py
@@ -18,11 +18,17 @@ import nemo_run as run
 
 from nemo.collections.llm.recipes.nemotronh_56b import pretrain_recipe
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
-from nemo.lightning.run.plugins import NsysPlugin, PerfEnvPlugin
+from nemo.lightning.run.plugins import NsysPlugin
 
 from ..argument_parser import parse_cli_args
 from ..executors import slurm_executor
-from ..helpers import args_sanity_check, get_user_configs, set_exp_logging_configs, set_primary_perf_configs
+from ..helpers import (
+    args_sanity_check,
+    build_perf_env_plugin,
+    get_user_configs,
+    set_exp_logging_configs,
+    set_primary_perf_configs,
+)
 from ..utils import dump_config_diff_from_base_recipe, hf_tokenizer
 
 
@@ -142,13 +148,8 @@ if __name__ == "__main__":
         wandb_key=args.wandb_key,
     )
 
-    plugins = [
-        PerfEnvPlugin(
-            enable_vboost=True,
-            nccl_pp_comm_chunksize=2097152 if pp_size > 1 else None,
-            gpu_sm100_or_newer=(args.gpu.lower() in ['b200', 'gb200']),
-        )
-    ]
+    plugins = [build_perf_env_plugin(args, pp_size=pp_size)]
+
     if args.enable_nsys:
         plugins.append(NsysPlugin(start_step=5, end_step=6))
 
