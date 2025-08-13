ca1bda9754e799414881fd1bdacfc8849fcd757d
Vasudevan Rengasamy <vrengasamy@nvidia.com>
2025-08-13 12:39:25 -0700

    Mcore MR 3808 moves FullCG to megatron.core
    Signed-off-by: Vasudevan Rengasamy <vrengasamy@nvidia.com>

--- nemo/lightning/megatron_parallel.py
+++ nemo/lightning/megatron_parallel.py
@@ -54,7 +54,7 @@ from megatron.core.distributed import DistributedDataParallel as McoreDDP
 from megatron.core.distributed import DistributedDataParallelConfig
 from megatron.core.optimizer import OptimizerConfig
 from megatron.core.transformer.transformer_config import TransformerConfig
-from megatron.training.full_cuda_graph import FullCudaGraphWrapper
+from megatron.core.full_cuda_graph import FullCudaGraphWrapper
 from torch import Tensor, nn
 from typing_extensions import override
 
