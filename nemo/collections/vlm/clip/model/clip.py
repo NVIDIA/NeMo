21fd267f77ef342e1dac9e8356c9088801fd7374
srkpmail <srkpmail@users.noreply.github.com>
2025-08-17 07:43:54 +0000

    Apply isort and black reformatting
    Signed-off-by: srkpmail <srkpmail@users.noreply.github.com>

--- nemo/collections/vlm/clip/model/clip.py
+++ nemo/collections/vlm/clip/model/clip.py
@@ -195,12 +195,14 @@ class CLIPConfigL14(CLIPConfig):
     text_transformer_config: CLIPTextModelConfig = field(default_factory=lambda: CLIPTextModelL_14_224_Config())
     vision_transformer_config: CLIPViTConfig = field(default_factory=lambda: CLIPViTL_14_224_Config())
 
+
 @dataclass
 class CLIPConfigL14_336(CLIPConfig):
     """Main Clip config for Large model 336"""
 
     text_transformer_config: CLIPTextModelConfig = field(default_factory=lambda: CLIPTextModelL_14_336_Config())
     vision_transformer_config: CLIPViTConfig = field(default_factory=lambda: CLIPViTL_14_336_Config())
+
 
 @dataclass
 class CLIPConfigB32(CLIPConfig):
