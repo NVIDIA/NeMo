ca635e674dd569ad393bf910b32d434ea8e86c89
prekshivyas <prekhsivyas@gmail.com>
2025-08-17 17:43:53 -0700

    debug env vars
    Signed-off-by: prekshivyas <prekhsivyas@gmail.com>

--- nemo/collections/llm/tools/autotuner/core/pretraining.py
+++ nemo/collections/llm/tools/autotuner/core/pretraining.py
@@ -70,6 +70,8 @@ def lepton_executor(
     if wandb_api_key:
         default_env_vars["WANDB_API_KEY"] = wandb_api_key
 
+    print(default_env_vars)
+    
     return run.LeptonExecutor(
         resource_shape=resource_shape,
         container_image=container_image,
