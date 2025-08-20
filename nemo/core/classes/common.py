07ad6a7eea279d22f20ca5b9d95526b20d85a6a2
Kunal Dhawan <kunaldhawan97@gmail.com>
2025-08-20 16:19:39 -0700

    Fix circular import in _is_target_allowed by importing ModelPT directly from nemo.core.classes.modelPT
    Signed-off-by: Kunal Dhawan <kunaldhawan97@gmail.com>

--- nemo/core/classes/common.py
+++ nemo/core/classes/common.py
@@ -44,6 +44,7 @@ from nemo.core.classes.mixins.hf_io_mixin import HuggingFaceFileIO
 from nemo.core.config.templates.model_card import NEMO_DEFAULT_MODEL_CARD_TEMPLATE
 from nemo.core.connectors.save_restore_connector import SaveRestoreConnector
 from nemo.core.neural_types import NeuralType, NeuralTypeComparisonResult
+from nemo.core.classes.modelPT import ModelPT
 from nemo.utils import logging
 from nemo.utils.cloud import maybe_download_from_cloud
 from nemo.utils.data_utils import resolve_cache_dir
@@ -76,7 +77,7 @@ ALLOWED_TARGET_PREFIXES = [
 ]
 
 
-SAFE_BASES = (torch.nn.Module, nemo.core.ModelPT)
+SAFE_BASES = (torch.nn.Module, ModelPT)
 
 
 def _is_target_allowed(target: str) -> bool:
