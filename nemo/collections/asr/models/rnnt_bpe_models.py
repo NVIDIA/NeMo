9f6dd0d2b85168389defcafbe1d2c55ec41a43af
Roman Korostik <rkorostik@nvidia.com>
2025-08-26 09:33:34 -0700

    make EncDecRNNTBPEModel use lhotse dataloader when transcribing
    Signed-off-by: Roman Korostik <rkorostik@nvidia.com>

--- nemo/collections/asr/models/rnnt_bpe_models.py
+++ nemo/collections/asr/models/rnnt_bpe_models.py
@@ -599,6 +599,7 @@ class EncDecRNNTBPEModel(EncDecRNNTModel, ASRBPEMixin):
             batch_size = min(config['batch_size'], len(config['paths2audio_files']))
 
         dl_config = {
+            'use_lhotse': True,
             'manifest_filepath': manifest_filepath,
             'sample_rate': self.preprocessor._sample_rate,
             'batch_size': batch_size,
