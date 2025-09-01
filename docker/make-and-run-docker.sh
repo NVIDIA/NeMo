5bad718d92229ff24e5be3412620220f724e75b7
Jaedeok Kim <jaedeokk@nvidia.com>
2025-09-01 12:38:17 +0900

    Add user dockerfile and launch script

---
+++ docker/make-and-run-docker.sh
@@ -0,0 +1,34 @@
+# Usage (at the root of the repository):
+#  $ export MODEL_DIR=/path/to/my/models
+#  $ export DATASET_DIR=/path/to/my/datasets
+#  $ ./docker/make-and-run-docker.sh
+
+
+BASE_IMAGE_WITH_TAG=nvcr.io/nvidia/nemo:25.07
+
+# Build the base image
+# docker build \
+#   -t ${BASE_IMAGE_WITH_TAG} \
+#   -f docker/Dockerfile .
+
+# Build the user image
+docker build \
+  --build-arg BASE_IMAGE_WITH_TAG=${BASE_IMAGE_WITH_TAG} \
+  --build-arg USER_ID=$(id -u) \
+  --build-arg USER_NAME=$(id -un) \
+  --build-arg GROUP_ID=$(id -g) \
+  --build-arg GROUP_NAME=$(id -gn) \
+  -t ${BASE_IMAGE_WITH_TAG}-$(whoami) \
+  -f docker/Dockerfile.user .
+
+# Run the container
+docker run --rm -it --gpus all \
+  --name nemo-$(whoami) --net=host \
+  --security-opt seccomp=unconfined --cap-add SYS_PTRACE \
+  --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+  -e HF_DATASETS_CACHE=/datasets/.cache/hf \
+  -v "$(pwd)":/workspace \
+  -v "${MODEL_DIR}":/models \
+  -v "${DATASET_DIR}":/datasets \
+  ${BASE_IMAGE_WITH_TAG}-$(whoami) \
+  bash
