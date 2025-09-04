29dc3badf898ed3ca5cc556f95c7a00504963e61
vipnydav <vipinydv@google.com>
2025-09-04 14:20:56 +0000

    remove imported Path from test_file.py
    Signed-off-by: vipnydav <vipinydv@google.com>

--- tests/lightning/_io/artifacts/test_file.py
+++ tests/lightning/_io/artifacts/test_file.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pathlib import Path
 from unittest.mock import patch
 
 import pytest
