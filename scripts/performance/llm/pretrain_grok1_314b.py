b034b922206c478ef5e9ca506808124822b13b41
Rahul Salagame <rsalagame@nvidia.com>
2025-09-09 13:15:14 -0700

    workaround for cuda graphs error on gb200/gb300 chips.

--- scripts/performance/llm/pretrain_grok1_314b.py
+++ scripts/performance/llm/pretrain_grok1_314b.py
@@ -452,6 +452,10 @@ if __name__ == "__main__":
         network='sharp' if args.use_sharp else None,
     )
 
+
+    if enable_cuda_graphs and "PYTORCH_CUDA_ALLOC_CONF" in executor.env_vars:
+        del executor.env_vars["PYTORCH_CUDA_ALLOC_CONF"]
+
     with run.Experiment(exp_name) as exp:
         exp.add(
             recipe,
