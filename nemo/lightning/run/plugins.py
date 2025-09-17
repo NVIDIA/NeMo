a94349436a28fc7dbf9625f6271a1d05164423f7
malay-nagda <malayn@nvidia.com>
2025-09-17 11:04:12 +0530

    remove env var (#14739)
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- nemo/lightning/run/plugins.py
+++ nemo/lightning/run/plugins.py
@@ -429,6 +429,9 @@ class PerfEnvPlugin(run.Plugin):
                         pytorch_cuda_alloc_conf.remove("expandable_segments:True")
                         executor.env_vars["PYTORCH_CUDA_ALLOC_CONF"] = ",".join(pytorch_cuda_alloc_conf)
 
+            if task.model.config.enable_cuda_graph and "PYTORCH_CUDA_ALLOC_CONF" in executor.env_vars:
+                del executor.env_vars["PYTORCH_CUDA_ALLOC_CONF"]
+
         # Improve perf by steering power to tensor cores, may not work on all systems
         if self.enable_vboost and isinstance(executor, run.SlurmExecutor):
             vboost_cmd = self.get_vboost_srun_cmd(executor.nodes, executor.tunnel.job_dir)
