350ec2d1551d614808076178a2786a9d4a1e0d3c
Prathamesh Kalamkar <prathamk@thoughtworks.com>
2025-09-17 06:47:16 +0530

    imported get_moe_layer_wise_logging_tracker from megatron core moe_utils (#14694)
    * imported get_moe_layer_wise_logging_tracker from megatron core moe_utils
    
    Signed-off-by: Prathamesh Kalamkar <prathamk@thoughtworks.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: prathamk-tw <prathamk-tw@users.noreply.github.com>
    
    * moved import to the top
    
    * Apply isort and black reformatting
    
    Signed-off-by: prathamk-tw <prathamk-tw@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Prathamesh Kalamkar <prathamk@thoughtworks.com>
    Signed-off-by: prathamk-tw <prathamk-tw@users.noreply.github.com>
    Co-authored-by: prathamk-tw <prathamk-tw@users.noreply.github.com>

--- nemo/lightning/megatron_parallel.py
+++ nemo/lightning/megatron_parallel.py
@@ -54,6 +54,7 @@ from megatron.core import parallel_state
 from megatron.core.distributed import DistributedDataParallel as McoreDDP
 from megatron.core.distributed import DistributedDataParallelConfig
 from megatron.core.optimizer import OptimizerConfig
+from megatron.core.transformer.moe.moe_utils import get_moe_layer_wise_logging_tracker
 from megatron.core.transformer.transformer_config import TransformerConfig
 from torch import Tensor, nn
 from typing_extensions import override
@@ -1889,7 +1890,8 @@ def moe_loss_tracker_ctx():
 @torch.no_grad()
 def aggregate_moe_loss_stats(loss_scale=1.0):
     with moe_loss_tracker_ctx():
-        tracker = parallel_state.get_moe_layer_wise_logging_tracker()
+
+        tracker = get_moe_layer_wise_logging_tracker()
         aux_losses = {k: v['values'].float() * loss_scale for k, v in tracker.items()}
         total_loss_dict = {}
         for name, loss_list in aux_losses.items():
